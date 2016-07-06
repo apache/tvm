5dc70763e173e71b2e52feea7240bd57def347cb
tqchen <tianqi.tchen@gmail.com>
2016-07-05 18:35:25 -0700

    checkin dfs visit from min

--- nnvm/include/nngraph/graph.h
+++ nnvm/include/nngraph/graph.h
@@ -8,7 +8,10 @@
 
 #include <vector>
 #include <string>
+#include <utility>
+#include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 #include "./node.h"
 
 namespace nngraph {
@@ -22,7 +25,74 @@ class Graph {
   std::vector<NodeEntry> outputs;
   /*! \brief attributes of a graph */
   std::unordered_map<std::string, any> attrs;
+
+  /*!
+   * \brief perform a Post Order DFS visit to each node in the graph.
+   *  This order is deterministic and is also topoligical sorted.
+   * \param fvisit a function of type std::function<void(const std::shared_ptr<Node>&)>
+   * \tparam FVisit The function type to perform the visit.
+   */
+  template<typename FVisit>
+  inline void DFSVisit(FVisit fvisit) const;
 };
+
+// inline function implementations
+template <typename GNode, typename HashType,
+           typename FVisit, typename HashFunc,
+          typename InDegree, typename GetInput>
+void PostOrderDFSVisit(const std::vector<GNode>& heads,
+                       FVisit fvisit,
+                       HashFunc hash,
+                       InDegree indegree,
+                       GetInput getinput) {
+  std::vector<std::pair<GNode, uint32_t> > stack;
+  std::unordered_set<HashType> visited;
+  for (auto& head : heads) {
+    HashType head_hash = hash(head);
+    if (visited.count(head_hash) == 0) {
+      stack.push_back(std::make_pair(head, 0));
+      visited.insert(head_hash);
+    }
+    while (!stack.empty()) {
+      std::pair<GNode, uint32_t>& back = stack.back();
+      if (back.second == indegree(back.first)) {
+        fvisit(back.first);
+        stack.pop_back();
+      } else {
+        const GNode& input = getinput(back.first, back.second++);
+        HashType input_hash = hash(input);
+        if (visited.count(input_hash) == 0) {
+          stack.push_back(std::make_pair(input, 0));
+          visited.insert(input_hash);
+        }
+      }
+    }
+  }
+}
+
+template<typename FVisit>
+inline void Graph::DFSVisit(FVisit fvisit) const {
+  typedef const std::shared_ptr<Node>* GNode;
+  std::vector<GNode> head_nodes(outputs.size());
+  std::transform(outputs.begin(), outputs.end(), head_nodes.begin(),
+                 [](const NodeEntry& e)->GNode {
+                   return &e.node;
+                 });
+  PostOrderDFSVisit<GNode, Node*>(
+      head_nodes,
+      [fvisit](GNode n) { fvisit(*n); },  // FVisit
+      [](GNode n)->Node* { return n->get(); },  // HashFunc
+      [](GNode n)->uint32_t {  // InDegree
+        return (*n)->inputs.size() + (*n)->control_deps.size();
+      },
+      [](GNode n, uint32_t index)->GNode {  // GetInput
+        if (index < (*n)->inputs.size()) {
+          return &(*n)->inputs.at(index).node;
+        } else {
+          return &(*n)->control_deps.at(index - (*n)->inputs.size());
+        }
+      });
+}
 
 }  // namespace nngraph
 
