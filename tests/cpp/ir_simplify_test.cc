69a80ccee729247edf71556dfb8be5b558a37d76
Haichen Shen <shenhaichen@gmail.com>
2017-01-13 15:09:37 -0800

    add simplify test (#12)

---
+++ tests/cpp/ir_simplify_test.cc
@@ -0,0 +1,15 @@
+#include <dmlc/logging.h>
+#include <gtest/gtest.h>
+#include <tvm/tvm.h>
+#include <pass/Simplify.h>
+
+TEST(IRSIMPLIFY, Basic) {
+  using namespace Halide::Internal;
+  simplify_test();
+}
+
+int main(int argc, char ** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
+  return RUN_ALL_TESTS();
+}
