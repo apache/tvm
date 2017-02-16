71349f61d8202e57a6f8dd50d321ea39bc55b227
ZihengJiang <jzhtomas@gmail.com>
2017-02-17 07:54:58 +0800

    add checker for BoundDeducer

--- python/tvm/arith.py
+++ python/tvm/arith.py
@@ -6,17 +6,22 @@ from . import _api_internal
 
 @register_node
 class IntSet(NodeBase):
+    """Represent a set of integer in one dimension."""
     pass
 
 @register_node
 class IntervalSet(IntSet):
+    """Represent set of continuous interval"""
     def min(self):
+        """get the minimum value"""
         return _api_internal._IntervalSetGetMin(self)
 
     def max(self):
+        """get the maximum value"""
         return _api_internal._IntervalSetGetMax(self)
 
 @register_node
 class StrideSet(IntSet):
+    """Represent set of strided integers"""
     pass
 
--- src/arithmetic/bound_deducer.cc
+++ src/arithmetic/bound_deducer.cc
@@ -62,25 +62,17 @@ std::vector<const Node*> GetPath(Var target, Expr expr) {
   return v.success ? v.path_ : std::vector<const Node*>();
 }
 
+class Checker;
+
 // a visitor to deduce the bound of a variable from a expression
 class BoundDeducer: public IRVisitor {
  public:
+  friend class Checker;
   BoundDeducer(Var target, Expr expr,
                const std::unordered_map<const Variable*, IntSet>& dom_map)
-  : target_(target), expr_(expr), dom_map_(dom_map) {
-    // get the path
-    path_ = GetPath(target, expr);
-    if (path_.empty()) {
-      success = false;
-      return;
-    }
-    iter_ = 0;
-    result = make_zero(expr.type());
-    // get the sign of every subexpr
-    expr_map_ = EvalSetForEachSubExpr(expr, dom_map);
+  : target_(target), expr_(expr), dom_map_(dom_map) {}
 
-    Visit(expr);
-  }
+  void Deduce();
 
   void Visit(const NodeRef& e) final {
     if (!success) return;
@@ -132,7 +124,7 @@ class BoundDeducer: public IRVisitor {
       result += op->b;
     } else {
       result -= op->a;
-      result = -1 * result;
+      result = - result;
       is_greater = !is_greater;
     }
     Visit(left ? op->a : op->b);
@@ -158,11 +150,7 @@ class BoundDeducer: public IRVisitor {
     }
 
     // always use relax bound
-    if (is_greater) {
-      result = result / operand + 1;
-    } else {
-      result = result / operand - 1;
-    }
+    result = result / operand + (is_greater ? 1 : -1);
     Visit(left ? op->a : op->b);
   }
 
@@ -175,10 +163,68 @@ class BoundDeducer: public IRVisitor {
   Var  target_;
   Expr expr_;
   const std::unordered_map<const Variable*, IntSet>& dom_map_;
-  std::vector<const Node*> path_;
-  size_t iter_;
   ExprIntSetMap expr_map_;
+  std::vector<const Node*> path_;
+  size_t iter_{0};
 };
+
+class Checker: public IRVisitor {
+ public:
+  bool Check(BoundDeducer* deducer) {
+    deducer_ = deducer;
+    Visit(deducer_->expr_);
+    return target_count == 1 && cmp_count == 1;
+  }
+
+  void Visit(const NodeRef& e) final {
+    if (e.same_as(deducer_->target_)) ++target_count;
+    IRVisitor::Visit(e);
+  }
+
+  void Visit_(const LT* op) final {
+    ++cmp_count;
+    Visit(op->a);
+    Visit(op->b);
+  }
+
+  void Visit_(const LE* op) final {
+    ++cmp_count;
+    Visit(op->a);
+    Visit(op->b);
+  }
+
+  void Visit_(const GT* op) final {
+    ++cmp_count;
+    Visit(op->a);
+    Visit(op->b);
+  }
+
+  void Visit_(const GE* op) final {
+    ++cmp_count;
+    Visit(op->a);
+    Visit(op->b);
+  }
+
+ private:
+  BoundDeducer* deducer_;
+  size_t target_count{0};
+  size_t cmp_count{0};
+};
+
+void BoundDeducer::Deduce() {
+    result = make_zero(expr_.type());
+    // get the path
+    path_ = GetPath(target_, expr_);
+    Checker checker;
+    if (!checker.Check(this) || path_.empty()) {
+      success = false;
+      return;
+    }
+    // get the sign of every subexpr
+    expr_map_ = EvalSetForEachSubExpr(expr_, dom_map_);
+
+    Visit(expr_);
+}
 
 // assuming e >= 0, deduce the bound of variable from it.
 // return empty set to represent deduce failure.
@@ -189,15 +235,16 @@ IntSet DeduceBound(Var v, Expr e,
     dmap[kv.first.get()] = kv.second;
   }
   BoundDeducer d(v, e, dmap);
+  d.Deduce();
   if (!d.success) return IntSet();
   Expr min = Interval::neg_inf, max = Interval::pos_inf;
   if (d.is_greater) {
-    min = d.is_equal ? d.result : d.result+1;
+    min = d.is_equal ? d.result : d.result + 1;
   } else {
-    max = d.is_equal ? d.result : d.result-1;
+    max = d.is_equal ? d.result : d.result - 1;
   }
   return IntSet::range(min, max);
 }
 
-} // namespace arith
-} // namespace tvm
+}  // namespace arith
+}  // namespace tvm
