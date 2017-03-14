c7acff49a4802d9aae7f4a6ff993b5d62a36ae52
tqchen <tianqi.tchen@gmail.com>
2017-03-13 20:37:22 -0700

    fix build

--- verilog/verilog.mk
+++ verilog/verilog.mk
@@ -6,4 +6,5 @@ VER_SRCS = $(wildcard verilog/*.v)
 VER_LIBS=lib/tvm_vpi.vpi
 
 lib/tvm_vpi.vpi: verilog/tvm_vpi.cc verilog/tvm_vpi.h
+	@mkdir -p $(@D)
 	$(CXX) $(VPI_CFLAGS) $(CFLAGS) -shared -o $@ $(filter %.cc, $^) $(LDFLAGS) $(VPI_LDFLAGS)
