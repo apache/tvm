b29f44c3eccae344535071904c07d7c62ba2e645
ZihengJiang <jzhtomas@gmail.com>
2017-04-17 21:26:31 -0700

    Finetune the doc

--- tutorials/python/schedule_primitives.py
+++ tutorials/python/schedule_primitives.py
@@ -6,7 +6,7 @@ Schedule Primitives in TVM
 TVM is a domain specific language for efficient kernel construction.
 
 In this tutorial, we will show you how to schedule the computation by
-primitives provided by TVM.
+various primitives provided by TVM.
 """
 from __future__ import absolute_import, print_function
 
@@ -16,7 +16,7 @@ import numpy as np
 ######################################################################
 #
 # There often exist several methods to compute the same result,
-# however, different methods result in diffrent locality and
+# however, different methods will result in different locality and
 # performance. So TVM asks user to provide how to execute the
 # computation called **Schedule**.
 #
@@ -27,12 +27,13 @@ import numpy as np
 n = tvm.var('n')
 m = tvm.var('m')
 
-# define a function to print the schedule out
+# this function will pint the schedule in C like format
 # let's skip the detail for now
 def print_schedule(s, ops):
-    s.normalize()
+    s = s.normalize()
     bounds = tvm.schedule.InferBound(s)
     stmt = tvm.schedule.ScheduleOps(s, bounds)
+    stmt = tvm.lower(s, ops, with_api_wrapper=False, max_auto_unroll_step=0)
     print(stmt)
 
 ######################################################################
@@ -45,7 +46,7 @@ B = tvm.placeholder((m, n), name='B')
 C = tvm.compute((m, n), lambda i, j: A[i, j] * B[i, j], name='C')
 
 s = tvm.create_schedule([C.op])
-print_schedule(s)
+print_schedule(s, [A, B, C])
 
 ######################################################################
 # One schedule is composed by multiple stages, and one
@@ -62,7 +63,7 @@ B = tvm.compute((m,), lambda i: A[i]*2, name='B')
 
 s = tvm.create_schedule(B.op)
 xo, xi = s[B].split(B.op.axis[0], factor=32)
-print_schedule(s)
+print_schedule(s, [A, B])
 
 ######################################################################
 # You can also split a axis by :code:`nparts`, which splits the axis
@@ -72,7 +73,7 @@ B = tvm.compute((m,), lambda i: A[i], name='B')
 
 s = tvm.create_schedule(B.op)
 bx, tx = s[B].split(B.op.axis[0], nparts=32)
-print_schedule(s)
+print_schedule(s, [A, B])
 
 ######################################################################
 # tile
@@ -84,7 +85,7 @@ B = tvm.compute((m, n), lambda i, j: A[i, j], name='B')
 
 s = tvm.create_schedule(B.op)
 xo, yo, xi, yi = s[B].tile(B.op.axis[0], B.op.axis[1], x_factor=10, y_factor=5)
-print_schedule(s)
+print_schedule(s, [A, B])
 
 ######################################################################
 # fuse
@@ -98,7 +99,7 @@ s = tvm.create_schedule(B.op)
 xo, yo, xi, yi = s[B].tile(B.op.axis[0], B.op.axis[1], x_factor=10, y_factor=5)
 # then fuse (i.inner, j.inner) into one axis: (i.inner.j.inner.fused)
 fused = s[B].fuse(yi, xi)
-print_schedule(s)
+print_schedule(s, [A, B])
 
 ######################################################################
 # reorder
@@ -112,7 +113,7 @@ s = tvm.create_schedule(B.op)
 xo, yo, xi, yi = s[B].tile(B.op.axis[0], B.op.axis[1], x_factor=10, y_factor=5)
 # then reorder the axises: (i.inner, j.outer, i.outer, j.inner)
 s[B].reorder(xi, yo, xo, yi)
-print_schedule(s)
+print_schedule(s, [A, B])
 
 ######################################################################
 # bind
@@ -126,7 +127,7 @@ s = tvm.create_schedule(B.op)
 bx, tx = s[B].split(B.op.axis[0], factor=64)
 s[B].bind(bx, tvm.thread_axis("blockIdx.x"))
 s[B].bind(tx, tvm.thread_axis("threadIdx.x"))
-print_schedule(s)
+print_schedule(s, [A, B])
 
 ######################################################################
 # compute_at
@@ -138,7 +139,7 @@ B = tvm.compute((m, ), lambda i: A[i]+1, name='B')
 C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
-print_schedule(s)
+print_schedule(s, [A, B, C])
 
 ######################################################################
 # :code:`compute_at` can move computation of `B` into the first axis
@@ -149,7 +150,7 @@ C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
 s[B].compute_at(s[C], C.op.axis[0])
-print_schedule(s)
+print_schedule(s, [A, B, C])
 
 ######################################################################
 # compute_inline
@@ -163,7 +164,7 @@ C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
 s[B].compute_inline()
-print_schedule(s)
+print_schedule(s, [A, B, C])
 
 ######################################################################
 # compute_root
@@ -172,25 +173,23 @@ print_schedule(s)
 A = tvm.placeholder((m, ), name='A')
 B = tvm.compute((m, ), lambda i: A[i]+1, name='B')
 C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
-s = tvm.create_schedule(C.op)
 
 s = tvm.create_schedule(C.op)
 s[B].compute_at(s[C], C.op.axis[0])
 s[B].compute_root()
-print_schedule(s)
+print_schedule(s, [A, B, C])
 
 ######################################################################
 # Summary
 # -------
-# This tutorial provides a walk through of TVM workflow using
-# a vector add example. The general workflow is
+# This tutorial provides an introduction to schedule primitives in
+# tvm, which permits users schedule the computation easily and
+# flexibly.
+#
+# In order to get an good performance kernel implementation, the
+# general workflow often is:
 #
 # - Describe your computation via series of operations.
-# - Describe how we want to compute use schedule primitives.
-# - Compile to the target function we want.
-# - Optionally, save the function to be loaded later.
-#
-# You are more than welcomed to checkout other examples and
-# tutorials to learn more about the supported operations, schedule primitives
-# and other features in TVM.
-#
+# - Try to schedule the computation with primitives.
+# - Compile and run to see the performance difference.
+# - Adjust your schedule according the running result.
