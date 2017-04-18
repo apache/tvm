af38ed6964767d1a6e6b896e3700485c5209d12b
ZihengJiang <jzhtomas@gmail.com>
2017-04-17 22:36:22 -0700

    Finetune the doc

--- tutorials/python/schedule_primitives.py
+++ tutorials/python/schedule_primitives.py
@@ -1,6 +1,6 @@
 """
 Schedule Primitives in TVM
-=================================
+==========================
 **Author**: `Ziheng Jiang <https://github.com/ZihengJiang>`_
 
 TVM is a domain specific language for efficient kernel construction.
@@ -27,15 +27,6 @@ import numpy as np
 n = tvm.var('n')
 m = tvm.var('m')
 
-# this function will pint the schedule in C like format
-# let's skip the detail for now
-def print_schedule(s, ops):
-    s = s.normalize()
-    bounds = tvm.schedule.InferBound(s)
-    stmt = tvm.schedule.ScheduleOps(s, bounds)
-    stmt = tvm.lower(s, ops, with_api_wrapper=False, max_auto_unroll_step=0)
-    print(stmt)
-
 ######################################################################
 # A schedule can be created from a list of ops, by default the
 # schedule compute tensor in a serial manner in a row-major order.
@@ -46,7 +37,11 @@ B = tvm.placeholder((m, n), name='B')
 C = tvm.compute((m, n), lambda i, j: A[i, j] * B[i, j], name='C')
 
 s = tvm.create_schedule([C.op])
-print_schedule(s, [A, B, C])
+# lower will transform the computation from definition to the real
+# callable function. With argument `with_api_wrapper=False`, it will
+# return you a readable C like statement, we use it here to print the
+# schedule result.
+print(tvm.lower(s, [A, B, C], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # One schedule is composed by multiple stages, and one
@@ -63,7 +58,7 @@ B = tvm.compute((m,), lambda i: A[i]*2, name='B')
 
 s = tvm.create_schedule(B.op)
 xo, xi = s[B].split(B.op.axis[0], factor=32)
-print_schedule(s, [A, B])
+print(tvm.lower(s, [A, B], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # You can also split a axis by :code:`nparts`, which splits the axis
@@ -73,7 +68,7 @@ B = tvm.compute((m,), lambda i: A[i], name='B')
 
 s = tvm.create_schedule(B.op)
 bx, tx = s[B].split(B.op.axis[0], nparts=32)
-print_schedule(s, [A, B])
+print(tvm.lower(s, [A, B], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # tile
@@ -85,7 +80,7 @@ B = tvm.compute((m, n), lambda i, j: A[i, j], name='B')
 
 s = tvm.create_schedule(B.op)
 xo, yo, xi, yi = s[B].tile(B.op.axis[0], B.op.axis[1], x_factor=10, y_factor=5)
-print_schedule(s, [A, B])
+print(tvm.lower(s, [A, B], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # fuse
@@ -99,7 +94,7 @@ s = tvm.create_schedule(B.op)
 xo, yo, xi, yi = s[B].tile(B.op.axis[0], B.op.axis[1], x_factor=10, y_factor=5)
 # then fuse (i.inner, j.inner) into one axis: (i.inner.j.inner.fused)
 fused = s[B].fuse(yi, xi)
-print_schedule(s, [A, B])
+print(tvm.lower(s, [A, B], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # reorder
@@ -113,7 +108,7 @@ s = tvm.create_schedule(B.op)
 xo, yo, xi, yi = s[B].tile(B.op.axis[0], B.op.axis[1], x_factor=10, y_factor=5)
 # then reorder the axises: (i.inner, j.outer, i.outer, j.inner)
 s[B].reorder(xi, yo, xo, yi)
-print_schedule(s, [A, B])
+print(tvm.lower(s, [A, B], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # bind
@@ -127,30 +122,30 @@ s = tvm.create_schedule(B.op)
 bx, tx = s[B].split(B.op.axis[0], factor=64)
 s[B].bind(bx, tvm.thread_axis("blockIdx.x"))
 s[B].bind(tx, tvm.thread_axis("threadIdx.x"))
-print_schedule(s, [A, B])
+print(tvm.lower(s, [A, B], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # compute_at
 # --------------------------
 # For a schedule consists of multiple operators, tvm will compute
 # tensors at the root separately by default.
-A = tvm.placeholder((m, ), name='A')
-B = tvm.compute((m, ), lambda i: A[i]+1, name='B')
-C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
+A = tvm.placeholder((m,), name='A')
+B = tvm.compute((m,), lambda i: A[i]+1, name='B')
+C = tvm.compute((m,), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
-print_schedule(s, [A, B, C])
+print(tvm.lower(s, [A, B, C], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # :code:`compute_at` can move computation of `B` into the first axis
 # of computation of `C`.
-A = tvm.placeholder((m, ), name='A')
-B = tvm.compute((m, ), lambda i: A[i]+1, name='B')
-C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
+A = tvm.placeholder((m,), name='A')
+B = tvm.compute((m,), lambda i: A[i]+1, name='B')
+C = tvm.compute((m,), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
 s[B].compute_at(s[C], C.op.axis[0])
-print_schedule(s, [A, B, C])
+print(tvm.lower(s, [A, B, C], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # compute_inline
@@ -158,26 +153,26 @@ print_schedule(s, [A, B, C])
 # :code:`compute_inline` can mark one stage as inline, then the body of
 # computation will be expanded and inserted at the address where the
 # tensor is required.
-A = tvm.placeholder((m, ), name='A')
-B = tvm.compute((m, ), lambda i: A[i]+1, name='B')
-C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
+A = tvm.placeholder((m,), name='A')
+B = tvm.compute((m,), lambda i: A[i]+1, name='B')
+C = tvm.compute((m,), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
 s[B].compute_inline()
-print_schedule(s, [A, B, C])
+print(tvm.lower(s, [A, B, C], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # compute_root
 # --------------------------
 # :code:`compute_root` can move computation of one stage to the root.
-A = tvm.placeholder((m, ), name='A')
-B = tvm.compute((m, ), lambda i: A[i]+1, name='B')
-C = tvm.compute((m, ), lambda i: B[i]*2, name='C')
+A = tvm.placeholder((m,), name='A')
+B = tvm.compute((m,), lambda i: A[i]+1, name='B')
+C = tvm.compute((m,), lambda i: B[i]*2, name='C')
 
 s = tvm.create_schedule(C.op)
 s[B].compute_at(s[C], C.op.axis[0])
 s[B].compute_root()
-print_schedule(s, [A, B, C])
+print(tvm.lower(s, [A, B, C], with_api_wrapper=False, max_auto_unroll_step=0))
 
 ######################################################################
 # Summary
