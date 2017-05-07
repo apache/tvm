a64a0f5ff2131dddf6995f51670b4321df2f4f05
Tianqi Chen <tqchen@users.noreply.github.com>
2017-05-07 12:45:56 -0700

    [BUGFIX/REGRESSION] Complex inline call, regression test on lstm cell (#128)

--- tests/python/unittest/test_pass_inline.py
+++ tests/python/unittest/test_pass_inline.py
@@ -19,7 +19,19 @@ def test_inline():
     except tvm.TVMError:
         pass
 
+def test_inline2():
+    m = tvm.var('m')
+    A = tvm.placeholder((m,), name='A')
+    T = tvm.compute((m,), lambda i,: A[i] + 10, name='T')
+    stmt = tvm.make.Evaluate(tvm.exp(T[10]) + 11 * T[100])
+    stmt = tvm.ir_pass.Inline(
+        stmt, T.op, [x.var for x in T.op.axis], T.op.body)
+    def check(op):
+        if isinstance(op, tvm.expr.Call):
+            assert op.func != T.op
+    tvm.ir_pass.PostOrderVisit(stmt, check)
 
 
 if __name__ == "__main__":
+    test_inline2()
     test_inline()
