ee0daf21a609e62808c8edac79ab3e9fffe2807d
tqchen <tianqi.tchen@gmail.com>
2017-07-03 17:53:03 -0700

    fix doc builder

--- tests/ci_build/install/ubuntu_install_sphinx.sh
+++ tests/ci_build/install/ubuntu_install_sphinx.sh
@@ -1,1 +1,1 @@
-pip install sphinx>=1.5.5 sphinx-gallery sphinx_rtd_theme matplotlib Image recommonmark
+pip install sphinx==1.6.2 sphinx-gallery sphinx_rtd_theme matplotlib Image commonmark>=0.7.3 docutils>=0.11
