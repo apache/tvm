9c954adab34f703cb0d7c792d54d83307fa89f0d
Tianqi Chen <tqchen@users.noreply.github.com>
2017-07-10 19:33:04 -0700

    [DOC] Fix c++ doc build (#240)

--- tests/scripts/task_python_docs.sh
+++ tests/scripts/task_python_docs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 mkdir -p docs/_build/html
 rm -rf docs/_build/html/jsdoc
-rm -rf docs/_build/html/doxygen
 # C++ doc
 make doc
 
 # JS doc
 jsdoc web/tvm_runtime.js web/README.md || exit -1
 mv out docs/_build/html/jsdoc || exit -1
-mv docs/doxygen docs/_build/html/doxygen || exit -1
 
 cd docs
 PYTHONPATH=../python make html || exit -1
