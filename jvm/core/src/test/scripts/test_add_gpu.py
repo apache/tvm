8a14f92106eddfda993e0034b04153795896c318
Yizhi Liu <javelinjs@gmail.com>
2017-07-13 01:24:58 +0800

    [tvm4j] add module load tests

---
+++ jvm/core/src/test/scripts/test_add_gpu.py
@@ -0,0 +1,30 @@
+import os
+
+import tvm
+from tvm.contrib import cc_compiler as cc
+from tvm.contrib import util
+
+def test_add(target_dir):
+    n = tvm.var("n")
+    A = tvm.placeholder((n,), name='A')
+    B = tvm.placeholder((n,), name='B')
+    C = tvm.compute(A.shape, lambda i: A[i] + B[i], name="C")
+
+    s = tvm.create_schedule(C.op)
+
+    bx, tx = s[C].split(C.op.axis[0], factor=64)
+    s[C].bind(bx, tvm.thread_axis("blockIdx.x"))
+    s[C].bind(tx, tvm.thread_axis("threadIdx.x"))
+    fadd_cuda = tvm.build(s, [A, B, C], "cuda", target_host="llvm", name="myadd")
+
+    fadd_cuda.save(os.path.join(target_dir, "add_gpu.o"))
+    fadd_cuda.imported_modules[0].save(os.path.join(target_dir, "add_gpu.ptx"))
+    cc.create_shared(os.path.join(target_dir, "add_gpu.so"),
+            [os.path.join(target_dir, "add_gpu.o")])
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        sys.exit(-1)
+    test_add(sys.argv[1])
+
--- tests/scripts/task_java_unittest.sh
+++ tests/scripts/task_java_unittest.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-make jvmpkg JVM_TEST_ARGS="-DskipTests=false" || exit -1
+export PYTHONPATH=python:apps/extension/python
+export PYTHONPATH=${PYTHONPATH}:apps/graph_executor/python:apps/graph_executor/nnvm/python
+export LD_LIBRARY_PATH=lib:${LD_LIBRARY_PATH}
+
+CURR_DIR=$(cd `dirname $0`; pwd)
+SCRIPT_DIR=$CURRDIR/../../jvm/core/src/test/scripts
+TEMP_DIR=$(mktemp -d)
+
+python $SCRIPT_DIR/test_add_cpu.py $TEMP_DIR || exit -1
+python $SCRIPT_DIR/test_add_gpu.py $TEMP_DIR || exit -1
+
+make jvmpkg JVM_TEST_ARGS="-DskipTests=false -Dtest.tempdir=$TEMP_DIR" || exit -1
