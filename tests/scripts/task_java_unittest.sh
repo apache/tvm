2c3879bbc42e342640d10e86f8be56a8c6d3747e
Yizhi Liu <javelinjs@gmail.com>
2017-07-15 01:08:31 +0800

    [tvm4j] add stylecheck in jenkins tests

--- tests/scripts/task_java_unittest.sh
+++ tests/scripts/task_java_unittest.sh
@@ -10,6 +10,7 @@ TEMP_DIR=$(mktemp -d)
 python $SCRIPT_DIR/test_add_cpu.py $TEMP_DIR || exit -1
 python $SCRIPT_DIR/test_add_gpu.py $TEMP_DIR || exit -1
 
-make jvmpkg JVM_TEST_ARGS="-DskipTests=false -Dcheckstyle.skip=true -Dtest.tempdir=$TEMP_DIR" || exit -1
+make jvmpkg || exit -1
+make jvmpkg JVM_TEST_ARGS="-DskipTests=false -Dtest.tempdir=$TEMP_DIR" || exit -1
 
 rm -rf $TEMP_DIR
