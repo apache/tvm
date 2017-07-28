b858179b9daf3e06694ea7ec7362534134ff96d4
ZihengJiang <ziheng@apache.org>
2017-07-28 15:36:13 -0700

    [GRAPH] Add GraphHandle

---
+++ src/lang/graph.cc
@@ -0,0 +1,19 @@
+/*!
+ *  Copyright (c) 2017 by Contributors
+ * \file target_info.cc
+ */
+#include <tvm/graph.h>
+#include <tvm/packed_func_ext.h>
+
+namespace tvm {
+
+TVM_STATIC_IR_FUNCTOR(IRPrinter, vtable)
+.set_dispatch<GraphHandleNode>([](const GraphHandleNode *op, IRPrinter *p) {
+    p->stream << "graph-handle("
+              << "handle=0x" << std::hex
+              << reinterpret_cast<uint64_t>(op->graph_handle) << ")";
+});
+
+TVM_REGISTER_NODE_TYPE(GraphHandleNode);
+
+}  // namespace tvm
