1623745f227abbfba84df4b0e6d9d4535aa6bb21
zihengjiang <ziheng@apache.org>
2017-08-07 03:27:05 +0000

    [NNPACK] Add nnpack.convolution

--- tests/python/contrib/test_nnpack.py
+++ tests/python/contrib/test_nnpack.py
@@ -1,5 +1,6 @@
 import tvm
 import numpy as np
+import scipy.signal
 from tvm.contrib import nnpack
 
 def test_fully_connected_output():
@@ -9,7 +10,6 @@ def test_fully_connected_output():
     bias = tvm.var('bias', dtype=tvm.float32)
     A = tvm.placeholder((n, l), name='A')
     B = tvm.placeholder((m, l), name='B')
-    C = nnpack.fully_connected_inference(A, B)
     C = nnpack.fully_connected_output(A, B)
     D = tvm.compute(C.shape, lambda i, j: C[i,j] + bias, name="D")
     s = tvm.create_schedule(D.op)
@@ -62,7 +62,100 @@ def test_fully_connected_inference():
             d.asnumpy(), np.dot(a.asnumpy(), b.asnumpy().T) + bb, rtol=1e-5)
     verify()
 
+def np_conv(na, nw, padding, stride=1):
+    batch, in_channel, in_height, in_width = na.shape
+    _, num_filter, kernel_h, kernel_w = nw.shape
+    if isinstance(stride, int):
+        stride_h = stride_w = stride
+    else:
+        stride_h, stride_w = stride
+
+    if isinstance(padding, int):
+        pad_h = pad_w = padding * 2
+    else:
+        pad_h, pad_w = padding
+        pad_h *= 2
+        pad_w *= 2
+
+    pad_top = int(np.ceil(float(pad_h) / 2))
+    pad_bottom = pad_h - pad_top
+    pad_left = int(np.ceil(float(pad_w) / 2))
+    pad_right = pad_w - pad_left
+
+    out_channel = num_filter
+    out_height = (in_height - kernel_h + pad_h) // stride_h + 1
+    out_width = (in_width - kernel_w + pad_w) // stride_w + 1
+    nb = np.zeros((batch, out_channel, out_height, out_width))
+    for n in range(batch):
+        for f in range(out_channel):
+            for c in range(in_channel):
+                if pad_h > 0:
+                    apad = np.zeros((in_height + pad_h, in_width + pad_w))
+                    apad[pad_top:-pad_bottom, pad_left:-pad_right] = na[n, c]
+                else:
+                    apad = na[n, c]
+                out = scipy.signal.convolve2d(
+                    apad, np.rot90(np.rot90(nw[f, c])), mode='valid')
+                nb[n, f] += out[::stride, ::stride]
+    return nb
+
+def test_convolution_inference():
+    f32 = 'float32'
+    ctx = tvm.cpu(0)
+    conv_infer = tvm.get_global_func("tvm.contrib.nnpack.convolution_inference")
+
+    IH = 48
+    IW = 48
+    IC = 16
+    OC = 16
+    K = 3
+    PAD = 1
+    STRIDE = 1
+    OH = (IH + 2*PAD - K) / STRIDE + 1
+    OW = (IW + 2*PAD - K) / STRIDE + 1
+
+    na = np.random.uniform(size=(IC, IH, IW)).astype(f32)
+    nb = np.random.uniform(size=(OC, IC, K, K)).astype(f32)
+    nc = np.zeros((OC, ), dtype=f32)
+    ta = tvm.nd.array(na, ctx)
+    tb = tvm.nd.array(nb, ctx)
+    tc = tvm.nd.array(nc, ctx)
+    td = tvm.nd.array(np.zeros((OC, OH, OW), dtype=f32), ctx)
+    conv_infer(ta, tb, tc, td, PAD, PAD, PAD, PAD, STRIDE, STRIDE)
+
+    nd = np_conv(np.reshape(na, (1, IC, IH, IW)), nb, PAD, STRIDE)
+    np.testing.assert_allclose(
+        td.asnumpy(), nd.reshape(IC, IH, IW), rtol=1e-5)
+
+
+def test_convolution_output():
+    f32 = 'float32'
+    ctx = tvm.cpu(0)
+    conv_output = tvm.get_global_func("tvm.contrib.nnpack.convolution_output")
+
+    BATCH = 1
+    IH = 5
+    IW = 5
+    IC = 2
+    OC = 2
+    K = 3
+    PAD = 1
+    OH = (IH + 2*PAD - K) + 1
+    OW = (IW + 2*PAD - K) + 1
+
+    na = np.random.uniform(size=(BATCH, IC, IH, IW)).astype(f32)
+    nb = np.random.uniform(size=(OC, IC, K, K)).astype(f32)
+    nc = np.zeros((OC, ), dtype=f32)
+    ta = tvm.nd.array(na, ctx)
+    tb = tvm.nd.array(nb, ctx)
+    tc = tvm.nd.array(nc, ctx)
+    td = tvm.nd.array(np.zeros((BATCH, OC, OH, OW), dtype=f32), ctx)
+    conv_output(ta, tb, tc, td, PAD, PAD, PAD, PAD)
+
+    nd = np_conv(na, nb, PAD)
+    np.testing.assert_allclose(td.asnumpy(), nd, rtol=1e-5)
+
 
 if __name__ == "__main__":
-    test_fully_connected_inference()
-    test_fully_connected_output()
+    import nose
+    nose.runmodule()
