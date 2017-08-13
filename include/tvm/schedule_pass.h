f08de2b690df20c97782827f8153f1d96a1bd97e
Nicolas Vasilache <nicolasvasilache@users.noreply.github.com>
2017-08-14 00:50:25 +0200

    [WIP] C++ topi contributions (#312)
    * [WIP] C++ topi contributions
    
    Summary:
    This diff implements C++ topi contributions for:
      - relu with parametrix threshold
      - pad with generic padBefore / padAfter specification
      - matmult with transposes
      - conv2d_nchw, conv2d_hwcn with runtime constant padding and strides
      - depthwise_conv2d_nchw with runtime constant padding and strides
      - group_conv2d_ngchw with runtime constant padding and strides
      - broadcast_to a broadcastable shape
      - broadcast_bop where bop is an usual binary op (+ - * / %)
    
    Convolution padding is implemented using the pad operation.
    To avoid extra memory consumption, it is generally recommended to inline the padding with the autoinliner.
    Unfortunately in its current form the elemwise checks are too restrictive to allow inlining.
    So this diff also proposes an extension to LHS injective (i.e. no reduction axis in the current IR design)
    
    Test Plan:
    Tested in C++ testsuite in a separate repository, I am looking for suggestions to quickly spin up some tests for tvm.
    
    Reviewers: tqchen
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Blame Revision:
    
    * Review + Lint + GSG C++

--- include/tvm/schedule_pass.h
+++ include/tvm/schedule_pass.h
@@ -40,6 +40,16 @@ Stmt ScheduleOps(Schedule s, Map<IterVar, Range> dom_map);
  */
 void AutoInlineElemWise(Schedule sch);
 
+/*!
+ * \brief To automatically inline operations with injective writes
+ *   (i.e. writes without reduction or sequential loops). Note
+ *   that in this case, guarantees about contiguity, transpose, stride,
+ *   alignemnt and memory footprint in general do not hold.
+ *
+ * \param sch The schedule to be inlined.
+ */
+void AutoInlineInjective(Schedule sch);
+
 }  // namespace schedule
 }  // namespace tvm
 #endif  // TVM_SCHEDULE_PASS_H_
