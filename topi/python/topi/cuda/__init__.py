c461fac652abd6bbf0367a786e1c8b3c5813154c
Huyuwei <huyuwei1995@gmail.com>
2017-08-15 09:22:16 +0800

    update depthwise_conv2d schedule and testing

--- topi/python/topi/cuda/__init__.py
+++ topi/python/topi/cuda/__init__.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import as _abs
 
 from .conv2d_nchw import schedule_conv2d_nchw
 from .conv2d_hwcn import schedule_conv2d_hwcn
-from .depthwise_conv2d_map import schedule_depthwise_conv2d_map
+from .depthwise_conv2d import schedule_depthwise_conv2d
 from .reduction import schedule_reduce
 from .broadcast import schedule_broadcast_to
