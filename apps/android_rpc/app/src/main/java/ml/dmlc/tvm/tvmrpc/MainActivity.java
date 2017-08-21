f03d8b5252b2c282c8877b88844b3b5dea0b80ac
Yizhi Liu <javelinjs@gmail.com>
2017-08-22 01:10:51 +0800

    [APP] optimize android app interface. add ndk compile tool

--- apps/android_rpc/app/src/main/java/ml/dmlc/tvm/tvmrpc/MainActivity.java
+++ apps/android_rpc/app/src/main/java/ml/dmlc/tvm/tvmrpc/MainActivity.java
@@ -1,11 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ml.dmlc.tvm.tvmrpc;
 
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import java.net.Socket;
 
@@ -26,10 +44,23 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    Switch switchConnect = findViewById(R.id.switch_connect);
+    switchConnect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+          connectProxy();
+        } else {
+          disconnect();
+        }
+      }
+    });
   }
 
   private Server tvmServer;
-  public void connectProxy(View view) {
+
+  private void connectProxy() {
     EditText edProxyAddress = findViewById(R.id.input_address);
     EditText edProxyPort = findViewById(R.id.input_port);
     EditText edAppKey = findViewById(R.id.input_key);
@@ -46,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
     tvmServer.start();
   }
 
-  public void disconnect(View view) {
+  private void disconnect() {
     if (tvmServer != null) {
       tvmServer.terminate();
       System.err.println("Disconnected.");
--- python/tvm/contrib/cc.py
+++ python/tvm/contrib/cc.py
@@ -7,7 +7,8 @@ import subprocess
 def create_shared(output,
                   objects,
                   options=None,
-                  cc="g++"):
+                  cc="g++",
+                  cross_compile=False):
     """Create shared library.
 
     Parameters
@@ -23,12 +24,19 @@ def create_shared(output,
 
     cc : str, optional
         The compile string.
+
+    cross_compile : bool, optional
+        Do not add additional compile options
+        (except for those specfied in argument 'options')
+        if it is cross compiling.
     """
     cmd = [cc]
-    cmd += ["-shared", "-fPIC"]
+    if not cross_compile:
+        cmd += ["-shared", "-fPIC"]
 
-    if sys.platform == "darwin":
-        cmd += ["-undefined", "dynamic_lookup"]
+        if sys.platform == "darwin":
+            cmd += ["-undefined", "dynamic_lookup"]
+
     cmd += ["-o", output]
 
     if isinstance(objects, str):
