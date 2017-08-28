4e8002059dcd9a2a358d88f0b83077c19e18ed45
Yizhi Liu <javelinjs@gmail.com>
2017-08-29 04:51:01 +0800

    [APP] Android RPC README (#395)

--- apps/android_rpc/dev_tools/sign_apk.sh
+++ apps/android_rpc/dev_tools/sign_apk.sh
@@ -3,4 +3,4 @@ CURR_DIR=$(cd `dirname $0`; pwd)
 APK_DIR=$CURR_DIR/../app/build/outputs/apk
 UNSIGNED_APK=$APK_DIR/app-release-unsigned.apk
 SIGNED_APK=$APK_DIR/tvmrpc-release.apk
-jarsigner -verbose -keystore tvmrpc.keystore -signedjar $SIGNED_APK $UNSIGNED_APK 'tvmrpc'
+jarsigner -verbose -keystore $CURR_DIR/tvmrpc.keystore -signedjar $SIGNED_APK $UNSIGNED_APK 'tvmrpc'
