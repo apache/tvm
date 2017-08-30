7c8ceabb8ab4b6115844ded7f4ac5bd16e614d05
tqchen <tianqi.tchen@gmail.com>
2017-08-30 15:37:00 -0700

    [PASS] IRTransform to enable IR pass proptype in python

--- src/api/api_pass.cc
+++ src/api/api_pass.cc
@@ -7,6 +7,7 @@
 #include <tvm/ir.h>
 #include <tvm/ir_pass.h>
 #include <tvm/ir_visitor.h>
+#include <tvm/ir_mutator.h>
 #include <tvm/api_registry.h>
 
 namespace tvm {
@@ -88,6 +89,7 @@ REGISTER_PASS1(VerifySSA);
 REGISTER_PASS1(RewriteUnsafeSelect);
 REGISTER_PASS4(Inline);
 REGISTER_PASS3(StorageFlatten);
+REGISTER_PASS4(IRTransform);
 REGISTER_PASS1(VectorizeLoop);
 REGISTER_PASS4(UnrollLoop);
 REGISTER_PASS2(ThreadSync);
