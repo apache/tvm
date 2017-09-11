175503839aea01025af99907876a75683e7e4e19
Huyuwei <huyuwei1995@gmail.com>
2017-09-11 16:37:47 +0800

    improve padding

--- topi/tests/python/test_topi_depthwise_conv2d_back_input.py
+++ topi/tests/python/test_topi_depthwise_conv2d_back_input.py
@@ -4,31 +4,30 @@ import numpy as np
 from tvm.contrib.pickle_memoize import memoize
 from scipy import signal
 from topi.util import get_const_tuple
+from topi.nn.util import get_pad_tuple
 from topi.cuda.depthwise_conv2d import schedule_depthwise_conv2d_backward_input_nhwc
 
-def depthwise_conv2d_with_workload_nhwc(batch, in_channel, in_height, channel_multiplier, filter_height, stride_h, padding_h):
-    in_width = in_height
+
+def verify_depthwise_conv2d_back_input(batch, in_channel, in_h, channel_multiplier, filter_h, stride_h, padding_h):
+    in_w = in_h
     filter_channel = in_channel
-    filter_width = filter_height
+    filter_w = filter_h
     stride_w = stride_h
     padding_w = padding_h
 
-    out_height = np.int((in_height+2*padding_h-filter_height)/stride_h+1)
-    out_width = np.int((in_width+2*padding_w-filter_width)/stride_w+1)
+    out_h = np.int((in_h+2*padding_h-filter_h)/stride_h+1)
+    out_w = np.int((in_w+2*padding_w-filter_w)/stride_w+1)
     out_channel = in_channel * channel_multiplier
 
-    ishape = [batch, in_height, in_width, in_channel]
-    oshape = [batch, out_height, out_width, out_channel]
+    ishape = [batch, in_h, in_w, in_channel]
+    oshape = [batch, out_h, out_w, out_channel]
 
     # placeholder
     Out_grad = tvm.placeholder(oshape, name='Out_grad')
-    Filter = tvm.placeholder((filter_height, filter_width, filter_channel, channel_multiplier), name='Filter')
-    stride = [stride_h, stride_w]
-    padding = [padding_h, padding_w]
-
+    Filter = tvm.placeholder((filter_h, filter_w, filter_channel, channel_multiplier))
     # declare
-    In_grad = topi.nn.depthwise_conv2d_backward_input_nhwc(Filter, Out_grad, oshape, ishape, stride, padding)
-
+    In_grad = topi.nn.depthwise_conv2d_backward_input_nhwc(Filter, Out_grad, oshape, ishape,
+        stride=[stride_h, stride_w], padding=[padding_h, padding_w])
     # schedule
     schedule = schedule_depthwise_conv2d_backward_input_nhwc(In_grad)
 
@@ -37,98 +36,74 @@ def depthwise_conv2d_with_workload_nhwc(batch, in_channel, in_height, channel_mu
             print("Skip because %s is not enabled" % device)
             return
         ctx = tvm.context(device, 0)
-
-        # build the kernels
+        # build the kernel
         f = tvm.build(schedule, [Filter, Out_grad, In_grad], device)
-
-        # Prepare pod type for test data closure
+        # prepare pod type for test data closure
         dtype = Out_grad.dtype
         out_grad_shape = get_const_tuple(Out_grad.shape)
         filter_shape = get_const_tuple(Filter.shape)
 
-        # Use memoize, pickle the test data for next time use.
+        # use memoize to pickle the test data for next time use
         @memoize("topi.tests.test_topi_depthwise_conv2d_backward_input.nhwc")
         def get_ref_data():
-            out_backprop_np = np.random.uniform(size=out_grad_shape).astype(dtype)
+            out_grad_np = np.random.uniform(size=out_grad_shape).astype(dtype)
             filter_np = np.random.uniform(size=filter_shape).astype(dtype)
-            
-            Dilated_out_grad = topi.testing.dilate_python(out_backprop_np, [1, stride_h, stride_w, 1]) 
-            if padding_h > 0 and padding_w > 0:
-                pad_top = int((filter_height - 1)/2)
-                pad_left = int((filter_width - 1)/2)
-                pad_bottom = int((filter_height - 1)/2)
-                pad_right = int((filter_width - 1)/2)
-            
-                padded_out_grad = np.zeros((batch, Dilated_out_grad.shape[1]+pad_top+pad_bottom, Dilated_out_grad.shape[2]+pad_left+pad_right, out_channel))
-                padded_out_grad[:, pad_top:-pad_bottom, pad_left:-pad_right, :] = Dilated_out_grad
+            dilated_out_grad_np = topi.testing.dilate_python(out_grad_np, [1, stride_h, stride_w, 1])
+            # padding params in forward propagation
+            fpad_top, fpad_left, fpad_bottom, fpad_right = get_pad_tuple([padding_h, padding_w], (filter_h, filter_w))
+            # padding params in backward propagation
+            bpad_top = filter_h - 1 - fpad_top
+            bpad_bottom = (filter_h - 1 - fpad_bottom) + (stride_h - 1)
+            bpad_left = filter_w - 1 - fpad_left
+            bpad_right = (filter_w - 1 - fpad_right) + (stride_w - 1)
 
-                if stride_h % 2 == 0: # it pads an addition row to pad_right and pad_bottom
-                    pad_top -= (in_height+stride_h+1)%2
-                if stride_w % 2 == 0:
-                    pad_left -= (in_width+stride_w+1)%2
-                input_grad_np = np.zeros((batch, in_height, in_width, in_channel))
-                for b in range(batch):
-                    for c in range(in_channel):
-                        for m in range(channel_multiplier):
-                            input_grad_np[b, :, :, c] += signal.convolve2d(padded_out_grad[b, :, :, c*channel_multiplier+m], \
-                                filter_np[:, :, c, m], \
-                                mode='same')[(pad_top):-(pad_bottom), (pad_left):-(pad_right)] 
-        
-            if padding_h == 0 and padding_w == 0:
-                pad_top = int((filter_height - 1)/2)
-                pad_left = int((filter_width - 1)/2)
-                pad_bottom = int((filter_height - 1)/2)
-                pad_right = int((filter_width - 1)/2)
-                if stride_h % 2 == 0: # it pads an addition row to pad_right and pad_bottom
-                    pad_bottom += (in_height+stride_h+1)%2
-                if stride_w % 2 == 0:
-                    pad_right += (in_width+stride_w+1)%2
-                padded_out_grad = np.zeros((batch, Dilated_out_grad.shape[1]+pad_top+pad_bottom, Dilated_out_grad.shape[2]+pad_left+pad_right, out_channel))
-                padded_out_grad[:, pad_top:-pad_bottom, pad_left:-pad_right, :] = Dilated_out_grad
-                input_grad_np = np.zeros((batch, in_height, in_width, in_channel))
-                for b in range(batch):
-                    for c in range(in_channel):
-                        for m in range(channel_multiplier):
-                            input_grad_np[b, :, :, c] += signal.convolve2d(padded_out_grad[b, :, :, c*channel_multiplier+m], \
-                                filter_np[:, :, c, m], \
-                                mode='same')
-            
-            return (out_backprop_np, filter_np, input_grad_np)
-        
-        (out_backprop_np, filter_np, input_grad_np) = get_ref_data()
+            padded_out_grad = np.zeros((batch, dilated_out_grad_np.shape[1]+bpad_top+bpad_bottom,
+                dilated_out_grad_np.shape[2]+bpad_left+bpad_right, out_channel))
+            padded_out_grad[:, bpad_top:dilated_out_grad_np.shape[1]+bpad_top,
+                bpad_left:dilated_out_grad_np.shape[2]+bpad_left, :] = dilated_out_grad_np
 
-        # prepare data
-        out_backprop_tvm = tvm.nd.array(out_backprop_np, ctx)
+            in_grad_np = np.zeros((batch, in_h, in_w, in_channel))
+            for b in range(batch):
+                for c in range(in_channel):
+                    for m in range(channel_multiplier):
+                        in_grad_np[b, :, :, c] += signal.convolve2d(padded_out_grad[b, :, :, c*channel_multiplier+m], \
+                                filter_np[:, :, c, m], mode='valid')[0:in_h, 0:in_w]
+            return (out_grad_np, filter_np, in_grad_np)
+
+        (out_grad_np, filter_np, in_grad_np) = get_ref_data()
+
+        out_grad_tvm = tvm.nd.array(out_grad_np, ctx)
         filter_tvm = tvm.nd.array(filter_np, ctx)
-        in_backprop_tvm = tvm.nd.array(np.zeros((batch, in_height, in_width, in_channel), dtype=Out_grad.dtype), ctx)
-
-        # launch kernel (depthwise_conv2d backward nhwc wrt input)
+        in_grad_tvm = tvm.nd.array(np.zeros(shape=ishape, dtype=dtype), ctx)
+        # launch the kernel
         timer = f.time_evaluator(f.entry_name, ctx, number=1)
-        tcost = timer(filter_tvm, out_backprop_tvm, in_backprop_tvm).mean
- 
-        np.testing.assert_allclose(input_grad_np, in_backprop_tvm.asnumpy(), rtol=1e-5)
-    
-    check_device("cuda")
+        tcost = timer(filter_tvm, out_grad_tvm, in_grad_tvm).mean
+        np.testing.assert_allclose(in_grad_np, in_grad_tvm.asnumpy(), rtol=1e-5)
 
-def test_depthwise_conv2d():
-    print("testing nhwc")
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 1, 3, 1, 1)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 57, 1, 3, 1, 1)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 2, 5, 1, 2)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 57, 2, 5, 1, 2)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 1, 3, 2, 1)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 57, 1, 3, 2, 1)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 2, 5, 2, 2)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 57, 2, 5, 2, 2)
-    
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 1, 3, 1, 0)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 57, 1, 3, 1, 0)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 2, 3, 1, 0)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 57, 2, 5, 1, 0) 
-    depthwise_conv2d_with_workload_nhwc(16, 256, 55, 1, 3, 2, 0)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 1, 3, 2, 0)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 55, 2, 5, 2, 0)
-    depthwise_conv2d_with_workload_nhwc(16, 256, 56, 2, 5, 2, 0)
-    
+    check_device("opencl")
+    check_device("cuda")
+    check_device("metal")
+
+
+def test_topi_depthwise_conv2d_backward_input_nhwc():
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 3, 1, 1)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 3, 1, 1)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 5, 1, 2)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 5, 1, 2)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 3, 2, 1)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 3, 2, 1)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 5, 2, 2)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 5, 2, 2)
+
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 3, 1, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 3, 1, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 5, 1, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 5, 1, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 3, 2, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 3, 2, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 1, 5, 2, 0)
+    verify_depthwise_conv2d_back_input(16, 256, 56, 2, 5, 2, 0)
+
+
 if __name__ == "__main__":
-    test_depthwise_conv2d()
+    test_topi_depthwise_conv2d_backward_input_nhwc()
