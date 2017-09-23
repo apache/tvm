262345faab9b5a17435434d48b01e0dfaa2f9157
Tianqi Chen <tqchen@users.noreply.github.com>
2017-09-22 23:14:23 -0700

    [TOPI] dense API to remove redudant use_bias (#476)

--- topi/python/topi/nn/dense.py
+++ topi/python/topi/nn/dense.py
@@ -4,7 +4,7 @@ import tvm
 from .. import tag
 
 
-def dense(data, weight, bias, use_bias=True):
+def dense(data, weight, bias=None):
     """Applies a linear transformation: :math:`Y = XW^T + b`.
 
     Parameters
@@ -15,27 +15,26 @@ def dense(data, weight, bias, use_bias=True):
     weight : tvm.Tensor
         2-D with shape [out_dim, in_dim]
 
-    bias : tvm.Tensor
+    bias : tvm.Tensor, optional
         1-D with shape [out_dim]
-
-    use_bias : bool, optional, default=True
-        Whether to use bias parameter
 
     Returns
     -------
     output : tvm.Tensor
         2-D with shape [batch, out_dim]
     """
-    assert len(data.shape) == 2 and len(weight.shape) == 2 and len(bias.shape) == 1, \
+    assert len(data.shape) == 2 and len(weight.shape) == 2, \
         "only support 2-dim dense"
+    if bias:
+        assert len(bias.shape) == 1
     batch, in_dim = data.shape
     out_dim, _ = weight.shape
     k = tvm.reduce_axis((0, in_dim), name='k')
     matmul = tvm.compute((batch, out_dim), \
                          lambda i, j: tvm.sum(data[i, k] * weight[j, k], axis=k), \
                          tag='dense')
-    if not use_bias:
-        return matmul
-    return tvm.compute((batch, out_dim), \
-                        lambda i, j: matmul[i, j] + bias[j], \
-                        tag=tag.BROADCAST)
+    if bias:
+        matmul = tvm.compute((batch, out_dim), \
+                             lambda i, j: matmul[i, j] + bias[j], \
+                             tag=tag.BROADCAST)
+    return matmul
