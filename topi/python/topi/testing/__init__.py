65f872641755daa4ef4f9aade985e0b5a3eef78b
Leyuan Wang <laurawly@gmail.com>
2017-09-24 12:01:09 -0700

    log_softmax added to topi (#483)

--- topi/python/topi/testing/__init__.py
+++ topi/python/topi/testing/__init__.py
@@ -8,4 +8,4 @@ from .conv2d_hwcn_python import conv2d_hwcn_python
 from .conv2d_nchw_python import conv2d_nchw_python
 from .depthwise_conv2d_python import depthwise_conv2d_python_nchw, depthwise_conv2d_python_nhwc
 from .dilate_python import dilate_python
-from .softmax_python import softmax_python
+from .softmax_python import softmax_python, log_softmax_python
--- topi/tests/python/test_topi_softmax.py
+++ topi/tests/python/test_topi_softmax.py
@@ -36,5 +36,36 @@ def test_softmax():
     verify_softmax(3, 4)
 
 
+def verify_log_softmax(m, n):
+    A = tvm.placeholder((m, n), name='A')
+    B = topi.nn.log_softmax(A)
+    # confirm lower works
+    s = tvm.create_schedule([B.op])
+    tvm.lower(s, [A, B], simple_mode=True)
+
+    s = topi.cuda.schedule_softmax(B)
+
+    a_np = np.random.uniform(size=get_const_tuple(A.shape)).astype(A.dtype)
+    b_np = topi.testing.log_softmax_python(a_np)
+
+    def check_device(device):
+        if not tvm.module.enabled(device):
+            print("Skip because %s is not enabled" % device)
+            return
+        ctx = tvm.gpu(0) if device == "cuda" else tvm.cl(0)
+        a = tvm.nd.array(a_np, ctx)
+        b = tvm.nd.array(np.zeros(get_const_tuple(B.shape), dtype=B.dtype), ctx)
+        foo = tvm.build(s, [A, B], device, name="log_softmax")
+        foo(a, b)
+        np.testing.assert_allclose(b.asnumpy(), b_np, rtol=1e-5)
+
+    for device in ['cuda', 'opencl', 'metal']:
+        check_device(device)
+
+def test_log_softmax():
+    verify_log_softmax(32, 10)
+    verify_log_softmax(3, 4)
+
 if __name__ == "__main__":
     test_softmax()
+    test_log_softmax()
