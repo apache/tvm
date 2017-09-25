97c7c75170c6456f8b6a9a24ebbf490928f552f3
tqchen <tianqi.tchen@gmail.com>
2017-09-24 20:59:49 -0700

    [RUNTIME] Remove parameter def from runtime

--- src/runtime/graph/graph_runtime.h
+++ src/runtime/graph/graph_runtime.h
@@ -8,7 +8,6 @@
 #ifndef TVM_RUNTIME_GRAPH_GRAPH_RUNTIME_H_
 #define TVM_RUNTIME_GRAPH_GRAPH_RUNTIME_H_
 
-#include <dmlc/parameter.h>
 #include <string>
 
 namespace tvm {
@@ -20,18 +19,11 @@ constexpr uint64_t kTVMNDArrayMagic = 0xDD5E40F096B4A13F;
 constexpr uint64_t kTVMNDArrayListMagic = 0xF7E58D4F05049CB7;
 
 /*! \brief operator attributes about tvm op */
-struct TVMOpParam : public dmlc::Parameter<TVMOpParam> {
+struct TVMOpParam {
   std::string func_name;
   uint32_t num_inputs;
   uint32_t num_outputs;
   uint32_t flatten_data;
-
-  DMLC_DECLARE_PARAMETER(TVMOpParam) {
-    DMLC_DECLARE_FIELD(func_name);
-    DMLC_DECLARE_FIELD(num_inputs).set_default(1);
-    DMLC_DECLARE_FIELD(num_outputs).set_default(1);
-    DMLC_DECLARE_FIELD(flatten_data).set_default(0);
-  }
 };
 
 }  // namespace runtime
