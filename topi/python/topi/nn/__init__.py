65d1e46e9f7128c34aff67a0e77a098c35fc4ccc
ZihengJiang <jzhtomas@gmail.com>
2017-09-27 20:27:19 -0700

    [TOPI] Update conv schedule on rasp

--- topi/python/topi/nn/__init__.py
+++ topi/python/topi/nn/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import as _abs
 
 from .batch_norm import *
 from .conv2d import *
-from .depthwise_convolution import *
+from .depthwise_conv2d import *
 from .elemwise import *
 from .dilate import *
 from .flatten import *
