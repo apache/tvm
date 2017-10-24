25f95766c9e7349dbc1874688fe92540a97712a6
Tianqi Chen <tqchen@users.noreply.github.com>
2017-10-24 16:58:04 -0700

    [PYTHON] Allow no de-allocation when exit (#583)

--- python/tvm/_ffi/_ctypes/ndarray.py
+++ python/tvm/_ffi/_ctypes/ndarray.py
@@ -22,7 +22,7 @@ class NDArrayBase(object):
         self.is_view = is_view
 
     def __del__(self):
-        if not self.is_view:
+        if not self.is_view and _LIB:
             check_call(_LIB.TVMArrayFree(self.handle))
 
     @property
