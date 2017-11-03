c7ac2d389a369bb502a942092d8c694c8be7c478
Yuwei Hu <huyuwei1995@gmail.com>
2017-11-03 13:27:39 +0800

    [TOP] add conv2d_transpose (#217)
    * [TOP] add conv2d_transpose
    
    * update tvm
    
    * fix pylint

--- nnvm/tests/python/compiler/test_top_level2.py
+++ nnvm/tests/python/compiler/test_top_level2.py
@@ -54,6 +54,31 @@ def test_grouped_conv2d():
         np.testing.assert_allclose(out.asnumpy(), c_np, rtol=1e-5)
 
 
+def test_conv2d_transpose():
+    x = sym.Variable("x")
+    y = sym.conv2d_transpose(x, channels=10, kernel_size=(3,3), strides=(2,2),
+                             name="y", padding=(1,1), output_padding=(2,2))
+    dtype = "float32"
+    dshape = (1, 3, 18, 18)
+    kshape = (10, 3, 3, 3)
+    oshape = (1, 10, 37, 37)
+    shape_dict = {"x": dshape}
+    for target, ctx in ctx_list():
+        graph, lib, _ = nnvm.compiler.build(y, target, shape_dict)
+        m = graph_runtime.create(graph, lib, ctx)
+        data = tvm.nd.array(np.random.uniform(size=dshape).astype(dtype))
+        kernel = tvm.nd.array(np.random.uniform(size=kshape).astype(dtype))
+        bias = tvm.nd.array(np.random.uniform(size=kshape[0]).astype(dtype))
+        m.run(x=data, y_weight=kernel, y_bias=bias)
+        out = m.get_output(0, tvm.nd.empty(oshape, dtype))
+        c_np = topi.testing.conv2d_transpose_nchw_python(
+            data.asnumpy(), kernel.asnumpy(), 2, 1)
+        c_np = c_np + bias.asnumpy().reshape(kshape[0], 1, 1)
+        d_np = np.zeros(shape=oshape)
+        d_np[:,:,0:c_np.shape[2],0:c_np.shape[3]] = c_np
+        np.testing.assert_allclose(out.asnumpy(), d_np, rtol=1e-5)
+
+
 def test_max_pool2d():
     x = sym.Variable("x")
     y = sym.max_pool2d(x, pool_size=(2,2), strides=(2,2),
@@ -126,6 +151,7 @@ def test_global_avg_pool2d():
 if __name__ == "__main__":
     test_conv2d()
     test_grouped_conv2d()
+    test_conv2d_transpose()
     test_max_pool2d()
     test_avg_pool2d()
     test_global_max_pool2d()
