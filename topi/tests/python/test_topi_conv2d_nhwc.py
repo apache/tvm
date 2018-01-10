788b1538ffc0f87265b71d5d8dcdcd92692be75c
Masahiro Masuda <mmasuda@windows.zio.co.jp>
2018-01-10 13:34:53 +0900

    update nhwc conv test

--- topi/tests/python/test_topi_conv2d_nhwc.py
+++ topi/tests/python/test_topi_conv2d_nhwc.py
@@ -13,8 +13,6 @@ def verify_conv2d_nhwc(batch, in_channel, in_size, num_filter, kernel, stride, p
     A = tvm.placeholder((batch, in_height, in_width, in_channel), name='A')
     W = tvm.placeholder((kernel, kernel, in_channel, num_filter), name='W')
     B = topi.nn.conv2d_nhwc(A, W, stride, padding)
-    with tvm.target.create("llvm"):
-        s1 = topi.generic.schedule_conv2d_nhwc([B])
 
     a_shape = get_const_tuple(A.shape)
     w_shape = get_const_tuple(W.shape)
@@ -33,31 +31,28 @@ def verify_conv2d_nhwc(batch, in_channel, in_size, num_filter, kernel, stride, p
             print("Skip because %s is not enabled" % device)
             return
         print("Running on target: %s" % device)
+        with tvm.target.create(device):
+            s = topi.generic.schedule_conv2d_nhwc([B])
         ctx = tvm.context(device, 0)
         a = tvm.nd.array(a_np, ctx)
         w = tvm.nd.array(w_np, ctx)
         b = tvm.nd.array(np.zeros(get_const_tuple(B.shape), dtype=B.dtype), ctx)
-        func1 = tvm.build(s1, [A, W, B], device)
-        func1(a, w, b)
-        print(tvm.lower(s1, [A, W, B], simple_mode=True))
+        func = tvm.build(s, [A, W, B], device)
+        func(a, w, b)
         np.testing.assert_allclose(b.asnumpy(), b_np, rtol=1e-5)
-        timer_f = func1.time_evaluator(func1.entry_name, ctx, number=5)
-        t = timer_f(a, w, b).mean
-        print("NHWC Time: ", t)
 
     for device in ['llvm']:
         check_device(device)
 
 
 def test_conv2d_nhwc():
-    verify_conv2d_nhwc(12, 32, 256, 16, 3, 1, "SAME")
-    # verify_conv2d_nhwc(1, 256, 32, 256, 3, 1, "SAME")
-    # verify_conv2d_nhwc(4, 128, 16, 128, 5, 2, "SAME")
-    # verify_conv2d_nhwc(4, 128, 16, 256, 5, 2, "SAME")
-    # verify_conv2d_nhwc(1, 256, 32, 256, 3, 1, "VALID")
-    # verify_conv2d_nhwc(1, 256, 32, 256, 3, 1, "VALID")
-    # verify_conv2d_nhwc(4, 128, 16, 128, 5, 2, "VALID")
-    # verify_conv2d_nhwc(4, 128, 16, 256, 5, 2, "VALID")
+    verify_conv2d_nhwc(1, 256, 32, 256, 3, 1, "SAME")
+    verify_conv2d_nhwc(4, 128, 16, 128, 5, 2, "SAME")
+    verify_conv2d_nhwc(4, 128, 16, 256, 5, 2, "SAME")
+    verify_conv2d_nhwc(1, 256, 32, 256, 3, 1, "VALID")
+    verify_conv2d_nhwc(1, 256, 32, 256, 3, 1, "VALID")
+    verify_conv2d_nhwc(4, 128, 16, 128, 5, 2, "VALID")
+    verify_conv2d_nhwc(4, 128, 16, 256, 5, 2, "VALID")
 
 
 if __name__ == "__main__":
