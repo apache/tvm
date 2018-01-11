beb681c5471b6a69dc2829ac5a41201e23a6348b
Masahiro Masuda <masahi129@gmail.com>
2018-01-11 09:28:49 +0900

    remove skimage dependency

--- topi/python/topi/testing/upsampling_python.py
+++ topi/python/topi/testing/upsampling_python.py
@@ -1,8 +1,9 @@
 # pylint: disable=invalid-name, line-too-long, unused-variable, too-many-locals
 """Upsampling in python"""
 import numpy as np
-from skimage.transform import resize
 
+def upsample_nearest(arr, scale):
+    return arr.repeat(scale, axis=0).repeat(scale, axis=1)
 
 def upsampling_python(data, scale):
     ishape = data.shape
@@ -10,5 +11,5 @@ def upsampling_python(data, scale):
     output_np = np.zeros(oshape, dtype=data.dtype)
     for b in range(oshape[0]):
         for c in range(oshape[1]):
-            output_np[b, c, :, :] = resize(data[b, c, :, :], (oshape[2], oshape[3]), order=0, mode="reflect")
+            output_np[b, c, :, :] = upsample_nearest(data[b, c, :, :], scale)
     return output_np
