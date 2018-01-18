ab0b65c89e827b1c9cfbc4a2be21df40193ee514
thefiddler <sa@xcv.io>
2018-01-18 19:12:58 +0100

    Fix keras frontend elementwise-ops for lists with len>2  (fixes issue #325) (#326)
    * Added elementwise-add test
    
    * Fix typo
    
    * Fixed elem-wise ops for lists with len>2

--- nnvm/tests/python/frontend/keras/test_forward.py
+++ nnvm/tests/python/frontend/keras/test_forward.py
@@ -39,6 +39,27 @@ def verify_keras_frontend(keras_model):
         np.testing.assert_allclose(keras_out, tvm_out, rtol=1e-5, atol=1e-5)
 
 
+def test_forward_elemwise_add():
+    print("test_forward_elemwise_add")
+    r = []
+    data = keras.layers.Input(shape=(32,32,3))
+    x = keras.layers.Conv2D(8, (3, 3), padding="same")(data)
+    r.append(x)
+    x = keras.layers.Conv2D(8, (3, 3), padding="same")(x)
+    r.append(x)
+    x = keras.layers.Conv2D(8, (3, 3), padding="same")(x)
+
+    y = keras.layers.add([keras.layers.add([x, r[0]]), r[1]])
+    y = keras.layers.GlobalAveragePooling2D()(y)
+    keras_model = keras.models.Model(data, y)
+    verify_keras_frontend(keras_model)
+
+    y = keras.layers.add([x, r[0], r[1]])
+    y = keras.layers.GlobalAveragePooling2D()(y)
+    keras_model = keras.models.Model(data, y)
+    verify_keras_frontend(keras_model)
+
+
 def test_forward_softrelu():
     data = keras.layers.Input(shape=(32,32,3))
     x = keras.layers.Activation('softplus')(data)
@@ -114,6 +135,7 @@ def test_forward_resnet50():
 
 
 if __name__ == '__main__':
+    test_forward_elemwise_add()
     test_forward_softrelu()
     test_forward_leaky_relu()
     test_forward_dense()
