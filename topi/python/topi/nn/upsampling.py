079e230719755790420c703126d3a5f97cf0fdb3
masahi <masahi129@gmail.com>
2018-01-20 03:31:51 +0900

    simplify expr in get_const_tuple (#795)
    * fix upsampling output shape
    
    * simplify expr in get_const_tuple

--- topi/python/topi/nn/upsampling.py
+++ topi/python/topi/nn/upsampling.py
@@ -1,6 +1,7 @@
 """TVM operator upsampling compute."""
 from __future__ import absolute_import
 import tvm
+from .. import util
 
 
 def upsampling(data, scale):
@@ -21,8 +22,8 @@ def upsampling(data, scale):
         4-D with shape [batch, channel, in_height*scale, in_width*scale]
     """
     batch, channel, height, width = data.shape
-    out_height = height * scale
-    out_width = width * scale
+    out_height = util.simplify(height * scale)
+    out_width = util.simplify(width * scale)
 
     return tvm.compute((batch, channel, out_height, out_width), \
                         lambda n, c, h, w: data[n, c, h/scale, w/scale])
