3cff6ef6c78ce820c4ba1b1c684018406434f608
zhixunt <zhixunt@cmu.edu>
2018-01-25 16:43:22 -0500

    Test xvfb

--- tests/ci_build/install/ubuntu_install_opengl.sh
+++ tests/ci_build/install/ubuntu_install_opengl.sh
@@ -1,4 +1,5 @@
 apt-get update --fix-missing
 
 apt-get install -y --no-install-recommends --force-yes \
-        libgl1-mesa-dev libglfw3-dev
+        libgl1-mesa-dev libglu1-mesa-dev libglfw3-dev \
+        xvfb x11proto-xf86vidmode-dev xorg-dev
