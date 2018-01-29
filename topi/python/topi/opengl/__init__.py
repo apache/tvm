018321e54970aeecdb355613c898790e9b0302a1
zhixunt <zhixunt@cmu.edu>
2018-01-28 19:22:18 -0500

    Added OpenGL topi dense.

--- topi/python/topi/opengl/__init__.py
+++ topi/python/topi/opengl/__init__.py
@@ -4,3 +4,4 @@ from __future__ import absolute_import as _abs
 
 from .injective import schedule_injective, schedule_elemwise, schedule_broadcast
 from .softmax import schedule_softmax
+from .dense import schedule_dense
