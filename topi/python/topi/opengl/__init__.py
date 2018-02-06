7a1a6631079399241a17ac5b622a07267ab8a7c6
zhixunt <zhixunt@cmu.edu>
2018-02-06 18:31:23 -0500

    Add OpenGL topi pool.

--- topi/python/topi/opengl/__init__.py
+++ topi/python/topi/opengl/__init__.py
@@ -5,3 +5,4 @@ from __future__ import absolute_import as _abs
 from .injective import schedule_injective, schedule_elemwise, schedule_broadcast
 from .softmax import schedule_softmax
 from .dense import schedule_dense
+from .pooling import schedule_pool, schedule_global_pool
