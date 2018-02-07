bdbe4f6739d0e694afe221e8049d5538bf20a25e
zhixunt <zhixunt@cmu.edu>
2018-02-06 21:37:10 -0500

    Add Opengl topi conv2d_nchw.

--- topi/python/topi/opengl/__init__.py
+++ topi/python/topi/opengl/__init__.py
@@ -2,6 +2,7 @@
 """CUDA specific declaration and schedules."""
 from __future__ import absolute_import as _abs
 
+from .conv2d_nchw import schedule_conv2d_nchw
 from .injective import schedule_injective, schedule_elemwise, schedule_broadcast
 from .softmax import schedule_softmax
 from .dense import schedule_dense
