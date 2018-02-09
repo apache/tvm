2a263767e9ebe4d854249b0df3eed1866bc3941f
Masahiro Masuda <masahi129@gmail.com>
2018-02-09 23:55:46 +0900

    cleanup softmax

--- topi/python/topi/nn/softmax.py
+++ topi/python/topi/nn/softmax.py
@@ -10,7 +10,7 @@ def softmax(x, axis=-1):
     Parameters
     ----------
     data : tvm.Tensor
-        input can be 2d, 4d, or 5d
+        can be any dimension
 
     axis : int
         channel axis
@@ -20,108 +20,33 @@ def softmax(x, axis=-1):
     output : tvm.Tensor
         output shape is the same as input
     """
-    if axis == 1:
-        return softmax_nchw(x, axis)
-    elif axis == 3 or axis == 4:
-        return softmax_nhwc(x, axis)
-    assert len(x.shape) == 2 and axis == -1, "only support 2-dim axis == -1 softmax"
-    m, n = x.shape
-    k = tvm.reduce_axis((0, n), name='k')
-    max_elem = tvm.compute((m, ), lambda i: tvm.max(x[i, k], axis=k))
-    k = tvm.reduce_axis((0, n), name='k')
-    expsum = tvm.compute(
-        (m, ), lambda i: tvm.sum(tvm.exp(x[i, k] - max_elem[i]), axis=k))
-    return tvm.compute(
-        x.shape, lambda i, j: tvm.exp(x[i, j] - max_elem[i]) / expsum[i])
+    assert axis < len(x.shape)
+    shape = x.shape
+    if axis == -1:
+        axis = 1
+    k1 = tvm.reduce_axis((0, shape[axis]), name='k')
+    k2 = tvm.reduce_axis((0, shape[axis]), name='k')
 
+    def insert_reduce_index(indices, reduce_index):
+        return indices[:axis] + (reduce_index,) + indices[axis:]
 
-def softmax_nchw(x, axis):
-    """Perform softmax activation on the data in NCHW layout
+    def _compute_max(*indices):
+        eval_range = insert_reduce_index(indices, k1)
+        print("eval_range", eval_range)
+        return tvm.max(x[eval_range], axis=k1)
 
-    Parameters
-    ----------
-    data : tvm.Tensor
-        4d data in NCHW layout or
-        5d data in NCDHW laout
+    def _compute_expsum(max_elem, *indices):
+        eval_range = insert_reduce_index(indices, k2)
+        return tvm.sum(tvm.exp(x[eval_range] - max_elem[indices]), axis=k2)
 
-    axis : int
-        channel axis
+    def _normalize(max_elem, expsum, *indices):
+        non_reduce_indices = tuple([var for (i, var) in enumerate(indices) if i != axis])
+        return tvm.exp(x[indices] - max_elem[non_reduce_indices]) / expsum[non_reduce_indices]
 
-    Returns
-    -------
-    output : tvm.Tensor
-        output shape is the same as input
-    """
-    if len(x.shape) == 5:
-        assert axis == 1, "only support 5-dim axis == 1 softmax for NCDHW layout"
-        n, c, d, h, w = x.shape
-        k = tvm.reduce_axis((0, c), name='k')
-        max_elem = tvm.compute((n, d, h, w),
-                               lambda n, d, h, w: tvm.max(x[n, k, d, h, w], axis=k))
-        k = tvm.reduce_axis((0, c), name='k')
-        expsum = tvm.compute((n, d, h, w),
-                             lambda n, d, h, w:
-                             tvm.sum(tvm.exp(x[n, k, d, h, w] - max_elem[n, d, h, w]), axis=k))
-        return tvm.compute(x.shape,
-                           lambda n, c, d, h, w:
-                           tvm.exp(x[n, c, d, h, w] - max_elem[n, d, h, w]) / expsum[n, d, h, w])
-    elif len(x.shape) == 4:
-        assert axis == 1, "only support 4-dim axis == 1 softmax for NCHW layout"
-        n, c, h, w = x.shape
-        k = tvm.reduce_axis((0, c), name='k')
-        max_elem = tvm.compute((n, h, w), lambda n, h, w: tvm.max(x[n, k, h, w], axis=k))
-        k = tvm.reduce_axis((0, c), name='k')
-        expsum = tvm.compute((n, h, w),
-                             lambda n, h, w:
-                             tvm.sum(tvm.exp(x[n, k, h, w] - max_elem[n, h, w]), axis=k))
-        return tvm.compute(x.shape,
-                           lambda n, c, h, w:
-                           tvm.exp(x[n, c, h, w] - max_elem[n, h, w]) / expsum[n, h, w])
-
-
-def softmax_nhwc(x, axis):
-    """Perform softmax activation on the data in NHWC layout
-
-    Parameters
-    ----------
-    data : tvm.Tensor
-        4d data in NHWC layout or
-        5d data in NDHWC laout
-
-    axis : int
-        channel axis
-
-    Returns
-    -------
-    output : tvm.Tensor
-        output shape is the same as input
-    """
-    if len(x.shape) == 5:
-        assert axis == 4, "only support 5-dim axis == 4 softmax for NDHWC layout"
-        n, d, h, w, c = x.shape
-        k = tvm.reduce_axis((0, c), name='k')
-        max_elem = tvm.compute((n, d, h, w),
-                               lambda n, d, h, w:
-                               tvm.max(x[n, d, h, w, k], axis=k))
-        k = tvm.reduce_axis((0, c), name='k')
-        expsum = tvm.compute((n, d, h, w),
-                             lambda n, d, h, w:
-                             tvm.sum(tvm.exp(x[n, d, h, w, k] - max_elem[n, d, h, w]), axis=k))
-        return tvm.compute(x.shape,
-                           lambda n, d, h, w, c:
-                           tvm.exp(x[n, d, h, w, c] - max_elem[n, d, h, w]) / expsum[n, d, h, w])
-    elif len(x.shape) == 4:
-        assert axis == 3, "only support 4-dim axis == 3 softmax for NHWC layout"
-        n, h, w, c = x.shape
-        k = tvm.reduce_axis((0, c), name='k')
-        max_elem = tvm.compute((n, h, w), lambda n, h, w: tvm.max(x[n, h, w, k], axis=k))
-        k = tvm.reduce_axis((0, c), name='k')
-        expsum = tvm.compute((n, h, w),
-                             lambda n, h, w:
-                             tvm.sum(tvm.exp(x[n, h, w, k] - max_elem[n, h, w]), axis=k))
-        return tvm.compute(x.shape,
-                           lambda n, h, w, c:
-                           tvm.exp(x[n, h, w, c] - max_elem[n, h, w]) / expsum[n, h, w])
+    reduced_shape = tuple([dim for (i, dim) in enumerate(shape) if i != axis])
+    max_elem = tvm.compute(reduced_shape, _compute_max)
+    expsum = tvm.compute(reduced_shape, lambda *indices: _compute_expsum(max_elem, *indices))
+    return tvm.compute(shape, lambda *indices: _normalize(max_elem, expsum, *indices))
 
 
 @tvm.tag_scope(tag='log_softmax_output')
