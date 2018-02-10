d0f47b0865473f6dff5e794659e8348b5ed625a7
Alex Weaver <awsweaver@gmail.com>
2018-02-10 17:39:58 +0000

    Fix lint

--- python/tvm/build_module.py
+++ python/tvm/build_module.py
@@ -206,8 +206,8 @@ def build_config(**kwargs):
     config: BuildConfig
         The build configuration
     """
-    node_args = { k: v if k not in kwargs else kwargs[k] 
-        for k, v in BuildConfig._node_defaults.items() }
+    node_args = {k: v if k not in kwargs else kwargs[k] 
+                  for k, v in BuildConfig._node_defaults.items()}
     config = make.node("BuildConfig", **node_args)
 
     for k in kwargs:
