f6317d7b70b3e70c806d4dedb24bf2665a0892d6
pariksheet.pinjari@huawei.com <pariksheet.pinjari@huawei.com>
2018-02-23 15:43:40 +0530

    CI Issue, import issue with ... resolved for vision folder python files

--- topi/python/topi/nn/vision/region.py
+++ topi/python/topi/nn/vision/region.py
@@ -58,7 +58,7 @@ def region(data, num, classes, coords, background, _):
             tmp_index = tmp_index + 1
         if mode == group_last:
             temp_out = transform.concatenate(temp_out, 1)
-            temp_out_x = softmax(temp_out, axis=1)
+            temp_out_x = nn.softmax(temp_out, axis=1)
             temp_out = transform.split(temp_out_x, groups, 1)
             for index_1 in range(groups):
                 split_res[index - index_1] = temp_out[groups - index_1 - 1]
