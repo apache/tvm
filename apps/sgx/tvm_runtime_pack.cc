6731f6607acfad872ed930a9c0e01980f73632fc
nhynes <nhynes@berkeley.edu>
2018-03-06 08:57:18 -0800

    Add SGX runtime (#963)

--- apps/sgx/tvm_runtime_pack.cc
+++ apps/sgx/tvm_runtime_pack.cc
@@ -5,13 +5,7 @@
  *   Please refer to the Makefile (rule lib/tvm_runtime_pack.o) for how to build.
  *
  */
-#include "../../src/runtime/c_runtime_api.cc"
-#include "../../src/runtime/cpu_device_api.cc"
-#include "../../src/runtime/workspace_pool.cc"
-#include "../../src/runtime/module_util.cc"
-#include "../../src/runtime/module.cc"
-#include "../../src/runtime/registry.cc"
-#include "../../src/runtime/system_lib_module.cc"
+#include "../../sgx/sgx_runtime.cc"
 #ifndef _LIBCPP_SGX_CONFIG
 #include "../../src/runtime/file_util.cc"
 #endif
