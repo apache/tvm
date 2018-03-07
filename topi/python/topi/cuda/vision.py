119ff6e19526daaf00149fc53a4c656f1c8d50f0
pariksheet.pinjari@huawei.com <pariksheet.pinjari@huawei.com>
2018-03-07 11:35:58 +0530

    schedule_injective used instead of our own scheduler

--- topi/python/topi/cuda/vision.py
+++ topi/python/topi/cuda/vision.py
@@ -2,6 +2,7 @@
 """Schedule for vision operators"""
 from __future__ import absolute_import as _abs
 import tvm
+import topi
 from .. import tag
 from .. import generic
 
@@ -20,39 +21,7 @@ def schedule_reorg(outs):
     s: Schedule
         The computation schedule for reorg.
     """
-    target = tvm.target.current_target()
-    if target.target_name == "cuda" and "cublas" in target.libs:
-        return generic.schedule_extern(outs)
-    outs = [outs] if isinstance(outs, tvm.tensor.Tensor) else outs
-    s = tvm.create_schedule([x.op for x in outs])
-    def _schedule_reorg(reorg_op):
-        num_thread = 64#tvm.target.current_target(allow_none=False).max_num_threads
-        bx, tx = s[reorg_op].split(reorg_op.op.axis[0], factor=num_thread)
-        s[reorg_op].bind(bx, tvm.thread_axis("blockIdx.x"))
-        s[reorg_op].bind(tx, tvm.thread_axis((0, num_thread), "threadIdx.x"))
-        output = outs[0].op.output(0)
-        s[reorg_op].compute_at(s[output], s[output].op.axis[1])
-        tx, xi = s[output].split(output.op.axis[0], nparts=num_thread)
-        s[output].bind(tx, tvm.thread_axis((0, num_thread), "threadIdx.x"))
-        s[reorg_op].set_store_predicate(tx.var.equal(0))
-        s[output].set_store_predicate(tx.var.equal(0))
-
-    def _traverse(op):
-        # inline all one-to-one-mapping operators except the last stage (output)
-        if tag.is_injective(op.tag):
-            if op not in s.outputs:
-                s[op].compute_inline()
-            for tensor in op.input_tensors:
-                if tensor.op.input_tensors:
-                    _traverse(tensor.op)
-        # schedule reorg
-        elif op.tag == 'reorg':
-            reorg_op = op.output(0)
-            _schedule_reorg(reorg_op)
-        else:
-            raise RuntimeError("Unsupported operator: %s" % op.tag)
-    _traverse(outs[0].op)
-    return s
+    return topi.cuda.schedule_injective(outs)
 
 @generic.schedule_shortcut.register(["cuda", "gpu"])
 def schedule_shortcut(outs):
@@ -69,38 +38,7 @@ def schedule_shortcut(outs):
     s: Schedule
         The computation schedule for shortcut.
     """
-
-    outs = [outs] if isinstance(outs, tvm.tensor.Tensor) else outs
-    s = tvm.create_schedule([x.op for x in outs])
-
-    def _schedule_shortcut(shortcut_op):
-        num_thread = 64#tvm.target.current_target(allow_none=False).max_num_threads
-        if shortcut_op.op in s.outputs:
-            output = shortcut_op
-        else:
-            output = outs[0].op.output(0)
-            s[shortcut_op].compute_at(s[output], s[output].op.axis[1])
-        k = output.op.axis[0]
-        bx, tx = s[output].split(k, factor=num_thread)
-        s[output].bind(bx, tvm.thread_axis("blockIdx.x"))
-        s[output].bind(tx, tvm.thread_axis("threadIdx.x"))
-
-    def _traverse(op):
-        # inline all one-to-one-mapping operators except the last stage (output)
-        # schedule shortcut
-        if tag.is_injective(op.tag):
-            if op not in s.outputs:
-                s[op].compute_inline()
-            for tensor in op.input_tensors:
-                if tensor.op.input_tensors:
-                    _traverse(tensor.op)
-        elif op.tag == 'shortcut':
-            shortcut_op = op.output(0)
-            _schedule_shortcut(shortcut_op)
-        else:
-            raise RuntimeError("Unsupported operator: %s" % op.tag)
-    _traverse(outs[0].op)
-    return s
+    return topi.cuda.schedule_injective(outs)
 
 @generic.schedule_region.register(["cuda", "gpu"])
 def schedule_region(outs):
