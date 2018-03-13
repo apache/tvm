91efa2d686a816269fd9ae05fd6bb2039364103e
Wang <wayao@9801a7a9c287.ant.amazon.com>
2018-03-12 22:26:11 -0700

    Add MultiBoxDetection

---
+++ src/contrib/generic/algorithm.cc
@@ -0,0 +1,8 @@
+/*!
+ *  Copyright (c) 2017 by Contributors
+ * \file Use standard C library call.
+ */
+#include <tvm/runtime/registry.h>
+#include <tvm/runtime/util.h>
+#include <dmlc/logging.h>
+
--- topi/python/topi/vision/ssd/multibox.py
+++ topi/python/topi/vision/ssd/multibox.py
@@ -17,9 +17,9 @@ def multibox_prior_IR(data, out, sizes, ratios, steps, offsets):
     offset_h = offsets[0]
     offset_w = offsets[1]
 
-    with ib.for_range(0, in_height, for_type='parallel', name='i') as i:
+    with ib.for_range(0, in_height, for_type="parallel", name="i") as i:
         center_h = (i + offset_h) * steps_h
-        with ib.for_range(0, in_width, name='j') as j:
+        with ib.for_range(0, in_width, name="j") as j:
             center_w = (j + offset_w) * steps_w
             for k in range(num_sizes + num_ratios - 1):
                 w = tvm.select(k < num_sizes,
@@ -48,4 +48,91 @@ def multibox_prior(data, sizes=(1,), ratios=(1,), steps=(-1, -1), offsets=(0.5, 
     if clip:
         out = topi.clip(out, 0, 1)
     return out
+
+
+
+def multibox_detection_IR(cls_prob, loc_pred, anchor, out, clip, threshold,
+                          nms_threshold, force_suppress, variances, nms_topk):
+    def transform_loc(loc_pred, loc_base_idx, anchor, anchor_base_idx, clip, vx, vy, vw, vh):
+        al = anchor[anchor_base_idx]
+        at = anchor[anchor_base_idx + 1]
+        ar = anchor[anchor_base_idx + 2]
+        ab = anchor[anchor_base_idx + 3]
+        aw = ar - al
+        ah = ab - at
+        ax = (al + at) / 2.0
+        ay = (at + ab) / 2.0
+        px = loc_pred[loc_base_idx]
+        py = loc_pred[loc_base_idx + 1]
+        pw = loc_pred[loc_base_idx + 2]
+        ph = loc_pred[loc_base_idx + 3]
+        ox = px * vx * aw + ax
+        oy = py * vy * ah + ay
+        ow= math.exp(pw * vw) * aw / 2
+        oh = math.exp(ph * vh) * ah / 2
+        return tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, ox - ow)), ox - ow), \
+               tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, oy - oh)), oy - oh), \
+               tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, ox + ow)), ox + ow), \
+               tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, oy + oh)), oy + oh)
+
+    def calculate_overlap(box_a, box_b):
+        w = tvm.make.Max(0, tvm.make.Min(box_a[2], box_b[2])) - tvm.make.Max(box_a[0], box_b[0])
+        h = tvm.make.Max(0, tvm.make.Min(box_a[3], box_b[3])) - tvm.make.Max(box_a[1], box_b[1])
+        i = w * h
+        u = (box_a[2] - box_a[0]) * (box_a[3] - box_a[1]) + \
+            (box_b[2] - box_b[0]) * (box_b[3] - box_b[1]) - i
+        return tvm.select(u <= 0, 0.0, i / u)
+
+    batch_size = cls_prob.shape[0]
+    num_classes = cls_prob.shape[1]
+    num_anchors = cls_prob.shape[2]
+
+    ib = tvm.ir_builder.create()
+    p_out = ib.buffer_ptr(out)
+    temp_space = ib.allocate('float32', (num_anchors, 6), name="temp_space", scope="local")
+    inner_loop_type = "parallel" if batch_size == 1 else "serial"
+    with ib.for_range(0, batch_size, for_type="parallel", name="n") as n:
+        valid_count = 0
+        with ib.for_range(0, num_anchors, name="i") as i:
+            # Find the predicted class id and probability
+            score = -1
+            id = 0
+            with ib.for_range(0, num_classes, name="j") as j:
+                temp = cls_prob[n * num_classes * num_classes + j * num_classes + i]
+                id = tvm.select(temp > score, id, j)
+                score = tvm.make.Max(temp, score)
+            with ib.if_scope(id > 0 and score < threshold):
+                id = 0
+            # [id, prob, xmin, ymin, xmax, ymax]
+            # Remove background, restore original id
+            with ib.if_scope(id > 0):
+                out_base_idx = n * num_anchors * 6 + valid_count * 6
+                p_out[out_base_idx] = tvm.select(id > 0, id - 1, p_out[out_base_idx])
+                p_out[out_base_idx + 1] = tvm.select(id > 0, score, p_out[out_base_idx + 1])
+                offsets = i * 4
+                p_out[out_base_idx + 2], p_out[out_base_idx + 3], p_out[out_base_idx + 4], \
+                p_out[out_base_idx + 5] = transform_loc(loc_pred, n * num_anchors * 4 + offsets,
+                                                      anchor, offsets, clip, variances[0],
+                                                      variances[1], variances[2], variances[3])
+                valid_count += 1
+
+        with ib.if_scope(valid_count > 0 and nms_threshold > 0 and nms_threshold <= 1):
+            # Sort and apply NMS
+            with ib.for_range(0, num_anchors, name="i") as i:
+                with ib.for_range(0, 6, name="j") as j:
+                    temp_space[i * 6 + j] = p_out[n * num_anchors * 6 + i * 6 + j]
+            # sort confidence in descend order
+            sorter = temp_space
+            nkeep = sorter.shape[0]
+            with ib.if_scope( 0 < nms_topk < nkeep and nms_topk < nkeep):
+
+
+
+
+
+
+
+
+
+
 
