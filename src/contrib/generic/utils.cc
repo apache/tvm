f1fb766a397f936d0d0bd686a8c5558efccc6d7e
Wang <wayao@9801a7a9c287.ant.amazon.com>
2018-03-15 11:07:42 -0700

    Fix sort util

--- src/contrib/generic/utils.cc
+++ src/contrib/generic/utils.cc
@@ -20,26 +20,37 @@ bool SortElem<float>::is_descend = true;
 TVM_REGISTER_GLOBAL("tvm.contrib.generic.utils.stable_sort")
 .set_body([](TVMArgs args, TVMRetValue *ret) {
   DLTensor* input = args[0];
-  DLTensor* output = args[1];
-  int32_t num_elem = args[2];
-  int32_t sort_value_index = args[3];
-  bool is_descend = args[4];
+  DLTensor* num_elem = args[1];
+  DLTensor* output = args[2];
+  int32_t batch_size = args[3];
+  int32_t sort_value_index = args[4];
+  bool is_descend = args[5];
 
-  CHECK_EQ(input->ndim, 2) << "Currently only support sorting 2-D tensor.";
-  CHECK_EQ(output->ndim, 1) << "Ouput should be a 1-D tensor contains index of sorted elements.";
+  CHECK_EQ(input->ndim, 3) << "Currently only support sorting 3-D tensor. "
+      "The first dimension should be batch axis.";
+  CHECK_EQ(output->ndim, 2) << "Ouput should be a 2-D tensor contains index of sorted elements."
+      "The first dimension should be batch axis.";
+  CHECK_EQ(num_elem->ndim, 1) << "num_elem should be a 1-D tensor.";
 
   float* data_ptr = static_cast<float*>(input->data);
+  int32_t* num_elem_ptr = static_cast<int32_t*>(num_elem->data);
   SortElem<float>::is_descend = is_descend;
   std::vector<SortElem<float>> sorter;
 
-  for( int32_t i = 0; i < num_elem; ++i) {
-    sorter.emplace_back(SortElem<float>(*(data_ptr + i * input->shape[1] + sort_value_index), i));
+  for (int32_t i = 0; i < batch_size; ++i) {
+    sorter.clear();
+    int32_t num_elem_val = *(num_elem_ptr + i);
+    for (int32_t j = 0; j < num_elem_val; ++j) {
+      sorter.emplace_back(SortElem<float>(*(data_ptr + i * input->shape[1] * input->shape[2] +
+        j * input->shape[2] + sort_value_index), j));
+    }
+    std::stable_sort(sorter.begin(), sorter.end());
+
+    for( int32_t j = 0; j < num_elem_val; ++j) {
+      *(static_cast<int32_t*>(output->data) + i * input->shape[1] + j) = sorter[j].index;
+    }
   }
 
-  std::stable_sort(sorter.begin(), sorter.end());
-  for( int32_t i = 0; i < num_elem; ++i) {
-    *(static_cast<int32_t*>(output->data) + i) = sorter[i].index;
-  }
 });
 
 
--- topi/python/topi/vision/ssd/multibox.py
+++ topi/python/topi/vision/ssd/multibox.py
@@ -51,114 +51,144 @@ def multibox_prior(data, sizes=(1,), ratios=(1,), steps=(-1, -1), offsets=(0.5, 
     return out
 
 
-def multibox_detection_ir(cls_prob, loc_pred, anchor, out, clip, threshold,
-                          nms_threshold, force_suppress, variances, nms_topk):
-    def transform_loc(loc_pred, loc_base_idx, anchor, anchor_base_idx, clip, vx, vy, vw, vh):
+def transform_loc_ir(cls_prob, loc_pred, anchor, valid_count, out, clip, threshold, variances):
+    def transform_loc(loc, loc_base_idx, anchor, anchor_base_idx, clip, vx, vy, vw, vh):
         al = anchor[anchor_base_idx]
         at = anchor[anchor_base_idx + 1]
         ar = anchor[anchor_base_idx + 2]
         ab = anchor[anchor_base_idx + 3]
         aw = ar - al
         ah = ab - at
-        ax = (al + at) / 2.0
+        ax = (al + ar) / 2.0
         ay = (at + ab) / 2.0
-        px = loc_pred[loc_base_idx]
-        py = loc_pred[loc_base_idx + 1]
-        pw = loc_pred[loc_base_idx + 2]
-        ph = loc_pred[loc_base_idx + 3]
+        px = loc[loc_base_idx]
+        py = loc[loc_base_idx + 1]
+        pw = loc[loc_base_idx + 2]
+        ph = loc[loc_base_idx + 3]
         ox = px * vx * aw + ax
         oy = py * vy * ah + ay
-        ow= math.exp(pw * vw) * aw / 2
-        oh = math.exp(ph * vh) * ah / 2
+        ow= tvm.exp(pw * vw) * aw / 2.0
+        oh = tvm.exp(ph * vh) * ah / 2.0
         return tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, ox - ow)), ox - ow), \
                tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, oy - oh)), oy - oh), \
                tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, ox + ow)), ox + ow), \
                tvm.select(clip, tvm.make.Max(0, tvm.make.Min(1, oy + oh)), oy + oh)
-
-    def calculate_overlap(out_tensor, box_a_idx, box_b_idx):
-        w = tvm.make.Max(0, tvm.make.Min(out_tensor[box_a_idx + 2], out_tensor[box_b_idx + 2])) \
-            - tvm.make.Max(out_tensor[box_a_idx], out_tensor[box_b_idx])
-        h = tvm.make.Max(0, tvm.make.Min(out_tensor[box_a_idx + 3], out_tensor[box_b_idx + 3])) \
-            - tvm.make.Max(out_tensor[box_a_idx + 1], out_tensor[box_b_idx + 1])
-        i = w * h
-        u = (out_tensor[box_a_idx + 2], out_tensor[box_a_idx]) * \
-            (out_tensor[box_a_idx + 3], out_tensor[box_a_idx + 1]) + \
-            (out_tensor[box_b_idx + 2], out_tensor[box_b_idx]) * \
-            (out_tensor[box_b_idx + 3], out_tensor[box_b_idx] + 1) - i
-        return tvm.select(u <= 0, 0.0, i / u)
 
     batch_size = cls_prob.shape[0]
     num_classes = cls_prob.shape[1]
     num_anchors = cls_prob.shape[2]
 
     ib = tvm.ir_builder.create()
+    p_cls_prob = ib.buffer_ptr(cls_prob)
+    p_loc_pred = ib.buffer_ptr(loc_pred)
+    p_anchor = ib.buffer_ptr(anchor)
+    p_valid_count = ib.buffer_ptr(valid_count)
     p_out = ib.buffer_ptr(out)
     inner_loop_type = "parallel" if batch_size == 1 else "serial"
     with ib.for_range(0, batch_size, for_type="parallel", name="n") as n:
-        valid_count = 0
+        p_valid_count[n] = 0
         with ib.for_range(0, num_anchors, name="i") as i:
             # Find the predicted class id and probability
-            score = -1
-            id = 0
+            score = ib.allocate('float32', (1,), name="score", scope="local")
+            id = ib.allocate('int32', (1,), name="id", scope="local")
+            score[0] = -1.0
+            id[0] = 0
             with ib.for_range(0, num_classes, name="j") as j:
-                temp = cls_prob[n * num_classes * num_classes + j * num_classes + i]
-                id = tvm.select(temp > score, id, j)
-                score = tvm.make.Max(temp, score)
-            with ib.if_scope(id > 0 and score < threshold):
-                id = 0
+                with ib.if_scope(j > 0):
+                    temp = p_cls_prob[n * num_anchors * num_classes + j * num_anchors + i]
+                    id[0] = tvm.select(temp > score[0], j, id[0])
+                    score[0] = tvm.make.Max(temp, score[0])
+            with ib.if_scope(tvm.all(id[0] > 0, score[0] < threshold)):
+                id[0] = 0
             # [id, prob, xmin, ymin, xmax, ymax]
             # Remove background, restore original id
-            with ib.if_scope(id > 0):
-                out_base_idx = n * num_anchors * 6 + valid_count * 6
-                p_out[out_base_idx] = tvm.select(id > 0, id - 1, p_out[out_base_idx])
-                p_out[out_base_idx + 1] = tvm.select(id > 0, score, p_out[out_base_idx + 1])
-                offsets = i * 4
+            with ib.if_scope(id[0] > 0):
+                out_base_idx = n * num_anchors * 6 + p_valid_count[n] * 6
+                p_out[out_base_idx] = id[0] - 1.0
+                p_out[out_base_idx + 1] = score[0]
+                offset = i * 4
                 p_out[out_base_idx + 2], p_out[out_base_idx + 3], p_out[out_base_idx + 4], \
-                p_out[out_base_idx + 5] = transform_loc(loc_pred, n * num_anchors * 4 + offsets,
-                                                      anchor, offsets, clip, variances[0],
-                                                      variances[1], variances[2], variances[3])
-                valid_count += 1
+                p_out[out_base_idx + 5] = transform_loc(p_loc_pred, n * num_anchors * 4 + offset,
+                                                        p_anchor, offset, clip, variances[0],
+                                                        variances[1], variances[2], variances[3])
+                p_valid_count[n] += 1
 
-        with ib.if_scope(valid_count > 0 and 0 < nms_threshold <= 1):
-            # Sort and apply NMS
-            temp_space = ib.allocate('float32', (valid_count, 6), name="temp_space", scope="local")
-            with ib.for_range(0, valid_count, name="i") as i:
-                with ib.for_range(0, 6, name="j") as j:
-                    temp_space[i * 6 + j] = p_out[n * num_anchors * 6 + i * 6 + j]
-            # Sort confidence in descend order
-            sort_result = tvm.extern((valid_count,), [temp_space], lambda ins, outs: \
-                tvm.intrin.call_packed("tvm.contrib.generic.utils.stable_sort",
-                                       ins[0], outs[0], 1, True), dtype='int64', name="C")
-            # Redorder output
-            nkeep = sort_result.shape[0]
-            with ib.if_scope( 0 < nms_topk < nkeep):
-                nkeep = nms_topk
-            with ib.for_range(0, nkeep, name="i") as i:
-                with ib.for_range(0, 6, name="j") as j:
-                    p_out[n * num_anchors * 6 + i * 6 + j] = temp_space[sort_result[i] * 6 + j]
+    return ib.get()
+
+
+def nms_ir(inter_out, sort_result, valid_count, out, nms_threshold, force_suppress, nms_topk):
+    def calculate_overlap(out_tensor, box_a_idx, box_b_idx):
+        w = tvm.make.Max(0.0, tvm.make.Min(out_tensor[box_a_idx + 2], out_tensor[box_b_idx + 2])
+                         - tvm.make.Max(out_tensor[box_a_idx], out_tensor[box_b_idx]))
+        h = tvm.make.Max(0.0, tvm.make.Min(out_tensor[box_a_idx + 3], out_tensor[box_b_idx + 3])
+                         - tvm.make.Max(out_tensor[box_a_idx + 1], out_tensor[box_b_idx + 1]))
+        i = w * h
+        u = (out_tensor[box_a_idx + 2] - out_tensor[box_a_idx]) * \
+            (out_tensor[box_a_idx + 3] - out_tensor[box_a_idx + 1]) + \
+            (out_tensor[box_b_idx + 2] - out_tensor[box_b_idx]) * \
+            (out_tensor[box_b_idx + 3] - out_tensor[box_b_idx + 1]) - i
+        return tvm.select(u <= 0.0, 0.0, i / u)
+
+    ib = tvm.ir_builder.create()
+    p_inter_out = ib.buffer_ptr(inter_out)
+    p_sort_result = ib.buffer_ptr(sort_result)
+    p_valid_count = ib.buffer_ptr(valid_count)
+    p_out = ib.buffer_ptr(out)
+    batch_size = out.shape[0]
+    num_anchors = out.shape[1]
+
+    nms_threshold_node = tvm.make.node("FloatImm", dtype="float32", value=nms_threshold)
+    nms_topk_node = tvm.make.node("IntImm", dtype="int32", value=nms_topk)
+    force_suppress_node = tvm.make.node("IntImm", dtype="int32", value=1 if force_suppress else 0)
+    with ib.for_range(0, batch_size, for_type="parallel", name="n") as n:
+        with ib.if_scope(tvm.all(nms_threshold_node > 0, nms_threshold_node < 1, p_valid_count[0] > 0)):
+            # Reorder output
+            nkeep = ib.allocate('int32', (1,), name="nkeep", scope="local")
+            nkeep[0] = p_valid_count[n]
+            with ib.if_scope(tvm.all(nms_topk_node > 0, nms_topk_node < p_valid_count[n])):
+                nkeep[0] = nms_topk
+            with ib.for_range(0, nkeep[0], name="l") as l:
+                with ib.for_range(0, 6, name="m") as m:
+                    p_out[n * num_anchors * 6 + l * 6 + m] = p_inter_out[p_sort_result[n * num_anchors + l] * 6 + m]
             # Apply nms
-            with ib.for_range(0, valid_count, name="i") as i:
-                offset_i = i * 6
-                with ib.if_scope(p_out[n * num_anchors * 6 + offset_i] >= 0):
-                    with ib.for_range(i + 1, valid_count, name="j") as j:
-                        offset_j = j * 6
-                        with ib.if_scope(p_out[n * num_anchors * 6 + offset_j] >= 0):
-                            with ib.if_scope(force_suppress or p_out[n * num_anchors * 6 + offset_i]
-                                             == p_out[n * num_anchors * 6 + offset_j]):
+            with ib.for_range(0, p_valid_count[n], name="l") as l:
+                offset_l = l * 6
+                with ib.if_scope(p_out[n * num_anchors * 6 + offset_l] >= 0):
+                    with ib.for_range(0, p_valid_count[n], name="m") as m:
+                        offset_m = m * 6
+                        with ib.if_scope(tvm.all(m > l, p_out[n * num_anchors * 6 + offset_m] >= 0)):
+                            with ib.if_scope(tvm.any(force_suppress_node > 0,
+                                                     p_out[n * num_anchors * 6 + offset_l]
+                                                     == p_out[n * num_anchors * 6 + offset_m]
+                                                     )):
                                 # When force_suppress == True or class_id equals
-                                iou = calculate_overlap(p_out, n * num_anchors * 6 + offset_i + 2,
-                                                        n * num_anchors * 6 + offset_j + 2)
+                                iou = calculate_overlap(p_out, n * num_anchors * 6 + offset_l + 2,
+                                                        n * num_anchors * 6 + offset_m + 2)
                                 with ib.if_scope(iou >= nms_threshold):
-                                    p_out[n * num_anchors * 6 + offset_j] = -1
+                                    p_out[n * num_anchors * 6 + offset_m] = -1.0
+        with ib.else_scope():
+            with ib.for_range(0, num_anchors, name="l") as l:
+                with ib.for_range(0, 6, name="m") as m:
+                    p_out[n * num_anchors * 6 + l * 6 + m] = p_inter_out[n * num_anchors * 6 + l * 6 + m]
+    return ib.get()
 
 
 @tvm.target.generic_func
 def multibox_detection(cls_prob, loc_pred, anchor, clip=True, threshold=0.01, nms_threshold=0.5,
                        force_suppress=False, variances=(0.1, 0.1, 0.2, 0.2), nms_topk=-1):
     batch_size = cls_prob.shape[0]
-    num_anchor = anchor.shape[1]
-    oshape = (batch_size, num_anchor, 6)
-    out = tvm.extern(oshape, [cls_prob, loc_pred, anchor], lambda ins, outs:
-                     multibox_detection_ir(ins[0], ins[1], ins[2], outs[0], clip, threshold, nms_threshold,
-                                           force_suppress, variances, nms_topk), tag="multibox_detection")
+    num_anchors = anchor.shape[1]
+    oshape = (batch_size, num_anchors, 6)
+    valid_count, inter_out = tvm.extern([(batch_size,), oshape], [cls_prob, loc_pred, anchor], lambda ins, outs:
+    transform_loc_ir(ins[0], ins[1], ins[2], outs[0], outs[1],
+                     clip, threshold, variances),
+                                        dtype=["int32", "float32"],
+                                        tag="multibox_detection_transform_loc")
+    sort_tensor = tvm.extern((batch_size, num_anchors), [inter_out, valid_count], lambda ins, outs:
+    tvm.call_packed("tvm.contrib.generic.utils.stable_sort",ins[0], ins[1],
+                    outs[0], batch_size, 1, True),
+                             dtype='int32', name="multibox_detection_sort")
+    out = tvm.extern(oshape, [inter_out, sort_tensor, valid_count], lambda ins, outs:
+    nms_ir(ins[0], ins[1], ins[2], outs[0], nms_threshold, force_suppress, nms_topk),
+                     dtype="float32", tag="multibox_detection_nms")
     return out
