971b615432b8a6b621b64edb265c5abcd38625b7
Tianqi Chen <tqchen@users.noreply.github.com>
2018-03-16 23:06:01 -0700

    [DOCS] Initial docs (#4)
    * [DOCS] Initial docs
    
    * update instruction

---
+++ vta/docs/index.rst
@@ -0,0 +1,20 @@
+VTA Documentation
+=================
+
+Welcome to VTA documentation.
+
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 1
+
+   self
+   how_to/install
+   tutorials/index
+   how_to/contribute
+   api/python/index
+   dev/index
+   api_links
+   genindex
---
+++ vta/tutorials/get_started.py
@@ -0,0 +1,39 @@
+"""
+Get Started with VTA
+====================
+**Author**: `Tianqi Chen <https://tqchen.github.io>`_
+
+This is an introduction tutorial to on how to use TVM to program VTA
+
+In this tutorial, we will demonstrate the basic workflow of VTA
+and how we can program the FPGA to run various instructions.
+
+To begin with, we need to import tvm which is our compiler stack for VTA.
+We also need to import vta python package which contains VTA specific
+extensions for compiler to generate code that runs on VTA.
+"""
+from __future__ import absolute_import, print_function
+
+import tvm
+import vta
+
+######################################################################
+# Program the FPGA with VTA Bistream
+# ----------------------------------
+# In the first step, we need to program the FPGA with VTA bitstream.
+#
+
+######################################################################
+# Run Simple Copy Instruction
+# ---------------------------
+#
+
+######################################################################
+# Run Matrix Instruction
+# ----------------------
+#
+
+######################################################################
+# Matrix Multiplication Example
+# -----------------------------
+#
