abe4cf969595600271bf4e8825b5e0e2ef502702
Pariksheet Pinjari <pariksheet.pinjari@huawei.com>
2018-03-27 23:49:52 +0530

    [TOPI] PReLU Support (#1008)

--- topi/tests/python_cpp/test_topi_relu.py
+++ topi/tests/python_cpp/test_topi_relu.py
@@ -50,6 +50,28 @@ def verify_leaky_relu(m, alpha):
     foo(a, b)
     np.testing.assert_allclose(b.asnumpy(), b_np, rtol=1e-5)
 
+def verify_prelu(x, w):
+    X = tvm.placeholder((x), name='X')
+    W = tvm.placeholder((w), name='W')
+    x_np = np.random.uniform(low=-1.0, high=1.0, size=get_const_tuple(X.shape)).astype(X.dtype)
+    w_np = np.random.uniform(low=-1.0, high=1.0, size=get_const_tuple(W.shape)).astype(W.dtype)
+    def _prelu_numpy(x, W):
+        return (x < 0) * (x *W.reshape(3, 1, 1)) + (x>=0) * x
+
+    out_np = _prelu_numpy(x_np, w_np)
+    B = topi.cpp.nn.prelu(X, W)
+    device = "llvm"
+    target = topi.cpp.TEST_create_target(device)
+    s = topi.cpp.generic.schedule_injective(target, [B])
+
+    ctx = tvm.cpu(0)
+    x_tvm = tvm.nd.array(x_np, ctx)
+    w_tvm = tvm.nd.array(w_np, ctx)
+
+    b = tvm.nd.array(np.zeros(get_const_tuple(X.shape), dtype=B.dtype), ctx)
+    foo = tvm.build(s, [X, W, B], "llvm", name="prelu")
+    foo(x_tvm, w_tvm, b)
+    np.testing.assert_allclose(b.asnumpy(), out_np, rtol=1e-5)
 
 def test_relu():
     for dtype in ['float32', 'float64', 'int32', 'int16', 'int8', 'int64']:
@@ -58,7 +80,10 @@ def test_relu():
 def test_leaky_relu():
     verify_leaky_relu(100, 0.1)
 
+def test_prelu():
+    verify_prelu((1, 3, 2, 2), (3,))
 
 if __name__ == "__main__":
     test_relu()
     test_leaky_relu()
+    test_prelu()
