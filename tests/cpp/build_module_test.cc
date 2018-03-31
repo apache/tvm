e44f2abcd677a735d9f6fdf3c2c656ec70c4c988
Eddie Yan <eqy@cs.washington.edu>
2018-03-30 23:11:03 -0700

    try fixing getting getting repr

--- tests/cpp/build_module_test.cc
+++ tests/cpp/build_module_test.cc
@@ -34,7 +34,9 @@ TEST(BuildModule, Basic) {
   auto module = build(lowered, target, Target(), config);
 
   auto mali_target = Target::create("opencl -model=Mali-T860MP4@800Mhz -device=mali");
-  CHECK_EQ(mali_target._format_str(), "opencl -model=Mali-T860MP4@800Mhz -device=mali"); 
+  std::ostringstram os;
+  os << mali_target.operator NodeRef();
+  CHECK_EQ(os.str(), "opencl -model=Mali-T860MP4@800Mhz -device=mali"); 
 }
 
 
