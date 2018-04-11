f1afda9187fa11ac2155851c44a39f0e651c1889
kun-zh <zhangkun01@hotmail.com>
2018-04-11 21:23:04 +0800

    fix regression failure

--- src/schedule/message_passing.cc
+++ src/schedule/message_passing.cc
@@ -479,11 +479,11 @@ std::vector<Expr> MakeBoundCheck(
       Expr value = ComputeExpr<Sub>(value_map.at(iv), iv->dom->min);
       Expr vmin = EvalSet(value, iset_dmap).min();
       Expr vmax = EvalSet(value, iset_dmap).max();
-      if (vmin.type() != value.type() || !can_prove(vmin > (iv->dom->min - 1))) {
-        preds.emplace_back(value > (iv->dom->min - 1));
+      if (vmin.type() != value.type() || !can_prove(vmin >= iv->dom->min)) {
+        preds.emplace_back(value >= 0);
       }
       if (vmax.type() != value.type() || !can_prove(vmax < iv->dom->extent)) {
-        preds.emplace_back(value < iv->dom->extent);
+        preds.emplace_back(value < (iv->dom->extent - iv->dom->min));
       }
     }
   }
