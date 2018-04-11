f1928add0d47af01b2db6c3790f9506c90d97a4b
Pariksheet Pinjari <pariksheet.pinjari@huawei.com>
2018-04-11 12:26:00 +0530

    Used broadcasting semantics in LRN testcases

--- topi/tests/python/test_topi_lrn.py
+++ topi/tests/python/test_topi_lrn.py
@@ -33,44 +33,33 @@ def lrn_python(a_np, size, axis, bias, alpha, beta):
         4-D with shape [batch, out_channel, out_height, out_width]
     """
     axis0, axis1, axis2, axis3 = a_np.shape
-    radius = int(size / 2)
+    radius = size // 2
     sqr_sum = np.zeros(shape=a_np.shape).astype(a_np.dtype)
     sqr_sum_up = np.zeros(shape=a_np.shape).astype(a_np.dtype)
     lrn_out = np.zeros(shape=a_np.shape).astype(a_np.dtype)
-    def add_dot_values(i, j, k, l):
+    def sum_dot_values(i, j, k, l):
+        axis_size = a_np.shape[axis]
         if (axis == 1):
             #NCHW layout
-            for rxj in range(-radius, radius+1):
-                if ((j + rxj) < axis1) and ((j + rxj) >= 0):
-                    sqr_sum[i, j, k, l] = sqr_sum[i, j, k, l] + \
-                                           (a_np[i, j + rxj, k, l] * \
-                                            a_np[i, j + rxj, k, l])
+            sum_start = j-radius if j-radius >= 0 else 0
+            sum_end = j+radius+1 if j+radius+1 < axis_size else axis_size
+            sqr_sum[i, j, k, l] = sum(a_np[i, sum_start:sum_end, k, l] * \
+                                      a_np[i, sum_start:sum_end, k, l])
         elif (axis == 3):
             #NHWC layout
-            for rxl in range(-radius, radius+1):
-                if ((l + rxl) < axis3) and ((l + rxl) >= 0):
-                    sqr_sum[i, j, k, l] = sqr_sum[i, j, k, l] + \
-                                           (a_np[i, j, k, l + rxl] * \
-                                            a_np[i, j, k, l + rxl])
+            sum_start = l-radius if l-radius >= 0 else 0
+            sum_end = l+radius+1 if l+radius+1 < axis_size else axis_size
+            sqr_sum[i, j, k, l] = sum(a_np[i, j, k, sum_start:sum_end] * \
+                                      a_np[i, j, k, sum_start:sum_end])
+
     for i in range(axis0):
         for j in range(axis1):
             for k in range(axis2):
                 for l in range(axis3):
-                    add_dot_values(i, j, k, l)
-    for i in range(axis0):
-        for j in range(axis1):
-            for k in range(axis2):
-                for l in range(axis3):
-                    sqr_sum_up[i, j, k, l] = \
-                        np.power((bias + (alpha * sqr_sum[i, j, k, l] / \
-                                          size)), beta)
-    for i in range(axis0):
-        for j in range(axis1):
-            for k in range(axis2):
-                for l in range(axis3):
-                    lrn_out[i, j, k, l] = a_np[i, j, k, l] / \
-                                           sqr_sum_up[i, j, k, l]
-    return lrn_out
+                    sum_dot_values(i, j, k, l)
+
+    sqr_sum_up = np.power((bias + (alpha * sqr_sum /size)), beta)
+    return np.divide(a_np, sqr_sum_up)
 
 def verify_lrn(shape, size, axis, bias, alpha, beta):
     A = tvm.placeholder(shape, name='A')
