d987d3cdf719df8a70ddeafc900a68961336f5ea
libing4752 <libing475211023@sjtu.edu.cn>
2018-04-11 22:32:37 +0800

    remove white space in storage_rewrite

--- src/pass/storage_rewrite.cc
+++ src/pass/storage_rewrite.cc
@@ -595,12 +595,12 @@ class StoragePlanRewriter : public IRMutator {
     CHECK_NE(e->const_nbits, 0U);
     MemoryInfo info = GetMemoryInfo(e->scope.to_string());
     uint64_t total_bits = e->const_nbits;
-    // By default, align to 32 bits.	
+    // By default, align to 32 bits.
     size_t align = 32;
     if (info.defined()) {
       align = info->max_simd_bits;
     }
-    // Always align to max_simd_bits	
+    // Always align to max_simd_bits
     // so we can remap types by keeping this property
     if (total_bits % align != 0) {
       total_bits += align  - (total_bits % align);
