01e1699d4721b5482bd4865e3b05726a60032a0b
kun-zh <32951065+kun-zh@users.noreply.github.com>
2018-04-12 10:12:24 +0800

    Generate Lower Bound Conditions for issue 1014 (#1091)

--- src/schedule/message_passing.cc
+++ src/schedule/message_passing.cc
@@ -477,9 +477,14 @@ std::vector<Expr> MakeBoundCheck(
     CHECK(iv->dom.defined());
     if (!skip_ivar_domain && !iv->dom.same_as(dom)) {
       Expr value = ComputeExpr<Sub>(value_map.at(iv), iv->dom->min);
-      Expr vmax = EvalSet(value, iset_dmap).max();
+      IntSet s = EvalSet(value, iset_dmap);
+      Expr vmin = s.min();
+      Expr vmax = s.max();
+      if (vmin.type() != value.type() || !can_prove(vmin >= iv->dom->min)) {
+        preds.emplace_back(value >= 0);
+      }
       if (vmax.type() != value.type() || !can_prove(vmax < iv->dom->extent)) {
-        preds.emplace_back(value < iv->dom->extent);
+        preds.emplace_back(value < (iv->dom->extent - iv->dom->min));
       }
     }
   }
