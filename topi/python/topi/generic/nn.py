20a482df944e983a27872a40c17b385c7fb7c7e5
Yizhi Liu <yizhiliu@amazon.com>
2018-04-21 21:46:14 -0700

    add layout_transform. add schedule support for ndim>=5 for x86

--- topi/python/topi/generic/nn.py
+++ topi/python/topi/generic/nn.py
@@ -54,6 +54,32 @@ def schedule_conv2d_nhwc(outs):
 
 
 @tvm.target.generic_func
+def schedule_conv2d_NCHWc(num_filter, kernel_size, strides, padding, outs):
+    """Schedule for conv2d_NCHW[x]c
+    
+    Parameters
+    ----------
+    num_filter: int
+                The number of filter, i.e., the output channel.
+    kernel_size: tuple of int
+                 (kernel_height, kernel_width)
+    strides: tuple of int
+             (stride_of_height, stride_of_width)
+    padding: tuple of int
+             (pad_of_height, pad_of_width)
+    outs: Array of Tensor
+          The computation graph description of conv2d_NCHWc
+          in the format of an array of tensors.
+    
+    Returns
+    -------
+    sch: Schedule
+        The computation schedule for the op.
+    """
+    return _default_schedule(outs, False)
+
+
+@tvm.target.generic_func
 def schedule_conv2d_transpose_nchw(outs):
     """Schedule for conv2d_transpose_nchw
 
