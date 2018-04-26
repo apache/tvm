dc601bb4e1b5490df1e481d9aa1bd41bd8d8754a
Jian Weng <jian.weng@ucla.edu>
2018-04-26 15:12:33 -0700

    veriable to single-celled arry buffer done; further tests are required

--- python/tvm/contrib/pyfrontend.py
+++ python/tvm/contrib/pyfrontend.py
@@ -4,37 +4,90 @@ from .. import expr as _expr
 from .. import stmt as _stmt
 from .. import make as _make
 from .. import api  as _api
+from .. import ir_pass  as _ir_pass
+
+def _list_to_block(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        body = lst[0]
+        for i in lst[1:]:
+            body = _make.Block(body, i)
+        return body
+
+
+# The AST visitor that detects the status of each variable, which is either
+# read only, or read and write.
+class PyVariableUsage(ast.NodeVisitor):
+    def __init__(self):
+        self.declared_vars = {}
+        self.loops_above = []
+        self.in_loop_decl = False
+    
+    def visit_For(self, node):
+        self.in_loop_decl = True
+        self.visit(node.target)
+        self.in_loop_decl = False
+
+        for i in node.body:
+            self.visit(i)
+
+        self.loops_above.pop()
+
+    def visit_Name(self, node):
+        if self.in_loop_decl:
+            self.loops_above.append(node.id)
+            return
+
+        # The loop variable cannot be overwritten when iteration
+        if isinstance(node.ctx, ast.Store):
+            assert node.id not in self.loops_above
+
+        if node.id not in self.declared_vars:
+            if isinstance(node.ctx, ast.Store):
+                self.declared_vars[node.id] = set()
+        else:
+            self.declared_vars[node.id].add(type(node.ctx))
+
+def _find_all_variable_decl(ir):
+    visitor = PyVariableUsage()
+    visitor.visit(ir)
+    return visitor.declared_vars
 
 class PyAST2HalideIR(ast.NodeVisitor):
 
     _binop_maker = {
-        ast.Add   :_make.Add,
-        ast.Sub   :_make.Sub,
-        ast.Mult  :_make.Mul,
-        ast.Div   :_make.Div,
-        ast.Mod   :_make.Mod,
-        ast.BitOr :operator.or_,
-        ast.BitAnd:operator.and_,
-        ast.BitXor:operator.xor
+        ast.Add   : _make.Add,
+        ast.Sub   : _make.Sub,
+        ast.Mult  : _make.Mul,
+        ast.Div   : _make.Div,
+        ast.Mod   : _make.Mod,
+        ast.BitOr : operator.or_,
+        ast.BitAnd: operator.and_,
+        ast.BitXor: operator.xor
     }
 
-    def __init__(self, inputs):
-        self.converted     = []
-        self.scope         = {}
-        self.variables     = set([])
-        self.inputs        = inputs
+    def __init__(self, inputs, usage):
+        self.input_param = {}
+        self.rw_status   = usage
+        self.vars_buffer = {}
+        self.vars_const  = {}
+        self.inputs      = inputs
+        self.loop_levels = []
 
     def visit_Module(self, node):
-        for i in node.body:
-            halide_stmt = self.visit(i)
-            self.converted.append(halide_stmt)
-        return self.converted
+        res = [self.visit(i) for i in node.body]
+        return _list_to_block(res)
 
     def visit_FunctionDef(self, node):
         assert len(node.args.args) == len(self.inputs)
         for idx, arg in enumerate(node.args.args):
-            self.scope[arg.arg] = self.inputs[idx]
+            self.input_param[arg.arg] = self.inputs[idx]
         res = [self.visit(i) for i in node.body]
+        res = _list_to_block(res)
+        one = _make.range_by_min_extent(0, 1)
+        for k, v in self.vars_buffer.items():
+            res = _make.Realize(v.op, 0, v.dtype, [one], _api.convert(True), res)
         return res
 
     def visit_Expr(self, node):
@@ -42,34 +95,56 @@ class PyAST2HalideIR(ast.NodeVisitor):
 
     def visit_Name(self, node):
         #print(node.id)
-        if node.id not in self.scope.keys():
+        if node.id in self.input_param.keys() and isinstance(self.input_param[node.id], _expr.Var):
+            return self.input_param[node.id]
+        elif node.id in self.loop_levels:
             return _api.var(node.id)
-        elif isinstance(self.scope[node.id], _expr.Var):
-            return self.scope[node.id]
         else:
-            _buf = self.scope[node.id]
-            return _make.Call(_buf.dtype, _buf.name, [_api.const(0)], _expr.Call.PureExtern, None, 0)
+            assert node.id not in self.input_param.keys()
+            assert node.id in self.rw_status.keys()
+            assert node.id in self.vars_buffer.keys() or node.id in self.vars_const.keys()
+            if node.id in self.vars_buffer.keys():
+                _buf = self.vars_buffer[node.id]
+                return _make.Call(_buf.dtype, node.id, [_api.const(0)],
+                    _expr.Call.Halide, _buf.op, 0)
+            else:
+                return self.vars_const[node.id]
 
     def visit_Num(self, node):
         return _api.const(node.n)
 
     def visit_Assign(self, node):
         assert len(node.targets) == 1
-        lhs = self.visit(node.targets[0])
         rhs = self.visit(node.value)
-        if isinstance(lhs, _expr.Var):
-            #TODO: Check [1] or LetStmt var
-            _buf = self.scope[lhs.name] = _api.placeholder((1,), rhs.dtype, name = lhs.name)
-            _body = _make.Provide(_buf.op, 0, rhs, [_api.const(0)])
-            _rng = [_make.range_by_min_extent(0, 1)]
-            return _make.Realize(_buf.op, 0, _buf.dtype, _rng, _api.const(0) == _api.const(0), _body)
-        elif isinstance(lhs, _expr.Call):
-            # Provide Stmt
-            #TODO: support tuple later(?)
-            assert lhs.name in self.scope.keys()
-            return _make.Provide(self.scope[lhs.name].op, 0, rhs, lhs.args)
+        rhs = _ir_pass.Simplify(rhs)
+        if isinstance(node.targets[0], ast.Name):
+            lhs = node.targets[0].id
+            #print(ast.Store, self.rw_status[lhs])
+            if lhs not in self.rw_status.keys():
+                print('Warning: Variable %s is not used after declaration! Discard stmt!' % lhs)
+                return _make.Evaluate(_api.const(0))
+            elif ast.Store in self.rw_status[lhs]:
+                #print('read write %s' % lhs)
+                if lhs not in self.vars_buffer.keys():
+                    self.vars_buffer[lhs] = _api.placeholder((1, ), dtype = rhs.dtype, name = lhs)
+                return _make.Provide(self.vars_buffer[lhs].op, 0, rhs, [_api.const(0)])
+            else:
+                if isinstance(rhs, _expr.FloatImm) or isinstance(rhs, _expr.IntImm):
+                    #print('read only const %s' % lhs)
+                    self.vars_const[lhs] = rhs
+                    return _make.Evaluate(_api.const(0))
+                else:
+                    self.vars_buffer[lhs] = _api.placeholder((1, ), dtype = rhs.dtype, name = lhs)
+                    #print('read only non-const %s' % lhs)
+                    return _make.Provide(self.vars_lut[lhs].op, 0, rhs, [_api.const(0)])
         else:
-            assert False
+            lhs = self.visit(node.targets[0])
+            assert isinstance(lhs, _expr.Call)
+            #TODO: support tuple later(?)
+            #TODO: support defined intermediate buffer later
+            assert lhs.name in self.input_param.keys()
+            return _make.Provide(self.input_param[lhs.name].op, 0, rhs, lhs.args)
+
 
     def visit_Index(self, node):
         #print(ast.dump(node))
@@ -86,8 +161,9 @@ class PyAST2HalideIR(ast.NodeVisitor):
         if isinstance(node.value, ast.Name):
             array = node.value.id
             #TODO: detect the type
-            assert array in self.scope.keys()
-            return _make.Call(self.scope[array].dtype, array, args, _expr.Call.PureExtern, None, 0)
+            assert array in self.input_param.keys()
+            _buf = self.input_param[array]
+            return _make.Call(_buf.dtype, array, args, _expr.Call.Halide, _buf, 0)
         elif isinstance(node.value, ast.Attribute):
             assert isinstance(node.value.value, ast.Name)
             assert node.value.attr == "shape"
@@ -95,8 +171,8 @@ class PyAST2HalideIR(ast.NodeVisitor):
             args = args[0]
             #TODO: maybe support non-constant value later?
             assert isinstance(args, _expr.IntImm) or isinstance(args, _expr.UIntImm)
-            assert node.value.value.id in self.scope.keys()
-            return self.scope[node.value.value.id].shape[args.value]
+            assert node.value.value.id in self.input_param.keys()
+            return self.input_param[node.value.value.id].shape[args.value]
         else:
             assert False
 
@@ -109,24 +185,19 @@ class PyAST2HalideIR(ast.NodeVisitor):
     def visit_For(self, node):
         assert isinstance(node.target, ast.Name)
         var = node.target.id
+        self.loop_levels.append(var)
         assert isinstance(node.iter, ast.Call)
         assert node.iter.func.id == 'range'
         if len(node.iter.args) == 1:
             low, high = _api.const(0), self.visit(node.iter.args[0])
         elif len(node.iter.args) == 2:
             low, high = self.visit(node.iter.args[0]), self.visit(node.iter.args[1])
-        ext  = high - low
+        ext  = high if isinstance(low, _expr.IntImm) and low.value == 0 else high - low
         _body = [self.visit(stmt) for stmt in node.body]
-        if len(_body) == 1:
-            return _make.For(_api.var(var), low, ext, _stmt.For.Serial, 0, _body[0])
-        else:
-            body = _body[0]
-            for i in _body[1:]:
-                body = _make.Block(body, i)
-            return _make.For(_api.var(var), low, ext, _stmt.For.Serial, 0, body)
-        #body = _make.Block(body)
+        _body = _list_to_block(_body)
+        self.loop_levels.pop()
+        return _make.For(_api.var(var), low, ext, _stmt.For.Serial, 0, _body)
 
-            
 
 def parse(func, **kwargs):
     """Parse a subset of Python to HalideIR
@@ -156,7 +227,9 @@ def parse(func, **kwargs):
         inputs = kwargs['inputs']
     else:
         inputs = []
-    return PyAST2HalideIR(inputs).visit(ir)
+    var_usage = _find_all_variable_decl(ir)
+    #print(var_usage)
+    return PyAST2HalideIR(inputs, var_usage).visit(ir)
 
 def allocate(shape, dtype = None):
     return numpy.zeros(shape).tolist()
