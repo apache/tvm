71c1c41605a9a6cd93a7eb84f909a70fdd53b0ea
Pariksheet Pinjari <pariksheet.pinjari@huawei.com>
2018-05-14 19:00:41 +0530

    Reverse operator

--- topi/src/topi.cc
+++ topi/src/topi.cc
@@ -241,6 +241,11 @@ TVM_REGISTER_GLOBAL("topi.transpose")
   *rv = transpose(args[0], args[1]);
   });
 
+TVM_REGISTER_GLOBAL("topi.reverse")
+.set_body([](TVMArgs args, TVMRetValue *rv) {
+  *rv = reverse(args[0], args[1]);
+  });
+
 TVM_REGISTER_GLOBAL("topi.reshape")
 .set_body([](TVMArgs args, TVMRetValue *rv) {
   *rv = reshape(args[0], args[1]);
