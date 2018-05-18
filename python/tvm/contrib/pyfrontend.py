09ff39bea03896b023ec0f9e578e11f1a1b04f9a
Jian Weng <jian.weng@ucla.edu>
2018-05-18 16:55:22 -0700

    pylint fixed

--- python/tvm/contrib/pyfrontend.py
+++ python/tvm/contrib/pyfrontend.py
@@ -1,5 +1,9 @@
-import ast, inspect, types, operator, numpy
-
+"""Compiling a subset of Python to HalideIR"""
+import ast
+import inspect
+import types
+import operator
+#import numpy
 from .. import expr as _expr
 from .. import stmt as _stmt
 from .. import make as _make
@@ -7,12 +11,33 @@ from .. import api  as _api
 from .. import ir_pass  as _ir_pass
 from .. import build_module as builder
 
-_noop = _make.Evaluate(_api.const(0))
+#pylint: disable=missing-docstring, invalid-name, consider-merging-isinstance, no-else-return
+#pylint: disable=inconsistent-return-statements
+
+NOOP = _make.Evaluate(_api.const(0))
+
+class Unrolled(object):
+    def __init__(self, factor=1):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, a, b, c):
+        return self
+
+#def allocate(shape, dtype=None):
+#    return numpy.zeros(shape).tolist()
+
+def py_frontend(func):
+    if 'Unrolled' not in func.__globals__.keys():
+        func.__globals__['Unrolled'] = Unrolled
+    return func
 
 def _list_to_block(lst):
-    lst = list(filter(lambda stmt: not _ir_pass.Equal(stmt, _noop), lst))
+    lst = list(filter(lambda stmt: not _ir_pass.Equal(stmt, NOOP), lst))
     if not lst:
-        return _noop
+        return NOOP
     if len(lst) == 1:
         return lst[0]
     body = lst[0]
@@ -20,15 +45,13 @@ def _list_to_block(lst):
         body = _make.Block(body, i)
     return body
 
-
-# The AST visitor that detects the status of each variable, which is either
-# read only, or read and write.
 class PyVariableUsage(ast.NodeVisitor):
+
     def __init__(self):
         self.declared_vars = {}
         self.loops_above = []
         self.in_loop_decl = False
-    
+
     def visit_For(self, node):
         self.in_loop_decl = True
         self.visit(node.target)
@@ -74,12 +97,12 @@ class PyAST2HalideIR(ast.NodeVisitor):
 
     def __init__(self, args, usage):
         self.input_param = {}
-        self.rw_status   = usage
         self.vars_buffer = {}
-        self.vars_const  = {}
-        self.args        = args
         self.loop_levels = {}
-        self.func_name   = ""
+        self.vars_const = {}
+        self.rw_status = usage
+        self.func_name = ""
+        self.args = args
 
     def visit_Module(self, node):
         assert len(node.body) == 1
@@ -92,7 +115,7 @@ class PyAST2HalideIR(ast.NodeVisitor):
         res = [self.visit(i) for i in node.body]
         res = _list_to_block(res)
         one = _make.range_by_min_extent(0, 1)
-        for k, v in self.vars_buffer.items():
+        for _, v in self.vars_buffer.items():
             res = _make.Realize(v.op, 0, v.dtype, [one], _api.convert(True), res)
         self.func_name = node.name
         return res
@@ -101,21 +124,19 @@ class PyAST2HalideIR(ast.NodeVisitor):
         return self.visit(node.value)
 
     def visit_Name(self, node):
-        #print(node.id)
-        if node.id in self.input_param.keys() and isinstance(self.input_param[node.id], _expr.Var):
-            return self.input_param[node.id]
-        elif node.id in self.loop_levels.keys():
-            return self.loop_levels[node.id]
+        _id = node.id
+        if _id in self.input_param.keys() and isinstance(self.input_param[_id], _expr.Var):
+            return self.input_param[_id]
+        elif _id in self.loop_levels.keys():
+            return self.loop_levels[_id]
+        assert _id not in self.input_param.keys()
+        assert _id in self.rw_status.keys()
+        assert _id in self.vars_buffer.keys() or _id in self.vars_const.keys()
+        if _id in self.vars_buffer.keys():
+            _buf = self.vars_buffer[_id]
+            return _make.Call(_buf.dtype, _id, [_api.const(0)], _expr.Call.Halide, _buf.op, 0)
         else:
-            assert node.id not in self.input_param.keys()
-            assert node.id in self.rw_status.keys()
-            assert node.id in self.vars_buffer.keys() or node.id in self.vars_const.keys()
-            if node.id in self.vars_buffer.keys():
-                _buf = self.vars_buffer[node.id]
-                return _make.Call(_buf.dtype, node.id, [_api.const(0)],
-                    _expr.Call.Halide, _buf.op, 0)
-            else:
-                return self.vars_const[node.id]
+            return self.vars_const[_id]
 
     def visit_Num(self, node):
         return _api.const(node.n)
@@ -129,21 +150,19 @@ class PyAST2HalideIR(ast.NodeVisitor):
             #print(ast.Store, self.rw_status[lhs])
             if lhs not in self.rw_status.keys():
                 print('Warning: Variable %s is not used after declaration! Discard stmt!' % lhs)
-                return _noop
-            elif ast.Store in self.rw_status[lhs]:
+                return NOOP
+            elif isinstance(rhs, _expr.FloatImm) or isinstance(rhs, _expr.IntImm):
+                #print('read only const %s' % lhs)
+                self.vars_const[lhs] = rhs
+                return NOOP
+            else:# ast.Store in self.rw_status[lhs]:
                 #print('read write %s' % lhs)
                 if lhs not in self.vars_buffer.keys():
-                    self.vars_buffer[lhs] = _api.placeholder((1, ), dtype = rhs.dtype, name = lhs)
+                    self.vars_buffer[lhs] = _api.placeholder((1, ), dtype=rhs.dtype, name=lhs)
                 return _make.Provide(self.vars_buffer[lhs].op, 0, rhs, [_api.const(0)])
-            else:
-                if isinstance(rhs, _expr.FloatImm) or isinstance(rhs, _expr.IntImm):
-                    #print('read only const %s' % lhs)
-                    self.vars_const[lhs] = rhs
-                    return _noop
-                else:
-                    #print('read only non-const %s' % lhs)
-                    self.vars_buffer[lhs] = _api.placeholder((1, ), dtype = rhs.dtype, name = lhs)
-                    return _make.Provide(self.vars_buffer[lhs].op, 0, rhs, [_api.const(0)])
+            #print('read only non-const %s' % lhs)
+            self.vars_buffer[lhs] = _api.placeholder((1, ), dtype=rhs.dtype, name=lhs)
+            return _make.Provide(self.vars_buffer[lhs].op, 0, rhs, [_api.const(0)])
         else:
             lhs = self.visit(node.targets[0])
             assert isinstance(lhs, _expr.Call)
@@ -156,8 +175,7 @@ class PyAST2HalideIR(ast.NodeVisitor):
         #print(ast.dump(node))
         if isinstance(node.value, ast.Tuple):
             return [self.visit(i) for i in node.value.elts]
-        else:
-            return [self.visit(node.value)]
+        return [self.visit(node.value)]
 
     def visit_Subscript(self, node):
         #print(node.value)
@@ -197,14 +215,14 @@ class PyAST2HalideIR(ast.NodeVisitor):
             low, high = _api.const(0), self.visit(node.iter.args[0])
         elif len(node.iter.args) == 2:
             low, high = self.visit(node.iter.args[0]), self.visit(node.iter.args[1])
-        ext  = high if isinstance(low, _expr.IntImm) and low.value == 0 else high - low
+        ext = high if isinstance(low, _expr.IntImm) and low.value == 0 else high - low
         _body = [self.visit(stmt) for stmt in node.body]
         _body = _list_to_block(_body)
         res = _make.For(self.loop_levels[var], low, ext, _stmt.For.Serial, 0, _body)
         self.loop_levels.pop(var)
         return res
 
-def parse(func, args, dump = False):
+def parse(func, args, dump=False):
     """Parse a subset of Python to HalideIR
 
     Parameters
@@ -231,11 +249,13 @@ def parse(func, args, dump = False):
     else:
         assert isinstance(func, types.FunctionType)
         ir = ast.parse(inspect.getsource(func))
+    if dump:
+        print(ir.show())
     var_usage = _find_all_variable_decl(ir)
     #print(var_usage)
     return PyAST2HalideIR(args, var_usage).visit(ir)
 
-def lower(func, args, binds = None):
+def lower(func, args, binds=None):
     """Lower a subset of Python to LoweredFunc
 
     Parameters
@@ -269,7 +289,7 @@ def lower(func, args, binds = None):
     parser = PyAST2HalideIR(args, var_usage)
     stmt = parser.visit(ir)
     binds_args, args = builder.get_binds(parser.args)
-    binds_vars, _    = builder.get_binds(parser.vars_buffer)
+    binds_vars, _ = builder.get_binds(parser.vars_buffer)
     _binds = binds_args.copy()
     _binds.update(binds_vars)
     _binds.update(binds_vars)
@@ -277,9 +297,5 @@ def lower(func, args, binds = None):
     #print(_binds)
     stmt = _ir_pass.StorageFlatten(stmt, _binds, 64)
     #print(stmt)
-    return _ir_pass.MakeAPI(stmt, parser.func_name, args, 0,
-                    builder.current_build_config().restricted_func)
-
-def allocate(shape, dtype = None):
-    return numpy.zeros(shape).tolist()
-
+    _config = builder.current_build_config().restricted_func
+    return _ir_pass.MakeAPI(stmt, parser.func_name, args, 0, _config)
