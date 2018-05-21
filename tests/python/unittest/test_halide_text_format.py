bf34e7af2bf811fb07c4e0627eeca05007d17091
Jian Weng <jian.weng@ucla.edu>
2018-05-21 11:29:26 -0700

    if then else supported and if expression

--- tests/python/unittest/test_halide_text_format.py
+++ tests/python/unittest/test_halide_text_format.py
@@ -10,9 +10,9 @@ def outer_product(n, m, a, b, c):
 def test_outer_product():
     n = tvm.var('n')
     m = tvm.var('m')
-    a = tvm.placeholder((n, ), name = 'a')
-    b = tvm.placeholder((m, ), name = 'b')
-    c = tvm.placeholder((n, m), name = 'c')
+    a = tvm.placeholder((n, ), name='a')
+    b = tvm.placeholder((m, ), name='b')
+    c = tvm.placeholder((n, m), name='c')
     ir, _ = frontend.parse(outer_product, [n, m, a, b, c])
     #print(ir)
     #Check for i in (0, n)
@@ -46,14 +46,14 @@ def test_outer_product():
     _m = 1001
     _a = numpy.random.rand(_n).astype('float32')
     _b = numpy.random.rand(_m).astype('float32')
-    c_python = numpy.zeros((_n, _m), dtype = 'float32')
+    c_python = numpy.zeros((_n, _m), dtype='float32')
     outer_product(_n, _m, _a, _b, c_python)
 
     tvm_a = tvm.ndarray.array(_a)
     tvm_b = tvm.ndarray.array(_b)
-    tvm_c = tvm.ndarray.array(numpy.zeros((_n, _m), dtype = 'float32'))
+    tvm_c = tvm.ndarray.array(numpy.zeros((_n, _m), dtype='float32'))
     func(_n, _m, tvm_a, tvm_b, tvm_c)
-    numpy.testing.assert_allclose(tvm_c.asnumpy(), c_python, rtol = 1e-5)
+    numpy.testing.assert_allclose(tvm_c.asnumpy(), c_python, rtol=1e-5)
     #print(func)
 
 def fanout(n, a, b):
@@ -67,8 +67,8 @@ def fanout(n, a, b):
 
 def test_fanout():
     n = tvm.var('n')
-    a = tvm.placeholder((n, ), name = 'a')
-    b = tvm.placeholder((n-3, ), name = 'b')
+    a = tvm.placeholder((n, ), name='a')
+    b = tvm.placeholder((n-3, ), name='b')
     ir, _ = frontend.parse(fanout, [n, a, b])
     #print(ir)
     assert isinstance(ir, tvm.stmt.Realize)
@@ -145,19 +145,43 @@ def annotation(a):
             a[i] = i
 
 def test_unroll():
-    a = tvm.placeholder((6, ), name = 'a')
+    a = tvm.placeholder((6, ), name='a')
     ir, _ = tvm.contrib.pyfrontend.parse(annotation, [a])
     assert isinstance(ir, tvm.stmt.For)
     assert ir.for_type == tvm.stmt.For.Unrolled
+
+def if_then_else(a, b):
+    for i in range(10):
+        if i % 2 == 0:
+            a[i] = -1
+        else:
+            a[i] = 1
+    for i in range(10):
+        b[i] = -1 if i % 2 == 0 else 1
+
+def test_if():
+    a = tvm.placeholder((10, ), dtype='int32', name='a')
+    b = tvm.placeholder((10, ), dtype='int32', name='b')
+    func = frontend.lower(if_then_else, [a, b])
+    func = tvm.build(func)
+    assert func
+
+    _a = numpy.zeros((10, ), dtype = 'int32')
+    _b = numpy.zeros((10, ), dtype = 'int32')
+    if_then_else(_a, _b)
+
+    tvm_a = tvm.ndarray.array(numpy.zeros((10, ), dtype='int32'))
+    tvm_b = tvm.ndarray.array(numpy.zeros((10, ), dtype='int32'))
+    func(tvm_a, tvm_b)
+
+    numpy.testing.assert_allclose(tvm_a.asnumpy(), _a, rtol=1e-5)
+    numpy.testing.assert_allclose(tvm_b.asnumpy(), _b, rtol=1e-5)
+    numpy.testing.assert_allclose(tvm_a.asnumpy(), tvm_b.asnumpy(), rtol=1e-5)
 
 if __name__ == "__main__":
     test_outer_product()
     test_fanout()
     test_failure()
     test_unroll()
-
-#test_annotation()
-#test_outer_product()
-#test_blur()
-#test_failure()
+    test_if()
 
