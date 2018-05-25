e2faf792000de7c56dbe04a3004efdaa5b3ba3e0
Thierry Moreau <moreau@cs.washington.edu>
2018-05-24 20:13:15 -0700

    [DOC, EXAMPLE] Updated READMEs, tests, etc. (#41)
    * bug fix for new drivers in new PYNQ image v2.1
    
    * updating instructions for resnet inference
    
    * updated the instructions for starting the RPC server
    
    * deriving host/port from env for unit tests

--- vta/tests/python/pynq/test_program_rpc.py
+++ vta/tests/python/pynq/test_program_rpc.py
@@ -3,6 +3,9 @@ import tvm
 from tvm.contrib import rpc
 from vta import get_bitstream_path, download_bitstream, program_fpga, reconfig_runtime
 
+host = os.environ.get("VTA_PYNQ_RPC_HOST", "pynq")
+port = int(os.environ.get("VTA_PYNQ_RPC_PORT", "9091"))
+
 def program_rpc_bitstream(path=None):
     """Program the FPGA on the RPC server
 
@@ -11,13 +14,6 @@ def program_rpc_bitstream(path=None):
     path : path to bitstream (optional)
     """
     assert tvm.module.enabled("rpc")
-    host = os.environ.get("VTA_PYNQ_RPC_HOST", None)
-    if not host:
-        raise RuntimeError(
-            "Error: VTA_PYNQ_RPC_HOST environment variable not set.")
-    # If a path to a bitstream is passed, make sure that it point to a valid bitstream
-    port = os.environ.get("VTA_PYNQ_RPC_PORT", "9091")
-    port = int(port)
     remote = rpc.connect(host, port)
     program_fpga(remote, path)
 
@@ -25,12 +21,8 @@ def reconfig_rpc_runtime():
     """Reconfig the RPC server runtime
     """
     assert tvm.module.enabled("rpc")
-    host = os.environ.get("VTA_PYNQ_RPC_HOST", None)
-    if host:
-        port = os.environ.get("VTA_PYNQ_RPC_PORT", "9091")
-        port = int(port)
-        remote = rpc.connect(host, port)
-        reconfig_runtime(remote)
+    remote = rpc.connect(host, port)
+    reconfig_runtime(remote)
 
 program_rpc_bitstream()
 reconfig_rpc_runtime()
