6e68d1df608b13c962dbfe665fa5ad7b8690c010
Pariksheet Pinjari <pariksheet.pinjari@huawei.com>
2018-05-25 16:09:35 +0530

    Test case updated for without stride input

--- topi/tests/python/test_topi_transform.py
+++ topi/tests/python/test_topi_transform.py
@@ -279,7 +279,7 @@ def test_stridedslice():
     verify_strided_slice((3, 4, 3), [1, -1, 0], [4, -5, 3], [2, -1, 1])
     verify_strided_slice((3, 4, 3), [1, 0, 0], [2, 2, 3], [1, 1, 2])
     verify_strided_slice((3, 4, 3), [1, -1, 0], [2, -3, 3], [1, -1, 1])
-    verify_slice((3, 4, 3), [1, 1, 0], [4, 4, 3])
+    verify_strided_slice((3, 4, 3), [1, 1, 0], [4, 4, 3])
 
 def test_expand_dims():
     verify_expand_dims((3, 10), (3, 10, 1, 1), 2, 2)
