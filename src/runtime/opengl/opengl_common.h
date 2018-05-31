61debb2318f7305991727acfcf4a631939228df4
tqchen <tianqi.tchen@gmail.com>
2018-05-30 21:51:23 -0700

    fix opengl runtime

--- src/runtime/opengl/opengl_common.h
+++ src/runtime/opengl/opengl_common.h
@@ -175,6 +175,7 @@ class OpenGLWorkspace final : public DeviceAPI {
                       size_t size,
                       TVMContext ctx_from,
                       TVMContext ctx_to,
+                      TVMType type_hint,
                       TVMStreamHandle stream) final;
   void StreamSync(TVMContext ctx, TVMStreamHandle stream) final;
 
