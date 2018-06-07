dcf18a3c20cc9402c027f430720fc877424ce4a5
Leyuan Wang <laurawly@gmail.com>
2018-06-06 22:52:03 -0700

    [RPC] default use spawn for fork safety (#1240)

--- python/tvm/exec/rpc_proxy.py
+++ python/tvm/exec/rpc_proxy.py
@@ -1,10 +1,14 @@
+# pylint: disable=redefined-outer-name, invalid-name
 """RPC web proxy, allows redirect to websocket based RPC servers(browsers)"""
 from __future__ import absolute_import
 
 import logging
 import argparse
+import multiprocessing
+import sys
 import os
 from ..contrib.rpc.proxy import Proxy
+
 
 def find_example_resource():
     """Find resource examples."""
@@ -22,22 +26,8 @@ def find_example_resource():
     return index_page, js_files
 
 
-def main():
+def main(args):
     """Main funciton"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default="0.0.0.0",
-                        help='the hostname of the server')
-    parser.add_argument('--port', type=int, default=9090,
-                        help='The port of the PRC')
-    parser.add_argument('--web-port', type=int, default=8888,
-                        help='The port of the http/websocket server')
-    parser.add_argument('--example-rpc', type=bool, default=False,
-                        help='Whether to switch on example rpc mode')
-    parser.add_argument('--tracker', type=str, default="",
-                        help="Report to RPC tracker")
-    args = parser.parse_args()
-    logging.basicConfig(level=logging.INFO)
-
     if args.tracker:
         url, port = args.tracker.split(":")
         port = int(port)
@@ -60,5 +50,33 @@ def main():
                      tracker_addr=tracker_addr)
     prox.proc.join()
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default="0.0.0.0",
+                        help='the hostname of the server')
+    parser.add_argument('--port', type=int, default=9090,
+                        help='The port of the PRC')
+    parser.add_argument('--web-port', type=int, default=8888,
+                        help='The port of the http/websocket server')
+    parser.add_argument('--example-rpc', type=bool, default=False,
+                        help='Whether to switch on example rpc mode')
+    parser.add_argument('--tracker', type=str, default="",
+                        help="Report to RPC tracker")
+    parser.add_argument('--no-fork', dest='fork', action='store_false',
+                        help="Use spawn mode to avoid fork. This option \
+                         is able to avoid potential fork problems with Metal, OpenCL \
+                         and ROCM compilers.")
+    parser.set_defaults(fork=True)
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+    if args.fork is False:
+        if sys.version_info[0] < 3:
+            raise RuntimeError(
+                "Python3 is required for spawn mode."
+            )
+        multiprocessing.set_start_method('spawn')
+    else:
+        logging.info("If you are running ROCM/Metal, \
+        fork with cause compiler internal error. Try to launch with arg ```--no-fork```")
+    main(args)
