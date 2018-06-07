3f674b31718e8189d5a9c1d3d281499eea085507
Leyuan Wang <laurawly@gmail.com>
2018-06-06 22:52:03 -0700

    [RPC] default use spawn for fork safety (#1240)

--- python/tvm/exec/rpc_tracker.py
+++ python/tvm/exec/rpc_tracker.py
@@ -1,21 +1,40 @@
+# pylint: disable=redefined-outer-name, invalid-name
 """Tool to start RPC tracker"""
 from __future__ import absolute_import
 
 import logging
 import argparse
+import multiprocessing
+import sys
 from ..contrib.rpc.tracker import Tracker
 
-def main():
+
+def main(args):
     """Main funciton"""
+    tracker = Tracker(args.host, port=args.port)
+    tracker.proc.join()
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, default="0.0.0.0",
                         help='the hostname of the tracker')
     parser.add_argument('--port', type=int, default=9190,
                         help='The port of the PRC')
+    parser.add_argument('--no-fork', dest='fork', action='store_false',
+                        help="Use spawn mode to avoid fork. This option \
+                         is able to avoid potential fork problems with Metal, OpenCL \
+                         and ROCM compilers.")
+    parser.set_defaults(fork=True)
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    tracker = Tracker(args.host, port=args.port)
-    tracker.proc.join()
-
-if __name__ == "__main__":
-    main()
+    if args.fork is False:
+        if sys.version_info[0] < 3:
+            raise RuntimeError(
+                "Python3 is required for spawn mode."
+            )
+        multiprocessing.set_start_method('spawn')
+    else:
+        logging.info("If you are running ROCM/Metal, \
+        fork with cause compiler internal error. Try to launch with arg ```--no-fork```")
+    main(args)
