a3e60d8f4309b537521f9c07a46072ace8212b9d
Tatsuya.Nishiyama <nishiyama.tatsuya@lab.ntt.co.jp>
2018-06-20 12:24:16 +0900

    fix split with squeeze_axis using Group

--- nnvm/python/nnvm/frontend/mxnet.py
+++ nnvm/python/nnvm/frontend/mxnet.py
@@ -189,14 +189,13 @@ def _reshape(inputs, attrs):
 
 def _split(inputs, attrs):
     op_name, new_attrs = 'split', {}
-    new_attrs['indices_or_sections'] = _required_attr(attrs, 'num_outputs')
     axis = attrs.get('axis', 1)
+    new_attrs['indices_or_sections'] = _required_attr(attrs, 'num_outputs')
     new_attrs['axis'] = axis
     outputs = _get_nnvm_op(op_name)(*inputs, **new_attrs)
     if _parse_bool_str(attrs, 'squeeze_axis'):
         squeeze_attrs = {'axis': axis}
-        for o in outputs:
-            o = _get_nnvm_op('squeeze')(o, **squeeze_attrs)
+        outputs = _sym.Group([_get_nnvm_op('squeeze')(o, **squeeze_attrs) for o in outputs])
     return outputs
 
 def _softmax_activation(inputs, attrs):
