90db723d287509705dcc93fde7ab0df380b9a4e5
Jian Weng <werefluke@gmail.com>
2018-06-22 13:47:45 -0700

    [FRONTEND] A Python hybrid frontend (#1251)

---
+++ docs/api/python/hybrid.rst
@@ -0,0 +1,15 @@
+tvm.hybrid
+----------
+.. automodule:: tvm.hybrid
+
+.. autosummary::
+
+   tvm.hybrid.parse
+   tvm.hybrid.script
+   tvm.hybrid.popcount
+   tvm.hybrid.sigmoid
+
+.. autofunction:: tvm.hybrid.parse
+.. autofunction:: tvm.hybrid.script
+.. autofunction:: tvm.hybrid.popcount
+.. autofunction:: tvm.hybrid.sigmoid
---
+++ python/tvm/hybrid/parser.py
@@ -0,0 +1,342 @@
+"""Hybrid Script Parser"""
+
+import ast
+import operator
+import sys
+from .util import make_nop, make_const_true, make_range_one, halide_imm_types
+from .intrin import LOOP_INTRIN, MATH_INTRIN
+from .var_decl import determine_variable_usage
+from ..api import thread_axis
+from .. import expr as _expr
+from .. import make as _make
+from .. import intrin
+from .. import api  as _api
+from .. import ir_pass as _ir_pass
+
+def list_to_block(visit, lst):
+    """Convert a list of Python IR nodes to HalideIR Block"""
+    lst = list(map(visit, lst))
+    lst = [stmt for stmt in lst if not _ir_pass.Equal(stmt, make_nop())]
+    if not lst:
+        return make_nop()
+    if len(lst) == 1:
+        return lst[0]
+    body = lst[0]
+    for i in lst[1:]:
+        body = _make.Block(body, i)
+    return body
+
+
+class HybridParser(ast.NodeVisitor):
+    """Python AST visitor pass which finally lowers it to HalideIR"""
+
+
+    _binop_maker = {
+        ast.Add   : operator.add,
+        ast.Sub   : operator.sub,
+        ast.Mult  : operator.mul,
+        ast.Div   : _make.Div,
+        ast.Mod   : operator.mod,
+        ast.BitOr : operator.or_,
+        ast.BitAnd: operator.and_,
+        ast.BitXor: operator.xor,
+        ast.Gt    : operator.gt,
+        ast.GtE   : operator.ge,
+        ast.Lt    : operator.lt,
+        ast.LtE   : operator.le,
+        ast.Eq    : operator.eq,
+        ast.NotEq : operator.ne,
+    }
+
+
+    _unaryop_maker = {
+        ast.USub   : operator.neg,
+        ast.Invert : operator.invert,
+        ast.Not    : operator.not_
+    }
+
+
+    def __init__(self, args, usage, func_name=None):
+        """
+        Parameters
+        ----------
+        args: A list of tvm.placeholder or tvm.var
+            Provided by the user, the argument list of the function to be lowered.
+
+        usage: A dict of variables used in last in this function
+            Provided by last lower pass, which collects this information
+
+        Returns
+        -------
+        func_name: str
+            The name of the function to be lowered; if not provided,
+            the compiler will use the name in the AST
+        """
+        self.args = args[:]
+        self.usage = usage.copy()
+        self._args = {} # Dict maps arg name to actual arg instance (either a var or a buffer)
+        self.buffers = {}
+        self.loops_above = {} # State variable that indicates loop levels above the current node
+        self.var_consts = {} # Variables that are determined as readonly in previous stage
+        self.func_name = func_name # The name of the function to be lowered
+        self.iter_axis = []
+
+
+    def wrap_up_realize(self, node, body):
+        """Wrap up all the variables which will no longer be used"""
+        for key, val in self.usage.items():
+            if key in self.var_consts.keys():
+                continue
+            _, scope, _ = val
+            if scope == node:
+                _buf = self.buffers[key]
+                _dtype = _buf.dtype
+                _one = make_range_one()
+                _true = make_const_true()
+                body = _make.Realize(_buf.op, 0, _dtype, [_one], _true, body)
+        return body
+
+
+    def _check_id_a_buffer(self, s):
+        if s not in self._args.keys():
+            raise ValueError("This %s is expected to be in argument list or allocated buffer!" % s)
+
+
+    #pylint: disable=invalid-name, missing-docstring
+    def visit_Module(self, node):
+        if len(node.body) != 1:
+            raise ValueError("Only one-function source code can be fed to this parser!")
+        return self.visit(node.body[0])
+
+
+    def visit_FunctionDef(self, node):
+        if len(node.args.args) != len(self.args):
+            raise ValueError("The number of arguments passed to the function\
+                should be the same as it is defined!")
+        for idx, arg in enumerate(node.args.args):
+            _attr = 'id' if sys.version_info[0] < 3 else 'arg' # To make py2 and 3 compatible
+            self._args[getattr(arg, _attr)] = self.args[idx]
+        res = list_to_block(self.visit, node.body)
+        res = self.wrap_up_realize(node, res)
+        if self.func_name is None:
+            self.func_name = node.name
+        return res
+
+
+    def visit_Expr(self, node):
+        return self.visit(node.value)
+
+
+    def visit_Name(self, node):
+        _id = node.id
+        if _id in self._args.keys() and isinstance(self._args[_id], _expr.Var):
+            return self._args[_id]
+        elif _id in self.loops_above.keys():
+            return self.loops_above[_id]
+        if _id in self._args.keys():
+            raise ValueError("This id %s should be handled in visit_Subscript!" % _id)
+        if _id  not in self.usage.keys():
+            raise ValueError("This id %s is expected to be a defined variable!" % _id)
+        # Buffer
+        if _id in self.buffers.keys():
+            _buf = self.buffers[_id]
+            return _make.Call(_buf.dtype, _id, [_api.const(0)], _expr.Call.Halide, _buf.op, 0)
+        # Compilation time constant
+        if _id not in self.var_consts.keys():
+            raise ValueError("This id %s is expected to a compilation time constant!" % _id)
+        return self.var_consts[_id]
+
+
+    def visit_Num(self, node):
+        return _api.const(node.n)
+
+
+    def visit_Assign(self, node):
+        if len(node.targets) != 1:
+            raise ValueError("So far only one-valued assignment is supported!")
+        lhs = node.targets[0]
+        rhs = _ir_pass.Simplify(self.visit(node.value))
+        if isinstance(lhs, ast.Name):
+            #TODO: support defined intermediate buffer later
+            lhs_ = lhs
+            lhs = lhs.id
+            if lhs in self.loops_above.keys():
+                raise ValueError("You CAN NEVER overwrite a loop variable!")
+            decl, _, rw = self.usage[lhs]
+            if decl == lhs_:
+                if lhs in self.var_consts.keys():
+                    raise ValueError("BUG: A constant cannot be overwritten!")
+                if lhs in self.buffers.keys():
+                    raise ValueError("BUG: This value should not be defined before this point!")
+                if isinstance(rhs, halide_imm_types) and ast.Store not in rw:
+                    self.var_consts[lhs] = rhs
+                else:
+                    self.buffers[lhs] = _api.placeholder((1, ), dtype=rhs.dtype, name=lhs)
+            if lhs in self.var_consts.keys():
+                return make_nop()
+            else:
+                if lhs not in self.buffers.keys():
+                    raise ValueError("BUG: This value should be defined before!")
+                return _make.Provide(self.buffers[lhs].op, 0, rhs, [_api.const(0, dtype=rhs.dtype)])
+        else:
+            lhs = self.visit(lhs)
+            if not isinstance(lhs, _expr.Call):
+                raise ValueError("An array access's LHS is expected to be a expr.Call!")
+            #TODO: support slice later
+            self._check_id_a_buffer(lhs.name)
+            return _make.Provide(self._args[lhs.name].op, 0, rhs, lhs.args)
+
+
+    def visit_Index(self, node):
+        if isinstance(node.value, ast.Tuple):
+            return [self.visit(i) for i in node.value.elts]
+        return [self.visit(node.value)]
+
+
+    def visit_Subscript(self, node):
+        args = self.visit(node.slice)
+        if isinstance(node.value, ast.Name):
+            array = node.value.id
+            self._check_id_a_buffer(array)
+            _buf = self._args[array]
+            return _make.Call(_buf.dtype, array, args, _expr.Call.Halide, _buf.op, 0)
+        elif isinstance(node.value, ast.Attribute):
+            if not isinstance(node.value.value, ast.Name):
+                raise ValueError("The root of array access is expect to be a id!")
+            if node.value.attr != "shape":
+                raise ValueError("Attribute access so far only 'shape' is supported!")
+            if len(args) != 1:
+                raise ValueError("For 'shape' access the argument should be only one!")
+            args = args[0]
+            #TODO: maybe support non-constant value later?
+            if not isinstance(args, (_expr.IntImm, _expr.UIntImm)):
+                raise ValueError("So far only constant shape access supported!")
+            self._check_id_a_buffer(node.value.value.id)
+            return self._args[node.value.value.id].shape[args.value]
+        else:
+            raise ValueError("Not supported yet!")
+
+
+    def visit_With(self, node):
+        if sys.version_info[0] < 3:
+            context = node.context_expr
+            option = node.optional_vars
+        else:
+            if len(node.items) != 1:
+                raise ValueError("Only one with element is supported so far!")
+            context = node.items[0].context_expr
+            option = node.items[0].optional_vars
+        if not isinstance(context, ast.Call):
+            raise ValueError("The object must be a Python function call!")
+        if not isinstance(option, ast.Name):
+            raise ValueError("The object after 'as' must be an id!")
+        self.annotation[option.id] = context.func.id
+        return list_to_block(self.visit, node.body)
+
+
+    def visit_If(self, node):
+        cond = self.visit(node.test)
+        if_body = list_to_block(self.visit, node.body)
+        if node.orelse:
+            else_body = list_to_block(self.visit, node.orelse)
+        else:
+            else_body = make_nop()
+        return _make.IfThenElse(cond, if_body, else_body)
+
+
+    def visit_IfExp(self, node):
+        cond = self.visit(node.test)
+        if_body = self.visit(node.body)
+        else_body = self.visit(node.orelse)
+        return _make.Select(cond, if_body, else_body)
+
+
+    def visit_Compare(self, node):
+        lhs = self.visit(node.left)
+        if len(node.ops) != 1:
+            raise ValueError("Only one compare op is supported!")
+        if len(node.comparators) != 1:
+            raise ValueError("Only one comparator is supported!")
+        rhs = self.visit(node.comparators[0])
+        return HybridParser._binop_maker[type(node.ops[0])](lhs, rhs)
+
+
+    def visit_UnaryOp(self, node):
+        operand = self.visit(node.operand)
+        return HybridParser._unaryop_maker[type(node.op)](operand)
+
+
+    def visit_BinOp(self, node):
+        lhs = self.visit(node.left)
+        rhs = self.visit(node.right)
+        return HybridParser._binop_maker[type(node.op)](lhs, rhs)
+
+
+    def visit_Call(self, node):
+        # Yet, no function pointer supported
+        if not isinstance(node.func, ast.Name):
+            raise ValueError("Only id-function function call is supported so far!")
+        func_id = node.func.id
+        n = len(node.args)
+        if func_id in LOOP_INTRIN.keys() and func_id != 'bind':
+            if n == 1:
+                low, ext = _api.const(0, dtype='int32'), self.visit(node.args[0])
+            else:
+                if n != 2:
+                    raise ValueError("A loop intrinsic should only have 1 or 2 arguments!")
+                low, ext = self.visit(node.args[0]), self.visit(node.args[1])
+            if not _ir_pass.Equal(low, _api.const(0, dtype='int32')):
+                ext = ext - low
+            for_type = LOOP_INTRIN[func_id]
+            iter_var = None
+            return iter_var, low, ext, for_type
+        elif func_id == 'bind':
+            if n != 2:
+                raise ValueError("A loop bind should only have 2 arguments!")
+            if not isinstance(node.args[0], ast.Str):
+                raise ValueError("A loop bind's first argument should be a string!")
+            _vn = node.args[0].s
+            iter_var = thread_axis(node.args[0].s)
+            low, ext = _api.const(0, dtype='int32'), self.visit(node.args[1])
+            for_type = None
+            return iter_var, low, ext, for_type
+        elif func_id in MATH_INTRIN:
+            return getattr(intrin, func_id)(*[self.visit(arg) for arg in node.args])
+        elif func_id == 'allocate':
+            #TODO: Support it later!
+            return make_nop()
+        else:
+            raise ValueError("Function call not supported yet!")
+
+
+    def visit_For(self, node):
+        iter_var, low, ext, for_type = self.visit(node.iter)
+        if not isinstance(node.target, ast.Name):
+            raise ValueError("The loop iterator should be a variable!")
+        _name = node.target.id
+        if iter_var is None:
+            if for_type is None:
+                raise ValueError("The loop bind function parse error!")
+            iter_var = _api.var(_name)
+            self.loops_above[_name] = iter_var
+        else:
+            if for_type is not None:
+                raise ValueError("The loop iterating function parse error!")
+            self.loops_above[_name] = iter_var.var
+        _body = list_to_block(self.visit, node.body)
+        _body = self.wrap_up_realize(node, _body)
+        if for_type is None:
+            res = _make.AttrStmt(iter_var, 'thread_extent', ext, _body)
+        else:
+            res = _make.For(iter_var, low, ext, for_type, 0, _body)
+        self.loops_above.pop(_name)
+        return res
+
+
+def parse_python(src, args):
+    """The helper function of calling the AST visitor"""
+    root = ast.parse(src)
+    var_usage = determine_variable_usage(root, args)
+    parser = HybridParser(args, var_usage)
+    halide_ir = parser.visit(root)
+    return halide_ir
