94b993c6a70b90abee66e179d6f95e617dcdea48
Eddie Yan <eqy@cs.washington.edu>
2018-07-10 22:06:45 +0000

    fix

--- src/runtime/thread_pool.cc
+++ src/runtime/thread_pool.cc
@@ -253,7 +253,7 @@ class ThreadPool {
         new tvm::runtime::threading::ThreadGroup(
           num_workers_, [this](int worker_id) { this->RunWorker(worker_id); },
           exclude_worker0_ /* include_main_thread */));
-    threads_->Config(1, 0, exclude_worker0_);
+    num_workers_used_ = threads_->Config(1, 0, exclude_worker0_);
   }
   ~ThreadPool() {
     for (std::unique_ptr<SpscTaskQueue>& q : queues_) {
