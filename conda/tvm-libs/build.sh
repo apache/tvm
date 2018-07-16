b3407fb25eb64dcf019ff86a08543d8d92009930
Arnaud Bergeron <abergeron@gmail.com>
2018-07-16 15:44:55 -0400

    The deployment target is handled by the conda compiler packages

--- conda/tvm-libs/build.sh
+++ conda/tvm-libs/build.sh
@@ -6,13 +6,6 @@ if [ -z "$PREFIX" ]; then
   PREFIX="$CONDA_PREFIX"
 fi
 
-if [ "$(uname)" = 'Darwin' ]
-then
-    # Without this, Apple's default shipped clang will refuse to see any
-    # headers like mutex.
-    export MACOSX_DEPLOYMENT_TARGET=10.9
-fi
-
 rm -rf build || true
 mkdir -p build
 cd build
