b350ef7562a3031391e3102314642a99e463b064
Arnaud Bergeron <abergeron@gmail.com>
2018-07-17 12:12:08 -0400

    Fix nnvm library loading

--- python/tvm/_ffi/libinfo.py
+++ python/tvm/_ffi/libinfo.py
@@ -40,7 +40,7 @@ def find_lib_path(name=None, search_path=None, optional=False):
         dll_path.extend([p.strip() for p in os.environ['DYLD_LIBRARY_PATH'].split(":")])
 
     # Pip lib directory
-    dll_path.append(os.path.join(ffi_dir, "../"))
+    dll_path.append(os.path.join(ffi_dir, ".."))
     # Default cmake build directory
     dll_path.append(os.path.join(source_dir, "build"))
     dll_path.append(os.path.join(source_dir, "build", "Release"))
