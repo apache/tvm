f1f30c4c4ab5e8a47a0d76c8cd2c5344c0a9096a
Lianmin Zheng <mercy_zheng@sjtu.edu.cn>
2018-07-26 12:14:47 -0700

    update dependency (#1495)

--- docker/install/ubuntu_install_python_package.sh
+++ docker/install/ubuntu_install_python_package.sh
@@ -1,3 +1,3 @@
 # install libraries for python package on ubuntu
-pip2 install nose pylint numpy nose-timer cython decorator scipy tornado
-pip3 install nose pylint numpy nose-timer cython decorator scipy tornado typed_ast pytest mypy orderedset
+pip2 install nose pylint numpy nose-timer cython decorator scipy tornado request
+pip3 install nose pylint numpy nose-timer cython decorator scipy tornado request typed_ast pytest mypy orderedset
