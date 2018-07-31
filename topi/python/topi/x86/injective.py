039088ca0a6d05b2db2232d959fefbec71ff4274
Wang <wayao@9801a7a9c287.ant.amazon.com>
2018-07-31 14:42:08 -0700

    Remove vectorization for 4D concat

--- topi/python/topi/x86/injective.py
+++ topi/python/topi/x86/injective.py
@@ -48,32 +48,35 @@ def schedule_concatenate(outs):
     sch: Schedule
         The computation schedule for the op.
     """
+    def vectorize(sch, tensor, vectorize_limit):
+        """Internal vectorization function for concatenate."""
+        inner_axis = s[tensor].op.axis[len(s[tensor].op.axis) - 1]
+        inner_length = tensor.shape[len(tensor.shape) - 1].value
+        if inner_length <= vectorize_limit:
+            sch[tensor].vectorize(inner_axis)
+        else:
+            split_factor = 1
+            for i in reversed(range(1, inner_length)):
+                if inner_length % i == 0 and i <= vectorize_limit:
+                    split_factor = i
+                    break
+            if split_factor > 1:
+                _, inner_i = sch[tensor].split(inner_axis, split_factor)
+                sch[tensor].vectorize(inner_i)
+
     outs = [outs] if isinstance(outs, tvm.tensor.Tensor) else outs
     x = outs[0]
     s = tvm.create_schedule([x.op for x in outs])
     tvm.schedule.AutoInlineInjective(s)
     if len(s[x].op.axis) >= 5:
         fused = s[x].fuse(s[x].op.axis[0], s[x].op.axis[1], s[x].op.axis[2])
+        vectorize(s, x, 64)
         s[x].parallel(fused)
     elif len(s[x].op.axis) >= 3:
         fused = s[x].fuse(s[x].op.axis[0], s[x].op.axis[1])
         s[x].parallel(fused)
     else:
         s[x].parallel(s[x].op.axis[0])
-    inner_axis = s[x].op.axis[len(s[x].op.axis) - 1]
-    inner_length = x.shape[len(x.shape) - 1].value
-    vectorize_limit = 64
-    if inner_length <= vectorize_limit:
-        s[x].vectorize(inner_axis)
-    else:
-        split_factor = 1
-        for i in reversed(range(1, inner_length)):
-            if inner_length % i == 0 and i <= vectorize_limit:
-                split_factor = i
-                break
-        if split_factor > 1:
-            _, inner_i = s[x].split(inner_axis, split_factor)
-            s[x].vectorize(inner_i)
     return s
 
 schedule_elemwise = schedule_injective
