b00ab85cc42d0c7c6f1cbd5035bbb36e73e9520d
Eddie Yan <eqy@cs.washington.edu>
2018-08-09 14:30:28 -0700

    update test

--- tests/python/contrib/test_dlpack.py
+++ tests/python/contrib/test_dlpack.py
@@ -2,20 +2,24 @@ import tvm
 import numpy as np
 
 def test():
-    import torch
-    import torch.utils.dlpack
-    x = torch.rand(56, 56)
-    tvm_x = tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(x))
-    np.testing.assert_equal(x.numpy(), tvm_x.asnumpy())
+    a = np.random.randn(1337)
+    tvm_a = tvm.nd.array(a)
+    np.testing.assert_equal(tvm.nd.from_dlpack(tvm_a.to_dlpack()).asnumpy(), a)
 
-    y = tvm.nd.from_dlpack(tvm_x.to_dlpack())
-    print("finish from dlpack")
-    print(y.asnumpy())
-    print(tvm_x.shape)
-    print(y.shape)
-    np.testing.assert_equal(y.asnumpy(), tvm_x.asnumpy())
+    try:
+        import torch
+        import torch.utils.dlpack
 
-    np.testing.assert_equal(torch.utils.dlpack.from_dlpack(y.to_dlpack()).numpy(), tvm_x.asnumpy())
+        x = torch.rand(56, 56)
+        tvm_x = tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(x))
+        np.testing.assert_equal(x.numpy(), tvm_x.asnumpy())
+        y = tvm.nd.from_dlpack(tvm_x.to_dlpack())
+        np.testing.assert_equal(y.asnumpy(), tvm_x.asnumpy())
+        np.testing.assert_equal(torch.utils.dlpack.from_dlpack(y.to_dlpack()).numpy(), tvm_x.asnumpy())
+
+    except ImportError:
+        pass
+
 
 if __name__ ==  '__main__':
     test()
