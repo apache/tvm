91f48b92ea416219359900c4309e37d527c576dd
Leyuan Wang <laurawly@gmail.com>
2018-08-09 12:37:38 -0700

    fix pylint errors

--- topi/python/topi/cuda/nms.py
+++ topi/python/topi/cuda/nms.py
@@ -6,12 +6,34 @@ import tvm
 from tvm import api
 from topi.vision import nms
 
-def sort_pre_ir(data, index, sizes_out, axis, axis_mul_before, axis_mul_after):
-    max_threads = int(tvm.target.current_target(allow_none=False).max_num_threads)
+
+def sort_pre_ir(index, sizes_out, axis_mul_before, axis_mul_after):
+    """Low level IR routing subfunction 1/4 for computing segments' staring locatons.
+
+    Parameters
+    ----------
+    index : Buffer
+        Buffer of number of valid output boxes.
+
+    sizes_out : Buffer
+        Output buffer of start locations of each sorting segment.
+
+    axis_mul_before : int
+        The multiplication result of axis dimensions before axis.
+
+    axis_mul_after : int
+        The multiplication result of axis dimensions after axis.
+
+    Returns
+    -------
+    stmt : Stmt
+        The result IR statement.
+    """
+    max_threads = int(
+        tvm.target.current_target(allow_none=False).max_num_threads)
     tx = tvm.thread_axis("threadIdx.x")
     bx = tvm.thread_axis("blockIdx.x")
     ib = tvm.ir_builder.create()
-    p_data = ib.buffer_ptr(data)
     p_index = ib.buffer_ptr(index)
     dshape = sizes_out.shape
     sizes = ib.buffer_ptr(sizes_out)
@@ -31,14 +53,50 @@ def sort_pre_ir(data, index, sizes_out, axis, axis_mul_before, axis_mul_after):
     body = ib.get()
     return body
 
-def sort_pre_ir_data(data, index, sizes_in, data_out, index_out, axis, axis_mul_before, axis_mul_after):
+
+def sort_pre_ir_data(data, index, sizes_in, data_out, index_out, \
+                     axis, axis_mul_before, axis_mul_after):
+    """Low level IR routing subfunction 2/4 for flattening data and indices into segmented format.
+
+    Parameters
+    ----------
+    data: Buffer
+        Buffer of output boxes with class and score.
+
+    index : Buffer
+        Buffer of number of valid output boxes.
+
+    sizes_in : Buffer
+        Buffer of start locations of each sorting segment.
+
+    data_out : Buffer
+        Buffer of flattened segmented data.
+
+    index_out : Buffer
+        Buffer of flattened segmented indices.
+
+    axis : int
+        The axis used for sorting.
+
+    axis_mul_before : int
+        The multiplication result of axis dimensions before axis.
+
+    axis_mul_after : int
+        The multiplication result of axis dimensions after axis.
+
+    Returns
+    -------
+    stmt : Stmt
+        The result IR statement.
+    """
     ib = tvm.ir_builder.create()
     sizes = ib.buffer_ptr(sizes_in)
     p_index = ib.buffer_ptr(index)
     p_data = ib.buffer_ptr(data)
     data_new = ib.buffer_ptr(data_out)
     index_new = ib.buffer_ptr(index_out)
-    max_threads = int(tvm.target.current_target(allow_none=False).max_num_threads)
+    max_threads = int(
+        tvm.target.current_target(allow_none=False).max_num_threads)
     tx = tvm.thread_axis("threadIdx.x")
     bx = tvm.thread_axis("blockIdx.x")
     dshape = tvm.max(sizes_in.shape[0], p_index[0])
@@ -53,7 +111,7 @@ def sort_pre_ir_data(data, index, sizes_in, data_out, index_out, axis, axis_mul_
             j = tid % axis_mul_after
             current_sort_num = p_index[tid]
             base_idx = i * data.shape[axis] * axis_mul_after + j
-            with ib.for_range(0, current_sort_num, name = "k") as k:
+            with ib.for_range(0, current_sort_num, name="k") as k:
                 full_idx = base_idx + k * axis_mul_after
                 with ib.if_scope(tid == 0):
                     start = 0
@@ -63,20 +121,62 @@ def sort_pre_ir_data(data, index, sizes_in, data_out, index_out, axis, axis_mul_
                 data_new[start + k] = p_data[full_idx]
     with ib.else_scope():
         with ib.if_scope(tid == 0):
-            with ib.for_range(0, p_index[0], name = "k") as k:
+            with ib.for_range(0, p_index[0], name="k") as k:
                 index_new[k] = k
 
     body = ib.get()
     return body
 
-def sort_oet_ir(data, index, new_data, new_index, loc, out_index, axis_mul_before, axis_mul_after, axis, is_descend):
-    max_threads = int(tvm.target.current_target(allow_none=False).max_num_threads)
+def sort_oet_ir(data, index, new_data, new_index, loc, out_index, axis_mul_before, \
+                axis_mul_after, axis, is_descend):
+    """Low level IR routing subfunction 3/4 for Odd-Even-Transposition sorting.
+
+    Parameters
+    ----------
+    data: Buffer
+        Buffer of output boxes with class and score.
+
+    index : Buffer
+        Buffer of number of valid output boxes.
+
+    new_data : Buffer
+        Buffer of flattened segmented data.
+
+    new_index : Buffer
+        Buffer of flattened segmented indices.
+
+    loc : Buffer
+        Buffer of start locations of each sorting segment.
+
+    out_index : Buffer
+        Output buffer of output box indexes sorted by score in a flattened segmented format.
+
+    axis_mul_before : int
+        The multiplication result of axis dimensions before axis.
+
+    axis_mul_after : int
+        The multiplication result of axis dimensions after axis.
+
+    axis : int
+        The axis used for sorting.
+
+    is_descend : bool
+        If the sorted data is in descending order.
+
+    Returns
+    -------
+    stmt : Stmt
+        The result IR statement.
+    """
+    max_threads = int(
+        tvm.target.current_target(allow_none=False).max_num_threads)
     tx = tvm.thread_axis("threadIdx.x")
     bx = tvm.thread_axis("blockIdx.x")
     ib = tvm.ir_builder.create()
     dshape = loc.shape
     fshape = data.shape[axis] * dshape[0]
-    temp_data = ib.allocate("float32", dshape, name="temp_data", scope = "local")
+    temp_data = ib.allocate(
+        "float32", dshape, name="temp_data", scope="local")
     p_data = ib.buffer_ptr(data)
     p_index = ib.buffer_ptr(index)
     data_new = ib.buffer_ptr(new_data)
@@ -96,8 +196,8 @@ def sort_oet_ir(data, index, new_data, new_index, loc, out_index, axis_mul_befor
             with ib.else_scope():
                 start = sizes[tid-1]
             # OddEvenTransposeSort
-            with ib.for_range(0, p_index[tid], name = "k") as k:
-                with ib.for_range(0, p_index[tid] - 1, name = "i") as i:
+            with ib.for_range(0, p_index[tid], name="k") as k:
+                with ib.for_range(0, p_index[tid] - 1, name="i") as i:
                     with ib.if_scope(i % 2 == k % 2):
                         with ib.if_scope(((data_new[i+start] < data_new[i+start+1]) == is_descend)):
                             temp_data[tid] = data_new[i+start]
@@ -107,20 +207,22 @@ def sort_oet_ir(data, index, new_data, new_index, loc, out_index, axis_mul_befor
                             index_new[i+start] = index_new[i+start+1]
                             index_new[i+start+1] = index_out[tid]
         with ib.if_scope(tid < 1):
-            with ib.for_range(0, sizes[dshape[0] - 1], name = "i") as i:
+            with ib.for_range(0, sizes[dshape[0] - 1], name="i") as i:
                 index_out[i] = index_new[i]
     with ib.else_scope():
-        with ib.for_range(0, fshape, name = "k", for_type="unroll") as k:
+        with ib.for_range(0, fshape, name="k", for_type="unroll") as k:
             with ib.if_scope(tvm.all(k % 2 == tid % 2, tid < fshape)):
                 with ib.if_scope(k % 2 == 0):
-                    with ib.if_scope(tvm.all(tid + 1 < fshape, (p_data[tid] < p_data[tid+1]) == is_descend)):
+                    with ib.if_scope(tvm.all(tid + 1 < fshape, (p_data[tid] < p_data[tid+1]) \
+                                             == is_descend)):
                         data_new[tid] = p_data[tid+1]
                         index_out[tid] = index_new[tid+1]
                     with ib.else_scope():
                         data_new[tid] = p_data[tid]
                         index_out[tid] = index_new[tid]
                 with ib.else_scope():
-                    with ib.if_scope(tvm.all(tid + 1 < fshape, (data_new[tid] < data_new[tid+1]) == is_descend)):
+                    with ib.if_scope(tvm.all(tid + 1 < fshape, (data_new[tid] < data_new[tid+1]) \
+                                             == is_descend)):
                         p_data[tid] = data_new[tid+1]
                         index_new[tid] = index_out[tid+1]
                     with ib.else_scope():
@@ -135,7 +237,8 @@ def sort_oet_ir(data, index, new_data, new_index, loc, out_index, axis_mul_befor
                         data_new[tid] = p_data[tid]
                         index_out[tid] = index_new[tid]
                 with ib.else_scope():
-                    with ib.if_scope(tvm.all(tid > 0, (data_new[tid-1] < data_new[tid]) == is_descend)):
+                    with ib.if_scope(tvm.all(tid > 0, (data_new[tid-1] < data_new[tid]) \
+                                             == is_descend)):
                         p_data[tid] = data_new[tid-1]
                         index_new[tid] = index_out[tid-1]
                     with ib.else_scope():
@@ -143,13 +246,51 @@ def sort_oet_ir(data, index, new_data, new_index, loc, out_index, axis_mul_befor
                         index_new[tid] = index_out[tid]
         with ib.if_scope(fshape % 2 == 1):
             with ib.if_scope(tid < 1):
-                with ib.for_range(0, fshape, name = "k") as k:
+                with ib.for_range(0, fshape, name="k") as k:
                     index_out[tid] = index_new[tid]
     body = ib.get()
     return body
 
+
 def sort_ir_out(data, index, new_index, loc, output, axis_mul_before, axis_mul_after, axis):
-    max_threads = int(tvm.target.current_target(allow_none=False).max_num_threads)
+    """Low level IR routing subfunction 4/4 for writing sorted indices to output format.
+
+    Parameters
+    ----------
+    data: Buffer
+        Buffer of output boxes with class and score.
+
+    index : Buffer
+        Buffer of number of valid output boxes.
+
+    new_index : Buffer
+        Buffer of sorted indices in a flatten format.
+
+    loc : Buffer
+        Buffer of start locations of each sorting segment.
+
+    output : Buffer
+        Output buffer of output box indexes sorted by score.
+
+    axis_mul_before : int
+        The multiplication result of axis dimensions before axis.
+
+    axis_mul_after : int
+        The multiplication result of axis dimensions after axis.
+
+    axis : int
+        The axis used for sorting.
+
+    is_descend : bool
+        If the sorted data is in descending order.
+
+    Returns
+    -------
+    stmt : Stmt
+        The result IR statement.
+    """
+    max_threads = int(
+        tvm.target.current_target(allow_none=False).max_num_threads)
     tx = tvm.thread_axis("threadIdx.x")
     bx = tvm.thread_axis("blockIdx.x")
     ib = tvm.ir_builder.create()
@@ -169,12 +310,13 @@ def sort_ir_out(data, index, new_index, loc, output, axis_mul_before, axis_mul_a
             i = tid / axis_mul_after
             j = tid % axis_mul_after
             base_idx = i * data.shape[axis] * axis_mul_after + j
-            with ib.for_range(0, data.shape[axis], name = "k") as k:
+            with ib.for_range(0, data.shape[axis], name="k") as k:
                 with ib.if_scope(tid == 0):
                     start = 0
                 with ib.else_scope():
                     start = sizes[tid-1]
-                p_out[base_idx + k * axis_mul_after] = tvm.select(k < p_index[tid], index_new[k+start], k)
+                p_out[base_idx + k * axis_mul_after] = tvm.select(
+                    k < p_index[tid], index_new[k+start], k)
     with ib.else_scope():
         with ib.if_scope(tid < data.shape[axis]):
             p_out[tid] = tvm.select(tid < p_index[0], index_new[tid], tid)
@@ -182,7 +324,41 @@ def sort_ir_out(data, index, new_index, loc, output, axis_mul_before, axis_mul_a
     body = ib.get()
     return body
 
+
 def sort_gpu(data, data_buf, index, index_buf, output_buf, axis, is_descend):
+    """Function to generate low level IR to do sorting on the GPU, use it by calling sort_gpu.
+
+    Parameters
+    ----------
+    data: tvm.Tensor
+        3-D tensor with shape [batch_size, num_anchors, 6].
+        The last dimension should be in format of
+        [class_id, score, box_left, box_top, box_right, box_bottom].
+
+    data_buf: Buffer
+        2D Buffer of input boxes' score with shape [batch_size, num_anchors].
+
+    index : tvm.Tensor
+        1-D tensor for valid number of boxes.
+
+    index_buf : Buffer
+        Buffer of number of valid number of boxes.
+
+    output_buf : Buffer
+        Output buffer of indicies of sorted tensor.
+
+    axis : int
+        The axis used for sorting.
+
+    is_descend : bool
+        If the sorted data is in descending order.
+
+    Returns
+    -------
+    out : tvm.Tensor
+        3-D tensor with shape [batch_size, num_anchors].
+    """
+
     ndim = len(data.shape)
     assert data.dtype == "float32", "Currently only supports input dtype to be float32"
     assert axis < ndim, "Axis out of boundary for input ndim %d" % ndim
@@ -200,18 +376,21 @@ def sort_gpu(data, data_buf, index, index_buf, output_buf, axis, is_descend):
     dshape = axis_mul_before*axis_mul_after
     fshape = data.shape[axis] * dshape
 
-    loc_buf = api.decl_buffer(dshape, index.dtype, "sizes", data_alignment = 8)
-    new_index_buf = api.decl_buffer(fshape, index.dtype, "index_new", data_alignment = 8)
-    out_index_buf = api.decl_buffer(fshape, index.dtype, "index_out", data_alignment = 8)
-    new_data_buf = api.decl_buffer(dshape, data.dtype, "data_new", data_alignment = 8)
+    loc_buf = api.decl_buffer(dshape, index.dtype, "sizes", data_alignment=8)
+    new_index_buf = api.decl_buffer(
+        fshape, index.dtype, "index_new", data_alignment=8)
+    out_index_buf = api.decl_buffer(
+        fshape, index.dtype, "index_out", data_alignment=8)
+    new_data_buf = api.decl_buffer(
+        dshape, data.dtype, "data_new", data_alignment=8)
 
     loc = \
         tvm.extern([(dshape,)],
-                   [data, index],
+                   [index],
                    lambda ins, outs: sort_pre_ir(
-                       ins[0], ins[1], outs[0], axis, axis_mul_before, axis_mul_after),
+                       ins[0], outs[0], axis_mul_before, axis_mul_after),
                    dtype=[index.dtype],
-                   in_buffers=[data_buf, index_buf],
+                   in_buffers=index_buf,
                    out_buffers=[loc_buf],
                    tag="sorting_prepare")
 
@@ -219,7 +398,8 @@ def sort_gpu(data, data_buf, index, index_buf, output_buf, axis, is_descend):
         tvm.extern([(dshape,), (fshape,)],
                    [data, index, loc],
                    lambda ins, outs: sort_pre_ir_data(
-                       ins[0], ins[1], ins[2], outs[0], outs[1], axis, axis_mul_before, axis_mul_after),
+                       ins[0], ins[1], ins[2], outs[0], outs[1], axis,
+                       axis_mul_before, axis_mul_after),
                    dtype=[data.dtype, index.dtype],
                    in_buffers=[data_buf, index_buf, loc_buf],
                    out_buffers=[new_data_buf, new_index_buf],
@@ -229,20 +409,23 @@ def sort_gpu(data, data_buf, index, index_buf, output_buf, axis, is_descend):
         tvm.extern([(fshape,)],
                    [data, index, data_new, index_new, loc],
                    lambda ins, outs: sort_oet_ir(
-                       ins[0], ins[1], ins[2], ins[3], ins[4], outs[0], axis_mul_before, axis_mul_after, axis, is_descend),
+                       ins[0], ins[1], ins[2], ins[3], ins[4], outs[0],
+                       axis_mul_before, axis_mul_after, axis, is_descend),
                    dtype=[index.dtype],
-                   in_buffers=[data_buf, index_buf, new_data_buf, new_index_buf, loc_buf],
+                   in_buffers=[data_buf, index_buf,
+                               new_data_buf, new_index_buf, loc_buf],
                    out_buffers=[out_index_buf],
                    tag="sorting_oet")
     out = \
         tvm.extern([data.shape],
-                    [data, index, index_out, loc],
-                    lambda ins, outs: sort_ir_out(
-                        ins[0], ins[1], ins[2], ins[3], outs[0], axis_mul_before, axis_mul_after, axis),
-                    dtype=[index.dtype],
-                    in_buffers=[data_buf, index_buf, out_index_buf, loc_buf],
-                    out_buffers=output_buf,
-                    tag="sorting_output")
+                   [data, index, index_out, loc],
+                   lambda ins, outs: sort_ir_out(
+                       ins[0], ins[1], ins[2], ins[3], outs[0],
+                       axis_mul_before, axis_mul_after, axis),
+                   dtype=[index.dtype],
+                   in_buffers=[data_buf, index_buf, out_index_buf, loc_buf],
+                   out_buffers=output_buf,
+                   tag="sorting_output")
     return out
 
 
@@ -439,7 +622,8 @@ def nms_gpu(data, valid_count, nms_threshold=0.5, force_suppress=False, nms_topk
     sort_tensor_buf = api.decl_buffer(score_shape, sort_tensor_dtype,
                                       "sort_tensor_buf", data_alignment=8)
 
-    sort_tensor = sort_gpu(score_tensor, score_tensor_buf, valid_count, valid_count_buf, sort_tensor_buf, score_axis, True)
+    sort_tensor = sort_gpu(score_tensor, score_tensor_buf, valid_count,
+                           valid_count_buf, sort_tensor_buf, score_axis, True)
     out = \
         tvm.extern(data.shape,
                    [data, sort_tensor, valid_count],
