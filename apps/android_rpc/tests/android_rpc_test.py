8e16fd36e290fbd8f47d7ac751f94c4247aae088
Dayananda V <dayanandasiet@gmail.com>
2018-08-10 00:29:38 +0530

    Vulkan TVM Android Support
    1. Review comment reworked.

--- apps/android_rpc/tests/android_rpc_test.py
+++ apps/android_rpc/tests/android_rpc_test.py
@@ -21,7 +21,9 @@ key = "android"
 arch = "arm64"
 target = "llvm -target=%s-linux-android" % arch
 
-test_opencl = True
+# whether enable to execute test on OpenCL target hardware
+test_opencl = False
+# whether enable to execute test on Vulkan target hardware
 test_vulkan = False
 
 def test_rpc_module():
@@ -29,7 +31,37 @@ def test_rpc_module():
     n = tvm.convert(1024)
     A = tvm.placeholder((n,), name='A')
     B = tvm.compute(A.shape, lambda *i: A(*i) + 1.0, name='B')
+    a_np = np.random.uniform(size=1024).astype(A.dtype)
     temp = util.tempdir()
+
+    # Establish remote connection with target hardware
+    tracker = rpc.connect_tracker(tracker_host, tracker_port)
+    remote = tracker.request(key, priority=0,
+                             session_timeout=60)
+
+    # Compile the Graph for CPU target
+    s = tvm.create_schedule(B.op)
+    xo, xi = s[B].split(B.op.axis[0], factor=64)
+    s[B].parallel(xi)
+    s[B].pragma(xo, "parallel_launch_point")
+    s[B].pragma(xi, "parallel_barrier_when_finish")
+    f = tvm.build(s, [A, B], target, name="myadd_cpu")
+    path_dso_cpu = temp.relpath("cpu_lib.so")
+    f.export_library(path_dso_cpu, ndk.create_shared)
+
+    # Execute the portable graph on cpu target
+    print('Run CPU test ...')
+    ctx = remote.cpu(0)
+    remote.upload(path_dso_cpu)
+    f2 = remote.load_module("cpu_lib.so")
+    a = tvm.nd.array(a_np, ctx)
+    b = tvm.nd.array(np.zeros(1024, dtype=A.dtype), ctx)
+    time_f = f2.time_evaluator(f2.entry_name, ctx, number=10)
+    cost = time_f(a, b).mean
+    print('%g secs/op\n' % cost)
+    np.testing.assert_equal(b.asnumpy(), a.asnumpy() + 1)
+
+    # Compile the Graph for OpenCL target
     if test_opencl:
         s = tvm.create_schedule(B.op)
         xo, xi = s[B].split(B.op.axis[0], factor=64)
@@ -38,9 +70,21 @@ def test_rpc_module():
         # Build the dynamic lib.
         # If we don't want to do metal and only use cpu, just set target to be target
         f = tvm.build(s, [A, B], "opencl", target_host=target, name="myadd")
-        path_dso1 = temp.relpath("dev_lib_cl.so")
-        f.export_library(path_dso1, ndk.create_shared)
+        path_dso_cl = temp.relpath("dev_lib_cl.so")
+        f.export_library(path_dso_cl, ndk.create_shared)
 
+        print('Run GPU(OpenCL Flavor) test ...')
+        ctx = remote.cl(0)
+        remote.upload(path_dso_cl)
+        f1 = remote.load_module("dev_lib_cl.so")
+        a = tvm.nd.array(a_np, ctx)
+        b = tvm.nd.array(np.zeros(1024, dtype=A.dtype), ctx)
+        time_f = f1.time_evaluator(f1.entry_name, ctx, number=10)
+        cost = time_f(a, b).mean
+        print('%g secs/op\n' % cost)
+        np.testing.assert_equal(b.asnumpy(), a.asnumpy() + 1)
+
+    # Compile the Graph for Vulkan target
     if test_vulkan:
         s = tvm.create_schedule(B.op)
         xo, xi = s[B].split(B.op.axis[0], factor=64)
@@ -49,58 +93,20 @@ def test_rpc_module():
         # Build the dynamic lib.
         # If we don't want to do metal and only use cpu, just set target to be target
         f = tvm.build(s, [A, B], "vulkan", target_host=target, name="myadd")
-        path_dso2 = temp.relpath("dev_lib_vulkan.so")
-        f.export_library(path_dso2, ndk.create_shared)
+        path_dso_vulkan = temp.relpath("dev_lib_vulkan.so")
+        f.export_library(path_dso_vulkan, ndk.create_shared)
 
-    s = tvm.create_schedule(B.op)
-    xo, xi = s[B].split(B.op.axis[0], factor=64)
-    s[B].parallel(xi)
-    s[B].pragma(xo, "parallel_launch_point")
-    s[B].pragma(xi, "parallel_barrier_when_finish")
-    f = tvm.build(s, [A, B], target, name="myadd_cpu")
-    path_dso3 = temp.relpath("cpu_lib.so")
-    f.export_library(path_dso3, ndk.create_shared)
-
-    tracker = rpc.connect_tracker(tracker_host, tracker_port)
-    remote = tracker.request(key, priority=0,
-                             session_timeout=60)
-
-    a_np = np.random.uniform(size=1024).astype(A.dtype)
-
-    if test_opencl:
-        print('Run GPU(OpenCL Flavor) test ...')
-        ctx = remote.cl(0)
-        remote.upload(path_dso1)
-        f1 = remote.load_module("dev_lib_cl.so")
-        a = tvm.nd.array(a_np, ctx)
-        b = tvm.nd.array(np.zeros(1024, dtype=A.dtype), ctx)
-        time_f = f1.time_evaluator(f1.entry_name, ctx, number=10)
-        cost = time_f(a, b).mean
-        print('%g secs/op' % cost)
-        np.testing.assert_equal(b.asnumpy(), a.asnumpy() + 1)
-
-    if test_vulkan:
         print('Run GPU(Vulkan Flavor) test ...')
         ctx = remote.vulkan(0)
-        remote.upload(path_dso2)
+        remote.upload(path_dso_vulkan)
         f1 = remote.load_module("dev_lib_vulkan.so")
         a = tvm.nd.array(a_np, ctx)
         b = tvm.nd.array(np.zeros(1024, dtype=A.dtype), ctx)
         time_f = f1.time_evaluator(f1.entry_name, ctx, number=10)
         cost = time_f(a, b).mean
-        print('%g secs/op' % cost)
+        print('%g secs/op\n' % cost)
         np.testing.assert_equal(b.asnumpy(), a.asnumpy() + 1)
 
-    print('Run CPU test ...')
-    ctx = remote.cpu(0)
-    remote.upload(path_dso3)
-    f2 = remote.load_module("cpu_lib.so")
-    a = tvm.nd.array(a_np, ctx)
-    b = tvm.nd.array(np.zeros(1024, dtype=A.dtype), ctx)
-    time_f = f2.time_evaluator(f2.entry_name, ctx, number=10)
-    cost = time_f(a, b).mean
-    print('%g secs/op' % cost)
-    np.testing.assert_equal(b.asnumpy(), a.asnumpy() + 1)
 
 if __name__ == "__main__":
     test_rpc_module()
