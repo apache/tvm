77dc1c446832a8c70b005690c744e00ff9bcf00a
Tianqi Chen <tqchen@users.noreply.github.com>
2018-08-11 09:15:05 -0700

    [RUNTIME] Refactor to enable stackvm in runtime. (#1588)

--- include/tvm/runtime/util.h
+++ include/tvm/runtime/util.h
@@ -21,7 +21,33 @@ namespace runtime {
 inline bool TypeMatch(TVMType t, int code, int bits, int lanes = 1) {
   return t.code == code && t.bits == bits && t.lanes == lanes;
 }
-
 }  // namespace runtime
+}  // namespace tvm
+// Forward declare the intrinsic id we need
+// in structure fetch to enable stackvm in runtime
+namespace tvm {
+namespace ir {
+namespace intrinsic {
+/*! \brief The kind of structure field info used in intrinsic */
+enum TVMStructFieldKind : int {
+  // array head address
+  kArrAddr,
+  kArrData,
+  kArrShape,
+  kArrStrides,
+  kArrNDim,
+  kArrTypeCode,
+  kArrTypeBits,
+  kArrTypeLanes,
+  kArrByteOffset,
+  kArrDeviceId,
+  kArrDeviceType,
+  kArrKindBound_,
+  // TVMValue field
+  kTVMValueContent,
+  kTVMValueKindBound_
+};
+}  // namespace intrinsic
+}  // namespace ir
 }  // namespace tvm
 #endif  // TVM_RUNTIME_UTIL_H_
