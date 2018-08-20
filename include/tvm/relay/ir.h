425d5f24d285163ee855b18db4be7f0b3113995d
Jared Roesch <roeschinc@gmail.com>
2018-08-19 23:05:16 -0700

    Add ir.h

---
+++ include/tvm/relay/ir.h
@@ -0,0 +1,20 @@
+/*!
+ *  Copyright (c) 2018 by Contributors
+ * \file tvm/relay/ir.h
+ * \brief The Relay intermediate representation's core data structures.
+ */
+#ifndef TVM_RELAY_IR_H_
+#define TVM_RELAY_IR_H_
+
+#include "./base.h"
+#include "./type.h"
+#include "./expr.h"
+#include "./op.h"
+
+// namespace tvm {
+// namespace relay {
+
+// }  // namespace relay
+// }  // namespace tvm
+
+#endif  // TVM_RELAY_IR_H_
