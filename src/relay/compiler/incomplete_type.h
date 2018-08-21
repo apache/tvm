4a84f2595e7ce10a146550609a271ffd8e0f07a5
Jared Roesch <roeschinc@gmail.com>
2018-08-21 00:06:44 -0700

    Add incomplete_type.h

---
+++ src/relay/compiler/incomplete_type.h
@@ -0,0 +1,36 @@
+/*!
+ *  Copyright (c) 2018 by Contributors
+ * \file incomplete_type.h
+ * \brief A way to defined arbitrary function signature with dispatch on types.
+ */
+
+#ifndef TVM_RELAY_COMPILER_INCOMPLETE_TYPE_H
+#define TVM_RELAY_COMPILER_INCOMPLETE_TYPE_H
+
+#include "tvm/relay/ir.h"
+
+namespace tvm {
+namespace relay {
+
+/*!
+ * \brief Represents a portion of an incomplete type.
+ */
+class IncompleteType;
+
+/*! \brief IncompleteType container node */
+class IncompleteTypeNode : public TypeNode {
+ public:
+  void VisitAttrs(tvm::AttrVisitor* v) final {}
+
+  TVM_DLL static IncompleteType make();
+
+  static constexpr const char* _type_key = "relay.IncompleteType";
+  TVM_DECLARE_NODE_TYPE_INFO(IncompleteTypeNode, TypeNode);
+};
+
+RELAY_DEFINE_NODE_REF(IncompleteType, IncompleteTypeNode, Type);
+
+} // namespace relay
+} // namespace tvm
+
+#endif  // TVM_RELAY_COMPILER_INCOMPLETE_TYPE_H
