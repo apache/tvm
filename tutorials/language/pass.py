6103ee2edf3b8d4377fee8af09752b5976a94494
Jian Weng <jian.weng@ucla.edu>
2018-08-30 15:33:37 -0700

    where does this eof come from

--- tutorials/language/pass.py
+++ tutorials/language/pass.py
@@ -46,7 +46,6 @@ print(ir)
 # Thus, we define this vectorize function and implement it step by step.
 #
 
-def vectorize(stmt):
 ######################################################################
 # TVM already provides two class for users to both analyze the IR and transform IR.
 #
@@ -64,18 +63,13 @@ def vectorize(stmt):
 #     refreshed every recursion but the array values will be reserved.
 #
 
-    loops = []
-    def find_width8(op):
-        """ Find all the 'For' nodes whose extent can be divided by 8. """
-        if isinstance(op, tvm.stmt.For):
-            if isinstance(op.extent, tvm.expr.IntImm):
-                if op.extent.value % 8 == 0:
-                    loops.append(op)
-
-    tvm.ir_pass.PostOrderVisit(stmt, find_width8)
-
-    if not loops:
-        return stmt
+loops = []
+def find_width8(op):
+    """ Find all the 'For' nodes whose extent can be divided by 8. """
+    if isinstance(op, tvm.stmt.For):
+        if isinstance(op.extent, tvm.expr.IntImm):
+            if op.extent.value % 8 == 0:
+                loops.append(op)
 
 #####################################################################
 # IR Transformation
@@ -91,17 +85,26 @@ def vectorize(stmt):
 #     If the pre-order function is called and returns a value which is not None, the post-order
 #     function will be skipped.
 #
-    def vectorize8(op):
-        """ Split can vectorize the loops found in `find_width8`. """
-        if op in loops:
-            extent = op.extent.value
-            name = op.loop_var.name
-            lo, li = tvm.var(name + '.outer'), tvm.var(name + '.inner')
-            body = tvm.ir_pass.Substitute(op.body, {op.loop_var: lo * 8 + li})
-            body = tvm.make.For(li, 0, 8, tvm.stmt.For.Vectorized, 0, body)
-            body = tvm.make.For(lo, 0, extent // 8, tvm.stmt.For.Serial, 0, body)
-            return body
-        return None
+
+def vectorize8(op):
+    """ Split can vectorize the loops found in `find_width8`. """
+    if op in loops:
+        extent = op.extent.value
+        name = op.loop_var.name
+        lo, li = tvm.var(name + '.outer'), tvm.var(name + '.inner')
+        body = tvm.ir_pass.Substitute(op.body, {op.loop_var: lo * 8 + li})
+        body = tvm.make.For(li, 0, 8, tvm.stmt.For.Vectorized, 0, body)
+        body = tvm.make.For(lo, 0, extent // 8, tvm.stmt.For.Serial, 0, body)
+        return body
+    return None
+
+def vectorize(stmt):
+    global loops
+
+    tvm.ir_pass.PostOrderVisit(stmt, find_width8)
+
+    if not loops:
+        return stmt
 
     # The last list arugment indicates what kinds of nodes will be transformed.
     # Thus, in this case only `For` nodes will call `vectorize8`
