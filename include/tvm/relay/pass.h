8db9d577940302fa30102b3a3ce7c9db0bdb8e81
Jared Roesch <roeschinc@gmail.com>
2018-08-31 17:01:45 -0700

    Add skeleton for kind checker

--- include/tvm/relay/pass.h
+++ include/tvm/relay/pass.h
@@ -6,8 +6,8 @@
 #ifndef TVM_RELAY_PASS_H_
 #define TVM_RELAY_PASS_H_
 
-#include "tvm/relay/expr.h"
-#include "tvm/relay/environment.h"
+#include <tvm/relay/expr.h>
+#include <tvm/relay/environment.h>
 
 namespace tvm {
 namespace relay {
@@ -17,6 +17,22 @@ namespace relay {
  * checked type field populated with the result type.
  */
 Expr InferType(const Environment & env, const Expr & e);
+
+/*!
+ * \brief Check that types are well formed by applying "kinding rules".
+ *
+ * This pass ensures we do not do things that violate the design of the
+ * type system when writing down types.
+ *
+ * For example tensors are not allowed to contain functions in Relay.
+ *
+ * We check this by ensuring the `dtype` field of a Tensor always contains
+ * a data type such as `int`, `float`, `uint`.
+ * 
+ * \param env The global environment.
+ * \param t The type to check.
+ */
+void KindCheck(const Environment& env, const Type& t);
 
 }  // namespace relay
 }  // namespace tvm
---
+++ src/relay/pass/kind_check.cc
@@ -0,0 +1,42 @@
+/*!
+ * Copyright (c) 2018 by Contributors
+ *
+ * \file kindchecker.cc
+ *
+ * \brief Check that types are well formed by applying "kinding rules".
+ *
+ * This pass ensures we do not do things that violate the design of the
+ * type system when writing down types.
+ *
+ * For example tensors are not allowed to contain functions in Relay.
+ *
+ * We check this by ensuring the `dtype` field of a Tensor always 
+ * contains a data type such as `int`, `float`, `uint`.
+ */
+#include <tvm/ir_functor.h>
+#include <tvm/relay/pass.h>
+#include "./type_visitor.h"
+
+namespace tvm {
+namespace relay {
+
+using namespace tvm::runtime;
+
+struct KindChecker : TypeVisitor<> {
+  bool valid;
+
+  KindChecker() : valid(true) {}
+
+  bool Check(const Type &t) {
+    this->VisitType(t);
+    return valid;
+  }
+};
+
+bool KindCheck(const Type &t) {
+  KindChecker kc;
+  return kc.Check(t);
+}
+
+}  // namespace relay
+}  // namespace tvm
