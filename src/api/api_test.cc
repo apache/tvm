8078eeb9fc55b438a1bde2fa635db2c65715d2e0
Tianqi Chen <tqchen@users.noreply.github.com>
2018-09-14 10:59:22 -0700

    [RUNTIME] Support TVMContext (#1720)

--- src/api/api_test.cc
+++ src/api/api_test.cc
@@ -35,6 +35,16 @@ TVM_REGISTER_API("_nop")
 .set_body([](TVMArgs args,  TVMRetValue *ret) {
   });
 
+TVM_REGISTER_API("_context_test")
+.set_body([](TVMArgs args,  TVMRetValue *ret) {
+    DLContext ctx = args[0];
+    int dtype = args[1];
+    int did = args[2];
+    CHECK_EQ(static_cast<int>(ctx.device_type), dtype);
+    CHECK_EQ(static_cast<int>(ctx.device_id), did);
+    *ret = ctx;
+  });
+
 // internal fucntion used for debug and testing purposes
 TVM_REGISTER_API("_ndarray_use_count")
 .set_body([](TVMArgs args,  TVMRetValue *ret) {
