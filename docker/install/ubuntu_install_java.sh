3d0be3b0e311160a88d77694667509cd8f70466f
Sergey Mironov <grrwlf@gmail.com>
2018-09-14 19:26:34 +0300

    Add demo_android Dockerfile (#1646)

--- docker/install/ubuntu_install_java.sh
+++ docker/install/ubuntu_install_java.sh
@@ -1,1 +1,4 @@
+set -o errexit -o nounset
 apt-get update && apt-get install -y openjdk-8-jdk maven
+test -d "/usr/lib/jvm/java-8-openjdk-amd64/jre"
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre" >> /etc/profile
