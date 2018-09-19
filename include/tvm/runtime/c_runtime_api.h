c4421f575528e8af46557d46ddbccd4e13cdcd93
Tianqi Chen <tqchen@users.noreply.github.com>
2018-09-18 23:28:12 -0700

    [SUBMODULE] update submodule to latest (#1728)

--- include/tvm/runtime/c_runtime_api.h
+++ include/tvm/runtime/c_runtime_api.h
@@ -62,11 +62,7 @@ typedef int64_t tvm_index_t;
 typedef enum {
   kDLAOCL = 5,
   kDLSDAccel = 6,
-  kDLVulkan = 7,
   kOpenGL = 11,
-  // Extension DRAM type, used for quickly test extension device
-  // The device api can differ depending on the xpu driver registered.
-  kExtDev = 12,
   // AddExtraTVMType which is not in DLPack here
 } TVMDeviceExtType;
 
