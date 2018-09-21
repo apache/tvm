ec0d497c69ca307fb998c3d81c0a7e48bb5f18d6
Tianqi Chen <tqchen@users.noreply.github.com>
2018-09-20 20:17:24 -0700

    [NODE][RELAY] Move most of the reference related code to node (#1747)

--- src/relay/ir/expr_functor.cc
+++ src/relay/ir/expr_functor.cc
@@ -13,33 +13,33 @@ namespace tvm {
 namespace relay {
 
 Expr ExprMutator::Mutate(const Expr& expr) {
-  auto cached_expr = this->memo_.find(expr);
-  if (cached_expr != this->memo_.end()) {
-    return (*cached_expr).second;
+  auto it = this->memo_.find(expr);
+  if (it != this->memo_.end()) {
+    return it->second;
   } else {
-    auto new_expr = this->ExprMutator::VisitExpr(expr, expr);
-    this->memo_.Set(expr, new_expr);
+    Expr new_expr = ExprMutator::VisitExpr(expr);
+    memo_[expr] = new_expr;
     return new_expr;
   }
 }
 
-Expr ExprMutator::VisitExpr_(const VarNode* op, const Expr& expr) {
-  return expr;
+Expr ExprMutator::VisitExpr_(const VarNode* op) {
+  return GetRef<Expr>(op);
 }
 
-Expr ExprMutator::VisitExpr_(const ConstantNode* op, const Expr& expr) {
-  return expr;
+Expr ExprMutator::VisitExpr_(const ConstantNode* op) {
+  return GetRef<Expr>(op);
 }
 
-Expr ExprMutator::VisitExpr_(const GlobalVarNode* op, const Expr& expr) {
-  return expr;
+Expr ExprMutator::VisitExpr_(const GlobalVarNode* op) {
+  return GetRef<Expr>(op);
 }
 
-Expr ExprMutator::VisitExpr_(const OpNode* op, const Expr& expr) {
-  return expr;
+Expr ExprMutator::VisitExpr_(const OpNode* op) {
+  return GetRef<Expr>(op);
 }
 
-Expr ExprMutator::VisitExpr_(const TupleNode* op, const Expr& e) {
+Expr ExprMutator::VisitExpr_(const TupleNode* op) {
   tvm::Array<Expr> fields;
   bool all_fields_unchanged = true;
   for (auto field : op->fields) {
@@ -49,23 +49,23 @@ Expr ExprMutator::VisitExpr_(const TupleNode* op, const Expr& e) {
   }
 
   if (all_fields_unchanged) {
-    return e;
+    return GetRef<Expr>(op);
   } else {
     return TupleNode::make(fields);
   }
 }
 
-Expr ExprMutator::VisitExpr_(const ParamNode* op, const Expr& e) {
+Expr ExprMutator::VisitExpr_(const ParamNode* op) {
   Var var = Downcast<Var>(this->Mutate(op->var));
   auto type = this->VisitType(op->type);
-  if (var == op->var && type == op->type) {
-    return e;
+  if (op->var.same_as(var) && op->type.same_as(type)) {
+    return GetRef<Expr>(op);
   } else {
     return ParamNode::make(var, type);
   }
 }
 
-Expr ExprMutator::VisitExpr_(const FunctionNode* op, const Expr& e) {
+Expr ExprMutator::VisitExpr_(const FunctionNode* op) {
   tvm::Array<TypeParam> ty_params;
   bool all_ty_params_changed = true;
 
@@ -86,74 +86,82 @@ Expr ExprMutator::VisitExpr_(const FunctionNode* op, const Expr& e) {
   auto ret_type = this->VisitType(op->ret_type);
   auto body = this->Mutate(op->body);
 
-  if (ty_params.same_as(op->type_params) && params.same_as(op->params) &&
-      ret_type.same_as(op->ret_type) && body.same_as(op->body)) {
-    return e;
+  if (ty_params.same_as(op->type_params) &&
+      params.same_as(op->params) &&
+      ret_type.same_as(op->ret_type) &&
+      body.same_as(op->body)) {
+    return GetRef<Expr>(op);
   } else {
     return FunctionNode::make(params, ret_type, body, ty_params);
   }
 }
 
-Expr ExprMutator::VisitExpr_(const CallNode* call_node, const Expr& e) {
-  auto op = this->Mutate(call_node->op);
+Expr ExprMutator::VisitExpr_(const CallNode* call_node) {
+  auto new_op = this->Mutate(call_node->op);
+  bool unchanged = call_node->op.same_as(new_op);
 
   tvm::Array<Type> ty_args;
-  bool all_ty_args_unchanged = true;
   for (auto ty_arg : call_node->type_args) {
     auto new_ty_arg = this->VisitType(ty_arg);
     ty_args.push_back(new_ty_arg);
-    all_ty_args_unchanged &= new_ty_arg.same_as(ty_arg);
+    unchanged &= new_ty_arg.same_as(ty_arg);
   }
 
   tvm::Array<Expr> call_args;
-  bool all_args_unchanged = true;
   for (auto arg : call_node->args) {
     auto new_arg = this->Mutate(arg);
     call_args.push_back(new_arg);
-    all_args_unchanged &= new_arg.same_as(arg);
+    unchanged &= new_arg.same_as(arg);
   }
 
-  if (all_ty_args_unchanged && all_args_unchanged &&
-      call_node->op.same_as(op)) {
-    return e;
+  if (unchanged) {
+    return GetRef<Expr>(call_node);
   } else {
-    return CallNode::make(op, call_args, call_node->attrs, ty_args);
+    return CallNode::make(new_op, call_args, call_node->attrs, ty_args);
   }
 }
 
-Expr ExprMutator::VisitExpr_(const LetNode* op, const Expr& e) {
+Expr ExprMutator::VisitExpr_(const LetNode* op) {
   Var var = Downcast<Var>(this->Mutate(op->var));
   auto type = this->VisitType(op->value_type);
   auto value = this->Mutate(op->value);
   auto body = this->Mutate(op->body);
 
-  if (var.same_as(op->var) && type.same_as(op->value_type) &&
-      value.same_as(op->value) && body.same_as(op->body)) {
-    return e;
+  if (var.same_as(op->var) &&
+      type.same_as(op->value_type) &&
+      value.same_as(op->value) &&
+      body.same_as(op->body)) {
+    return GetRef<Expr>(op);
   } else {
     return LetNode::make(var, value, body, type);
   }
 }
 
-Expr ExprMutator::VisitExpr_(const IfNode* op, const Expr& e) {
+Expr ExprMutator::VisitExpr_(const IfNode* op) {
   auto guard = this->Mutate(op->cond);
   auto true_b = this->Mutate(op->true_branch);
   auto false_b = this->Mutate(op->false_branch);
-  if (op->cond == guard && true_b == op->true_branch &&
-      false_b == op->false_branch) {
-    return e;
+  if (op->cond.same_as(guard) &&
+      op->true_branch.same_as(true_b) &&
+      op->false_branch.same_as(false_b)) {
+    return GetRef<Expr>(op);;
   } else {
     return IfNode::make(guard, true_b, false_b);
   }
 }
 
-Type ExprMutator::VisitType(const Type& t) { return t; }
+Type ExprMutator::VisitType(const Type& t) {
+  return t;
+}
 
-void ExprVisitor::ExprVisitor::VisitExpr_(const VarNode* op) { return; }
+void ExprVisitor::ExprVisitor::VisitExpr_(const VarNode* op) {
+}
 
-void ExprVisitor::ExprVisitor::VisitExpr_(const GlobalVarNode* op) { return; }
+void ExprVisitor::ExprVisitor::VisitExpr_(const GlobalVarNode* op) {
+}
 
-void ExprVisitor::ExprVisitor::VisitExpr_(const ConstantNode* op) { return; }
+void ExprVisitor::ExprVisitor::VisitExpr_(const ConstantNode* op) {
+}
 
 void ExprVisitor::ExprVisitor::VisitExpr_(const TupleNode* op) {
   for (auto field : op->fields) {
@@ -202,4 +210,3 @@ void ExprVisitor::VisitType(const Type& t) { return; }
 
 }  // namespace relay
 }  // namespace tvm
-
