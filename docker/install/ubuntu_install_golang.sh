90c8dba36b9be5b1b044e0e41f9953594fdca0f4
SRK Reddy <sivar.b@huawei.com>
2018-09-23 09:03:51 +0530

    * Review commnets.

--- docker/install/ubuntu_install_golang.sh
+++ docker/install/ubuntu_install_golang.sh
@@ -1,4 +1,4 @@
 #install the necessary dependancies for golang build
-apt-get install -y golang-go
-apt-get install -y godoc
-apt-get install -y golint
+apt-get update && apt-get install -y golang-go
+apt-get update && apt-get install -y godoc
+apt-get update && apt-get install -y golint
