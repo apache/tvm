5ed52a5ff199daf6c5e37406cd2631bfadb33ddf
Siva <sivar.b@huawei.com>
2018-09-23 09:55:06 +0530

    [DOCKER] Golang CI recipe. (#1759)

---
+++ docker/install/ubuntu_install_golang.sh
@@ -0,0 +1,4 @@
+#install the necessary dependancies for golang build
+apt-get update && apt-get install -y golang-go
+apt-get update && apt-get install -y godoc
+apt-get update && apt-get install -y golint
