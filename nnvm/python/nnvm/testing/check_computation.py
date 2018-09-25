0dbf428d922ff6ad6a9de1e634f1b89c00f77208
Sergei Grechanik <grechanik.sergey@huawei.com>
2018-09-25 15:49:00 +0300

    Fix lint errors

--- nnvm/python/nnvm/testing/check_computation.py
+++ nnvm/python/nnvm/testing/check_computation.py
@@ -65,11 +65,11 @@ def infer_shapes_dtypes(graph, shape=None, dtype=None, fallback_dtype=None):
 
     if not isinstance(provided_shapes, dict):
         provided_shapes = {x: provided_shapes
-                                 for x in graph.symbol.list_input_variables()}
+                           for x in graph.symbol.list_input_variables()}
 
     if not isinstance(provided_dtypes, dict):
         provided_dtypes = {x: provided_dtypes
-                                 for x in graph.symbol.list_input_variables()}
+                           for x in graph.symbol.list_input_variables()}
 
     provided_shapes = _dict_var_to_dict_str(provided_shapes)
     provided_dtypes = _dict_var_to_dict_str(provided_dtypes)
