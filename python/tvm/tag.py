b14bb7f9f9758165d6726c46d03ceb35fafb1c53
Sergey Mironov <grrwlf@gmail.com>
2018-09-27 05:02:27 +0300

    [TOPI] Access topi::matmul from Python (#1744)

--- python/tvm/tag.py
+++ python/tvm/tag.py
@@ -1,25 +1,36 @@
 """Tag class for TVM operators."""
+import warnings
 from ._ffi.base import decorate
 
 class TagScope(object):
     """Tag scope object to set tag for operators, working as context
     manager and decorator both. See also tag_scope.
     """
-    current = None
+    _current = None
+
+    @classmethod
+    def get_current(cls):
+        if cls._current:
+            cls._current.accessed = True
+        return cls._current
+
     def __init__(self, tag):
         self._old_scope = None
         self.tag = tag
+        self.accessed = False
 
     def __enter__(self):
-        if TagScope.current is not None:
+        if TagScope._current is not None:
             raise ValueError("nested op_tag is not allowed for now")
-        self._old_scope = TagScope.current
-        TagScope.current = self
+        self._old_scope = TagScope._current
+        TagScope._current = self
         return self
 
     def __exit__(self, ptype, value, trace):
         assert self._old_scope is None
-        TagScope.current = self._old_scope
+        if not self.accessed:
+            warnings.warn("Tag '%s' declared via TagScope was not used." % (self.tag,))
+        TagScope._current = self._old_scope
 
     def __call__(self, fdecl):
         def tagged_fdecl(func, *args, **kwargs):
