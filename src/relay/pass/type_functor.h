ce4f521fd1518f6a487616501815825813957592
Marisa Kirisame <lolisa@marisa.moe>
2018-09-30 07:56:28 -0700

    fix

--- src/relay/pass/type_functor.h
+++ src/relay/pass/type_functor.h
@@ -21,11 +21,11 @@ class TypeFunctor;
 #define TYPE_FUNCTOR_DEFAULT \
   { return VisitTypeDefault_(op, std::forward<Args>(args)...); }
 
-#define RELAY_TYPE_FUNCTOR_DISPATCH(OP)                       \
-  vtable.template set_dispatch<OP>(                           \
-      [](const NodeRef& n, TSelf* self, Args... args) {       \
-        return self->VisitType_(static_cast<const OP*>(n.node_.get()),    \
-                                std::forward<Args>(args)...); \
+#define RELAY_TYPE_FUNCTOR_DISPATCH(OP)                                \
+  vtable.template set_dispatch<OP>(                                    \
+      [](const NodeRef& n, TSelf* self, Args... args) {                \
+        return self->VisitType_(static_cast<const OP*>(n.node_.get()), \
+                                std::forward<Args>(args)...);          \
       });
 
 template <typename R, typename... Args>
