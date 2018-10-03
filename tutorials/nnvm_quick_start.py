31bb62bc6af089eb98e1a78cc35a126c4b3e4260
Alexander Pivovarov <apivovarov@gmail.com>
2018-10-03 10:15:36 -0700

    Use ctx instead of tvm.gpu(0) in nnvm_quick_start tutorial (#1801)

--- tutorials/nnvm_quick_start.py
+++ tutorials/nnvm_quick_start.py
@@ -133,7 +133,7 @@ loaded_lib = tvm.module.load(path_lib)
 loaded_params = bytearray(open(temp.relpath("deploy_param.params"), "rb").read())
 input_data = tvm.nd.array(np.random.uniform(size=data_shape).astype("float32"))
 
-module = graph_runtime.create(loaded_json, loaded_lib, tvm.gpu(0))
+module = graph_runtime.create(loaded_json, loaded_lib, ctx)
 module.load_params(loaded_params)
 module.run(data=input_data)
 out = module.get_output(0).asnumpy()
