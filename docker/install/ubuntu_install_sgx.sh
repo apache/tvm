4ddadd6fa3094bea3c18e79aaaffcb68d5109387
Tianqi Chen <tqchen@users.noreply.github.com>
2018-10-04 22:54:31 -0700

    [DOCKER] Fix CI script (#1826)

--- docker/install/ubuntu_install_sgx.sh
+++ docker/install/ubuntu_install_sgx.sh
@@ -1,10 +1,8 @@
-apt-get update -qq && apt-get install -qq \
-    # for SGX SDK
+apt-get update && apt-get install -y --no-install-recommends --force-yes \
     build-essential git cmake \
     wget python pkg-config software-properties-common \
-    autoconf automake libtool ocaml ocamlbuild \
-    # for SGX PSW
-    libssl-dev libcurl4-openssl-dev
+    autoconf automake libtool ocaml \
+    libssl-dev libcurl4-openssl-dev curl
 
 git clone https://github.com/intel/linux-sgx.git
 cd linux-sgx
@@ -19,10 +17,3 @@ git clone https://github.com/baidu/rust-sgx-sdk.git /opt/rust-sgx-sdk
 cd /opt/rust-sgx-sdk
 git checkout bdd75ca05f66d1f5df637182ec335970f769b03a
 cd -
-
-curl -sSo rustup.sh 'https://sh.rustup.rs'
-# rustc nightly-2018-08-25 is the version supported by the above version of rust-sgx-sdk
-bash rustup.sh -y --no-modify-path --default-toolchain nightly-2018-08-25
-rustup component add rust-src
-cargo install rustfmt-nightly --force
-cargo install xargo
