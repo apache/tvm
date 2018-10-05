315a894a4a9628f9bccc8926885c53b9f81ace4d
Junru Shao <junrushao1994@gmail.com>
2018-10-04 21:37:15 -0400

    Put it into the root namespace

--- python/tvm/relay/op/__init__.py
+++ python/tvm/relay/op/__init__.py
@@ -6,7 +6,7 @@ from .op import get, register, Op
 # Operators
 from .tensor import *
 from . import nn
-from . import transform
+from .transform import *
 
 
 # operator registry
--- tests/python/relay/test_op_level1.py
+++ tests/python/relay/test_op_level1.py
@@ -8,7 +8,7 @@ def test_expand_dims_infer_type():
     # let's mimic a batch of sequences
     x = ib.param("x", relay.ty.TensorType((n, t, d), "float32"))
     with ib.function(x) as func:
-        ib.ret(relay.transform.expand_dims(x, axis=2))
+        ib.ret(relay.expand_dims(x, axis=2))
     ib.ret(func)
     func = relay.ir_pass.infer_type(ib.env, func.to_func())
     ftype = func.checked_type()
