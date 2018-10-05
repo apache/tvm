589f244581cb3cf6657d9cbdf7f7354604dc8374
Tianqi Chen <tqchen@users.noreply.github.com>
2018-10-05 13:50:22 -0700

    [OP] right_shift (#1832)

--- tests/python/relay/test_op_level4.py
+++ tests/python/relay/test_op_level4.py
@@ -20,5 +20,19 @@ def test_cmp_type():
         assert ftype.ret_type == relay.TensorType((5, 10, 4), "uint1")
 
 
+def test_binary_broadcast():
+    for op in [relay.right_shift]:
+        ib = relay.ir_builder.IRBuilder()
+        x = ib.param("x", relay.TensorType((10, 4), "int32"))
+        y = ib.param("y", relay.TensorType((5, 10, 1), "int32"))
+        with ib.function(x, y) as func:
+            ib.ret(op(x.var, y.var))
+        ib.ret(func)
+        func = relay.ir_pass.infer_type(ib.env, func.to_func())
+        ftype = func.checked_type()
+        assert ftype.ret_type == relay.TensorType((5, 10, 4), "int32")
+
+
 if __name__ == "__main__":
     test_cmp_type()
+    test_binary_broadcast()
