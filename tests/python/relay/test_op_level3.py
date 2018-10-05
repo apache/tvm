eadfbf4fccd49291715ec6ae039188654431f7f7
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-10-05 16:52:29 -0700

    add clip test

--- tests/python/relay/test_op_level3.py
+++ tests/python/relay/test_op_level3.py
@@ -11,3 +11,17 @@ def test_unary_identity():
         func = relay.ir_pass.infer_type(ib.env, func.to_func())
         ftype = func.checked_type()
         assert ftype.ret_type == relay.TensorType((8, 9, 4), "int32")
+
+def test_clip_type():
+    ib = relay.ir_builder.IRBuilder()
+    a = ib.param("a", relay.TensorType((10, 4), "float32"))
+    with ib.function(a) as func:
+        ib.ret(relay.clip(a.var, relay.Constant(tvm.nd.array(1.)), relay.Constant(tvm.nd.array(4.))))
+    ib.ret(func)
+    func = relay.ir_pass.infer_type(ib.env, func.to_func())
+    ftype = func.checked_type()
+    assert ftype.ret_type == relay.TensorType((10, 4), "float32")
+
+if __name__ == "__main__":
+    test_unary_identity()
+    test_clip_type()
