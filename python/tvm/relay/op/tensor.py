bb87f4d755c5f3be33fa0d9650b017bed6d315ce
Liang Luo <llmainland@live.cn>
2018-10-05 22:09:57 -0700

    maximum relay op V2 (#1838)

--- python/tvm/relay/op/tensor.py
+++ python/tvm/relay/op/tensor.py
@@ -228,6 +228,24 @@ def greater_equal(lhs, rhs):
     return _make.greater_equal(lhs, rhs)
 
 
+def maximum(lhs, rhs):
+    """Maximum with numpy-style broadcasting.
+
+    Parameters
+    ----------
+    lhs : relay.Expr
+        The left hand side input data
+    rhs : relay.Expr
+        The right hand side input data
+
+    Returns
+    -------
+    result : relay.Expr
+        The computed result.
+    """
+    return _make.maximum(lhs, rhs)
+
+
 def right_shift(lhs, rhs):
     """Right shift with numpy-style broadcasting.
 
