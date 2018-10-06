b313c021361e5d605ca3a1d0e5552a37e12c5e56
nhynes <nhynes@berkeley.edu>
2018-10-06 15:16:38 -0700

    [Rust] Update rust install in dockerfile (#1855)
    * Update rust docker
    
    * minor edit for consistency

--- docker/install/ubuntu_install_rust.sh
+++ docker/install/ubuntu_install_rust.sh
@@ -4,6 +4,7 @@ curl -sSo rustup.sh 'https://sh.rustup.rs'
 # rustc nightly-2018-08-25 is the version supported by the above version of rust-sgx-sdk
 bash rustup.sh -y --no-modify-path --default-toolchain nightly-2018-08-25
 . $HOME/.cargo/env
+rustup toolchain add nightly
 rustup component add rust-src
-cargo install rustfmt-nightly --force
-cargo install xargo
+cargo +nightly install rustfmt-nightly --version 0.99.5 --force
+cargo +nightly install xargo
