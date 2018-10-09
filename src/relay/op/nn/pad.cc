8c9a50925c8503dfab7628c16637be4e24cd70a6
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2018-10-09 14:18:30 -0700

    Use make_const to avoid casting to int

--- src/relay/op/nn/pad.cc
+++ src/relay/op/nn/pad.cc
@@ -50,10 +50,7 @@ bool PadRel(const Array<Type>& types,
       << "Param width elements should be positive but first pad width at "
       << "index " << i << " is " << *width2 << ".";
 
-    int padding = static_cast<int>(*width1 + *width2);
-    CHECK(padding >= 0)
-      << "Overflow when attempting padding";
-
+    auto padding = make_const(data->shape[i].type(), *width1 + *width2);
     oshape.push_back(data->shape[i] + padding);
   }
 
