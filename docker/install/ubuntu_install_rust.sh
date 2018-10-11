d82f96e4fbf1d9c639cf8f6a3bb3eaa20700b39b
Nick Hynes <nhynes@oasislabs.com>
2018-10-11 18:11:45 +0000

    Make /opt/rust world writable for real

--- docker/install/ubuntu_install_rust.sh
+++ docker/install/ubuntu_install_rust.sh
@@ -7,10 +7,9 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchai
 . $CARGO_HOME/env
 rustup toolchain add nightly
 rustup component add rust-src
+cargo +nightly install sccache
 cargo +nightly install rustfmt-nightly --version 0.99.5 --force
 cargo +nightly install xargo
-cargo +nightly install sccache
 
 # make rust usable by all users
-chmod a+w /opt/rust
-sudo find /opt/rust -type d -exec chmod a+w {} \;
+chmod -R a+w /opt/rust
