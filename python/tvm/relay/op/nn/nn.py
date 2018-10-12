da1cef8d4eade2017780caf7a5f7d174a7284bc9
Steven S. Lyubomirsky <slyubomirsky@gmail.com>
2018-10-11 21:57:51 -0700

    [Relay][Op] Pad operator (#1843)

--- python/tvm/relay/op/nn/nn.py
+++ python/tvm/relay/op/nn/nn.py
@@ -429,7 +429,6 @@ def batch_flatten(data):
     """
     return _make.batch_flatten(data)
 
-
 def relu(data):
     """Rectified linear unit.
 
@@ -447,6 +446,32 @@ def relu(data):
         The computed result.
     """
     return _make.relu(data)
+
+
+def pad(data,
+        pad_width,
+        pad_value=0.0):
+    r"""Padding
+
+    This operator takes in a tensor and pads each axis by the specified
+    widths using the specified value.
+
+    Parameters
+    ----------
+    data: relay.Expr
+        The input data to the operator
+    pad_width: tuple of <tuple of <int>>, required
+        Number of values padded to the edges of each axis, in the format
+        of ((before_1, after_1), ..., (before_N, after_N))
+    pad_value: float, optional, default=0.0
+        The value used for padding
+
+    Returns
+    -------
+    result : relay.Expr
+        The computed result.
+    """
+    return _make.pad(data, pad_width, pad_value)
 
 
 def lrn(data, size=5, axis=1, bias=2, alpha=.00001, beta=0.75):
@@ -484,8 +509,8 @@ def lrn(data, size=5, axis=1, bias=2, alpha=.00001, beta=0.75):
     result : relay.Expr
         The computed result.
     """
-
     return _make.lrn(data, size, axis, alpha, beta, bias)
+
 
 def l2_normalize(data, eps, axis=None):
     """Perform L2 normalization on the input data
--- tests/python/relay/test_op_level2.py
+++ tests/python/relay/test_op_level2.py
@@ -196,10 +196,35 @@ def test_flatten_infer_type():
     ftype = func.checked_type
     assert ftype.ret_type == relay.ty.TensorType((d1, ((2*d3)*3)), "float32")
 
+def test_pad_infer_type():
+    # entirely concrete case
+    ib = relay.ir_builder.IRBuilder()
+    n, c, h, w = 1, 2, 3, 4
+    t = ib.param("t", relay.TensorType((n, c, h, w), "float32"))
+    with ib.function(t) as func:
+        ib.ret(relay.nn.pad(t.var, ((1, 1), (2, 2), (3, 3), (4, 4))))
+    ib.ret(func)
+    func = relay.ir_pass.infer_type(ib.env, func.to_func())
+    ftype = func.checked_type
+    assert ftype.ret_type == relay.TensorType((3, 6, 9, 12), "float32")
+
+    # some symbolic values
+    ib = relay.ir_builder.IRBuilder()
+    n, c, h, w = tvm.var("n"), 2, 3, tvm.var("w")
+    t = ib.param("t", relay.TensorType((n, c, h, w), "float32"))
+    with ib.function(t) as func:
+        ib.ret(relay.nn.pad(t.var, ((1, 1), (2, 2), (3, 3), (4, 4))))
+    ib.ret(func)
+    func = relay.ir_pass.infer_type(ib.env, func.to_func())
+    ftype = func.checked_type
+    assert ftype.ret_type == relay.TensorType((n + 2, 6, 9, w + 8), "float32")
+
 
 if __name__ == "__main__":
     test_conv2d_infer_type()
     test_pool2d_infer_type()
     test_upsampling_infer_type()
     test_flatten_infer_type()
+    test_pad_infer_type()
     test_conv2d_transpose_infer_type()
+
