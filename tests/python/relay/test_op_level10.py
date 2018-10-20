72f6f135695d210d3d20c5966ad247af9b55aece
Marisa Kirisame <lolisa@marisa.moe>
2018-10-20 00:05:19 -0700

    fix

--- tests/python/relay/test_op_level10.py
+++ tests/python/relay/test_op_level10.py
@@ -4,27 +4,19 @@ import tvm
 from tvm import relay
 
 def test_collapse_sum_like():
-    ib = relay.ir_builder.IRBuilder()
-    x = ib.param("x", relay.ty.TensorType((3, 4, 5, 6), "int8"))
-    y = ib.param("y", relay.ty.TensorType((4, 1, 6), "int8"))
-    with ib.function(x, y) as func:
-        ib.ret(relay.collapse_sum_like(x, y))
-    ib.ret(func)
-    func = relay.ir_pass.infer_type(ib.env, func.to_func())
-    ftype = func.checked_type
-    assert ftype.ret_type == relay.ty.TensorType((4, 1, 6), "int8")
+    x = relay.Var("x", relay.ty.TensorType((3, 4, 5, 6), "int8"))
+    y = relay.Var("y", relay.ty.TensorType((4, 1, 6), "int8"))
+    z = relay.collapse_sum_like(x, y)
+    zz = relay.ir_pass.infer_type(z)
+    assert zz.checked_type == relay.ty.TensorType((4, 1, 6), "int8")
 
 
 def test_broadcast_to_like():
-    ib = relay.ir_builder.IRBuilder()
-    x = ib.param("x", relay.ty.TensorType((3, 4, 5, 6), "int8"))
-    y = ib.param("x", relay.ty.TensorType((4, 1, 6), "int8"))
-    with ib.function(x, y) as func:
-        ib.ret(relay.broadcast_to_like(y, x))
-    ib.ret(func)
-    func = relay.ir_pass.infer_type(ib.env, func.to_func())
-    ftype = func.checked_type
-    assert ftype.ret_type == relay.ty.TensorType((3, 4, 5, 6), "int8")
+    x = relay.Var("x", relay.ty.TensorType((3, 4, 5, 6), "int8"))
+    y = relay.Var("y", relay.ty.TensorType((4, 1, 6), "int8"))
+    z = relay.broadcast_to_like(y, x)
+    zz = relay.ir_pass.infer_type(z)
+    assert zz.checked_type == relay.ty.TensorType((3, 4, 5, 6), "int8")
 
 if __name__ == "__main__":
     test_collapse_sum_like()
