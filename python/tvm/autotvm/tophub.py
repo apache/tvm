13f8d2f2a6b91564936edff494eb9946e88f2985
Yao Wang <kevinthesunwy@gmail.com>
2018-10-22 21:55:16 -0700

    Add tophub for x86 (#1955)

--- python/tvm/autotvm/tophub.py
+++ python/tvm/autotvm/tophub.py
@@ -21,6 +21,7 @@ AUTOTVM_TOPHUB_ROOT_PATH = os.path.join(os.path.expanduser('~'), ".tvm", "tophub
 # the version of each package
 PACKAGE_VERSION = {
     'arm_cpu': "v0.03",
+    'llvm':    "v0.01",
 
     'cuda':    "v0.03",
     'rocm':    "v0.01",
