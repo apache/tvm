2d5cfe20b5d8c98c204f2b238ab971c4ceb2c0d7
Wuwei Lin <vincentl13x@gmail.com>
2018-10-24 19:11:18 +0800

    Update mali conv2d with dilation

--- topi/python/topi/mali/conv2d.py
+++ topi/python/topi/mali/conv2d.py
@@ -16,7 +16,7 @@ from ..arm_cpu.conv2d import _decl_spatial_pack, _alter_conv2d_layout_arm
 
 
 @autotvm.register_topi_compute(conv2d, 'mali', ['direct'])
-def conv2d_mali(cfg, data, kernel, strides, padding, layout, out_dtype):
+def conv2d_mali(cfg, data, kernel, strides, padding, dilation, layout, out_dtype):
     """TOPI compute callback for conv2d
 
     Parameters
@@ -38,6 +38,9 @@ def conv2d_mali(cfg, data, kernel, strides, padding, layout, out_dtype):
     padding : list of two ints
         [pad_height, pad_width]
 
+    dilation : list of two ints
+        [dilation_height, dilation_width]
+
     layout : str
         layout of data
 
@@ -49,7 +52,7 @@ def conv2d_mali(cfg, data, kernel, strides, padding, layout, out_dtype):
     output : tvm.Tensor
         4-D with shape [batch, out_channel, out_height, out_width]
     """
-    return _decl_spatial_pack(cfg, data, kernel, strides, padding, layout, out_dtype, num_tile=3)
+    return _decl_spatial_pack(cfg, data, kernel, strides, padding, dilation, layout, out_dtype, num_tile=3)
 
 @autotvm.register_topi_schedule(schedule_conv2d_nchw, 'mali', ['direct', 'winograd'])
 def schedule_conv2d_nchw_mali(cfg, outs):
@@ -175,16 +178,23 @@ def _pick_tile_size(data, kernel):
         return 2
 
 @autotvm.register_topi_compute(conv2d, 'mali', ['winograd'])
-def conv2d_mali_winograd(cfg, data, kernel, strides, padding, layout, out_dtype):
+def conv2d_mali_winograd(cfg, data, kernel, strides, padding, dilation, layout, out_dtype):
     tile_size = _pick_tile_size(data, kernel)
-    return _decl_winograd(cfg, data, kernel, strides, padding, layout, out_dtype, tile_size)
+    return _decl_winograd(cfg, data, kernel, strides, padding, dilation, layout, out_dtype, tile_size)
 
-def _decl_winograd(cfg, data, kernel, strides, padding, layout, out_dtype, tile_size):
+def _decl_winograd(cfg, data, kernel, strides, padding, dilation, layout, out_dtype, tile_size):
     N, CI, IH, IW = get_const_tuple(data.shape)
     if len(kernel.shape) == 4:
+        if isinstance(dilation, int):
+            dilation_h, dilation_w = dilation
+        else:
+            dilation_h, dilation_w = dilation
+        if dilation_h != 1 or dilation_w != 1:
+            kernel = dilate(kernel, (1, 1, dilation_h, dilation_w))
         pre_computed = False
         CO, _, KH, KW = get_const_tuple(kernel.shape)
     else:
+        # dilation is not supported
         pre_computed = True
         H_CAT, W_CAT, CO, CI, VC = get_const_tuple(kernel.shape)
         CO *= VC
@@ -425,10 +435,25 @@ def _schedule_winograd(cfg, s, op):
     s[Y].compute_at(s[output], tt)
 
 ##### REGISTER TOPI COMPUTE / SCHEDULE FOR WINOGRAD WITH WEIGHT TRANSFORM #####
+<<<<<<< HEAD
 @autotvm.register_topi_compute(conv2d_winograd_without_weight_transform, 'mali', ['winograd'])
 def conv2d_winograd_ww(cfg, data, kernel, strides, padding, layout, out_dtype, tile_size):
     """TOPI compute callback"""
-    return _decl_winograd(cfg, data, kernel, strides, padding, layout, out_dtype, tile_size)
+    return _decl_winograd(cfg, data, kernel, strides, padding, dilation, layout, out_dtype, 
+                          tile_size)
+=======
+@conv2d_winograd_without_weight_transform.register(['mali'])
+@autotvm.task.dispatcher
+def winograd_ww_config_dispatcher_(data, kernel, strides, padding, dilation, layout, out_dtype, tile_size):
+    return _winograd_conv_arg_to_workload(data, kernel, strides, padding, dilatiion, layout, out_dtype,
+                                          tile_size)
+
+
+@winograd_ww_config_dispatcher_.register(['winograd'])
+def decl_winograd_ww(cfg, data, kernel, strides, padding, dilation, layout, out_dtype, tile_size):
+    return _decl_winograd(cfg, data, kernel, strides, padding, dilation, layout, out_dtype,
+                          tile_size)
+>>>>>>> Update mali conv2d with dilation
 
 
 @autotvm.register_topi_schedule(schedule_conv2d_winograd_without_weight_transform,
