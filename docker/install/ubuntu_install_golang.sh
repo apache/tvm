89601c6b2ce239bc10dfadd6998bb0c01450e8a2
Siva <sivar.b@huawei.com>
2018-10-28 22:13:06 +0530

    [DOCKER][GOLANG] fix golang version. (#2023)

--- docker/install/ubuntu_install_golang.sh
+++ docker/install/ubuntu_install_golang.sh
@@ -1,4 +1,4 @@
 #install the necessary dependancies for golang build
-apt-get update && apt-get install -y golang-0.10-go
-apt-get update && apt-get install -y godoc
+apt-get update && apt-get install -y golang-1.10-go
+apt-get update && apt-get install -y golang-1.10-doc
 apt-get update && apt-get install -y golint
