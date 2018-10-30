6c4aa8190b24a5afdac611e4f07f7a9049292fe1
Tianqi Chen <tqchen@users.noreply.github.com>
2018-10-30 09:53:30 -0700

    [RELAY][OP]  Maketuple to be resolved when containing incompleteType (#2031)

--- tests/python/relay/test_op_level1.py
+++ tests/python/relay/test_op_level1.py
@@ -87,6 +87,7 @@ def test_concatenate_infer_type():
     zz = relay.ir_pass.infer_type(z)
     assert zz.checked_type == relay.TensorType((n, t, 200))
 
+    x = relay.exp(x)
     z = relay.concatenate((x, y), axis=2)
     zz = relay.ir_pass.infer_type(z)
     assert zz.checked_type == relay.TensorType((n, t, 200))
