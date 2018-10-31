b690b4c5aa3a07a5a9e51024fd73dd004a17fd86
Liangfu Chen <liangfu.chen@icloud.com>
2018-10-31 11:18:20 +0800

    bug fix in printing RuntimeError and add an additional search path

--- vta/python/vta/libinfo.py
+++ vta/python/vta/libinfo.py
@@ -15,11 +15,13 @@ def find_libvta(optional=False):
     """Find VTA library"""
     curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
     lib_search = [curr_path]
+    lib_search += [os.path.join(curr_path, "..", "..", "..", "lib",)]
     lib_search += [os.path.join(curr_path, "..", "..", "..", "build",)]
     lib_search += [os.path.join(curr_path, "..", "..", "..", "build", "Release")]
     lib_name = _get_lib_name()
     lib_path = [os.path.join(x, lib_name) for x in lib_search]
     lib_found = [x for x in lib_path if os.path.exists(x)]
     if not lib_found and not optional:
-        raise RuntimeError("Cannot find libvta: candidates are: " % str(lib_path))
+        raise RuntimeError('Cannot find the files.\n' +
+                           'List of candidates:\n' + str('\n'.join(lib_path)))
     return lib_found
