3fd20b10f615821bc863d90f7577eefded3b2780
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-11-01 17:24:18 -0700

    linting

--- python/tvm/relay/parser.py
+++ python/tvm/relay/parser.py
@@ -1,13 +1,16 @@
 from __future__ import absolute_import
 
 def enabled():
+    """Is the parser enabled/Can we import the parser?"""
     try:
+        # pylint: disable=unused-variable
         import tvm.relay._parser
         return True
+    # pylint: disable=broad-except
     except Exception:
         return False
 
 def fromtext(data):
     """Parse a Relay program."""
-    from tvm.relay import _parser    
+    from tvm.relay import _parser
     return _parser.fromtext(data)
