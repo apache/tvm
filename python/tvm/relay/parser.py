59411dc657992438f4f36d24ae52945c73f0914e
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-11-02 12:46:07 -0700

    simplify failing test. rework import

--- python/tvm/relay/parser.py
+++ python/tvm/relay/parser.py
@@ -5,7 +5,7 @@ def enabled():
     """Is the parser enabled/Can we import the parser?"""
     try:
         # pylint: disable=unused-variable
-        import tvm.relay._parser
+        from tvm.relay import _parser
         return True
     # pylint: disable=broad-except
     except Exception:
