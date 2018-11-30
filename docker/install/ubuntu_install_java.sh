f837861190d72087fa02d04d8b005a0208f0b881
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-11-29 20:59:53 -0800

    source /etc/profile to add JAVA_HOME

--- docker/install/ubuntu_install_java.sh
+++ docker/install/ubuntu_install_java.sh
@@ -2,3 +2,5 @@ set -o errexit -o nounset
 apt-get update && apt-get install -y openjdk-8-jdk maven
 test -d "/usr/lib/jvm/java-8-openjdk-amd64/jre"
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre" >> /etc/profile
+
+source /etc/profile
