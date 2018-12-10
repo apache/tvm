59eabc72a569f1ca234af1451db95a268db55d1f
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2018-12-09 19:02:07 -0800

    Fix error in type inference cpp test

--- tests/cpp/relay_pass_type_infer_test.cc
+++ tests/cpp/relay_pass_type_infer_test.cc
@@ -15,11 +15,11 @@ TEST(Relay, SelfReference) {
   auto y = relay::VarNode::make("y", type_a);
   auto call = relay::CallNode::make(f, Array<relay::Expr>{ y });
   auto fx = relay::FunctionNode::make(tvm::Array<relay::Var>{ y }, call, type_b,
-                                      Array<relay::TypeVar>{type_a, type_b});
+                                      tvm::Array<relay::TypeVar>{type_a, type_b});
   auto type_fx = relay::InferType(fx, relay::ModuleNode::make(Map<relay::GlobalVar, relay::Function>{}));
 
   auto expected = relay::FuncTypeNode::make(tvm::Array<relay::Type>{ type_a }, type_a,
-                                            relay::Array<relay::TypeVar>{type_a} , {});
+                                            tvm::Array<relay::TypeVar>{type_a} , {});
   CHECK_EQ(type_fx->checked_type(), expected);
 }
 
