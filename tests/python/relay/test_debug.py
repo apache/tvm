694d46de51275c001682a81065ef7ed7cfc0890c
Jared Roesch <roeschinc@gmail.com>
2018-12-12 10:42:22 -0800

    Add test for debugging code

--- tests/python/relay/test_debug.py
+++ tests/python/relay/test_debug.py
@@ -2,13 +2,16 @@ from tvm.relay import var, const, create_executor
 from tvm.relay.op import debug
 
 
+_test_debug_hit = False
+
 def test_debug():
+    global _test_debug_hit
     exec = create_executor()
     x = var('x', shape=(), dtype='int32')
-    hit = False
+    _test_debug_hit = False
     def did_exec(x):
-        global hit
-        hit = True
+        global _test_debug_hit
+        _test_debug_hit = True
     prog = debug(x, debug_func=did_exec)
     exec.evaluate(prog, { x: const(1) })
-    assert hit
+    assert _test_debug_hit
