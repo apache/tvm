a39f01d51a2f8dd321bec700979d992d6253ec3a
Joshua Z. Zhang <cheungchih@gmail.com>
2018-12-18 14:06:09 -0800

    fix pylint

--- python/tvm/relay/frontend/onnx.py
+++ python/tvm/relay/frontend/onnx.py
@@ -355,8 +355,10 @@ class Selu(OnnxOpConverter):
     def _impl_v1(cls, inputs, attr, params):
         alpha = float(attr.get('alpha', 1.6732))
         gamma = float(attr.get('gamma', 1.0507))
-        return _expr.const(gamma) * (_expr.const(-alpha) * relay.nn.relu(_expr.const(1) -
-            relay.exp(inputs[0])) + relay.nn.relu(inputs[0]))
+        return return _expr.const(gamma) * (_expr.const(-alpha)
+                                            * relay.nn.relu(_expr.const(1)
+                                            - relay.exp(inputs[0]))
+                                            + relay.nn.relu(inputs[0]))
 
 
 class ScaledTanh(OnnxOpConverter):
