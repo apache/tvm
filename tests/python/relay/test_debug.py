160ad3d5bba7f7da3cb5bee2f1259c0c50ad6c7f
Jared Roesch <roeschinc@gmail.com>
2018-12-21 20:55:28 -0800

    Fix 2.7 failure

--- tests/python/relay/test_debug.py
+++ tests/python/relay/test_debug.py
@@ -6,27 +6,27 @@ _test_debug_hit = False
 
 def test_debug():
     global _test_debug_hit
-    exec = create_executor()
+    ex = create_executor()
     x = var('x', shape=(), dtype='int32')
     _test_debug_hit = False
     def did_exec(x):
         global _test_debug_hit
         _test_debug_hit = True
     prog = debug(x, debug_func=did_exec)
-    result = exec.evaluate(prog, { x: const(1) })
+    result = ex.evaluate(prog, { x: const(1) })
     assert _test_debug_hit
     assert result.asnumpy() == 1
 
 def test_debug_with_expr():
     global _test_debug_hit
     _test_debug_hit = False
-    exec = create_executor()
+    ex = create_executor()
     x = var('x', shape=(), dtype='int32')
     _test_debug_hit = False
     def did_exec(x):
         global _test_debug_hit
         _test_debug_hit = True
     prog = debug(x + x * x, debug_func=did_exec)
-    result = exec.evaluate(prog, { x: const(2) })
+    result = ex.evaluate(prog, { x: const(2) })
     assert _test_debug_hit
     assert result.asnumpy() == 6
