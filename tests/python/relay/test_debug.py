6dc19418dfd997d5d7f0960a8e1ba8fc6ccdf1f5
Jared Roesch <roeschinc@gmail.com>
2018-12-21 20:52:27 -0800

    Add second test

--- tests/python/relay/test_debug.py
+++ tests/python/relay/test_debug.py
@@ -16,3 +16,17 @@ def test_debug():
     result = exec.evaluate(prog, { x: const(1) })
     assert _test_debug_hit
     assert result.asnumpy() == 1
+
+def test_debug_with_expr():
+    global _test_debug_hit
+    _test_debug_hit = False
+    exec = create_executor()
+    x = var('x', shape=(), dtype='int32')
+    _test_debug_hit = False
+    def did_exec(x):
+        global _test_debug_hit
+        _test_debug_hit = True
+    prog = debug(x + x * x, debug_func=did_exec)
+    result = exec.evaluate(prog, { x: const(2) })
+    assert _test_debug_hit
+    assert result.asnumpy() == 6
