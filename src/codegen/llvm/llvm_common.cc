455b52f34a452058967b3bb216661f240d392fbe
Li-Wen Chang <30609447+liwchang@users.noreply.github.com>
2019-01-08 17:34:48 -0800

    Fix LLVM initialization again (#2399)

--- src/codegen/llvm/llvm_common.cc
+++ src/codegen/llvm/llvm_common.cc
@@ -5,6 +5,7 @@
 #ifdef TVM_LLVM_VERSION
 
 #include <tvm/base.h>
+#include <atomic>
 #include <mutex>
 #include "llvm_common.h"
 
@@ -13,7 +14,7 @@ namespace codegen {
 
 struct LLVMEnv {
   std::mutex mu;
-  volatile bool all_initialized{false};
+  std::atomic<bool> all_initialized{false};
 
   static LLVMEnv* Global() {
     static LLVMEnv inst;
@@ -23,15 +24,15 @@ struct LLVMEnv {
 
 void InitializeLLVM() {
   LLVMEnv* e = LLVMEnv::Global();
-  if (!e->all_initialized) {
+  if (!e->all_initialized.load(std::memory_order::memory_order_acquire)) {
     std::lock_guard<std::mutex> lock(e->mu);
-    if (!e->all_initialized) {
+    if (!e->all_initialized.load(std::memory_order::memory_order_acquire)) {
       llvm::InitializeAllTargetInfos();
       llvm::InitializeAllTargets();
       llvm::InitializeAllTargetMCs();
       llvm::InitializeAllAsmParsers();
       llvm::InitializeAllAsmPrinters();
-      e->all_initialized = true;
+      e->all_initialized.store(true, std::memory_order::memory_order_release);
     }
   }
 }
