8b428905a372a592e85a242380ae01bcce8a84e9
Masahiro Masuda <masahi129@gmail.com>
2019-01-15 23:28:24 +0900

    fix handling a tuple node in op fusion

--- src/relay/pass/fuse_ops.cc
+++ src/relay/pass/fuse_ops.cc
@@ -740,8 +740,9 @@ class FuseMutator : private ExprMutator {
     Array<Expr> new_fields = GetNewArguments(tuple->fields, ret_group);
     Tuple new_tuple = TupleNode::make(new_fields);
     if (ret_group == gmap_.at(tuple)) {
-      bool isolated = true;
-      for (size_t i = 0; i < new_fields.size(); ++i) {
+      // This tuple is the root of its group. Check if all fields come from other groups.
+      bool isolated = new_fields.size() == ginfo_[ret_group].params.size();
+      for (size_t i = 0; i < new_fields.size() && isolated; ++i) {
         isolated &= (new_fields[i].same_as(ginfo_[ret_group].params[i]));
       }
       if (isolated) {
