3b337bb00230f21b0d08690c4bafb8d0b7270f28
reminisce <wujun.nju@gmail.com>
2019-01-19 10:05:25 -0800

    Fix broadcast add and subtract grad (#2465)

--- tests/python/relay/test_ad.py
+++ tests/python/relay/test_ad.py
@@ -69,8 +69,64 @@ def test_sub():
     np.testing.assert_allclose(grad.asnumpy(), np.zeros_like(x.asnumpy()))
 
 
+def test_broadcast_add():
+    shape1 = (3, 4, 1)
+    shape2 = (1, 5)
+    dtype = 'float32'
+    x_nd = rand(dtype, *shape1)
+    y_nd = rand(dtype, *shape2)
+    x_np = x_nd.asnumpy()
+    y_np = y_nd.asnumpy()
+    expected_forward = x_np + y_np
+    t1 = relay.TensorType(shape1, dtype)
+    t2 = relay.TensorType(shape2, dtype)
+    x = relay.var("x", t1)
+    y = relay.var("y", t2)
+    func = relay.Function([x, y], x + y)
+    full_func = relay.ir_pass.infer_type(gradient(func))
+    assert full_func.checked_type == relay.FuncType([t1, t2],
+                                                    relay.TupleType([relay.TensorType(expected_forward.shape, dtype),
+                                                                     relay.TupleType([t1, t2])]))
+    ex = create_executor()
+    forward, (grad_x, grad_y) = ex.evaluate(full_func)(x_nd, y_nd)
+    np.testing.assert_allclose(forward.asnumpy(), expected_forward)
+    np.testing.assert_allclose(grad_x.asnumpy(),
+                               np.ones_like(expected_forward).sum(axis=2, keepdims=True))
+    np.testing.assert_allclose(grad_y.asnumpy(),
+                               np.ones_like(expected_forward).sum(axis=(0, 1), keepdims=True).squeeze(axis=0))
+
+
+def test_broadcast_subtract():
+    shape1 = (3, 4, 1)
+    shape2 = (1, 5)
+    dtype = 'float32'
+    x_nd = rand(dtype, *shape1)
+    y_nd = rand(dtype, *shape2)
+    x_np = x_nd.asnumpy()
+    y_np = y_nd.asnumpy()
+    expected_forward = x_np - y_np
+    t1 = relay.TensorType(shape1, dtype)
+    t2 = relay.TensorType(shape2, dtype)
+    x = relay.var("x", t1)
+    y = relay.var("y", t2)
+    func = relay.Function([x, y], x - y)
+    full_func = relay.ir_pass.infer_type(gradient(func))
+    assert full_func.checked_type == relay.FuncType([t1, t2],
+                                                    relay.TupleType([relay.TensorType(expected_forward.shape, dtype),
+                                                                     relay.TupleType([t1, t2])]))
+    ex = create_executor()
+    forward, (grad_x, grad_y) = ex.evaluate(full_func)(x_nd, y_nd)
+    np.testing.assert_allclose(forward.asnumpy(), expected_forward)
+    np.testing.assert_allclose(grad_x.asnumpy(),
+                               np.ones_like(expected_forward).sum(axis=2, keepdims=True))
+    np.testing.assert_allclose(grad_y.asnumpy(),
+                               -np.ones_like(expected_forward).sum(axis=(0, 1), keepdims=True).squeeze(axis=0))
+
+
 if __name__ == "__main__":
     test_id()
     test_add()
     test_temp_add()
     test_sub()
+    test_broadcast_add()
+    test_broadcast_subtract()
