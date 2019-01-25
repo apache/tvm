4b4172ae0f6e1c030232c4b7ced2bfe5366a1380
makihiro <makihiro@gmail.com>
2019-01-25 15:02:21 +0900

    [Relay][Frontend] Add Caffe2 Support

---
+++ tests/python/frontend/caffe2/model_zoo/__init__.py
@@ -0,0 +1,23 @@
+"""Store for caffe2 examples and common models."""
+from __future__ import absolute_import as _abs
+import os
+import importlib
+from . import squeezenet
+
+models = [
+    'squeezenet',
+    'resnet50',
+    'vgg19',
+]
+
+# skip download if model exist
+for model in models:
+    try:
+        locals()['c2_' + model] = importlib.import_module('caffe2.python.models.' + model)
+    except ImportError:
+        os.system("python -m caffe2.python.models.download -i -f " + model)
+        locals()['c2_' + model] = importlib.import_module('caffe2.python.models.' + model)
+
+# squeezenet
+def relay_squeezenet():
+    return squeezenet.get_workload()
