afa7aecd6fd2898ac54e6108857e19f3d6e33bec
Yizhi Liu <yizhiliu@amazon.com>
2019-01-24 18:55:53 -0800

    remove triggering nnvm coreml test

--- tests/scripts/task_python_frontend.sh
+++ tests/scripts/task_python_frontend.sh
@@ -27,9 +27,6 @@ python3 -m nose -v nnvm/tests/python/frontend/keras || exit -1
 echo "Running nnvm Tensorflow frontend test..."
 python3 -m nose -v nnvm/tests/python/frontend/tensorflow || exit -1
 
-echo "Running nnvm CoreML frontend test..."
-python3 -m nose -v nnvm/tests/python/frontend/coreml || exit -1
-
 echo "Running relay MXNet frontend test..."
 python3 -m nose -v tests/python/frontend/mxnet || exit -1
 
