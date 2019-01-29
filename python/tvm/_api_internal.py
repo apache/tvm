174c92a83c53211e9e856ed09e83b5673dc9eeac
lee <cy-l@live.com>
2019-01-29 13:12:30 +0800

    [DOC]Update doc in _api_internal.py and ir_pass.py (#2514)

--- python/tvm/_api_internal.py
+++ python/tvm/_api_internal.py
@@ -1,1 +1,9 @@
-"""namespace of internal API"""
+"""Namespace of internal API
+
+The functions in this namespace are automatically exported from C++ side via PackedFunc
+that is registered by "TVM_REGISTER_*" macro. This way makes calling Python functions from C++
+side very easily.
+
+Each string starts with "_" in the "TVM_REGISTER_*" macro is an internal API. You can find
+all the functions in "api_lang.cc", "api_base.cc", "api_arith.cc" and "api_ir.cc" under "src/api".
+"""
