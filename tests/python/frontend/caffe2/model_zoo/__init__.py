4cb77b4900b197022b5b0bc6350fb8431a074ccb
makihiro <makihiro@gmail.com>
2019-01-30 11:14:32 +0900

    [Relay][Frontend] Add Caffe2 Support (fix caffe2 model import)

--- tests/python/frontend/caffe2/model_zoo/__init__.py
+++ tests/python/frontend/caffe2/model_zoo/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 import importlib
 from . import squeezenet
+from caffe2.python.models.download import ModelDownloader
 
 models = [
     'squeezenet',
@@ -11,33 +12,12 @@ models = [
     'vgg19',
 ]
 
-base_url = "https://s3.amazonaws.com/download.caffe2.ai/models"
-# save the model data temporary for the test
-model_base_dir = "/tmp"
+mf = ModelDownloader()
 
-def _download(model, overwrite=False):
-    model_dir = '{folder}/{m}'.format(folder=model_base_dir, m=model)
-    if not os.path.isdir(model_dir):
-        os.makedirs(model_dir)
+class Model:
+    def __init__(self, model_name):
+        self.init_net, self.predict_net, self.value_info = mf.get_c2_model(model_name)
 
-    for filename in ['predict_net.pb', 'init_net.pb']:
-        if os.path.isfile('{folder}/{f}'.format(folder=model_dir, f=filename)) and not overwrite:
-            return model_dir
-        try:
-            import urllib.request
-            urllib.request.urlretrieve('{url}/{m}/{f}'.format(url=base_url, m=model, f=filename),
-                                       '{folder}/{f}'.format(folder=model_dir, f=filename))
-        except Exception:
-            import urllib
-            urllib.urlretrieve('{url}/{m}/{f}'.format(url=base_url, m=model, f=filename),
-                               '{folder}/{f}'.format(folder=model_dir, f=filename))
-
-    os.symlink("{folder}/__sym_init__.py".format(folder=os.path.abspath(os.path.dirname(__file__))),
-               "{folder}/__init__.py".format(folder=model_dir))
-    return model_dir
-
-
-# skip download if model exist
 for model in models:
     try:
         locals()['c2_' + model] = importlib.import_module('caffe2.python.models.' + model)
@@ -46,10 +26,7 @@ for model in models:
             os.system("python3 -m caffe2.python.models.download -i -f " + model)
             locals()['c2_' + model] = importlib.import_module('caffe2.python.models.' + model)
         except (PermissionError, ModuleNotFoundError):
-            _download(model)
-            if model_base_dir not in sys.path:
-                sys.path.append(model_base_dir)
-            locals()['c2_' + model] = importlib.import_module(model)
+            locals()['c2_' + model] = Model(model)
 
 # squeezenet
 def relay_squeezenet():
