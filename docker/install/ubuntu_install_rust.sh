b6ae3e15ccc5949982b2b8e13737ebd28f2277f1
Nick Hynes <nhynes@berkeley.edu>
2019-01-31 16:54:44 -0800

    Update rustc (#2524)

--- docker/install/ubuntu_install_rust.sh
+++ docker/install/ubuntu_install_rust.sh
@@ -3,12 +3,12 @@ apt-get update && apt-get install -y --no-install-recommends --force-yes curl
 export RUSTUP_HOME=/opt/rust
 export CARGO_HOME=/opt/rust
 # this rustc is one supported by the installed version of rust-sgx-sdk
-curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchain nightly-2018-10-01
+curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchain nightly-2019-01-28
 . $CARGO_HOME/env
 rustup toolchain add nightly
 rustup component add rust-src
 cargo +nightly install sccache
-cargo +nightly install rustfmt-nightly --version 0.99.5 --force
+cargo +nightly install rustfmt-nightly --version 1.0.1 --force
 cargo +nightly install xargo
 
 # make rust usable by all users
