40f76825473992212a520f3dd38d6444f7daaf01
Haichen Shen <shenhaichen@gmail.com>
2019-02-04 15:00:22 -0800

    [Relay][OP] Add reverse_reshape (#2503)
    * Enable reverse in reshape
    
    * Fix lint and typo
    
    * Put reverse_reshape into a separate op
    
    * Fix pylint

--- tests/python/relay/test_op_level10.py
+++ tests/python/relay/test_op_level10.py
@@ -121,8 +121,31 @@ def test_slice_like():
                       axes=(2, 3),
                       output=(1, 3, 112, 112))
 
+def test_reverse_reshape():
+    def verify_reverse_reshape(shape, newshape, oshape):
+        x = relay.var("x", relay.TensorType(shape, "float32"))
+        z = relay.reverse_reshape(x, newshape=newshape)
+        zz = relay.ir_pass.infer_type(z)
+        print(zz.checked_type)
+        assert "newshape=" in z.astext()
+        assert zz.checked_type == relay.ty.TensorType(oshape, "float32")
+
+        func = relay.Function([x], z)
+        x_data = np.random.uniform(low=-1, high=1, size=shape).astype("float32")
+        ref_res = np.reshape(x_data, oshape)
+        for target, ctx in ctx_list():
+            for kind in ["graph", "debug"]:
+                intrp = relay.create_executor(kind, ctx=ctx, target=target)
+                op_res = intrp.evaluate(func)(x_data)
+                tvm.testing.assert_allclose(op_res.asnumpy(), ref_res, rtol=1e-5)
+    verify_reverse_reshape((2, 3, 4), (4, 0, 2), (4, 3, 2))
+    verify_reverse_reshape((2, 3, 4), (2, 0, 0), (2, 3, 4))
+    verify_reverse_reshape((2, 3, 4), (0, -1), (3, 8))
+    verify_reverse_reshape((2, 3, 4), (-1, 0), (6, 4))
+    verify_reverse_reshape((2, 3, 4), (0, -3), (2, 12))
 
 if __name__ == "__main__":
     test_collapse_sum_like()
     test_broadcast_to_like()
     test_slice_like()
+    test_reverse_reshape()
