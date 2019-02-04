1552c6abad2a51863d55c51a9b983337566eedef
Sergei Grechanik <grechanik.sergey@huawei.com>
2019-02-04 12:19:39 +0300

    Fix the fix: skip loops that are related to reduction AND are unrelated to axis

--- tests/python/unittest/test_schedule_schedule_ops.py
+++ tests/python/unittest/test_schedule_schedule_ops.py
@@ -434,7 +434,7 @@ def test_loop_dep_reduce_cache_write():
 
 def test_reduction_and_dummy_fuse_split():
     n = 10
-    X = tvm.placeholder(shape=(10,), dtype='int32', name="X")
+    X = tvm.placeholder(shape=(n,), dtype='int32', name="X")
     k = tvm.reduce_axis((0, n))
     Y = tvm.compute((), lambda: tvm.sum(X[k], k), name="Y")
     s = tvm.create_schedule([Y.op])
@@ -445,6 +445,19 @@ def test_reduction_and_dummy_fuse_split():
     args = [tvm.nd.empty((), 'int32')] + [tvm.ndarray.array(np.ones((n,), dtype='int32'))]
     f(*args)
     assert args[0].asnumpy() == n
+
+    n = 10
+    X = tvm.placeholder(shape=(n,), dtype='int32', name="X")
+    k = tvm.reduce_axis((0, n))
+    Y = tvm.compute((n,), lambda i: tvm.sum(X[k], k), name="Y")
+    s = tvm.create_schedule([Y.op])
+    ax = s[Y.op].fuse(*(list(Y.op.axis) + list(Y.op.reduce_axis)))
+    f = tvm.build(s, [Y, X])
+
+    args = [tvm.ndarray.array(np.ones((n,), dtype='int32'))] + \
+        [tvm.ndarray.array(np.ones((n,), dtype='int32'))]
+    f(*args)
+    assert np.all(args[0].asnumpy() == n)
 
 if __name__ == "__main__":
     test_loop_dep_reduce()
