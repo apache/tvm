db076e293aed23592a2ca76db5e70a5b5343733e
Liangfu Chen <liangfu.chen@harman.com>
2019-02-12 11:16:19 +0800

    update s2g_xxx_dep dequeue condition, and replace abs path with relpath in `update.sh` scripts;

--- vta/hardware/intel/ip/compute/update.sh
+++ vta/hardware/intel/ip/compute/update.sh
@@ -1,5 +1,4 @@
-sed -r -i -e 's/reg\ \[31:0\]\ uop_mem\ \[0:1023\];/reg\ \[31:0\]\ uop_mem\ \[0:1023\]\ \/\*\ synthesis\ ramstyle\ =\ "M20K"\ \*\/;/g' /home/liangfu/workspace/chisel-vta/chisel/ip/vta/compute/Compute.v
-sed -r -i -e 's/reg\ \[511:0\]\ acc_mem\ \[0:255\];/reg\ \[511:0\]\ acc_mem\ \[0:255\]\ \/\*\ synthesis\ ramstyle\ =\ "M20K"\ \*\/;/g' /home/liangfu/workspace/chisel-vta/chisel/ip/vta/compute/Compute.v
-# sed -r -i -e 's/reg\ \[31:0\]\ mem\ \[0:1023\];/reg\ \[31:0\]\ mem\ \[0:1023\]\ \/\*\ synthesis\ ramstyle\ =\ "M20K"\ \*\/;/g' /home/liangfu/workspace/chisel-vta/chisel/ip/vta/compute/Compute.v
-cp /home/liangfu/workspace/chisel-vta/chisel/ip/vta/compute/Compute.v /home/liangfu/workspace/tvm/vta/hardware/intel/ip/compute/compute.v
-cp /home/liangfu/workspace/chisel-vta/chisel/ip/vta/compute/Compute.v /home/liangfu/workspace/tvm/vta/hardware/intel/scripts/soc_system/synthesis/submodules/compute.v
+sed -r -i -e 's/reg\ \[31:0\]\ uop_mem\ \[0:1023\];/reg\ \[31:0\]\ uop_mem\ \[0:1023\]\ \/\*\ synthesis\ ramstyle\ =\ "M20K"\ \*\/;/g' ../../../chisel/ip/vta/compute/Compute.v
+sed -r -i -e 's/reg\ \[511:0\]\ acc_mem\ \[0:255\];/reg\ \[511:0\]\ acc_mem\ \[0:255\]\ \/\*\ synthesis\ ramstyle\ =\ "M20K"\ \*\/;/g' ../../../chisel/ip/vta/compute/Compute.v
+cp ../../../chisel/ip/vta/compute/Compute.v ../../ip/compute/compute.v
+cp ../../../chisel/ip/vta/compute/Compute.v ../../scripts/soc_system/synthesis/submodules/compute.v
--- vta/hardware/intel/ip/vta_adaptor/update.sh
+++ vta/hardware/intel/ip/vta_adaptor/update.sh
@@ -1,2 +1,2 @@
-cp ~/workspace/chisel-vta/chisel/ip/vta/memarbiter/MemArbiter.v .
-cp ~/workspace/chisel-vta/chisel/ip/vta/memarbiter/MemArbiter.v ../../scripts/soc_system/synthesis/submodules/
+cp ../../../chisel/ip/vta/memarbiter/MemArbiter.v .
+cp ../../../chisel/ip/vta/memarbiter/MemArbiter.v ../../scripts/soc_system/synthesis/submodules/
