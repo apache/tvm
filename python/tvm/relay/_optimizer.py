4cf584354cd832d812a2ceb8b94a1ca53ee38445
Zhi Chen <chzhi@amazon.com>
2019-02-20 22:16:38 +0000

    remove deleted files

--- python/tvm/relay/_optimizer.py
+++
@@ -1,6 +0,0 @@
-# pylint: disable=no-else-return, unidiomatic-typecheck, undefined-variable
-"""The interface to the Optimizer exposed from C++."""
-
-from .._ffi.function import _init_api
-
-_init_api("relay._optimize", __name__)
