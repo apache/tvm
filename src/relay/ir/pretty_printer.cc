1d78b979ff5e81f3238072a3dae2f2f17c83f5d7
Josh Pollock <joshpoll@cs.washington.edu>
2019-02-23 14:44:38 -0800

    fix lets for real (mostly) and combine anf and gnf printing

--- src/relay/ir/pretty_printer.cc
+++ src/relay/ir/pretty_printer.cc
@@ -2,7 +2,7 @@
  *  Copyright (c) 2019 by Contributors
  * \file pretty_printer.cc
  * \brief Pretty printer for Relay programs
- * Supports functional style and metadata.
+ * Supports ANF, GNF, and metadata.
  */
 #include "doc.h"
 #include <tvm/relay/expr_functor.h>
@@ -13,24 +13,79 @@ namespace relay {
 class PrettyPrinter :
     public ExprFunctor<Doc(const Expr&)> {
   public:
-    explicit PrettyPrinter() {}
+    explicit PrettyPrinter(const std::unordered_map<Expr, Doc, NodeHash, NodeEqual>& memo_, size_t temp_var_counter_, bool GNF_) : memo_(memo_), temp_var_counter_(temp_var_counter_), GNF_(GNF_) {}
 
-    Doc Print(const NodeRef& node) {
+    explicit PrettyPrinter() : temp_var_counter_(0), GNF_(true) {}
+
+    explicit PrettyPrinter(bool GNF_) : temp_var_counter_(0), GNF_(GNF_) {}
+
+    // indent a new body
+    Doc PrintBody(const NodeRef& node, int indent = 2) {
+      Doc doc = Nil();
+      Doc body = Nil();
+      doc << "{";
+      doc << Indent(indent, body << "\n" << PrintNestedScope(node)) << "\n";
+      doc << "}";
+      return doc;
+    }
+
+    // create a new scope by creating a new printer object.
+    Doc PrintNestedScope(const NodeRef& node) {
+      if (GNF_) {
+        return PrettyPrinter(memo_, temp_var_counter_, GNF_).PrintFinal(node);
+      } else {
+        return Print(node);
+      }
+    }
+
+    Doc PrintFinal(const NodeRef& node) {
+     Print(node, true, false);
+      return doc;
+    }
+
+    // note: gnf flag is only one level deep
+    Doc Print(const NodeRef& node, bool gnf = true, bool hoist = true) {
       if (node.as_derived<ExprNode>()) {
-        return this->PrintExpr(Downcast<Expr>(node));
+        return PrintExpr(Downcast<Expr>(node), gnf, hoist);
       } else { assert(false); }
     }
 
-    Doc PrintExpr(const Expr& expr) {
+    Doc TempVar(int n) {
+      Doc doc = Nil();
+      return doc << "\%" << n;
+    }
+
+    Doc AllocTemp() {
+      return TempVar(temp_var_counter_++);
+    }
+
+    Doc PrintExpr(const Expr& expr, bool gnf = true, bool hoist = true) {
+      // Exploit memoization to print GNF.
+      // The first time we visit an expression, we need to allocate a temp var
+      // for it. Every subsequent time we can just use its assigned variable.
+      // This works since hashing uses pointer equality.
       auto it = memo_.find(expr);
       if (it != memo_.end()) return it->second;
-      Doc val = this->VisitExpr(expr);
-      memo_[expr] = val;
-      return val;
+      Doc printed_expr = VisitExpr(expr);
+      if (gnf && GNF_) {
+        if (hoist) {
+          Doc temp_var = AllocTemp();
+          memo_[expr] = temp_var;
+          doc << temp_var << " = " << printed_expr << "\n";
+          return temp_var;
+        } else {
+          memo_[expr] = printed_expr;
+          doc << printed_expr;
+          return printed_expr;
+        }
+      } else {
+        memo_[expr] = printed_expr;
+        return printed_expr;
+      }
     }
 
     Doc VisitExpr_(const ConstantNode* op) final {
-      // Print out simple scalar directly.
+      // Print out simple scalars directly.
       if (op->is_scalar()) {
         std::ostringstream os;
         DataType dtype = TVMType2Type(op->data->dtype);
@@ -54,7 +109,7 @@ class PrettyPrinter :
     Doc VisitExpr_(const TupleNode* op) final {
       std::vector<Doc> fields;
       for (Expr field : op->fields) {
-        fields.push_back(this->Print(field));
+        fields.push_back(Print(field));
       }
       Doc doc = Nil();
       return doc << "(" << PrintVec(fields, Text(", ")) << ")";
@@ -62,20 +117,71 @@ class PrettyPrinter :
 
     Doc VisitExpr_(const TupleGetItemNode* op) final {
       Doc doc = Nil();
-      return doc << this->Print(op->tuple) << "." << op->index;
+      return doc << Print(op->tuple) << "." << op->index;
     }
+
+    Doc VisitExpr_(const IfNode* op) final {
+      Doc doc = Nil();
+      doc << "if (" << Print(op->cond) << ") ";
+      doc << PrintBody(op->true_branch);
+      doc << " else ";
+      doc << PrintBody(op->false_branch);
+      return doc;
+    }
+
+    Doc VisitExpr_(const LetNode* op) final {
+      Doc doc = Nil();
+      // TODO: this should call a var printer, which needs to differentiate
+      //    between free and bound vars
+      // TODO: this should have a type annotation
+      // TODO: lets in value position need to be scoped
+      // 
+      // we use ANF mode for the first level of the value position so the final
+      // expression isn't hoisted or added to the doc stream
+      doc << "let \%" << op->var->name_hint() << " = " << Print(op->value, false) << ";" << "\n";
+      doc << PrintNestedScope(op->body);
+      return doc;
+    }
+
+    // Doc PrintFunc(const Doc& prefix, const FunctionNode* fn) {
+    //     // TODO(tqchen, M.K.) support generic function
+    //     // Possibly through meta-data
+    //     CHECK_EQ(fn->type_params.size(), 0U)
+    //     << "generic fn not yet supported";
+    //     Doc doc = Nil();
+    //     doc << prefix << "(";
+    //     AllocVarName(fn->params[i]);
+    //     this->PrintVarDecl(fn->params[i], stream_);
+    //     doc << ')';
+    //     /* if (fn->ret_type.defined()) {
+    //       doc << " -> ";
+    //       this->PrintType(fn->ret_type, stream_);
+    //     } */
+    //     doc << PrintBody(fn->body);
+    //     return doc;
+    // }
 
   private:
     /*! \brief Map from Expr to Doc */
+    Doc doc = Nil();
     std::unordered_map<Expr, Doc, NodeHash, NodeEqual> memo_;
+    size_t temp_var_counter_;
+    bool GNF_;
 };
 
-std::string RelayPrettyPrint(const NodeRef& node) {
-  return "v0.0.1\n" + Layout(PrettyPrinter().Print(node));
+std::string RelayGNFPrint(const NodeRef& node) {
+  return "v0.0.1\n" + Layout(PrettyPrinter().PrintFinal(node)) + "\n";
 }
 
-TVM_REGISTER_API("relay._expr.pretty_print")
-.set_body_typed<std::string(const NodeRef&)>(RelayPrettyPrint);
+std::string RelayANFPrint(const NodeRef& node) {
+  return "v0.0.1\n" + Layout(PrettyPrinter(false).Print(node)) + "\n";
+}
+
+TVM_REGISTER_API("relay._expr.gnf_print")
+.set_body_typed<std::string(const NodeRef&)>(RelayGNFPrint);
+
+TVM_REGISTER_API("relay._expr.anf_print")
+.set_body_typed<std::string(const NodeRef&)>(RelayANFPrint);
 
 } // relay
 } // tvm
--- tests/python/relay/test_ir_parser_roundtrip.py
+++ tests/python/relay/test_ir_parser_roundtrip.py
@@ -1,7 +1,7 @@
 import tvm
 from tvm import relay
 from tvm.relay.ir_pass import alpha_equal
-from tvm.relay._expr import pretty_print
+from tvm.relay._expr import anf_print, gnf_print
 import numpy as np
 
 from hypothesis import given, reject, settings
@@ -31,28 +31,35 @@ def projections(draw, field_type):
 @settings(deadline=500, derandomize=True)
 @given(exprs)
 def test_roundtrip_pp(e):
-    alpha_equal(relay.fromtext(pretty_print(e)), e)
+    alpha_equal(relay.fromtext(anf_print(e)), e)
 
 def test_gnf():
-    assert relay._expr.gnf_print(relay.const(1)) == "v0.0.1\n%0 = 1\n%0"
-    assert relay._expr.gnf_print(relay.Tuple([relay.const(1), relay.const(1)])) == "v0.0.1\n%0 = 1\n%1 = 1\n%2 = (%0, %1)\n%2"
+    assert gnf_print(relay.const(1)) == "v0.0.1\n1\n"
+    assert gnf_print(relay.Tuple([relay.const(1), relay.const(1)])) == "v0.0.1\n%0 = 1\n%1 = 1\n(%0, %1)\n"
     one = relay.const(1)
-    assert relay._expr.gnf_print(relay.Tuple([one, one])) == "v0.0.1\n%0 = 1\n%1 = (%0, %0)\n%1"
+    assert gnf_print(relay.Tuple([one, one])) == "v0.0.1\n%0 = 1\n(%0, %0)\n"
         
-    assert relay._expr.gnf_print(relay.If(relay.const(True), relay.TupleGetItem(relay.Tuple([one, one]), 0), relay.TupleGetItem(relay.Tuple([one, one, relay.const(1)]), 0))) == "v0.0.1\n%0 = True\n%1 = if (%0) {\n  %1 = 1\n  %2 = (%1, %1)\n  %3 = %2.0\n  %3\n} else {\n  %1 = 1\n  %2 = 1\n  %3 = (%1, %1, %2)\n  %4 = %3.0\n  %4\n}\n%1"
+    assert gnf_print(relay.If(relay.const(True), relay.TupleGetItem(relay.Tuple([one, one]), 0), relay.TupleGetItem(relay.Tuple([one, one, relay.const(1)]), 0))) == "v0.0.1\n%0 = True\nif (%0) {\n  %1 = 1\n  %2 = (%1, %1)\n  %2.0\n} else {\n  %1 = 1\n  %2 = 1\n  %3 = (%1, %1, %2)\n  %3.0\n}\n"
 
 if __name__ == "__main__":
     # for _ in range(10):
-    #     print(pretty_print(exprs.example()))
+    #     print(anf_print(exprs.example()))
     one = relay.const(1)
-    print(relay._expr.gnf_print(relay.TupleGetItem(relay.Tuple([one, one]), 0)))
+    tup = relay.Tuple([relay.const(1), relay.const(1)])
+    print(gnf_print(relay.TupleGetItem(relay.Tuple([one, one]), 0)))
     print()
-    print(relay._expr.gnf_print(relay.If(relay.const(True), relay.TupleGetItem(relay.Tuple([one, one]), 0), relay.TupleGetItem(relay.Tuple([one, one, relay.const(1)]), 0))))
+    print(gnf_print(relay.If(relay.const(True), relay.TupleGetItem(relay.Tuple([one, one]), 0), relay.TupleGetItem(relay.Tuple([one, one, relay.const(1)]), 0))))
+    print()
+    print(anf_print(relay.If(relay.const(True), relay.TupleGetItem(relay.Tuple([one, one]), 0), relay.TupleGetItem(relay.Tuple([one, one, relay.const(1)]), 0))))
     print()
     SEMVER = "v0.0.1"
-    print(relay._expr.gnf_print(relay.fromtext(SEMVER+"let %x = 1; 5")))
-    print()
+    print(gnf_print(relay.fromtext(SEMVER+"let %x = 1; 5")))
     print(relay.fromtext(SEMVER+"let %x = 1; %x").astext())
     print(relay.fromtext(SEMVER+"let %x = (1, 1); %x").astext())
     print(relay.TupleGetItem(relay.Tuple([one, one]), 0).astext())
-    print()
+    print(relay.fromtext(SEMVER+"let %x = 1; let %x = 2; %x").astext())
+    print(relay.Let(relay.var("x"), relay.Tuple([tup, tup]), relay.const(5)).astext())
+    print(gnf_print(relay.Let(relay.var("x"), relay.Tuple([tup, tup]), relay.const(5))))
+    print(anf_print(relay.Let(relay.var("x"), relay.Tuple([tup, tup]), relay.const(5))))
+    print(anf_print(relay.fromtext(SEMVER+"let %x = 1; let %x = 2; 3")))
+    print(gnf_print(relay.fromtext(SEMVER+"let %x = 1; let %x = 2; 3")))
