e5fb60f9ab24c7479df67d3940d311da90b91777
Marcus Shawcroft <marcus.shawcroft@arm.com>
2019-02-28 18:23:59 +0000

    Docker updates (#2702)
    * [DOCKER] Switch from yes|apt-get to apt-get -y
    
    The yes | apt-get idom guarantees that the 'yes' process always exists
    with exit code 141 (pipe broken).  This is fine while the script
    generally ignores failures but won't work when the script behaviour is
    tightened to robustly catch errors.
    
    * [DOCKER] Turn down the wget/curl volume

--- docker/install/ubuntu_install_gradle.sh
+++ docker/install/ubuntu_install_gradle.sh
@@ -7,7 +7,7 @@ GRADLE_VERSION=4.10-rc-2
 GRADLE_SHA256=e90d3c32910e259814bcca82b3911172ecca1ff1ab5ed69b4de3c1df8b378b40
 
 echo "Downloading Gradle"
-wget --output-document=gradle.zip "https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip"
+wget -q --output-document=gradle.zip "https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip"
 echo "Checking Gradle hash"
 echo "${GRADLE_SHA256} *gradle.zip" | sha256sum --check -
 echo "Installing Gradle"
--- docker/install/ubuntu_install_python.sh
+++ docker/install/ubuntu_install_python.sh
@@ -2,11 +2,11 @@
 apt-get update && apt-get install -y python-dev
 
 # python 3.6
-apt-get update && yes | apt-get install software-properties-common
+apt-get update && apt-get install -y software-properties-common
 add-apt-repository ppa:jonathonf/python-3.6 &&\
     apt-get update && apt-get install -y python-pip python-dev python3.6 python3.6-dev
 
 rm -f /usr/bin/python3 && ln -s /usr/bin/python3.6 /usr/bin/python3
 
 # Install pip
-cd /tmp && wget https://bootstrap.pypa.io/get-pip.py && python2 get-pip.py && python3.6 get-pip.py
+cd /tmp && wget -q https://bootstrap.pypa.io/get-pip.py && python2 get-pip.py && python3.6 get-pip.py
