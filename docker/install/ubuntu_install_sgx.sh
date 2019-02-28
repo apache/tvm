78c383e52833d8aa09ae108bb3b214ef86740dcc
Marcus Shawcroft <marcus.shawcroft@arm.com>
2019-02-28 18:23:59 +0000

    Docker updates (#2702)
    * [DOCKER] Switch from yes|apt-get to apt-get -y
    
    The yes | apt-get idom guarantees that the 'yes' process always exists
    with exit code 141 (pipe broken).  This is fine while the script
    generally ignores failures but won't work when the script behaviour is
    tightened to robustly catch errors.
    
    * [DOCKER] Turn down the wget/curl volume

--- docker/install/ubuntu_install_sgx.sh
+++ docker/install/ubuntu_install_sgx.sh
@@ -8,7 +8,7 @@ apt-get update && apt-get install -y --no-install-recommends --force-yes \
 git clone https://github.com/intel/linux-sgx.git
 cd linux-sgx
 git checkout sgx_2.2
-curl 'https://gist.githubusercontent.com/nhynes/c770b0e91610f8c020a8d1a803a1e7cb/raw/8f5372d9cb88929b3cc49a384943bb363bc06827/intel-sgx.patch' | git apply
+curl -s -S 'https://gist.githubusercontent.com/nhynes/c770b0e91610f8c020a8d1a803a1e7cb/raw/8f5372d9cb88929b3cc49a384943bb363bc06827/intel-sgx.patch' | git apply
 ./download_prebuilt.sh
 make -j4 sdk && make -j4 sdk_install_pkg
 ./linux/installer/bin/sgx_linux_x64_sdk*.bin --prefix /opt
@@ -17,5 +17,5 @@ cd -
 git clone https://github.com/baidu/rust-sgx-sdk.git /opt/rust-sgx-sdk
 cd /opt/rust-sgx-sdk
 git checkout 6098af # v1.0.5
-curl 'https://gist.githubusercontent.com/nhynes/37164039c5d3f33aa4f123e4ba720036/raw/b0de575fe937231799930764e76c664b92975163/rust-sgx-sdk.diff' | git apply
+curl -s -S 'https://gist.githubusercontent.com/nhynes/37164039c5d3f33aa4f123e4ba720036/raw/b0de575fe937231799930764e76c664b92975163/rust-sgx-sdk.diff' | git apply
 cd -
