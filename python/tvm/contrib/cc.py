c8a3a59b76f0496c57c89c185cc404ca3b9f7b40
Martin Boos <mboos@outlook.com>
2019-03-05 16:48:03 +0100

    use LLVM linker (#2713)
    * use LLVM linker
    
    * error message improved in case of filenotfound
    
    * linting error fixed

--- python/tvm/contrib/cc.py
+++ python/tvm/contrib/cc.py
@@ -85,13 +85,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,\
             cl_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         (out, _) = proc.communicate()
     except FileNotFoundError:
-        raise RuntimeError("can not found cl.exe,"
+        raise RuntimeError("Can not find cl.exe,"
                            "please run this in Vistual Studio Command Prompt.")
     if proc.returncode != 0:
         msg = "Compilation error:\n"
         msg += py_str(out)
         raise RuntimeError(msg)
-    link_cmd = ["link"]
+    link_cmd = ["lld-link"]
     link_cmd += ["-dll", "-FORCE:MULTIPLE"]
 
     for obj in objects:
@@ -111,8 +111,11 @@ BOOL APIENTRY DllMain( HMODULE hModule,\
             link_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         (out, _) = proc.communicate()
     except FileNotFoundError:
-        raise RuntimeError("can not found link.exe,"
-                           "please run this in Vistual Studio Command Prompt.")
+        raise RuntimeError("Can not find the LLVM linker for Windows (lld-link.exe)."
+                           "Make sure it's installed"
+                           " and the installation directory is in the %PATH% environment "
+                           "variable. Prebuilt binaries can be found at: https://llvm.org/"
+                           "For building the linker on your own see: https://lld.llvm.org/#build")
     if proc.returncode != 0:
         msg = "Compilation error:\n"
         msg += py_str(out)
