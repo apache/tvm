3f8bb7f9f0ba4685979b8f2ab0db6ee73aa5c60a
Nick Hynes <nhynes@berkeley.edu>
2019-03-19 11:34:24 -0700

    Pin rust version used to compile tools (#2852)

--- docker/install/ubuntu_install_rust.sh
+++ docker/install/ubuntu_install_rust.sh
@@ -11,11 +11,10 @@ export CARGO_HOME=/opt/rust
 # this rustc is one supported by the installed version of rust-sgx-sdk
 curl -s -S -L https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchain nightly-2019-01-28
 . $CARGO_HOME/env
-rustup toolchain add nightly
 rustup component add rust-src
-cargo +nightly install sccache
-cargo +nightly install rustfmt-nightly --version 1.0.1 --force
-cargo +nightly install xargo
+cargo install sccache
+cargo install rustfmt-nightly --version 1.0.1 --force
+cargo install xargo
 
 # make rust usable by all users
 chmod -R a+w /opt/rust
