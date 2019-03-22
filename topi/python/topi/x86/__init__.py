9faf42e7994c62ba2ecd701ae2d88d281720a881
Ubuntu <ubuntu@ip-172-31-70-212.ec2.internal>
2019-03-22 23:44:04 +0000

    Add batch_matmul import

--- topi/python/topi/x86/__init__.py
+++ topi/python/topi/x86/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import as _abs
 
 from .conv2d import schedule_conv2d, schedule_conv2d_nhwc
+from .batch_matmul import schedule_batch_matmul
 from .binarize_pack import schedule_binarize_pack
 from .binary_dense import schedule_binary_dense
 from .dense import *
