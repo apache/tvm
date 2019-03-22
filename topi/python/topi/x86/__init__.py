803907576458661f4a4fd4f7758046f827baec71
Ubuntu <ubuntu@ip-172-31-70-212.ec2.internal>
2019-03-22 20:02:32 +0000

    Fix x86 dense

--- topi/python/topi/x86/__init__.py
+++ topi/python/topi/x86/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import as _abs
 from .conv2d import schedule_conv2d, schedule_conv2d_nhwc
 from .binarize_pack import schedule_binarize_pack
 from .binary_dense import schedule_binary_dense
+from .dense import *
 from .nn import *
 from .injective import *
 from .pooling import schedule_pool, schedule_global_pool
