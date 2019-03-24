d04c41c4f1200d8bd3dbdbbd5d91e4dea6273000
Nick Hynes <nhynes@berkeley.edu>
2019-03-24 10:42:59 -0700

    Use version invariant rustfmt (#2886)

--- docker/install/ubuntu_install_rust.sh
+++ docker/install/ubuntu_install_rust.sh
@@ -9,12 +9,10 @@ apt-get update && apt-get install -y --no-install-recommends curl
 export RUSTUP_HOME=/opt/rust
 export CARGO_HOME=/opt/rust
 # this rustc is one supported by the installed version of rust-sgx-sdk
-curl -s -S -L https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchain nightly-2019-01-28
+curl -s -S -L https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path --default-toolchain nightly-2019-03-24
 . $CARGO_HOME/env
-rustup component add rust-src
-cargo install sccache
-cargo install rustfmt-nightly --version 1.0.1 --force
-cargo install xargo
+rustup component add rustfmt
+cargo install sccache --no-default-features
 
 # make rust usable by all users
 chmod -R a+w /opt/rust
