1c3e4491d86175a7b6e27e1b22f74f67e04b4d28
Pratyush Patel <pratyushpatel.1995@gmail.com>
2019-03-28 15:23:49 +0000

    implemented HostLowLevelDevice

--- src/runtime/micro/host_low_level_device.cc
+++ src/runtime/micro/host_low_level_device.cc
@@ -4,7 +4,10 @@
  * \brief emulated low-level micro device implementation on host machine
  */
 
+#include <sys/mman.h>
+#include <cstring>
 #include "low_level_device.h"
+#include "micro_common.h"
 
 namespace tvm {
 namespace runtime {
@@ -17,24 +20,44 @@ class HostLowLevelDevice final : public LowLevelDevice {
    * \brief constructor to initialize on-host memory region to act as device
    * \param num_bytes size of the emulated on-device memory region
    */
-  HostLowLevelDevice(size_t num_bytes);
+  HostLowLevelDevice(size_t num_bytes)
+    : size_(num_bytes) {
+    size_t size_in_pages = (num_bytes + kPageSize - 1) / kPageSize;
+    int mmap_prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+    int mmap_flags = MAP_ANONYMOUS | MAP_PRIVATE;
+    base_addr_ = mmap(nullptr, size_in_pages * kPageSize,
+                      mmap_prot, mmap_flags, -1, 0);
+  }
 
   /*!
    * \brief destructor to deallocate on-host device region
    */
-  ~HostLowLevelDevice();
+  ~HostLowLevelDevice() {
+    munmap(base_addr_, size_);
+  }
 
   void Write(void* offset,
              void* buf,
-             size_t num_bytes) final;
+             size_t num_bytes) final {
+    void* addr = GetAddr(offset, base_addr_);
+    std::memcpy(addr, buf, num_bytes);
+  }
 
   void Read(void* offset,
             void* buf,
-            size_t num_bytes) final;
+            size_t num_bytes) final {
+    void* addr = GetAddr(offset, base_addr_);
+    std::memcpy(buf, addr, num_bytes);
+  }
 
-  void Execute(void* func_addr, void* breakpoint) final;
+  void Execute(void* func_addr, void* breakpoint) final {
+    void (*func)(void) = (void (*)(void)) func_addr;
+    func();
+  }
 
-  const void* base_addr() const final;
+  const void* base_addr() const final {
+    return base_addr_;
+  }
 
  private:
   /*! \brief base address of the micro device memory region */
@@ -44,7 +67,9 @@ class HostLowLevelDevice final : public LowLevelDevice {
 };
 
 const std::shared_ptr<LowLevelDevice> HostLowLevelDeviceCreate(size_t num_bytes) {
-  return nullptr;
+  std::shared_ptr<LowLevelDevice> lld = 
+      std::make_shared<HostLowLevelDevice>(num_bytes);
+  return lld;
 }
 } // namespace runtime
 } // namespace tvm
