7e732dc4df6885868777f14f037c7eab4e864e2d
Leyuan Wang <laurawly@gmail.com>
2019-04-02 18:24:44 +0000

    comment addressed

--- tests/python/frontend/mxnet/test_forward.py
+++ tests/python/frontend/mxnet/test_forward.py
@@ -506,6 +506,7 @@ def test_forward_gather_nd():
     verify((2, 2, 2), (2, 2), [[0, 1], [1, 0]])
 
 def test_forward_bilinear_resize():
+    # add tests including scale_height and scale_width when mxnet is updated to version 1.5
     data = mx.sym.var('data')
     mx_sym = mx.sym.contrib.BilinearResize2D(data, height=5, width=10)
     verify_mxnet_frontend_impl(mx_sym, (1, 2, 3, 4), (1, 2, 5, 10))
