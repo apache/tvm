57f37c73a5447119f451ad623b2117d2f0d4c2bf
tqchen <tianqi.tchen@gmail.com>
2019-04-07 13:35:30 -0700

    [HEADER] ASF Header --dir=docs

--- tests/lint/add_asf_header.py
+++ tests/lint/add_asf_header.py
@@ -78,12 +78,32 @@ header_pystyle = """
 # under the License.
 """.strip()
 
+header_rststyle = """
+..  Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+..    http://www.apache.org/licenses/LICENSE-2.0
+
+..  Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+""".strip()
+
 FMT_MAP = {
     "toml" : header_pystyle,
     "yml": header_pystyle,
     "rs" : header_cstyle,
     "md" : header_mdstyle,
     "cmake" : header_pystyle,
+    "rst" : header_rststyle,
 }
 
 def add_header(fname, header):
