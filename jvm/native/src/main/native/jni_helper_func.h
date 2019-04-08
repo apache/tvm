5d383eaed3d7b6fdf256a54036de21a4e934d987
tqchen <tianqi.tchen@gmail.com>
2019-04-07 18:29:54 -0700

    Fix java test

--- jvm/native/src/main/native/jni_helper_func.h
+++ jvm/native/src/main/native/jni_helper_func.h
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -16,9 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 /*!
- *  Copyright (c) 2017 by Contributors
  * \file jni_helper_func.h
  * \brief Helper functions for operating JVM objects
  */
