6b189ee7ebeacd7fe733a32bf2d02bf033012571
Jared Roesch <roeschinc@gmail.com>
2019-04-08 01:15:16 -0700

    Rename RelayPrint to AsText

--- src/relay/pass/fuse_ops.cc
+++ src/relay/pass/fuse_ops.cc
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -890,7 +890,7 @@ class FuseMutator : private ExprMutator {
 
   // Debug function, dump the group assignment in text.
   void DebugDumpGroup(const Expr& body) {
-    std::string text = RelayPrint(body, false, [this](const Expr& expr) -> std::string {
+    std::string text = AsText(body, false, [this](const Expr& expr) -> std::string {
         auto it = gmap_.find(expr.get());
         if (it == gmap_.end()) return "";
         std::ostringstream os;
