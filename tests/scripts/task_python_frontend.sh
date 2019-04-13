3cd601dcd65ea3bd91bbf32dffd739a85028a2b8
hlu1 <14827759+hlu1@users.noreply.github.com>
2019-04-12 22:53:20 -0700

    Add caffe2 nnvm frontend to CI

--- tests/scripts/task_python_frontend.sh
+++ tests/scripts/task_python_frontend.sh
@@ -6,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -49,6 +49,9 @@ python3 -m nose -v nnvm/tests/python/frontend/tensorflow
 
 echo "Running nnvm CoreML frontend test..."
 python3 -m nose -v nnvm/tests/python/frontend/coreml
+
+echo "Running nnvm Caffe2 frontend test..."
+python3 -m nose -v nnvm/tests/python/frontend/caffe2
 
 echo "Running nnvm DarkNet frontend test..."
 python3 -m nose -v nnvm/tests/python/frontend/darknet || exit -1
