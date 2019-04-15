7c01028c517862ea9634e8f521a4eb144ff8315b
Gus Smith <gussmith@cs.washington.edu>
2019-04-15 16:19:36 -0700

    Correctly implement datatype lowering in Python

--- python/tvm/__init__.py
+++ python/tvm/__init__.py
@@ -43,7 +43,7 @@ from . import ndarray as nd
 from .ndarray import context, cpu, gpu, opencl, cl, vulkan, metal, mtl
 from .ndarray import vpi, rocm, opengl, ext_dev
 
-from ._ffi.runtime_ctypes import TypeCode
+from ._ffi.runtime_ctypes import TypeCode, TVMType
 from ._ffi.ndarray import TVMContext
 from ._ffi.function import Function
 from ._ffi.base import TVMError, __version__
--- python/tvm/datatype.py
+++ python/tvm/datatype.py
@@ -5,6 +5,8 @@ from ._ffi.function import register_func as _register_func
 from . import make as _make
 from .api import convert
 from .expr import Call as _Call
+from ._ffi.runtime_ctypes import TVMType as _TVMType
+from . import _api_internal
 
 def _register_op(op_name, target, datatype_name, extern_func_name):
     """Register an external function which computes the given op.
@@ -26,13 +28,23 @@ def _register_op(op_name, target, datatype_name, extern_func_name):
     """
 
     def create_op_to_call_lower(extern_func_name):
-        """Create a function which lowers an operation to a function call."""
+        """Create a function which lowers an operation to a function call.
+
+        The returned function takes an op (e.g. an Add) and returns a call to
+        the specified external function, passing the op's two arguments.
+        The return type of the call depends on the type of the op: if it is
+        a custom type, then a uint of the same width as the custom type is
+        returned. Otherwise, the type is unchanged."""
         def lower(op):
-            print(op.dtype)
-            # TODO(gus) need to make bits available by parsing the type string
-            # return _make.Call(
-            #     "uint" + str(op.dtype.bits), extern_func_name,
-            #     convert([op.a, op.b]), _Call.Extern, None, 0)
+            dtype = op.dtype
+            t = _TVMType(dtype)
+            if _api_internal._get_custom_datatype_registered(t.type_code):
+                dtype = "uint" + str(t.bits)
+                if t.lanes > 1:
+                    dtype += "x" + str(t.lanes)
+            return _make.Call(
+                dtype, extern_func_name,
+                convert([op.a, op.b]), _Call.Extern, None, 0)
         return lower
 
     lower_func_name = "tvm.datatype.lower." + target + "." + op_name + "." \
