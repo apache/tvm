e2d5f1bb6dac6f9914311442c6d500639768d6d5
Yinghai Lu <yinghai@fb.com>
2019-04-15 21:22:44 -0700

    Address comments

--- topi/include/topi/nn/bias_add.h
+++ topi/include/topi/nn/bias_add.h
@@ -1,10 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 /*!
  *  Copyright (c) 2017 by Contributors
  * \brief bias_add op constructions
  * \file nn/bias_add.h
  */
-#ifndef TOPI_NN_BIAS_ADD_H
-#define TOPI_NN_BIAS_ADD_H
+#ifndef TOPI_NN_BIAS_ADD_H_
+#define TOPI_NN_BIAS_ADD_H_
 
 #include <string>
 
@@ -15,7 +34,6 @@
 
 namespace topi {
 namespace nn {
-using namespace tvm;
 
 /*!
 * \brief Creates an operation that calculates data + bias
@@ -37,4 +55,4 @@ inline tvm::Tensor bias_add(const tvm::Tensor& data, const tvm::Tensor& bias, in
 }
 }  // namespace nn
 }  // namespace topi
-#endif  // TOPI_NN_BIAS_ADD_H
+#endif  // TOPI_NN_BIAS_ADD_H_
