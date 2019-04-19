5a713ca8cb9504bb2c2c92e728242b40edfc4f99
Gus Smith <gussmith@cs.washington.edu>
2019-04-19 09:30:44 -0700

    Add bfloat16 datatype unittest

---
+++ src/codegen/datatype/mybfloat16.cc
@@ -0,0 +1,72 @@
+/*
+  Code originally from Tensorflow; taken and simplified. Original license:
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  ==============================================================================*/
+
+#include <cstddef>
+#include <cstdint>
+
+void FloatToBFloat16(const float* src, uint16_t* dst, size_t size) {
+  const uint16_t* p = reinterpret_cast<const uint16_t*>(src);
+  uint16_t* q = reinterpret_cast<uint16_t*>(dst);
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+  for (; size != 0; p += 2, q++, size--) {
+    *q = p[0];
+  }
+#else
+  for (; size != 0; p += 2, q++, size--) {
+    *q = p[1];
+  }
+#endif
+}
+
+void BFloat16ToFloat(const uint16_t* src, float* dst, size_t size) {
+  const uint16_t* p = reinterpret_cast<const uint16_t*>(src);
+  uint16_t* q = reinterpret_cast<uint16_t*>(dst);
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+  for (; size != 0; p++, q += 2, size--) {
+    q[0] = *p;
+    q[1] = 0;
+  }
+#else
+  for (; size != 0; p++, q += 2, size--) {
+    q[0] = 0;
+    q[1] = *p;
+  }
+#endif
+}
+
+void BFloat16Add(const uint16_t* a, const uint16_t* b, uint16_t* dst, size_t size) {
+  float a_f, b_f;
+  BFloat16ToFloat(a, &a_f, 1);
+  BFloat16ToFloat(b, &b_f, 1);
+  float out_f = a_f + b_f;
+  FloatToBFloat16(&out_f, dst, 1);
+}
+
+extern "C" uint16_t FloatToBFloat16_wrapper(float in) {
+  uint16_t out;
+  FloatToBFloat16(&in, &out, 1);
+  return out;
+}
+
+extern "C" float BFloat16ToFloat_wrapper(uint16_t in) {
+  float out;
+  BFloat16ToFloat(&in, &out, 1);
+  return out;
+}
+
+extern "C" uint16_t BFloat16Add_wrapper(uint16_t a, uint16_t b) {
+  uint16_t out;
+  BFloat16Add(&a, &b, &out, 1);
+  return out;
+}
---
+++ tests/python/unittest/test_datatype_mybfloat16.py
@@ -0,0 +1,64 @@
+import tvm
+from ctypes import *
+import topi
+import tvm.ir_pass as ir_pass
+import numpy as np
+
+
+def test_bfloat():
+    tgt = "llvm"
+
+    # You must first load the library containing the datatype implementation.
+    # In this case, we have built the test functions used below right into TVM.
+    # CDLL("libmybfloat16.so", RTLD_GLOBAL)
+
+    # TODO(gus) having numbers in typenames causes some weird parsing bug somewhere
+    #tvm.register_datatype("bfloat16", 24)
+    tvm.datatype.register("bfloat", 24)
+
+    tvm.datatype.register_op("FloatToBFloat16_wrapper", "Cast", "llvm",
+                             "bfloat", "float")
+    tvm.datatype.register_op("BFloat16ToFloat_wrapper", "Cast", "llvm",
+                             "float", "bfloat")
+    tvm.datatype.register_op("BFloat16Add_wrapper", "Add", "llvm", "bfloat")
+
+    X = tvm.placeholder((3, ), name="X")
+    Y = tvm.placeholder((3, ), name="Y")
+    Z = topi.cast(
+        topi.cast(X, dtype="custom[bfloat]") +
+        topi.cast(Y, dtype="custom[bfloat]"),
+        dtype="float")
+
+    # Create schedule and lower, manually lowering datatypes. Once datatype
+    # lowering is integrated directly into TVM's lower/build process, we won't
+    # need to do this manually.
+    s = tvm.create_schedule([Z.op])
+    flist = tvm.lower(s, [X, Y, Z])
+    flist = [flist]
+    # print(flist[0].body)
+    # def callback(stmt):
+    #     if isinstance(stmt, tvm.expr.Load):
+    #         print(stmt)
+    # tvm.ir_pass.PostOrderVisit(flist[0].body, callback)
+    #def callback(stmt):
+    #if isinstance(stmt, tvm.expr.Call):
+    #print(stmt.name + " " + stmt.dtype)
+    flist = [ir_pass.LowerDatatypes(func, tgt) for func in flist]
+    #print(flist[0].body)
+    #tvm.ir_pass.PostOrderVisit(flist[0].body, callback)
+    built_cast = tvm.build(flist[0], target=tgt)
+    #print(built_cast.get_source())
+    #exit(0)
+
+    ctx = tvm.context(tgt, 0)
+    x = tvm.nd.array(np.random.uniform(size=3).astype(X.dtype), ctx)
+    y = tvm.nd.array(np.random.uniform(size=3).astype(Y.dtype), ctx)
+    z = tvm.nd.empty(Z.shape, dtype=Z.dtype, ctx=ctx)
+
+    # TODO(gus) currently this test just ensures that this runs without error; it
+    # doesn't check the sanity of the output
+    built_cast(x, y, z)
+
+
+if __name__ == "__main__":
+    test_bfloat()
