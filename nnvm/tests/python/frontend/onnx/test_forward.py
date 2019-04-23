bdb0ae12fa204dc1100d282c16385ae3062255a6
Gemfield <gemfield@civilnet.cn>
2019-04-23 11:50:55 +0800

    Enhance upsample operator to adapt onnx opset version 9 for nnvm comp… (#2968)
    * Enhance upsample operator to adapt onnx opset version 9 for nnvm compiler
    
    * Add upsample test case for newer opset in nnvm
    
    * re-trigger the CI

--- nnvm/tests/python/frontend/onnx/test_forward.py
+++ nnvm/tests/python/frontend/onnx/test_forward.py
@@ -438,9 +438,41 @@ def _test_upsample_bilinear():
         tvm_out = get_tvm_output(model, in_array, target, ctx, out_shape, 'float32')
         tvm.testing.assert_allclose(out_array, tvm_out, rtol=1e-5, atol=1e-5)
 
+def _test_upsample_bilinear_opset9():
+    scale = 2
+    in_shape = (1, 1, 3, 3)
+    out_shape = (1, 1, 3*scale, 3*scale)
+    y = helper.make_node("Upsample", ['in','scales'], ['out'], mode='linear')
+    scales=[1.0, 1.0, 2.0, 2.0]
+    in_array = np.random.uniform(size=in_shape).astype(np.float32)
+    out_array = topi.testing.bilinear_resize_python(in_array, (3*scale, 3*scale), "NCHW")
+
+    ref_array = np.array(scales)
+    ref_node = helper.make_node('Constant',
+                                 inputs=[],
+                                 outputs=['scales'],
+                                 value=onnx.helper.make_tensor(name = 'const_tensor',
+                                                               data_type = TensorProto.FLOAT,
+                                                               dims = ref_array.shape,
+                                                               vals = ref_array.flatten().astype(float)))
+
+    graph = helper.make_graph([ref_node, y],
+                              'upsample_bilinear_opset9_test',
+                              inputs = [helper.make_tensor_value_info("in", TensorProto.FLOAT, list(in_shape))],
+                              outputs = [helper.make_tensor_value_info("out", TensorProto.FLOAT, list(out_shape))])
+
+    model = helper.make_model(graph, producer_name='upsample_bilinear_opset9_test')
+    inputs = []
+    inputs.append(in_array)
+
+    for target, ctx in ctx_list():
+        tvm_out = get_tvm_output(model, inputs, target, ctx, out_shape, 'float32')
+        tvm.testing.assert_allclose(out_array, tvm_out, rtol=1e-5, atol=1e-5)
+
 def test_upsample():
     _test_upsample_nearest()
     _test_upsample_bilinear()
+    _test_upsample_bilinear_opset9()
 
 def _test_softmax(inshape, axis):
     opname = 'Softmax'
