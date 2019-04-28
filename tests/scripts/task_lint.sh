9214e7456622d575631f41a78ee1fa5f8d02b467
Tianqi Chen <tqchen@users.noreply.github.com>
2019-04-28 12:04:19 -0700

    [CI] Add file type check (#3116)

--- tests/scripts/task_lint.sh
+++ tests/scripts/task_lint.sh
@@ -6,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -25,6 +25,9 @@ cleanup()
   rm -rf /tmp/$$.*
 }
 trap cleanup 0
+
+echo "Check file types..."
+python3 tests/lint/check_file_type.py
 
 echo "Check codestyle of c++ code..."
 make cpplint
