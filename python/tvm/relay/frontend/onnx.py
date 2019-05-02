97406a24e09952b4a969528465c8c1db796948e6
Jared Roesch <roeschinc@gmail.com>
2019-05-02 16:50:40 -0400

    Remove compiler

--- python/tvm/relay/frontend/onnx.py
+++ python/tvm/relay/frontend/onnx.py
@@ -944,7 +944,10 @@ class GraphProto(object):
                                               dtype=self._params[i_name].dtype)
             else:
                 self._num_input += 1
-                tshape = self._shape[i_name] if i_name in self._shape else ()
+                if i_name in self._shape:
+                    tshape = self._shape[i_name]
+                else:
+                    raise ValueError("Must provide an input shape for `{0}`.".format(i_name))
                 if isinstance(self._dtype, dict):
                     dtype = self._dtype[i_name] if i_name in self._dtype else d_type
                 else:
