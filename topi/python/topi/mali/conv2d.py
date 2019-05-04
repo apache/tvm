3961c329dc5f147747d51463b02729e78700f22f
Lianmin Zheng <lianminzheng@gmail.com>
2019-05-04 18:15:04 +0800

    fix typo

--- topi/python/topi/mali/conv2d.py
+++ topi/python/topi/mali/conv2d.py
@@ -352,7 +352,7 @@ def _decl_winograd(cfg, data, kernel, strides, padding, dilation, layout, out_dt
     # unpack output
     output = tvm.compute((N, CO, H, W), lambda n, co, h, w:
                          Y[co][n * nH * nW + (h//m) * nW + w//m][h % m][w % m]
-                         # thw following hack term is used to make the padding in batch gemm ("M")
+                         # the following hack term is used to make the padding in batch gemm ("M")
                          # effective, otherwise the padding will be eliminated by bound inference
                          + tvm.expr.Mul(tvm.const(0, out_dtype),
                                         M[alpha-1][alpha-1][CO-1][P_round-1]),
