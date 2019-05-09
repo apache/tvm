035a9d2189524e6e684e3cc8195313b381eebf89
Wang Yao <kevinthesunwy@gmail.com>
2019-05-09 11:18:54 -0700

    Add ASF header for testing files

--- tests/python/unittest/test_autotvm_graph_tuner_utils.py
+++ tests/python/unittest/test_autotvm_graph_tuner_utils.py
@@ -1,10 +1,23 @@
-import json
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 import tvm
-import topi
 
 from tvm import autotvm, relay
 from tvm.relay.testing import resnet
-from tvm.autotvm.task import ConfigEntity
 from tvm.autotvm.graph_tuner.utils import has_multiple_inputs, get_direct_ancestor, get_in_nodes, \
     get_out_nodes, expr2graph, bind_inputs
 from tvm.relay.expr import Call, TupleGetItem, Tuple
