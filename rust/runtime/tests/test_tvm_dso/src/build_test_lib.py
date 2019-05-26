eecd64a599c118a1f78f5f0c303ba78fc70d3227
Nick Hynes <nhynes@berkeley.edu>
2019-05-26 03:01:21 -0700

    Move shebang

--- rust/runtime/tests/test_tvm_dso/src/build_test_lib.py
+++ rust/runtime/tests/test_tvm_dso/src/build_test_lib.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,8 +15,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-#!/usr/bin/env python3
 
 """Prepares a simple TVM library for testing."""
 
