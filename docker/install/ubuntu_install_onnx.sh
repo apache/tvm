7b4187bfb9a4a718ba42561ee156768863c45229
Jared Roesch <roeschinc@gmail.com>
2019-06-04 01:56:21 -0700

    Bump ONNX version

--- docker/install/ubuntu_install_onnx.sh
+++ docker/install/ubuntu_install_onnx.sh
@@ -6,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,7 +21,7 @@ set -u
 set -o pipefail
 
 # fix to certain version for now
-pip3 install onnx>=1.1.0
+pip3 install onnx>=1.4.1
 
 pip3 install https://download.pytorch.org/whl/cu80/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl
 pip3 install torchvision
