29b0b4c11d64c3c688aff01f10d997349b49596c
Ramana Radhakrishnan <ramana.radhakrishnan+github@arm.com>
2019-06-05 18:19:13 +0100

    Add support for overloading comparison operations in relay (#2910) (#3168)

---
+++ tests/python/relay/test_cmp_op.py
@@ -0,0 +1,36 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from tvm import relay
+a = relay.Var("a")
+b = relay.expr.const (1.0, dtype='float32')
+
+c = a < b
+d = relay.less (a, b)
+assert (c.astext() == d.astext())
+
+c = a > b
+d = relay.greater (a, b)
+assert (c.astext() == d.astext())
+
+c = (a >= b)
+d = relay.greater_equal(a, b)
+assert (c.astext() == d.astext())
+
+c = (a <= b)
+d = relay.less_equal(a, b)
+assert (c.astext() == d.astext())
