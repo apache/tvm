db7462def68339f3bb82461b55d71faec5fa274f
Thierry Moreau <moreau@uw.edu>
2019-06-18 18:37:32 -0700

    support for callable build func

--- python/tvm/autotvm/measure/measure_methods.py
+++ python/tvm/autotvm/measure/measure_methods.py
@@ -86,10 +86,9 @@ class LocalBuilder(Builder):
                 build_func = ndk.create_shared
             else:
                 raise ValueError("Invalid build_func" + build_func)
-
-        # FIXME: right now we're circumventing the wrap_build_func
-        # self.build_func = _wrap_build_func(build_func)
-        self.build_func = build_func
+            self.build_func = _wrap_build_func(build_func)
+        else:
+            self.build_func = build_func
         self.executor = LocalExecutor(timeout=timeout)
         self.tmp_dir = tempfile.mkdtemp()
 
