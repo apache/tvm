4be3cbcfcbff4162d62408fd222933a0f8ae0bfe
Thierry Moreau <moreau@uw.edu>
2019-06-18 21:37:15 -0700

    adding headers

--- vta/scripts/tune_dense.py
+++ vta/scripts/tune_dense.py
@@ -1,4 +1,22 @@
-"""Tuning a single conv2d operator"""
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Tuning a single dense operator"""
+
 from collections import namedtuple
 import logging
 import os
@@ -81,7 +99,7 @@ if __name__ == '__main__':
         tuner = autotvm.tuner.RandomTuner(task)
         tuner.tune(n_trial=len(task.config_space),
                 measure_option=measure_option,
-                callbacks=[autotvm.callback.log_to_file('conv2d.log')])
+                callbacks=[autotvm.callback.log_to_file('dense.log')])
 
         print("\nBest tuner config:")
         print(tuner.best_config)
