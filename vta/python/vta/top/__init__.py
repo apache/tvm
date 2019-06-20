31beec6239f42fdbfa8922af8195ac2451167885
Thierry Moreau <moreau@uw.edu>
2019-06-19 18:17:45 -0700

    deprecating nnvm for VTA

--- vta/python/vta/top/__init__.py
+++ vta/python/vta/top/__init__.py
@@ -2,9 +2,11 @@
 
 from . import bitpack
 from .graphpack import graph_pack
-from . import nnvm_bitpack
-from .nnvm_graphpack import nnvm_graph_pack
-from . import nnvm_op
 from . import op
 from . import vta_conv2d
 from . import vta_dense
+
+# NNVM is deprecated for VTA
+# from . import nnvm_bitpack
+# from .nnvm_graphpack import nnvm_graph_pack
+# from . import nnvm_op
