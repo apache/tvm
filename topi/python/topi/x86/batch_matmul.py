422b9ed3d527a621de5b9e088063956d89af82e1
Wang Yao <kevinthesunwy@gmail.com>
2019-06-30 00:49:55 -0700

    Add default

--- topi/python/topi/x86/batch_matmul.py
+++ topi/python/topi/x86/batch_matmul.py
@@ -20,8 +20,18 @@ from __future__ import absolute_import as _abs
 import tvm
 
 from tvm import autotvm
+from tvm.autotvm.task.space import SplitEntity
 from .. import generic
-from ..util import traverse_inline
+from ..util import traverse_inline, get_const_tuple, get_max_power2_factor
+
+def _default_dense_pack_config(cfg, M, N, K):
+    tile_y = get_max_power2_factor(M, 8)
+    tile_x = get_max_power2_factor(N, 8)
+    tile_k = get_max_power2_factor(N, 16)
+
+    cfg["tile_y"] = SplitEntity([M // tile_y, tile_y])
+    cfg["tile_x"] = SplitEntity([N // tile_x, tile_x])
+    cfg["tile_k"] = SplitEntity([K // tile_k, tile_k])
 
 
 @autotvm.register_topi_schedule(generic.schedule_batch_matmul, 'cpu', ['direct'])
@@ -44,17 +54,23 @@ def schedule_batch_matmul(cfg, outs):
     def _callback(op):
         if "batch_matmul" in op.tag:
             C = op.output(0)
+            A = s[C].op.input_tensors[0]
+            _, K = get_const_tuple(A.shape)
+            _, M, N = get_const_tuple(C.shape)
+            b, y, x = s[C].op.axis
             k, = s[C].op.reduce_axis
             cfg.define_split("tile_k", k, num_outputs=2,
                              filter=lambda item: item.size[-1] <= 64)
-            ko, ki = cfg["tile_k"].apply(s, C, k)
-            CC = s.rfactor(C, ki)
-
-            b, y, x = s[C].op.axis
             cfg.define_split("tile_y", y, num_outputs=2,
                              filter=lambda item: item.size[-1] <= 32)
             cfg.define_split("tile_x", x, num_outputs=2,
                              filter=lambda item: item.size[-1] <= 32)
+
+            if cfg.is_fallback:
+                _default_dense_pack_config(cfg, M, N, K)
+
+            ko, ki = cfg["tile_k"].apply(s, C, k)
+            CC = s.rfactor(C, ki)
             yo, yi = cfg["tile_y"].apply(s, C, y)
             xo, xi = cfg["tile_x"].apply(s, C, x)
             s[C].reorder(b, yo, xo, yi, xi)
