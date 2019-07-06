8124f7edf8efeadfcba546e6169fb53d17959383
Yida Wang <yidawa@gmail.com>
2019-07-06 06:04:40 -0700

    address review comments; remove duplicate code

--- topi/python/topi/x86/conv2d_transpose.py
+++ topi/python/topi/x86/conv2d_transpose.py
@@ -20,7 +20,7 @@
 import tvm
 from tvm import autotvm
 from .. import generic, tag
-from ..nn.conv2d_transpose import conv2d_transpose_nchw
+from ..nn.conv2d_transpose import conv2d_transpose_nchw, declaration_conv2d_transpose_impl
 from ..nn.dilate import dilate
 from ..nn.pad import pad
 from ..nn.util import get_pad_tuple
@@ -28,40 +28,8 @@ from ..util import simplify
 
 @autotvm.register_topi_compute(conv2d_transpose_nchw, 'cpu', ['direct'])
 def _declaration_conv2d_transpose(cfg, data, kernel, strides, padding, out_dtype):
-    return _declaration_conv2d_transpose_impl(cfg, data, kernel, strides, padding, out_dtype)
-
-def _declaration_conv2d_transpose_impl(cfg, data, kernel, strides, padding, out_dtype):
-    batch, in_c, in_h, in_w = data.shape
-    _, out_c, filter_h, filter_w = kernel.shape
-    stride_h, stride_w = strides
-    # dilate stage
-    DilatedInput = dilate(data, [1, 1, stride_h, stride_w], name='DilatedInput')
-    # padding stage
-    fpad_top, fpad_left, fpad_bottom, fpad_right = get_pad_tuple(padding, (filter_h, filter_w))
-    bpad_top = filter_h - 1 - fpad_top
-    bpad_bottom = filter_h - 1 - fpad_bottom
-    bpad_left = filter_w - 1 - fpad_left
-    bpad_right = filter_w - 1 - fpad_right
-    PaddedInput = pad(DilatedInput, \
-                        [0, 0, bpad_top, bpad_left], \
-                        [0, 0, bpad_bottom, bpad_right], \
-                        name='PaddedInput')
-    # convolution stage
-    out_c = simplify(out_c)
-    out_h = simplify((in_h - 1) * stride_h - fpad_top - fpad_bottom + filter_h)
-    out_w = simplify((in_w - 1) * stride_w - fpad_left - fpad_right + filter_w)
-    dc = tvm.reduce_axis((0, in_c), name='dc')
-    dh = tvm.reduce_axis((0, filter_h), name='dh')
-    dw = tvm.reduce_axis((0, filter_w), name='dw')
-
-    Output = tvm.compute(
-        (batch, out_c, out_h, out_w),
-        lambda b, c, h, w: tvm.sum(
-            PaddedInput[b, dc, h+dh, w+dw].astype(out_dtype) *
-            kernel[dc, c, filter_h-1-dh, filter_w-1-dw].astype(out_dtype),
-            axis=[dc, dh, dw]), tag="conv2d_transpose_nchw")
-
-    return Output
+    # TODO cfg is not used for now
+    return declaration_conv2d_transpose_impl(data, kernel, strides, padding, out_dtype)
 
 @autotvm.register_topi_schedule(generic.schedule_conv2d_transpose_nchw, 'cpu', ['direct'])
 def schedule_conv2d_transpose(cfg, outs):
@@ -73,7 +41,7 @@ def schedule_conv2d_transpose(cfg, outs):
     def traverse(op):
         """Traverse operators from computation graph"""
         # inline all one-to-one-mapping operators except the last stage (output)
-        if tag.is_broadcast(op.tag) or tag.is_injective(op.tag):
+        if tag.is_injective(op.tag):
             if op not in s.outputs:
                 s[op].compute_inline()
             for tensor in op.input_tensors:
