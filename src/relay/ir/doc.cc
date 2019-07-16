0867f3e1edfd1bbeb3c3a4697b472bd7eb39e413
Yinghai Lu <yinghai@fb.com>
2019-07-16 15:15:19 -0700

    Fix build error

--- src/relay/ir/doc.cc
+++ src/relay/ir/doc.cc
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -23,6 +23,7 @@
  * \brief Doc ADT used for pretty printing.
  * Based on Section 1 of https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf.
  */
+#include <cassert>
 #include <memory>
 #include <vector>
 #include "doc.h"
