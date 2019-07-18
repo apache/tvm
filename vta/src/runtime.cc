98fee4439b0220d0034ac0fcfa79de18ee77b9c9
Thierry Moreau <moreau@uw.edu>
2019-07-18 15:28:23 -0700

    fix

--- vta/src/runtime.cc
+++ vta/src/runtime.cc
@@ -442,8 +442,8 @@ class UopQueue : public BaseQueue<VTAUop> {
     }
     CHECK(buff_size <= kMaxBytes);
     // Move kernel contents to FPGA readable buffer
-    int32_t offset = 0;
-    for (int32_t i = 0; i < cache_.size(); ++i) {
+    uint32_t offset = 0;
+    for (uint32_t i = 0; i < cache_.size(); ++i) {
       uint32_t ksize = cache_[i]->size() * kElemBytes;
       VTAMemMoveToBuffer(static_cast<char*>(fpga_buff_) + offset,
                          cache_[i]->data(),
