ab49b637b106ba34d603f97723192fb3a7d75671
Jared Roesch <roeschinc@gmail.com>
2019-07-29 16:51:38 -0700

    Fix converter

--- python/tvm/relay/frontend/mxnet.py
+++ python/tvm/relay/frontend/mxnet.py
@@ -1183,8 +1183,10 @@ def from_mxnet(symbol,
         params = {}
         for k, v in symbol.collect_params().items():
             params[k] = _nd.array(v.data().asnumpy())
-        data = mx.sym.Variable("data")
-        sym = symbol(data)
+        inputs = []
+        for name in shape:
+            inputs.append(mx.sym.Variable(name))
+        sym = symbol(*inputs)
         if isinstance(sym, (list, tuple)):
             sym = mx.sym.Group(sym)
         shape, dtype = _update_shape_dtype(shape, dtype, params)
