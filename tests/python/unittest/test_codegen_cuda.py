f091e8e1e252e6a3ae03db22bce0abac126418f1
Christian Sarofeen <csarofeen@nvidia.com>
2019-07-29 10:16:55 -0400

    Guard test on CUDA support.

--- tests/python/unittest/test_codegen_cuda.py
+++ tests/python/unittest/test_codegen_cuda.py
@@ -199,6 +199,10 @@ def test_cuda_shuffle():
 
 
 def test_cuda_reducition_binding():
+    if not tvm.gpu(0).exist or not tvm.module.enabled("cuda"):
+        print("skip because cuda is not enabled..")
+        return
+
     k = tvm.reduce_axis((0, 32), 'k')
     A = tvm.placeholder((96, 32), name='A')
     B = tvm.compute( (96,), lambda m:
@@ -210,6 +214,7 @@ def test_cuda_reducition_binding():
 
     mo, _ = s[B].split(B.op.axis[0], 32)
     s[B].bind(mo, tvm.thread_axis("blockIdx.x"))
+
     fcuda = tvm.build(s, [A, B], "cuda")
 
 
