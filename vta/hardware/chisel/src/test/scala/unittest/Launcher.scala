214146eacbc63fd1cbc8b9a2bf53245645a6919c
Benjamin Tu <tu.benjamin1115@gmail.com>
2019-07-31 16:55:32 -0700

    added some comments

--- vta/hardware/chisel/src/test/scala/unittest/Launcher.scala
+++ vta/hardware/chisel/src/test/scala/unittest/Launcher.scala
@@ -28,6 +28,15 @@ import vta.shell._
 
 class TestConfig extends Config(new CoreConfig ++ new PynqConfig)
 
+/* 
+ * The Launcher object includes a test list for the TestRunner to check.
+ * Users can utilize this Launcher to run custom tests.
+ *
+ * How to Use:
+ * When the user input: sbt 'test:runMain unittest.Launcher gemv'
+ * the TestRunner will look for 'gemv' in the map and executes the 
+ * test that 'gemv' is mapped to
+ */
 object Launcher {
   implicit val p: Parameters = new TestConfig
   val tests = Map(
