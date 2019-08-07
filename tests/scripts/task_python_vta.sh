6026e04cb8aa2152249eb4045a0011220507cb32
Thierry Moreau <moreau@uw.edu>
2019-08-07 08:53:41 -0700

    [VTA][Dockerfile] Chisel dependencies for TSIM CI (#3721)

--- tests/scripts/task_python_vta.sh
+++ tests/scripts/task_python_vta.sh
@@ -27,8 +27,27 @@ rm -rf ~/.tvm
 # Rebuild cython
 make cython3
 
-echo "Running unittest..."
+# Run unit tests in functional/fast simulator
+echo "Running unittest in fsim..."
 python3 -m nose -v vta/tests/python/unittest
 
-echo "Running integration test..."
+# Run unit tests in functional/fast simulator
+echo "Running integration test in fsim..."
 python3 -m nose -v vta/tests/python/integration
+
+# # Build VTA chisel design and verilator simulator
+# cd vta/hardware/chisel/ && make && cd -
+
+# # Set default VTA config to use TSIM cycle accurate sim
+# cp vta/config/tsim_sample.json vta/config/vta_config.json
+
+# # Run unit tests in functional/fast simulator
+# echo "Running unittest in tsim..."
+# python3 -m nose -v vta/tests/python/unittest
+
+# # Run unit tests in functional/fast simulator
+# echo "Running integration test in tsim..."
+# python3 -m nose -v vta/tests/python/integration
+
+# # Reset default fsim simulation
+# cp vta/config/fsim_sample.json vta/config/vta_config.json
