7bbe52691780a16165bbb65b7fa1df7bf4dfa985
Thierry Moreau <moreau@uw.edu>
2019-08-06 23:03:28 -0700

    fix commands

--- docker/install/ubuntu_install_chisel.sh
+++ docker/install/ubuntu_install_chisel.sh
@@ -23,4 +23,5 @@ set -o pipefail
 # Install the necessary dependencies for Chisel
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update && apt-get install -y verilator sbt
+sudo apt-get update
+sudo apt-get install -y verilator sbt
