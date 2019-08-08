cdbe0fdf99b99ee362f6d47f17c1cf8bb1551c3a
Marcus Shawcroft <marcus.shawcroft@gmail.com>
2019-08-08 15:21:42 +0100

    [DOCKER] Drop superflous explicit sudo's

--- docker/install/ubuntu_install_chisel.sh
+++ docker/install/ubuntu_install_chisel.sh
@@ -25,7 +25,7 @@ set -o pipefail
 apt-get install apt-transport-https
 
 # Install the necessary dependencies for Chisel
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install -y verilator sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+apt-get update
+apt-get install -y verilator sbt
