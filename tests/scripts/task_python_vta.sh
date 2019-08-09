6b29445e326634a63f98efaaff712fefd50e3da5
Thierry Moreau <moreau@uw.edu>
2019-08-09 14:02:42 -0700

    disabling TSIM tests for now

--- tests/scripts/task_python_vta.sh
+++ tests/scripts/task_python_vta.sh
@@ -35,19 +35,19 @@ python3 -m nose -v vta/tests/python/unittest
 echo "Running integration test in fsim..."
 python3 -m nose -v vta/tests/python/integration
 
-# Build VTA chisel design and verilator simulator
-(make -C vta/hardware/chisel/)
+# # Build VTA chisel design and verilator simulator
+# (make -C vta/hardware/chisel/)
 
-# Set default VTA config to use TSIM cycle accurate sim
-cp vta/config/tsim_sample.json vta/config/vta_config.json
+# # Set default VTA config to use TSIM cycle accurate sim
+# cp vta/config/tsim_sample.json vta/config/vta_config.json
 
-# Run unit tests in cycle accurate simulator
-echo "Running unittest in tsim..."
-python3 -m nose -v vta/tests/python/unittest
+# # Run unit tests in cycle accurate simulator
+# echo "Running unittest in tsim..."
+# python3 -m nose -v vta/tests/python/unittest
 
-# Run unit tests in cycle accurate simulator
-echo "Running integration test in tsim..."
-python3 -m nose -v vta/tests/python/integration
+# # Run unit tests in cycle accurate simulator
+# echo "Running integration test in tsim..."
+# python3 -m nose -v vta/tests/python/integration
 
-# Reset default fsim simulation
-cp vta/config/fsim_sample.json vta/config/vta_config.json
+# # Reset default fsim simulation
+# cp vta/config/fsim_sample.json vta/config/vta_config.json
