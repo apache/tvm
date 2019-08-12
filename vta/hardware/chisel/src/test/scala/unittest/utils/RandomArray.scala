986b07810ed1448b1135e8dbc51ca47d0fb3a555
Benjamin Tu <tu.benjamin1115@gmail.com>
2019-08-12 10:42:54 -0700

    Revert "add"
    This reverts commit 87077daebbe055dee11f80e37da3a6291138e0f0.

--- vta/hardware/chisel/src/test/scala/unittest/utils/RandomArray.scala
+++ vta/hardware/chisel/src/test/scala/unittest/utils/RandomArray.scala
@@ -22,26 +22,18 @@ package unittest.util
 import scala.util.Random
 import scala.math.pow
 
-<<<<<<< HEAD
 class RandomArray(val len: Int, val bits: Int) {
   if (bits < 1) throw new IllegalArgumentException ("bits should be greater than 1")
   val r = new Random
   def any : Array[Int] = {
-=======
-class VTARandomArray(val len: Int, val bits: Int) {
-  if (bits < 1) throw new IllegalArgumentException ("bits should be greater than 1")
-  val r = new Random
-
-  def get_random() : Array[Int] = {
->>>>>>> parent of 3545ef3e... fixes
     return Array.fill(len) { r.nextInt(pow(2, bits).toInt) - pow(2, bits-1).toInt }
   }
 
-  def get_positive() : Array[Int] = {
+  def positive : Array[Int] = {
     return Array.fill(len) { r.nextInt(pow(2, bits-1).toInt) }
   }
 
-  def get_negative() : Array[Int] = {
+  def negative : Array[Int] = {
     return Array.fill(len) { 0 - r.nextInt(pow(2, bits-1).toInt) }
   }
 }
