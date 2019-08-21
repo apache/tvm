a6616ee8999ead00afc0551f33c31cdbf522e957
Logan Weber <weberlo@cs.washington.edu>
2019-08-21 23:40:06 +0000

    Add comments on magic number in section size calc

--- python/tvm/contrib/binutil.py
+++ python/tvm/contrib/binutil.py
@@ -75,9 +75,21 @@ def tvm_callback_get_section_size(binary_path, section_name, toolchain_prefix):
         entry_size = int(tokens[1])
         if entry_name in sections_to_sum:
             section_size += entry_size
-    # TODO(weberlo): For some reason, section sizes on RISC-V are sometimes
-    # reported to be smaller than they are.  Figure out why.
-    if "riscv" in toolchain_prefix:
+
+    # NOTE: For some reason, the size of the BSS section on the RISC-V
+    # GCC is sometimes reported to be smaller than it is, so we need to adjust
+    # for this.
+    if "riscv" in toolchain_prefix and section_name == 'bss':
+        # TODO(weberlo): Figure out why 32 is the minimum constant that works.
+        #
+        # The current hypothesis is that the last symbols in the ".bss" and
+        # ".sbss" sections may have size zero, since the symbols in these
+        # sections are uninitialized and there's no address that follows that
+        # would enforce a particular size.
+        #
+        # If this is the case, then 32 just happens to be a safe amount of
+        # padding for most cases, but symbols can be arbitrarily large, so this
+        # isn't bulletproof.
         return section_size + 32
     return section_size
 
