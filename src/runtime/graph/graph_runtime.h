7a0ac08dc137e9b4eee0eebb251cf94e26e8ed48
Marisa Kirisame <lolisa@marisa.moe>
2019-09-02 21:43:07 -0700

    quick fix

--- src/runtime/graph/graph_runtime.h
+++ src/runtime/graph/graph_runtime.h
@@ -430,6 +430,8 @@ class GraphRuntime : public ModuleNode {
   std::vector<size_t> data_alignment_;
   /*! \brief Operator on each node. */
   std::vector<std::function<void()> > op_execs_;
+  /*! \brief Arg info of TVM ops */
+  std::vector<std::shared_ptr<OpArgs> > op_args_;
 };
 
 std::vector<TVMContext> GetAllContext(const TVMArgs& args);
