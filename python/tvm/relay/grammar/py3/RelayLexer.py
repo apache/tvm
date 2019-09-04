4e4775c640e02d34286317e8089fa2c6242df02b
Marisa Kirisame <lolisa@marisa.moe>
2019-09-04 12:02:10 -0700

    undo lexer

--- python/tvm/relay/grammar/py3/RelayLexer.py
+++ python/tvm/relay/grammar/py3/RelayLexer.py
@@ -1,4 +1,4 @@
-# Generated from /home/marisa/Work/tvm/python/tvm/relay/grammar/Relay.g4 by ANTLR 4.7.1
+# Generated from /workspace/python/tvm/relay/grammar/Relay.g4 by ANTLR 4.7.1
 from antlr4 import *
 from io import StringIO
 from typing.io import TextIO
