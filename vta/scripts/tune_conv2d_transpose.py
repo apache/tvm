226011e2a0262ae31cbb14342624039e88254e31
Thierry Moreau <moreau@uw.edu>
2019-09-05 11:29:42 -0700

    [VTA][TOPI] Conv2d transpose (deconvolution) operator support (#3777)
    * initial conv2d_transpose
    
    * correct select operator
    
    * cleanup
    
    * fix
    
    * fix correcness check
    
    * conv2d transpose declaration fix
    
    * autotvm conv2d_transpose tuning script
    
    * ir pass fix
    
    * fix tuning script
    
    * deriving params from env, adding bias
    
    * removing bias comp from deconvolution
    
    * lint
    
    * fix
    
    * lint
    
    * lint
    
    * turning off cpu
    
    * lint, ops
    
    * lint
    
    * import fix
    
    * removing hard coded values
    
    * lint

--- vta/scripts/tune_conv2d_transpose.py
+++ vta/scripts/tune_conv2d_transpose.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""Tuning a single conv2d operator"""
+"""Tuning a single conv2d transpose operator"""
 
 from collections import namedtuple
 import logging
@@ -28,25 +28,18 @@ import topi
 import vta
 import vta.testing
 
+# Get batch info from env
 env = vta.get_env()
 
-Workload = namedtuple("Conv2DWorkload",
+Workload = namedtuple("Conv2DTransposeWorkload",
                       ['batch', 'height', 'width', 'in_filter', 'out_filter',
                        'hkernel', 'wkernel', 'hpad', 'wpad', 'hstride', 'wstride'])
 
-resnet_wkls = [
-    # Workloads of resnet18 on imagenet
-    # ('resnet-18.C1',  Workload(env.BATCH, 224, 224, 3,   64,  7, 7, 3, 3, 2, 2)),
-    ('resnet-18.C2',  Workload(env.BATCH,  56,  56, 64,  64,  3, 3, 1, 1, 1, 1)),
-    ('resnet-18.C3',  Workload(env.BATCH,  56,  56, 64,  128, 3, 3, 1, 1, 2, 2)),
-    ('resnet-18.C4',  Workload(env.BATCH,  56,  56, 64,  128, 1, 1, 0, 0, 2, 2)),
-    ('resnet-18.C5',  Workload(env.BATCH,  28,  28, 128, 128, 3, 3, 1, 1, 1, 1)),
-    ('resnet-18.C6',  Workload(env.BATCH,  28,  28, 128, 256, 3, 3, 1, 1, 2, 2)),
-    ('resnet-18.C7',  Workload(env.BATCH,  28,  28, 128, 256, 1, 1, 0, 0, 2, 2)),
-    ('resnet-18.C8',  Workload(env.BATCH,  14,  14, 256, 256, 3, 3, 1, 1, 1, 1)),
-    ('resnet-18.C9',  Workload(env.BATCH,  14,  14, 256, 512, 3, 3, 1, 1, 2, 2)),
-    ('resnet-18.C10', Workload(env.BATCH,  14,  14, 256, 512, 1, 1, 0, 0, 2, 2)),
-    ('resnet-18.C11', Workload(env.BATCH,   7,   7, 512, 512, 3, 3, 1, 1, 1, 1)),
+dcgan_wkls = [
+    # dcgan
+    ('DCGAN.CT1', Workload(env.BATCH,  4,  4, 1024, 512, 4, 4, 1, 1, 2, 2)),
+    ('DCGAN.CT2', Workload(env.BATCH,  8,  8,  512, 256, 4, 4, 1, 1, 2, 2)),
+    ('DCGAN.CT3', Workload(env.BATCH, 16, 16,  256, 128, 4, 4, 1, 1, 2, 2)),
 ]
 
 @tvm.tag_scope(tag=topi.tag.ELEMWISE)
@@ -58,35 +51,30 @@ def my_clip(x, a_min, a_max):
     x = tvm.compute(x.shape, lambda *i: tvm.max(x(*i), const_min), name="clipB")
     return x
 
-def conv2d(N, CI, H, W, CO, KH, KW, strides, padding, dilation):
+def conv2d_transpose(N, CI, H, W, CO, KH, KW, strides, padding):
     data_shape = (N//env.BATCH, CI//env.BLOCK_IN, H, W, env.BATCH, env.BLOCK_IN)
     kernel_shape = (CO//env.BLOCK_OUT, CI//env.BLOCK_IN, KH, KW, env.BLOCK_OUT, env.BLOCK_IN)
-    bias_shape = (N//env.BATCH, CO//env.BLOCK_OUT, 1, 1, env.BATCH, env.BLOCK_OUT)
 
     data = tvm.placeholder(data_shape, name="data", dtype=env.inp_dtype)
     kernel = tvm.placeholder(kernel_shape, name="kernel", dtype=env.wgt_dtype)
-    bias = tvm.placeholder(bias_shape, name="bias", dtype=env.acc_dtype)
 
     with tvm.target.vta():
-        res = topi.nn.conv2d(
-            input=data,
-            filter=kernel,
-            padding=padding,
+        res = topi.nn.conv2d_transpose_nchw(
+            Input=data,
+            Filter=kernel,
             strides=strides,
-            dilation=dilation,
-            layout='NCHW%dn%dc' % (env.BATCH, env.BLOCK_IN),
+            padding=padding,
             out_dtype=env.acc_dtype)
         res = topi.right_shift(res, env.WGT_WIDTH)
-        res = topi.add(res, bias)
         res = my_clip(res, 0, (1 << env.OUT_WIDTH - 1) - 1)
         res = topi.cast(res, env.out_dtype)
 
     if tvm.target.current_target().device_name == 'vta':
-        s = topi.generic.schedule_conv2d_nchw([res])
+        s = topi.generic.schedule_conv2d_transpose_nchw([res])
     else:
         s = tvm.create_schedule([res.op])
 
-    return s, [data, kernel, bias, res]
+    return s, [data, kernel, res]
 
 if __name__ == '__main__':
 
@@ -95,7 +83,7 @@ if __name__ == '__main__':
     # logging.getLogger('autotvm').setLevel(logging.DEBUG)
 
     # Tuning log files
-    log_file = "%s.conv2d.log" % (env.TARGET)
+    log_file = "%s.conv2d_transpose.log" % (env.TARGET)
     # create tmp log file
     tmp_log_file = log_file + ".tmp"
     if os.path.exists(log_file):
@@ -108,25 +96,24 @@ if __name__ == '__main__':
         print("Set your AutoTVM tracker node host and port variables to run the autotuner")
         exit()
 
-    for idx, (wl_name, wl) in enumerate(resnet_wkls):
-        prefix = "[Task %2d/%2d] " % (idx, len(resnet_wkls))
+    for idx, (wl_name, wl) in enumerate(dcgan_wkls):
+        prefix = "[Task %2d/%2d] " % (idx, len(dcgan_wkls))
 
         # Read in workload parameters
         N = wl.batch
-        CI = wl.in_filter
         H = wl.height
         W = wl.width
+        CI = wl.in_filter
         CO = wl.out_filter
         KH = wl.hkernel
         KW = wl.wkernel
         strides = (wl.hstride, wl.wstride)
         padding = (wl.hpad, wl.wpad)
-        dilation = (1, 1)
 
         # Create task
         task = autotvm.task.create(
-                conv2d,
-                args=(N, CI, H, W, CO, KH, KW, strides, padding, dilation),
+                conv2d_transpose,
+                args=(N, CI, H, W, CO, KH, KW, strides, padding),
                 target=tvm.target.vta(),
                 target_host=env.target_host,
                 template_key='direct')
