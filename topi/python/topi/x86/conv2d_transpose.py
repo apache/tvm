b21f94b085f957f5bfe9caecb0ef3954b69445d3
Yuwei Hu <huyuwei1995@gmail.com>
2019-09-07 01:04:51 +0000

    improve conv2d_transpose x86 performance by reusing conv2d schedule

--- topi/python/topi/x86/conv2d_transpose.py
+++ topi/python/topi/x86/conv2d_transpose.py
@@ -16,50 +16,76 @@
 # under the License.
 # pylint: disable=invalid-name,unused-variable,unused-argument,no-member
 """Conv2D Transpose schedule on x86"""
-
 import tvm
 from tvm import autotvm
-from .. import generic, tag
-from ..nn.conv2d_transpose import conv2d_transpose_nchw, declaration_conv2d_transpose_impl
+from .. import generic
+from ..util import get_const_tuple, traverse_inline
+from ..nn import conv2d_transpose_nchw, dilate, pad, get_pad_tuple
+from . import conv2d_avx_1x1, conv2d_avx_common
+from .conv2d import _declaration_conv_impl
+from .conv2d import _create_tuning_space as _create_tuning_space_conv2d
+from .conv2d import _get_default_config as _get_default_config_conv2d
+
 
 @autotvm.register_topi_compute(conv2d_transpose_nchw, 'cpu', ['direct'])
-def _declaration_conv2d_transpose(cfg, data, kernel, strides, padding, out_dtype):
-    # TODO cfg is not used for now
-    return declaration_conv2d_transpose_impl(data, kernel, strides, padding, out_dtype)
+def _conv2d_transpose_nchw(cfg, data, kernel, strides, padding, out_dtype):
+    batch, in_c, in_h, in_w = data.shape
+    _, out_c, filter_h, filter_w = kernel.shape
+    stride_h, stride_w = strides
+    # dilate data
+    data_dilate = dilate(data, [1, 1, stride_h, stride_w], name='data_dilate')
+    # pad data
+    fpad_top, fpad_left, fpad_bottom, fpad_right = get_pad_tuple(padding, (filter_h, filter_w))
+    bpad_top = filter_h - 1 - fpad_top
+    bpad_bottom = filter_h - 1 - fpad_bottom
+    bpad_left = filter_w - 1 - fpad_left
+    bpad_right = filter_w - 1 - fpad_right
+    data_pad = pad(data_dilate, \
+                   [0, 0, bpad_top, bpad_left], \
+                   [0, 0, bpad_bottom, bpad_right], \
+                   name='data_pad')
+    # transform kernel layout from IOHW to OIHW, and rotate kernel by 180 degrees
+    kernel_transform = tvm.compute((out_c, in_c, filter_h, filter_w), \
+                                   lambda o, i, h, w: kernel[i][o][filter_h-1-h][filter_w-1-w], \
+                                   name='kernel_transform')
+    # reuse conv2d implementation
+    _create_tuning_space_conv2d(cfg, data_pad, kernel_transform, strides=(1,1), padding=(0,0), \
+                                dilation=(1,1), layout="NCHW")
+    if cfg.is_fallback:
+        _get_default_config_conv2d(cfg, data_pad, kernel_transform, strides=(1,1), padding=(0,0), \
+                                   out_dtype=out_dtype, layout='NCHW')
+    return _declaration_conv_impl(cfg, data_pad, kernel_transform, strides=(1,1), padding=(0,0), \
+                                  dilation=(1,1), layout="NCHW", out_dtype=out_dtype)
+
 
 @autotvm.register_topi_schedule(generic.schedule_conv2d_transpose_nchw, 'cpu', ['direct'])
-def schedule_conv2d_transpose(cfg, outs):
+def _schedule_conv2d_transpose_nchw(cfg, outs):
     """Create schedule for tensors"""
     outs = [outs] if isinstance(outs, tvm.tensor.Tensor) else outs
     s = tvm.create_schedule([x.op for x in outs])
-    scheduled_ops = []
 
-    def traverse(op):
-        """Traverse operators from computation graph"""
-        # inline all one-to-one-mapping operators except the last stage (output)
-        if tag.is_injective(op.tag):
-            if op not in s.outputs:
-                s[op].compute_inline()
-            for tensor in op.input_tensors:
-                if isinstance(tensor.op, tvm.tensor.ComputeOp) and tensor.op not in scheduled_ops:
-                    traverse(tensor.op)
+    def _callback(op):
+        # reuse conv2d schedule
+        if 'conv2d_nchw' in op.tag:
+            output = op.output(0)
+            conv_out = op.input_tensors[0]
+            # retrieve data
+            data_vec = conv_out.op.input_tensors[0]
+            data_pad = data_vec.op.input_tensors[0]
+            data_dilate = data_pad.op.input_tensors[0]
+            s[data_dilate].compute_inline()
+            # retrieve kernel
+            kernel_vec = conv_out.op.input_tensors[1]
+            kernel_transform = kernel_vec.op.input_tensors[0]
+            s[kernel_transform].compute_inline()
+            # call conv2d schedule
+            _, _, kh, kw = get_const_tuple(kernel_transform.shape)
+            is_kernel_1x1 = kh == 1 and kw == 1
+            args = [s, cfg, data_dilate, data_pad, data_vec, kernel_vec, conv_out, output, outs[0]]
+            if is_kernel_1x1:
+                conv2d_avx_1x1._schedule_conv(*args)
+            else:
+                conv2d_avx_common._schedule_conv(*args)
 
-        if 'conv2d_transpose_nchw' in op.tag:
-            C = op.output(0)
-
-            N, OC, OH, OW = C.op.axis
-            rc, ry, rx = C.op.reduce_axis
-
-            OH, oh = s[C].split(OH, factor=2)
-            OC, oc = s[C].split(OC, factor=32)
-            IC, ic = s[C].split(rc, factor=32)
-
-            s[C].reorder(N, OC, OH, OW, oc, IC, ry, rx, ic)
-            N = s[C].fuse(N, OC)
-            s[C].vectorize(oc)
-            s[C].parallel(N)
-
-        scheduled_ops.append(op)
-
-    traverse(outs[0].op)
+    traverse_inline(s, outs[0].op, _callback)
     return s
