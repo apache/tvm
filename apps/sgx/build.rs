521bfc42bd77f95769246e633f4526162ba098ac
Nick Hynes <nhynes@oasislabs.com>
2019-09-23 01:49:10 +0000

    Fix SGX build script

--- apps/sgx/build.rs
+++ apps/sgx/build.rs
@@ -40,10 +40,7 @@ fn main() {
     let sysroot = String::from_utf8(sysroot_output.stdout).unwrap();
     let sysroot = sysroot.trim();
     let mut llvm_tools_path = std::path::PathBuf::from(&sysroot);
-    let target = sysroot.splitn(2, "-").nth(1).expect(&sysroot);
-    llvm_tools_path.push("lib/rustlib");
-    llvm_tools_path.push(target);
-    llvm_tools_path.push("bin");
+    llvm_tools_path.push("lib/rustlib/x86_64-unknown-linux-gnu/bin");
 
     std::process::Command::new(llvm_tools_path.join("llvm-objcopy"))
         .arg("--globalize-symbol=__tvm_module_startup")
