f2abd9f6e09b608c8f0a6ffb54347bdd20078459
Andrew Tulloch <andrew@tullo.ch>
2019-10-09 09:35:49 -0700

    [TVM] Rewrite simplification rule to eliminate unnecessary conditionals. (#4076)
    The current bounds checking infrastructure inserts checks like:
    
    ```
    for (i, 0, bounds[n]) {
      if (likely(i < bounds[n]) {
         ...
      }
    }
    ```
    
    into the TVM IR which is currently not removed by simplification infrastructure.
    This is a little unclean, as these are trivially true since for a loop var `i`
    with a given min and extent, we are guaranteed that `i >= min` and `i < min +
    extent`. Thus, we can insert these checks into the IR and use them to eliminate
    trivial bounds checks early on.

--- tests/python/unittest/test_arith_stmt_simplify.py
+++ tests/python/unittest/test_arith_stmt_simplify.py
@@ -47,6 +47,62 @@ def test_thread_extent_simplify():
     assert isinstance(body.body.body.body, tvm.stmt.Store)
 
 
+def test_basic_likely_elimination():
+    n = tvm.var('n')
+    X = tvm.placeholder(shape=(n,), name="x")
+    W = tvm.placeholder(shape=(n + 1,), dtype="int32", name="w")
+
+    def f(i):
+        start = W[i]
+        extent = W[i+1] - W[i]
+        rv = tvm.reduce_axis((0, extent))
+        return tvm.sum(X[rv + start], axis=rv)
+    Y = tvm.compute(X.shape, f, name="y")
+    s = tvm.create_schedule([Y.op])
+    stmt = tvm.lower(s, [X, W, Y], simple_mode=True)
+    assert('if' not in str(stmt))
+
+def test_complex_likely_elimination():
+    def cumsum(X):
+        """
+        Y[i] = sum(X[:i])
+        """
+        (m, ) = X.shape
+        s_state = tvm.placeholder((m + 1, ), dtype="int32", name="state")
+        s_init = tvm.compute((1, ), lambda _: tvm.const(0, "int32"))
+        s_update = tvm.compute((m + 1, ), lambda l: s_state[l - 1] + X[l - 1])
+        return tvm.scan(s_init, s_update, s_state, inputs=[X], name="cumsum")
+
+    def sparse_lengths_sum(data, indices, lengths):
+        oshape = list(data.shape)
+        oshape[0] = lengths.shape[0]
+        length_offsets = cumsum(lengths)
+
+        def sls(n, d):
+            gg = tvm.reduce_axis((0, lengths[n]))
+            indices_idx = length_offsets[n] + gg
+            data_idx = indices[indices_idx]
+            data_val = data[data_idx, d]
+            return tvm.sum(data_val, axis=gg)
+
+        return tvm.compute(oshape, sls)
+
+    m, n, d, i, l = tvm.var('m'), tvm.var('n'), tvm.var('d'), tvm.var('i'), tvm.var('l')
+    data_ph = tvm.placeholder((m, d * 32), name="data")
+    indices_ph = tvm.placeholder((i,), name="indices", dtype="int32")
+    lengths_ph = tvm.placeholder((n,), name="lengths", dtype="int32")
+    Y = sparse_lengths_sum(data_ph, indices_ph, lengths_ph)
+    s = tvm.create_schedule([Y.op])
+    (n, d) = s[Y].op.axis
+    (do, di) = s[Y].split(d, factor=32)
+    (gg,) = s[Y].op.reduce_axis
+    s[Y].reorder(n, do, gg, di)
+    s[Y].vectorize(di)
+    stmt = tvm.lower(s, [data_ph, indices_ph, lengths_ph, Y], simple_mode=True)
+    assert('if' not in str(stmt))
+
 if __name__ == "__main__":
     test_stmt_simplify()
     test_thread_extent_simplify()
+    test_basic_likely_elimination()
+    test_complex_likely_elimination()
