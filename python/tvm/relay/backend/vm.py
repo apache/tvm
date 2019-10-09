7c7ce6f3df23f94f2876815dbbc7beba0803ca03
Wei Chen <ipondering.weic@gmail.com>
2019-10-09 12:20:31 -0700

    compile clashes with builtin name

--- python/tvm/relay/backend/vm.py
+++ python/tvm/relay/backend/vm.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=no-else-return, unidiomatic-typecheck, undefined-variable, invalid-name
+# pylint: disable=no-else-return, unidiomatic-typecheck, undefined-variable, invalid-name, redefined-builtin
 """
 The Relay Virtual Machine.
 
