b96e14d0e279eb20305355c3a0a8d229a58bdf26
Cody Hao Yu <comaniac0422@gmail.com>
2019-10-21 00:36:45 +0000

    support conv2d HWCN in AutoTVM and Relay

--- src/pass/vectorize_loop.cc
+++ src/pass/vectorize_loop.cc
@@ -386,7 +386,7 @@ class Vectorizer : public IRMutator {
     CHECK(!op->condition.type().is_vector());
     Expr condition = this->Mutate(op->condition);
     if (condition.type().is_vector()) {
-      LOG(WARNING) << "Detect vector condition in Vectorized Loop, scalarizing...";
+      //LOG(WARNING) << "Detect vector condition in Vectorized Loop, scalarizing...";
       return Scalarize(s);
     }
     Stmt then_case = this->Mutate(op->then_case);
