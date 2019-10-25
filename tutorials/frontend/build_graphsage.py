f467fe3c26436364ec74d94eecffd85a2e1ee23b
Chien-Yu Lin <cyulin@cs.washington.edu>
2019-10-24 21:31:34 -0700

    use relay ops to implement mean aggregator

--- tutorials/frontend/build_graphsage.py
+++ tutorials/frontend/build_graphsage.py
@@ -38,6 +38,7 @@ class GraphSAGE(nn.Module):
 from dgl.data import load_data
 from collections import namedtuple
 import numpy as np
+import time
 
 def load_dataset(dataset="cora"):
     args = namedtuple("args", ["dataset"])
@@ -45,21 +46,11 @@ def load_dataset(dataset="cora"):
 
     # Remove self-loops to avoid duplicate passing of a node's feature to itself
     g = data.graph
-    g.add_edges_from(zip(g.nodes, g.nodes))
+    g.remove_edges_from(g.selfloop_edges())
 
-    mean_g = g
-    mean_g.remove_edges_from(g.selfloop_edges())
-    mean_adj = nx.to_numpy_array(mean_g)
-    #mean_adj = nx.to_numpy_matrix(mean_g)
-    #print(mean_adj.shape)
-    #print(mean_adj[0, [8,14,258,435,544]])
-    #print(mean_g.edges(0))
-    #print(np.nonzero(mean_adj[0]))
-    adj_cnt = np.sum(mean_adj, axis=1)
-    avg = np.reciprocal(adj_cnt)
-    mean_adj = (mean_adj.T * avg).T
+    adj = nx.to_numpy_array(g)
 
-    return g, data, mean_adj
+    return g, adj, data
 
 
 def evaluate(data, logits):
@@ -67,13 +58,23 @@ def evaluate(data, logits):
 
     pred = logits.argmax(axis=1)
     acc = ((pred == data.labels) * test_mask).sum() / test_mask.sum()
-
     return acc
+
+def time_evaluator(model, input, times):
+    model.eval()
+    time_costs = []
+    with torch.no_grad():
+        for i in range(times):
+            start = time.time()
+            logits= model(input)
+            if i > 0:
+                time_costs.append(time.time()-start)
+    return time_costs
 
 
 dataset = "cora"
 
-g, data, mean_adj = load_dataset(dataset)
+g, adj, data = load_dataset(dataset)
 
 num_layers = 1
 num_hidden = 16
@@ -102,8 +103,7 @@ model_path = "/sampa/home/cyulin/dgl/examples/pytorch/graphsage/graphsage_cora.t
 #
 ## Load the weights into the model
 torch_model.load_state_dict(torch.load(model_path))
-#
-#
+
 torch_model.eval()
 with torch.no_grad():
     logits_torch = torch_model(features)
@@ -112,35 +112,43 @@ print("Print the first five outputs from DGL-PyTorch execution\n", logits_torch[
 acc = evaluate(data, logits_torch.numpy())
 print("Test accuracy of DGL results: {:.2%}".format(acc))
 
+time_costs = time_evaluator(torch_model, features, 10)
+prof_res = np.array(time_costs) * 1000  # convert to millisecond
+print("DGL mean inference time (std dev): %.6f ms (%.6f ms)" %
+              (np.mean(prof_res), np.std(prof_res)))
+
+
 from tvm import relay
 from tvm.contrib import graph_runtime
 import tvm
+
+def mean_aggregator(feats, adj):
+    adj_sum = relay.sum(adj, axis=1)
+    adj_sum = relay.transpose(adj_sum)
+    feats = relay.transpose(feats)
+    agg_feats = relay.nn.dense(feats, adj)
+    agg_feats = relay.divide(agg_feats, adj_sum)
+    agg_feats = relay.transpose(agg_feats)
+    return agg_feats
 
 def SageConv(layer_name,
               input_dim,
               output_dim,
               adj,
-              mean_adj,
               input,
               norm=None,
               bias=True,
               activation=None):
-    #if norm is not None:
-    #    input = relay.multiply(input, norm)
+    neigh_feat = mean_aggregator(input, adj)
+
+    neigh_w = relay.var(layer_name + ".fc_neigh.weight", shape=(input_dim, output_dim))
+    neigh_o = relay.nn.dense(neigh_feat, neigh_w)
 
     self_w = relay.var(layer_name + ".fc_self.weight", shape=(input_dim, output_dim))
     self_o = relay.nn.dense(input, self_w)
     if bias is True:
         self_bias = relay.var(layer_name + ".fc_self.bias", shape=(output_dim, 1))
         self_o = relay.nn.bias_add(self_o, self_bias, axis=-1)
-    
-    input_t = relay.transpose(input)
-    neigh_feat = relay.nn.dense(input_t, mean_adj)
-    neigh_feat = relay.transpose(neigh_feat)
-
-    neigh_w = relay.var(layer_name + ".fc_neigh.weight", shape=(input_dim, output_dim))
-    neigh_o = relay.nn.dense(neigh_feat, neigh_w)
-    if bias is True:
         neigh_bias = relay.var(layer_name + ".fc_neigh.bias", shape=(output_dim, 1))
         neigh_o = relay.nn.bias_add(neigh_o, neigh_bias, axis=-1)
 
@@ -148,44 +156,12 @@ def SageConv(layer_name,
 
     if activation is not None:
         output = activation(output)
-    #if norm is not None:
-    #    output = relay.multiply(output, norm)
     return output
 
-def prepare_params(g, data):
-    params = {}
-    params['infeats'] = data.features.astype('float32') # Only support float32 as feature for now
-
-    # Generate adjacency matrix
-    adjacency = nx.to_scipy_sparse_matrix(g)
-    params['g_data'] = adjacency.data.astype('float32')
-    params['indices'] = adjacency.indices.astype('int32')
-    params['indptr'] = adjacency.indptr.astype('int32')
-
-    # Normalization w.r.t. node degrees
-    degs = [g.in_degree[i] for i in range(g.number_of_nodes())]
-    params['norm'] = np.power(degs, -0.5).astype('float32')
-    params['norm'] = params['norm'].reshape((params['norm'].shape[0], 1))
-
-    return params
-
-params = prepare_params(g, data)
-
-# Check shape of features and the validity of adjacency matrix
-assert len(params['infeats'].shape) == 2
-assert params['g_data'] is not None and params['indices'] is not None and params['indptr'] is not None
-assert params['infeats'].shape[0] == params['indptr'].shape[0] - 1
 
 infeats = relay.var("infeats", shape=data.features.shape)
-norm = relay.Constant(tvm.nd.array(params['norm']))
-g_data = relay.Constant(tvm.nd.array(params['g_data']))
-indices = relay.Constant(tvm.nd.array(params['indices']))
-indptr = relay.Constant(tvm.nd.array(params['indptr']))
 
-Adjacency = namedtuple('Adjacency', ['data', 'indices', 'indptr'])
-adj = Adjacency(g_data, indices, indptr)
-
-mean_adj = relay.Constant(tvm.nd.array(mean_adj.astype('float32'))) 
+adj = relay.Constant(tvm.nd.array(adj.astype('float32'))) 
 
 # Construct the 2-layer GCN
 layers = []
@@ -194,9 +170,7 @@ layers.append(SageConv(
     input_dim=infeat_dim,
     output_dim=num_hidden,
     adj=adj,
-    mean_adj=mean_adj,
     input=infeats,
-    norm=norm,
     activation=relay.nn.relu
 ))
 layers.append(SageConv(
@@ -204,28 +178,22 @@ layers.append(SageConv(
     input_dim=num_hidden,
     output_dim=num_classes,
     adj=adj,
-    mean_adj=mean_adj,
     input=layers[-1],
-    norm=norm,
     activation=None
 ))
 
 # Analyze free variables and generate Relay function
 output = layers[-1]
 func = relay.Function(relay.analysis.free_vars(output), output)
-#mod = relay.Module.from_expr(func)
-#mod = relay.transform.InferType()(mod)
-#print(mod.astext(show_meta_data=False))
 
-model_params = {}
+params = {}
+params['infeats'] = data.features.astype('float32') # Only support float32 as feature for now
+
 for param_tensor in torch_model.state_dict():
-    model_params[param_tensor] = torch_model.state_dict()[param_tensor].numpy()
+    params[param_tensor] = torch_model.state_dict()[param_tensor].numpy()
 
-for i in range(num_layers+1):
-    params["layers.%d.fc_self.weight"%(i)] = model_params["layers.%d.fc_self.weight"%(i)]
-    params["layers.%d.fc_self.bias"%(i)] = model_params["layers.%d.fc_self.bias"%(i)]
-    params["layers.%d.fc_neigh.weight"%(i)] = model_params["layers.%d.fc_neigh.weight"%(i)]
-    params["layers.%d.fc_neigh.bias"%(i)] = model_params["layers.%d.fc_neigh.bias"%(i)]
+# Check shape of features and the validity of adjacency matrix
+assert len(params['infeats'].shape) == 2
 
 # Set the TVM build target
 target = 'llvm' # Currently only support `llvm` as target
@@ -251,3 +219,10 @@ print("Test accuracy of TVM results: {:.2%}".format(acc))
 
 # Verify the results with the DGL model
 tvm.testing.assert_allclose(logits_torch, logits_tvm, atol=1e-3)
+
+timer = m.module.time_evaluator("run", ctx, number=1, repeat=10)
+tcost = timer()
+prof_res = tcost.results
+prof_res = np.array(tcost.results) * 1000  # convert to millisecond
+print("TVM mean inference time (std dev): %.6f ms (%.6f ms)" %
+              (np.mean(prof_res), np.std(prof_res)))
