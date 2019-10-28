70d1e33d6442d28893a4de425436045fac53e10f
Zhi Chen <chzhi@amazon.com>
2019-10-28 20:55:30 +0000

    Refine tutorial a bit

--- tutorials/dev/custom_relay_backend.py
+++ tutorials/dev/custom_relay_backend.py
@@ -18,27 +18,31 @@
 
 .. _tutorial-custom-relay-backend
 
-Design a New Relay Backend for Third-Parties
+Bring Your Own Codegen To TVM
 ============================================
 **Author**: `Zhi Chen <https://github.com/zhiics>`_, `Cody Hao Yu <https:://github.com/comaniac>`_
 
 As the hardware devices targeted by deep learning workloads keep increasing, the required knowledge
-for users to achieve high performance on vary devices keeps increasing as well. To free data
+for users to achieve high performance on various devices keeps increasing as well. To free data
 scientists from worrying about the performance when developing a new model, hardware vendors either
 provide libraries such as MKLDNN or cuDNN with many commonly used deep learning operators,
 or provide frameworks such as TensorRT to let users describle their models in a certain way to
 achieve high performance. However, users have to learn a new programming interface when they
-attempt to work on a new libaray or device. As a result, the demeand of a unified programming
+attempt to work on a new libaray or device. As a result, the demand of a unified programming
 interface becomes more and more important to 1) let all users and hardware vendors stand on the
-same page, and 2) provide a feasbile solution to allow a specialized hardware or library to only
+same page, and 2) provide a feasible solution to allow a specialized hardware or library to only
 support widely used operators with extremely high perofrmance, but fallback unsupported operators
 to general devices like CPU/GPU.
 
-In this tutorial, we introduce how a hardware vendor can easily implement a Relay backend to support
-a specialized hardware device/library. It mainly takes three steps: 1) define whether an operator is
-supported, 2) specify how to compile and serialize the supported operators, and 3) specify how to
-execute the compiled operators on a certain device. We will demonstrate how to add a new backend
-that uses GCC compiler to execute a subgraph of a model. Note that you will need to add the
+In this tutorial, we demonstrate how a hardware vendor can easily implement
+a Relay backend to support a specialized hardware device/library. It mainly
+takes three steps: 1) define whether an operator is supported under a given
+template, 2) specify how to compile and serialize the supported operators so
+that it can ingest TVM specific data format, e.g. NDArray, and 3) specify how
+to execute the compiled operators on a certain device. We will demonstrate how
+to add a new backend that uses open source compilers (e.g. GCC, LLVM, etc) or any
+proprietary compilers to execute a subgraph of a model without the exposure of
+the IP of customer's codegen tool chain. Note that you will need to add the
 specialized Relay backend to the TVM codebase and rebuild TVM for enabling.
 
 """
@@ -46,10 +50,13 @@ specialized Relay backend to the TVM codebase and rebuild TVM for enabling.
 ######################################################################
 # Define The Supported Operators
 # ------------------------------
-# The first step is to define which operators are supported by our backend.
-# We first create a new Python file at python/relay/backend/op/contrib/gcc/extern_op.py,
+# The first step is to define which operators are supported by your backend.
+# A templated is provided to ease vendor's effort to add the supported
+# operators.
+#
+# For example, We create a new Python file at python/relay/backend/op/contrib/gcc/extern_op.py,
 # and implement a set of boolean functions with corresponding operator names. A boolean
-# function should return `True` if we allow it to be executed by our backend; `False`
+# function should return `True` if we allow it to be executed by the given backend; `False`
 # otherwise.
 
 from __future__ import absolute_import
@@ -75,9 +82,21 @@ def multiply(attrs, args):
     return True
 
 ######################################################################
-# Note that since we include `attrs` and `args` into the function signature, we can
-# define more complicate rules. For example, we can only support conv2d with float32
-# data type or with kernel size 1x1.
+# Note that since we include `attrs` and `args` into the function signature, we
+# can define more complicated rules. For example, we can only support conv2d
+# with float32 data type or with kernel size 1x1. In addition, the vendors can
+# also check the attributes associated with a given operator to decide if it is
+# supported by checking the fields in `attrs`. In a even more complicated but
+# interesting scenario, we also allow developers to check the sequence of
+# operators through iterating on the `agrs`. However, this is only
+# unidirectional as only the inputs are visible.
+#
+# After annotating whether an operator can be executed on the given backend.
+# Users can directly invoke the partitioning pass to separate the graph into
+# multiple segments. The C++ backend implements a partitioning pass to fullfil
+# the task and creates subgraphs/sub-functions with *External* attribute,
+# indicating that this function will be handled by external codegen tool.
+# Therefore, Relay passes should skip optimizations on them.
 
 ######################################################################
 # Customize Subgraph Annotations
@@ -85,9 +104,9 @@ def multiply(attrs, args):
 # In addition to specifying a set of rules for supported operators, we can also implement
 # a Relay IR mutator to find the supported subgraphs, which may include multiple operators,
 # for the target backend. Here we implement an annotator that includes an entire Relay graph
-# to be offloaded. Specifically, we are going
-# to do two tasks: 1) insert `aubgraph_begin` after all input variables, and 2) insert
-# `subgraph_end` before the primary output. For example, given a Relay graph as follows:
+# to be offloaded. Specifically, we are going to do two tasks:
+# - insert `aubgraph_begin` after all input variables
+# - insert `subgraph_end` before the primary output. For example, given a Relay graph as follows:
 #       input_a
 #          |
 #         add    --- input_b
@@ -150,7 +169,8 @@ class WholeGraphAnnotator(ExprMutator):
         return new_call
 
 ######################################################################
-# Finally, we apply the annotator to our workload. Let's first build a Relay function:
+# Finally, we apply the annotator to our workload. Let's first build a Relay
+# function:
 
 input_a = relay.var('a', shape=(10, 10))
 input_b = relay.var('b', shape=(10, 10))
@@ -175,21 +195,22 @@ mod['main'] = WholeGraphAnnotator('gcc').visit(func)
 mod = relay.transform.PartitionGraph()(mod)
 
 ######################################################################
-# Accordingly, the IR is transformed to the following. We can see that the entire Relay graph
-# is enclosed to a function with `External="gcc"` attribute. This indicates that this function
-# will be offloaded to an external backend during the runtime.
+# Accordingly, the IR is transformed to the following. We can see that the
+# entire Relay graph is enclosed in a function with `External="gcc"` attribute.
+# It indicates that this function will be offloaded to an external backend
+# during the runtime.
 
 print(mod['main'])
 
 ######################################################################
 # Implement The Codegen
 # ---------------------
-# The second and the thrid step are implemented in C++ instead of Python.
+# The second and the third step are implemented in C++ instead of Python.
 # Specifically, we create src/relay/backend/contrib/gcc/codegen.cc and
 # implement the codegen and runtime dispatcher here. For the codegen,
 # we need to implement two functions: `CompileExternalLib()` and `Build()`.
-# `Build()` accepts a Relay subgraph and generate the library or device code
-# accordingly. In the GCC example, we implement a Relay IR visitor to generate
+# `Build()` accepts a Relay module or subgraph and generate the library or device
+# code accordingly. In the GCC example, we implement a Relay IR visitor to generate
 # C++ code for subgraphs.
 
 ######################################################################
@@ -197,17 +218,23 @@ print(mod['main'])
 # serialize an external library for the generated device code (C++ in this
 # example). The generated library/executable binary can either be materialized
 # to disk and load back during runtime, or stored in memory directly for
-# later usage.
+# later usage using whatever user defined mechanism. In the GCC case, the
+# stand system calls e.g. dlopen/dlsym or LoadLibrary/GetProcAddress are used
+# for Linux and Windows, respectively.
 
 ######################################################################
 # Implement The Runtime Dispather
 # -------------------------------
 # The last step is invoking the generated external library in runtime.
-# Specifically, we need to implement `GetFunction()` in codegen.cc.
-# The function takes a subgraph name and returns a `PackedFunc` that
-# executes the subgraph with runtime input data. If the subgraph is
-# compiled by `Build` in advance and the shared library or executable
-# binary is available, then we can invoke it here.
+# Specifically, we need to implement tvm runtime `Module` compatible
+# `GetFunction()` in codegen.cc. The function takes a subgraph name and returns
+# a `PackedFunc` that executes the subgraph with runtime input data. Note that
+# the runtime data in TVM is provided in the tvm `NDArray` format. It's
+# vendors' repsonsiblity to deserialize it into the format that they library
+# can ingest. For example, we unpack it and extract the raw pointers for
+# MKL-DNN. If the subgraph is compiled by `Build` in advance and the shared
+# library or executable binary is available, then we can invoke it here.
+#
 # `GetFunction()` will be invoked by Relay runtime, including interpreter,
 # graph runtime, and VM, meaning that this one implemtation works for all
 # kinds of Relay runtimes.
