7a1d98d2314884549bf8dd5feca27637d7269d16
Chien-Yu Lin <cyulin@cs.washington.edu>
2019-11-08 16:56:29 -0800

    fix outs when deal with sche for cuda sparse_dense

--- topi/python/topi/cuda/sparse.py
+++ topi/python/topi/cuda/sparse.py
@@ -6,9 +6,10 @@ from .injective import schedule_injective
 @generic.schedule_sparse_dense.register(["gpu", "cuda"])
 def _schedule_sparse_dense(outs):
     target = tvm.target.current_target()
+
+    outs = [outs] if isinstance(outs, tvm.tensor.Tensor) else outs
     if target.target_name == "cuda" and "cusparse" in target.libs:
         return generic.schedule_extern(outs)
 
     s = schedule_injective(outs)
-
     return s
