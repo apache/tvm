89a842745b3bf03a4aac199619d04fcbe181c37a
FrozenGene <wuzhaozju@gmail.com>
2019-11-10 20:45:51 +0800

    Pylint singleton-comparison issue

--- topi/python/topi/vision/rcnn/proposal.py
+++ topi/python/topi/vision/rcnn/proposal.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name, singleton-comparison
 """Proposal operator"""
 import math
 import tvm
