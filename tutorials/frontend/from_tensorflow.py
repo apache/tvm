e541c75863775f9011a658a36b86f084133bfbb7
Eric Platon <zaraki@gmx.com>
2019-11-13 00:52:24 +0100

    Fix the TF tutorial to run against TF2.0 and TF1.x (#4104)
    * WIP Run the TF tutorial on TF2
    
    * Remove debugger statement.
    
    * Complete the support for TF2.0's `resize`.
    
    TF2.0 adds a `half_pixel_centers` attribute to the `resize` function in
    the image API. This commit completes the hooks in Relay's TF frontend.
    
    At the point of this commit, no new test yet. Also, this commit
    addresses solely the `resize` change. Other commits address other
    changes in TF2.0.
    
    * Support TF2.0 in the tutorial by using the compat API.
    
    This looks cleaner than trying to detect the TF version.
    
    * Use the TF compat API, so as to support TF2.0.
    
    This is a direct change, relying on the compat API provided by the TF
    team.
    
    This code will last as long as the compat API exists, so a
    "proper" support for TF1.x and 2.x will require more work in some
    future.
    
    * Partial support for EXPLICIT padding introduced in TF2.0.
    
    Explicit padding is a special case in TF2.0 (see reference linked
    below). Some models are serialized with that mode, and break TF support
    in TVM.
    
    Support is *partial* as EXPLICIT falls back to set padding on the
    Relay op, which only supports 2 values. At some point, padding may need
    to be extended to support 4 values, but that is out of scope of this
    support commit.
    
    Reference on EXPLICIT padding: https://github.com/tensorflow/tensorflow/commit/ec81825aaf7e848d9f8ddffdf1e0d20aebe9172c#diff-1d1c0bb0a880f85b6164f71dbb2f446e
    
    * Guard on checking for optional TF2.0 attribute.
    
    * Do not expect Relay to implement TF-specific attributes.
    
    The `half_pixel_centers` attribute is a new feature in TF2.0. Earlier
    commits of mine mistakenly introduce them in the Relay API. This is
    probably not what Relay is expected to support, and the semantics of
    `half_pixel_centers` is unclear (to me, at least) at this point.
    
    * Remove unclear comment.
    
    CR https://github.com/dmlc/tvm/pull/4104#discussion_r338705742
    
    Addresses #4104
    
    * Changes after review.
    
    Complying without understanding the rationale for now.
    
    * Fix the arguments set mistakenly.
    
    An argument ignored for the wrong operation.

--- tutorials/frontend/from_tensorflow.py
+++ tutorials/frontend/from_tensorflow.py
@@ -89,14 +89,14 @@ label_path = download_testdata(label_map_url, label_map, module='data')
 # ------------
 # Creates tensorflow graph definition from protobuf file.
 
-with tf.gfile.FastGFile(model_path, 'rb') as f:
-    graph_def = tf.GraphDef()
+with tf.compat.v1.gfile.GFile(model_path, 'rb') as f:
+    graph_def = tf.compat.v1.GraphDef()
     graph_def.ParseFromString(f.read())
     graph = tf.import_graph_def(graph_def, name='')
     # Call the utility to import the graph definition into default graph.
     graph_def = tf_testing.ProcessGraphDefParam(graph_def)
     # Add shapes to the graph.
-    with tf.Session() as sess:
+    with tf.compat.v1.Session() as sess:
         graph_def = tf_testing.AddShapesToGraphDef(sess, 'softmax')
 
 ######################################################################
@@ -187,8 +187,8 @@ for node_id in top_k:
 def create_graph():
     """Creates a graph from saved GraphDef file and returns a saver."""
     # Creates graph from saved graph_def.pb.
-    with tf.gfile.FastGFile(model_path, 'rb') as f:
-        graph_def = tf.GraphDef()
+    with tf.compat.v1.gfile.GFile(model_path, 'rb') as f:
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
         graph = tf.import_graph_def(graph_def, name='')
         # Call the utility to import the graph definition into default graph.
@@ -206,14 +206,14 @@ def run_inference_on_image(image):
     -------
         Nothing
     """
-    if not tf.gfile.Exists(image):
+    if not tf.compat.v1.io.gfile.exists(image):
         tf.logging.fatal('File does not exist %s', image)
-    image_data = tf.gfile.FastGFile(image, 'rb').read()
+    image_data = tf.compat.v1.gfile.GFile(image, 'rb').read()
 
     # Creates graph from saved GraphDef.
     create_graph()
 
-    with tf.Session() as sess:
+    with tf.compat.v1.Session() as sess:
         softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
         predictions = sess.run(softmax_tensor,
                                {'DecodeJpeg/contents:0': image_data})
