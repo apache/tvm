6274a8e7bf50c00407274ef416facc9b8293fb80
Jian Weng <jian.weng@ucla.edu>
2019-11-14 22:33:12 -0800

    hmmmmmmm

--- topi/python/topi/cpp/vision/__init__.py
+++ topi/python/topi/cpp/vision/__init__.py
@@ -1,3 +1,4 @@
 from . import yolo
+from tvm._ffi.function import _init_api_prefix
 
 _init_api_prefix("topi.cpp.vision", "topi.vision")
--- topi/python/topi/cpp/vision/yolo.py
+++ topi/python/topi/cpp/vision/yolo.py
@@ -1,1 +1,3 @@
+from tvm._ffi.function import _init_api_prefix
+
 _init_api_prefix("topi.cpp.vision.yolo", "topi.vision.yolo")
