c14371d1341fc8eba189ac92f1e85982709a9148
Jian Weng <jian.weng@ucla.edu>
2019-11-14 23:17:26 -0800

    fix lint

--- topi/python/topi/cpp/vision/__init__.py
+++ topi/python/topi/cpp/vision/__init__.py
@@ -1,6 +1,7 @@
 """FFI for vision TOPI ops and schedules"""
 
-from . import yolo
 from tvm._ffi.function import _init_api_prefix
+
+from . import yolo
 
 _init_api_prefix("topi.cpp.vision", "topi.vision")
