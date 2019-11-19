f930b23659f22bf2a7b349dc03a208def3bd0f11
Animesh Jain <anijain@umich.edu>
2019-11-19 05:10:06 +0000

    [Relay] Convert Layout Pass.

--- src/relay/op/nn/convolution.cc
+++ src/relay/op/nn/convolution.cc
@@ -27,7 +27,7 @@
 #include <tvm/relay/attrs/nn.h>
 #include <vector>
 
-#include "../../pass/alter_op_layout.h"
+#include "../../pass/infer_layout_util.h"
 #include "../op_common.h"
 #include "convolution.h"
 
