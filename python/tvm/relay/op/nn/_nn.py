01c414e190302cd0c021257f8dc3920c7649be1b
FrozenGene <wuzhaozju@gmail.com>
2019-11-20 19:15:57 +0800

    diable no-else-return as other files

--- python/tvm/relay/op/nn/_nn.py
+++ python/tvm/relay/op/nn/_nn.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=invalid-name, unused-argument, too-many-arguments
+# pylint: disable=no-else-return, invalid-name, unused-argument, too-many-arguments
 """Backend compiler related feature registration"""
 from __future__ import absolute_import
 
