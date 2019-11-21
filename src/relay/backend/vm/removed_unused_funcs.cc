2672aad4feaca8b2a40af59ae766c0e704c4cc65
Zhi <5145158+zhiics@users.noreply.github.com>
2019-11-20 16:50:01 -0800

    [fix][pass] Save the function when it is used as a call arg (#4389)

--- src/relay/backend/vm/removed_unused_funcs.cc
+++ src/relay/backend/vm/removed_unused_funcs.cc
@@ -54,12 +54,8 @@ struct CallTracer : ExprVisitor {
       called_funcs_{},
       visiting_{} {}
 
-  void VisitExpr_(const CallNode* call_node) final {
-    Expr op = call_node->op;
-    for (auto param : call_node->args) {
-      VisitExpr(param);
-    }
-    if (auto func_node = op.as<FunctionNode>()) {
+  void CheckExpr(const Expr& expr) {
+    if (auto func_node = expr.as<FunctionNode>()) {
       auto func = GetRef<Function>(func_node);
       auto it = visiting_.find(func);
       if (it != visiting_.end()) {
@@ -67,7 +63,7 @@ struct CallTracer : ExprVisitor {
       }
       visiting_.insert(func);
       VisitExpr(func);
-    } else if (auto global = op.as<GlobalVarNode>()) {
+    } else if (auto global = expr.as<GlobalVarNode>()) {
       called_funcs_.insert(global->name_hint);
       auto func = module_->Lookup(global->name_hint);
       auto it = visiting_.find(func);
@@ -76,6 +72,15 @@ struct CallTracer : ExprVisitor {
       }
       visiting_.insert(func);
       VisitExpr(func);
+    } else {
+      VisitExpr(expr);
+    }
+  }
+
+  void VisitExpr_(const CallNode* call_node) final {
+    CheckExpr(call_node->op);
+    for (auto param : call_node->args) {
+      CheckExpr(param);
     }
   }
 
