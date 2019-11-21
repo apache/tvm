4263150bdf7b9d12fbdc385bd512db9d750f9275
Haichen Shen <shenhaichen@gmail.com>
2019-11-21 14:25:51 -0800

    address comments

--- include/tvm/runtime/vm.h
+++ include/tvm/runtime/vm.h
@@ -268,125 +268,142 @@ struct Instruction {
     } alloc_storage;
   };
 
-  /*! \brief Construct a return instruction.
-   *  \param return_reg The register containing the return value.
-   *  \return The return instruction.
-   * */
+  /*!
+   * \brief Construct a return instruction.
+   * \param return_reg The register containing the return value.
+   * \return The return instruction.
+   */
   static Instruction Ret(RegName return_reg);
-  /*! \brief Construct a fatal instruction.
-   *  \return The fatal instruction.
-   * */
+  /*!
+   * \brief Construct a fatal instruction.
+   * \return The fatal instruction.
+   */
   static Instruction Fatal();
-  /*! \brief Construct a invoke packed instruction.
-   *  \param packed_index The index of the packed function.
-   *  \param arity The arity of the function.
-   *  \param output_size The number of outputs of the packed function.
-   *  \param args The argument registers.
-   *  \return The invoke packed instruction.
+  /*!
+   * \brief Construct a invoke packed instruction.
+   * \param packed_index The index of the packed function.
+   * \param arity The arity of the function.
+   * \param output_size The number of outputs of the packed function.
+   * \param args The argument registers.
+   * \return The invoke packed instruction.
    */
   static Instruction InvokePacked(Index packed_index, Index arity, Index output_size,
                                   const std::vector<RegName>& args);
-  /*! \brief Construct an allocate tensor instruction with constant shape.
-   *  \param storage The storage to allocate out of.
-   *  \param shape The shape of the tensor.
-   *  \param dtype The dtype of the tensor.
-   *  \param dst The destination register.
-   *  \return The allocate tensor instruction.
+  /*!
+   * \brief Construct an allocate tensor instruction with constant shape.
+   * \param storage The storage to allocate out of.
+   * \param shape The shape of the tensor.
+   * \param dtype The dtype of the tensor.
+   * \param dst The destination register.
+   * \return The allocate tensor instruction.
    */
   static Instruction AllocTensor(RegName storage,
                                  const std::vector<int64_t>& shape, DLDataType dtype, RegName dst);
-  /*! \brief Construct an allocate tensor instruction with register.
-   *  \param storage The storage to allocate out of.
-   *  \param shape_register The register containing the shape.
-   *  \param dtype The dtype of the tensor.
-   *  \param dst The destination register.
-   *  \return The allocate tensor instruction.
+  /*!
+   * \brief Construct an allocate tensor instruction with register.
+   * \param storage The storage to allocate out of.
+   * \param shape_register The register containing the shape.
+   * \param dtype The dtype of the tensor.
+   * \param dst The destination register.
+   * \return The allocate tensor instruction.
    */
   static Instruction AllocTensorReg(RegName storage,
                                     RegName shape_register, DLDataType dtype, RegName dst);
-  /*! \brief Construct an allocate datatype instruction.
-   *  \param tag The datatype tag.
-   *  \param num_fields The number of fields for the datatype.
-   *  \param fields The registers containing the fields.
-   *  \param dst The register name of the destination.
-   *  \return The allocate instruction tensor.
+  /*!
+   * \brief Construct an allocate datatype instruction.
+   * \param tag The datatype tag.
+   * \param num_fields The number of fields for the datatype.
+   * \param fields The registers containing the fields.
+   * \param dst The register name of the destination.
+   * \return The allocate instruction tensor.
    */
   static Instruction AllocADT(Index tag, Index num_fields, const std::vector<RegName>& fields,
                               RegName dst);
-  /*! \brief Construct an allocate closure instruction.
-   *  \param func_index The index of the function table.
-   *  \param num_freevar The number of free variables.
-   *  \param free_vars The registers of the free variables.
-   *  \param dst The destination register.
-   *  \return The allocate closure instruction.
+  /*!
+   * \brief Construct an allocate closure instruction.
+   * \param func_index The index of the function table.
+   * \param num_freevar The number of free variables.
+   * \param free_vars The registers of the free variables.
+   * \param dst The destination register.
+   * \return The allocate closure instruction.
    */
   static Instruction AllocClosure(Index func_index, Index num_freevar,
                                   const std::vector<RegName>& free_vars, RegName dst);
-  /*! \brief Construct a get field instruction.
-   *  \param object_reg The register containing the object to project from.
-   *  \param field_index The field to read out of the object.
-   *  \param dst The destination register.
-   *  \return The get field instruction.
+  /*!
+   * \brief Construct a get field instruction.
+   * \param object_reg The register containing the object to project from.
+   * \param field_index The field to read out of the object.
+   * \param dst The destination register.
+   * \return The get field instruction.
    */
   static Instruction GetField(RegName object_reg, Index field_index, RegName dst);
-  /*! \brief Construct a get_tag instruction.
-   *  \param object_reg The register containing the object to project from.
-   *  \param dst The destination register.
-   *  \return The get_tag instruction.
+  /*!
+   * \brief Construct a get_tag instruction.
+   * \param object_reg The register containing the object to project from.
+   * \param dst The destination register.
+   * \return The get_tag instruction.
    */
   static Instruction GetTag(RegName object_reg, RegName dst);
-  /*! \brief Construct an if instruction.
-   *  \param test The register containing the test value.
-   *  \param target The register containing the target value.
-   *  \param true_branch The offset to the true branch.
-   *  \param false_branch The offset to the false branch.
-   *  \return The if instruction.
+  /*!
+   * \brief Construct an if instruction.
+   * \param test The register containing the test value.
+   * \param target The register containing the target value.
+   * \param true_branch The offset to the true branch.
+   * \param false_branch The offset to the false branch.
+   * \return The if instruction.
    */
   static Instruction If(RegName test, RegName target, Index true_branch, Index false_branch);
-  /*! \brief Construct a goto instruction.
-   *  \param pc_offset The offset from the current pc.
-   *  \return The goto instruction.
+  /*!
+   * \brief Construct a goto instruction.
+   * \param pc_offset The offset from the current pc.
+   * \return The goto instruction.
    */
   static Instruction Goto(Index pc_offset);
-  /*! \brief Construct an invoke instruction.
-   *  \param func_index The index of the function to invoke.
-   *  \param args The registers containing the arguments.
-   *  \param dst The destination register.
-   *  \return The invoke instruction.
+  /*!
+   * \brief Construct an invoke instruction.
+   * \param func_index The index of the function to invoke.
+   * \param args The registers containing the arguments.
+   * \param dst The destination register.
+   * \return The invoke instruction.
    */
   static Instruction Invoke(Index func_index, const std::vector<RegName>& args, RegName dst);
-  /*! \brief Construct an invoke closure instruction.
-   *  \param closure The register of the closure to invoke.
-   *  \param args The registers containing the arguments.
-   *  \param dst The destination register.
-   *  \return The invoke closure instruction.
+  /*!
+   * \brief Construct an invoke closure instruction.
+   * \param closure The register of the closure to invoke.
+   * \param args The registers containing the arguments.
+   * \param dst The destination register.
+   * \return The invoke closure instruction.
    */
   static Instruction InvokeClosure(RegName closure, const std::vector<RegName>& args, RegName dst);
-  /*! \brief Construct a load constant instruction.
-   *  \param const_index The index of the constant.
-   *  \param dst The destination register.
-   *  \return The load constant instruction.
+  /*!
+   * \brief Construct a load constant instruction.
+   * \param const_index The index of the constant.
+   * \param dst The destination register.
+   * \return The load constant instruction.
    */
   static Instruction LoadConst(Index const_index, RegName dst);
-  /*! \brief Construct a load_constanti instruction.
-   *  \param val The interger constant value.
-   *  \param dst The destination register.
-   *  \return The load_constanti instruction.
+  /*!
+   * \brief Construct a load_constanti instruction.
+   * \param val The interger constant value.
+   * \param dst The destination register.
+   * \return The load_constanti instruction.
    */
   static Instruction LoadConsti(Index val, RegName dst);
-  /*! \brief Construct a move instruction.
-   *  \param src The source register.
-   *  \param dst The destination register.
-   *  \return The move instruction.
+  /*!
+   * \brief Construct a move instruction.
+   * \param src The source register.
+   * \param dst The destination register.
+   * \return The move instruction.
    */
   static Instruction Move(RegName src, RegName dst);
 
-   /*! \brief Allocate a storage block.
-   *  \param size The size of the allocation.
-   *  \param alignment The allocation's alignment.
-   *  \param dtype_hint The data type hint for the allocator.
-   *  \param dst The destination to place the storage.
-   *  \return The alloc storage instruction.
+  /*!
+   * \brief Allocate a storage block.
+   * \param size The size of the allocation.
+   * \param alignment The allocation's alignment.
+   * \param dtype_hint The data type hint for the allocator.
+   * \param dst The destination to place the storage.
+   * \return The alloc storage instruction.
    */
   static Instruction AllocStorage(RegName size, RegName alignment,
                                   DLDataType dtype_hint, RegName dst);
@@ -399,7 +416,8 @@ struct Instruction {
   friend std::ostream& operator<<(std::ostream& os, const Instruction&);
 };
 
-/*! \brief A representation of a Relay function in the VM.
+/*!
+ * \brief A representation of a Relay function in the VM.
  *
  * Contains metadata about the compiled function, as
  * well as the compiled VM instructions.
@@ -427,7 +445,8 @@ struct VMFunction {
   friend std::ostream& operator<<(std::ostream& os, const VMFunction&);
 };
 
-/*! \brief A representation of a stack frame.
+/*!
+ * \brief A representation of a stack frame.
  *
  * A stack frame is a record containing the information needed
  * to restore the caller's virtual machine state after returning
@@ -458,7 +477,8 @@ struct VMFrame {
         caller_return_register(0) {}
 };
 
-/*! \brief The executable emitted by the VM compiler.
+/*!
+ * \brief The executable emitted by the VM compiler.
  *
  * The executable contains information (e.g. data in different memory regions)
  * to run in a virtual machine.
@@ -540,15 +560,27 @@ class Executable : public ModuleNode {
    */
   std::string Stats() const;
 
-  /*! \brief Get the `lib` module in an executable. Users have the flexibility to call
+  /*!
+   * \brief Get the `lib` module in an executable. Users have the flexibility to call
    * `export_library` from the frontend to save the library to disk.
    *
    * \return The runtime module that contains the hardwre dependent code.
    */
   runtime::Module GetLib() const { return lib; }
 
+  /*!
+   * \brief Get the arity of the VM Fucntion.
+   * \param func Function name.
+   * \return The number of parameters.
+   */
   int GetFunctionArity(std::string func) const;
 
+  /*!
+   * \brief Get the parameter name given the function name and parameter index.
+   * \param func Function name.
+   * \param index Parameter index.
+   * \return The parameter name.
+   */
   std::string GetFunctionParameterName(std::string func, uint32_t index) const;
 
   virtual ~Executable() {}
@@ -632,7 +664,8 @@ class Executable : public ModuleNode {
   std::string code_;
 };
 
-/*! \brief The virtual machine.
+/*!
+ * \brief The virtual machine.
  *
  * The virtual machine contains all the current execution state,
  * as well as the executable.
@@ -672,8 +705,9 @@ class VirtualMachine : public runtime::ModuleNode {
 
   VirtualMachine() : frames_(), func_index_(0), code_(nullptr), pc_(0), exec_(nullptr) {}
 
-  /*! \brief load the executable for the virtual machine.
-   *  \param exec The executable.
+  /*!
+   * \brief load the executable for the virtual machine.
+   * \param exec The executable.
    */
   virtual void LoadExecutable(const Executable* exec);
 
