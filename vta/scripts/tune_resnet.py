c6f8c23c349f3ef8bacceaf3203f7cc08e6529de
Thierry Moreau <moreau@uw.edu>
2019-11-26 19:21:56 -0800

    [VTA][HotFix] Relay->VTA quantization fix (#4433)
    * relay -> vta fix
    
    * setting optlevel to 3 for quantization to fold batchnorm

--- vta/scripts/tune_resnet.py
+++ vta/scripts/tune_resnet.py
@@ -125,9 +125,11 @@ def compile_network(opt, env, target):
     dtype_dict.update({k: str(v.dtype) for k, v in params.items()})
 
     # Perform quantization in Relay
-    with relay.quantize.qconfig(global_scale=8.0,
-                                skip_conv_layers=[0]):
-        relay_prog = relay.quantize.quantize(mod["main"], params=params)
+    # Note: We set opt_level to 3 in order to fold batch norm
+    with relay.build_config(opt_level=3):
+        with relay.quantize.qconfig(global_scale=8.0,
+                                    skip_conv_layers=[0]):
+            relay_prog = relay.quantize.quantize(mod["main"], params=params)
 
     # Perform graph packing and constant folding for VTA target
     if target.device_name == "vta":
