3a3d2ad62d832c48fe08c2fac8928fbabefbcb37
BenjaminTu <tu.benjamin1115@gmail.com>
2019-12-11 17:51:02 +0000

    insert header

--- vta/tests/hardware/metal_test/device.py
+++ vta/tests/hardware/metal_test/device.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 from ctypes import *
 from _macros_h import *
 from insn_lib import *
@@ -7,9 +24,9 @@ import numpy as np
 ACTIONS = {"1DLOAD":VTA_OPCODE_LOAD, "2DLOAD":VTA_OPCODE_LOAD, "ALU":None, "GEMM":None, 
  "1DSTORE":VTA_OPCODE_STORE, "2DSTORE":VTA_OPCODE_STORE, "FINISH":None}
 # dictionary that maps items to their corresponding ID
-ITEMS = {"UOP":VTA_MEM_ID_UOP, "INP":VTA_MEM_ID_INP, "WGT":VTA_MEM_ID_WGT, "ACC":VTA_MEM_ID_ACC, "OUT":VTA_MEM_ID_OUT}
-# dictionary that maps alu opcodes to their corresponding value
-ALU = {"MIN":VTA_ALU_OPCODE_MIN, "MAX":VTA_ALU_OPCODE_MAX, "ADD":VTA_ALU_OPCODE_ADD, "SHR":VTA_ALU_OPCODE_SHR, "EMPTY":None}
+ITEMS = {"UOP":VTA_MEM_ID_UOP, "INP":VTA_MEM_ID_INP, "WGT":VTA_MEM_ID_WGT, "ACC":VTA_MEM_ID_ACC, "OUT":VTA_MEM_ID_OUT,
+# alu opcodes to their corresponding value
+"MIN":VTA_ALU_OPCODE_MIN, "MAX":VTA_ALU_OPCODE_MAX, "ADD":VTA_ALU_OPCODE_ADD, "SHR":VTA_ALU_OPCODE_SHR, "EMPTY":None}
 
 class InsnStream:
 
@@ -140,7 +157,7 @@ class Device:
 				cmp_ind += 1
 
 			elif action == "ALU":
-				self.insn_buf[index] = get_alu_insn(ALU.get(item), vec_len, use_imm, imm_val, self.uop_compression, 
+				self.insn_buf[index] = get_alu_insn(ITEMS.get(item), vec_len, use_imm, imm_val, self.uop_compression, 
 				 0, 0, 0, 1)
 				cmp_ind += 1
 
