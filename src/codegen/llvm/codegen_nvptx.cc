fb12f35622770b2724aaf4e4747c53a3e71fce54
Dmitri Makarov <dmakarov@users.noreply.github.com>
2019-12-12 17:00:32 +0100

    Fix build for llvm newer than 9.0 (#4515)

--- src/codegen/llvm/codegen_nvptx.cc
+++ src/codegen/llvm/codegen_nvptx.cc
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -245,7 +245,7 @@ runtime::Module BuildNVPTX(Array<LoweredFunc> funcs, std::string target) {
       << "Cannot emit target CGFT_ObjectFile";
 #else
   CHECK(tm->addPassesToEmitFile(
-      pass, dest_ptx, nullptr, llvm::TargetMachine::CGFT_AssemblyFile) == 0)
+      pass, dest_ptx, nullptr, llvm::CGFT_AssemblyFile) == 0)
       << "Cannot emit target CGFT_ObjectFile";
 #endif
   pass.run(*module);
