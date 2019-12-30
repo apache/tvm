3ea528ffd71f1677342de0d16929b6f3077009fc
pankratz <pankratz@ualberta.ca>
2019-12-30 11:57:20 -0700

    Added regression tests for python and c++ to check that placeholder expressions in tensor shapes are flattened during the build of the module

--- tests/python/unittest/test_build_lower.py
+++ tests/python/unittest/test_build_lower.py
@@ -32,6 +32,35 @@ def test_lower_rfactor():
     s[BF].compute_at(s[B], s[B].op.reduce_axis[0])
     fapi = tvm.lower(s, [A, B])
 
+def test_placeholder_defined_output_shape():
+    dims = tvm.placeholder((1,3), dtype='int32',name="dims")
+    n = dims[0][0]
+    l = dims[0][1]
+    m = dims[0][2]
+
+    n = tvm.max(n - l, tvm.const(512,'int32'))
+
+    A = tvm.placeholder((n, l), name='A', dtype='float32')
+    B = tvm.placeholder((l, m), name='B', dtype='float32')
+
+    k = tvm.reduce_axis((0, l), name='k')
+    k2 = tvm.reduce_axis((0, l), name='k2')
+
+    C = tvm.compute((n, m), lambda i, j: tvm.sum(A[i, k] * B[k, j], axis=k), name='C')
+    D = tvm.compute(C.shape, lambda i, j: tvm.sum(A[i, k2] * B[k2, j], axis=k2), name='D')
+    E = tvm.compute(C.shape, lambda i, j: tvm.if_then_else(i==0,C(i,j),D(i,j)) , name='E')
+
+    sch = tvm.create_schedule(E.op)
+    stmt = tvm.lower(sch,[dims,A,B,E],simple_mode=True)
+
+    def assert_flat(op):
+        if not isinstance(op,tvm.expr.Call):
+            return
+        assert(op.call_type != tvm.expr.Call.Halide)
+    tvm.ir_pass.PostOrderVisit(stmt, assert_flat)
+
+    mod = tvm.build(sch,[dims,A,B,E])
+
 def test_dependent_output_shape():
     n, m, x = tvm.var('n'), tvm.var('m'), tvm.var('x')
     A = tvm.placeholder((n, m))
@@ -41,4 +70,5 @@ def test_dependent_output_shape():
 
 if __name__ == "__main__":
     test_lower_rfactor()
+    test_placeholder_defined_output_shape()
     test_dependent_output_shape()
