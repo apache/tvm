6d4d03a3238c372dedfdffac48e3882e7c355173
alexwong <11878166+alexwong@users.noreply.github.com>
2020-01-06 11:34:23 -0800

    Remove commented out section

--- python/tvm/relay/frontend/pytorch.py
+++ python/tvm/relay/frontend/pytorch.py
@@ -942,12 +942,6 @@ class Graph(object):
                 if "None" not in node_str and node_expr != "prim::Constant()" and \
                         "?" not in node_str:
                     node_value = ((node_str.split(' = ')[1]).split('value=')[1]).split(']')[0]
-                    # Quick fix for shufflenet, assume we always have shape w/ a Float tensor
-
-                    #if node_value == '<Tensor>':
-                    #    dims = (node_assign[1][6:-1]).split(', ')
-                    #    node_value = [int(dim) for dim in dims]
-
                 self._consts[node_name] = node_value
             elif node.kind() == "prim::ListConstruct":
                 list_shape = []
