1d4850d40a38d63e8715d062a530e659be71e7f4
Yao Wang <kevinthesunwy@gmail.com>
2020-01-05 19:50:09 -0800

    [Topi]Allow empty tensor for reshape, tile and strided_slice (#4618)
    * Support empty tensor
    
    * Fix schedule
    
    * Refactor
    
    * Minor fix
    
    * Fix pylint
    
    * Merge cpp and python is_empty_shape

--- topi/python/topi/util.py
+++ topi/python/topi/util.py
@@ -21,7 +21,7 @@ from numbers import Integral
 
 import tvm
 from tvm.api import layout, bijective_layout
-from . import tag
+from . import tag, cpp
 
 class InvalidShapeError(ValueError):
     """Invalid shape for a topi function. i.e. call winograd template for non-3x3 kernel)"""
@@ -417,3 +417,19 @@ def make_idx(b, e, s, z, i):
                           (b - i) // tvm.abs(s),
                           (i - b) // s)
     return tvm.if_then_else(tvm.expr.Or(bc, ec), 88, ss)
+
+
+def is_empty_shape(shape):
+    """Check whether an input shape has dimesion with size 0.
+
+    Parameter
+    ---------
+    shape : list of Expr
+      Input shape
+
+    Returns
+    -------
+    is_empty: bool
+      Whether input shape is empty or has dimesion with size 0.
+    """
+    return cpp.util.is_empty_shape(shape)
