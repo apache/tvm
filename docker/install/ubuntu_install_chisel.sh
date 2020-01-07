c59ead432fc52767efcd3e6eb28cd8249f7ac507
Liangfu Chen <liangfu.chen@icloud.com>
2020-01-07 16:27:41 +0800

    install sbt=1.1.1 with apt-get

--- docker/install/ubuntu_install_chisel.sh
+++ docker/install/ubuntu_install_chisel.sh
@@ -28,4 +28,6 @@ apt-get install apt-transport-https
 echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 apt-get update
-apt-get install -y verilator sbt
+# Note: The settings in vta/hardware/chisel/project/build.properties
+# file determines required sbt version.
+apt-get install -y verilator sbt=1.1.1
