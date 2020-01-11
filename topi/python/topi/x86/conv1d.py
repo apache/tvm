ac77dd065f4c7979c8aa883877c5981966b08343
Josh Fromm <jwfromm@uw.edu>
2020-01-10 16:36:33 -0800

    Added nwc schedule.

--- topi/python/topi/x86/conv1d.py
+++ topi/python/topi/x86/conv1d.py
@@ -21,6 +21,7 @@ import tvm
 from tvm import autotvm
 from .. import generic, tag
 
+
 @generic.schedule_conv1d_ncw.register(["cpu"])
 def schedule_conv1d_nwc(outs):
     """Create schedule for tensors"""
@@ -68,6 +69,60 @@ def schedule_conv1d_nwc(outs):
                 s[C].compute_at(s[output_op], w_out)
             else:
                 fused = s[C].fuse(n, c)
+                s[C].parallel(fused)
+
+        scheduled_ops.append(op)
+
+    traverse(output_op)
+    return s
+
+@generic.schedule_conv1d_nwc.register(["cpu"])
+def schedule_conv1d_nwc(outs):
+    """Create schedule for tensors"""
+    s = tvm.create_schedule([x.op for x in outs])
+    output_op = outs[0].op
+    scheduled_ops = []
+
+    def traverse(op):
+        """Traverse operators from computation graph"""
+        # inline all one-to-one-mapping operators except the last stage (output)
+        if tag.is_broadcast(op.tag):
+            if op not in s.outputs:
+                s[op].compute_inline()
+            else: # inject custom schedule
+                if len(op.axis) == 3: # schedule bias + bn + relu
+                    n, w, c = op.axis
+                    fused = s[op].fuse(n, w)
+                    s[op].parallel(fused)
+                    s[op].vectorize(c)
+            for tensor in op.input_tensors:
+                if isinstance(tensor.op, tvm.tensor.ComputeOp) and tensor.op not in scheduled_ops:
+                    traverse(tensor.op)
+
+        if 'conv1d_nwc' in op.tag:
+            conv = op.output(0)
+            kernel = op.input_tensors[1]
+            if isinstance(kernel.op, tvm.tensor.ComputeOp) and "dilate" in kernel.op.tag:
+                s[kernel].compute_inline()
+
+            data = op.input_tensors[0]
+            data_pad = None
+            if isinstance(data.op, tvm.tensor.ComputeOp) and "pad" in data.op.tag:
+                data_pad = data
+                data = data_pad.op.input_tensors[0]
+
+            n_pad, c_pad, w_pad = data_pad.op.axis
+            pad_fused = s[data_pad].fuse(n_pad, w_pad)
+            s[data_pad].parallel(pad_fused)
+            C = conv
+            n, w, c = C.op.axis
+            rc, rw = C.op.reduce_axis
+            n_out, w_out, c_out = output_op.axis
+            s[C].vectorize(c)
+            if op != output_op: # fuse bias + bn + relu into conv
+                s[C].compute_at(s[output_op], c_out)
+            else:
+                fused = s[C].fuse(n, w)
                 s[C].parallel(fused)
 
         scheduled_ops.append(op)
