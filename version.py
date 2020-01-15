d194929ae49ae2cdc7d6879c0a2c3b7addeaae22
tqchen <tqchenml@gmail.com>
2020-01-15 14:58:30 -0800

    [VERSION] Update mainline version to 0.7.dev0

--- version.py
+++ version.py
@@ -31,7 +31,7 @@ import re
 # current version
 # We use the version of the incoming release for code
 # that is under development
-__version__ = "0.6.0"
+__version__ = "0.7.dev0"
 
 # Implementations
 def update(file_name, pattern, repl):
