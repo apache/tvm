0ec9d8109ac6ec436f54d815f6a6a1c97f28c477
Liangfu Chen <liangfu.chen@harman.com>
2020-01-17 10:05:54 +0800

    split fsim/tsim script

--- tests/scripts/task_python_vta_fsim.sh
+++ tests/scripts/task_python_vta_fsim.sh
@@ -39,26 +39,3 @@ python3 -m pytest -v vta/tests/python/unittest
 # Run unit tests in functional/fast simulator
 echo "Running integration test in fsim..."
 python3 -m pytest -v vta/tests/python/integration
-
-# Check style of scala code
-echo "Check style of scala code..."
-make -C vta/hardware/chisel lint
-
-# Build VTA chisel design and verilator simulator
-echo "Building VTA chisel design..."
-make -C vta/hardware/chisel cleanall
-make -C vta/hardware/chisel USE_THREADS=0 lib
-
-# Set default VTA config to use TSIM cycle accurate sim
-cp vta/config/tsim_sample.json vta/config/vta_config.json
-
-# Run unit tests in cycle accurate simulator
-echo "Running unittest in tsim..."
-python3 -m pytest -v vta/tests/python/unittest
-
-# Run unit tests in cycle accurate simulator
-echo "Running integration test in tsim..."
-python3 -m pytest -v vta/tests/python/integration
-
-# Reset default fsim simulation
-cp vta/config/fsim_sample.json vta/config/vta_config.json
