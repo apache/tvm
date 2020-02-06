792feb1136f0125f45d9b6259eab7b924aa504a6
alexwong <11878166+alexwong@users.noreply.github.com>
2020-02-06 15:58:24 -0800

    Fix lint

--- python/tvm/relay/frontend/pytorch.py
+++ python/tvm/relay/frontend/pytorch.py
@@ -735,7 +735,7 @@ class Graph(object):
         # TODO: Temporary fix to remove prim::CallMethod node introduced in PT 1.4
         import torch
         from packaging import version
-        if (version.parse(torch.__version__) >= version.parse("1.4.0")):
+        if version.parse(torch.__version__) >= version.parse("1.4.0"):
             torch._C._jit_pass_inline(self._graph)
 
         self._inputs_r = {}
