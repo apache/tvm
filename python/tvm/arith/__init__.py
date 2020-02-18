d1e1ac49b37210334e543f6c4cd8813cbe80e26d
Tianqi Chen <tqchen@users.noreply.github.com>
2020-02-18 08:14:12 -0800

    [REFACTOR][PY] Establish tvm.arith (#4904)

--- python/tvm/arith/__init__.py
+++ python/tvm/arith/__init__.py
@@ -14,24 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import tvm
+"""Integer bound analysis, simplification and pattern detection."""
 
-def test_basic():
-    a = tvm.var("a")
-    b = tvm.var("b")
-    c = tvm.var("c")
-    m = tvm.arith.detect_clip_bound(tvm.all(a * 1 < b * 6,
-                                          a - 1 > 0), [a])
-    assert tvm.ir_pass.Simplify(m[1] - (b * 6 - 1)).value == 0
-    assert m[0].value == 2
-    m = tvm.arith.detect_clip_bound(tvm.all(a * 1 < b * 6,
-                                          a - 1 > 0), [a, b])
-    assert len(m) == 0
-    m = tvm.arith.detect_clip_bound(tvm.all(a + 10 * c <= 20,
-                                          b - 1 > 0), [a, b])
-    assert tvm.ir_pass.Simplify(m[1] - (20 - 10 * c)).value == 0
-    assert tvm.ir_pass.Simplify(m[2] - 2).value == 0
-
-
-if __name__ == "__main__":
-    test_basic()
+from .int_set import IntSet, IntervalSet
+from .analyzer import ModularSet, ConstIntBound, Analyzer
+from .bound import deduce_bound
+from .pattern import detect_linear_equation, detect_clip_bound
--- tests/python/unittest/test_arith_detect_linear_equation.py
+++ tests/python/unittest/test_arith_detect_linear_equation.py
@@ -19,50 +19,50 @@ import tvm
 def test_basic():
     a = tvm.var("a")
     b = tvm.var("b")
-    m = tvm.arith.DetectLinearEquation(a * 4 + b * 6 + 7, [a])
+    m = tvm.arith.detect_linear_equation(a * 4 + b * 6 + 7, [a])
     assert m[0].value == 4
     assert tvm.ir_pass.Simplify(m[1] - (b * 6 + 7)).value == 0
 
-    m = tvm.arith.DetectLinearEquation(a * 4 * (a+1) + b * 6 + 7, [a])
+    m = tvm.arith.detect_linear_equation(a * 4 * (a+1) + b * 6 + 7, [a])
     assert len(m) == 0
 
-    m = tvm.arith.DetectLinearEquation(a * 4  + (a+1) + b * 6 + 7, [a])
+    m = tvm.arith.detect_linear_equation(a * 4  + (a+1) + b * 6 + 7, [a])
     assert m[0].value == 5
     assert tvm.ir_pass.Simplify(m[1] - (b * 6 + 7 + 1)).value == 0
 
-    m = tvm.arith.DetectLinearEquation(a * b + 7, [a])
+    m = tvm.arith.detect_linear_equation(a * b + 7, [a])
     assert m[0] == b
 
-    m = tvm.arith.DetectLinearEquation(b * 7, [a])
+    m = tvm.arith.detect_linear_equation(b * 7, [a])
     assert m[0].value == 0
 
-    m = tvm.arith.DetectLinearEquation(b * 7, [])
+    m = tvm.arith.detect_linear_equation(b * 7, [])
     assert len(m) == 1
     assert tvm.ir_pass.Simplify(m[0] - b * 7).value == 0
 
 def test_multivariate():
     v = [tvm.var("v%d" % i) for i in range(4)]
     b = tvm.var("b")
-    m = tvm.arith.DetectLinearEquation(v[0] * (b + 4) + v[0] + v[1] * 8, v)
+    m = tvm.arith.detect_linear_equation(v[0] * (b + 4) + v[0] + v[1] * 8, v)
     assert(tvm.ir_pass.Equal(tvm.ir_pass.Simplify(m[0]), b + 5))
     assert(m[1].value == 8)
 
-    m = tvm.arith.DetectLinearEquation(v[0] * (b + 4) + v[0] + v[1] * 8 * v[2], v)
+    m = tvm.arith.detect_linear_equation(v[0] * (b + 4) + v[0] + v[1] * 8 * v[2], v)
     assert(len(m) == 0)
 
-    m = tvm.arith.DetectLinearEquation(v[0] * (b + 4) + v[0] + v[1] * 8 * v[1] + v[3], v)
+    m = tvm.arith.detect_linear_equation(v[0] * (b + 4) + v[0] + v[1] * 8 * v[1] + v[3], v)
     assert(len(m) == 0)
 
-    m = tvm.arith.DetectLinearEquation(((v[0] * b + v[1]) * 8 + v[2] + 1) * 2, v)
+    m = tvm.arith.detect_linear_equation(((v[0] * b + v[1]) * 8 + v[2] + 1) * 2, v)
     assert(m[1].value == 16)
     assert(m[2].value == 2)
     assert(m[len(m)-1].value == 2)
 
-    m = tvm.arith.DetectLinearEquation((v[0] - v[1]), [v[2]])
+    m = tvm.arith.detect_linear_equation((v[0] - v[1]), [v[2]])
     assert(m[0].value == 0)
     assert(tvm.ir_pass.Simplify(m[1] - (v[0] - v[1])).value == 0)
 
-    m = tvm.arith.DetectLinearEquation((v[0] - v[1]), [])
+    m = tvm.arith.detect_linear_equation((v[0] - v[1]), [])
     assert(len(m) == 1)
     assert(tvm.ir_pass.Simplify(m[0] - (v[0] - v[1])).value == 0)
 
