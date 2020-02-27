c998b99acdf498abb5ecb4fc7b7888468f9dfaaa
Jon <jonso@microsoft.com>
2020-02-27 11:28:29 -0800

    [Runtime] Fixed TVM_DLL_EXPORT_TYPED_FUNC to work on Windows

--- include/tvm/runtime/packed_func.h
+++ include/tvm/runtime/packed_func.h
@@ -919,6 +919,11 @@ class TVMRetValue : public TVMPODValue_ {
                          int* type_code,                                \
                          int num_args,                                  \
                          TVMValue* out_value,                           \
+                         int* out_type_code);                           \
+  int ExportName(TVMValue* args,                                        \
+                         int* type_code,                                \
+                         int num_args,                                  \
+                         TVMValue* out_value,                           \
                          int* out_type_code) {                          \
     try {                                                               \
       ::tvm::runtime::TVMRetValue rv;                                   \
