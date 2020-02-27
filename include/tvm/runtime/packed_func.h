6f3ba607d5f61e4f47207a82d5494806c1debdc8
Jon <jonso@microsoft.com>
2020-02-27 11:30:57 -0800

    fix style

--- include/tvm/runtime/packed_func.h
+++ include/tvm/runtime/packed_func.h
@@ -921,10 +921,10 @@ class TVMRetValue : public TVMPODValue_ {
                          TVMValue* out_value,                           \
                          int* out_type_code);                           \
   int ExportName(TVMValue* args,                                        \
-                         int* type_code,                                \
-                         int num_args,                                  \
-                         TVMValue* out_value,                           \
-                         int* out_type_code) {                          \
+                 int* type_code,                                        \
+                 int num_args,                                          \
+                 TVMValue* out_value,                                   \
+                 int* out_type_code) {                                  \
     try {                                                               \
       ::tvm::runtime::TVMRetValue rv;                                   \
       Function(::tvm::runtime::TVMArgs(                                 \
