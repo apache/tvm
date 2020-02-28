0b6f69a05f9474c0e9d0336784e5121c419df415
Jeremiah Morrill <jeremiah.morrill@gmail.com>
2020-02-28 13:22:14 -0800

    Added CopyFromBytes and CopyToBytes convenience methods.  Fixed typos.

--- include/tvm/runtime/ndarray.h
+++ include/tvm/runtime/ndarray.h
@@ -68,19 +68,37 @@ class NDArray : public ObjectRef {
   /*!
    * \brief Copy data content from another array.
    * \param other The source array to be copied from.
-   * \note The copy may happen asynchrously if it involves a GPU context.
+   * \note The copy may happen asynchronously if it involves a GPU context.
    *       TVMSynchronize is necessary.
    */
   inline void CopyFrom(const DLTensor* other);
   inline void CopyFrom(const NDArray& other);
   /*!
+ * \brief Copy data content from a byte buffer.
+ * \param data The source bytes to be copied from.
+ * \param nbytes The size of the buffer in bytes
+ *        Must be equal to the size of the NDArray.
+ * \note The copy may happen asynchronously if it involves a GPU context.
+ *       TVMSynchronize is necessary.
+ */
+  inline void CopyFromBytes(const void* data, size_t nbytes);
+  /*!
    * \brief Copy data content into another array.
    * \param other The source array to be copied from.
-   * \note The copy may happen asynchrously if it involves a GPU context.
+   * \note The copy may happen asynchronously if it involves a GPU context.
    *       TVMSynchronize is necessary.
    */
   inline void CopyTo(DLTensor* other) const;
   inline void CopyTo(const NDArray& other) const;
+  /*!
+   * \brief Copy data content into a byte buffer
+   * \param data The source bytes to be copied from.
+   * \param nbytes The size of the data buffer.
+   *        Must be equal to the size of the NDArray.
+   * \note The copy may happen asynchronously if it involves a GPU context.
+   *       TVMSynchronize is necessary.
+   */
+  inline void CopyToBytes(void* data, size_t nbytes) const;
   /*!
    * \brief Copy the data to another context.
    * \param ctx The target context.
@@ -182,7 +200,7 @@ class NDArray : public ObjectRef {
 
 /*!
  * \brief Save a DLTensor to stream
- * \param strm The outpu stream
+ * \param strm The output stream
  * \param tensor The tensor to be saved.
  */
 inline bool SaveDLTensor(dmlc::Stream* strm, const DLTensor* tensor);
@@ -205,7 +223,7 @@ class NDArray::ContainerBase {
   DLTensor dl_tensor;
 
   /*!
-   * \brief addtional context, reserved for recycling
+   * \brief additional context, reserved for recycling
    * \note We can attach additional content here
    *  which the current container depend on
    *  (e.g. reference to original memory when creating views).
@@ -306,6 +324,27 @@ inline void NDArray::CopyFrom(const NDArray& other) {
   CopyFromTo(&(other.get_mutable()->dl_tensor), &(get_mutable()->dl_tensor));
 }
 
+inline void NDArray::CopyFromBytes(const void* data, size_t nbytes) {
+  CHECK(data != nullptr);
+  CHECK(data_ != nullptr);
+  CHECK(nbytes > 0);
+
+  // Make a temporary copy of the dltensor
+  DLTensor input = get_mutable()->dl_tensor;
+
+  CHECK_EQ(nbytes, GetDataSize(input))
+      << "CopyFromBytes: The size must exactly match";
+
+  TVMContext cpu_ctx;
+  cpu_ctx.device_type = kDLCPU;
+  cpu_ctx.device_id = 0;
+  // Overwrite our temporary dltensor with a CPU context
+  input.ctx = cpu_ctx;
+  input.data = const_cast<void*>(data);
+  
+  CopyFrom(&input);
+}
+
 inline void NDArray::CopyTo(DLTensor* other) const {
   CHECK(data_ != nullptr);
   CopyFromTo(&(get_mutable()->dl_tensor), other);
@@ -315,6 +354,26 @@ inline void NDArray::CopyTo(const NDArray& other) const {
   CHECK(data_ != nullptr);
   CHECK(other.data_ != nullptr);
   CopyFromTo(&(get_mutable()->dl_tensor), &(other.get_mutable()->dl_tensor));
+}
+
+inline void NDArray::CopyToBytes(void* data, size_t nbytes) const {
+  CHECK(data != nullptr);
+  CHECK(data_ != nullptr);
+
+  // Make a temporary copy of the dltensor
+  DLTensor output = get_mutable()->dl_tensor;
+  
+  CHECK_EQ(nbytes, GetDataSize(output))
+    << "CopyToBytes: The size must exactly match";
+  
+  TVMContext cpu_ctx;
+  cpu_ctx.device_type = kDLCPU;
+  cpu_ctx.device_id = 0;
+  // Overwrite our temporary dltensor with a CPU context
+  output.ctx = cpu_ctx;
+  output.data = data;
+  
+  CopyTo(&output);
 }
 
 inline NDArray NDArray::CopyTo(const DLContext& ctx) const {
