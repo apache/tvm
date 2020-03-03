465dbe9370214703a89993d8f1b644b44a12c9de
maheshambule <mahesh_ambule@persistent.com>
2020-03-03 23:01:28 +0530

    isfinit expr

--- include/tvm/tir/expr.h
+++ include/tvm/tir/expr.h
@@ -829,6 +829,7 @@ class CallNode : public PrimExprNode {
   static constexpr const char* glsl_texture_store = "glsl_texture_store";
   static constexpr const char* prefetch = "prefetch";
   static constexpr const char* isnan = "isnan";
+  static constexpr const char* isfinite = "isfinite";
 
   /*! \brief Vectorizable intrinsic list. */
   static const char* vectorizable_intrinsics[];
