29908b1e131539d21c24689754f165b891064f60
maheshambule <mahesh_ambule@persistent.com>
2020-03-03 23:05:29 +0530

    relay register isfinite

--- src/relay/op/tensor/unary.cc
+++ src/relay/op/tensor/unary.cc
@@ -404,5 +404,14 @@ ElemwiseArbitraryLayout)
 .set_support_level(10)
 .set_attr<FTVMCompute>("FTVMCompute", NdarraySizeCompute);
 
+RELAY_REGISTER_UNARY_OP("isfinite")
+.describe(R"code(Returns the finiteness of input, computed element-wise.
+.. math::
+   isfinite(x)
+)code" TVM_ADD_FILELINE)
+.set_support_level(1)
+.add_type_rel("IdentityCompRel", IdentityCompRel)
+.set_attr<FTVMCompute>("FTVMCompute", RELAY_UNARY_COMPUTE(topi::isfinite));
+
 }  // namespace relay
 }  // namespace tvm
