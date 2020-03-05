945018d2a6f0ec724bfe807fa6b3c656530e1ded
Liangfu Chen <liangfu.chen@harman.com>
2020-03-05 14:24:24 +0800

    merge into apps/bundle_deploy directory
    Change-Id: I51904db81b8589e65d107d8ca77b47452e3812b5

--- apps/bundle_deploy/build_model.py
+++ apps/bundle_deploy/build_model.py
@@ -21,15 +21,9 @@ import os
 from tvm import relay
 import tvm
 import logging
+import json
 
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--out-dir', default='.')
-    opts = parser.parse_args()
-
+def build_module(opts):
     dshape = (1, 3, 224, 224)
     from mxnet.gluon.model_zoo.vision import get_model
     block = get_model('mobilenet0.25', pretrained=True)
@@ -52,6 +46,37 @@ def main():
     with open(os.path.join(build_dir, 'params.bin'), 'wb') as f_params:
         f_params.write(relay.save_param_dict(params))
 
+def build_inputs(opts):
+    from tvm.contrib import download
+    from PIL import Image
+    import numpy as np
+
+    build_dir = os.path.abspath(opts.out_dir)
+
+    # Download test image
+    image_url = 'https://homes.cs.washington.edu/~moreau/media/vta/cat.jpg'
+    image_fn = os.path.join(build_dir, "cat.png")
+    download.download(image_url, image_fn)
+    image = Image.open(image_fn).resize((224, 224))
+
+    def transform_image(image):
+        image = np.array(image) - np.array([123., 117., 104.])
+        image /= np.array([58.395, 57.12, 57.375])
+        image = image.transpose((2, 0, 1))
+        image = image[np.newaxis, :]
+        return image
+
+    x = transform_image(image)
+    print('x', x.shape)
+    with open(os.path.join(build_dir, "cat.bin"), "wb") as fp:
+        fp.write(x.astype(np.float32).tobytes())
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--out-dir', default='.')
+    opts = parser.parse_args()
+
+    build_module(opts)
+    build_inputs(opts)
