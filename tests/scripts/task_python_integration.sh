0ee43ca4deb0ab2e78b2e55143825c58bfa5bcc5
Liangfu Chen <liangfu.chen@harman.com>
2020-03-05 16:10:41 +0800

    fix ci testing
    Change-Id: Ic2e82fb3051b6c254ef32a964f976b61e3e5fe4d

--- tests/scripts/task_python_integration.sh
+++ tests/scripts/task_python_integration.sh
@@ -19,7 +19,7 @@
 set -e
 set -u
 
-export PYTHONPATH=python:topi/python:apps/extension/python
+export PYTHONPATH=`pwd`/python:`pwd`/topi/python:`pwd`/apps/extension/python
 export LD_LIBRARY_PATH="build:${LD_LIBRARY_PATH:-}"
 export TVM_BIND_THREADS=0
 export TVM_NUM_THREADS=2
