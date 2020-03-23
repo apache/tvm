318bb7abbfd6d412c2e2294797eb388c2f242183
Leyuan Wang <laurawly@gmail.com>
2020-03-23 00:08:17 +0000

    fix lint

--- src/runtime/contrib/thrust/thrust.cu
+++ src/runtime/contrib/thrust/thrust.cu
@@ -40,7 +40,7 @@ void thrust_sort(DLTensor* input,
                  DLTensor* out_values,
                  DLTensor* out_indices,
                  bool is_ascend,
-                 F &get_sort_len) {
+                 const F &get_sort_len) {
   thrust::device_ptr<DataType> data_ptr(static_cast<DataType *>(input->data));
   thrust::device_ptr<DataType> values_ptr(static_cast<DataType *>(out_values->data));
   thrust::device_ptr<IndicesType> indices_ptr(static_cast<IndicesType *>(out_indices->data));
@@ -72,7 +72,7 @@ void thrust_sort_common(DLTensor* input,
                         DLTensor* values_out,
                         DLTensor* indices_out,
                         bool is_ascend,
-                        F &get_sort_len,
+                        const F &get_sort_len,
                         std::string data_dtype,
                         std::string out_dtype) {
   if (data_dtype == "float32") {
@@ -142,7 +142,8 @@ TVM_REGISTER_GLOBAL("tvm.contrib.thrust.sort_nms")
 
   thrust::device_ptr<int> valid_count_ptr(static_cast<int *>(valid_count->data));
   auto get_sort_len = [&valid_count_ptr](int i) { return valid_count_ptr[i]; };
-  thrust_sort_common(input, values_out, indices_out, is_ascend, get_sort_len, data_dtype, out_dtype);
+  thrust_sort_common(input, values_out, indices_out, is_ascend, get_sort_len,
+                     data_dtype, out_dtype);
 });
 
 
@@ -159,7 +160,8 @@ TVM_REGISTER_GLOBAL("tvm.contrib.thrust.sort")
 
   int n_values = input->shape[input->ndim - 1];
   auto get_sort_len = [=](int i) { return n_values; };
-  thrust_sort_common(input, values_out, indices_out, is_ascend, get_sort_len, data_dtype, out_dtype);
+  thrust_sort_common(input, values_out, indices_out, is_ascend, get_sort_len,
+                     data_dtype, out_dtype);
 });
 }  // namespace contrib
 }  // namespace tvm
