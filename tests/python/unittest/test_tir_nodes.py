fb5fbc29d30752266d42886621d3bbd0571fe2f9
pankratz <35379668+dpankratz@users.noreply.github.com>
2020-03-23 09:47:29 -0600

    [Bugfix] Fixed bug where shifting by out-of-bounds value results in no compute code being emitted. (#5115)
    * Fixed bug where shifting by out-of-bounds RHS values results in LLVM to codegen nothing. Added regression testcase
    
    * Updated testcase to be more precise.
    
    * Fixed testcase

--- tests/python/unittest/test_tir_nodes.py
+++ tests/python/unittest/test_tir_nodes.py
@@ -207,6 +207,23 @@ def test_float_bitwise():
         pass
 
 
+def test_shift_bounds():
+    x = te.var('x')
+    for test in [lambda lhs, rhs : lhs << rhs,
+                    lambda lhs, rhs : lhs >> rhs]:
+        #negative case
+        for testcase in [(x,-1), (x,32)]:
+            try:
+                test(*testcase)
+                assert False
+            except tvm.TVMError:
+                pass
+
+        #positive case
+        for testcase in [(x,0), (x,16), (x,31)]:
+            test(*testcase)
+
+
 def test_divide_by_zero():
     for test in [lambda lhs, rhs : tvm.tir.floormod(lhs,rhs),
                     lambda lhs, rhs : tvm.tir.floordiv(lhs,rhs),
@@ -293,6 +310,7 @@ if __name__ == "__main__":
     test_all()
     test_bitwise()
     test_float_bitwise()
+    test_shift_bounds()
     test_divide_by_zero()
     test_isnan()
     test_equality()
