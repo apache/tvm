ed55f23dff46a4cfe7912d970282dc9256d3787b
tqchen <tianqi.tchen@gmail.com>
2020-03-30 12:18:16 -0700

    [CI] Improve the docs generation script

--- tests/scripts/task_python_docs.sh
+++ tests/scripts/task_python_docs.sh
@@ -20,8 +20,8 @@ set -e
 set -u
 
 # cleanup old states
+rm -rf docs/_build/html
 mkdir -p docs/_build/html
-rm -rf docs/_build/html/*
 rm -rf docs/gen_modules
 rm -rf docs/doxygen
 
@@ -31,24 +31,30 @@ rm -rf docs/vta/tutorials
 
 # cleanup stale log files
 find . -type f -path "*.log" | xargs rm -f
-
-# C++ doc
-make doc
-rm -f docs/doxygen/html/*.map docs/doxygen/html/*.md5
-mv docs/doxygen/html docs/_build/html/doxygen
-
-# JS doc
-jsdoc -c web/.jsdoc_conf.json web/tvm_runtime.js web/README.md
-mv out docs/_build/html/jsdoc
-
-# Java doc
-make javadoc
-mv jvm/core/target/site/apidocs docs/_build/html/javadoc
-
 find . -type f -path "*.pyc" | xargs rm -f
 
 cd docs
 PYTHONPATH=`pwd`/../python make html
-cd _build/html
-tar czf docs.tgz *
-mv docs.tgz ../../../
+cd ..
+
+rm -rf _docs
+mv docs/_build/html _docs
+
+# C++ doc
+make doc
+rm -f docs/doxygen/html/*.map docs/doxygen/html/*.md5
+mv docs/doxygen/html _docs/doxygen
+
+# JS doc
+jsdoc -c web/.jsdoc_conf.json web/tvm_runtime.js web/README.md
+mv out _docs/jsdoc
+
+# Java doc
+make javadoc
+mv jvm/core/target/site/apidocs _docs/javadoc
+
+echo "Start creating the docs tarball.."
+# make the tarball
+tar -C _docs -czvf docs.tgz .
+echo "Finish creating the docs tarball"
+du -h docs.tgz
