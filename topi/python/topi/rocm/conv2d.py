1e7ccc0d7f5b54f03e5c89be6694c9069c60c46c
Thomas Viehmann <tv.code@beamnet.de>
2020-03-31 00:48:17 +0200

    rocm: fix miopen convolutions (#5179)
    * fix miopen convolutions
    
    * fix overly long lines

--- topi/python/topi/rocm/conv2d.py
+++ topi/python/topi/rocm/conv2d.py
@@ -24,7 +24,8 @@ from ..util import get_const_tuple
 from ..nn.util import get_pad_tuple
 
 @autotvm.register_topi_compute("conv2d_nchw_miopen.rocm")
-def conv2d_nchw_miopen(cfg, data, kernel, strides, padding, dilation, out_dtype='float32'):
+def conv2d_nchw_miopen(cfg, data, kernel, strides, padding, dilation,
+                       layout='NCHW', out_dtype='float32'):
     """Conv2D operator for rocm backend.
 
     Parameters
@@ -57,6 +58,8 @@ def conv2d_nchw_miopen(cfg, data, kernel, strides, padding, dilation, out_dtype=
 
     CO, CI, KH, KW = get_const_tuple(kernel.shape)
     N, _, H, W = get_const_tuple(data.shape)
+
+    assert layout == 'NCHW'
 
     # handle dilation
     stride_h, stride_w = (strides, strides) if isinstance(strides, int) else strides
