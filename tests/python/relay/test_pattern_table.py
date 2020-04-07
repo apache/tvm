6d691f646996f43754f32050a4088562593ef250
Matthew Barrett <matthew.barrett@arm.com>
2020-04-07 11:59:05 +0100

    [RELAY][BYOC] Register pattern tables from external codegens
    This adds utility functions to support registering
    and retrieving pattern tables used by MergeComposite for
    external codegens.
    
    Change-Id: I5be165a321440e48b15ff6aff4970e0c67496aaa

--- tests/python/relay/test_pattern_table.py
+++ tests/python/relay/test_pattern_table.py
@@ -14,8 +14,32 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=wildcard-import
-"""Contrib modules."""
-from .register import get_pattern_table, register_pattern_table
+"""Unit test for pattern table registry (BYOC)."""
+from tvm.relay.op.contrib import get_pattern_table, register_pattern_table
+from tvm import relay
 
-from .dnnl import *
+
+@register_pattern_table("test_pattern_table")
+def pattern_table():
+    def _make_add_relu_pattern():
+        x = relay.var('x')
+        y = relay.var('y')
+        add_node = relay.add(x, y)
+        r = relay.nn.relu(add_node)
+        return r
+
+    def _check_add_relu_pattern():
+        return True
+
+    return [
+        ("test_pattern_table.add_relu", _make_add_relu_pattern(), _check_add_relu_pattern)
+    ]
+
+
+def test_retrieve_pattern_table():
+    table = get_pattern_table("test_pattern_table")
+    assert table[0][0] == "test_pattern_table.add_relu"
+
+
+if __name__ == "__main__":
+    test_retrieve_pattern_table()
