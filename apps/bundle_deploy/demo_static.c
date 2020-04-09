c719b2324ce82abc860c0a90f45d394acaddd714
tqchen <tqchen@octoml.ai>
2020-04-09 09:54:13 -0700

    Fix demo_static

--- apps/bundle_deploy/demo_static.c
+++ apps/bundle_deploy/demo_static.c
@@ -22,6 +22,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <stdlib.h>
 #include <float.h>
 
 #include "bundle.h"
@@ -56,7 +57,7 @@ int main(int argc, char **argv) {
   DLDataType dtype = {kDLFloat, 32, 1};
   input.dtype = dtype;
   int64_t shape [4] = {1, 3, 224, 224};
-  input.shape = &shape;
+  input.shape = shape;
   input.strides = NULL;
   input.byte_offset = 0;
 
@@ -74,8 +75,8 @@ int main(int argc, char **argv) {
   output.ndim = 2;
   DLDataType out_dtype = {kDLFloat, 32, 1};
   output.dtype = out_dtype;
-  int64_t out_shape [2] = {1, OUTPUT_LEN};
-  output.shape = &out_shape;
+  int64_t out_shape[2] = {1, OUTPUT_LEN};
+  output.shape = out_shape;
   output.strides = NULL;
   output.byte_offset = 0;
 
@@ -103,6 +104,6 @@ int main(int argc, char **argv) {
          (t3.tv_sec-t2.tv_sec)*1000000 + (t3.tv_usec-t2.tv_usec)/1000.f,
          (t4.tv_sec-t3.tv_sec)*1000000 + (t4.tv_usec-t3.tv_usec)/1000.f,
          (t5.tv_sec-t4.tv_sec)*1000000 + (t5.tv_usec-t4.tv_usec)/1000.f);
-  
+
   return 0;
 }
