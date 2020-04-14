686c43cd01b0307d92ef5daa6b32e9736e5b9369
Matthew Brookhart <matthewbrookhart@gmail.com>
2020-04-14 09:29:31 -0700

    document python API

--- python/tvm/relay/df_pattern/__init__.py
+++ python/tvm/relay/df_pattern/__init__.py
@@ -21,6 +21,8 @@ from ...runtime import Object
 from ... import _ffi as tvm_ffi
 from ..op import get
 from . import _ffi as ffi
+from tvm.relay import Expr
+
 
 def register_df_node(type_key=None):
     """Register a Relay node type.
@@ -35,12 +37,11 @@ def register_df_node(type_key=None):
             "relay.df_pattern." + type_key.__name__)(type_key)
     return tvm_ffi.register_object(type_key)
 
-class DFPattern(Node):
-    """Base class of all primitive expressions.
 
-    PrimExpr is used in the low-level code
-    optimizations and integer analysis.
+class DFPattern(Node):
+    """Base class of all Patterns.
     """
+
     def __call__(self, *args):
         return CallPattern(self, list(args))
 
@@ -59,12 +60,153 @@ class DFPattern(Node):
     def __truediv__(self, other):
         return is_op("divide")(self, other)
 
-    def has_attr(self, attr_name, attr_value):
-        attrs = make_node("DictAttrs", **{attr_name:attr_value})
+    def has_attr(self, attr_name: str, attr_value):
+        """
+        Add an attribute constraint to this pattern
+
+        Parameters
+        ----------
+        attr_name: str
+            The name of the attribute to match
+        attr_value: Any
+            The value of the attribute to match
+        """
+        attrs = make_node("DictAttrs", **{attr_name: attr_value})
         return AttrPattern(self, attrs)
 
-    def match(self, expr):
+    def has_type(self, ttype):
+        """
+        Add a type constraint to this pattern
+
+        Parameters
+        ----------
+        ttype: tvm.relay.Type
+            The type to match
+        """
+        return has_type(ttype, self)
+
+    def match(self, expr: Expr) -> bool:
+        """
+        Match this pattern to an expression
+
+        Parameters
+        ----------
+        expr : tvm.relay.Expr
+            The expression to match.
+        """
         return match(self, expr)
+
+    def dominates(self, parent, path=None):
+        """
+        Create a dominator for this partern
+
+        Parameters
+        ----------
+        parent: tvm.relay.df_pattern.DFPattern
+            The parent pattern this pattern dominates.
+        path: tvm.relay.df_pattern.DFPattern
+            The fuzzy path pattern.
+        """
+        if path == None:
+            path = wildcard()
+        return DominatorPattern(parent, path, self)
+
+
+def is_input(name: str = "") -> DFPattern:
+    """
+    Syntatic sugar for creating an optionally named VarPattern
+
+    Parameters
+    ----------
+    name: str
+        The name of the input pattern to match
+    """
+    return VarPattern(name)
+
+
+def is_op(op_name: str) -> DFPattern:
+    """
+    Syntatic sugar for creating an operator ExprPattern
+
+    Parameters
+    ----------
+    op_name: String
+        The name of the relay op
+    """
+    op = get(op_name)
+    return ExprPattern(op)
+
+
+def wildcard() -> DFPattern:
+    """
+    Syntatic sugar for creating a WildcardPattern
+    """
+    return WildcardPattern()
+
+
+def has_type(ttype, pattern: DFPattern = None) -> DFPattern:
+    """
+    Syntatic sugar for creating a TypePattern
+
+    Parameters
+    ----------
+    pattern: tvm.relay.df_pattern.DFPattern
+        The pattern that needs type annotation
+
+    ttype: tvm.relay.Type
+        The type to match
+    """
+    if pattern is None:
+        pattern = wildcard()
+    return TypePattern(pattern, ttype)
+
+
+def has_attr(attr_name: DFPattern, attr_value, pattern=None) -> DFPattern:
+    """
+    Syntatic sugar for creating an AttrPattern
+
+    Parameters
+    ----------
+    pattern: tvm.relay.df_pattern.DFPattern
+        The input pattern.
+
+    attrs: tvm.Attrs
+        The attributes to match
+    """
+    if pattern is None:
+        pattern = wildcard()
+    return pattern.has_attr(attr_name, attr_value)
+
+
+def dominates(parent: DFPattern, path: DFPattern, child: DFPattern) -> DFPattern:
+    """
+    Syntatic sugar for creating an Dominator pattern
+
+    Parameters
+    ----------
+    parent: tvm.relay.df_pattern.DFPattern
+        The parent pattern.
+    path: tvm.relay.df_pattern.DFPattern
+        The fuzzy path pattern.
+    child: tvm.relay.df_pattern.DFPattern
+        The child pattern.
+    """
+    return DominatorPattern(parent, path, child)
+
+
+def match(pattern: DFPattern, expr: Expr) -> bool:
+    """
+    Match a pattern to an expression
+
+    Parameters
+    ----------
+    pattern: tvm.relay.df_pattern.DFPattern
+        The input pattern.
+    expr : tvm.relay.Expr
+        The expression to match.
+    """
+    return ffi.match(pattern, expr)
+
 
 @register_df_node
 class ExprPattern(DFPattern):
@@ -75,8 +217,10 @@ class ExprPattern(DFPattern):
     expr : tvm.relay.Expr
         The expression to match.
     """
-    def __init__(self, expr):
+
+    def __init__(self, expr: Expr):
         self.__init_handle_by_constructor__(ffi.ExprPattern, expr)
+
 
 @register_df_node
 class VarPattern(DFPattern):
@@ -95,15 +239,10 @@ class VarPattern(DFPattern):
     type_annotation: tvm.relay.Type, optional
         The type annotation on the variable.
     """
-    def __init__(self, name_hint, type_annotation=None):
+
+    def __init__(self, name_hint: str, type_annotation=None):
         self.__init_handle_by_constructor__(
             ffi.VarPattern, name_hint, type_annotation)
-
-#     @property
-#     def name_hint(self):
-#         """Get name hint of the current var."""
-#         name = self.name
-#         return name
 
 
 @register_df_node
@@ -125,11 +264,13 @@ class CallPattern(DFPattern):
         The additional type arguments, this is only
         used in advanced usecase of template functions.
     """
+
     def __init__(self, op, args, attrs=None, type_args=None):
         if not type_args:
             type_args = []
         self.__init_handle_by_constructor__(
             ffi.CallPattern, op, args, attrs, type_args)
+
 
 @register_df_node
 class TuplePattern(DFPattern):
@@ -140,6 +281,7 @@ class TuplePattern(DFPattern):
     fields : List[tvm.relay.df_pattern.DFPattern]
         The fields in the tuple.
     """
+
     def __init__(self, fields):
         self.__init_handle_by_constructor__(ffi.TuplePattern, fields)
 
@@ -154,6 +296,7 @@ class TuplePattern(DFPattern):
     def astype(self, _):
         raise TypeError("astype cannot be used on TuplePattern")
 
+
 @register_df_node
 class TupleGetItemPattern(DFPattern):
     """Get index-th item from a TuplePattern.
@@ -166,9 +309,11 @@ class TupleGetItemPattern(DFPattern):
     index: int
         The index.
     """
-    def __init__(self, tuple_value, index):
+
+    def __init__(self, tuple_value: DFPattern, index):
         self.__init_handle_by_constructor__(
             ffi.TupleGetItemPattern, tuple_value, index)
+
 
 @register_df_node
 class AltPattern(DFPattern):
@@ -181,16 +326,20 @@ class AltPattern(DFPattern):
     right: tvm.relay.df_pattern.DFPattern
         One possible matching Pattern
     """
-    def __init__(self, tuple_value, index):
+
+    def __init__(self, left: DFPattern, right: DFPattern):
         self.__init_handle_by_constructor__(
-            ffi.AltPattern, tuple_value, index)
+            ffi.AltPattern, left, right)
+
 
 @register_df_node
 class WildcardPattern(DFPattern):
     """A pattern which matches anything.
     """
+
     def __init__(self):
         self.__init_handle_by_constructor__(ffi.WildcardPattern)
+
 
 @register_df_node
 class TypePattern(DFPattern):
@@ -199,80 +348,100 @@ class TypePattern(DFPattern):
     Parameters
     ----------
     pattern: tvm.relay.df_pattern.DFPattern
-        The input tuple expression.
+        The input pattern that needs type annotation
 
     ttype: tvm.relay.Type
         The type to match
     """
-    def __init__(self, pattern, ttype):
+
+    def __init__(self, pattern: DFPattern, ttype):
         self.__init_handle_by_constructor__(
             ffi.TypePattern, pattern, ttype)
 
+
 @register_df_node
 class AttrPattern(DFPattern):
-    """Get index-th item from a TuplePattern.
+    """Get match an expression with a certain attributes.
+    Currently only supports Op Attributes, not call Attributes
 
     Parameters
     ----------
     pattern: tvm.relay.df_pattern.DFPattern
-        The input tuple expression.
+        The input pattern.
 
     attrs: tvm.Attrs
         The attributes to match
     """
-    def __init__(self, pattern, attrs):
+
+    def __init__(self, pattern: DFPattern, attrs):
         self.__init_handle_by_constructor__(
             ffi.AttrPattern, pattern, attrs)
 
+
 @register_df_node
 class DominatorPattern(DFPattern):
-    """Get index-th item from a TuplePattern.
+    """Match a domination graph.
 
     Parameters
     ----------
     parent: tvm.relay.df_pattern.DFPattern
-        The root of domination
+        The parent, i.e., the single node which produces something,
+        later aggregated by the child
     path: tvm.relay.df_pattern.DFPattern
-        The fuzzy path pattern between parent and child
+        The fuzzy path pattern between parent and child,
+        typically matches elementwise ops
     child: tvm.relay.df_pattern.DFPattern
-        The last node in the domination
+        The last node in the domination which is the end user
+        for all nodes in the path and the parent
     """
-    def __init__(self, parent, path, child):
+
+    def __init__(self, parent: DFPattern, path: DFPattern, child: DFPattern):
         self.__init_handle_by_constructor__(
             ffi.DominatorPattern, parent, path, child)
 
+
 class DFPatternCallback(Object):
-    def __init__(self, pattern, callback):
+    """A Callback for Pattern Rewriting
+
+    When rewrite is called on this DFPatternCallback, the backend will find matches for the
+    pattern, call the callback function, and replace the matched expression with whatever
+    the callback returns.
+
+    Parameters
+    ----------
+    pattern: tvm.relay.df_pattern.DFPattern
+        The Pattern to match
+    callback: PackedFunc
+        The callback function.
+    """
+
+    def __init__(self, pattern: DFPattern, callback):
         self.__init_handle_by_constructor__(
             ffi.DFPatternCallback, pattern, callback)
 
-def is_input(name="") -> DFPattern:
-    return VarPattern(name)
+    def rewrite(self, expr: Expr) -> Expr:
+        """
+        Rewrite expression with this callback
 
-def is_op(op_name: str) -> DFPattern:
-    op = get(op_name)
-    return ExprPattern(op)
+        Parameters
+        ----------
+        expr : tvm.relay.Expr
+            The expression to rewrite.
+        """
+        return rewrite(self, expr)
 
-def wildcard() -> DFPattern:
-    return WildcardPattern()
 
-def has_type(ttype, pattern=None):
-    if pattern is None:
-        pattern = wildcard()
-    return TypePattern(pattern, ttype)
+def rewrite(callbacks, expr: Expr) -> Expr:
+    """
+    Rewrite expression with the given callbacks
 
-def has_attr(attr_name, attr_value, pattern=None):
-    if pattern is None:
-        pattern = wildcard()
-    return patter.has_attr(attr_name, attr_value)
-
-def match(pattern, expr):
-    return ffi.match(pattern, expr)
-
-def rewrite(callbacks, expr):
+    Parameters
+    ----------
+    callbacks: tvm.relay.df_pattern.DFPatternCallback
+        The input callback or list of callbacks.
+    expr : tvm.relay.Expr
+        The expression to rewrite.
+    """
     if isinstance(callbacks, DFPatternCallback):
         callbacks = [callbacks]
     return ffi.rewrite(callbacks, expr)
-
-def dominates(parent, path, child):
-    return DominatorPattern(parent, path, child)
