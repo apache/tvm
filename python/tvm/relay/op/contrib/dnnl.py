b739883bbbcdcfe491608f3b496021715ba1ec1b
mbaret <55580676+mbaret@users.noreply.github.com>
2020-04-16 09:36:38 +0100

    [RELAY][BYOC] Register pattern tables from external codegens (#5262)
    * [RELAY][BYOC] Register pattern tables from external codegens
    
    This adds utility functions to support registering
    and retrieving pattern tables used by MergeComposite for
    external codegens.
    
    Change-Id: I5be165a321440e48b15ff6aff4970e0c67496aaa
    
    * Updated DNNL tests to use pattern table mechanism
    
    * Removed pattern table standalone test
    
    * Change reg to _op

--- python/tvm/relay/op/contrib/dnnl.py
+++ python/tvm/relay/op/contrib/dnnl.py
@@ -32,7 +32,9 @@ it is supported. For example:
 - The other way is to implement the function by themselves to
 check the attributes of the op and decide if it should be offloaded to DNNL.
 """
-from ... import op as reg
+from ... import expr as _expr
+from ... import op as _op
+from .register import register_pattern_table
 
 
 def _register_external_op_helper(op_name, supported=True):
@@ -49,7 +51,7 @@ def _register_external_op_helper(op_name, supported=True):
     f : callable
         A function that returns if the operator is supported by DNNL.
     """
-    @reg.register(op_name, "target.dnnl")
+    @_op.register(op_name, "target.dnnl")
     def _func_wrapper(attrs, args):
         return supported
 
@@ -63,3 +65,23 @@ _register_external_op_helper("nn.relu")
 _register_external_op_helper("add")
 _register_external_op_helper("subtract")
 _register_external_op_helper("multiply")
+
+
+def make_pattern(with_bias=True):
+    data = _expr.var("data")
+    weight = _expr.var("weight")
+    bias = _expr.var("bias")
+    conv = _op.nn.conv2d(data, weight)
+    if with_bias:
+        conv_out = _op.add(conv, bias)
+    else:
+        conv_out = conv
+    return _op.nn.relu(conv_out)
+
+
+@register_pattern_table("dnnl")
+def pattern_table():
+    conv2d_bias_relu_pat = ("dnnl.conv2d_bias_relu", make_pattern(with_bias=True))
+    conv2d_relu_pat = ("dnnl.conv2d_relu", make_pattern(with_bias=False))
+    dnnl_patterns = [conv2d_bias_relu_pat, conv2d_relu_pat]
+    return dnnl_patterns
