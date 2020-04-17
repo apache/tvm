8a1f1f89b3177f6a49e940dfbc5f381098d2af08
Matthew Barrett <matthew.barrett@arm.com>
2020-04-17 16:16:37 +0100

    Fix lint

--- src/relay/qnn/op/concatenate.cc
+++ src/relay/qnn/op/concatenate.cc
@@ -156,7 +156,7 @@ Expr ConcatenateQnnCanonicalize(const Attrs& attrs, const Array<Expr>& new_args,
   // if the data is a CallNode, use TupleGetItems
   if (data->IsInstance<CallNode>()) {
     auto call = Downcast<Call>(data);
-    for (size_t i=0;i<tuple_type->fields.size();i++) {
+    for (size_t i=0; i < tuple_type->fields.size(); i++) {
       tuple_exprs.push_back(TupleGetItem(call, i));
     }
   }
