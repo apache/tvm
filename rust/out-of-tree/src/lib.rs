6a1534961184fd15f59a1933ce6dc874f84eb91c
Jared Roesch <jroesch@octoml.ai>
2020-04-20 17:41:44 -0700

    WIP

--- rust/out-of-tree/src/lib.rs
+++ rust/out-of-tree/src/lib.rs
@@ -2,16 +2,14 @@ use tvm::runtime::function::register;
 use tvm_sys::ffi;
 use std::ffi::{c_void};
 use std::os::raw::c_int;
-
+use tvm::runtime::ObjectRef;
 use tvm::transform::{PassInfo, create_func_pass};
 
 use tvm::runtime::{TVMArgValue, TVMRetValue};
 
-// fn the_pass(args: &[TVMArgValue]) -> anyhow::Result<TVMRetValue> {
-//     let pass_info = PassInfo::new();
-//     create_function_pass(
-// }
+type IRModule = ObjectRef;
 
+fn pass_function(Object
 fn the_pass(args: &[TVMArgValue]) -> anyhow::Result<TVMRetValue> {
     let pass_info = PassInfo::new(15, "RustPass".into(), vec![])?;
     let pass = create_func_pass(pass_func, pass_info)?;
