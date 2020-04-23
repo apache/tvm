9c12ec81206b948a180590e387b49d84d39ede35
Wei Pan <60017475+wpan11nv@users.noreply.github.com>
2020-04-23 11:59:03 -0700

    [cuDNN] Add cuDNN grouped convolutions support (#5319)
    Signed-off-by: Wei Pan <weip@nvidia.com>

--- tests/python/contrib/test_cudnn.py
+++ tests/python/contrib/test_cudnn.py
@@ -17,11 +17,11 @@
 import tvm
 from tvm import te
 from tvm.contrib import cudnn
+from tvm.contrib.nvcc import have_fp16
 import numpy as np
 import topi.testing
 
-
-def verify_conv2d(data_dtype, conv_dtype, tensor_format=0):
+def verify_conv2d(data_dtype, conv_dtype, tensor_format=0, groups=1):
     in_channel = 4
     out_channel = 16
     filter_h = 3
@@ -34,7 +34,7 @@ def verify_conv2d(data_dtype, conv_dtype, tensor_format=0):
     dilation_w = 1
     batch = 3
     height = 32
-    weight = 32
+    width = 32
 
     if not tvm.runtime.enabled("cuda"):
         print("skip because cuda is not enabled...")
@@ -42,12 +42,17 @@ def verify_conv2d(data_dtype, conv_dtype, tensor_format=0):
     if not tvm.get_global_func("tvm.contrib.cudnn.conv.output_shape", True):
         print("skip because cudnn is not enabled...")
         return
+    if data_dtype == "float16" and not have_fp16(tvm.gpu(0).compute_version):
+        print("Skip because gpu does not have fp16 support")
+        return
+
+    # schedule
     if tensor_format == 0:
-        xshape = [batch, in_channel, height, weight]
-        wshape = [out_channel, in_channel, filter_h, filter_w]
+        xshape = [batch, in_channel, height, width]
+        wshape = [out_channel, in_channel // groups, filter_h, filter_w]
     else:
-        xshape = [batch, height, weight, in_channel]
-        wshape = [out_channel, filter_h, filter_w, in_channel]
+        xshape = [batch, height, width, in_channel]
+        wshape = [out_channel, filter_h, filter_w, in_channel // groups]
 
     X = te.placeholder(xshape, name='X', dtype=data_dtype)
     W = te.placeholder(wshape, name='W', dtype=data_dtype)
@@ -59,39 +64,41 @@ def verify_conv2d(data_dtype, conv_dtype, tensor_format=0):
                            conv_mode=1,
                            tensor_format=tensor_format,
                            conv_dtype=conv_dtype,
-                           algo=-1)
+                           algo=-1,
+                           groups=groups)
     yshape = [x.value for x in Y.shape]
     s = te.create_schedule(Y.op)
 
-    def verify():
-        ctx = tvm.gpu(0)
-        f = tvm.build(s, [X, W, Y], "cuda", target_host="llvm", name="conv2d")
-        x_np = np.random.uniform(-1, 1, xshape).astype(data_dtype)
-        w_np = np.random.uniform(-1, 1, wshape).astype(data_dtype)
-        y_np = np.zeros(yshape).astype(data_dtype)
-        x = tvm.nd.array(x_np, ctx)
-        w = tvm.nd.array(w_np, ctx)
-        y = tvm.nd.array(y_np, ctx)
-        if tensor_format == 0:
-            c_np = topi.testing.conv2d_nchw_python(x_np, w_np, 1, 1)
-        elif tensor_format == 1:
-            wt = w_np.transpose((1, 2, 3, 0))  #OHWI => HWIO
-            c_np = topi.testing.conv2d_nhwc_python(x_np, wt, 1, 1)
+    # validation
+    ctx = tvm.gpu(0)
+    f = tvm.build(s, [X, W, Y], "cuda", target_host="llvm", name="conv2d")
+    x_np = np.random.uniform(-1, 1, xshape).astype(data_dtype)
+    w_np = np.random.uniform(-1, 1, wshape).astype(data_dtype)
+    y_np = np.zeros(yshape).astype(data_dtype)
+    x = tvm.nd.array(x_np, ctx)
+    w = tvm.nd.array(w_np, ctx)
+    y = tvm.nd.array(y_np, ctx)
+    if tensor_format == 0:
+        c_np = topi.testing.conv2d_nchw_python(x_np, w_np, 1, 1, groups=groups)
+    elif tensor_format == 1:
+        wt = w_np.transpose((1, 2, 3, 0))  #OHWI => HWIO
+        c_np = topi.testing.conv2d_nhwc_python(x_np, wt, 1, 1, groups=groups)
 
-        f(x, w, y)
-        tvm.testing.assert_allclose(y.asnumpy(), c_np, atol=3e-5, rtol=1e-3)
-
-    verify()
+    f(x, w, y)
+    tvm.testing.assert_allclose(y.asnumpy(), c_np, atol=1e-2, rtol=1e-2)
 
 def test_conv2d():
     verify_conv2d("float32", "float32", tensor_format=0)
     verify_conv2d("float16", "float32", tensor_format=1)
-    #Not pass accuracy test, need check
-    #verify_conv2d("float16", "float16", tensor_format=0)
+    verify_conv2d("float16", "float16", tensor_format=0)
     verify_conv2d("int8", "int32", tensor_format=1)
 
+    verify_conv2d("float32", "float32", tensor_format=0, groups=2)
+    verify_conv2d("float16", "float32", tensor_format=1, groups=2)
+    verify_conv2d("float16", "float16", tensor_format=0, groups=2)
+    verify_conv2d("int8", "int32", tensor_format=1, groups=2)
 
-def verify_conv3d(data_dtype, conv_dtype, tensor_format=0):
+def verify_conv3d(data_dtype, conv_dtype, tensor_format=0, groups=1):
     in_channel = 4
     out_channel = 16
     filter_d = 3
@@ -109,7 +116,7 @@ def verify_conv3d(data_dtype, conv_dtype, tensor_format=0):
     batch = 3
     depth = 32
     height = 32
-    weight = 32
+    width = 32
 
     if not tvm.runtime.enabled("cuda"):
         print("skip because cuda is not enabled...")
@@ -118,8 +125,9 @@ def verify_conv3d(data_dtype, conv_dtype, tensor_format=0):
         print("skip because cudnn is not enabled...")
         return
 
-    xshape = [batch, in_channel, depth, height, weight]
-    wshape = [out_channel, in_channel, filter_d, filter_h, filter_w]
+    # schedule
+    xshape = [batch, in_channel, depth, height, width]
+    wshape = [out_channel, in_channel // groups, filter_d, filter_h, filter_w]
 
     X = te.placeholder(xshape, name='X', dtype=data_dtype)
     W = te.placeholder(wshape, name='W', dtype=data_dtype)
@@ -131,33 +139,31 @@ def verify_conv3d(data_dtype, conv_dtype, tensor_format=0):
                            conv_mode=1,
                            tensor_format=tensor_format,
                            algo=-1,
-                           conv_dtype=conv_dtype)
+                           conv_dtype=conv_dtype,
+                           groups=groups)
     yshape = [x.value for x in Y.shape]
     s = te.create_schedule(Y.op)
 
-    def verify():
-        ctx = tvm.gpu(0)
-        f = tvm.build(s, [X, W, Y], "cuda", target_host="llvm", name="conv3d")
-        x_np = np.random.uniform(-1, 1, xshape).astype(data_dtype)
-        w_np = np.random.uniform(-1, 1, wshape).astype(data_dtype)
-        y_np = np.zeros(yshape).astype(data_dtype)
-        x = tvm.nd.array(x_np, ctx)
-        w = tvm.nd.array(w_np, ctx)
-        y = tvm.nd.array(y_np, ctx)
-        if tensor_format == 0:
-            c_np = topi.testing.conv3d_ncdhw_python(x_np, w_np, 1, 1)
-        else:
-            raise AssertionError("For now, conv3d tensor format only support: 0(NCHW)")
+    # validation
+    ctx = tvm.gpu(0)
+    f = tvm.build(s, [X, W, Y], "cuda", target_host="llvm", name="conv3d")
+    x_np = np.random.uniform(-1, 1, xshape).astype(data_dtype)
+    w_np = np.random.uniform(-1, 1, wshape).astype(data_dtype)
+    y_np = np.zeros(yshape).astype(data_dtype)
+    x = tvm.nd.array(x_np, ctx)
+    w = tvm.nd.array(w_np, ctx)
+    y = tvm.nd.array(y_np, ctx)
+    if tensor_format == 0:
+        c_np = topi.testing.conv3d_ncdhw_python(x_np, w_np, 1, 1, groups)
+    else:
+        raise AssertionError("For now, conv3d tensor format only support: 0(NCHW)")
 
-        f(x, w, y)
-        tvm.testing.assert_allclose(y.asnumpy(), c_np, atol=3e-5, rtol=1e-4)
-
-    verify()
-
+    f(x, w, y)
+    tvm.testing.assert_allclose(y.asnumpy(), c_np, atol=3e-5, rtol=1e-4)
 
 def test_conv3d():
     verify_conv3d("float32", "float32", tensor_format=0)
-
+    verify_conv3d("float32", "float32", tensor_format=0, groups=2)
 
 def verify_softmax(shape, axis, dtype="float32"):
     A = te.placeholder(shape, dtype=dtype, name='A')
