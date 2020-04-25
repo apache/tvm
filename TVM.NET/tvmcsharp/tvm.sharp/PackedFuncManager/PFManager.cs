b21b8402d19319fb47be28ef3230ffb416ee4f2c
ANSHUMAN TRIPATHY <Anshuman.T@huawei.com>
2020-04-25 21:29:35 +0530

    [16] PFManager updated

--- TVM.NET/tvmcsharp/tvm.sharp/PackedFuncManager/PFManager.cs
+++ TVM.NET/tvmcsharp/tvm.sharp/PackedFuncManager/PFManager.cs
@@ -1,11 +1,52 @@
 ﻿using System;
 
+
 namespace TVMRuntime
 {
-    public class PFManager
+    /// <summary>
+    /// TVMT ype code.
+    /// </summary>
+    public enum TVMTypeCode
     {
-        public PFManager()
-        {
-        }
+        TVMOpaqueHandle = 3,
+
+        TVMNullptr = 4,
+
+        TVMDataType = 5,
+
+        TVMContext = 6,
+
+        TVMDLTensorHandle = 7,
+
+        TVMObjectHandle = 8,
+
+        TVMModuleHandle = 9,
+
+        TVMPackedFuncHandle = 10,
+
+        TVMStr = 11,
+
+        TVMBytes = 12,
+
+        TVMNDArrayHandle = 13,
+
+        TVMObjectRValueRefArg = 14,
+
+        TVMExtBegin = 15,
+
+        TVMNNVMFirst = 16,
+
+        TVMNNVMLast = 20,
+
+        TVMExtReserveEnd = 64,
+
+        TVMExtEnd = 128,
+
+        TVMCustomBegin = 129,
+    }
+
+    public static class PFManager
+    {
+
     }
 }
