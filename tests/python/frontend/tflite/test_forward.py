409dd83535bee7a8a97a30d8337d2d758f8b4ec7
Thierry Moreau <tmoreau@octoml.ai>
2020-05-01 21:01:04 +0000

    fix

--- tests/python/frontend/tflite/test_forward.py
+++ tests/python/frontend/tflite/test_forward.py
@@ -82,7 +82,7 @@ def run_tvm_graph(tflite_model_buf, input_data, input_node, num_output=1, target
         tflite_model = tflite.Model.Model.GetRootAsModel(tflite_model_buf, 0)
     except AttributeError:
         import tflite
-        tflite_model = tflite.Mode.GetRootAsModel(tflite_model_buf, 0)
+        tflite_model = tflite.Model.GetRootAsModel(tflite_model_buf, 0)
     except ImportError:
         raise ImportError("The tflite package must be installed")
 
