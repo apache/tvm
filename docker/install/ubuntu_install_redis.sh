4da82a33e9c4368f7c62bc7e385e286f30275336
Tianqi Chen <tqchen@users.noreply.github.com>
2020-05-11 09:04:28 -0700

    [CI] Update ci-cpu to bionic (#5554)

--- docker/install/ubuntu_install_redis.sh
+++ docker/install/ubuntu_install_redis.sh
@@ -6,9 +6,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -21,5 +21,4 @@ set -u
 set -o pipefail
 
 apt-get update && apt-get install -y redis-server
-pip2 install xgboost psutil
 pip3 install xgboost psutil
