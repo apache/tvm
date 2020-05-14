e3517ddfca4904f4a0897a5fe709c1f4c5c8a9bf
Prashant Sail <psail4444@gmail.com>
2020-05-14 19:39:19 +0530

    move code inside mapped operator function

--- python/tvm/relay/frontend/tensorflow.py
+++ python/tvm/relay/frontend/tensorflow.py
@@ -1931,26 +1931,34 @@ def _add_n():
     return _impl
 
 def _partitioned_call():
-    def _impl(inputs, attr, params):
-        if not isinstance(inputs, tuple):
-            inputs = list(inputs)
-        assert len(inputs) > 0, "add_n take >=1 inputs, but 0 given."
-        _res = inputs[0]
-        for each in inputs[1:]:
-            _res = _op.add(_res, each)
-        return  _res
+    def _impl(inputs, attr, params, mod, graph):
+        node_func_name = attr.get('f').name
+        func = next((f for f in graph.library.function if f.signature.name == node_func_name), None)
+        if func:
+            from tensorflow.python.framework import function_def_to_graph
+
+            # Convert function definition to graph
+            func_input_shapes = func.attr["_input_shapes"].list.shape
+            subgraph, flat_tensor_name = function_def_to_graph.function_def_to_graph_def(func, func_input_shapes)
+
+            # Computing subgraph's input shape dictionary
+            subgraph_shape_dict = {}
+            for f_arg, input in zip(func.signature.input_arg, inputs):
+                subgraph_shape_dict[f_arg.name] = _infer_shape(input)
+
+            # Construct relay nodes from the subgraph
+            g = GraphProto()
+            mod, params = g.from_tensorflow(subgraph, shape=subgraph_shape_dict)
+            wl = tvm.relay.var('partitioned_call')
+            sb = tvm.relay.scope_builder.ScopeBuilder()
+            sb.let(wl, mod["main"])
+            sb.ret(wl(*inputs))
+            op = sb.get()
+            return op
     return _impl
 
 def _stateful_partitioned_call():
-    def _impl(inputs, attr, params):
-        if not isinstance(inputs, tuple):
-            inputs = list(inputs)
-        assert len(inputs) > 0, "add_n take >=1 inputs, but 0 given."
-        _res = inputs[0]
-        for each in inputs[1:]:
-            _res = _op.add(_res, each)
-        return  _res
-    return _impl
+    return _partitioned_call()
 
 
 # compatible operators that do NOT require any conversion.
@@ -2057,7 +2065,6 @@ _convert_map = {
     'OneHot'                            : _one_hot(),
     'Pack'                              : _pack(),
     'PartitionedCall'                   : _partitioned_call(),
-    'StatefulPartitionedCall'           : _stateful_partitioned_call(),
     'Pad'                               : _pad('Pad'),
     'PadV2'                             : _pad('PadV2'),
     'Pow'                               : _elemwise('power'),
@@ -2094,6 +2101,7 @@ _convert_map = {
     'Square'                            : _square(),
     'SquaredDifference'                 : _squared_difference(),
     'Squeeze'                           : _squeeze(),
+    'StatefulPartitionedCall'           : _stateful_partitioned_call(),
     'StopGradient'                      : _identity(),
     'StridedSlice'                      : _stridedSlice(),
     'Sub'                               : _elemwise('subtract'),
@@ -2744,7 +2752,6 @@ class GraphProto(object):
         self._loop_var_order = {}
         self._hash2tfnode = {}
         self._while_loop_name_set = set()
-        self._graphLibraryFunctions = []
 
     def from_tensorflow(self, graph, layout="NHWC", shape=None, outputs=None):
         """Construct relay nodes from tensorflow graph definition - GraphDef.
@@ -3215,7 +3222,9 @@ class GraphProto(object):
         if op_name in identity_list:
             sym = get_relay_op(op_name)(*inputs, **attrs)
         elif op_name in convert_map:
-            if _need_prelude_for_shape_inference(op_name):
+            if op_name in ["PartitionedCall", "StatefulPartitionedCall"]:
+                sym = convert_map[op_name](inputs, attrs, self._params, self._mod, self._graph)
+            elif _need_prelude_for_shape_inference(op_name):
                 sym = convert_map[op_name](inputs, attrs, self._params, self._prelude)
             else:
                 sym = convert_map[op_name](inputs, attrs, self._params, self._mod)
