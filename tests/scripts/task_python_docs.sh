bea60a7d5746c890b75149e1374f51cece2a9b75
tqchen <tqchen@octoml.ai>
2020-05-19 13:08:51 -0700

    [DOCS] Move the api docs to the api subfolder

--- tests/scripts/task_python_docs.sh
+++ tests/scripts/task_python_docs.sh
@@ -56,9 +56,9 @@ cd ..
 rm -rf _docs
 mv docs/_build/html _docs
 rm -f _docs/.buildinfo
-mv docs/doxygen/html _docs/doxygen
-mv jvm/core/target/site/apidocs _docs/javadoc
-mv web/dist/docs _docs/typedoc
+mv docs/doxygen/html _docs/api/doxygen
+mv jvm/core/target/site/apidocs _docs/api/javadoc
+mv web/dist/docs _docs/api/typedoc
 
 echo "Start creating the docs tarball.."
 # make the tarball
