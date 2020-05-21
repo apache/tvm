329e264dad6baef396337e55f9cc4f3f43f03c16
mei-ye <meiandmimi@yahoo.com>
2020-05-21 21:43:52 +0000

    correct python format issues

--- python/tvm/target/__init__.py
+++ python/tvm/target/__init__.py
@@ -55,7 +55,8 @@ We can use :py:func:`tvm.target.create` to create a tvm.target.Target from the t
 We can also use other specific function in this module to create specific targets.
 """
 from .target import Target, create
-from .target import cuda, rocm, mali, intel_graphics, opengl, arm_cpu, rasp, vta, bifrost, hexagon, amd_gpu
+from .target import cuda, rocm, mali, intel_graphics, opengl, arm_cpu, rasp, vta, bifrost, hexagon
+from .target import amd_gpu
 from .generic_func import GenericFunc
 from .generic_func import generic_func, get_native_generic_func, override_native_generic_func
 from . import datatype
