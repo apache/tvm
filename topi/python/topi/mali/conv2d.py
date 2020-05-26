1629b567270eab07d71121bf1fafd85bd3797c3e
cchung100m <cchung100m@cs.ccu.edu.tw>
2020-05-26 19:01:15 +0800

    update _schedule_winograd

--- topi/python/topi/mali/conv2d.py
+++ topi/python/topi/mali/conv2d.py
@@ -138,15 +138,14 @@ def _schedule_spatial_pack(cfg, s, output, conv, data_vec, kernel_vec):
         s[data_vec].unroll(vw)
 
     if isinstance(kernel_vec.op, tvm.te.ComputeOp) and kernel_vec.name == 'kernel_vec':
+        co, ci, kh, kw, vc = s[kernel_vec].op.axis
         if autotvm.GLOBAL_SCOPE.in_tuning:
             # Directly use modified data layout placeholder.
-            co, ci, kh, kw, vc = s[kernel_vec].op.axis
             kvshape = (co // vc, ci, kh, kw, vc)
             kernel_vec = tvm.te.placeholder(kvshape, kernel_vec.dtype, name="kernel")
             s[kernel_vec] = kernel_vec
         else:
             max_threads = tvm.target.Target.current(allow_none=False).max_num_threads
-            co, ci, kh, kw, vc = s[kernel_vec].op.axis
             fused = s[kernel_vec].fuse(co, ci, kh, kw, vc)
             fused, vec = s[kernel_vec].split(fused, VC)
             bb, tt = s[kernel_vec].split(fused, max_threads)
@@ -347,11 +346,12 @@ def _schedule_winograd(cfg, s, op):
         kernel, G = s[U].op.input_tensors
         s[G].compute_inline()
         eps, nu, co, ci, vco, = s[U].op.axis
-        r_kh, r_kw = s[U].op.reduce_axis
-        s[U].reorder(co, ci, eps, nu, r_kh, r_kw, vco)
-        _ = [s[U].unroll(x) for x in [eps, nu, r_kh, r_kw]]
-        s[U].vectorize(vco)
-        tile_and_bind(s, U, co, ci, 1, 256)
+        if not autotvm.GLOBAL_SCOPE.in_tuning:
+            r_kh, r_kw = s[U].op.reduce_axis
+            s[U].reorder(co, ci, eps, nu, r_kh, r_kw, vco)
+            _ = [s[U].unroll(x) for x in [eps, nu, r_kh, r_kw]]
+            s[U].vectorize(vco)
+            tile_and_bind(s, U, co, ci, 1, 256)
 
         # dilation
         if isinstance(kernel.op, tvm.te.ComputeOp) and "dilate" in kernel.op.tag:
