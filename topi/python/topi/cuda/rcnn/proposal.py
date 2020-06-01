898a93019682245a9d5ae55e3e1d1ff83bfd5909
Rand Xie <randxiexyy29@gmail.com>
2020-06-01 08:39:36 -0700

    fix typo: anchor windoes should be anchor windows (#5706)

--- topi/python/topi/cuda/rcnn/proposal.py
+++ topi/python/topi/cuda/rcnn/proposal.py
@@ -43,10 +43,10 @@ def predict_bbox_ir(cls_prob_buf, bbox_pred_buf, im_info_buf, out_buf, scales, r
         The last dimension is in format of [w_start, h_start, w_end, h_end, score]
 
     scales : list/tuple of float
-        Scales of anchor windoes.
+        Scales of anchor windows.
 
     ratios : list/tuple of float
-        Ratios of anchor windoes.
+        Ratios of anchor windows.
 
     feature_stride : int
         The size of the receptive field each unit in the convolution layer of the rpn, for example
@@ -325,10 +325,10 @@ def proposal(cls_prob, bbox_pred, im_info, scales, ratios, feature_stride, thres
         2-D with shape [batch, 3]
 
     scales : list/tuple of float
-        Scales of anchor windoes.
+        Scales of anchor windows.
 
     ratios : list/tuple of float
-        Ratios of anchor windoes.
+        Ratios of anchor windows.
 
     feature_stride : int
         The size of the receptive field each unit in the convolution layer of the rpn, for example
