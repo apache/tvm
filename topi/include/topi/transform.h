c62a2bc419c9f7063f4ae65da3c5cefbb2e79245
notoraptor <stevenbocco@gmail.com>
2020-06-02 13:18:16 -0400

    Fix format

--- topi/include/topi/transform.h
+++ topi/include/topi/transform.h
@@ -1025,11 +1025,11 @@ inline Tensor gather(const Tensor& data, int axis, const Tensor& indices,
         }
         Array<PrimExpr> real_indices;
         for (size_t i = 0; i < ndim_i; ++i) {
-            if (i == (size_t)axis) {
-              real_indices.push_back(indices(indices_position));
-            } else {
-              real_indices.push_back(indices_position[i]);
-            }
+          if (i == (size_t)axis) {
+            real_indices.push_back(indices(indices_position));
+          } else {
+            real_indices.push_back(indices_position[i]);
+          }
         }
         return data(real_indices);
       },
