e76fb519405c04179ff59752b84cdda51cd7dc2a
maheshambule <mahesh_ambule@persistent.com>
2020-06-13 01:01:00 +0530

    review comment

--- python/tvm/contrib/target/__init__.py
+++ python/tvm/contrib/target/__init__.py
@@ -14,7 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Codegen and runtime APIs for targets.
-"""
-
-from . import onnx
