ccfbcedfa6fa42c5a09289d34066b8aaa49aa2bc
MORITA Kazutaka <morita.kazutaka@gmail.com>
2020-06-13 00:31:31 +0900

    [CODEGEN][CONTRIB] CoreML codegen (#5634)
    * [CODEGEN][CONTRIB] CoreML codegen
    
    * import coremltools only when it is necessary
    
    * fix pylint errors
    
    * don't import contrib.coreml when using runtime lib
    
    * skip coreml codegen test in CI
    
    * don't register relay.ext.coremlcompiler in __init__.py
    
    * move tvm/contrib/coreml.py to tvm/contrib/target/coreml.py
    
    * use existing transformers for graph partitioning
    
    * skip test only when coremltools is not available
    
    * add check for annotation
    
    * move _register_coreml_op to python/tvm/relay/op/contrib/coreml.py
    
    * skip compile when xcode is unavailable
    
    * relay.op.Op -> tvm.ir.Op
    
    * set USE_COREML on
    
    * refine test

--- python/tvm/contrib/target/__init__.py
+++ python/tvm/contrib/target/__init__.py
@@ -14,9 +14,5 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=wildcard-import
-"""Contrib modules."""
-from .register import get_pattern_table, register_pattern_table
-
-from .dnnl import *
-from .coreml import *
+"""Codegen and runtime APIs for targets.
+"""
