463396436ca7892d9a313873cfb3719788ef7e2e
Gus Smith <guscomps@gmail.com>
2020-06-14 21:01:21 -0700

    Fix function name in GetPackedFunc call

--- src/relay/backend/utils.h
+++ src/relay/backend/utils.h
@@ -149,9 +149,9 @@ inline std::string DType2String(const tvm::DataType dtype) {
     os << "int";
   } else if (dtype.is_uint()) {
     os << "uint";
-  } else if ((*GetPackedFunc("_datatype_get_type_registered"))(dtype.code())) {
+  } else if ((*GetPackedFunc("runtime._datatype_get_type_registered"))(dtype.code())) {
     os << "custom["
-       << (*GetPackedFunc("_datatype_get_type_name"))(dtype.code()).operator std::string() << "]";
+       << (*GetPackedFunc("runtime._datatype_get_type_name"))(dtype.code()).operator std::string() << "]";
   } else {
     LOG(FATAL) << "Unknown type with code "
                << static_cast<unsigned>(dtype.code());
