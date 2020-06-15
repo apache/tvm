bab3bb3d9ed4b176d7366ef4d82c25552bf94c53
Gus Smith <guscomps@gmail.com>
2020-06-14 22:38:03 -0700

    Remove check for float

--- src/arith/rewrite_simplify.cc
+++ src/arith/rewrite_simplify.cc
@@ -460,7 +460,8 @@ PrimExpr RewriteSimplifier::Impl::VisitExpr_(const DivNode* op) {
 
   // x / 2.0 = x * 0.5
   if (const FloatImmNode* ptr = op->b.as<FloatImmNode>()) {
-    CHECK(op->dtype.is_float());
+    // TODO(@gussmith23) is this ok?
+    // CHECK(op->dtype.is_float());
     return op->a * make_const(op->b.dtype(), 1.0 / ptr->value);
   }
 
