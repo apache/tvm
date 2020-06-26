16c5d4da29c40702a15dc5b637f93edfb2012221
Yizhi Liu <liuyizhi@apache.org>
2020-06-25 19:13:03 -0700

    [BACKPORT-0.6][Bugfix][Arith] keep div_mode during floordiv simplify (#5922)

--- src/arith/canonical_simplify.cc
+++ src/arith/canonical_simplify.cc
@@ -692,6 +692,7 @@ SplitExpr CanonicalSimplifier::Impl::SplitDivConst(SplitExpr lhs, int64_t cval, 
   CHECK(lhs->DivModeCompatibleTo(div_mode));
   CHECK_EQ(lhs->scale, 1);
   lhs.CopyOnWrite()->lower_factor *= cval;
+  lhs.CopyOnWrite()->div_mode = div_mode;
   return lhs;
 }
 
