a063a0abc94aa71232bcfca0f609eef8c92a31ae
Giuseppe Rossini <giuseppe.rossini@arm.com>
2020-07-02 16:02:42 +0100

    Fix python linting errors - 2
    Change-Id: Ic864a235aa5da5786393cbf6146dd815c121df5e

--- topi/python/topi/arm_cpu/injective.py
+++ topi/python/topi/arm_cpu/injective.py
@@ -63,7 +63,7 @@ def schedule_injective(outs):
     s = te.create_schedule([x.op for x in outs])
     out = outs[0]
     ins = out.op.input_tensors
-    dtype = ins[0].dtype if len(ins) else out.dtype
+    dtype = ins[0].dtype if len(ins) > 0 else out.dtype
     max_vlen = 4 if dtype == 'int32' else 8
 
     if list(s[x].op.axis):
