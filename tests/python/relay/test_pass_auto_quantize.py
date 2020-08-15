86e3f132117060cdb97139ac1011ade8c29d1918
Logan Weber <36520469+weberlo@users.noreply.github.com>
2020-08-15 14:48:24 -0700

    Add Quantize/Dequantize Partitioning (#5940)
    * Implement quant/dequant partitioning
    
    on our way
    
    get clooooooser
    
    clean up (part 1)
    
    clean up (part 2)
    
    clean up (part 3)
    
    clean up (part 4)
    
    clean clean
    
    cleaanaannanaaananaananaananaan
    
    clkjsdflkjlfsjdflkj
    
    revert parser changes
    
    add docs
    
    roll lint
    
    roll lint
    
    * add option to toggle fully integral check
    
    * convert dtype collector to C++
    
    * remove need for `with_dtype`
    
    * remove unused imports
    
    * roll lint
    
    * partially address feedback
    
    * roll lint
    
    * upgrade to new parser
    
    * retrigger CI
    
    * roll the dice again

--- tests/python/relay/test_pass_auto_quantize.py
+++ tests/python/relay/test_pass_auto_quantize.py
@@ -22,6 +22,7 @@ from tvm import te
 from tvm import relay
 from tvm.relay import testing
 from tvm.relay.expr import Call
+from tvm.topi.util import get_const_tuple
 
 
 def quantize_and_build(out):
@@ -104,9 +105,214 @@ def test_calibrate_memory_bound():
         relay.quantize.quantize(mod, params, dataset)
 
 
+####################################
+# Quant/Dequant Partitioning Tests #
+####################################
+
+BASE_CFG = {
+  'skip_conv_layers': [],
+  'skip_dense_layers': False,
+  'dtype_input': "int8",
+  'dtype_weight': "int8",
+  'dtype_activation': "int32",
+}
+
+def gen_rand_tvm(tt, low, high):
+    if 'int' in tt.dtype:
+        data_np = np.random.randint(low, high, size=get_const_tuple(tt.shape), dtype=tt.dtype)
+    elif 'float' in tt.dtype:
+        data_np = np.random.uniform(low, high, size=get_const_tuple(tt.shape)).astype(tt.dtype)
+    else:
+        assert False, 'unknown dtype'
+    return tvm.nd.array(data_np, ctx=tvm.cpu(0))
+
+
+def verify_partition_fails(mod, params):
+    # standard partition should always succeed
+    with relay.quantize.qconfig(**BASE_CFG, partition_conversions='enabled'):
+        partitioned_mod = relay.quantize.quantize(mod, params)
+
+    try:
+        with relay.quantize.qconfig(**BASE_CFG, partition_conversions='fully_integral'):
+            partitioned_mod = relay.quantize.quantize(mod, params)
+        raise RuntimeError('partitioning should have failed')
+    except AssertionError:
+        pass
+
+
+def verify_partition(mod, params):
+    with relay.quantize.qconfig(**BASE_CFG, paritition_conversions='disabled'):
+        unpartitioned_mod = relay.quantize.quantize(mod, params)
+        assert len(unpartitioned_mod.get_global_vars()) == 1, \
+            'unpartitioned module should only have one function'
+    with relay.quantize.qconfig(**BASE_CFG, partition_conversions='fully_integral'):
+        partitioned_mod = relay.quantize.quantize(mod, params)
+
+    # ensure partitioned and unpartitioned results agree
+    params = [
+        gen_rand_tvm(param.type_annotation, 0, 1)
+        for param in partitioned_mod['main'].params
+    ]
+    def _eval_mod(mod):
+        vm = relay.create_executor('vm', ctx=tvm.cpu(0), target='llvm', mod=mod)
+        return vm.evaluate()(*params)
+    partitioned_mod_result = _eval_mod(partitioned_mod)
+    unpartitioned_mod_result = _eval_mod(unpartitioned_mod)
+    tvm.testing.assert_allclose(
+        unpartitioned_mod_result.asnumpy(), partitioned_mod_result.asnumpy())
+
+
+def test_add_partition():
+    mod = tvm.parser.parse("""
+    #[version = "0.0.5"]
+    def @main(
+        %x: Tensor[(10, 10), float32],
+        %y: Tensor[(10, 10), float32]) {
+      add(%x, %y)
+    }
+    """)
+    params = {}
+    verify_partition_fails(mod, params)
+
+
+def test_conv2d_partition():
+    mod = tvm.parser.parse("""
+    #[version = "0.0.5"]
+    def @main(
+        %x: Tensor[(1, 4, 16, 16), float32],
+        %w: Tensor[(4, 4, 3, 3), float32]) -> Tensor[(1, 4, 16, 16), float32] {
+      nn.conv2d(%x, %w,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3])
+    }
+    """)
+    weight_ty = mod['main'].params[1].checked_type
+    params = {
+        'w': gen_rand_tvm(weight_ty, 0, 1)
+    }
+    verify_partition(mod, params)
+
+
+def test_multiple_arg_conversions_partition():
+    mod = tvm.parser.parse("""
+    #[version = "0.0.5"]
+    def @main(
+        %x1: Tensor[(1, 4, 16, 16), float32],
+        %w1: Tensor[(4, 4, 3, 3), float32],
+        %x2: Tensor[(1, 4, 16, 16), float32],
+        %w2: Tensor[(4, 4, 3, 3), float32]
+        ) -> Tensor[(1, 4, 16, 16), float32] {
+      %0 = nn.conv2d(%x1, %w1,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3]);
+      %1 = nn.conv2d(%x2, %w2,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3]);
+      add(%0, %1)
+    }
+    """)
+
+    w1_ty = mod['main'].params[1].checked_type
+    w2_ty = mod['main'].params[3].checked_type
+    params = {
+        'w1': gen_rand_tvm(w1_ty, 0, 1),
+        'w2': gen_rand_tvm(w2_ty, 0, 1)
+    }
+    verify_partition(mod, params)
+
+
+def test_unquantizable_prefix_partition():
+    mod = tvm.parser.parse("""
+    #[version = "0.0.5"]
+    def @main(
+        %x: Tensor[(1, 4, 16, 16), float32],
+        %b: Tensor[(4), float32],
+        %w: Tensor[(4, 4, 3, 3), float32]) -> Tensor[(1, 4, 16, 16), float32] {
+      // NOTE bias_add isn't currently quantizable
+      %0 = nn.bias_add(%x, %b);
+      nn.conv2d(%0, %w,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3])
+    }
+    """)
+    bias_ty = mod['main'].params[1].checked_type
+    weight_ty = mod['main'].params[2].checked_type
+    params = {
+        'b': gen_rand_tvm(bias_ty, 0, 1),
+        'w': gen_rand_tvm(weight_ty, 0, 1)
+    }
+    verify_partition_fails(mod, params)
+
+
+def test_unquantizable_core_partition():
+    mod = tvm.parser.parse("""
+    #[version = "0.0.5"]
+    def @main(
+        %x1: Tensor[(1, 4, 16, 16), float32],
+        %w1: Tensor[(4, 4, 3, 3), float32],
+        %b: Tensor[(4), float32],
+        %w2: Tensor[(4, 4, 3, 3), float32]) -> Tensor[(1, 4, 16, 16), float32] {
+      %0 = nn.conv2d(%x1, %w1,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3]);
+      // NOTE bias_add isn't currently quantizable
+      %1 = nn.bias_add(%0, %b);
+      nn.conv2d(%1, %w2,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3])
+    }
+    """)
+    w1_ty = mod['main'].params[1].checked_type
+    bias_ty = mod['main'].params[2].checked_type
+    w2_ty = mod['main'].params[3].checked_type
+    params = {
+        'w1': gen_rand_tvm(w1_ty, 0, 1),
+        'w2': gen_rand_tvm(w2_ty, 0, 1),
+        'b': gen_rand_tvm(bias_ty, 0, 1)
+    }
+    verify_partition_fails(mod, params)
+
+
+def test_unquantizable_suffix_partition():
+    mod = tvm.parser.parse("""
+    #[version = "0.0.5"]
+    def @main(
+        %x: Tensor[(1, 4, 16, 16), float32],
+        %w: Tensor[(4, 4, 3, 3), float32],
+        %b: Tensor[(4), float32]) -> Tensor[(1, 4, 16, 16), float32] {
+      %0 = nn.conv2d(%x, %w,
+        padding=[1, 1, 1, 1],
+        channels=4,
+        kernel_size=[3, 3]);
+      // NOTE bias_add isn't currently quantizable
+      nn.bias_add(%0, %b)
+    }
+    """)
+    weight_ty = mod['main'].params[1].checked_type
+    bias_ty = mod['main'].params[2].checked_type
+    params = {
+        'w': gen_rand_tvm(weight_ty, 0, 1),
+        'b': gen_rand_tvm(bias_ty, 0, 1)
+    }
+    verify_partition_fails(mod, params)
+
+
 if __name__ == "__main__":
     test_mul_rewrite()
     test_batch_flatten_rewrite()
     test_calibrate_target(False)
     test_calibrate_target(True)
     test_calibrate_memory_bound()
+
+    test_add_partition()
+    test_conv2d_partition()
+    test_multiple_arg_conversions_partition()
+    test_unquantizable_prefix_partition()
+    test_unquantizable_core_partition()
+    test_unquantizable_suffix_partition()
