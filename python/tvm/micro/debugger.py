33b0e81bfc61e5599f59eeb0dda5f62ccc800d17
Andrew Reusch <areusch@octoml.ai>
2020-08-24 22:09:21 -0700

    asf header

--- python/tvm/micro/debugger.py
+++ python/tvm/micro/debugger.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 """Defines functions for controlling debuggers for micro TVM binaries."""
 
 import abc
@@ -6,7 +23,7 @@ import signal
 import subprocess
 import threading
 
-from . import transport
+from . import transport as _transport
 
 
 class Debugger(metaclass=abc.ABCMeta):
@@ -116,7 +133,7 @@ class GdbTransportDebugger(GdbDebugger):
         self.stdout.close()
         super(GdbTransportDebugger, self).Stop()
 
-    class _Transport(transport.Transport):
+    class _Transport(_transport.Transport):
         def __init__(self, gdb_transport_debugger):
             self.gdb_transport_debugger = gdb_transport_debugger
 
