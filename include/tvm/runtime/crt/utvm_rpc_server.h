97581ac89959ea8eb2b8f58209fde21e585ae501
Andrew Reusch <areusch@octoml.ai>
2020-08-24 22:20:40 -0700

    cppdocs

--- include/tvm/runtime/crt/utvm_rpc_server.h
+++ include/tvm/runtime/crt/utvm_rpc_server.h
@@ -48,9 +48,17 @@ typedef void* utvm_rpc_server_t;
  *
  * Call this on device startup before calling anyother utvm_rpc_server_ functions.
  *
+ * \param memory A memory block used by the runtime as dynamic memory, primarily to allocate
+ *               tensors.
+ * \param memory_size_bytes Size of the memory block, in bytes. Should be a multiple of
+ *                          (1 << page_size_bytes_log2)
+ * \param page_size_bytes_log2 Log2 of the size of each memory page. The internal allocator
+ *                             allocates one page at a time; more pages reduces waste but
+ *                             increases overhead.
  * \param write_func A callback function invoked by the TVM RPC Server to write data back to the
  *                   host. Internally, the TVM RPC Server will block until all data in a reply
  *                   packet has been written.
+ * \param write_func_ctx An opaque pointer passed to write_func when it is called.
  * \return A pointer to the TVM RPC Server. The pointer is allocated in the same memory space as
  *         the TVM workspace.
  */
