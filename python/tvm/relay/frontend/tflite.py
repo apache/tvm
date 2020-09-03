c2937c1955e582168035b6bd2bec075704efa201
Rishabh Jain <rishabh.jain2@arm.com>
2020-09-03 10:20:27 +0100

    Minor Changes.

--- python/tvm/relay/frontend/tflite.py
+++ python/tvm/relay/frontend/tflite.py
@@ -2696,11 +2696,11 @@ class OperatorConverter(object):
 
         # Input (data) Tensor. NHWC layout
         input_tensor = input_tensors[2]
-        input_shape = input_tensor.tensor.ShapeAsNumpy()
+        _, input_h, input_w, input_c = input_tensor.tensor.ShapeAsNumpy()
         # Weights tensor. TFLite uses OHWI layout
         weights_tensor = input_tensors[1]
         out_channels, kernel_h, kernel_w, in_channels = weights_tensor.tensor.ShapeAsNumpy()
-        assert input_shape[3] == in_channels, \
+        assert input_c == in_channels, \
             "Input channel in the filter should match to channel in the input"
         # output_shape Tensor. NHWC layout
         output_shape_tensor = input_tensors[0]
@@ -2743,15 +2743,15 @@ class OperatorConverter(object):
             "Output channel in the filter should match to channel in the output_shape"
 
         if padding == Padding.SAME:
-            pad_top, pad_bottom = get_pad_value(input_shape[1], kernel_h, stride_h)
-            pad_left, pad_right = get_pad_value(input_shape[2], kernel_w, stride_w)
-            pads = (pad_top, pad_left, pad_bottom, pad_right)
+            pad_top, pad_bottom = get_pad_value(input_h, kernel_h, stride_h)
+            pad_left, pad_right = get_pad_value(input_w, kernel_w, stride_w)
+            padding = (pad_top, pad_left, pad_bottom, pad_right)
         else:
-            pads = (0, 0, 0, 0)
+            padding = (0, 0, 0, 0)
 
         out = _op.nn.conv2d_transpose(in_expr, weight_expr_iohw,
                                       strides=(stride_h, stride_w),
-                                      padding=pads,
+                                      padding=padding,
                                       channels=int(out_channels),
                                       kernel_size=(int(kernel_h), int(kernel_w)),
                                       data_layout="NHWC",
