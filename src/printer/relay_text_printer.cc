52627654ef9e112600a4eb200d079f23b8fb4bad
Yizhi Liu <liuyizhi@apache.org>
2020-09-03 09:57:13 -0700

    [Bugfix][Printer] Avoid adding annotation twice for ConstantNode (#6364)
    * [Relay] Add user-defined constant node printer
    
    * fix constant node printer, which appends annotator twice when meta=true
    
    * fix lint

--- src/printer/relay_text_printer.cc
+++ src/printer/relay_text_printer.cc
@@ -239,7 +239,7 @@ bool RelayTextPrinter::AlwaysInline(const Expr& expr) {
 //------------------------------------
 // Overload of Expr printing functions
 //------------------------------------
-Doc RelayTextPrinter::PrintExpr(const Expr& expr, bool meta, bool try_inline) {
+Doc RelayTextPrinter::PrintExpr(const Expr& expr, bool meta, bool try_inline, bool optional_info) {
   // Exploit memoization to print GNF.
   // The first time we visit an expression, we need to allocate a temp var
   // for it. Every subsequent time we can just use its assigned variable.
@@ -269,7 +269,9 @@ Doc RelayTextPrinter::PrintExpr(const Expr& expr, bool meta, bool try_inline) {
     printed_expr = VisitExpr(expr);
   }
 
-  printed_expr << PrintOptionalInfo(expr);
+  if (optional_info) {
+    printed_expr << PrintOptionalInfo(expr);
+  }
 
   // add expr to doc
   if (expr.as<VarNode>()) {
@@ -335,7 +337,9 @@ Doc RelayTextPrinter::VisitExpr_(const ConstantNode* op) {
   }
   // default fall-back, record it as meta node.
   Doc doc;
-  return doc << Print(GetRef<ObjectRef>(op), true);
+  // Don't append optional_info. Because the entry function is Print,
+  // and it will append the optional_info afterwards.
+  return doc << PrintExpr(GetRef<Expr>(op), true, false, false);
 }
 
 Doc RelayTextPrinter::VisitExpr_(const TupleNode* op) {
