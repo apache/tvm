3343f61676a8f17a1a1b01c27d903f3c19c76a94
Max Willsey <me@mwillsey.com>
2020-09-21 12:03:28 -0700

    Add some more types

--- rust/tvm/src/ir/ty.rs
+++ rust/tvm/src/ir/ty.rs
@@ -1,3 +1,32 @@
-use crate::runtime::ObjectRef;
+use tvm_macros::Object;
+use tvm_rt::{array::Array, DataType};
+use crate::runtime::{ObjectRef, Object};
 
-pub type Type = ObjectRef;
+use super::PrimExpr;
+
+#[repr(C)]
+#[derive(Object)]
+#[ref_name = "Type"]
+#[type_key = "Type"]
+pub struct TypeNode {
+    pub base: Object,
+    pub span: ObjectRef,
+}
+
+#[repr(C)]
+#[derive(Object)]
+#[ref_name = "BaseTensorType"]
+#[type_key = "relay.BaseTensorType"]
+pub struct BaseTensorTypeNode {
+    pub base: TypeNode,
+}
+
+#[repr(C)]
+#[derive(Object)]
+#[ref_name = "TensorType"]
+#[type_key = "relay.TensorType"]
+pub struct TensorTypeNode {
+    pub base: TypeNode,
+    pub shape: Array<PrimExpr>,
+    pub dtype: DataType,
+}
