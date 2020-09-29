e8b0fb73f7cd8df99c4a395db4be05f09404c4ec
Tianqi Chen <tqchen@users.noreply.github.com>
2020-09-29 13:40:41 -0700

    [RUNTIME] NDArray CopyFrom/To Bytes always synchronize (#6586)
    * [RUNTIME] NDArray CopyFrom/To Bytes always synchronize
    
    The previous behavior of non-sync can be unsafe for GPU devices.
    In particular, the need for an explicit synchronization could
    leads to confusion behavior e.g. asnumpy does not immediately return
    the right content for vulkan.
    
    Also brings the requirement of array being contiguous.
    Right now we encourage compact array since they are easier for optimization.
    We can consider bring support later by introducing a compactify PackedFunc(which might need be jitted).

--- include/tvm/runtime/ndarray.h
+++ include/tvm/runtime/ndarray.h
@@ -80,8 +80,7 @@ class NDArray : public ObjectRef {
    * \param data The source bytes to be copied from.
    * \param nbytes The size of the buffer in bytes
    *        Must be equal to the size of the NDArray.
-   * \note The copy may happen asynchronously if it involves a GPU context.
-   *       TVMSynchronize is necessary.
+   * \note The copy always triggers a TVMSynchronize.
    */
   TVM_DLL void CopyFromBytes(const void* data, size_t nbytes);
   /*!
@@ -97,8 +96,7 @@ class NDArray : public ObjectRef {
    * \param data The source bytes to be copied from.
    * \param nbytes The size of the data buffer.
    *        Must be equal to the size of the NDArray.
-   * \note The copy may happen asynchronously if it involves a GPU context.
-   *       TVMSynchronize is necessary.
+   * \note The copy always triggers a TVMSynchronize.
    */
   TVM_DLL void CopyToBytes(void* data, size_t nbytes) const;
   /*!
