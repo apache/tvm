878bef1cf6c00a50817ef5beba9a19bfa4687f12
Leandro Nunes <leandron85@gmail.com>
2020-09-29 17:40:46 +0100

    [tvmc] Introduce 'tune' subcommand (part 3/4) (#6537)
    * tvmc: introduce 'tune' subcommand (part 3/4)
    
     * introduces a subcommand to drive auto-tuning
    
    Co-authored-by: Matthew Barrett <matthew.barrett@arm.com>
    Co-authored-by: Luke Hutton <luke.hutton@arm.com>
    Co-authored-by: Giuseppe Rossini <giuseppe.rossini@arm.com>
    
    * [tvmc] address code review comments
    
    * adjust --min-repeat-ms default value logic
    
    * re-arrange rpc arguments to be --rpc-tracker=hostname:port and --rpc-key=str
    
    * use a local reference of the tvmc logger
    
    * add --target-host, default to llvm
    
    Co-authored-by: Matthew Barrett <matthew.barrett@arm.com>
    Co-authored-by: Luke Hutton <luke.hutton@arm.com>
    Co-authored-by: Giuseppe Rossini <giuseppe.rossini@arm.com>

--- python/tvm/driver/tvmc/common.py
+++ python/tvm/driver/tvmc/common.py
@@ -17,8 +17,17 @@
 """
 Common utility functions shared by TVMC modules.
 """
+import logging
+import os.path
+
+import tvm
+
 from tvm import relay
 from tvm import transform
+
+
+# pylint: disable=invalid-name
+logger = logging.getLogger("TVMC")
 
 
 class TVMCException(Exception):
@@ -63,3 +72,33 @@ def convert_graph_layout(mod, desired_layout):
             raise TVMCException(
                 "Error converting layout to {0}: {1}".format(desired_layout, str(err))
             )
+
+
+# TODO In a separate PR, eliminate the duplicated code here and in compiler.py (@leandron)
+def target_from_cli(target):
+    """
+    Create a tvm.target.Target instance from a
+    command line interface (CLI) string.
+
+    Parameters
+    ----------
+    target : str
+        compilation target as plain string,
+        inline JSON or path to a JSON file
+
+    Returns
+    -------
+    tvm.target.Target
+        an instance of target device information
+    """
+
+    if os.path.exists(target):
+        with open(target) as target_file:
+            logger.info("using target input from file: %s", target)
+            target = "".join(target_file.readlines())
+
+    # TODO(@leandron) We don't have an API to collect a list of supported
+    #       targets yet
+    logger.debug("creating target from input: %s", target)
+
+    return tvm.target.Target(target)
