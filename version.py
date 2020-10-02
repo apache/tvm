530a852bfd1097a8874265a693fa3104241c75f6
tqchen <tqchenml@gmail.com>
2020-10-02 14:15:21 -0400

    [VERSION] Version for v0.8 cycle

--- version.py
+++ version.py
@@ -31,7 +31,7 @@ import re
 # current version
 # We use the version of the incoming release for code
 # that is under development
-__version__ = "0.7.0"
+__version__ = "0.8.dev0"
 
 # Implementations
 def update(file_name, pattern, repl):
