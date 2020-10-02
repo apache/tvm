52b776aa708d7c3a4db928e9f269375e4984a2b0
ziheng <ziheng@apache.org>
2020-10-02 06:35:49 -0700

    [RELEASE] Bump version to 0.7.0 (#6614)

--- python/tvm/_ffi/libinfo.py
+++ python/tvm/_ffi/libinfo.py
@@ -217,4 +217,4 @@ def find_include_path(name=None, search_path=None, optional=False):
 # We use the version of the incoming release for code
 # that is under development.
 # The following line is set by tvm/python/update_version.py
-__version__ = "0.7.dev1"
+__version__ = "0.7.0"
