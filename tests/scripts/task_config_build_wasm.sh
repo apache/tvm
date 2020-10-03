802307a46856dc3900ebc8d24e56e9cf017d8c78
Andrew Reusch <areusch@octoml.ai>
2020-10-02 17:08:35 -0700

    obliterate USE_ANTLR from cmake.config

--- tests/scripts/task_config_build_wasm.sh
+++ tests/scripts/task_config_build_wasm.sh
@@ -30,7 +30,6 @@ echo set\(USE_STANDALONE_CRT ON\) >> config.cmake
 echo set\(USE_GRAPH_RUNTIME_DEBUG ON\) >> config.cmake
 echo set\(USE_VM_PROFILER ON\) >> config.cmake
 echo set\(USE_LLVM llvm-config-11\) >> config.cmake
-echo set\(USE_ANTLR ON\) >> config.cmake
 echo set\(CMAKE_CXX_COMPILER g++\) >> config.cmake
 echo set\(CMAKE_CXX_FLAGS -Werror\) >> config.cmake
 echo set\(HIDE_PRIVATE_SYMBOLS ON\) >> config.cmake
