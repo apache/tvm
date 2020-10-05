abf39170839cfe8e0d4b9863db3129328c09d236
Matthew Brookhart <mbrookhart@octoml.ai>
2020-10-05 14:22:09 -0600

    strided-slice can't do 0-sized output tensors, remove test

--- tests/python/relay/dyn/test_dynamic_op_level4.py
+++ tests/python/relay/dyn/test_dynamic_op_level4.py
@@ -63,7 +63,6 @@ def test_dynamic_strided_slice():
             op_res = intrp.evaluate()(x_data)
             tvm.testing.assert_allclose(op_res.asnumpy(), ref_res)
 
-    verify((1, 3, 10, 10), [0, 0, 0, 0], [-1, 3, 10, 10], [1], (0, 3, 10, 10), dtype="int64")
     verify(
         (1, 224, 224, 3),
         [0, 20, 20, 0],
