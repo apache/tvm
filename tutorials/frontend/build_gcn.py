ecf8b656abcfb0d73903aee9a0ee3dea039c0ff8
Jared Roesch <roeschinc@gmail.com>
2020-10-14 13:31:27 -0700

    Format

--- tutorials/frontend/build_gcn.py
+++ tutorials/frontend/build_gcn.py
@@ -242,7 +242,9 @@ import networkx as nx
 
 def prepare_params(g, data):
     params = {}
-    params["infeats"] = data.features.numpy().astype("float32")  # Only support float32 as feature for now
+    params["infeats"] = data.features.numpy().astype(
+        "float32"
+    )  # Only support float32 as feature for now
 
     # Generate adjacency matrix
     adjacency = nx.to_scipy_sparse_matrix(g)
@@ -351,5 +353,6 @@ acc = evaluate(data, logits_tvm)
 print("Test accuracy of TVM results: {:.2%}".format(acc))
 
 import tvm.testing
+
 # Verify the results with the DGL model
 tvm.testing.assert_allclose(logits_torch, logits_tvm, atol=1e-3)
