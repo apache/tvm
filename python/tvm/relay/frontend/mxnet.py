80a88dd51f880a823f2826f4202c65b1f31960f5
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-15 13:32:47 -0700

    update
    Update type_relations.cc
    
    Update transform.cc
    
    Update transform.cc
    
    Update transform.cc
    
    Update transform.cc
    
    Update transform.cc
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    update
    
    Update mxnet.py
    
    debug
    
    Update generic.py
    
    Update topi_integration.py
    
    fix bug
    
    update
    
    Update test_forward.py
    
    Update test_forward.py
    
    fix test case
    
    Update mxnet.py
    
    update
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update test_forward.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update test_forward.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update mxnet.py
    
    debug
    
    Update mxnet.py
    
    Update mxnet.py
    
    Update test_forward.py
    
    Update mxnet.py

--- python/tvm/relay/frontend/mxnet.py
+++ python/tvm/relay/frontend/mxnet.py
@@ -58,6 +58,11 @@ __all__ = ["from_mxnet"]
 _activation_map = {"sigmoid": _op.sigmoid, "tanh": _op.tanh, "relu": _op.nn.relu}
 
 
+def get_tuple_shape(shape_expr):
+    """Get the tuple shape from a shape expression"""
+    return tuple([ele.value for ele in shape_expr])
+
+
 def _mx_fully_connected(inputs, attrs):
     import mxnet as mx  # pylint: disable=import-outside-toplevel
 
@@ -627,6 +632,21 @@ def _mx_expand_dims(inputs, attrs):
     return _op.expand_dims(inputs[0], axis=axis)
 
 
+def _mx_where(inputs, attrs):
+    cond, lhs, rhs = inputs
+    cond_shape = get_tuple_shape(_infer_type(cond).checked_type.shape)
+    lhs_shape = get_tuple_shape(_infer_type(lhs).checked_type.shape)
+    rhs_shape = get_tuple_shape(_infer_type(rhs).checked_type.shape)
+    out_shape = np.broadcast(np.empty(cond_shape), np.empty(lhs_shape), np.empty(rhs_shape)).shape
+    if out_shape != cond_shape:
+        cond = _op.broadcast_to(cond, out_shape)
+    if out_shape != lhs_shape:
+        lhs = _op.broadcast_to(lhs, out_shape)
+    if out_shape != rhs_shape:
+        rhs = _op.broadcast_to(rhs, out_shape)
+    return _op.where(cond, lhs, rhs)
+
+
 def _mx_pad(inputs, attrs):
     pad_mode = attrs.get_str("mode", None)
     if pad_mode is None:
@@ -790,6 +810,16 @@ def _mx_dot(inputs, attrs):
 def _mx_batch_dot(inputs, attrs):
     assert len(inputs) == 2
     a, b = inputs
+    a_shape = _infer_type(a).checked_type.shape
+    batch_shapes = None
+    if len(a_shape) > 3:
+        batch_shapes = a_shape[:-2]
+        a = _op.reverse_reshape(a, newshape=(-1, 0, 0))
+    b_shape = _infer_type(b).checked_type.shape
+    if len(b_shape) > 3:
+        if batch_shapes is None:
+            batch_shapes = b_shape[:-2]
+        b = _op.reverse_reshape(b, newshape=(-1, 0, 0))
     transpose_a = attrs.get_bool("transpose_a", False)
     transpose_b = attrs.get_bool("transpose_b", False)
     if transpose_a is True:
@@ -797,7 +827,10 @@ def _mx_batch_dot(inputs, attrs):
         raise tvm.error.OpAttributeInvalid(msg.format(transpose_a))
     if transpose_b is False:
         b = _op.transpose(b, axes=[0, 2, 1])
-    return _op.nn.batch_matmul(a, b)
+    out = _op.nn.batch_matmul(a, b)
+    if batch_shapes is not None:
+        out = _op.reverse_reshape(out, newshape=tuple(batch_shapes) + (0, 0))
+    return out
 
 
 def _mx_arange(inputs, attrs):
@@ -2312,23 +2345,76 @@ def _mx_npx_reshape(inputs, attrs):
     reverse = attrs.get_bool("reverse", False)
     shape_list = list(shape)
     new_shape_list = []
-    for num in shape_list:
-        if num > 0 or num == -1:
-            new_shape_list.append(num)
-        elif num == -2:
-            new_shape_list.append(0)
-        elif num == -4:
-            new_shape_list.append(-2)
-        elif num == -5:
-            new_shape_list.append(-3)
-        elif num == -6:
-            new_shape_list.append(-4)
-        else:
-            raise tvm.error.OpAttributeInvalid("Shape dimension %d is not supported" % num)
-    shape = tuple(new_shape_list)
-    if reverse:
-        return _op.reverse_reshape(inputs[0], newshape=shape)
-    return _op.reshape(inputs[0], newshape=shape)
+    if -3 not in shape_list:
+        for num in shape_list:
+            if num > 0 or num == -1:
+                new_shape_list.append(num)
+            elif num == -2:
+                new_shape_list.append(0)
+            elif num == -4:
+                new_shape_list.append(-2)
+            elif num == -5:
+                new_shape_list.append(-3)
+            elif num == -6:
+                new_shape_list.append(-4)
+            else:
+                raise tvm.error.OpAttributeInvalid("Shape dimension %d is not supported" % num)
+        shape = tuple(new_shape_list)
+        if reverse:
+            return _op.reverse_reshape(inputs[0], newshape=shape)
+        return _op.reshape(inputs[0], newshape=shape)
+    else:
+        old_shape = get_tuple_shape(_infer_type(inputs[0]).checked_type.shape)
+        new_shape = []
+        if reverse:
+            old_shape = old_shape[::-1]
+            shape_list = shape_list[::-1]
+        ptr = 0
+        unknown_axis = None
+        src_ptr = 0
+        while src_ptr < len(shape_list):
+            ele = shape_list[src_ptr]
+            src_ptr += 1
+            if ele > 0:
+                new_shape.append(ele)
+                ptr += 1
+            elif ele == -1:
+                new_shape.append(-1)
+                assert unknown_axis is None, "Can only have one unknown axis."
+                unknown_axis = len(new_shape)
+                ptr += 1
+            elif ele == -2:
+                new_shape.append(old_shape[ptr])
+                ptr += 1
+            elif ele == -3:
+                assert old_shape[ptr] == 1
+                ptr += 1
+            elif ele == -4:
+                new_shape += old_shape[ptr:]
+                break
+            elif ele == -5:
+                new_shape.append(old_shape[ptr] * old_shape[ptr + 1])
+                ptr += 2
+            elif ele == -6:
+                # Split axis
+                lhs = shape_list[src_ptr]
+                rhs = shape_list[src_ptr + 1]
+                src_ptr += 2
+                assert not (lhs == -1 and rhs == -1)
+                if lhs == -1:
+                    assert old_shape[ptr] % rhs == 0
+                    lhs = old_shape[ptr] // rhs
+                if rhs == -1:
+                    assert old_shape[ptr] % lhs == 0
+                    rhs = old_shape[ptr] // lhs
+                new_shape.append(lhs)
+                new_shape.append(rhs)
+                ptr += 1
+            else:
+                raise tvm.error.OpAttributeInvalid("Shape dimension %d is not supported" % ele)
+        if reverse:
+            new_shape = new_shape[::-1]
+        return _op.reshape(inputs[0], newshape=new_shape)
 
 
 def _mx_split_v2(inputs, attrs):
@@ -2346,12 +2432,21 @@ def _mx_split_v2(inputs, attrs):
 
 
 def _mx_npi_where_rscalar(inputs, attrs):
+    cond, dat = inputs
     scalar = attrs.get_float("scalar")
-    dtype = _infer_type(inputs[1]).checked_type.dtype
+    cond_shape = get_tuple_shape(_infer_type(cond).checked_type.shape)
+    dat_shape = get_tuple_shape(_infer_type(dat).checked_type.shape)
+    dtype = _infer_type(dat).checked_type.dtype
+    # Check for broadcasting
+    out_shape = np.broadcast(np.empty(cond_shape), np.empty(dat_shape)).shape
+    if out_shape != cond_shape:
+        cond = _op.broadcast_to(cond, out_shape)
+    if out_shape != dat_shape:
+        dat = _op.broadcast_to(dat, out_shape)
     scalar = _expr.const(scalar, dtype=dtype)
-    ones = _op.ones_like(inputs[1])
+    ones = _op.ones_like(dat)
     scalar = _op.multiply(ones, scalar)
-    return _op.where(inputs[0], inputs[1], scalar)
+    return _op.where(cond, dat, scalar)
 
 
 # Note: due to attribute conversion constraint
@@ -2372,7 +2467,6 @@ _identity_list = [
     "reshape_like",
     "zeros_like",
     "ones_like",
-    "where",
     "cos",
     "cosh",
     "sin",
@@ -2384,6 +2478,7 @@ _identity_list = [
 _convert_map = {
     "_copy": _rename(_op.copy),
     "relu": _rename(_op.nn.relu),
+    "where": _mx_where,
     "broadcast_add": _rename(_op.add),
     "broadcast_plus": _rename(_op.add),
     "broadcast_sub": _rename(_op.subtract),
@@ -2598,6 +2693,7 @@ _convert_map = {
     "_npi_concatenate": _mx_npi_concatenate,
     "_npx_reshape": _mx_npx_reshape,
     "_np_copy": _rename(_op.copy),
+    "_npi_copy": _rename(_op.copy),
     "_npi_power": _rename(_op.power),
     "_npi_power_scalar": _binop_scalar(_op.power),
     "_npi_multiply": _rename(_op.multiply),
@@ -2717,7 +2813,6 @@ def _from_mxnet_impl(symbol, shape_dict, dtype_info, params=None, mod=None):
             else:
                 raise RuntimeError("unexpected type %s" % type(res))
             node_map[nid] = res
-
     outputs = [node_map[e[0]][e[1]] for e in jgraph["heads"]]
     outputs = outputs[0] if len(outputs) == 1 else _expr.Tuple(outputs)
     func = _function.Function(analysis.free_vars(outputs), outputs)
