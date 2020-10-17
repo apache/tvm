127d50763eade4e81b083e7df875e0ed366616c6
Andrew Reusch <areusch@octoml.ai>
2020-10-16 17:42:12 -0700

    fix option parsing

--- tests/micro/qemu/conftest.py
+++ tests/micro/qemu/conftest.py
@@ -1,11 +1,11 @@
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--microtvm-platforms", default=["host"], action='append',
+        "--microtvm-platforms", default="host",
         help=("Specify a comma-separated list of test models (i.e. as passed to tvm.target.micro()) "
               "for microTVM tests."))
 
 
 def pytest_generate_tests(metafunc):
     if "platform" in metafunc.fixturenames:
-       metafunc.parametrize("platform", metafunc.config.getoption("microtvm_platforms"))
+        metafunc.parametrize("platform", metafunc.config.getoption("microtvm_platforms").split(","))
