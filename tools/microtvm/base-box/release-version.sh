b6b2de04e3a8a8f51dec2d2a62e8e277f0d82d4f
Andrew Reusch <areusch@octoml.ai>
2020-10-16 21:21:40 -0700

    asf lint, somehow local pass didn't work

--- tools/microtvm/base-box/release-version.sh
+++ tools/microtvm/base-box/release-version.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -e
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
 SKIP_ADD=( )
 if [ "$1" == "--skip-add" ]; then
@@ -24,12 +40,14 @@ if [[ ! "$1" =~ ^[0-9].[0-9].[0-9]$ ]]; then
     exit 2
 fi
 
+cat packer.json.template | sed 's/^#.*//g' >packer.json
+
 ALL_PROVIDERS=( \
 #    virtualbox \
     parallels \
 )
 for provider in "${ALL_PROVIDERS[@]}"; do
     set -x
-    packer build -var-file=api-token "${SKIP_ADD[@]}" -var "provider=${provider}" -var "version=$1" packer.hcl
+    packer build -var-file=api-token "${SKIP_ADD[@]}" -var "provider=${provider}" -var "version=$1" packer.json
     set +x
 done
