03f5bde9d63d5e1341637c1a7c8e350849ab7a81
Jared Roesch <jroesch@octoml.ai>
2020-10-19 22:44:27 -0700

    Tweak

--- rust/tvm/examples/resnet/build.rs
+++ rust/tvm/examples/resnet/build.rs
@@ -18,12 +18,13 @@
  */
 
 use anyhow::{Context, Result};
-use std::{io::Write, path::Path, process::Command};
+use std::{io::Write, path::{Path}, process::Command};
 
 fn main() -> Result<()> {
+    let out_dir = std::env::var("CARGO_MANIFEST_DIR")?;
     let output = Command::new("python3")
         .arg(concat!(env!("CARGO_MANIFEST_DIR"), "/src/build_resnet.py"))
-        .arg(&format!("--build-dir={}", env!("CARGO_MANIFEST_DIR")))
+        .arg(&format!("--build-dir={}", out_dir))
         .output()
         .with_context(|| anyhow::anyhow!("failed to run python3"))?;
     if !output.status.success() {
@@ -33,7 +34,7 @@ fn main() -> Result<()> {
         panic!("Failed to execute build script");
     }
     assert!(
-        Path::new(&format!("{}/deploy_lib.o", env!("CARGO_MANIFEST_DIR"))).exists(),
+        Path::new(&format!("{}/deploy_lib.o", out_dir)).exists(),
         "Could not prepare demo: {}",
         String::from_utf8(output.stderr)
             .unwrap()
@@ -44,7 +45,7 @@ fn main() -> Result<()> {
     );
     println!(
         "cargo:rustc-link-search=native={}",
-        env!("CARGO_MANIFEST_DIR")
+        out_dir
     );
 
     Ok(())
