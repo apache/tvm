ee9b6d6de52accf6515d69c7f565aeefdf19bddc
Jared Roesch <roeschinc@gmail.com>
2020-10-24 00:15:38 -0700

    [Docker] Turn on Rust docs and MxNet based ResNet (#6640)
    * Enable ResNet and Rust docs
    
    * Tweak
    
    * Format
    
    * Fix issue with overwriting

--- tests/scripts/task_python_docs.sh
+++ tests/scripts/task_python_docs.sh
@@ -73,12 +73,10 @@ npm install
 npm run typedoc
 cd ..
 
-# TODO(@jroesch): add Rust to CI container
-# see: https://github.com/apache/incubator-tvm/issues/6628
 # Rust doc
-# cd rust
-# cargo doc --workspace --no-deps
-# cd ..
+cd rust
+cargo doc --workspace --no-deps
+cd ..
 
 # Prepare the doc dir
 rm -rf _docs
@@ -87,8 +85,7 @@ rm -f _docs/.buildinfo
 mkdir -p _docs/api
 mv docs/doxygen/html _docs/api/doxygen
 mv jvm/core/target/site/apidocs _docs/api/javadoc
-# See above TODO
-# mv rust/target/doc _docs/api/rust
+mv rust/target/doc _docs/api/rust
 mv web/dist/docs _docs/api/typedoc
 
 echo "Start creating the docs tarball.."
