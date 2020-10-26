28ff4392b56e4691c8274019b6d53d0b32f7457d
Altan Haan <ahaan@octoml.ai>
2020-10-26 14:32:32 -0700

    fix a typo in type solver diagnostics

--- src/relay/analysis/type_solver.cc
+++ src/relay/analysis/type_solver.cc
@@ -615,7 +615,7 @@ bool TypeSolver::Solve() {
 
       rnode->resolved = resolved;
     } catch (const Error& err) {
-      this->diag_ctx_.Emit(Diagnostic::Error(rnode->span) << "err");
+      this->diag_ctx_.Emit(Diagnostic::Error(rnode->span) << err.what());
       rnode->resolved = false;
     } catch (const dmlc::Error& e) {
       ICHECK(false) << e.what();
