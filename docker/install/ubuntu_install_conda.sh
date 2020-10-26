052d70155c01b5fd05fd88f5f3dadb737fcb4628
shibuiwilliam <shibuiyusuke@gmail.com>
2020-10-26 21:21:27 +0900

    [Fix,Conda] update conda download url (#6760)
    Co-authored-by: Shibui Yusuke <yusuke.shibui@ShibuinoMacBook-Pro.local>

--- docker/install/ubuntu_install_conda.sh
+++ docker/install/ubuntu_install_conda.sh
@@ -20,7 +20,7 @@ set -e
 set -u
 set -o pipefail
 
-cd /tmp && wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+cd /tmp && wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda
 rm /tmp/Miniconda3-latest-Linux-x86_64.sh
