e016461f7f7ed2eaaebc7912f71921c1a81085dd
Leandro Nunes <leandron85@gmail.com>
2020-10-27 15:46:21 +0000

    Revert "[Relay][Frontend] Tensorflow version support upgrade from 2.1.0 to 2.3.1 (#6706)"
    This reverts commit d50bb0344a45cf99a180ab51134893618aecbd92.

--- python/tvm/relay/frontend/tensorflow.py
+++ python/tvm/relay/frontend/tensorflow.py
@@ -1334,7 +1334,7 @@ def _fused_batch_norm():
             op_name="batch_norm",
             transforms={"scale_after_normalization": "scale", "variance_epsilon": "epsilon"},
             extras={"axis": axis},
-            ignores=["data_format", "U", "exponential_avg_factor"],
+            ignores=["data_format", "U"],
             disables=["momentum"],
         )(inputs, attr)
 
@@ -1364,7 +1364,7 @@ def _batch_norm():
             op_name="batch_norm",
             transforms={"scale_after_normalization": "scale", "variance_epsilon": "epsilon"},
             extras={"axis": axis},
-            ignores=["data_format", "exponential_avg_factor"],
+            ignores=["data_format"],
             disables=["momentum"],
         )(new_inputs, attr)
 
