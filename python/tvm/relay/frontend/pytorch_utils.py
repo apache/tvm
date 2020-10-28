a0709bb2415d7fb30a81d5b7d5860d493eb46b0a
Ritwik Das <ritwikdas54@gmail.com>
2020-10-28 01:25:17 -0700

    Update pytorch_utils.py

--- python/tvm/relay/frontend/pytorch_utils.py
+++ python/tvm/relay/frontend/pytorch_utils.py
@@ -22,6 +22,6 @@ def is_version_greater_than(ver):
     import torch
     import re
 
-    return "".join(re.findall("(\d+\.)(\d+\.)(\d)", torch.__version__)[0]) > "".join(
-        re.findall("(\d+\.)(\d+\.)(\d)", ver)[0]
+    return "".join(re.findall(r"(\d+\.)(\d+\.)(\d)", torch.__version__)[0]) > "".join(
+        re.findall(r"(\d+\.)(\d+\.)(\d)", ver)[0]
     )
