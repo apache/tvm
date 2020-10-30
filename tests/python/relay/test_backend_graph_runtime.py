1ad12917187009bcbb5bf304a542c86eec85f0c5
Masahiro Masuda <masahi129@gmail.com>
2020-10-30 22:13:16 +0900

    add test

--- tests/python/relay/test_backend_graph_runtime.py
+++ tests/python/relay/test_backend_graph_runtime.py
@@ -184,10 +184,41 @@ def test_gru_like():
             tvm.testing.assert_allclose(out, ref, rtol=1e-5, atol=1e-5)
 
 
+def test_plan_memory_nested_tuples():
+    # it is sufficient to cycle through two memories.
+    x = relay.var("x", shape=(10,))
+    x1 = relay.exp(x)
+    x2 = relay.exp(x)
+    x3 = relay.exp(x)
+    out = relay.Tuple([x1, relay.Tuple([x2, x3])])
+    func = relay.Function([x], out)
+    mod = tvm.IRModule.from_expr(func)
+    mod = relay.transform.InferType()(mod)
+    mod = relay.transform.FuseOps(0)(mod)
+    func = mod["main"]
+    mod = relay.transform.InferType()(mod)
+    smap = relay.backend._backend.GraphPlanMemory(func)
+    # storage_ids = set()
+    # device_types = set()
+    # for k, v in smap.items():
+    #     assert len(v) == 2
+    #     for x in v[0]:
+    #         storage_ids.add(x.value)
+    #     for x in v[1]:
+    #         device_types.add(x.value)
+
+    # # Current rule requires vars have unique storage id
+    # # because we don't do inplace, we will need another
+    # # two alternating temporary space.
+    # assert len(storage_ids) == 4
+    # assert len(device_types) == 1
+
+
 if __name__ == "__main__":
-    test_plan_memory()
-    test_with_params()
-    test_add_op_scalar()
-    test_add_op_tensor()
-    test_add_op_broadcast()
-    test_gru_like()
+    # test_plan_memory()
+    # test_with_params()
+    # test_add_op_scalar()
+    # test_add_op_tensor()
+    # test_add_op_broadcast()
+    # test_gru_like()
+    test_plan_memory_nested_tuples()
