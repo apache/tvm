8625b02ab57fe23f193a03d5c0c21fcda21ba97c
Zhi <5145158+zhiics@users.noreply.github.com>
2020-11-03 09:31:38 -0800

    [CI] remove unused environment var (#6824)

--- tests/scripts/task_config_build_cpu.sh
+++ tests/scripts/task_config_build_cpu.sh
@@ -26,7 +26,6 @@ cp ../cmake/config.cmake .
 echo set\(USE_SORT ON\) >> config.cmake
 echo set\(USE_MICRO ON\) >> config.cmake
 echo set\(USE_MICRO_STANDALONE_RUNTIME ON\) >> config.cmake
-echo set\(USE_STANDALONE_CRT ON\) >> config.cmake
 echo set\(USE_GRAPH_RUNTIME_DEBUG ON\) >> config.cmake
 echo set\(USE_VM_PROFILER ON\) >> config.cmake
 echo set\(USE_DNNL_CODEGEN ON\) >> config.cmake
