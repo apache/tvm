a532d28720e074680826d48681f12485e77257d0
chengfan.jcf <chengfan.jcf@alibaba-inc.com>
2020-11-03 20:34:59 +0800

    Update llvm version for i386

--- tests/scripts/task_config_build_i386.sh
+++ tests/scripts/task_config_build_i386.sh
@@ -29,7 +29,7 @@ echo set\(USE_GRAPH_RUNTIME_DEBUG ON\) >> config.cmake
 echo set\(USE_MICRO ON\) >> config.cmake
 echo set\(USE_MICRO_STANDALONE_RUNTIME ON\) >> config.cmake
 echo set\(USE_VM_PROFILER ON\) >> config.cmake
-echo set\(USE_LLVM llvm-config-4.0\) >> config.cmake
+echo set\(USE_LLVM llvm-config-8\) >> config.cmake
 echo set\(CMAKE_CXX_COMPILER g++\) >> config.cmake
 echo set\(CMAKE_CXX_FLAGS -Werror\) >> config.cmake
 echo set\(USE_VTA_TSIM ON\) >> config.cmake
