3e65b12d0eefa74c7818e6a766c8d6ba3cbd71a9
Josh Fromm <jwfromm@uw.edu>
2020-11-05 18:05:46 +0000

    Just dont use onnxruntime for now i guess.

--- tests/python/frontend/onnx/test_forward.py
+++ tests/python/frontend/onnx/test_forward.py
@@ -3841,9 +3841,8 @@ def test_loop():
     verify_count_loop()
 
 
-# TODO(jwfromm): enable cuda testing once onnxruntime version is updated in CI.
-@tvm.testing.parametrize_targets("llvm")
-def test_if(ctx, target):
+@tvm.testing.uses_gpu
+def test_if():
     # Given a bool scalar input cond.
     # return constant tensor x if cond is True, otherwise return constant tensor y.
     then_out = onnx.helper.make_tensor_value_info("then_out", onnx.TensorProto.FLOAT, [5])
@@ -3881,11 +3880,12 @@ def test_if(ctx, target):
 
     if_model = onnx.helper.make_model(if_graph)
     cond = np.array(1).astype("bool")
-    onnx_out = get_onnxruntime_output(if_model, [cond], dtype="bool")
+    correct_out = x if cond else y
 
-    tvm_out = get_tvm_output_with_vm(if_model, [cond], target, ctx, freeze_params=True)
-    for i in range(len(tvm_out)):
-        tvm.testing.assert_allclose(onnx_out[i], tvm_out[i], rtol=1e-05, atol=1e-05)
+    for target, ctx in tvm.testing.enabled_targets():
+        tvm_out = get_tvm_output_with_vm(if_model, [cond], target, ctx, freeze_params=True)
+        for i in range(len(tvm_out)):
+            tvm.testing.assert_allclose(correct_out[i], tvm_out[i], rtol=1e-05, atol=1e-05)
 
 
 if __name__ == "__main__":
