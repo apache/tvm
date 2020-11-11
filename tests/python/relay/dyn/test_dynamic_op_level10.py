6a5a0646e37ae3bca0a47b1496f38dde88d95911
Matthew Brookhart <mbrookhart@octoml.ai>
2020-11-11 16:58:23 -0700

    Dynamic gpu tests, add dynamic strided slice to topi (#6870)
    * enable GPU tests for dynamic ops
    
    * strided-slice can't do 0-sized output tensors, remove test
    
    * move dynamic strided slice into topi
    
    * add python interface to topi dynamic strided slice
    
    add python interface, tests
    
    * autoformat
    
    * fix bad copy/paste
    
    * fix doc string
    
    * disable topk on gpu for now, remove invalid slice test

--- tests/python/relay/dyn/test_dynamic_op_level10.py
+++ tests/python/relay/dyn/test_dynamic_op_level10.py
@@ -27,8 +27,8 @@ import tvm.topi.testing
 import random
 import tvm.testing
 
-# TODO(mbrookhart): Enable when the VM supports heterogenus execution
-# @tvm.testing.uses_gpu
+
+@tvm.testing.uses_gpu
 def test_broadcast_to():
     def verify_more_dynamic_broadcast_to(x_shape, out_shape):
         rank = len(out_shape)
@@ -82,8 +82,33 @@ def test_broadcast_to():
     verify_broadcast_to((4, 1), (1, 4, 3))
 
 
-# TODO(mbrookhart): Enable when the VM supports heterogenus execution
-# @tvm.testing.uses_gpu
+@tvm.testing.uses_gpu
+def test_dyn_broadcast_to():
+    dtype = "uint8"
+    rank = 3
+    shape_type = "int64"
+    dyn_shape = relay.Var("shape", relay.ty.TensorType((rank,), shape_type))
+    x_shape = (1,)
+    x = relay.Var("x", relay.ty.TensorType(x_shape, dtype))
+    z = relay.broadcast_to(x, dyn_shape)
+    zz = run_infer_type(z)
+
+    assert zz.checked_type == relay.ty.TensorType((relay.Any(),) * rank, dtype)
+
+    func = relay.Function([x, dyn_shape], z)
+
+    x = np.random.uniform(size=x_shape).astype(dtype)
+    dyn_shape = (1,) * rank
+    ref_res = np.broadcast_to(x, dyn_shape)
+    for target, ctx in tvm.testing.enabled_targets():
+        for kind in ["vm", "debug"]:
+            mod = tvm.ir.IRModule.from_expr(func)
+            intrp = relay.create_executor(kind, mod=mod, ctx=ctx, target=target)
+            op_res = intrp.evaluate(func)(x, np.array(dyn_shape).astype(shape_type))
+            tvm.testing.assert_allclose(op_res.asnumpy(), ref_res, rtol=1e-5)
+
+
+@tvm.testing.uses_gpu
 def test_dyn_one_hot():
     def _get_oshape(indices_shape, depth, axis):
         oshape = []
--- tests/python/relay/dyn/test_dynamic_op_level4.py
+++ tests/python/relay/dyn/test_dynamic_op_level4.py
@@ -23,8 +23,7 @@ from tvm.relay.testing import run_infer_type
 import tvm.topi.testing
 
 
-# TODO(mbrookhart): Enable when VM supports heterogenus execution
-# @tvm.testing.uses_gpu
+@tvm.testing.uses_gpu
 def test_dynamic_strided_slice():
     def verify(dshape, begin, end, strides, output, slice_mode="end", test_ref=True, dtype="int32"):
         x = relay.var("x", relay.TensorType(dshape, "float32"))
@@ -64,7 +63,6 @@ def test_dynamic_strided_slice():
             op_res = intrp.evaluate()(x_data)
             tvm.testing.assert_allclose(op_res.asnumpy(), ref_res)
 
-    verify((1, 3, 10, 10), [0, 0, 0, 0], [-1, 3, 10, 10], [1], (0, 3, 10, 10), dtype="int64")
     verify(
         (1, 224, 224, 3),
         [0, 20, 20, 0],
