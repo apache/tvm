fb2fa36d51233d3eac4f0e00becb66f8561601b1
Cody Yu <comaniac0422@gmail.com>
2020-11-18 00:13:02 +0000

    fix test

--- tests/python/unittest/test_auto_scheduler_compute_dag.py
+++ tests/python/unittest/test_auto_scheduler_compute_dag.py
@@ -126,11 +126,8 @@ def test_stage_order():
         hardware_params=auto_scheduler.HardwareParams(100000, 16, 64),
     )
 
-    auto_scheduler.workload_registry.WORKLOAD_FUNC_REGISTRY.clear()
     task2 = pickle.loads(pickle.dumps(task))
-    assert (
-        json.loads(task2.workload_key)[0] in auto_scheduler.workload_registry.WORKLOAD_FUNC_REGISTRY
-    )
+    assert "test-key" in auto_scheduler.workload_registry.WORKLOAD_FUNC_REGISTRY
     assert str(task.dag.get_init_state()) == str(task2.dag.get_init_state())
     assert len(task.dag.get_init_state().stage_ops) == len(task2.dag.get_init_state().stage_ops)
     assert task.workload_key == task2.workload_key
