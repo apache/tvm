2d7413393ef0602ef61ca5c5a5e33c7e2b361b07
Andrew Reusch <areusch@octoml.ai>
2020-11-18 18:49:38 -0800

    black format

--- python/tvm/micro/debugger.py
+++ python/tvm/micro/debugger.py
@@ -113,16 +113,18 @@ class GdbDebugger(Debugger):
 
     @classmethod
     def _sigusr1_handler(cls, signum, stack_frame):  # pylint: disable=unused-argument
-        assert cls._STARTED_INSTANCE is not None, (
-            "overridden sigusr1 handler should not be invoked when GDB not started")
+        assert (
+            cls._STARTED_INSTANCE is not None
+        ), "overridden sigusr1 handler should not be invoked when GDB not started"
         signal.signal(signal.SIGINT, cls._STARTED_INSTANCE.old_sigint_handler)
         signal.signal(signal.SIGUSR1, cls._STARTED_INSTANCE.old_sigusr1_handler)
         cls._STARTED_INSTANCE._signals_reset_event.set()
 
     @classmethod
     def _sigint_handler(cls, signum, stack_frame):  # pylint: disable=unused-argument
-        assert cls._STARTED_INSTANCE is not None, (
-            "overridden sigint handler should not be invoked when GDB not started")
+        assert (
+            cls._STARTED_INSTANCE is not None
+        ), "overridden sigint handler should not be invoked when GDB not started"
         with cls._STARTED_INSTANCE._is_running_lock:
             exists = cls._STARTED_INSTANCE._is_running
         if exists:
