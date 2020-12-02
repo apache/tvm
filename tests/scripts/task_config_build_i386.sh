721a4b5c348faa11c86cf78e63da5f37a1392293
Luis Vega <vegaluisjose@gmail.com>
2020-12-02 16:34:49 +0000

    rollback ci files

--- tests/scripts/task_config_build_i386.sh
+++ tests/scripts/task_config_build_i386.sh
@@ -34,4 +34,3 @@ echo set\(CMAKE_CXX_COMPILER g++\) >> config.cmake
 echo set\(CMAKE_CXX_FLAGS -Werror\) >> config.cmake
 echo set\(USE_VTA_TSIM ON\) >> config.cmake
 echo set\(USE_VTA_FSIM ON\) >> config.cmake
-echo set\(USE_VERILATOR_HW ON\) >> config.cmake
