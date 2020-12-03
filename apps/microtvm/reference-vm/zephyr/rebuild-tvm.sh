0f4e9800ea8cbf6d5f335d4f7db47f160352ccca
Andrew Reusch <areusch@octoml.ai>
2020-12-02 17:07:41 -0800

    [µTVM] Modify reference VMs to support new µTVM demo (#7001)

--- apps/microtvm/reference-vm/zephyr/rebuild-tvm.sh
+++ apps/microtvm/reference-vm/zephyr/rebuild-tvm.sh
@@ -28,6 +28,7 @@ fi
 cp cmake/config.cmake "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 sed -i 's/USE_MICRO OFF/USE_MICRO ON/' config.cmake
+sed -i 's/USE_GRAPH_RUNTIME_DEBUG OFF/USE_GRAPH_RUNTIME_DEBUG ON/' config.cmake
 sed -i 's/USE_LLVM OFF/USE_LLVM ON/' config.cmake
 cmake ..
 make -j4
