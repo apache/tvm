23d393a764929eb8451b39236d17df9a9f88434f
Lianmin Zheng <lianminzheng@gmail.com>
2020-12-03 02:30:47 +0000

    update

--- python/tvm/auto_scheduler/measure.py
+++ python/tvm/auto_scheduler/measure.py
@@ -330,8 +330,8 @@ class LocalRunner(ProgramRunner):
         enable_cpu_cache_flush=False,
     ):
         if enable_cpu_cache_flush:
-            number=1
-            min_repeat_ms=0
+            number = 1
+            min_repeat_ms = 0
 
         self.__init_handle_by_constructor__(
             _ffi_api.LocalRunner,
