424093be2163920cbefabba8dd5863c5722864ae
ZihengJiang <ziheng@apache.org>
2020-12-14 14:49:52 -0800

    [TIR] Update build.

--- tests/python/unittest/test_tir_build.py
+++ tests/python/unittest/test_tir_build.py
@@ -1,15 +1,20 @@
 import tvm
 from tvm import tir
+from tvm.ir.transform import PassContext
 
 def test_scalar_add():
-    a = tir.Var("a", "float32") 
-    b = tir.Var("b", "float32") 
+    a = tir.Var("a", "float32")
+    b = tir.Var("b", "float32")
     c = a + b
-    c = tir.call_intrin("float32", "tir.ret", c) 
+    c = tir.call_intrin("float32", "tir.ret", c)
     c = tir.Evaluate(c)
     func = tir.PrimFunc([a, b], c)
-    mod = tvm.IRModule({'add': func})
-    func = tvm.build(mod['add'])
+    func = func.with_attr("global_symbol", "main")
+    pass_ctx = PassContext.current()
+    if pass_ctx.config.get("tir.noalias", True):
+        func = func.with_attr("tir.noalias", True)
+    mod = tvm.IRModule({'main': func})
+    func = tvm.build(mod)
     out = func(1.0, 2.0)
     assert out == 3.0
 
