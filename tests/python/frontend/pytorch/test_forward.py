53caa84ba77661d43b427583427c9cae184c1cc9
Masahiro Masuda <masahi129@gmail.com>
2020-12-20 19:01:48 +0900

    revert pytorch frontend change

--- tests/python/frontend/pytorch/test_forward.py
+++ tests/python/frontend/pytorch/test_forward.py
@@ -1675,10 +1675,10 @@ def test_forward_nms():
         boxes = torch.rand(num_boxes, box_len, dtype=torch.float) * 0.5
         boxes[:, 2] += boxes[:, 0]
         boxes[:, 3] += boxes[:, 1]
-        scores = torch.from_numpy(np.random.uniform(-1, 1, size=(num_boxes,)).astype(np.float32))
+        scores = torch.rand(num_boxes, dtype=torch.float)
         return boxes, scores
 
-    targets = ["llvm", "cuda"]
+    targets = ["llvm"]  # dynamic nms does not work on gpu
 
     for num_boxes, iou_thres in [(10, 0.3), (100, 0.5), (500, 0.9)]:
         in_boxes, in_scores = _gen_rand_inputs(num_boxes)
