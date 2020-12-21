0722844a6b53d35aa7db13923116edd090566de5
Perry Gibson <perry@gibsonic.org>
2020-12-21 19:31:13 +0000

    fixing linter formatting part 1

--- python/tvm/topi/x86/group_conv2d.py
+++ python/tvm/topi/x86/group_conv2d.py
@@ -32,11 +32,11 @@ from ..nn.util import infer_pad
 from ..nn.conv2d import _get_workload as _get_conv2d_workload
 
 
-def group_conv2d_nchw(data, kernel, strides, padding, dilation, groups,
-                      out_dtype):
+def group_conv2d_nchw(data, kernel, strides, padding, dilation, groups, out_dtype):
     """Compute group_conv2d with NCHW layout"""
-    return group_conv2d_nchw_spatial_pack(data, kernel, strides, padding,
-                                          dilation, groups, out_dtype)
+    return group_conv2d_nchw_spatial_pack(
+        data, kernel, strides, padding, dilation, groups, out_dtype
+    )
 
 
 def schedule_group_conv2d_nchw(outs):
@@ -44,8 +44,7 @@ def schedule_group_conv2d_nchw(outs):
     return schedule_group_conv2d_nchwc(outs)
 
 
-def _get_default_config(cfg, data, kernel, strides, padding, groups,
-                        out_dtype, layout='NCHW'):
+def _get_default_config(cfg, data, kernel, strides, padding, groups, out_dtype, layout='NCHW'):
     """
     Get default schedule config for the workload
     """
@@ -57,8 +56,7 @@ def _get_default_config(cfg, data, kernel, strides, padding, groups,
             static_data_shape.append(dim)
     data = te.placeholder(static_data_shape, dtype=data.dtype)
 
-    wkl = _get_conv2d_workload(data, kernel, strides, padding, out_dtype,
-                               layout, asymmetric_pad=True)
+    wkl = _get_conv2d_workload(data, kernel, strides, padding, out_dtype, layout)
     _fallback_schedule(cfg, wkl)
 
 
@@ -100,8 +98,9 @@ def _fallback_schedule(cfg, wkl):
 
 
 @autotvm.register_topi_compute("group_conv2d_nchw.x86")
-def group_conv2d_nchw_spatial_pack(cfg, data, kernel, strides, padding,
-                                   dilation, groups, out_dtype='float32'):
+def group_conv2d_nchw_spatial_pack(
+        cfg, data, kernel, strides, padding, dilation, groups, out_dtype='float32'
+):
     """
     Compute group conv2d with NCHW layout, using GSPC algorithm.
     https://arxiv.org/abs/2006.09791
@@ -151,71 +150,100 @@ def group_conv2d_nchw_spatial_pack(cfg, data, kernel, strides, padding,
 
     # If no config was set, we can fallback to default config.
     if cfg.is_fallback:
-        _get_default_config(cfg, te.placeholder((batch_size, in_channel, in_height, in_width),
-                                                dtype=data.dtype),
-                            te.placeholder((out_channel, in_channel // groups, k_height, k_width),
-                                           dtype=kernel.dtype),
-                            strides, padding, groups, out_dtype)
+        _get_default_config(
+            cfg,
+            te.placeholder((batch_size, in_channel, in_height, in_width), dtype=data.dtype),
+            te.placeholder(
+                (out_channel, in_channel // groups, k_height, k_width), dtype=kernel.dtype
+            ),
+            strides,
+            padding,
+            groups,
+            out_dtype,
+        )
 
-    oc_bn = cfg['tile_oc'].size[-1]
-    ic_bn = cfg['tile_ic'].size[-1]
+    oc_bn = cfg["tile_oc"].size[-1]
+    ic_bn = cfg["tile_ic"].size[-1]
+
     # pack data
-    DOPAD = (hpad != 0 or wpad != 0)
+    DOPAD = hpad != 0 or wpad != 0
     if DOPAD:
-        data_pad = pad(data,
-                       (0, 0, pad_top, pad_left),
-                       (0, 0, pad_bottom, pad_right),
-                       name="data_pad")
+        data_pad = pad(
+            data, (0, 0, pad_top, pad_left), (0, 0, pad_bottom, pad_right), name="data_pad"
+        )
     else:
         data_pad = data
 
-    shape = (groups, batch_size, kernel_depth // ic_bn,
-             pad_height, ic_bn, pad_width)
+    shape = (groups, batch_size, kernel_depth // ic_bn, pad_height, ic_bn, pad_width)
 
-    data_vec = te.compute(shape,
-                          lambda g, n, C, h, c, w:
-                          data_pad[n, C * ic_bn + c + kernel_depth * g, h, w],
-                          name='data_vec')
+    data_vec = te.compute(
+        shape,
+        lambda g, n, C, h, c, w: data_pad[n, C * ic_bn + c + kernel_depth * g, h, w],
+        name="data_vec",
+    )
 
     # pack kernel
-    shape = (groups, kernels_per_group//oc_bn, kernel_depth//ic_bn,
-             k_height, k_width, ic_bn, oc_bn)
-    kernel_vec = te.compute(shape,
-                            lambda g, out_channel, in_channel, h, w, ci, co:
-                            kernel[(out_channel * oc_bn + co + g * kernels_per_group),
-                                   in_channel * ic_bn + ci, h, w],
-                            name='kernel_vec')
+    shape = (
+        groups,
+        kernels_per_group // oc_bn,
+        kernel_depth // ic_bn,
+        k_height,
+        k_width,
+        ic_bn,
+        oc_bn,
+    )
+
+    kernel_vec = te.compute(
+        shape,
+        lambda g, out_channel, in_channel, h, w, ci, co: kernel[
+            (out_channel * oc_bn + co + g * kernels_per_group), in_channel * ic_bn + ci, h, w
+        ],
+        name="kernel_vec",
+    )
 
     # convolution
-    oshape = (groups, batch_size, kernels_per_group//oc_bn,
-              out_height, out_width, oc_bn)
+    oshape = (groups, batch_size, kernels_per_group//oc_bn, out_height, out_width, oc_bn)
     unpack_shape = (batch_size, out_channel, out_height, out_width)
 
-    ic = te.reduce_axis((0, (kernel_depth)), name='ic')
-    kh = te.reduce_axis((0, k_height), name='kh')
-    kw = te.reduce_axis((0, k_width), name='kw')
+    ic = te.reduce_axis((0, (kernel_depth)), name="ic")
+    kh = te.reduce_axis((0, k_height), name="kh")
+    kw = te.reduce_axis((0, k_width), name="kw")
+
     idxmod = tvm.tir.indexmod
     idxdiv = tvm.tir.indexdiv
+    conv = te.compute(
+        oshape,
+        lambda g, n, oc_chunk, oh, ow, oc_block: te.sum(
+            data_vec[
+                g,
+                n,
+                idxdiv(ic, ic_bn),
+                oh * stride_h + kh * dilation_h,
+                idxmod(ic, ic_bn),
+                ow * stride_w + kw * dilation_w,
+            ].astype(out_dtype)
+            * kernel_vec[
+                g, oc_chunk, idxdiv(ic, ic_bn), kh, kw, idxmod(ic, ic_bn), oc_block
+            ].astype(out_dtype),
+            axis=[ic, kh, kw],
+        ),
+        name="conv",
+    )
 
-    conv = te.compute(oshape, lambda g, n, oc_chunk, oh, ow, oc_block:
-                      te.sum(data_vec[g, n, idxdiv(ic, ic_bn),
-                                      oh * stride_h + kh * dilation_h,
-                                      idxmod(ic, ic_bn),
-                                      ow * stride_w + kw * dilation_w].astype(out_dtype) *
-                             kernel_vec[g, oc_chunk, idxdiv(ic, ic_bn),
-                                        kh, kw, idxmod(ic, ic_bn),
-                                        oc_block].astype(out_dtype),
-                             axis=[ic, kh, kw]), name='conv')
+    unpack = te.compute(
+        unpack_shape,
+        lambda n, c, h, w: conv[
+            idxdiv(c, kernels_per_group),
+            n,
+            idxmod(idxdiv(c, oc_bn), (kernels_per_group // oc_bn)),
+            h,
+            w,
+            idxmod(idxmod(c, oc_bn), kernels_per_group),
+        ].astype(out_dtype),
+        name="output_unpack",
+        tag="group_conv2d_nchw",
+    )
 
-    unpack = te.compute(unpack_shape,
-                        lambda n, c, h, w:
-                        conv[idxdiv(c, kernels_per_group), n,
-                             idxmod(idxdiv(c, oc_bn), (kernels_per_group // oc_bn)),
-                             h, w,
-                             idxmod(idxmod(c, oc_bn), kernels_per_group)]
-                        .astype(out_dtype),
-                        name='output_unpack',
-                        tag='group_conv2d_nchw')
     return unpack
 
 
@@ -235,7 +263,7 @@ def schedule_group_conv2d_nchwc(cfg, outs):
                 if isinstance(tensor.op, tvm.te.ComputeOp) and tensor.op not in scheduled_ops:
                     traverse(tensor.op)
 
-        if 'group_conv2d_nchw' in op.tag:
+        if "group_conv2d_nchw" in op.tag:
             output = op.output(0)
 
             if "tile_ic" not in cfg:
@@ -252,8 +280,7 @@ def schedule_group_conv2d_nchwc(cfg, outs):
                 data_pad = data
                 data = data_pad.op.input_tensors[0]
 
-            args = [s, cfg, data, data_pad, data_vec, kernel_vec, conv_out,
-                    output, outs[0]]
+            args = [s, cfg, data, data_pad, data_vec, kernel_vec, conv_out, output, outs[0]]
             _schedule_gspc_nchw(*args)
 
         scheduled_ops.append(op)
@@ -265,13 +292,18 @@ def schedule_group_conv2d_nchwc(cfg, outs):
 def _schedule_gspc_nchw(s, cfg, data, data_pad, data_vec, kernel_vec,
                         conv_out, output, last):
     """Schedule GSPC"""
-    ic_bn, oc_bn, reg_n, unroll_kw = (cfg["tile_ic"].size[-1], cfg["tile_oc"].size[-1],
-                                      cfg["tile_ow"].size[-1], cfg["unroll_kw"].val)
+    ic_bn, oc_bn, reg_n, unroll_kw = (
+        cfg["tile_ic"].size[-1],
+        cfg["tile_oc"].size[-1],
+        cfg["tile_ow"].size[-1],
+        cfg["unroll_kw"].val,
+    )
+
 
     # no stride and padding info here
     padding = infer_pad(data, data_pad)
     hpad, wpad = padding
-    DOPAD = (hpad != 0 or wpad != 0)
+    DOPAD = hpad != 0 or wpad != 0
 
     _, W = data, kernel_vec
     A0, A1 = data_pad, data_vec
@@ -297,7 +329,7 @@ def _schedule_gspc_nchw(s, cfg, data, data_pad, data_vec, kernel_vec,
 
     # schedule conv
     C, O0, O = conv_out, output, last
-    CC = s.cache_write(C, 'global')
+    CC = s.cache_write(C, "global")
 
     _, _, oc_chunk, oh, ow, oc_block = s[C].op.axis
 
@@ -314,12 +346,10 @@ def _schedule_gspc_nchw(s, cfg, data, data_pad, data_vec, kernel_vec,
     ic_chunk, ic_block = s[CC].split(ic, factor=ic_bn)
 
     if unroll_kw:
-        s[CC].reorder(oc_chunk, oh, ow_chunk, ic_chunk, kh, ic_block, kw,
-                      ow_block, oc_block)
+        s[CC].reorder(oc_chunk, oh, ow_chunk, ic_chunk, kh, ic_block, kw, ow_block, oc_block)
         s[CC].unroll(kw)
     else:
-        s[CC].reorder(oc_chunk, oh, ow_chunk, ic_chunk, kh, kw, ic_block,
-                      ow_block, oc_block)
+        s[CC].reorder(oc_chunk, oh, ow_chunk, ic_chunk, kh, kw, ic_block, ow_block, oc_block)
 
     parallel_axis = s[CC].fuse(groups, batch, oc_chunk, oh)
 
