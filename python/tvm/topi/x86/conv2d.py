4fe405d5a8ca6d57445005c0bfeb0c463b1b3798
Perry Gibson (gabriel) <p.gibson.2@research.gla.ac.uk>
2020-12-24 15:33:57 +0000

    Fixed bug, tested locally

--- python/tvm/topi/x86/conv2d.py
+++ python/tvm/topi/x86/conv2d.py
@@ -69,8 +69,9 @@ def _conv2d_infer_layout(workload, cfg):
     idxdiv = tvm.tir.indexdiv
 
     pt, pl, pb, pr = get_pad_tuple(padding, (k_height, k_width))
-    dilated_kernel_h = (workload.hkernel - 1) * workload.hdilation + 1
-    dilated_kernel_w = (workload.wkernel - 1) * workload.wdilation + 1
+    hdilation, wdilation = dilation if isinstance(dilation, (tuple, list)) else (dilation, dilation)
+    dilated_kernel_h = (k_height - 1) * hdilation + 1
+    dilated_kernel_w = (k_width - 1) * wdilation + 1
     out_height = idxdiv(in_height + pt + pb - dilated_kernel_h, strides[0]) + 1
     out_width = idxdiv(in_width + pl + pr - dilated_kernel_w, strides[1]) + 1
     tile_ic, tile_oc = cfg["tile_ic"].size[-1], cfg["tile_oc"].size[-1]
