6ffd740ade7fe3d06c108d6c0167f93666ce03ed
Lianmin Zheng <lianminzheng@gmail.com>
2020-12-24 19:26:52 -0800

    [Relay] Add fast_softmax (#7163)
    * [Relay] Add fast_softmax
    
    * fix
    
    * fix

--- python/tvm/relay/op/strategy/generic.py
+++ python/tvm/relay/op/strategy/generic.py
@@ -154,6 +154,20 @@ def softmax_strategy(attrs, inputs, out_type, target):
     return strategy
 
 
+@override_native_generic_func("fast_softmax_strategy")
+def fast_softmax_strategy(attrs, inputs, out_type, target):
+    """fast softmax generic strategy"""
+    # NOTE: This op does not have an optimized manual schedule,
+    # so it should only be used together with auto-scheduler.
+    strategy = _op.OpStrategy()
+    strategy.add_implementation(
+        wrap_compute_softmax(topi.nn.fast_softmax),
+        naive_schedule,
+        name="fast_softmax.generic",
+    )
+    return strategy
+
+
 # log_softmax
 @generic_func
 def schedule_log_softmax(attrs, outs, target):
