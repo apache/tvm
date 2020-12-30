f2ab977de0ac543cae77d3bef76af1b56dd61eed
roger-zhao <roger-zhao@users.noreply.github.com>
2020-12-30 15:45:52 +0800

    [AutoTVM-FIX] avoid unexpected value(1) of search space when get length for uninitiated search space (#7175)
    * [AutoTVM-FIX] avoid unexpected value(1) of search space when get length for uninitiated search space
    
    * Update python/tvm/autotvm/task/space.py
    
    Co-authored-by: Cody Yu <comaniac0422@gmail.com>
    
    Co-authored-by: ZhaoYanjie <roger.zhao@montage-tech.com>
    Co-authored-by: Cody Yu <comaniac0422@gmail.com>

--- python/tvm/autotvm/task/space.py
+++ python/tvm/autotvm/task/space.py
@@ -836,6 +836,8 @@ class ConfigSpace(object):
         return [Axis(None, i) for i in range(space_class.get_num_output(axes, policy, **kwargs))]
 
     def __len__(self):
+        if not self.space_map:
+            return 0
         if self._length is None:
             self._length = int(np.prod([len(x) for x in self.space_map.values()]))
         return self._length
