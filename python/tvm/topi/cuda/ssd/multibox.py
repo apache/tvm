5bd29677cb3226519914616d705711fd39d56602
hsuanguo <hsuan.guo@gmail.com>
2021-01-04 19:24:45 +0000

    format code

--- python/tvm/topi/cuda/ssd/multibox.py
+++ python/tvm/topi/cuda/ssd/multibox.py
@@ -344,11 +344,22 @@ def transform_loc_ir(
         out_base_idx = i * num_anchors * 6 + tid * 6
         out_loc[out_base_idx] = cls_id[tid] - 1.0
         out_loc[out_base_idx + 1] = score[tid]
-        out_loc[out_base_idx + 2], out_loc[out_base_idx + 3], out_loc[out_base_idx + 4], \
-            out_loc[out_base_idx + 5] = transform_loc(loc_pred, tid * 4,
-                                                      anchor, j * 4, clip, variances[0],
-                                                      variances[1], variances[2],
-                                                      variances[3])
+        (
+            out_loc[out_base_idx + 2],
+            out_loc[out_base_idx + 3],
+            out_loc[out_base_idx + 4],
+            out_loc[out_base_idx + 5],
+        ) = transform_loc(
+            loc_pred,
+            tid * 4,
+            anchor,
+            j * 4,
+            clip,
+            variances[0],
+            variances[1],
+            variances[2],
+            variances[3],
+        )
 
     return ib.get()
 
