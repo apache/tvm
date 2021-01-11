3a7e190e4a3add5ad0e427a9774ddea5508bff6f
Tristan Konolige <tristan.konolige@gmail.com>
2021-01-11 14:00:16 -0800

    [FIX,TUTORIALS] Import tvm.testing in tutorials that use it (#7248)

--- tutorials/get_started/relay_quick_start.py
+++ tutorials/get_started/relay_quick_start.py
@@ -44,6 +44,7 @@ from tvm.relay import testing
 import tvm
 from tvm import te
 from tvm.contrib import graph_runtime
+import tvm.testing
 
 ######################################################################
 # Define Neural Network in Relay
