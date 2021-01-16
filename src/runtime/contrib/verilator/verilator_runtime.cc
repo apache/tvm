3e695cd999207856d7ff8376f29e64355042d2d9
Luis Vega <vegaluisjose@gmail.com>
2021-01-16 00:29:04 +0000

    fix header order cpplint

--- src/runtime/contrib/verilator/verilator_runtime.cc
+++ src/runtime/contrib/verilator/verilator_runtime.cc
@@ -21,21 +21,20 @@
  * \file src/runtime/contrib/verilator/verilator_runtime.cc
  * \brief A simple JSON runtime for Verilator.
  */
+#include <dlfcn.h>
 
 #include <tvm/runtime/ndarray.h>
 #include <tvm/runtime/registry.h>
+
+#include <cstddef>
+#include <string>
+#include <vector>
 
 #include "../../library_module.h"
 #include "../json/json_node.h"
 #include "../json/json_runtime.h"
 #include "verilator_device.h"
 #include "verilator_kernel.h"
-
-#include <cstddef>
-#include <string>
-#include <vector>
-
-#include <dlfcn.h>
 
 namespace tvm {
 namespace runtime {
