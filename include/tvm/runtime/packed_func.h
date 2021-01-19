c29b07c40c707a217a13e672802c2d5c2dcfbc3e
Tristan Konolige <tristan.konolige@gmail.com>
2021-01-19 09:32:41 -0800

    documentation

--- include/tvm/runtime/packed_func.h
+++ include/tvm/runtime/packed_func.h
@@ -584,8 +584,11 @@ class TVMMovableArgValue_ : public TVMPODValue_ {
 class TVMMovableArgValueWithContext_ {
  public:
   /*!
-   * \brief move constructor from anoter return value.
+   * \brief move constructor from another return value.
    * \param other The other return value.
+   * \param type_code The code associated with the type of the value.
+   * \param arg_index In a function call, this argument is at index arg_index (0-indexed).
+   * \param optional_name Name of the function being called. Can be nullptr if the function is not named.
    */
   TVMMovableArgValueWithContext_(TVMValue value, int type_code, int arg_index,
                                  const std::string* optional_name)
