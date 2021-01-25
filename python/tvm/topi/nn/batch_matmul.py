01c469d35ec035d0ceff2d7d8c09255d65e7af38
liuxin.ai <liuxin.ai@bytedance.com>
2021-01-25 15:27:53 +0800

    code format

--- python/tvm/topi/nn/batch_matmul.py
+++ python/tvm/topi/nn/batch_matmul.py
@@ -99,5 +99,5 @@ def batch_matmul_legalize(attrs, inputs, types):
         The legalized expr
     """
     # not to change by default
-    #pylint: disable=unused-argument
+    # pylint: disable=unused-argument
     return None
