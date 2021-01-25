53d042039bc506ce6f82ddf09c926168c817e7ad
Cody Yu <comaniac0422@gmail.com>
2021-01-25 03:27:34 -0800

    [AutoScheduler] Separate shapes from DAG hash and enable schedule sharing (#7317)
    * [AutoScheduler] Separate shapes from DAG hash and enable schedule sharing
    
    * Update CI logs
    
    * lint
    
    * fix registry
    
    * add message; fix layout rewrite mismatch
    
    * update message
    
    * support other formats

--- python/tvm/auto_scheduler/measure_record.py
+++ python/tvm/auto_scheduler/measure_record.py
@@ -27,6 +27,7 @@ import numpy as np
 import tvm._ffi
 from tvm.runtime import Object
 from .measure import MeasureErrorNo, MeasureCallback
+from .utils import decode_workload_key
 from . import _ffi_api
 
 logger = logging.getLogger("auto_scheduler")
@@ -59,7 +60,36 @@ class RecordReader(Object):
     """
 
     def __init__(self, filename):
+        # a set to prevent print duplicated message
+        self.messages = set()
+
         self.__init_handle_by_constructor__(_ffi_api.RecordReader, filename)
+
+    def check_workload_key(self, inputs):
+        """Check and throw warnings for records with old format workload key.
+
+        Parameters
+        ----------
+        inputs: List[MeasureInput]
+            The measure inputs to be checked.
+
+        Notes
+        -----
+        This checker could be deprecated in the future.
+        """
+        for inp in inputs:
+            _, args = decode_workload_key(inp.task.workload_key)
+            if args is None:
+                continue
+            if not args:
+                msg = (
+                    "MeasureInput with old format workload key %s should be updated "
+                    "using the script from https://github.com/apache/tvm/pull/7317."
+                    % inp.task.workload_key
+                )
+                if msg not in self.messages:
+                    self.messages.add(msg)
+                    logger.warning(msg)
 
     def read_lines(self, max_lines=None, skip_lines=0):
         """Read multiple lines from the log file.
@@ -88,6 +118,7 @@ class RecordReader(Object):
         inputs, results = _ffi_api.RecordReaderReadLines(
             self, max_lines if max_lines else -1, skip_lines
         )
+        self.check_workload_key(inputs)
         return inputs, results
 
     def __iter__(self):
@@ -95,7 +126,67 @@ class RecordReader(Object):
             ret = _ffi_api.RecordReaderReadNext(self)
             if not ret:
                 break
+            self.check_workload_key([ret[0]])
             yield ret[0], ret[1]  # (input, result)
+
+
+def calc_workload_dis_factor(target_workload_key, workload_key):
+    """Calculate the distance factor of the workload to the target workload.
+    If two workloads are not compatible at all (i.e., different compute DAG or function),
+    then the distance factor is "inf". Otherwise, we calculate the factor by traversing
+    the workload arguments, which are the arguments of the compute function,
+    or the output shapes for the ComputeDAG. The factor is calculated by the following rules:
+
+    1. For non-zero integer values: `product(target_arg / candidate_arg)`.
+    2. For non-integer or zero values: "inf" if not equal else 1.
+
+    As a result, factor=1 is the optimal when two workloads are identical.
+
+    Parameters
+    ----------
+    target_workload_key: str
+        The target workload key in JSON string.
+
+    workload_key: str
+        The candidate workload key in JSON string.
+
+    Returns
+    -------
+    dis_f: float
+        The distance factor.
+    """
+
+    def flatten_list(inp):
+        ret = []
+        for elt in inp:
+            if isinstance(elt, list):
+                ret += flatten_list(elt)
+            else:
+                ret.append(elt)
+        return ret
+
+    target_key, target_args = decode_workload_key(target_workload_key)
+    target_args = flatten_list(target_args) if target_args is not None else []
+    key, args = decode_workload_key(workload_key)
+    args = flatten_list(args) if args is not None else []
+
+    # Not even the same func/DAG.
+    if key != target_key or len(target_args) != len(args):
+        return float("inf")
+
+    dis_f = 1
+    for target_arg, arg in zip(target_args, args):
+        if isinstance(target_arg, int):
+            if target_arg == 0 or arg == 0:
+                if target_arg != arg:
+                    return float("inf")
+            elif target_arg % arg != 0:
+                return float("inf")
+            else:
+                dis_f *= target_arg / arg
+        elif target_arg != arg:
+            return float("inf")
+    return dis_f
 
 
 def load_record_from_string(record):
@@ -174,7 +265,7 @@ def save_records(filename, inputs, results):
     _ffi_api.SaveRecords(filename, inputs, results)
 
 
-def load_best_record(filename, workload_key=None, target=None):
+def load_best_record(filename, workload_key=None, target=None, include_compatible=False):
     """Return the best measurement pair form a log file. This may return none results if
     there is no legal measure pair with the specified workload_key/target found from the log file.
 
@@ -188,6 +279,8 @@ def load_best_record(filename, workload_key=None, target=None):
     target : Optional[tvm.target.Target]
         The target device.
         With `None`, this returns the best measure pair of all target devices.
+    include_compatible: bool
+        When set to True, all compatible records in the log file will be considered.
 
     Returns
     -------
@@ -204,13 +297,23 @@ def load_best_record(filename, workload_key=None, target=None):
     for inp, res in log_reader:
         if res.error_no != MeasureErrorNo.NO_ERROR:
             continue
-        if workload_key and inp.task.workload_key != workload_key:
-            continue
         if target and inp.task.target.kind.name != target.kind.name:
             continue
 
         costs = [v.value for v in res.costs]
         cost = np.mean(costs)
+
+        if workload_key is not None:
+            dis_f = calc_workload_dis_factor(workload_key, inp.task.workload_key)
+            if dis_f == float("inf"):
+                continue
+            if not include_compatible and dis_f != 1:
+                continue
+
+            # Since different workloads have different FLOPS, we multiply the factor to
+            # eliminate this difference, which is basically the concept of throughput.
+            cost *= dis_f
+
         if cost < best_cost:
             best_cost = cost
             best_inp = inp
@@ -267,12 +370,8 @@ def distill_record_file(in_file, out_file):
     logger.info("Extract %d best records from %s to %s", len(inputs), in_file, out_file)
 
 
-"""
-Usage:
-* Distill the best entries from a large log file
-e.g. python -m tvm.auto_scheduler.measure_record --mode distill --i input.json
-"""
-if __name__ == "__main__":
+def main():
+    """The main function for CLI."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", choices=["distill"], required=True)
     parser.add_argument("--i", type=str, help="input file")
@@ -285,3 +384,12 @@ if __name__ == "__main__":
     if args.mode == "distill":
         args.o = args.o or args.i + ".best.json"
         distill_record_file(args.i, args.o)
+
+
+"""
+Usage:
+* Distill the best entries from a large log file
+e.g. python -m tvm.auto_scheduler.measure_record --mode distill --i input.json
+"""
+if __name__ == "__main__":
+    main()
