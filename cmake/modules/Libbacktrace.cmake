0a83a2f6a8f715b9714a1b51d8d8c621876bff0d
Tristan Konolige <tristan.konolige@gmail.com>
2021-01-29 09:46:55 -0800

    apply patch after update

--- cmake/modules/Libbacktrace.cmake
+++ cmake/modules/Libbacktrace.cmake
@@ -22,7 +22,7 @@ set(TAG dedbe13fda00253fe5d4f2fb812c909729ed5937)
 ExternalProject_Add(project_libbacktrace
   PREFIX libbacktrace
   GIT_REPOSITORY https://github.com/ianlancetaylor/libbacktrace.git
-  GIT_TAG ${TAG}
+  GIT_TAG "${TAG}"
   CONFIGURE_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/src/project_libbacktrace/configure"
                     "--prefix=${CMAKE_CURRENT_BINARY_DIR}/libbacktrace" --with-pic
   INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/libbacktrace"
@@ -40,7 +40,8 @@ ExternalProject_Add(project_libbacktrace
 ExternalProject_Add_Step(project_libbacktrace update-new
   DEPENDERS configure
   DEPENDS "${CMAKE_CURRENT_LIST_DIR}/Libbacktrace.cmake"
-  COMMAND cd ${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/src/project_libbacktrace; git checkout -f ${TAG}
+  COMMAND cd ${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/src/project_libbacktrace; git checkout -f ${TAG};
+          git apply ${CMAKE_CURRENT_LIST_DIR}/libbacktrace_macos.patch
   )
 
 add_library(libbacktrace STATIC IMPORTED)
