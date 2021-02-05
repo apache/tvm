4df530d1ea88a5b4028a1879311b1032645be253
Xiyou Zhou <xiyou.zhou@gmail.com>
2021-02-05 10:57:45 -0800

    Add cuda tags and unit test (#7410)
    * Add cuda tags and unit test
    
    * Add missing space
    
    * Remove extra indent
    
    * Modify macro def position
    
    * Fix clang format
    
    * Fix clang format for set_config

--- tests/python/unittest/test_target_target.py
+++ tests/python/unittest/test_target_target.py
@@ -131,6 +131,26 @@ def test_composite_target():
     assert opencl_device.kind.name == "opencl"
 
 
+def test_target_tag_0():
+    tgt = tvm.target.Target("nvidia/geforce-rtx-2080-ti")
+    assert tgt.kind.name == "cuda"
+    assert tgt.attrs["arch"] == "sm_75"
+    assert tgt.attrs["shared_memory_per_block"] == 49152
+    assert tgt.attrs["max_threads_per_block"] == 1024
+    assert tgt.attrs["thread_warp_size"] == 32
+    assert tgt.attrs["registers_per_block"] == 65536
+
+
+def test_target_tag_1():
+    tgt = tvm.target.Target("nvidia/jetson-nano")
+    assert tgt.kind.name == "cuda"
+    assert tgt.attrs["arch"] == "sm_53"
+    assert tgt.attrs["shared_memory_per_block"] == 49152
+    assert tgt.attrs["max_threads_per_block"] == 1024
+    assert tgt.attrs["thread_warp_size"] == 32
+    assert tgt.attrs["registers_per_block"] == 32768
+
+
 if __name__ == "__main__":
     test_target_dispatch()
     test_target_string_parse()
