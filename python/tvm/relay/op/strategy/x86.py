68b7e7147fff8960754e4a3a788fd2474749d121
Yao Wang <kevinthesunwy@gmail.com>
2021-02-09 15:58:52 -0800

    [Relay][Topi][CPU] Dense with weight transform (#7404)
    * Add CPU dense weight transform
    
    * Fix format
    
    * Fix python format
    
    * Fix pylint
    
    * Minor fix
    
    * Add test
    
    * Do not need to infer layout for dense
    
    * Fix test
    
    * Rename dense_pack
    
    * Fix test
    
    * Fix lint
    
    * Fix dynamic shape dense
    
    * Fix lint
    
    * Fix autotvm task extraction test
    
    * Disable AlterOpLayout in micro_tflite.py tutorial

--- python/tvm/relay/op/strategy/x86.py
+++ python/tvm/relay/op/strategy/x86.py
@@ -364,7 +364,6 @@ def conv1d_strategy_cpu(attrs, inputs, out_type, target):
 def dense_strategy_cpu(attrs, inputs, out_type, target):
     """dense x86 strategy"""
     strategy = _op.OpStrategy()
-    m, _ = inputs[0].shape
     same_type = inputs[0].dtype == inputs[1].dtype == out_type.dtype
     dtype = inputs[0].dtype
     u8s8s32 = dtype == "uint8" and inputs[1].dtype == "int8" and out_type.dtype == "int32"
@@ -372,6 +371,13 @@ def dense_strategy_cpu(attrs, inputs, out_type, target):
         wrap_compute_dense(topi.x86.dense_nopack),
         wrap_topi_schedule(topi.x86.schedule_dense_nopack),
         name="dense_nopack.x86",
+        plevel=5,
+    )
+
+    strategy.add_implementation(
+        wrap_compute_dense(topi.x86.dense_pack),
+        wrap_topi_schedule(topi.x86.schedule_dense_pack),
+        name="dense_pack.x86",
         plevel=10,
     )
 
@@ -407,14 +413,18 @@ def dense_strategy_cpu(attrs, inputs, out_type, target):
                 name="dense_mkldnn.x86",
                 plevel=15,
             )
-    with SpecializedCondition(m >= 16):
-        # this implementation may not be well-optimized, so use plevel=5 for now.
-        strategy.add_implementation(
-            wrap_compute_dense(topi.x86.dense_pack),
-            wrap_topi_schedule(topi.x86.schedule_dense_pack),
-            name="dense_pack.x86",
-            plevel=5,
-        )
+    return strategy
+
+
+@dense_pack_strategy.register("cpu")
+def dense_pack_strategy_cpu(attrs, inputs, out_type, target):
+    """dense_pack x86 strategy"""
+    strategy = _op.OpStrategy()
+    strategy.add_implementation(
+        wrap_compute_dense(topi.x86.dense_pack),
+        wrap_topi_schedule(topi.x86.schedule_dense_pack),
+        name="dense_pack.x86",
+    )
     return strategy
 
 
