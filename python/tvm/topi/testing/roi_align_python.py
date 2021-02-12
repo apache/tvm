14a364f9845e15f80c6b4bbf7ad5d4a9b65afe7b
electriclilies <lilyorthsmith@gmail.com>
2021-02-12 11:43:20 -0800

    shorten code

--- python/tvm/topi/testing/roi_align_python.py
+++ python/tvm/topi/testing/roi_align_python.py
@@ -87,10 +87,8 @@ def roi_align_nchw_python(a_np, rois_np, pooled_size, spatial_scale, sample_rati
                             y = roi_start_h + ph * bin_h + (iy + 0.5) * bin_h / roi_bin_grid_h
                             x = roi_start_w + pw * bin_w + (ix + 0.5) * bin_w / roi_bin_grid_w
                             if avg_mode:
-                                total += _bilinear(batch_index, c, y, x)
+                                total += _bilinear(batch_index, c, y, x) / count
                             if max_mode:
                                 total = max(total, _bilinear(batch_index, c, y, x))
-                    if avg_mode:
-                        total = total / count
                     b_np[i, c, ph, pw] = total
     return b_np
