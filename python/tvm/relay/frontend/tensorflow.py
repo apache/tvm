187e081f47ec51c0b7484a3703d4db8af8a32921
zxy844288792 <zhoxingy@amazon.com>
2021-02-23 01:13:28 +0000

    add TF2.x raw_ops.all axis range support

--- python/tvm/relay/frontend/tensorflow.py
+++ python/tvm/relay/frontend/tensorflow.py
@@ -1976,6 +1976,12 @@ def _range():
                 # Symbolic delta
                 delta = inputs[2]
 
+        # if all attributes are constant, evalute the range function and return relay.const
+        if all([isinstance(start, (np.int32, np.int64, int, np.float32, np.float64, float)),
+                isinstance(limit, (np.int32, np.int64, int, np.float32, np.float64, float)),
+                isinstance(delta, (np.int32, np.int64, int, np.float32, np.float64, float))]):
+            return tvm.relay.const(list(range(start, limit, delta)))
+
         dtype = attr["Tidx"].name if "Tidx" in attr else str(start.dtype)
         if isinstance(start, (np.int32, np.int64, int, np.float32, np.float64, float)):
             start = _expr.const(start, dtype=dtype)
