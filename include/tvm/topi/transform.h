e9a01141a704e34da5efdc28be55a0ebe229333c
Ubuntu <ubuntu@ip-172-31-42-251.us-east-2.compute.internal>
2021-03-05 04:17:44 +0000

    Clang Format

--- include/tvm/topi/transform.h
+++ include/tvm/topi/transform.h
@@ -1134,7 +1134,7 @@ inline Tensor gather(const Tensor& data, int axis, const Tensor& indices,
   size_t ndim_i = indices->shape.size();
   ICHECK_GE(ndim_d, 1) << "Cannot gather from a scalar.";
   ICHECK_EQ(ndim_d, ndim_i);
-  if(axis < 0){
+  if (axis < 0) {
     axis += ndim_d;
   }
   ICHECK_GE(axis, 0);
