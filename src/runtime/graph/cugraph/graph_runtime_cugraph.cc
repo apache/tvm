e43df6141d9ed9148de2c436a90934233c421c54
zhuochen <zhuochen@outlook.com>
2021-03-09 12:19:12 +0800

    fix format lint issue

--- src/runtime/graph/cugraph/graph_runtime_cugraph.cc
+++ src/runtime/graph/cugraph/graph_runtime_cugraph.cc
@@ -51,15 +51,12 @@ class GraphRuntimeCuGraph : public GraphRuntime {
 
   int EndCapture() {
     cudaGraph_t graph;
-    CUDA_CALL(cudaStreamEndCapture(static_cast<cudaStream_t>(capture_stream_),
-                                   &graph));
+    CUDA_CALL(cudaStreamEndCapture(static_cast<cudaStream_t>(capture_stream_), &graph));
 
     cudaGraphNode_t* nodes = NULL;
     size_t numNodes = 0;
     CUDA_CALL(cudaGraphGetNodes(graph, nodes, &numNodes));
-    LOG(INFO)
-        << "Num of nodes in the cuda graph created using stream capture API = "
-        << numNodes;
+    LOG(INFO) << "Num of nodes in the cuda graph created using stream capture API = " << numNodes;
 
     CUDA_CALL(cudaGraphInstantiate(&cu_graph_exec_, graph, NULL, NULL, 0));
     return 0;
@@ -70,35 +67,30 @@ class GraphRuntimeCuGraph : public GraphRuntime {
    * \param name The function which needs to be invoked.
    * \param sptr_to_self Packed function pointer.
    */
-  PackedFunc GetFunction(const std::string& name,
-                         const ObjectPtr<Object>& sptr_to_self);
+  PackedFunc GetFunction(const std::string& name, const ObjectPtr<Object>& sptr_to_self);
 
  private:
   TVMStreamHandle capture_stream_;
   cudaGraphExec_t cu_graph_exec_;
 };
 
-PackedFunc GraphRuntimeCuGraph::GetFunction(
-    const std::string& name, const ObjectPtr<Object>& sptr_to_self) {
+PackedFunc GraphRuntimeCuGraph::GetFunction(const std::string& name,
+                                            const ObjectPtr<Object>& sptr_to_self) {
   if (name == "run_cuda_graph") {
-    return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
-      *rv = this->RunCudaGraph();
-    });
+    return PackedFunc(
+        [sptr_to_self, this](TVMArgs args, TVMRetValue* rv) { *rv = this->RunCudaGraph(); });
   } else if (name == "start_capture") {
-    return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
-      *rv = this->StartCapture();
-    });
+    return PackedFunc(
+        [sptr_to_self, this](TVMArgs args, TVMRetValue* rv) { *rv = this->StartCapture(); });
   } else if (name == "end_capture") {
-    return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
-      *rv = this->EndCapture();
-    });
+    return PackedFunc(
+        [sptr_to_self, this](TVMArgs args, TVMRetValue* rv) { *rv = this->EndCapture(); });
   } else {
     return GraphRuntime::GetFunction(name, sptr_to_self);
   }
 }
 
-Module GraphRuntimeCuGraphCreate(const std::string& sym_json,
-                                 const tvm::runtime::Module& m,
+Module GraphRuntimeCuGraphCreate(const std::string& sym_json, const tvm::runtime::Module& m,
                                  const std::vector<TVMContext>& ctxs,
                                  PackedFunc lookup_linked_param_func) {
   auto exec = make_object<GraphRuntimeCuGraph>();
@@ -106,23 +98,21 @@ Module GraphRuntimeCuGraphCreate(const std::string& sym_json,
   return Module(exec);
 }
 
-TVM_REGISTER_GLOBAL("tvm.graph_runtime_cugraph.create")
-    .set_body([](TVMArgs args, TVMRetValue* rv) {
-      ICHECK_GE(args.num_args, 4)
-          << "The expected number of arguments for graph_runtime.create is "
-             "at least 4, but it has "
-          << args.num_args;
-      PackedFunc lookup_linked_param_func;
-      int ctx_start_arg = 2;
-      if (args[2].type_code() == kTVMPackedFuncHandle) {
-        lookup_linked_param_func = args[2];
-        ctx_start_arg++;
-      }
+TVM_REGISTER_GLOBAL("tvm.graph_runtime_cugraph.create").set_body([](TVMArgs args, TVMRetValue* rv) {
+  ICHECK_GE(args.num_args, 4) << "The expected number of arguments for graph_runtime.create is "
+                                 "at least 4, but it has "
+                              << args.num_args;
+  PackedFunc lookup_linked_param_func;
+  int ctx_start_arg = 2;
+  if (args[2].type_code() == kTVMPackedFuncHandle) {
+    lookup_linked_param_func = args[2];
+    ctx_start_arg++;
+  }
 
-      *rv = GraphRuntimeCuGraphCreate(args[0], args[1],
-                                      GetAllContext(args, ctx_start_arg),
-                                      lookup_linked_param_func);
-    });
+  *rv = GraphRuntimeCuGraphCreate(args[0], args[1], GetAllContext(args, ctx_start_arg),
+                                  lookup_linked_param_func);
+});
 
 }  // namespace runtime
 }  // namespace tvm
+
