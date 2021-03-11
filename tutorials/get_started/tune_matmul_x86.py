f020ca7c9ae5f1c58376a9b7d720fac750b41387
Chris Hoge <chris@hogepodge.com>
2021-03-11 15:12:56 -0800

    [docs] Getting Started with TVM: Auto Scheduler and MatMul
    Moves the auto scheduler with matmul example into the tutorial,
    expands to follow the flow of the larger getting started tutorial.
    Indended to follow the AutoTVM tutorial on matrix multiplication.

--- tutorials/get_started/tune_matmul_x86.py
+++ tutorials/get_started/tune_matmul_x86.py
@@ -15,24 +15,27 @@
 # specific language governing permissions and limitations
 # under the License.
 """
-Auto-scheduling Matrix Multiplication for CPU
-=============================================
+Optimizing Operators with Auto-scheduling
+=========================================
 **Author**: `Lianmin Zheng <https://github.com/merrymercy>`_, \
             `Chengfan Jia <https://github.com/jcf94/>`_
 
-This is a tutorial on how to use the auto-scheduler for CPUs.
+In this tutorial, we will show how TVM's Auto Scheduling feature can find
+optimal schedules without the need for writing a custom template.
 
-Different from the template-based :ref:`autotvm <tutorials-autotvm-sec>` which relies on
-manual templates to define the search space, the auto-scheduler does not require any templates.
-Users only need to write the computation declaration without any schedule commands or templates.
-The auto-scheduler can automatically generate a large search space and
-find a good schedule in the space.
+Different from the template-based :ref:`<autotvm_matmul>` which relies on
+manual templates to define the search space, the auto-scheduler does not
+require any templates.  Users only need to write the computation declaration
+without any schedule commands or templates.  The auto-scheduler can
+automatically generate a large search space and find a good schedule in the
+space.
 
 We use matrix multiplication as an example in this tutorial.
 
-Note that this tutorial will not run on Windows or recent versions of macOS. To
-get it to run, you will need to wrap the body of this tutorial in a :code:`if
-__name__ == "__main__":` block.
+.. note::
+  Note that this tutorial will not run on Windows or recent versions of macOS. To
+  get it to run, you will need to wrap the body of this tutorial in a :code:`if
+  __name__ == "__main__":` block.
 """
 
 import os
@@ -41,15 +44,18 @@ import numpy as np
 import tvm
 from tvm import te, auto_scheduler
 
-######################################################################
-# Define the computation
-# ^^^^^^^^^^^^^^^^^^^^^^
-# To begin with, let us define the computation of a matmul with bias add.
-# The function should return the list of input/output tensors.
-# From these tensors, the auto-scheduler can get the whole computational graph.
+################################################################################
+# Defining the Matrix Multiplication
+# ----------------------------------
+# To start, we define a matrix multiplication with a bias addition.  Note that
+# this uses standard operations available in TVMs Tensor Expression language.
+# The major difference is the use of the `auto_sceduler` decorator at the top
+# of the function definition.  The function should return a list of
+# input/output tensors.  From these tensors, the auto-scheduler can get the
+# whole computational graph.
 
 
-@auto_scheduler.register_workload
+@auto_scheduler.register_workload  # Note the auto_scheduler decorator
 def matmul_add(N, L, M, dtype):
     A = te.placeholder((N, L), name="A", dtype=dtype)
     B = te.placeholder((L, M), name="B", dtype=dtype)
@@ -67,12 +73,17 @@ def matmul_add(N, L, M, dtype):
     return [A, B, C, out]
 
 
-######################################################################
+################################################################################
 # Create the search task
-# ^^^^^^^^^^^^^^^^^^^^^^
-# We then create a search task with N=L=M=1024 and dtype="float32"
-# If your machine supports avx instructions, you can
+# ----------------------
+# With the function defined, we can now create the task for the auto_scheduler
+# to search against. We specify the particular parameters for this matrix
+# multiplication, in this case a multiplication of to square matricies of size
+# 1024x1024. We then create a search task with N=L=M=1024 and dtype="float32"
 #
+# .. note:: Improve performance with custom targets
+#   In order for TVM to take full advantage of specific hardware platforms,
+#   you will want to manuall specify your CPU capabilities. For example:
 #   - replace "llvm" below with "llvm -mcpu=core-avx2" to enable AVX2
 #   - replace "llvm" below with "llvm -mcpu=skylake-avx512" to enable AVX-512
 
@@ -84,15 +95,18 @@ task = tvm.auto_scheduler.SearchTask(func=matmul_add, args=(N, L, M, "float32"),
 print("Computational DAG:")
 print(task.compute_dag)
 
-######################################################################
+################################################################################
+# Set Parameters for Auto-Scheduler
+# ---------------------------------
 # Next, we set parameters for the auto-scheduler.
 #
-# * :code:`num_measure_trials` is the number of measurement trials we can use during the search.
-#   We only make 10 trials in this tutorial for a fast demonstration. In practice, 1000 is a
-#   good value for the search to converge. You can do more trials according to your time budget.
-# * In addition, we use :code:`RecordToFile` to dump measurement records into a file `matmul.json`.
-#   The measurement records can be used to query the history best, resume the search,
-#   and do more analyses later.
+# * :code:`num_measure_trials` is the number of measurement trials we can use
+#   during the search.  We only make 10 trials in this tutorial for a fast
+#   demonstration. In practice, 1000 is a good value for the search to converge.
+#   You can do more trials according to your time budget.
+# * In addition, we use :code:`RecordToFile` to log measurement records into a
+#   file `matmul.json`.  The measurement records can be used to query the history
+#   best, resume the search, and do more analyses later.
 # * see :any:`auto_scheduler.TuningOptions` for more parameters
 
 log_file = "matmul.json"
@@ -102,30 +116,32 @@ tune_option = auto_scheduler.TuningOptions(
     verbose=2,
 )
 
-######################################################################
+################################################################################
 # Run the search
-# ^^^^^^^^^^^^^^
-# Now we get all inputs ready. Pretty simple, isn't it?
-# We can kick off the search and let the auto-scheduler do its magic.
-# After some measurement trials, we can load the best schedule from the log
-# file and apply it.
+# --------------
+# Now we get all inputs ready. Pretty simple, isn't it?  We can kick off the
+# search and let the auto-scheduler do its magic.  After some measurement
+# trials, we can load the best schedule from the log file and apply it.
 
 # Run auto-tuning (search)
 task.tune(tune_option)
 # Apply the best schedule
 sch, args = task.apply_best(log_file)
 
-######################################################################
-# We can lower the schedule to see the IR after auto-scheduling.
-# The auto-scheduler correctly performs optimizations including multi-level tiling,
-# layout transformation, parallelization, vectorization, unrolling, and operator fusion.
+################################################################################
+# Inspecting the Optimized Schedule
+# ---------------------------------
+# We can lower the schedule to see the IR after auto-scheduling.  The
+# auto-scheduler correctly performs optimizations including multi-level tiling,
+# layout transformation, parallelization, vectorization, unrolling, and
+# operator fusion.
 
 print("Lowered TIR:")
 print(tvm.lower(sch, args, simple_mode=True))
 
-######################################################################
+################################################################################
 # Check correctness and evaluate performance
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ------------------------------------------
 # We build the binary and check its correctness and performance.
 
 func = tvm.build(sch, args, target)
@@ -152,26 +168,25 @@ print(
 )
 
 
-######################################################################
+################################################################################
 # Using the record file
-# ^^^^^^^^^^^^^^^^^^^^^
-# During the search, all measurement records are dumped into the record
-# file "matmul.json". The measurement records can be used to re-apply search results,
-# resume the search, and perform other analyses.
-
-######################################################################
-# Here is an example where we load the best schedule from a file,
-# and print the equivalent python schedule API. This can be used for
-# debugging and learning the behavior of the auto-scheduler.
+# ---------------------
+# During the search, all measurement records are logged into the record file
+# "matmul.json". The measurement records can be used to re-apply search
+# results, resume the search, and perform other analyses.
+#
+# Here is an example where we load the best schedule from a file, and print the
+# equivalent python schedule API. This can be used for debugging and learning
+# the behavior of the auto-scheduler.
 
 print("Equivalent python schedule:")
 print(task.print_best(log_file))
 
-######################################################################
-# A more complicated example is to resume the search.
-# In this case, we need to create the search policy and cost model by ourselves
-# and resume the status of search policy and cost model with the log file.
-# In the example below we resume the status and do more 5 trials.
+################################################################################
+# A more complicated example is to resume the search.  In this case, we need to
+# create the search policy and cost model by ourselves and resume the status of
+# search policy and cost model with the log file.  In the example below we
+# resume the status and do more 5 trials.
 
 
 def resume_search(task, log_file):
@@ -188,3 +203,12 @@ def resume_search(task, log_file):
 
 
 resume_search(task, log_file)
+
+################################################################################
+# Final Notes and Summary
+# -----------------------
+# In this tutorial, we have shown how to use the TVM Auto-Scheduler to
+# automatically optimize a matrix multiplication, without the need to specify a
+# search template.  It ends a series of examples that starts from the Tensor
+# Expression (TE) language that demonstrates how TVM can optimize computational
+# operations.
