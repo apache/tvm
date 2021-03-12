299b7f273f94c531f76276cbbc50e0863ac3bfa9
Chris Hoge <chris@hogepodge.com>
2021-03-11 20:48:19 -0800

    Apply patch suggestions from black

--- tutorials/get_started/auto_tuning_with_python.py
+++ tutorials/get_started/auto_tuning_with_python.py
@@ -84,9 +84,9 @@ import timeit
 
 model_url = "".join(
     [
-    "https://github.com/onnx/models/raw/",
-    "master/vision/classification/resnet/model/",
-    "resnet50-v2-7.onnx"
+        "https://github.com/onnx/models/raw/",
+        "master/vision/classification/resnet/model/",
+        "resnet50-v2-7.onnx",
     ]
 )
 
@@ -128,7 +128,7 @@ imagenet_mean = np.array([0.485, 0.456, 0.406])
 imagenet_stddev = np.array([0.229, 0.224, 0.225])
 norm_img_data = np.zeros(img_data.shape).astype("float32")
 for i in range(img_data.shape[0]):
-  norm_img_data[i, :, :] = (img_data[i, :, :] / 255 - imagenet_mean[i]) / imagenet_stddev[i]
+    norm_img_data[i, :, :] = (img_data[i, :, :] / 255 - imagenet_mean[i]) / imagenet_stddev[i]
 
 # Add the batch dimension
 img_data = np.expand_dims(norm_img_data, axis=0)
@@ -185,7 +185,7 @@ module = graph_runtime.GraphModule(lib["default"](ctx))
 dtype = "float32"
 module.set_input(input_name, img_data)
 module.run()
-output_shape=(1,1000)
+output_shape = (1, 1000)
 tvm_output = module.get_output(0, tvm.nd.empty(output_shape)).asnumpy()
 
 ################################################################################
@@ -199,13 +199,16 @@ tvm_output = module.get_output(0, tvm.nd.empty(output_shape)).asnumpy()
 
 timing_number = 10
 timing_repeat = 10
-unoptimized = np.array(
-        timeit.Timer(
-            lambda: module.run()).repeat(repeat=timing_repeat, number=timing_number)
-        )*1000/timing_number
-unoptimized = { "mean": np.mean(unoptimized),
-        "median": np.median(unoptimized),
-        "std": np.std(unoptimized)}
+unoptimized = (
+    np.array(timeit.Timer(lambda: module.run()).repeat(repeat=timing_repeat, number=timing_number))
+    * 1000
+    / timing_number
+)
+unoptimized = {
+    "mean": np.mean(unoptimized),
+    "median": np.median(unoptimized),
+    "std": np.std(unoptimized),
+}
 
 print(unoptimized)
 
@@ -278,14 +281,11 @@ from tvm import autotvm
 # set up some basic parameters
 number = 10
 repeat = 1
-min_repeat_ms = 0 # since we're tuning on a CPU, can be set to 0
-timeout = 10 # in seconds
+min_repeat_ms = 0  # since we're tuning on a CPU, can be set to 0
+timeout = 10  # in seconds
 
 # begin by extracting the taks from the onnx model
-tasks = autotvm.task.extract_from_program(
-    mod['main'],
-    target=target,
-    params=params)
+tasks = autotvm.task.extract_from_program(mod["main"], target=target, params=params)
 
 # create a TVM runner
 runner = autotvm.LocalRunner(
@@ -297,13 +297,13 @@ runner = autotvm.LocalRunner(
 
 # create a simple structure for holding tuning options
 tuning_option = {
-        "tuner": "xgb",
-        "trials": 100,
-        "early_stopping": 100,
-        "measure_option": autotvm.measure_option(
-            builder=autotvm.LocalBuilder(build_func="default"), runner=runner
-        ),
-        "tuning_records": "resnet-50-v2-autotuning.json",
+    "tuner": "xgb",
+    "trials": 100,
+    "early_stopping": 100,
+    "measure_option": autotvm.measure_option(
+        builder=autotvm.LocalBuilder(build_func="default"), runner=runner
+    ),
+    "tuning_records": "resnet-50-v2-autotuning.json",
 }
 
 ################################################################################
@@ -327,14 +327,14 @@ for i, task in enumerate(tasks):
     prefix = "[Task %2d/%2d] " % (i + 1, len(tasks))
     tuner_obj = XGBTuner(task, loss_type="rank")
     tuner_obj.tune(
-            n_trial=min(tuning_option["trials"], len(task.config_space)),
-            early_stopping=tuning_option["early_stopping"],
-            measure_option=tuning_option["measure_option"],
-            callbacks=[
-                autotvm.callback.progress_bar(tuning_option["trials"], prefix=prefix),
-                autotvm.callback.log_to_file(tuning_option["tuning_records"]),
-            ],
-        )
+        n_trial=min(tuning_option["trials"], len(task.config_space)),
+        early_stopping=tuning_option["early_stopping"],
+        measure_option=tuning_option["measure_option"],
+        callbacks=[
+            autotvm.callback.progress_bar(tuning_option["trials"], prefix=prefix),
+            autotvm.callback.log_to_file(tuning_option["tuning_records"]),
+        ],
+    )
 
 ################################################################################
 # The output from this tuning process will look something like this:
@@ -380,8 +380,7 @@ for i, task in enumerate(tasks):
 
 with autotvm.apply_history_best(tuning_option["tuning_records"]):
     with tvm.transform.PassContext(opt_level=3, config={}):
-        lib = relay.build(
-                mod, target=target, params=params)
+        lib = relay.build(mod, target=target, params=params)
 
 ctx = tvm.context(str(target), 0)
 module = graph_runtime.GraphModule(lib["default"](ctx))
@@ -392,7 +391,7 @@ module = graph_runtime.GraphModule(lib["default"](ctx))
 dtype = "float32"
 module.set_input(input_name, img_data)
 module.run()
-output_shape=(1,1000)
+output_shape = (1, 1000)
 tvm_output = module.get_output(0, tvm.nd.empty(output_shape)).asnumpy()
 
 scores = softmax(tvm_output)
@@ -421,13 +420,12 @@ for rank in ranks[0:5]:
 
 timing_number = 10
 timing_repeat = 10
-optimized = np.array(
-        timeit.Timer(
-            lambda: module.run()).repeat(repeat=timing_repeat, number=timing_number)
-        )*1000/timing_number
-optimized = { "mean": np.mean(optimized),
-        "median": np.median(optimized),
-        "std": np.std(optimized)}
+optimized = (
+    np.array(timeit.Timer(lambda: module.run()).repeat(repeat=timing_repeat, number=timing_number))
+    * 1000
+    / timing_number
+)
+optimized = {"mean": np.mean(optimized), "median": np.median(optimized), "std": np.std(optimized)}
 
 
 print(optimized)
