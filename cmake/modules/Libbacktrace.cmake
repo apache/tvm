0999ce1111fbcaf755378761c7423221c2f1950f
Tristan Konolige <tristan.konolige@gmail.com>
2021-03-16 11:25:55 -0700

    switch to submodule

--- cmake/modules/Libbacktrace.cmake
+++ cmake/modules/Libbacktrace.cmake
@@ -16,38 +16,26 @@
 # under the License.
 include(ExternalProject)
 
-# Git tag for libbacktrace
-set(TAG dedbe13fda00253fe5d4f2fb812c909729ed5937)
-
 ExternalProject_Add(project_libbacktrace
   PREFIX libbacktrace
-  GIT_REPOSITORY https://github.com/ianlancetaylor/libbacktrace.git
-  GIT_TAG "${TAG}"
-  CONFIGURE_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/src/project_libbacktrace/configure"
+  SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../../3rdparty/libbacktrace
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/libbacktrace
+  CONFIGURE_COMMAND "${CMAKE_CURRENT_LIST_DIR}/../../3rdparty/libbacktrace/configure"
                     "--prefix=${CMAKE_CURRENT_BINARY_DIR}/libbacktrace" --with-pic
   INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/libbacktrace"
   BUILD_COMMAND make
   INSTALL_COMMAND make install
   BUILD_BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/lib/libbacktrace.a"
                    "${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/include/backtrace.h"
-  # disable the builtin update because it rebuilds on every build
-  UPDATE_COMMAND ""
   )
 
-# Only rebuild libbacktrace if this file changes
-# libbacktrace has a bug on macOS with shared libraries: when looking for symbols in shared
-# libraries, if any library is missing symbols, then the process stops and no symbols are used. We
-# include a patch that changes libbacktrace to instead perform a best-effort lookup of symbols. This
-# is a one-line change where a return is changed to a continue within the symbol lookup loop.
+# Custom step to rebuild libbacktrace if any of the source files change
+file(GLOB LIBBACKTRACE_SRCS "${CMAKE_CURRENT_LIST_DIR}/../../3rdparty/libbacktrace/*.c")
 ExternalProject_Add_Step(project_libbacktrace checkout
   DEPENDERS configure
   DEPENDEES download
-  DEPENDS "${CMAKE_CURRENT_LIST_DIR}/Libbacktrace.cmake" "${CMAKE_CURRENT_LIST_DIR}/libbacktrace_macos.patch"
-  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/libbacktrace/src/project_libbacktrace
-  COMMAND git checkout -f ${TAG};
-  COMMAND git apply ${CMAKE_CURRENT_LIST_DIR}/libbacktrace_macos.patch
-  COMMENT "update and patch"
-  )
+  DEPENDS ${LIBBACKTRACE_SRCS}
+)
 
 add_library(libbacktrace STATIC IMPORTED)
 add_dependencies(libbacktrace project_libbacktrace)
