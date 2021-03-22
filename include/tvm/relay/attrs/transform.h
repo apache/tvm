5062a3c8374a90bbdff57cb7ec864a292ac9fea9
Andrew Zhao Luo <andrewzhaoluo@Andrews-MacBook-Pro.local>
2021-03-22 13:14:58 -0700

    change attrs to be reusuable

--- include/tvm/relay/attrs/transform.h
+++ include/tvm/relay/attrs/transform.h
@@ -438,27 +438,13 @@ struct MatrixSetDiagAttrs : public tvm::AttrsNode<MatrixSetDiagAttrs> {
   }
 };  // struct MatrixSetDiagAttrs
 
-/*! \brief Attributes used in cumsum operator */
-struct CumsumAttrs : public tvm::AttrsNode<CumsumAttrs> {
+/*! \brief Attributes used in cumsum and cumprod operator */
+struct CumbinopAttrs : public tvm::AttrsNode<CumbinopAttrs> {
   Integer axis;
   DataType dtype;
   Integer exclusive;
-  TVM_DECLARE_ATTRS(CumsumAttrs, "relay.attrs.CumsumAttrs") {
-    TVM_ATTR_FIELD(axis).describe("The axis to sum over").set_default(NullValue<Integer>());
-    TVM_ATTR_FIELD(dtype).describe("Output data type").set_default(NullValue<DataType>());
-    TVM_ATTR_FIELD(exclusive)
-        .describe("The first element is not included")
-        .set_default(NullValue<Integer>());
-  }
-};
-
-/*! \brief Attributes used in cumprod operator */
-struct CumprodAttrs : public tvm::AttrsNode<CumprodAttrs> {
-  Integer axis;
-  DataType dtype;
-  Integer exclusive;
-  TVM_DECLARE_ATTRS(CumprodAttrs, "relay.attrs.CumprodAttrs") {
-    TVM_ATTR_FIELD(axis).describe("The axis to multiply over").set_default(NullValue<Integer>());
+  TVM_DECLARE_ATTRS(CumbinopAttrs, "relay.attrs.CumbinopAttrs") {
+    TVM_ATTR_FIELD(axis).describe("The axis to operate over").set_default(NullValue<Integer>());
     TVM_ATTR_FIELD(dtype).describe("Output data type").set_default(NullValue<DataType>());
     TVM_ATTR_FIELD(exclusive)
         .describe("The first element is not included")
