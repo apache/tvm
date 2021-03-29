areusch/incubator-tvm

git-clang-format

include/tvm/runtime/crt/graph_executor.h
@@ -67,7 +67,7 @@ typedef struct TVMGraphExecutor TVMGraphExecutor;
  * \return 0 if successful.
  */
 int TVMGraphExecutor_Create(const char* sym_json, TVMModuleHandle module_handle,
-                           const TVMContext* ctxs, TVMGraphExecutor** runtime);
+                            const TVMContext* ctxs, TVMGraphExecutor** runtime);
 
 int TVMGraphExecutor_GetInputIndex(TVMGraphExecutor* runtime, const char* name);
 
@@ -108,7 +108,7 @@ int TVMGraphExecutor_GetOutput(TVMGraphExecutor* runtime, const int32_t index, D
  * \return The result of this function execution.
  */
 int TVMGraphExecutor_LoadParams(TVMGraphExecutor* runtime, const char* param_blob,
-                               const uint32_t param_size);
+                                const uint32_t param_size);
 
 /*!
  * \brief Execute the graph.

src/runtime/crt/graph_executor/graph_executor.c
@@ -75,7 +75,7 @@ int NodeEntry_Load(TVMGraphExecutorNodeEntry* entry, JSONReader* reader) {
 }
 
 void TVMGraphExecutorNode_LoadAttrs(TVMGraphExecutorNode* node, JSONReader* reader,
-                                   TVMOpParam* param) {
+                                    TVMOpParam* param) {
   int bitmask = 0;
   char key[20], value[120];
   memset(param, 0, sizeof(TVMOpParam));
@@ -138,8 +138,8 @@ int TVMGraphExecutorNode_Load(TVMGraphExecutorNode* node, JSONReader* reader) {
         break;
       }
       DLContext ctx = {kDLCPU, 0};
-      tvm_crt_error_t err = TVMPlatformMemoryAllocate(sizeof(TVMGraphExecutorNodeEntry) * num_inputs,
-                                                      ctx, (void**)&node->inputs);
+      tvm_crt_error_t err = TVMPlatformMemoryAllocate(
+          sizeof(TVMGraphExecutorNodeEntry) * num_inputs, ctx, (void**)&node->inputs);
       if (err != kTvmErrorNoError) {
         fprintf(stderr, "memory allocate error: %08x", err);
         return -1;
@@ -773,7 +773,7 @@ void TVMGraphExecutor_SetInput(TVMGraphExecutor* runtime, const char* name, DLTe
  * \return The result of this function execution.
  */
 int TVMGraphExecutor_LoadParams(TVMGraphExecutor* runtime, const char* param_blob,
-                               const uint32_t param_size) {
+                                const uint32_t param_size) {
   int status = 0;
   const char* bptr = param_blob;
   uint64_t header, reserved;
@@ -1086,7 +1086,7 @@ int TVMGraphExecutor_SetupOpExecs(TVMGraphExecutor* runtime) {
 #endif  // TVM_CRT_DEBUG
       TVMPackedFunc pf;
       TVMGraphExecutor_CreateTVMOp(runtime, &(inode->param), args, args_count, inode->inputs_count,
-                                  &pf);
+                                   &pf);
       runtime->op_execs[nid] = pf;
     }
   }
@@ -1105,8 +1105,8 @@ typedef struct TVMOpArgs {
 } TVMOpArgs;
 
 int32_t TVMGraphExecutor_CreateTVMOp(TVMGraphExecutor* runtime, const TVMOpParam* param,
-                                    DLTensorPtr* args, const uint32_t args_count,
-                                    uint32_t num_inputs, TVMPackedFunc* pf) {
+                                     DLTensorPtr* args, const uint32_t args_count,
+                                     uint32_t num_inputs, TVMPackedFunc* pf) {
   int status = 0;
   uint32_t idx;
   TVMOpArgs arg_ptr;
@@ -1152,7 +1152,7 @@ int32_t TVMGraphExecutor_CreateTVMOp(TVMGraphExecutor* runtime, const TVMOpParam
  * \return 0 on success.
  */
 int TVMGraphExecutor_Init(TVMGraphExecutor* runtime, const char* graph_json,
-                         TVMModuleHandle module_handle, const TVMContext* ctxs) {
+                          TVMModuleHandle module_handle, const TVMContext* ctxs) {
   JSONReader reader;
   tvm_crt_error_t err = JSONReader_Create(graph_json, &reader);
   if (err != kTvmErrorNoError) {
@@ -1185,7 +1185,7 @@ int TVMGraphExecutor_Init(TVMGraphExecutor* runtime, const char* graph_json,
 }
 
 int TVMGraphExecutor_Create(const char* sym_json, TVMModuleHandle module_handle,
-                           const TVMContext* ctxs, TVMGraphExecutor** runtime) {
+                            const TVMContext* ctxs, TVMGraphExecutor** runtime) {
   DLContext ctx = {kDLCPU, 0};
   tvm_crt_error_t err = TVMPlatformMemoryAllocate(sizeof(TVMGraphExecutor), ctx, (void**)runtime);
   if (err != kTvmErrorNoError) {

src/runtime/graph/debug/graph_executor_debug.cc
@@ -128,8 +128,8 @@ class GraphExecutorDebug : public GraphExecutor {
           << "Don't know how to run op type " << nodes_[index].op_type
           << " remotely over RPC right now";
 
-      // NOTE: GraphExecutorDebug expects graph nodes to have an "op" attribute of "tvm_op" or "null"
-      // and "null" is a placeholder node for a parameter or input.
+      // NOTE: GraphExecutorDebug expects graph nodes to have an "op" attribute of "tvm_op" or
+      // "null" and "null" is a placeholder node for a parameter or input.
       return 0;
     }
 
@@ -236,7 +236,7 @@ class GraphExecutorDebug : public GraphExecutor {
  * \param sptr_to_self Packed function pointer.
  */
 PackedFunc GraphExecutorDebug::GetFunction(const std::string& name,
-                                          const ObjectPtr<Object>& sptr_to_self) {
+                                           const ObjectPtr<Object>& sptr_to_self) {
   // return member functions during query.
   if (name == "get_output_by_layer") {
     return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
@@ -272,8 +272,8 @@ PackedFunc GraphExecutorDebug::GetFunction(const std::string& name,
  * \param ctxs All devices contexts.
  */
 Module GraphExecutorDebugCreate(const std::string& sym_json, const tvm::runtime::Module& m,
-                               const std::vector<TVMContext>& ctxs,
-                               PackedFunc lookup_linked_param_func) {
+                                const std::vector<TVMContext>& ctxs,
+                                PackedFunc lookup_linked_param_func) {
   auto exec = make_object<GraphExecutorDebug>();
   exec->Init(sym_json, m, ctxs, lookup_linked_param_func);
   return Module(exec);
@@ -291,7 +291,7 @@ TVM_REGISTER_GLOBAL("tvm.graph_executor_debug.create").set_body([](TVMArgs args,
   }
 
   *rv = GraphExecutorDebugCreate(args[0], args[1], GetAllContext(args, ctx_start_arg),
-                                lookup_linked_param_func);
+                                 lookup_linked_param_func);
 });
 }  // namespace runtime
 }  // namespace tvm

src/runtime/graph/graph_executor.cc
@@ -69,7 +69,7 @@ void GraphExecutor::Run() {
  * \param lookup_linked_param_func Linked parameter lookup function.
  */
 void GraphExecutor::Init(const std::string& graph_json, tvm::runtime::Module module,
-                        const std::vector<TVMContext>& ctxs, PackedFunc lookup_linked_param_func) {
+                         const std::vector<TVMContext>& ctxs, PackedFunc lookup_linked_param_func) {
   std::istringstream is(graph_json);
   dmlc::JSONReader reader(&is);
   this->Load(&reader);
@@ -386,8 +386,9 @@ void GraphExecutor::SetupOpExecs() {
   }
 }
 
-std::pair<std::function<void()>, std::shared_ptr<GraphExecutor::OpArgs> > GraphExecutor::CreateTVMOp(
-    const TVMOpParam& param, const std::vector<DLTensor>& args, size_t num_inputs) {
+std::pair<std::function<void()>, std::shared_ptr<GraphExecutor::OpArgs> >
+GraphExecutor::CreateTVMOp(const TVMOpParam& param, const std::vector<DLTensor>& args,
+                           size_t num_inputs) {
   std::shared_ptr<GraphExecutor::OpArgs> arg_ptr = std::make_shared<GraphExecutor::OpArgs>();
   // setup address.
   arg_ptr->args = args;
@@ -436,7 +437,7 @@ std::pair<std::function<void()>, std::shared_ptr<GraphExecutor::OpArgs> > GraphE
 }
 
 PackedFunc GraphExecutor::GetFunction(const std::string& name,
-                                     const ObjectPtr<Object>& sptr_to_self) {
+                                      const ObjectPtr<Object>& sptr_to_self) {
   // Return member functions during query.
   if (name == "set_input") {
     return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
@@ -502,8 +503,8 @@ PackedFunc GraphExecutor::GetFunction(const std::string& name,
 }
 
 Module GraphExecutorCreate(const std::string& sym_json, const tvm::runtime::Module& m,
-                          const std::vector<TVMContext>& ctxs,
-                          const PackedFunc lookup_linked_param_func) {
+                           const std::vector<TVMContext>& ctxs,
+                           const PackedFunc lookup_linked_param_func) {
   auto exec = make_object<GraphExecutor>();
   exec->Init(sym_json, m, ctxs, lookup_linked_param_func);
   return Module(exec);

web/src/runtime.ts
@@ -992,12 +992,8 @@ export class Instance implements Disposable {
    * @param lib The underlying library.
    * @param ctx The execution context of the graph.
    */
-  createGraphExecutor(
-    graphJson: string,
-    lib: Module,
-    ctx: DLContext
-  ): GraphExecutor {
-    const fcreate = this.getGlobalFunc("tvm.graph_executor.create");
+  createGraphExecutor(graphJson: string, lib: Module, ctx: DLContext): GraphExecutor {
+    const fcreate = this.getGlobalFunc('tvm.graph_executor.create');
     const module = fcreate(
       graphJson,
       lib,

apache/tvm

include/tvm/runtime/crt/graph_executor.h
@@ -67,7 +67,7 @@ typedef struct TVMGraphExecutor TVMGraphExecutor;
  * \return 0 if successful.
  */
 int TVMGraphExecutor_Create(const char* sym_json, TVMModuleHandle module_handle,
-                           const DLDevice* devices, TVMGraphExecutor** runtime);
+                            const DLDevice* devices, TVMGraphExecutor** runtime);
 
 int TVMGraphExecutor_GetInputIndex(TVMGraphExecutor* runtime, const char* name);
 
@@ -108,7 +108,7 @@ int TVMGraphExecutor_GetOutput(TVMGraphExecutor* runtime, const int32_t index, D
  * \return The result of this function execution.
  */
 int TVMGraphExecutor_LoadParams(TVMGraphExecutor* runtime, const char* param_blob,
-                               const uint32_t param_size);
+                                const uint32_t param_size);
 
 /*!
  * \brief Execute the graph.

src/runtime/crt/graph_executor/graph_executor.c
@@ -75,7 +75,7 @@ int NodeEntry_Load(TVMGraphExecutorNodeEntry* entry, JSONReader* reader) {
 }
 
 void TVMGraphExecutorNode_LoadAttrs(TVMGraphExecutorNode* node, JSONReader* reader,
-                                   TVMOpParam* param) {
+                                    TVMOpParam* param) {
   int bitmask = 0;
   char key[20], value[120];
   memset(param, 0, sizeof(TVMOpParam));
@@ -138,8 +138,8 @@ int TVMGraphExecutorNode_Load(TVMGraphExecutorNode* node, JSONReader* reader) {
         break;
       }
       DLDevice dev = {kDLCPU, 0};
-      tvm_crt_error_t err = TVMPlatformMemoryAllocate(sizeof(TVMGraphExecutorNodeEntry) * num_inputs,
-                                                      dev, (void**)&node->inputs);
+      tvm_crt_error_t err = TVMPlatformMemoryAllocate(
+          sizeof(TVMGraphExecutorNodeEntry) * num_inputs, dev, (void**)&node->inputs);
       if (err != kTvmErrorNoError) {
         fprintf(stderr, "memory allocate error: %08x", err);
         return -1;
@@ -773,7 +773,7 @@ void TVMGraphExecutor_SetInput(TVMGraphExecutor* runtime, const char* name, DLTe
  * \return The result of this function execution.
  */
 int TVMGraphExecutor_LoadParams(TVMGraphExecutor* runtime, const char* param_blob,
-                               const uint32_t param_size) {
+                                const uint32_t param_size) {
   int status = 0;
   const char* bptr = param_blob;
   uint64_t header, reserved;
@@ -967,8 +967,8 @@ int TVMGraphExecutor_SetupStorage(TVMGraphExecutor* runtime) {
   }
 
   // Allocate the space.
-  err = TVMPlatformMemoryAllocate(sizeof(TVMGraphExecutorStorageEntry) * pool_entry_count, alloc_dev,
-                                  (void**)&runtime->storage_pool);
+  err = TVMPlatformMemoryAllocate(sizeof(TVMGraphExecutorStorageEntry) * pool_entry_count,
+                                  alloc_dev, (void**)&runtime->storage_pool);
   if (err != kTvmErrorNoError) {
     fprintf(stderr, "memory allocate error: %08x", err);
     return -1;
@@ -1086,7 +1086,7 @@ int TVMGraphExecutor_SetupOpExecs(TVMGraphExecutor* runtime) {
 #endif  // TVM_CRT_DEBUG
       TVMPackedFunc pf;
       TVMGraphExecutor_CreateTVMOp(runtime, &(inode->param), args, args_count, inode->inputs_count,
-                                  &pf);
+                                   &pf);
       runtime->op_execs[nid] = pf;
     }
   }
@@ -1105,8 +1105,8 @@ typedef struct TVMOpArgs {
 } TVMOpArgs;
 
 int32_t TVMGraphExecutor_CreateTVMOp(TVMGraphExecutor* runtime, const TVMOpParam* param,
-                                    DLTensorPtr* args, const uint32_t args_count,
-                                    uint32_t num_inputs, TVMPackedFunc* pf) {
+                                     DLTensorPtr* args, const uint32_t args_count,
+                                     uint32_t num_inputs, TVMPackedFunc* pf) {
   int status = 0;
   uint32_t idx;
   TVMOpArgs arg_ptr;
@@ -1152,7 +1152,7 @@ int32_t TVMGraphExecutor_CreateTVMOp(TVMGraphExecutor* runtime, const TVMOpParam
  * \return 0 on success.
  */
 int TVMGraphExecutor_Init(TVMGraphExecutor* runtime, const char* graph_json,
-                         TVMModuleHandle module_handle, const DLDevice* devs) {
+                          TVMModuleHandle module_handle, const DLDevice* devs) {
   JSONReader reader;
   tvm_crt_error_t err = JSONReader_Create(graph_json, &reader);
   if (err != kTvmErrorNoError) {
@@ -1185,7 +1185,7 @@ int TVMGraphExecutor_Init(TVMGraphExecutor* runtime, const char* graph_json,
 }
 
 int TVMGraphExecutor_Create(const char* sym_json, TVMModuleHandle module_handle,
-                           const DLDevice* devs, TVMGraphExecutor** runtime) {
+                            const DLDevice* devs, TVMGraphExecutor** runtime) {
   DLDevice dev = {kDLCPU, 0};
   tvm_crt_error_t err = TVMPlatformMemoryAllocate(sizeof(TVMGraphExecutor), dev, (void**)runtime);
   if (err != kTvmErrorNoError) {

src/runtime/graph_executor/debug/graph_executor_debug.cc
@@ -128,8 +128,8 @@ class GraphExecutorDebug : public GraphExecutor {
           << "Don't know how to run op type " << nodes_[index].op_type
           << " remotely over RPC right now";
 
-      // NOTE: GraphExecutorDebug expects graph nodes to have an "op" attribute of "tvm_op" or "null"
-      // and "null" is a placeholder node for a parameter or input.
+      // NOTE: GraphExecutorDebug expects graph nodes to have an "op" attribute of "tvm_op" or
+      // "null" and "null" is a placeholder node for a parameter or input.
       return 0;
     }
 
@@ -236,7 +236,7 @@ class GraphExecutorDebug : public GraphExecutor {
  * \param sptr_to_self Packed function pointer.
  */
 PackedFunc GraphExecutorDebug::GetFunction(const std::string& name,
-                                          const ObjectPtr<Object>& sptr_to_self) {
+                                           const ObjectPtr<Object>& sptr_to_self) {
   // return member functions during query.
   if (name == "get_output_by_layer") {
     return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
@@ -272,8 +272,8 @@ PackedFunc GraphExecutorDebug::GetFunction(const std::string& name,
  * \param devs All devices.
  */
 Module GraphExecutorDebugCreate(const std::string& sym_json, const tvm::runtime::Module& m,
-                               const std::vector<Device>& devs,
-                               PackedFunc lookup_linked_param_func) {
+                                const std::vector<Device>& devs,
+                                PackedFunc lookup_linked_param_func) {
   auto exec = make_object<GraphExecutorDebug>();
   exec->Init(sym_json, m, devs, lookup_linked_param_func);
   return Module(exec);
@@ -291,7 +291,7 @@ TVM_REGISTER_GLOBAL("tvm.graph_executor_debug.create").set_body([](TVMArgs args,
   }
 
   *rv = GraphExecutorDebugCreate(args[0], args[1], GetAllDevice(args, dev_start_arg),
-                                lookup_linked_param_func);
+                                 lookup_linked_param_func);
 });
 }  // namespace runtime
 }  // namespace tvm

src/runtime/graph_executor/graph_executor.cc
@@ -69,8 +69,8 @@ void GraphExecutor::Run() {
  * \param lookup_linked_param_func Linked parameter lookup function. Default is nullptr.
  */
 void GraphExecutor::Init(const std::string& graph_json, tvm::runtime::Module module,
-                        const std::vector<Device>& devs,
-                        const PackedFunc lookup_linked_param_func) {
+                         const std::vector<Device>& devs,
+                         const PackedFunc lookup_linked_param_func) {
   std::istringstream is(graph_json);
   dmlc::JSONReader reader(&is);
   this->Load(&reader);
@@ -389,8 +389,9 @@ void GraphExecutor::SetupOpExecs() {
   }
 }
 
-std::pair<std::function<void()>, std::shared_ptr<GraphExecutor::OpArgs> > GraphExecutor::CreateTVMOp(
-    const TVMOpParam& param, const std::vector<DLTensor>& args, size_t num_inputs) {
+std::pair<std::function<void()>, std::shared_ptr<GraphExecutor::OpArgs> >
+GraphExecutor::CreateTVMOp(const TVMOpParam& param, const std::vector<DLTensor>& args,
+                           size_t num_inputs) {
   std::shared_ptr<GraphExecutor::OpArgs> arg_ptr = std::make_shared<GraphExecutor::OpArgs>();
   // setup address.
   arg_ptr->args = args;
@@ -439,7 +440,7 @@ std::pair<std::function<void()>, std::shared_ptr<GraphExecutor::OpArgs> > GraphE
 }
 
 PackedFunc GraphExecutor::GetFunction(const std::string& name,
-                                     const ObjectPtr<Object>& sptr_to_self) {
+                                      const ObjectPtr<Object>& sptr_to_self) {
   // Return member functions during query.
   if (name == "set_input") {
     return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
@@ -505,8 +506,8 @@ PackedFunc GraphExecutor::GetFunction(const std::string& name,
 }
 
 Module GraphExecutorCreate(const std::string& sym_json, const tvm::runtime::Module& m,
-                          const std::vector<Device>& devs,
-                          const PackedFunc lookup_linked_param_func) {
+                           const std::vector<Device>& devs,
+                           const PackedFunc lookup_linked_param_func) {
   auto exec = make_object<GraphExecutor>();
   exec->Init(sym_json, m, devs, lookup_linked_param_func);
   return Module(exec);

web/src/runtime.ts
@@ -992,12 +992,8 @@ export class Instance implements Disposable {
    * @param lib The underlying library.
    * @param dev The execution device of the graph.
    */
-  createGraphExecutor(
-    graphJson: string,
-    lib: Module,
-    dev: DLDevice
-  ): GraphExecutor {
-    const fcreate = this.getGlobalFunc("tvm.graph_executor.create");
+  createGraphExecutor(graphJson: string, lib: Module, dev: DLDevice): GraphExecutor {
+    const fcreate = this.getGlobalFunc('tvm.graph_executor.create');
     const module = fcreate(
       graphJson,
       lib,