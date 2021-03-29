areusch/incubator-tvm

graph_runtime -> graph_executor

python/tvm/contrib/graph_executor.py
@@ -50,7 +50,7 @@ def create(graph_json_str, libmod, ctx):
 
     Note
     ----
-    See also :py:class:`tvm.contrib.graph_runtime.GraphModule`
+    See also :py:class:`tvm.contrib.graph_executor.GraphModule`
     for examples to directly construct a GraphModule from an exported
     relay compiled library.
     """
@@ -59,9 +59,9 @@ def create(graph_json_str, libmod, ctx):
     ctx, num_rpc_ctx, device_type_id = get_device_ctx(libmod, ctx)
 
     if num_rpc_ctx == len(ctx):
-        fcreate = ctx[0]._rpc_sess.get_function("tvm.graph_runtime.create")
+        fcreate = ctx[0]._rpc_sess.get_function("tvm.graph_executor.create")
     else:
-        fcreate = tvm._ffi.get_global_func("tvm.graph_runtime.create")
+        fcreate = tvm._ffi.get_global_func("tvm.graph_executor.create")
 
     return GraphModule(fcreate(graph_json_str, libmod, *device_type_id))
 
@@ -135,7 +135,7 @@ class GraphModule(object):
 
         import tvm
         from tvm import relay
-        from tvm.contrib import graph_runtime
+        from tvm.contrib import graph_executor
 
         # build the library using graph runtime
         lib = relay.build(...)
@@ -144,7 +144,7 @@ class GraphModule(object):
         lib: tvm.runtime.Module = tvm.runtime.load_module("compiled_lib.so")
         # Call the library factory function for default and create
         # a new runtime.Module, wrap with graph module.
-        gmod = graph_runtime.GraphModule(lib["default"](ctx))
+        gmod = graph_executor.GraphModule(lib["default"](ctx))
         # use the graph module.
         gmod.set_input("x", data)
         gmod.run()
@@ -270,7 +270,7 @@ class GraphModule(object):
         out : NDArray
             The output array container
         """
-        raise NotImplementedError("Please use debugger.debug_runtime as graph_runtime instead.")
+        raise NotImplementedError("Please use debugger.debug_runtime as graph_executor instead.")
 
     def load_params(self, params_bytes):
         """Load parameters from serialized byte array of parameter dict.

python/tvm/micro/session.py
@@ -22,7 +22,7 @@ import sys
 
 from ..error import register_error
 from .._ffi import get_global_func
-from ..contrib import graph_runtime
+from ..contrib import graph_executor
 from ..contrib.debugger import debug_runtime
 from ..rpc import RPCSession
 from .transport import IoTimeoutError
@@ -92,7 +92,7 @@ class Session:
         self.timeout_override = timeout_override
 
         self._rpc = None
-        self._graph_runtime = None
+        self._graph_executor = None
 
     def get_system_lib(self):
         return self._rpc.get_function("runtime.SystemLib")()
@@ -192,7 +192,7 @@ def lookup_remote_linked_param(mod, storage_id, template_tensor, ctx):
     )
 
 
-def create_local_graph_runtime(graph_json_str, mod, ctx):
+def create_local_graph_executor(graph_json_str, mod, ctx):
     """Create a local graph runtime driving execution on the remote CPU context given.
 
     Parameters
@@ -212,8 +212,8 @@ def create_local_graph_runtime(graph_json_str, mod, ctx):
          A local graph runtime instance that executes on the remote device.
     """
     device_type_id = [ctx.device_type, ctx.device_id]
-    fcreate = get_global_func("tvm.graph_runtime.create")
-    return graph_runtime.GraphModule(
+    fcreate = get_global_func("tvm.graph_executor.create")
+    return graph_executor.GraphModule(
         fcreate(graph_json_str, mod, lookup_remote_linked_param, *device_type_id)
     )
 
@@ -241,7 +241,7 @@ def create_local_debug_runtime(graph_json_str, mod, ctx, dump_root=None):
          A local graph runtime instance that executes on the remote device.
     """
     device_type_id = [ctx.device_type, ctx.device_id]
-    fcreate = get_global_func("tvm.graph_runtime_debug.create")
+    fcreate = get_global_func("tvm.graph_executor_debug.create")
     return debug_runtime.GraphModuleDebug(
         fcreate(graph_json_str, mod, lookup_remote_linked_param, *device_type_id),
         [ctx],

python/tvm/relay/frontend/common.py
@@ -530,13 +530,13 @@ def infer_value(input_val, params, mod=None):
     try:
         # TODO(kevinthesun): Use VM for all cases.
         # pylint: disable=import-outside-toplevel
-        from tvm.contrib import graph_runtime
+        from tvm.contrib import graph_executor
 
         func = _function.Function(analysis.free_vars(input_val), input_val)
         with tvm.transform.PassContext(opt_level=0):
             lib = tvm.relay.build(func, target="llvm", params=params)
         ctx = tvm.cpu(0)
-        m = graph_runtime.GraphModule(lib["default"](ctx))
+        m = graph_executor.GraphModule(lib["default"](ctx))
         m.run()
         return m.get_output(0)
     except Exception:

tests/python/frontend/coreml/test_forward.py
@@ -21,7 +21,7 @@ from coremltools.models import datatypes
 
 import tvm
 from tvm import te
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 from tvm import topi
 import tvm.topi.testing
 from tvm import relay
@@ -37,7 +37,7 @@ def get_tvm_output(
 ):
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(func, target, params=params)
-    m = graph_runtime.GraphModule(lib["default"](ctx))
+    m = graph_executor.GraphModule(lib["default"](ctx))
     # set inputs
     m.set_input(input_name, tvm.nd.array(x.astype(dtype)))
     m.run()
@@ -88,9 +88,9 @@ def run_tvm_graph(
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(mod, target, params=params)
 
-    from tvm.contrib import graph_runtime
+    from tvm.contrib import graph_executor
 
-    m = graph_runtime.GraphModule(lib["default"](ctx))
+    m = graph_executor.GraphModule(lib["default"](ctx))
     # set inputs
     if isinstance(input_data, list):
         for i, e in enumerate(input_name):

tests/python/relay/test_auto_scheduler_tuning.py
@@ -20,7 +20,7 @@ import tempfile
 import numpy as np
 
 from tvm import auto_scheduler, relay
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 import tvm.testing
 
 from test_auto_scheduler_task_extraction import get_network
@@ -70,7 +70,7 @@ def tune_network(network, target):
         # Check the correctness
         def get_output(data, lib):
             ctx = tvm.gpu()
-            module = graph_runtime.GraphModule(lib["default"](ctx))
+            module = graph_executor.GraphModule(lib["default"](ctx))
             module.set_input("data", data)
             module.run()
             return module.get_output(0).asnumpy()

tests/python/relay/test_simplify_fc_transpose.py
@@ -31,11 +31,11 @@ def run_func(func, params, x):
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(func, "llvm", params=params)
 
-    from tvm.contrib import graph_runtime
+    from tvm.contrib import graph_executor
 
     ctx = tvm.cpu(0)
     dtype = "float32"
-    m = graph_runtime.GraphModule(lib["default"](ctx))
+    m = graph_executor.GraphModule(lib["default"](ctx))
     # set inputs
     m.set_input("data", tvm.nd.array(x.astype(dtype)))
     # execute

tests/python/unittest/test_runtime_module_based_interface.py
@@ -18,7 +18,7 @@ import numpy as np
 from tvm import relay, runtime
 from tvm.relay import testing
 import tvm
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 from tvm.contrib.debugger import debug_runtime
 import tvm.testing
 
@@ -36,7 +36,7 @@ def verify(data):
         graph, lib, graph_params = relay.build_module.build(mod, "llvm", params=params)
 
     ctx = tvm.cpu()
-    module = graph_runtime.create(graph, lib, ctx)
+    module = graph_executor.create(graph, lib, ctx)
     module.set_input("data", data)
     module.set_input(**graph_params)
     module.run()
@@ -54,7 +54,7 @@ def test_legacy_compatibility():
         graph, lib, graph_params = relay.build_module.build(mod, "llvm", params=params)
     data = np.random.uniform(-1, 1, size=input_shape(mod)).astype("float32")
     ctx = tvm.cpu()
-    module = graph_runtime.create(graph, lib, ctx)
+    module = graph_executor.create(graph, lib, ctx)
     module.set_input("data", data)
     module.set_input(**graph_params)
     module.run()
@@ -82,7 +82,7 @@ def test_cpu():
     tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
     # graph runtime wrapper
-    gmod = graph_runtime.GraphModule(complied_graph_lib["default"](ctx))
+    gmod = graph_executor.GraphModule(complied_graph_lib["default"](ctx))
     gmod.set_input("data", data)
     gmod.run()
     out = gmod.get_output(0).asnumpy()
@@ -109,7 +109,7 @@ def test_gpu():
     tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
     # graph runtime wrapper
-    gmod = graph_runtime.GraphModule(complied_graph_lib["default"](ctx))
+    gmod = graph_executor.GraphModule(complied_graph_lib["default"](ctx))
     gmod.set_input("data", data)
     gmod.run()
     out = gmod.get_output(0).asnumpy()
@@ -151,7 +151,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -190,7 +190,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -234,7 +234,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -279,7 +279,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -333,7 +333,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         loaded_params = bytearray(open(temp.relpath("deploy_param.params"), "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -380,7 +380,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         loaded_params = bytearray(open(temp.relpath("deploy_param.params"), "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -433,7 +433,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         loaded_params = bytearray(open(path_params, "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -486,7 +486,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](ctx))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](ctx))
         loaded_params = bytearray(open(path_params, "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -501,7 +501,7 @@ def test_remove_package_params():
         verify_rpc_gpu_remove_package_params(obj_format)
 
 
-def test_debug_graph_runtime():
+def test_debug_graph_executor():
     if not tvm.testing.device_enabled("llvm"):
         print("Skip because llvm is not enabled")
         return
@@ -515,7 +515,7 @@ def test_debug_graph_runtime():
     try:
         gmod = complied_graph_lib["debug_create"]("default", ctx)
     except:
-        print("Skip because debug graph_runtime not enabled")
+        print("Skip because debug graph_executor not enabled")
         return
     set_input = gmod["set_input"]
     run = gmod["run"]
@@ -572,5 +572,5 @@ if __name__ == "__main__":
     test_gpu()
     test_mod_export()
     test_remove_package_params()
-    test_debug_graph_runtime()
+    test_debug_graph_executor()
     test_multiple_imported_modules()

tutorials/auto_scheduler/tune_network_x86.py
@@ -49,7 +49,7 @@ import numpy as np
 import tvm
 from tvm import relay, auto_scheduler
 import tvm.relay.testing
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 
 #################################################################
 # Define a Network
@@ -281,7 +281,7 @@ with auto_scheduler.ApplyHistoryBest(log_file):
 
 # Create graph runtime
 ctx = tvm.context(str(target), 0)
-module = graph_runtime.GraphModule(lib["default"](ctx))
+module = graph_executor.GraphModule(lib["default"](ctx))
 data_tvm = tvm.nd.array((np.random.uniform(size=input_shape)).astype(dtype))
 module.set_input("data", data_tvm)
 

apache/tvm

python/tvm/contrib/graph_executor.py
@@ -50,7 +50,7 @@ def create(graph_json_str, libmod, device):
 
     Note
     ----
-    See also :py:class:`tvm.contrib.graph_runtime.GraphModule`
+    See also :py:class:`tvm.contrib.graph_executor.GraphModule`
     for examples to directly construct a GraphModule from an exported
     relay compiled library.
     """
@@ -59,9 +59,9 @@ def create(graph_json_str, libmod, device):
     dev, num_rpc_dev, device_type_id = get_device(libmod, device)
 
     if num_rpc_dev == len(dev):
-        fcreate = dev[0]._rpc_sess.get_function("tvm.graph_runtime.create")
+        fcreate = dev[0]._rpc_sess.get_function("tvm.graph_executor.create")
     else:
-        fcreate = tvm._ffi.get_global_func("tvm.graph_runtime.create")
+        fcreate = tvm._ffi.get_global_func("tvm.graph_executor.create")
 
     return GraphModule(fcreate(graph_json_str, libmod, *device_type_id))
 
@@ -135,7 +135,7 @@ class GraphModule(object):
 
         import tvm
         from tvm import relay
-        from tvm.contrib import graph_runtime
+        from tvm.contrib import graph_executor
 
         # build the library using graph runtime
         lib = relay.build(...)
@@ -144,7 +144,7 @@ class GraphModule(object):
         lib: tvm.runtime.Module = tvm.runtime.load_module("compiled_lib.so")
         # Call the library factory function for default and create
         # a new runtime.Module, wrap with graph module.
-        gmod = graph_runtime.GraphModule(lib["default"](dev))
+        gmod = graph_executor.GraphModule(lib["default"](dev))
         # use the graph module.
         gmod.set_input("x", data)
         gmod.run()
@@ -270,7 +270,7 @@ class GraphModule(object):
         out : NDArray
             The output array container
         """
-        raise NotImplementedError("Please use debugger.debug_runtime as graph_runtime instead.")
+        raise NotImplementedError("Please use debugger.debug_runtime as graph_executor instead.")
 
     def load_params(self, params_bytes):
         """Load parameters from serialized byte array of parameter dict.

python/tvm/micro/session.py
@@ -22,7 +22,7 @@ import sys
 
 from ..error import register_error
 from .._ffi import get_global_func
-from ..contrib import graph_runtime
+from ..contrib import graph_executor
 from ..contrib.debugger import debug_runtime
 from ..rpc import RPCSession
 from .transport import IoTimeoutError
@@ -92,7 +92,7 @@ class Session:
         self.timeout_override = timeout_override
 
         self._rpc = None
-        self._graph_runtime = None
+        self._graph_executor = None
 
     def get_system_lib(self):
         return self._rpc.get_function("runtime.SystemLib")()
@@ -192,7 +192,7 @@ def lookup_remote_linked_param(mod, storage_id, template_tensor, device):
     )
 
 
-def create_local_graph_runtime(graph_json_str, mod, device):
+def create_local_graph_executor(graph_json_str, mod, device):
     """Create a local graph runtime driving execution on the remote CPU device given.
 
     Parameters
@@ -212,8 +212,8 @@ def create_local_graph_runtime(graph_json_str, mod, device):
          A local graph runtime instance that executes on the remote device.
     """
     device_type_id = [device.device_type, device.device_id]
-    fcreate = get_global_func("tvm.graph_runtime.create")
-    return graph_runtime.GraphModule(
+    fcreate = get_global_func("tvm.graph_executor.create")
+    return graph_executor.GraphModule(
         fcreate(graph_json_str, mod, lookup_remote_linked_param, *device_type_id)
     )
 
@@ -241,7 +241,7 @@ def create_local_debug_runtime(graph_json_str, mod, device, dump_root=None):
          A local graph runtime instance that executes on the remote device.
     """
     device_type_id = [device.device_type, device.device_id]
-    fcreate = get_global_func("tvm.graph_runtime_debug.create")
+    fcreate = get_global_func("tvm.graph_executor_debug.create")
     return debug_runtime.GraphModuleDebug(
         fcreate(graph_json_str, mod, lookup_remote_linked_param, *device_type_id),
         [device],

python/tvm/relay/frontend/common.py
@@ -530,13 +530,13 @@ def infer_value(input_val, params, mod=None):
     try:
         # TODO(kevinthesun): Use VM for all cases.
         # pylint: disable=import-outside-toplevel
-        from tvm.contrib import graph_runtime
+        from tvm.contrib import graph_executor
 
         func = _function.Function(analysis.free_vars(input_val), input_val)
         with tvm.transform.PassContext(opt_level=0):
             lib = tvm.relay.build(func, target="llvm", params=params)
         dev = tvm.cpu(0)
-        m = graph_runtime.GraphModule(lib["default"](dev))
+        m = graph_executor.GraphModule(lib["default"](dev))
         m.run()
         return m.get_output(0)
     except Exception:

tests/python/frontend/coreml/test_forward.py
@@ -21,7 +21,7 @@ from coremltools.models import datatypes
 
 import tvm
 from tvm import te
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 from tvm import topi
 import tvm.topi.testing
 from tvm import relay
@@ -37,7 +37,7 @@ def get_tvm_output(
 ):
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(func, target, params=params)
-    m = graph_runtime.GraphModule(lib["default"](device))
+    m = graph_executor.GraphModule(lib["default"](device))
     # set inputs
     m.set_input(input_name, tvm.nd.array(x.astype(dtype)))
     m.run()
@@ -88,9 +88,9 @@ def run_tvm_graph(
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(mod, target, params=params)
 
-    from tvm.contrib import graph_runtime
+    from tvm.contrib import graph_executor
 
-    m = graph_runtime.GraphModule(lib["default"](device))
+    m = graph_executor.GraphModule(lib["default"](device))
     # set inputs
     if isinstance(input_data, list):
         for i, e in enumerate(input_name):

tests/python/relay/test_auto_scheduler_tuning.py
@@ -20,7 +20,7 @@ import tempfile
 import numpy as np
 
 from tvm import auto_scheduler, relay
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 import tvm.testing
 
 from test_auto_scheduler_task_extraction import get_network
@@ -70,7 +70,7 @@ def tune_network(network, target):
         # Check the correctness
         def get_output(data, lib):
             dev = tvm.gpu()
-            module = graph_runtime.GraphModule(lib["default"](dev))
+            module = graph_executor.GraphModule(lib["default"](dev))
             module.set_input("data", data)
             module.run()
             return module.get_output(0).asnumpy()

tests/python/relay/test_simplify_fc_transpose.py
@@ -31,11 +31,11 @@ def run_func(func, params, x):
     with tvm.transform.PassContext(opt_level=3):
         lib = relay.build(func, "llvm", params=params)
 
-    from tvm.contrib import graph_runtime
+    from tvm.contrib import graph_executor
 
     dev = tvm.cpu(0)
     dtype = "float32"
-    m = graph_runtime.GraphModule(lib["default"](dev))
+    m = graph_executor.GraphModule(lib["default"](dev))
     # set inputs
     m.set_input("data", tvm.nd.array(x.astype(dtype)))
     # execute

tests/python/unittest/test_runtime_module_based_interface.py
@@ -18,9 +18,9 @@ import numpy as np
 from tvm import relay, runtime
 from tvm.relay import testing
 import tvm
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 from tvm.contrib.debugger import debug_runtime
-from tvm.contrib.cuda_graph import cuda_graph_runtime
+from tvm.contrib.cuda_graph import cuda_graph_executor
 import tvm.testing
 
 
@@ -37,7 +37,7 @@ def verify(data):
         graph, lib, graph_params = relay.build_module.build(mod, "llvm", params=params)
 
     dev = tvm.cpu()
-    module = graph_runtime.create(graph, lib, dev)
+    module = graph_executor.create(graph, lib, dev)
     module.set_input("data", data)
     module.set_input(**graph_params)
     module.run()
@@ -55,7 +55,7 @@ def test_legacy_compatibility():
         graph, lib, graph_params = relay.build_module.build(mod, "llvm", params=params)
     data = np.random.uniform(-1, 1, size=input_shape(mod)).astype("float32")
     dev = tvm.cpu()
-    module = graph_runtime.create(graph, lib, dev)
+    module = graph_executor.create(graph, lib, dev)
     module.set_input("data", data)
     module.set_input(**graph_params)
     module.run()
@@ -83,7 +83,7 @@ def test_cpu():
     tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
     # graph runtime wrapper
-    gmod = graph_runtime.GraphModule(complied_graph_lib["default"](dev))
+    gmod = graph_executor.GraphModule(complied_graph_lib["default"](dev))
     gmod.set_input("data", data)
     gmod.run()
     out = gmod.get_output(0).asnumpy()
@@ -110,7 +110,7 @@ def test_gpu():
     tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
     # graph runtime wrapper
-    gmod = graph_runtime.GraphModule(complied_graph_lib["default"](dev))
+    gmod = graph_executor.GraphModule(complied_graph_lib["default"](dev))
     gmod.set_input("data", data)
     gmod.run()
     out = gmod.get_output(0).asnumpy()
@@ -152,7 +152,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -191,7 +191,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -235,7 +235,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -280,7 +280,7 @@ def test_mod_export():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         gmod.set_input("data", data)
         gmod.run()
         out = gmod.get_output(0).asnumpy()
@@ -334,7 +334,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         loaded_params = bytearray(open(temp.relpath("deploy_param.params"), "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -381,7 +381,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         loaded_params = bytearray(open(temp.relpath("deploy_param.params"), "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -434,7 +434,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         loaded_params = bytearray(open(path_params, "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -487,7 +487,7 @@ def test_remove_package_params():
         tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
         # graph runtime wrapper
-        gmod = graph_runtime.GraphModule(loaded_lib["default"](dev))
+        gmod = graph_executor.GraphModule(loaded_lib["default"](dev))
         loaded_params = bytearray(open(path_params, "rb").read())
         gmod.set_input("data", data)
         gmod.load_params(loaded_params)
@@ -502,7 +502,7 @@ def test_remove_package_params():
         verify_rpc_gpu_remove_package_params(obj_format)
 
 
-def test_debug_graph_runtime():
+def test_debug_graph_executor():
     if not tvm.testing.device_enabled("llvm"):
         print("Skip because llvm is not enabled")
         return
@@ -516,7 +516,7 @@ def test_debug_graph_runtime():
     try:
         gmod = complied_graph_lib["debug_create"]("default", dev)
     except:
-        print("Skip because debug graph_runtime not enabled")
+        print("Skip because debug graph_executor not enabled")
         return
     set_input = gmod["set_input"]
     run = gmod["run"]
@@ -540,7 +540,7 @@ def test_debug_graph_runtime():
 
 
 @tvm.testing.requires_cudagraph
-def test_cuda_graph_runtime():
+def test_cuda_graph_executor():
     mod, params = relay.testing.synthetic.get_workload()
     with tvm.transform.PassContext(opt_level=3):
         complied_graph_lib = relay.build_module.build(mod, "cuda", params=params)
@@ -561,7 +561,7 @@ def test_cuda_graph_runtime():
     tvm.testing.assert_allclose(out, verify(data), atol=1e-5)
 
     # cuda graph runtime wrapper
-    cu_gmod = cuda_graph_runtime.GraphModuleCudaGraph(gmod)
+    cu_gmod = cuda_graph_executor.GraphModuleCudaGraph(gmod)
     cu_gmod.set_input("data", data)
     cu_gmod.run()
     out = cu_gmod.get_output(0).asnumpy()
@@ -602,5 +602,5 @@ if __name__ == "__main__":
     test_gpu()
     test_mod_export()
     test_remove_package_params()
-    test_debug_graph_runtime()
+    test_debug_graph_executor()
     test_multiple_imported_modules()

tutorials/auto_scheduler/tune_network_x86.py
@@ -49,7 +49,7 @@ import numpy as np
 import tvm
 from tvm import relay, auto_scheduler
 import tvm.relay.testing
-from tvm.contrib import graph_runtime
+from tvm.contrib import graph_executor
 
 #################################################################
 # Define a Network
@@ -281,7 +281,7 @@ with auto_scheduler.ApplyHistoryBest(log_file):
 
 # Create graph runtime
 dev = tvm.device(str(target), 0)
-module = graph_runtime.GraphModule(lib["default"](dev))
+module = graph_executor.GraphModule(lib["default"](dev))
 data_tvm = tvm.nd.array((np.random.uniform(size=input_shape)).astype(dtype))
 module.set_input("data", data_tvm)
 