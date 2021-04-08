c70a5a84b3097f837971d1636fb90df59f2973b5
Xiyou Zhou <xiyou@octoml.ai>
2021-04-08 12:57:45 -0700

    Fix format

--- include/tvm/tir/op_attr_types.h
+++ include/tvm/tir/op_attr_types.h
@@ -28,9 +28,9 @@
 #ifndef TVM_TIR_OP_ATTR_TYPES_H_
 #define TVM_TIR_OP_ATTR_TYPES_H_
 
+#include <tvm/ir/expr.h>
 #include <tvm/runtime/container.h>
 #include <tvm/runtime/packed_func.h>
-#include <tvm/ir/expr.h>
 
 namespace tvm {
 namespace tir {
