3a2518eb94b01cf15718fd308835e1d75e1b5937
Mehrdad Hessar <mhessar@octoml.ai>
2021-04-08 15:47:18 -0700

    [microTVM] Zephyr: RISCV support for Zephyr QEMU RISCV-32/64 (#7804)
    * working on qemu
    
    * debugging
    
    * riscv hacks
    
    * config added
    
    * change target platforms
    
    * fix merge
    
    * debugging issue with zephyr 2.5
    
    * cleanup
    
    * working on qemu
    
    * debugging
    
    * riscv hacks
    
    * config added
    
    * change target platforms
    
    * fix merge
    
    * debugging issue with zephyr 2.5
    
    * cleanup
    
    * testing
    
    * pass riscv64
    
    * fix merge
    
    * small fix
    
    * update vm_name
    
    * add zephyr version
    
    * add comment for riscv32 issue
    
    * remove debug messages
    
    * cleanup
    
    * cleanup
    
    * change workspace
    
    * fix zephyr version
    
    * cleanup
    
    * change to symlink
    
    * fix flag
    
    * add comment
    
    * lint check
    
    * lint fix
    
    * fix format
    
    * rename debugger
    
    * rework args
    
    Co-authored-by: Mehrdad Hessar <mhessar@octoml.local>
    Co-authored-by: Andrew Reusch <areusch@octoml.ai>

--- tests/lint/check_file_type.py
+++ tests/lint/check_file_type.py
@@ -131,11 +131,15 @@ ALLOW_SPECIFIC_FILE = {
     "tests/micro/zephyr/testdata/mnist-8.onnx",
     # microTVM Zephyr runtime
     "apps/microtvm/zephyr/demo_runtime/prj.conf",
+    "apps/microtvm/zephyr/demo_runtime/boards/qemu_x86.conf",
+    "apps/microtvm/zephyr/demo_runtime/boards/qemu_riscv32.conf",
+    "apps/microtvm/zephyr/demo_runtime/boards/qemu_riscv64.conf",
     "apps/microtvm/zephyr/demo_runtime/boards/nrf5340dk_nrf5340_cpuapp.conf",
     "apps/microtvm/zephyr/demo_runtime/boards/nucleo_f746zg.conf",
-    "apps/microtvm/zephyr/demo_runtime/boards/qemu_x86.conf",
     "apps/microtvm/zephyr/demo_runtime/boards/stm32f746g_disco.conf",
     "apps/microtvm/zephyr/demo_runtime/qemu-hack/qemu-system-i386",
+    "apps/microtvm/zephyr/demo_runtime/qemu-hack/qemu-system-riscv32",
+    "apps/microtvm/zephyr/demo_runtime/qemu-hack/qemu-system-riscv64",
     # microTVM Virtual Machines
     "apps/microtvm/reference-vm/zephyr/Vagrantfile",
     "apps/microtvm/reference-vm/zephyr/base-box/Vagrantfile.packer-template",
