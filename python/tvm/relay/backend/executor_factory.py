559fcd2c228ef1dd71811b7bc43e95e10f1b46a0
Giuseppe Rossini <giuseppe.rossini@arm.com>
2021-04-16 14:46:15 +0100

    fix tests
    Change-Id: I1297ccc54dd6d93647f421e0beb226f410bf73f5

--- python/tvm/relay/backend/executor_factory.py
+++ python/tvm/relay/backend/executor_factory.py
@@ -16,6 +16,7 @@
 # under the License.
 """Executor factory modules."""
 from abc import abstractmethod
+import warnings
 
 from tvm import tir
 
@@ -50,6 +51,35 @@ class ExecutorFactoryModule:
     def get_lib(self):
         """ Return the generated library"""
         raise NotImplementedError
+
+    @abstractmethod
+    def get_internal_repr(self):
+        """ Return the internal representation used to execute the network"""
+        raise NotImplementedError
+
+    def __getitem__(self, item):
+        print(item)
+        return self.module.__getitem__(item)
+
+    def __iter__(self):
+        warnings.warn(
+            "legacy graph executor behavior of producing json / lib / params will be "
+            "removed in the next release."
+            " Please see documents of tvm.contrib.graph_executor.GraphModule for the "
+            " new recommended usage.",
+            DeprecationWarning,
+            2,
+        )
+        return self
+
+    def __next__(self):
+        if self.iter_cnt > 2:
+            raise StopIteration
+
+        objs = [self.get_internal_repr(), self.lib, self.params]
+        obj = objs[self.iter_cnt]
+        self.iter_cnt += 1
+        return obj
 
 
 class AOTExecutorFactoryModule(ExecutorFactoryModule):
@@ -102,7 +132,7 @@ class GraphExecutorFactoryModule(ExecutorFactoryModule):
 
     Parameters
     ----------
-    graph_str : the json graph to be deployed in json format output by graph compiler.
+    graph_json_str : the json graph to be deployed in json format output by graph compiler.
         The graph can contain operator(tvm_op) that points to the name of
         PackedFunc in the libmod.
     target : tvm.Target
@@ -115,8 +145,8 @@ class GraphExecutorFactoryModule(ExecutorFactoryModule):
         The parameters of module
     """
 
-    def __init__(self, ir_mod, target, graph_str, libmod, libmod_name, params):
-        assert isinstance(graph_str, string_types)
+    def __init__(self, ir_mod, target, graph_json_str, libmod, libmod_name, params):
+        assert isinstance(graph_json_str, string_types)
         fcreate = get_global_func("tvm.graph_executor_factory.create")
         args = []
         for k, v in params.items():
@@ -125,8 +155,8 @@ class GraphExecutorFactoryModule(ExecutorFactoryModule):
 
         self.ir_mod = ir_mod
         self.target = target
-        self.module = fcreate(graph_str, libmod, libmod_name, *args)
-        self.graph = graph_str
+        self.module = fcreate(graph_json_str, libmod, libmod_name, *args)
+        self.graph_json = graph_json_str
         self.lib = libmod
         self.libmod_name = libmod_name
         self.params = params
@@ -136,19 +166,16 @@ class GraphExecutorFactoryModule(ExecutorFactoryModule):
         return self.module.export_library(file_name, fcompile, addons, **kwargs)
 
     def save_executor_config(self):
-        return self.graph
+        return self.internal_repr
 
     def get_params(self):
         return self.params
 
     def get_graph_json(self):
-        return self.internal_repr
+        return self.graph_json
 
     def get_internal_repr(self):
-        return self.graph
+        return self.graph_json
 
     def get_lib(self):
         return self.lib
-
-    def __getitem__(self, item):
-        return self.module.__getitem__(item)
