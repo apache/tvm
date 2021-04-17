2e9666f240758f73eeaaa0bf5049c13e518205da
Junru Shao <junrushao1994@gmail.com>
2021-04-17 06:36:46 -0700

    [TensorIR][M1b] Schedule class (#7847)
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>
    Co-authored-by: Jared Roesch <roeschinc@gmail.com>

--- src/tir/schedule/analysis.h
+++ src/tir/schedule/analysis.h
@@ -34,6 +34,21 @@ void VerifySRefTree(const ScheduleState& self);
 
 /******** Block-loop relation ********/
 /*!
+ * \brief Retrieve blocks in a specific function with its name
+ * \param self The schedule state
+ * \param name The name of the blocks to be retrieved
+ * \param func_name The name of the function
+ * \return A list of blocks with the specific name
+ */
+Array<StmtSRef> GetBlocks(const ScheduleState& self, const String& name, const String& func_name);
+/*!
+ * \brief Get the parent loops of the block in its scope, from outer to inner
+ * \param self The schedule state
+ * \param block_sref The query block
+ * \return A list of loops above the given block in its scope, from outer to inner
+ */
+Array<StmtSRef> GetLoops(const StmtSRef& block_sref);
+/*!
  * \brief Get the leaf blocks of a scope where a specific block/loop is in
  * \param self The schedule state
  * \param parent_sref The StmtSRef that points to the parent block/loop
