2990e8e4e005de6ec406a288045d511dd2d71008
Siyuan Feng <Hzfengsy@vip.qq.com>
2021-04-20 04:52:23 +0800

    [TensorIR][PASS][M1c] PlanUpdateBufferAllocationLocation (#7873)
    Co-authored-by: Tianqi Chen <tqchen@users.noreply.github.com>
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>

--- include/tvm/tir/transform.h
+++ include/tvm/tir/transform.h
@@ -352,6 +352,14 @@ TVM_DLL Pass HoistIfThenElse();
  */
 TVM_DLL Pass LowerInitBlock();
 
+/*!
+ * \brief Locate the buffer allocation to the exact position (usually is
+ *        the lca of buffer access). This pass will inject opaque block
+ *        with alloc_buffers at the allocation site.
+ * \return The pass.
+ */
+TVM_DLL Pass PlanAndUpdateBufferAllocationLocation();
+
 }  // namespace transform
 }  // namespace tir
 }  // namespace tvm
