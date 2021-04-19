4b06e6e0b3bd6ad6b19b1d4d2353897dd09b4c39
Matthew <mbrookhart@octoml.ai>
2021-04-19 09:53:12 -0600

    remove dead code

--- python/tvm/relay/frontend/onnx.py
+++ python/tvm/relay/frontend/onnx.py
@@ -2096,12 +2096,6 @@ class Resize(OnnxOpConverter):
             size = _op.cast(shape_of(inputs[0]), infer_type(scale).checked_type.dtype) * scale
         out_size = fold_constant(_op.strided_slice(size, [2], [4]))
 
-        ## TODO(mbrookhart): Need Dynamic Crop and Resize :(
-        # if coord_trans == "tf_crop_and_resize":
-        #     extrapolation_value = attr.get("extrapolation_value", 0.0)
-        #     boxes = _op.reshape(inputs[1], [-1, 4])
-        #     box_indices = fold_constant(_op.take(shape_of(boxes), _op.const(0, "int64"), axis=0))
-        #     return _op.image.crop_and_resize(inputs[1], boxes, box_indices, out_size, layout, method, extrapolation_value)
         return _op.image.resize(
             inputs[0], out_size, layout, method, coord_trans, nearest_mode, alpha, exclude
         )
