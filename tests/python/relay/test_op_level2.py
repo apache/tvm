86b4b529733ffeaf2c254040175d852eaa58e433
Josh Fromm <jwfromm@octoml.ai>
2021-04-22 01:01:37 +0000

    1D adaptive pooling added and tested.

--- tests/python/relay/test_op_level2.py
+++ tests/python/relay/test_op_level2.py
@@ -1007,37 +1007,66 @@ def test_pool2d():
     _test_global_pool2d(relay.nn.global_avg_pool2d, np.mean)
 
 
+def _test_pool1d(opfunc, pool_size=(2,), strides=(2,), padding=(0, 0), dtype="float32"):
+    n, c, w = te.var("n"), 10, 224
+    x = relay.var("x", relay.TensorType((n, c, w), "float32"))
+    y = opfunc(x, pool_size=(1,))
+    assert "pool_size=" in y.astext()
+    yy = run_infer_type(y)
+    assert yy.checked_type == relay.TensorType((n, 10, 224), "float32")
+    # test execution
+    dshape = (1, 3, 32)
+    for shape_dtype in ["int32", "int64"]:
+        x = relay.var("x", shape=[tvm.tir.IntImm(shape_dtype, x) for x in dshape], dtype=dtype)
+        pool_type = "max" if "max" in str(opfunc) else "avg"
+        y = opfunc(x, pool_size=pool_size, strides=strides, padding=padding)
+        func = relay.Function([x], y)
+        data = np.random.uniform(size=dshape).astype(dtype)
+        ref_res = tvm.topi.testing.pool1d_ncw_python(
+            data, (2,), (2,), (0, 0), (1, 3, 16), pool_type, False
+        )
+        for target, dev in tvm.testing.enabled_targets():
+            intrp1 = relay.create_executor("graph", device=dev, target=target)
+            op_res1 = intrp1.evaluate(func)(data)
+            tvm.testing.assert_allclose(op_res1.asnumpy(), ref_res, rtol=1e-5, atol=1e-5)
+
+
+def _test_global_pool1d(opfunc, reffunc):
+    n, c, w = te.size_var("n"), te.size_var("c"), 224
+    x = relay.var("x", relay.TensorType((n, w, c), "float32"))
+    y = opfunc(x, layout="NWC")
+    yy = run_infer_type(y)
+    assert yy.checked_type == relay.TensorType((n, 1, c), "float32")
+
+    n, c, w = te.size_var("n"), te.size_var("c"), te.size_var("w")
+    x = relay.var("x", relay.TensorType((n, c, w), "float32"))
+    y = opfunc(x)
+    yy = run_infer_type(y)
+    assert yy.checked_type == relay.TensorType((n, c, 1), "float32")
+    # test execution
+    dtype = "float32"
+    dshape = (1, 1024, 7)
+    x = relay.var("x", shape=dshape)
+    y = opfunc(x)
+    func = relay.Function([x], y)
+    data = np.random.uniform(size=dshape).astype(dtype)
+    ref_res = reffunc(data, axis=(2,), keepdims=True)
+    for target, dev in tvm.testing.enabled_targets():
+        intrp1 = relay.create_executor("graph", device=dev, target=target)
+        op_res1 = intrp1.evaluate(func)(data)
+        tvm.testing.assert_allclose(op_res1.asnumpy(), ref_res, rtol=1e-5, atol=1e-5)
+
+
 @tvm.testing.uses_gpu
 def test_pool1d():
-    def _test_pool1d(opfunc, pool_size=(2,), strides=(2,), padding=(0, 0), dtype="float32"):
-        n, c, w = te.var("n"), 10, 224
-        x = relay.var("x", relay.TensorType((n, c, w), "float32"))
-        y = opfunc(x, pool_size=(1,))
-        assert "pool_size=" in y.astext()
-        yy = run_infer_type(y)
-        assert yy.checked_type == relay.TensorType((n, 10, 224), "float32")
-        # test execution
-        dshape = (1, 3, 32)
-        for shape_dtype in ["int32", "int64"]:
-            x = relay.var("x", shape=[tvm.tir.IntImm(shape_dtype, x) for x in dshape], dtype=dtype)
-            pool_type = "max" if "max" in str(opfunc) else "avg"
-            y = opfunc(x, pool_size=pool_size, strides=strides, padding=padding)
-            func = relay.Function([x], y)
-            data = np.random.uniform(size=dshape).astype(dtype)
-            ref_res = tvm.topi.testing.pool1d_ncw_python(
-                data, (2,), (2,), (0, 0), (1, 3, 16), pool_type, False
-            )
-            for target, dev in tvm.testing.enabled_targets():
-                intrp1 = relay.create_executor("graph", device=dev, target=target)
-                op_res1 = intrp1.evaluate(func)(data)
-                tvm.testing.assert_allclose(op_res1.asnumpy(), ref_res, rtol=1e-5, atol=1e-5)
-
     _test_pool1d(relay.nn.max_pool1d)
     _test_pool1d(relay.nn.max_pool1d, dtype="int32")
     _test_pool1d(relay.nn.max_pool1d, pool_size=2, strides=2, padding=0)
     _test_pool1d(relay.nn.avg_pool1d)
     _test_pool1d(relay.nn.avg_pool1d, dtype="int32")
     _test_pool1d(relay.nn.avg_pool1d, pool_size=2, strides=2, padding=0)
+    _test_global_pool1d(relay.nn.global_max_pool1d, np.max)
+    _test_global_pool1d(relay.nn.global_avg_pool1d, np.mean)
 
 
 @tvm.testing.uses_gpu
