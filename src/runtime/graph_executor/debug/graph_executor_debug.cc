c6aaec965719caaf68c79119cdb8e6c65ab773c7
Mehrdad Hessar <mhessar@octoml.ai>
2021-04-26 11:39:06 -0700

    bring back for java api

--- src/runtime/graph_executor/debug/graph_executor_debug.cc
+++ src/runtime/graph_executor/debug/graph_executor_debug.cc
@@ -276,6 +276,28 @@ class GraphExecutorDebug : public GraphExecutor {
   }
 
   /*!
+   * \brief Copy index-th node to data_out.
+   *
+   * This method will do a partial run of the the graph
+   * from begining upto the index-th node and return output of index-th node.
+   * This is costly operation and suggest to use only for debug porpose.
+   *
+   * \param index: The  index of the node.
+   * \param data_out the node data.
+   */
+  void DebugGetNodeOutput(int index, DLTensor* data_out) {
+    ICHECK_LT(static_cast<size_t>(index), op_execs_.size());
+    uint32_t eid = index;
+
+    for (size_t i = 0; i < op_execs_.size(); ++i) {
+      if (op_execs_[i]) op_execs_[i]();
+      if (static_cast<int>(i) == index) break;
+    }
+
+    data_entry_[eid].CopyTo(data_out);
+  }
+
+  /*!
    * \brief Profile execution time of the module.
    *
    * We run the entire module while recording overall and per-op timing
@@ -339,6 +361,14 @@ PackedFunc GraphExecutorDebug::GetFunction(const std::string& name,
   } else if (name == "run_get_layers_outputs") {
     return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
       *rv = this->RunAndGetLayersOutputs();
+    });
+  } else if (name == "debug_get_output") {
+    return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
+      if (String::CanConvertFrom(args[0])) {
+        this->DebugGetNodeOutput(this->GetNodeIndex(args[0]), args[1]);
+      } else {
+        this->DebugGetNodeOutput(args[0], args[1]);
+      }
     });
   } else if (name == "run_individual") {
     return PackedFunc([sptr_to_self, this](TVMArgs args, TVMRetValue* rv) {
