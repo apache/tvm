684e513042f32de77aad5a190683cfe92962c034
Chris Sullivan <csullivan@octoml.ai>
2021-04-29 15:32:57 -0700

    Dispatch texture allocas via target specialized tir.tvm_call_packed

--- src/tir/transforms/lower_tvm_builtin.cc
+++ src/tir/transforms/lower_tvm_builtin.cc
@@ -360,20 +360,25 @@ class BuiltinLower : public StmtExprMutator {
                                     throw_last_error),
                          let->body});
     DataType dtype = let->var->type_annotation.as<TextureTypeNode>()->element_type.as<PrimTypeNode>()->dtype;
-    Stmt alloca = LetStmt(
-        let->var,
-        Call(let->var.dtype(), Op::Get("tir.TVMBackendAllocTexture"),
-                    {cast(DataType::Int(32), device_type_),
-                        cast(DataType::Int(32), device_id_),
-                        cast(DataType::UInt(64), call->args[0]),
-                        cast(DataType::UInt(64), call->args[1]),
-                        IntImm(DataType::Int(32), dtype.code()),
-                        IntImm(DataType::Int(32), dtype.bits())}),
-        body);
 
-    PrimExpr free_op = Call(DataType::Int(32), Op::Get("tir.TVMBackendFreeTexture"),
-                            {cast(DataType::Int(32), device_type_),
-                             cast(DataType::Int(32), device_id_), let->var});
+    std::string fdevapi_prefix = "device_api.";
+    fdevapi_prefix += runtime::DeviceName(device_type_.as<IntImmNode>()->value);
+    Call call_packed = Call(let->var.dtype(), builtin::tvm_call_packed(),
+                            {StringImm(fdevapi_prefix + ".AllocTexture"),
+                                cast(DataType::Int(32), device_type_),
+                                cast(DataType::Int(32), device_id_),
+                                cast(DataType::UInt(64), call->args[0]),
+                                cast(DataType::UInt(64), call->args[1]),
+                                IntImm(DataType::Int(32), dtype.code()),
+                                IntImm(DataType::Int(32), dtype.bits())});
+
+    Stmt alloca = LetStmt(let->var, call_packed, body);
+
+    Call free_op = Call(DataType::Int(32), builtin::tvm_call_packed(),
+                     {StringImm(fdevapi_prefix + ".FreeTexture"),
+                         cast(DataType::Int(32), device_type_),
+                         cast(DataType::Int(32), device_id_), let->var});
+
     Stmt free_stmt = IfThenElse(free_op != make_zero(DataType::Int(32)), throw_last_error);
     body = SeqStmt({alloca, free_stmt});
     return body;
