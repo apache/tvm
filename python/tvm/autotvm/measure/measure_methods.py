6f72a2498d4b49e41d329f837235882a47596630
Andrew Liu <andrewlliu@gmail.com>
2021-05-01 01:27:36 -0700

    [AutoTVM] [TOPI] Support AutoTVM for int4 tensorcore (#7831)
    * initial
    
    * int4 asnumpy
    
    * remove
    
    * random test
    
    * format
    
    * random
    
    * remove unused import
    
    * change dist range
    
    * add fuse_pack in
    
    * random engine
    
    * reformat
    
    * remove import
    
    * add cuda context
    
    * refactor code

--- python/tvm/autotvm/measure/measure_methods.py
+++ python/tvm/autotvm/measure/measure_methods.py
@@ -32,7 +32,6 @@ import typing
 from random import getrandbits
 from collections import namedtuple
 import tempfile
-import numpy as np
 
 import tvm._ffi
 import tvm.ir.transform
@@ -583,7 +582,7 @@ def run_through_rpc(
                 raise AttributeError(
                     "Please make sure USE_RANDOM is ON in the config.cmake " "on the remote devices"
                 )
-            args = [nd.array(np.zeros(x[0], dtype=x[1]), device=dev) for x in build_result.arg_info]
+            args = [nd.empty(x[0], x[1], dev) for x in build_result.arg_info]
             if "scatter" not in measure_input.task.name:
                 # the index tensor of scatter op cannot be randomly initialized
                 for arg in args:
