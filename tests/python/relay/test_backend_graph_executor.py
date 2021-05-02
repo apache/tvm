a8a835866daf341a9fa8aa9025d6e6b7cd465510
Tianqi Chen <tqchen@users.noreply.github.com>
2021-05-02 00:00:10 -0400

    [RELAY] Turn reshape into nop in graph executor backend. (#7945)
    * [RELAY] Turn reshape into nop in graph executor backend.
    
    Previously we are generating the function calls for reshape.
    This PR updates the optimization to turn reshape into nop:
    
    - Tag a fused function as reshape only if it only contains reshape.
    - Update memory planner to force input output to share the same piece of memory
    - Update the graph runtime codegen to emit nop when reshape only function is encountered.
    
    * Address review comments.
    
    * Additional comment and TODOs on the rationale

--- tests/python/relay/test_backend_graph_executor.py
+++ tests/python/relay/test_backend_graph_executor.py
@@ -17,6 +17,7 @@
 import numpy as np
 
 import tvm
+import json
 from tvm import relay
 from tvm.contrib import graph_executor
 from tvm.relay.op import add
@@ -146,6 +147,51 @@ def test_plan_memory():
     assert len(device_types) == 1
 
 
+def test_reshape_nop():
+    # test that reshape can be turned into nop
+    x = relay.var("x", shape=(10, 4))
+    xx = relay.abs(x)
+    y = relay.expand_dims(xx, axis=1)
+    t0 = relay.reshape(y, (1, 40))
+    t1 = relay.abs(y)
+
+    z0 = relay.reshape(t0, (2, 20))
+    z1 = relay.sqrt(t1)
+    z2 = relay.reshape(t1, (1, 40))
+
+    func = relay.Function([x], relay.Tuple([z0, z1, z2]))
+    x_data = np.random.rand(10, 4).astype("float32")
+    graph = relay.build(tvm.IRModule.from_expr(func), "llvm")
+    graph_json_str = graph.get_json()
+
+    graph_json = json.loads(graph_json_str)
+
+    # reshape must force sharing memory
+    storage_ids = graph_json["attrs"]["storage_id"][1]
+    assert tuple(storage_ids) == (0, 1, 1, 2, 3, 2)
+    assert graph_json["nodes"][2]["attrs"]["func_name"] == "__nop"
+    assert graph_json["nodes"][5]["attrs"]["func_name"] == "__nop"
+
+    gmod = graph_executor.GraphModule(graph["default"](tvm.cpu(0)))
+
+    gmod.set_input(x=x_data)
+    gmod.run()
+    z0_np = x_data.reshape(2, 20)
+    z1_np = np.sqrt(
+        np.abs(
+            x_data.reshape(
+                10,
+                1,
+                4,
+            )
+        )
+    )
+    z2_np = np.abs(x_data).reshape(1, 40)
+    tvm.testing.assert_allclose(gmod.get_output(0).asnumpy(), z0_np)
+    tvm.testing.assert_allclose(gmod.get_output(1).asnumpy(), z1_np)
+    tvm.testing.assert_allclose(gmod.get_output(2).asnumpy(), z2_np)
+
+
 @tvm.testing.uses_gpu
 def test_gru_like():
     def unit(rnn_dim):
@@ -231,6 +277,7 @@ def test_graph_executor_nested_tuples():
 
 
 if __name__ == "__main__":
+    test_reshape_nop()
     test_plan_memory()
     test_with_params()
     test_add_op_scalar()
