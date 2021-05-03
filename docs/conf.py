aacbdb2432cc906b0363382822c12c78decb7258
Tianqi Chen <tqchen@users.noreply.github.com>
2021-05-03 13:29:25 -0400

    [DOCS] Update to show github version (#7948)
    * [DOCS] Update to show github version
    
    * Remove dup code

--- docs/conf.py
+++ docs/conf.py
@@ -47,18 +47,31 @@ sys.path.insert(0, os.path.join(curr_path, "../vta/python"))
 # General information about the project.
 project = "tvm"
 author = "Apache Software Foundation"
-copyright = "2020, %s" % author
+copyright = "2020 - 2021, %s" % author
 github_doc_root = "https://github.com/apache/tvm/tree/main/docs/"
 
 os.environ["TVM_BUILD_DOC"] = "1"
+
+
+def git_describe_version(original_version):
+    """Get git describe version."""
+    ver_py = os.path.join(curr_path, "..", "version.py")
+    libver = {"__file__": ver_py}
+    exec(compile(open(ver_py, "rb").read(), ver_py, "exec"), libver, libver)
+    _, gd_version = libver["git_describe_version"]()
+    if gd_version != original_version:
+        print("Use git describe based version %s" % gd_version)
+    return gd_version
+
+
 # Version information.
 import tvm
 from tvm import topi
 from tvm import te
 from tvm import testing
 
-version = tvm.__version__
-release = tvm.__version__
+version = git_describe_version(tvm.__version__)
+release = version
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones
