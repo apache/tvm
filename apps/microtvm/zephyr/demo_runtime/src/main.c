b84fe4f82077ff899c470dccf0df3de0ca32a0d4
Mehrdad Hessar <mhessar@octoml.ai>
2021-05-11 15:55:15 -0700

    cleanup

--- apps/microtvm/zephyr/demo_runtime/src/main.c
+++ apps/microtvm/zephyr/demo_runtime/src/main.c
@@ -215,7 +215,7 @@ tvm_crt_error_t TVMPlatformTimerStop(double* elapsed_time_seconds) {
 
 // Ring buffer used to store data read from the UART on rx interrupt.
 // Should be larger than TVM_CRT_MAX_PACKET_SIZE_BYTES
-#define RING_BUF_SIZE_BYTES TVM_CRT_MAX_PACKET_SIZE_BYTES + 100
+#define RING_BUF_SIZE_BYTES (TVM_CRT_MAX_PACKET_SIZE_BYTES + 100)
 RING_BUF_DECLARE(uart_rx_rbuf, RING_BUF_SIZE_BYTES);
 
 // Small buffer used to read data from the UART into the ring buffer.
