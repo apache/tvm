78914eebe2bb38c751f70a453847cf752d980434
Josh Fromm <jwfromm@octoml.ai>
2021-05-11 22:46:30 +0000

    Fixed strided_slice alteroplayout bug.

--- src/relay/op/tensor/transform.cc
+++ src/relay/op/tensor/transform.cc
@@ -2570,16 +2570,17 @@ Array<Array<Layout>> StridedSliceInferCorrectLayout(const Attrs& attrs,
     if (params->begin && params->end && params->strides) {
       for (Integer i : params->strides.value()) {
         ICHECK(i.defined());
-        strides.push_back(params->slice_mode == "size" ? 1 : i->value);
+        auto slice_val = Integer(IntImm(DataType::Int(64), i->value));
+        strides.push_back(params->slice_mode == "size" ? Integer(1) : slice_val);
       }
 
       for (Integer i : params->begin.value()) {
         ICHECK(i.defined());
-        begin.push_back(i->value);
+        begin.push_back(IntImm(DataType::Int(64), i->value));
       }
       for (Integer i : params->end.value()) {
         ICHECK(i.defined());
-        end.push_back(i->value);
+        end.push_back(IntImm(DataType::Int(64), i->value));
       }
     }
 
@@ -2619,9 +2620,9 @@ Array<Array<Layout>> StridedSliceInferCorrectLayout(const Attrs& attrs,
             ed = shape[new_index].as<IntImmNode>()->value;
           }
 
-          new_begin.push_back(bg);
-          new_end.push_back(ed);
-          new_strides.push_back(st);
+          new_begin.push_back(IntImm(DataType::Int(64), bg));
+          new_end.push_back(IntImm(DataType::Int(64), ed));
+          new_strides.push_back(IntImm(DataType::Int(64), st));
         }
         params->begin = new_begin;
         params->end = new_end;
@@ -2637,8 +2638,8 @@ Array<Array<Layout>> StridedSliceInferCorrectLayout(const Attrs& attrs,
         }
         auto factor = new_layout.FactorOf(axis);
         if (factor == -1) {
-          new_begin.push_back(begin[i]);
-          new_end.push_back(end[i]);
+          new_begin.push_back(IntImm(DataType::Int(64), begin[i]));
+          new_end.push_back(IntImm(DataType::Int(64), end[i]));
         } else {
           if (strides.defined() && i < strides.size()) {
             auto stride = strides[i];
@@ -2665,8 +2666,8 @@ Array<Array<Layout>> StridedSliceInferCorrectLayout(const Attrs& attrs,
             // transform to original layout
             return {{Layout::Undef()}, {Layout::Undef()}};
           }
-          new_begin.push_back(tvm::Integer(bg / factor));
-          new_end.push_back(tvm::Integer(ed / factor));
+          new_begin.push_back(IntImm(DataType::Int(64), (bg / factor)));
+          new_end.push_back(IntImm(DataType::Int(64), (ed / factor)));
         }
       }
 
