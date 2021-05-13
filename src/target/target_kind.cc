af6e7665487e4c1db08db5f55c1c7878cf082144
Chris Sidebottom <christopher.sidebottom@arm.com>
2021-05-13 08:42:41 +0000

    Enable --no-typed-operators for llvm

--- src/target/target_kind.cc
+++ src/target/target_kind.cc
@@ -258,6 +258,7 @@ TVM_REGISTER_TARGET_KIND("llvm", kDLCPU)
     .add_attr_option<Bool>("system-lib")
     .add_attr_option<String>("runtime")
     .add_attr_option<Bool>("link-params", Bool(false))
+    .add_attr_option<Bool>("no-typed-operators", Bool(false))
     .set_default_keys({"cpu"});
 
 TVM_REGISTER_TARGET_KIND("c", kDLCPU)
