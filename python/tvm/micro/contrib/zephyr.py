a4d9508e2c675feda22a4f3931629e0029faaf41
Mehrdad Hessar <mhessar@octoml.ai>
2021-05-14 09:02:09 -0700

    hack for testing

--- python/tvm/micro/contrib/zephyr.py
+++ python/tvm/micro/contrib/zephyr.py
@@ -639,13 +639,15 @@ class ZephyrQemuTransport(Transport):
         )
 
     def open(self):
-        self.pipe_dir = tempfile.mkdtemp()
+        # self.pipe_dir = tempfile.mkdtemp()
+        self.pipe_dir = "/tinymlperf/test_fifo"
+        
         self.pipe = os.path.join(self.pipe_dir, "fifo")
         self.write_pipe = os.path.join(self.pipe_dir, "fifo.in")
         self.read_pipe = os.path.join(self.pipe_dir, "fifo.out")
 
-        os.mkfifo(self.write_pipe)
-        os.mkfifo(self.read_pipe)
+        # os.mkfifo(self.write_pipe)
+        # os.mkfifo(self.read_pipe)
         if self.qemu_debugger is not None:
             if "env" in self.kwargs:
                 self.kwargs["env"] = copy.copy(self.kwargs["env"])
@@ -689,9 +691,9 @@ class ZephyrQemuTransport(Transport):
         if self.proc is not None:
             self.proc = None
 
-        if self.pipe_dir is not None:
-            shutil.rmtree(self.pipe_dir)
-            self.pipe_dir = None
+        # if self.pipe_dir is not None:
+        #     shutil.rmtree(self.pipe_dir)
+        #     self.pipe_dir = None
 
     def read(self, n, timeout_sec):
         if self.fd_transport is None:
