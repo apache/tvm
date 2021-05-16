9f34b7cdc988020d9cecff1ef5ca88f39af87a0b
Zack Chen <quic_zackc@quicinc.com>
2021-05-17 00:03:40 +0800

    Fix test_pass_annotation.py

--- tests/python/relay/test_pass_annotation.py
+++ tests/python/relay/test_pass_annotation.py
@@ -32,6 +32,7 @@ class Trace:
     def run_before_pass(self, module, pass_info):
         if pass_info.name == "ManifestAlloc":
             pass  # import pdb; pdb.set_trace()
+        return True
 
     def run_after_pass(self, module, pass_info):
         if pass_info.name == "ManifestAlloc":
