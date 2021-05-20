e27bdd7c75d0e8468eb6bce0a066b34759ed2d2f
Mehrdad Hessar <mhessar@octoml.ai>
2021-05-19 18:56:52 -0700

    cleanup

--- apps/microtvm/zephyr/aot_demo/include/output_data.h
+++
@@ -1,6 +0,0 @@
-#include <stddef.h>
-#include <stdint.h>
-#include <dlpack/dlpack.h>
-const size_t output_data_len = 10;
-float output_data[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, };
-
