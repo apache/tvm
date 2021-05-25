0dd603f2caef98c548332258a4b9ad2177c97cdf
Zack Chen <quic_zackc@quicinc.com>
2021-05-25 09:56:40 +0800

    Doc override_instruments()

--- python/tvm/ir/transform.py
+++ python/tvm/ir/transform.py
@@ -105,6 +105,14 @@ class PassContext(tvm.runtime.Object):
         _ffi_transform_api.ExitPassContext(self)
 
     def override_instruments(self, instruments):
+        """Override instruments within this PassContext.
+
+        If there are existing instruments, their exit_pass_ctx callbacks are called.
+        Then switching to new instruments and calling new enter_pass_ctx callbacks.
+
+        instruments : Sequence[PassInstrument]
+            The list of pass instrument implementations.
+        """
         _ffi_transform_api.OverrideInstruments(self, instruments)
 
     @staticmethod
