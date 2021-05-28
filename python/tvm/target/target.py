a3cab8de23c1916529888093c792504a505d8901
Mehrdad Hessar <mhessar@octoml.ai>
2021-05-28 10:13:17 -0700

    [microTVM] AOT Demo (#8075)
    * initial
    
    * remove compare
    
    * temp fix
    
    * debugging
    
    * hack
    
    * hack for testing
    
    * both test pass
    
    * cleanup
    
    * fix tests and tutorials
    
    * restructure
    
    * cleanup
    
    * cleanup
    
    * fix check files
    
    * fixed for physical devices
    
    * address comments
    
    * reduce nrf stack size
    
    * update sample url
    
    * format

--- python/tvm/target/target.py
+++ python/tvm/target/target.py
@@ -296,9 +296,12 @@ def micro(model="unknown", options=None):
     if model not in MICRO_SUPPORTED_MODELS:
         raise ValueError(f"Model {model} not supported by tvm.target.micro.")
     opts = _merge_opts(
-        MICRO_SUPPORTED_MODELS[model] + ["-runtime=c", "--system-lib", f"-model={model}"],
+        MICRO_SUPPORTED_MODELS[model] + ["-runtime=c", f"-model={model}"],
         options,
     )
+
+    if (not options) or (options and "--executor=aot" not in options):
+        opts = _merge_opts(opts, "--system-lib")
 
     # NOTE: in the future, the default micro target will be LLVM except when
     # external dependencies are present.
