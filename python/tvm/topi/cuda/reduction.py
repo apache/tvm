ce8060962fbac86cca578b068fcdf2f3addebe9c
Egor Churaev <egor.churaev@gmail.com>
2021-06-07 16:51:56 +0300

    [Metal] Reduce number of threads for reduction layers
    Reduced default number of threads in reduction kernels for Metal.
    Default code generation generated thread block with the following size:
    32x32x1. With this size number of threads per threadgroup was equal to
    1024 (32 * 32 * 1). Sometimes device doesn't have enough resources and
    in this case we will get an exception that the block size is greater
    than value of maxTotalThreadsPerThreadgroup.
    To prevent such situation we decrease default number of threads. With
    this fix every model should work with default codegen and auto-tuning or
    auto-scheduling will select the optimal number of threads.

--- python/tvm/topi/cuda/reduction.py
+++ python/tvm/topi/cuda/reduction.py
@@ -37,7 +37,7 @@ def _schedule_reduce(op, sch, is_idx_reduce=False):
         all_reduce = False
         num_thread = 32
         target = tvm.target.Target.current()
-        if target and target.kind.name == "opencl":
+        if target and (target.kind.name == "opencl" or target.kind.name == "metal"):
             # without it, CL_INVALID_WORK_GROUP_SIZE occurred when running test_topi_reduce.py
             # don't know why
             num_thread = 16
