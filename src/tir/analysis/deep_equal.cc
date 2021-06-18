60fb0d389a04a44cc4556470988a1d8f0dea080e
hgt312 <guangtai@amazon.com>
2021-06-18 19:38:37 +0800

    fix lint

--- src/tir/analysis/deep_equal.cc
+++ src/tir/analysis/deep_equal.cc
@@ -59,7 +59,7 @@ bool ExprDeepEqual::operator()(const PrimExpr& lhs, const PrimExpr& rhs) const {
     auto* prhs = rhs.as<IntImmNode>();
     return plhs->dtype == prhs->dtype && plhs->value == prhs->value;
   }
-  if (auto* plhs = lhs.as<AnyNode>()) {
+  if (lhs.as<AnyNode>()) {
     return lhs.same_as(rhs);
   }
   return DeepCmpSEqualHandler().SEqualReduce(lhs, rhs, false);
