68fafa974188d82d49c4f9a9b30085a8c47b3bde
root <schilkunda@ambarella.com>
2021-06-24 06:37:56 +0000

    * fixed formatting issues in relay to onnx conversion script

--- python/tvm/contrib/target/onnx.py
+++ python/tvm/contrib/target/onnx.py
@@ -618,18 +618,18 @@ class ConstantOfShapeOnes(ConstantOfShapeZeros):
 
 
 class LRN(OpConverter):
-    """ Operator converter for LRN.
-    """
+  """ Operator converter for LRN."""
 
-    @classmethod
-    def convert_attributes(cls, attrs):
-        return {
-            'alpha': attrs.alpha,
-            'beta': attrs.beta,
-            'bias': attrs.bias,
-            'size': attrs.size
-            #axis?
-        }
+  @classmethod
+  def convert_attributes(cls, attrs):
+    return {
+      'alpha': attrs.alpha,
+      'beta': attrs.beta,
+      'bias': attrs.bias,
+      'size': attrs.size
+      #axis?
+    }
+
 
 relay_to_onnx_op_mapping = {
     "reshape": Reshape,
@@ -664,7 +664,7 @@ relay_to_onnx_op_mapping = {
     "layout_transform": LayoutTransform,
     "clip": Clip,
     "expand_dims": Expand,
-    'nn.lrn': LRN,
+    "nn.lrn": LRN,
 }
 
 
