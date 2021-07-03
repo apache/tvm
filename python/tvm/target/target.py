7e3f068373937c0ae08d58f67b84030a027db1c9
Mehrdad Hessar <mhessar@octoml.ai>
2021-07-03 03:48:06 -0700

    [microTVM] Add Nucleo stm32l4r5zi board to zephyr (#8386)
    * add stm32l4r5zi_nucleo
    
    * add parameter for test qemu
    
    * file type check
    
    * fix test
    
    * change order
    
    * revert

--- python/tvm/target/target.py
+++ python/tvm/target/target.py
@@ -281,9 +281,10 @@ def intel_graphics(model="unknown", options=None):
 
 MICRO_SUPPORTED_MODELS = {
     "host": [],
-    "stm32f746xx": ["-mcpu=cortex-m7", "-march=armv7e-m"],
-    "nrf5340dk": ["-mcpu=cortex-m33"],
     "mps2_an521": ["-mcpu=cortex-m33"],
+    "nrf5340dk": ["-mcpu=cortex-m33"],
+    "stm32f746xx": ["-mcpu=cortex-m7", "-march=armv7e-m"],
+    "stm32l4r5zi": ["-mcpu=cortex-m4"],
 }
 
 
