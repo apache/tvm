ae4a200f84847ac80d8cacae6b9b49989c07453f
Tantalus <jsl_713@live.com>
2021-07-07 12:33:34 +0800

    [relay] pass to convert spconv2d with const args

--- python/tvm/autotvm/measure/measure_methods.py
+++ python/tvm/autotvm/measure/measure_methods.py
@@ -254,13 +254,14 @@ class RPCRunner(Runner):
 
     @ref_input.setter
     def ref_input(self, val):
-        warnings.warn(
-            "You are specifying fixed input for tuning the operator. "
-            "Be sure your input always fits the operator. Some "
-            "operators may conduct layout transformation during tuning, "
-            "thus can lead to unexpected behaviors. ",
-            RuntimeWarning,
-        )
+        if val is not None:
+            warnings.warn(
+                "You are specifying fixed input for tuning the operator. "
+                "Be sure your input always fits the operator. Some "
+                "operators may conduct layout transformation during tuning, "
+                "thus can lead to unexpected behaviors. ",
+                RuntimeWarning,
+            )
         self._ref_input = val
 
     def set_task(self, task):
