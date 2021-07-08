e84aa8ea578e8408bd656a61e2529159c1ec52dc
Andrew Reusch <areusch@octoml.ai>
2021-07-08 11:53:18 -0700

    Delete old build abstractions

--- python/tvm/micro/__init__.py
+++ python/tvm/micro/__init__.py
@@ -16,13 +16,8 @@
 # under the License.
 """MicroTVM module for bare-metal backends"""
 
-from .artifact import Artifact
 from .build import default_options, get_standalone_crt_dir
 from .build import get_standalone_crt_lib, Workspace
-from .compiler import Compiler, DefaultCompiler, Flasher
-from .debugger import GdbRemoteDebugger
-from .micro_library import MicroLibrary
-from .micro_binary import MicroBinary
 from .model_library_format import export_model_library_format, UnsupportedInModelLibraryFormatError
 from .project import generate_project, GeneratedProject, TemplateProject
 from .session import (
