d8edc358c78acb6a243ef54d0ce65fe0cdad6e63
Andrew Luo <andrew.zhao.luo@gmail.com>
2021-07-13 10:59:03 -0700

    blackify

--- python/tvm/relay/transform/__init__.py
+++ python/tvm/relay/transform/__init__.py
@@ -16,7 +16,8 @@
 # under the License.
 # pylint: disable=wildcard-import, redefined-builtin, invalid-name
 """The Relay IR namespace containing transformations."""
+from . import fake_quantization_to_integer, mixed_precision
+from .recast import recast
+
 # transformation passes
 from .transform import *
-from .recast import recast
-from . import fake_quantization_to_integer, mixed_precision
