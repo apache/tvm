1ef21320ef65abdbaaa152cddd218a1ccca83631
zhuwenxi <zhuwenxi8935@gmail.com>
2021-07-13 10:57:16 +0800

    [BugFix][TOPI] Fix the integer overflow problem of the scatter_nd op. (#8415)
    * Fix the integer overflow problem of the scatter_nd op.
    
    * Fix scatter_nd's crash problem:
    1. Existing scatter_nd cuda implementation has a very large bound,
       which could overflow int32 range when input tensor shape is
       large enough;
    2. The overflow could cause the if statement always evaluate to
       true, thus conducts invalid memory accesses;
    3. We fix this problem in this commit by reducing the bound, the
       original large bound is not only unnecessary, but also degrading
       the performance; With this fix, scatter_op's performance improves
       100x on some cases.
    
    Co-authored-by: wenxizhu <wenxizhu@tencent.com>

--- python/tvm/topi/cuda/scatter.py
+++ python/tvm/topi/cuda/scatter.py
@@ -801,9 +801,11 @@ def scatter_nd(data, indices, updates, mode):
         bdim = ceil_div(fused_updates_dimension, tdim)
         ib.scope_attr(bx, "thread_extent", bdim)
 
-        with ib.for_range(0, ceil_div(fused_shape, bdim)) as i:
+        # Copy data into the output. This loop writes to the same portions of
+        # memory as the following loop, so we do not need a memory sync.
+        with ib.for_range(0, ceil_div(fused_shape, fused_updates_dimension), name="i") as i:
             index = i * fused_updates_dimension + bx * tdim + tx
-            with ib.if_scope(index < fused_shape):
+            with ib.if_scope(bx * tdim + tx < fused_updates_dimension):
                 out[index] = data[index]
 
         with ib.for_range(0, fused_indices_dimension) as i:
