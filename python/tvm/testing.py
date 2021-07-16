744dc66902e76bf4cf9560258eea77aa9e4d3724
Raghav-Chakravarthy <60329964+Raghav-Chakravarthy@users.noreply.github.com>
2021-07-16 02:08:10 -0400

    fix typo (#8484)
    * fix typo
    
    * Fixed typos in documentation

--- python/tvm/testing.py
+++ python/tvm/testing.py
@@ -78,7 +78,7 @@ def assert_allclose(actual, desired, rtol=1e-7, atol=1e-7):
     """Version of np.testing.assert_allclose with `atol` and `rtol` fields set
     in reasonable defaults.
 
-    Arguments `actual` and `desired` are not interchangable, since the function
+    Arguments `actual` and `desired` are not interchangeable, since the function
     compares the `abs(actual-desired)` with `atol+rtol*abs(desired)`.  Since we
     often allow `desired` to be close to zero, we generally want non-zero `atol`.
     """
