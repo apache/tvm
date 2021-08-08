2aa311470d732d6c1567066b6e4f2a86c2dc3dda
YuchenJin <yuchenj@cs.washington.edu>
2021-08-08 06:02:14 -0700

    remove the unnecessary loop in tecompiler

--- src/relay/backend/te_compiler.cc
+++ src/relay/backend/te_compiler.cc
@@ -348,15 +348,6 @@ class LowerTensorExpr : public ExprMutator {
                                  << ext_func->prim_fn_var->name_hint;
 
       Map<GlobalVar, tir::PrimFunc> prim_fns;
-
-      for (auto prim_fn : ext_func->funcs->functions) {
-        if (prim_fn.second->GetAttr<String>(attr::kCompiler).defined()) {
-          continue;
-        }
-        CHECK(prim_fn.second.as<tir::PrimFuncNode>()) << "must be a prim fn";
-        prim_fns.Set(prim_fn.first, Downcast<tir::PrimFunc>(prim_fn.second));
-      }
-
       relay::Function func_with_metadata = func;
       func_with_metadata = WithAttr(func_with_metadata, "prim_fn_var", ext_func->prim_fn_var);
       func_with_metadata = WithAttr(func_with_metadata, "prim_funcs", prim_fns);
