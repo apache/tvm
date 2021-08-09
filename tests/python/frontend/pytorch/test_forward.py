effe8f28207e80bd528e7c938b8359792e282af3
Alperen Bag <alperen.bag@boun.edu.tr>
2021-08-09 11:27:51 +0300

    [Frontend][Pytorch] Unit Test added for aten::silu

--- tests/python/frontend/pytorch/test_forward.py
+++ tests/python/frontend/pytorch/test_forward.py
@@ -698,6 +698,14 @@ def test_forward_selu():
     input_shape = [1, 3, 10, 10]
     input_data = torch.rand(input_shape).float()
     verify_model(torch.nn.SELU().eval(), input_data=input_data)
+    
+
+@tvm.testing.uses_gpu
+def test_forward_silu():
+    torch.set_grad_enabled(False)
+    input_shape = [1, 3, 10, 10]
+    input_data = torch.rand(input_shape).float()
+    verify_model(torch.nn.SiLU().eval(), input_data=input_data)
 
 
 @tvm.testing.uses_gpu
