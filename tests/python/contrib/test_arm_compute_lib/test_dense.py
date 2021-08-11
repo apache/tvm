7b86a7f2a9ca15cb6d370242385843e7969d1813
masahi <masahi129@gmail.com>
2021-08-12 04:08:24 +0900

    Update tests/python/contrib/test_arm_compute_lib/test_dense.py
    Co-authored-by: lhutton1 <35535092+lhutton1@users.noreply.github.com>

--- tests/python/contrib/test_arm_compute_lib/test_dense.py
+++ tests/python/contrib/test_arm_compute_lib/test_dense.py
@@ -154,7 +154,17 @@ def _get_expected_codegen(shape, weight_shape, units, dtype, has_bias=False):
             {
                 "op": "const",
                 "name": "",
-                "attrs": {"shape": [[[1, weight_shape[0]]]], "dtype": [[bias_dtype]]},
+if has_bias:
+        bias_dtype = "int32" if dtype == "uint8" else "float32"
+        bias_shape = [1, weight_shape[0]] if dtype == "float32" and \
+                     weight_shape[0] != 1 else [weight_shape[0]]
+        inputs.append(
+            {
+                "op": "const",
+                "name": "",
+                "attrs": {"shape": [[bias_shape]], "dtype": [[bias_dtype]]},
+            }
+        )
             }
         )
 
