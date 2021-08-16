05fa60efcae51dbaa6dadae5e39ec0b1affbe12f
Ruihang Lai <lairuihangdongdong@qq.com>
2021-08-16 20:49:16 +0800

    Fix lint

--- tests/python/unittest/test_tir_transform_unify_thread_binding.py
+++ tests/python/unittest/test_tir_transform_unify_thread_binding.py
@@ -37,12 +37,20 @@ def element_wise_thread_x(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
     tir.launch_thread(i, 128)
     with tir.launch_thread(j0_0, 4):
         for j0_1 in tir.serial(0, 32):
-            tir.store(B.data, i * 128 + j0_0 * 32 + j0_1,
-                      tir.load("float32", A.data, i * 128 + j0_0 * 32 + j0_1) * 2.0, True)
+            tir.store(
+                B.data,
+                i * 128 + j0_0 * 32 + j0_1,
+                tir.load("float32", A.data, i * 128 + j0_0 * 32 + j0_1) * 2.0,
+                True,
+            )
     tir.launch_thread(j1_0, 4)
     for j1_1 in tir.serial(0, 32):
-        tir.store(C.data, i * 128 + j1_0 * 32 + j1_1,
-                  tir.load("float32", A.data, i * 128 + j1_0 * 32 + j1_1) + 1.0, True)
+        tir.store(
+            C.data,
+            i * 128 + j1_0 * 32 + j1_1,
+            tir.load("float32", A.data, i * 128 + j1_0 * 32 + j1_1) + 1.0,
+            True,
+        )
 
 
 @tvm.script.tir
@@ -55,14 +63,20 @@ def unified_element_wise_thread_x(a: ty.handle, b: ty.handle, c: ty.handle) -> N
     tir.launch_thread(block_x, 128)
     with tir.launch_thread(thread_x, 4):
         for j0_1 in tir.serial(0, 32):
-            tir.store(B.data, block_x * 128 + thread_x * 32 + j0_1,
-                      tir.load("float32", A.data, block_x * 128 + thread_x * 32 + j0_1) * 2.0,
-                      True)
+            tir.store(
+                B.data,
+                block_x * 128 + thread_x * 32 + j0_1,
+                tir.load("float32", A.data, block_x * 128 + thread_x * 32 + j0_1) * 2.0,
+                True,
+            )
     tir.launch_thread(thread_x, 4)
     for j1_1 in tir.serial(0, 32):
-        tir.store(C.data, block_x * 128 + thread_x * 32 + j1_1,
-                  tir.load("float32", A.data, block_x * 128 + thread_x * 32 + j1_1) + 1.0,
-                  True)
+        tir.store(
+            C.data,
+            block_x * 128 + thread_x * 32 + j1_1,
+            tir.load("float32", A.data, block_x * 128 + thread_x * 32 + j1_1) + 1.0,
+            True,
+        )
 
 
 @tvm.script.tir
@@ -76,8 +90,12 @@ def element_wise_vthread(a: ty.handle, b: ty.handle) -> None:
     tir.launch_thread(i_1, 64)
     tir.launch_thread(j_0, 2)
     for j_1 in tir.serial(0, 64):
-        tir.store(B.data, i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1,
-                  tir.load("float32", A.data, i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1) * 2.0, True)
+        tir.store(
+            B.data,
+            i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1,
+            tir.load("float32", A.data, i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1) * 2.0,
+            True,
+        )
 
 
 @tvm.script.tir
@@ -91,9 +109,13 @@ def unified_element_wise_vthread(a: ty.handle, b: ty.handle) -> None:
     tir.launch_thread(thread_x, 64)
     tir.launch_thread(vthread_1, 2)
     for j_1 in tir.serial(0, 64):
-        tir.store(B.data, vthread * 8192 + thread_x * 128 + vthread_1 * 64 + j_1,
-                  tir.load("float32", A.data,
-                           vthread * 8192 + thread_x * 128 + vthread_1 * 64 + j_1) * 2.0, True)
+        tir.store(
+            B.data,
+            vthread * 8192 + thread_x * 128 + vthread_1 * 64 + j_1,
+            tir.load("float32", A.data, vthread * 8192 + thread_x * 128 + vthread_1 * 64 + j_1)
+            * 2.0,
+            True,
+        )
 
 
 @tvm.script.tir
@@ -107,8 +129,12 @@ def element_wise_vthread_x(a: ty.handle, b: ty.handle) -> None:
     tir.launch_thread(i_1, 64)
     tir.launch_thread(j_0, 2)
     for j_1 in tir.serial(0, 64):
-        tir.store(B.data, i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1,
-                  tir.load("float32", A.data, i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1) * 2.0, True)
+        tir.store(
+            B.data,
+            i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1,
+            tir.load("float32", A.data, i_0 * 8192 + i_1 * 128 + j_0 * 64 + j_1) * 2.0,
+            True,
+        )
 
 
 @tvm.script.tir
@@ -121,9 +147,12 @@ def unified_element_wise_vthread_x(a: ty.handle, b: ty.handle) -> None:
     tir.launch_thread(thread_x, 64)
     tir.launch_thread(vthread_x, 2)
     for j_1 in tir.serial(0, 64):
-        tir.store(B.data, vthread_x * 8256 + thread_x * 128 + j_1,
-                  tir.load("float32", A.data, vthread_x * 8256 + thread_x * 128 + j_1) * 2.0,
-                  True)
+        tir.store(
+            B.data,
+            vthread_x * 8256 + thread_x * 128 + j_1,
+            tir.load("float32", A.data, vthread_x * 8256 + thread_x * 128 + j_1) * 2.0,
+            True,
+        )
 
 
 def test_parallel_relationship_thread_x():
