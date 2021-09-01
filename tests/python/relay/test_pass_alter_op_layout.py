051f9ccba68ee9e2988a0236093cddf9ed120dcf
Andrey Malyshev <elvin.nnov@gmail.com>
2021-09-01 22:38:25 +0300

    Change test target to get NCHW8c

--- tests/python/relay/test_pass_alter_op_layout.py
+++ tests/python/relay/test_pass_alter_op_layout.py
@@ -841,7 +841,7 @@ def test_alter_layout_depthwise_conv2d():
     from tvm import topi
 
     def alter_conv2d(attrs, inputs, tinfos, out_type):
-        with tvm.target.Target("llvm"):
+        with tvm.target.Target("llvm -mcpu=core-avx2"):
             return topi.nn.conv2d_alter_layout(attrs, inputs, tinfos, out_type)
 
     def expected():
@@ -1325,7 +1325,7 @@ def test_alter_op_dense():
         y = relay.Function(analysis.free_vars(y), y)
         return y
 
-    target = "llvm"
+    target = "llvm -mcpu=core-avx2"
     with tvm.target.Target(target):
         with TempOpAttr(
             "nn.dense", "FTVMAlterOpLayout", topi.x86.dense_alter_op._alter_dense_layout
@@ -1393,7 +1393,7 @@ def test_alter_op_dense_packed_data():
         )
         return relay.Function(analysis.free_vars(dense), dense)
 
-    with tvm.target.Target("llvm"):
+    with tvm.target.Target("llvm -mcpu=core-avx2"):
         with TempOpAttr(
             "nn.dense", "FTVMAlterOpLayout", topi.x86.dense_alter_op._alter_dense_layout
         ):
