c30ecbdc9f617e0deb1e78178810024dfa211226
Mike Vermeulen <mike@vermeulen.com>
2021-09-01 04:11:42 -0500

    Add ROCm to list of TVMC devices.

--- python/tvm/driver/tvmc/runner.py
+++ python/tvm/driver/tvmc/runner.py
@@ -50,7 +50,7 @@ def add_run_parser(subparsers):
     #      like 'webgpu', etc (@leandron)
     parser.add_argument(
         "--device",
-        choices=["cpu", "cuda", "cl", "metal", "vulkan"],
+        choices=["cpu", "cuda", "cl", "metal", "vulkan", "rocm"],
         default="cpu",
         help="target device to run the compiled module. Defaults to 'cpu'",
     )
