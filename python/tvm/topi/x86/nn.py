fa5a495559309ed10a4d8a92775b89317d2ea623
masa <masa@pop-os.localdomain>
2021-09-02 18:28:22 +0900

    try make diff smaller

--- python/tvm/topi/x86/nn.py
+++ python/tvm/topi/x86/nn.py
@@ -20,6 +20,58 @@ from tvm import te
 from ..utils import traverse_inline
 
 
+def _schedule_softmax(softmax_op, s, outs):
+    op_tag = softmax_op.tag
+    if op_tag == "softmax_output":
+        exp = softmax_op.input_tensors[0]
+        expsum = softmax_op.input_tensors[1]
+        max_elem = s[exp].op.input_tensors[1]
+        delta = None
+        axis = int(softmax_op.attrs["axis"])
+    elif op_tag == "fast_softmax_output":
+        exp = softmax_op.input_tensors[0]
+        expsum = softmax_op.input_tensors[1]
+        delta = s[exp].op.input_tensors[0]
+        max_elem = s[delta].op.input_tensors[1]
+        axis = int(softmax_op.attrs["axis"])
+    elif op_tag == "log_softmax_output":
+        exp = None
+        delta = None
+        max_elem = softmax_op.input_tensors[1]
+        expsum = softmax_op.input_tensors[2]
+        axis = 1
+    else:
+        raise ValueError(
+            "Tag is expected to be softmax_output or log_softmax_output. \
+                         Got {0}".format(
+                op_tag
+            )
+        )
+
+    # only parallelize outer dimensions up to axis
+    outer_axes = [s[softmax_op].op.axis[i] for i in range(0, axis)]
+    fused_outer_axes = s[softmax_op].fuse(*outer_axes)
+    s[softmax_op].parallel(fused_outer_axes)
+
+    # move computations with the same outer dimensions under the same root
+    s[max_elem].compute_at(s[softmax_op], fused_outer_axes)
+    s[expsum].compute_at(s[softmax_op], fused_outer_axes)
+
+    if delta is not None:
+        s[exp].compute_inline()
+        s[delta].compute_inline()
+    if exp is not None:
+        s[exp].compute_at(s[softmax_op], fused_outer_axes)
+
+    if softmax_op != outs[0].op:
+        # fuse softmax output with following elemwise ops.
+        output = outs[0]
+        outer_axes = [s[output].op.axis[i] for i in range(0, axis)]
+        fused_outer_axes = s[output].fuse(*outer_axes)
+        s[output].parallel(fused_outer_axes)
+        s[softmax_op].compute_at(s[output], fused_outer_axes)
+
+
 def schedule_softmax(outs):
     """Schedule for softmax
 
@@ -37,60 +89,9 @@ def schedule_softmax(outs):
     outs = [outs] if isinstance(outs, te.tensor.Tensor) else outs
     s = te.create_schedule([x.op for x in outs])
 
-    def _schedule(softmax_op):
-        op_tag = softmax_op.tag
-        if op_tag == "softmax_output":
-            exp = softmax_op.input_tensors[0]
-            expsum = softmax_op.input_tensors[1]
-            max_elem = s[exp].op.input_tensors[1]
-            delta = None
-            axis = int(softmax_op.attrs["axis"])
-        elif op_tag == "fast_softmax_output":
-            exp = softmax_op.input_tensors[0]
-            expsum = softmax_op.input_tensors[1]
-            delta = s[exp].op.input_tensors[0]
-            max_elem = s[delta].op.input_tensors[1]
-            axis = int(softmax_op.attrs["axis"])
-        elif op_tag == "log_softmax_output":
-            exp = None
-            delta = None
-            max_elem = softmax_op.input_tensors[1]
-            expsum = softmax_op.input_tensors[2]
-            axis = 1
-        else:
-            raise ValueError(
-                "Tag is expected to be softmax_output or log_softmax_output. \
-                             Got {0}".format(
-                    op_tag
-                )
-            )
-
-        # only parallelize outer dimensions up to axis
-        outer_axes = [s[softmax_op].op.axis[i] for i in range(0, axis)]
-        fused_outer_axes = s[softmax_op].fuse(*outer_axes)
-        s[softmax_op].parallel(fused_outer_axes)
-
-        # move computations with the same outer dimensions under the same root
-        s[max_elem].compute_at(s[softmax_op], fused_outer_axes)
-        s[expsum].compute_at(s[softmax_op], fused_outer_axes)
-
-        if delta is not None:
-            s[exp].compute_inline()
-            s[delta].compute_inline()
-        if exp is not None:
-            s[exp].compute_at(s[softmax_op], fused_outer_axes)
-
-        if softmax_op != outs[0].op:
-            # fuse softmax output with following elemwise ops.
-            output = outs[0]
-            outer_axes = [s[output].op.axis[i] for i in range(0, axis)]
-            fused_outer_axes = s[output].fuse(*outer_axes)
-            s[output].parallel(fused_outer_axes)
-            s[softmax_op].compute_at(s[output], fused_outer_axes)
-
     def _callback(op):
         if "softmax" in op.tag:
-            _schedule(op)
+            _schedule_softmax(op, s, outs)
 
     traverse_inline(s, outs[0].op, _callback)
     return s
