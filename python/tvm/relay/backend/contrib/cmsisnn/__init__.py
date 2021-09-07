79c6f9fcefd55a55de31962f84f2841491b148e5
Ashutosh Parkhi <86472128+ashutosh-arm@users.noreply.github.com>
2021-09-08 00:53:18 +0100

    [CMSIS-NN] code generator for softmax (#8833)

--- python/tvm/relay/backend/contrib/cmsisnn/__init__.py
+++ python/tvm/relay/backend/contrib/cmsisnn/__init__.py
@@ -14,6 +14,5 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Backend codegen modules for relay."""
-from . import compile_engine
-from .contrib import cmsisnn
+"""CMSIS-NN codegen modules for relay."""
+from . import codegen
