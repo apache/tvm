2640ab344366a8e16593272fb3459281df64d994
cuiqing.li <cuiqing.li@bytedance.com>
2021-09-08 10:45:07 +0800

    update trt int8 fikle

--- tests/python/contrib/test_tensorrt_int8_exp.py
+++ tests/python/contrib/test_tensorrt_int8_exp.py
@@ -14,10 +14,15 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import pytest
+import os
+import numpy as np
+import cv2
+from PIL import Image
+from scipy.spatial import distance
 
-# FP16 TRT command to run : TVM_TENSORRT_USE_FP16=1 python test_trt.py
-# INT8 TRT command to run : TVM_TENSORRT_USE_INT8=1 TENSORRT_NUM_CALI_INT8=10 python test_trt.py
 import tvm
+import tvm.relay.testing
 from tvm import relay
 from tvm.contrib.download import download_testdata
 from tvm.relay.op.contrib.tensorrt import partition_for_tensorrt
@@ -27,104 +32,94 @@ import torch
 import torchvision
 from torchvision import transforms
 
-# additonal imports
-import os
-import numpy as np
-import cv2
-from PIL import Image
-from scipy.spatial import distance
 
 def cosine_distance(a, b):
     res = distance.cosine(a, b)
     return res
 
-# you can change model name into resnet18, mobilenet_v2 ...  
-model_name = "resnet34"
-model = getattr(torchvision.models, model_name)(pretrained=True)
-model = model.eval()
+def test_trt_int8():
+    """
+    This Function is used to use tensorrt int8 to compile a resnet34 model,
+    and compare cosine distance between the output of the original model and trt int8 tvm ouput
 
-# We grab the TorchScripted model via tracing
-input_shape = [1, 3, 224, 224]
-input_data = torch.randn(input_shape)
-scripted_model = torch.jit.trace(model, input_data).eval()
-scripted_model.save("mobilenet_v2.pt")
+    """
+    os.environ["TVM_TENSORRT_USE_INT8"] = "1"
+    os.environ["TENSORRT_NUM_CALI_INT8"] = "10"
+    model_name = "resnet34"
+    model = getattr(torchvision.models, model_name)(pretrained=True)
+    model = model.eval()
 
-img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
-img_path = download_testdata(img_url, "cat.png", module="data")
-img = Image.open(img_path).resize((224, 224))
-my_preprocess = transforms.Compose(
-    [
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
-img = my_preprocess(img)
-img = np.expand_dims(img, 0)
+    # We grab the TorchScripted model via tracing
+    input_shape = [1, 3, 224, 224]
+    input_data = torch.randn(input_shape)
+    scripted_model = torch.jit.trace(model, input_data).eval()
 
-input_name = "input0"
-shape_list = [(input_name, img.shape)]
-mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
+    img_url = "https://github.com/dmlc/mxnet.js/blob/main/data/cat.png?raw=true"
+    img_path = download_testdata(img_url, "cat.png", module="data")
+    img = Image.open(img_path).resize((224, 224))
+    my_preprocess = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    img = my_preprocess(img)
+    img = np.expand_dims(img, 0)
 
-# compile the model
-target = "cuda"
-dev = tvm.cuda(1)
-mod, config = partition_for_tensorrt(mod, params)
+    input_name = "input0"
+    shape_list = [(input_name, img.shape)]
+    mod, params = relay.frontend.from_pytorch(scripted_model, shape_list)
 
-print("python script started building --------------")
-with tvm.transform.PassContext(opt_level=3, config={'relay.ext.tensorrt.options': config}):
-    lib = relay.build(mod, target=target, params=params)
-print("python script finsihed building -------------------")
-
-
-dtype = "float32"
-lib.export_library('compiled.so')
-loaded_lib = tvm.runtime.load_module('compiled.so')
-gen_module = tvm.contrib.graph_executor.GraphModule(loaded_lib['default'](dev))
-
-num_cali_int8 = 0
-try:
-    num_cali_int8 = os.environ["TENSORRT_NUM_CALI_INT8"]
-    print("we are going to set {} times calibration in this case".format(num_cali_int8))
-except:
-    print("no TENSORRT_NUM_CALI_INT8 found in this case ... ")
-
-num_cali_int8 = int(num_cali_int8)
-if num_cali_int8 != 0:
-    print("calibration steps ... ")
-    for i in range(num_cali_int8):
-        tvm_data = tvm.nd.array(img)
-        gen_module.set_input(input_name, tvm_data)
-        gen_module.run(data=tvm_data)
-    print("finished calibration step")
+    # compile the model
+    target = "cuda"
+    dev = tvm.cuda(1)
+    mod, config = partition_for_tensorrt(mod, params)
+    with tvm.transform.PassContext(opt_level=3, config={'relay.ext.tensorrt.options': config}):
+        lib = relay.build(mod, target=target, params=params)
 
 
-# get output of tvm model 
-print("test run ... ")
-tvm_data = tvm.nd.array(img)
-gen_module.set_input(input_name, tvm_data)
-gen_module.run(data=tvm_data)
-out = gen_module.get_output(0)
+    dtype = "float32"
+    lib.export_library('compiled.so')
+    loaded_lib = tvm.runtime.load_module('compiled.so')
+    gen_module = tvm.contrib.graph_executor.GraphModule(loaded_lib['default'](dev))
+
+    num_cali_int8 = int(os.environ["TENSORRT_NUM_CALI_INT8"])
+    if num_cali_int8 != 0:
+        print("start calibrating data ... ")
+        for i in range(num_cali_int8):
+            tvm_data = tvm.nd.array(img)
+            gen_module.set_input(input_name, tvm_data)
+            gen_module.run(data=tvm_data)
+        print("finished calibrating data ... ")
 
 
-# check output of tvm and output of pytorch model are equal
-data_np = img
-device = "cuda:1"
-torch_data = torch.from_numpy(data_np)
-torch_data = torch_data.to(device)
-model = scripted_model
-model = model.eval()
-model = model.to(device)
-torch_output = model(torch_data)
+    # get output of tvm model 
+    print("rebuild engine and test to run ... ")
+    tvm_data = tvm.nd.array(img)
+    gen_module.set_input(input_name, tvm_data)
+    gen_module.run(data=tvm_data)
+    out = gen_module.get_output(0)
 
-print("the cosine distance between torch output and trt int8 output of tvm : ")
-cosine_distance_res = cosine_distance(out.numpy(), torch_output.detach().cpu().numpy())
-print(cosine_distance_res)
 
-# Evaluate
-print("Evaluate inference time cost...")
-ftimer = gen_module.module.time_evaluator("run", dev, repeat=10, min_repeat_ms=500)
-prof_res = np.array(ftimer().results) * 1e3  # convert to millisecond
-message = "Mean inference time (std dev): %.2f ms (%.2f ms)" % (np.mean(prof_res), np.std(prof_res))
-print(message)
+    # check output of tvm and output of pytorch model are equal
+    torch_data = torch.from_numpy(img)
+    model = scripted_model.eval()
+    torch_output = model(torch_data)
+
+    print("the cosine distance between torch output and trt int8 output of tvm : ")
+    cosine_distance_res = cosine_distance(out.numpy(), torch_output.detach().cpu().numpy())
+    assert cosine_distance_res <= 0.02
+
+    # Evaluate
+    print("Evaluate inference time cost...")
+    ftimer = gen_module.module.time_evaluator("run", dev, repeat=10, min_repeat_ms=500)
+    prof_res = np.array(ftimer().results) * 1e3  # convert to millisecond
+    message = "Mean inference time (std dev): %.2f ms (%.2f ms)" % (np.mean(prof_res), np.std(prof_res))
+    print(message)
+
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
