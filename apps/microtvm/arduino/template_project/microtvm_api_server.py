f6a404447406f91ddf6195ac044dc1f0b0e867e7
Mehrdad Hessar <mhessar@octoml.ai>
2021-09-09 22:09:20 +0200

    [microTVM] Refactor `platform` used as board name in microTVM (#8940)

--- apps/microtvm/arduino/template_project/microtvm_api_server.py
+++ apps/microtvm/arduino/template_project/microtvm_api_server.py
@@ -57,6 +57,7 @@ BOARD_PROPERTIES = {
         "package": "arduino",
         "architecture": "sam",
         "board": "arduino_due_x_dbg",
+        "model": "sam3x8e",
     },
     # Due to the way the Feather S2 bootloader works, compilation
     # behaves fine but uploads cannot be done automatically
@@ -64,27 +65,32 @@ BOARD_PROPERTIES = {
         "package": "esp32",
         "architecture": "esp32",
         "board": "feathers2",
+        "model": "esp32",
     },
     "metrom4": {
         "package": "adafruit",
         "architecture": "samd",
         "board": "adafruit_metro_m4",
+        "model": "atsamd51",
     },
     # Spresense only works as of its v2.3.0 sdk
     "spresense": {
         "package": "SPRESENSE",
         "architecture": "spresense",
         "board": "spresense",
+        "model": "cxd5602gg",
     },
     "nano33ble": {
         "package": "arduino",
         "architecture": "mbed_nano",
         "board": "nano33ble",
+        "model": "nrf52840",
     },
     "pybadge": {
         "package": "adafruit",
         "architecture": "samd",
         "board": "adafruit_pybadge_m4",
+        "model": "atsamd51",
     },
     # The Teensy boards are listed here for completeness, but they
     # won't work until https://github.com/arduino/arduino-cli/issues/700
@@ -93,16 +99,19 @@ BOARD_PROPERTIES = {
         "package": "teensy",
         "architecture": "avr",
         "board": "teensy40",
+        "model": "imxrt1060",
     },
     "teensy41": {
         "package": "teensy",
         "architecture": "avr",
         "board": "teensy41",
+        "model": "imxrt1060",
     },
     "wioterminal": {
         "package": "Seeeduino",
         "architecture": "samd",
         "board": "seeed_wio_terminal",
+        "model": "atsamd51",
     },
 }
 
@@ -113,6 +122,11 @@ PROJECT_OPTIONS = [
         "arduino_board",
         choices=list(BOARD_PROPERTIES),
         help="Name of the Arduino board to build for",
+    ),
+    server.ProjectOption(
+        "arduino_model",
+        choices=[board["model"] for _, board in BOARD_PROPERTIES.items()],
+        help="Name of the model for each Arduino board.",
     ),
     server.ProjectOption("arduino_cli_cmd", help="Path to the arduino-cli tool."),
     server.ProjectOption("port", help="Port to use for connecting to hardware"),
