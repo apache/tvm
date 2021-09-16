0b140bce9a94695e506ba5b21cc56ba6ef395210
KJlaccHoeUM9l <wotpricol@mail.ru>
2021-09-16 17:55:40 +0300

    init infrastructure of tests

---
+++ tests/python/contrib/test_rpc_server_device.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+from tvm import rpc
+from tvm.autotvm.measure import request_remote
+from tvm.auto_scheduler.utils import call_func_with_timeout
+from tvm.contrib.popen_pool import PopenWorker, StatusKind
+from tvm.rpc import tracker, proxy, server_ios_launcher
+
+
+def can_create_connection_without_deadlock(timeout, func, args=(), kwargs=None):
+    def wrapper(*_args, **_kwargs):
+        """
+            This wrapper is needed because the cloudpicle
+            cannot serialize objects that contain pointers (RPCSession)
+        """
+        func(*_args, **_kwargs)
+        return StatusKind.COMPLETE
+
+    worker = PopenWorker()
+    ret = call_func_with_timeout(worker, timeout=timeout, func=wrapper, args=args, kwargs=kwargs)
+    if isinstance(ret, Exception):
+        raise ret
+    return ret
+
+
+def test_rpc_proxy():
+    """
+    Host -- Proxy -- RPC server
+    """
+    proxy_server = proxy.Proxy(host=host_url, port=host_port)
+    device_server_launcher = server_ios_launcher.ServerIOSLauncher(mode=server_ios_launcher.RPCServerMode.proxy.value,
+                                                                   host=proxy_server.host,
+                                                                   port=proxy_server.port,
+                                                                   key=key)
+    try:
+        results = []
+        for _ in range(2):
+            ret = can_create_connection_without_deadlock(timeout=10, func=rpc.connect,
+                                                         args=(proxy_server.host, proxy_server.port, key))
+            results.append(ret)
+        if not np.all(np.array(results) == StatusKind.COMPLETE):
+            raise ValueError("One or more sessions ended incorrectly.")
+    except Exception as e:
+        print(e)
+
+    device_server_launcher.terminate()
+    proxy_server.terminate()
+
+
+def test_rpc_tracker():
+    """
+         tracker
+         /     \
+    Host   --   RPC server
+    """
+    tracker_server = tracker.Tracker(host=host_url, port=host_port, silent=True)
+    device_server_launcher = server_ios_launcher.ServerIOSLauncher(mode=server_ios_launcher.RPCServerMode.tracker.value,
+                                                                   host=tracker_server.host,
+                                                                   port=tracker_server.port,
+                                                                   key=key)
+    try:
+        results = []
+        for _ in range(2):
+            ret = can_create_connection_without_deadlock(timeout=10, func=request_remote,
+                                                         args=(key, tracker_server.host, tracker_server.port))
+            results.append(ret)
+        if not np.all(np.array(results) == StatusKind.COMPLETE):
+            raise ValueError("One or more sessions ended incorrectly.")
+    except Exception as e:
+        print(e)
+
+    device_server_launcher.terminate()
+    tracker_server.terminate()
+
+
+if __name__ == '__main__':
+    host_url = "0.0.0.0"
+    host_port = 9190
+    key = "ios_mobile_device"
+
+    test_rpc_proxy()
+    test_rpc_tracker()
