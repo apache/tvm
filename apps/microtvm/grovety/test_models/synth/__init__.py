e333d0e19b0b9f5dbdb3c676832f8dbfbf832145
Sergey Smirnov <Sergey.Smirnov@mir.dev>
2021-09-20 09:07:56 +0300

    Added networks for avg/max pool 1d

--- apps/microtvm/grovety/test_models/synth/__init__.py
+++ apps/microtvm/grovety/test_models/synth/__init__.py
@@ -27,18 +27,116 @@ def read_relay_mod_from_txt(file_name):
     return relay_mod, _generate_params(relay_mod)
 
 
-def _maxpool_1d_relay_func():
+def _maxpool_1d_relay_func(shape):
     from tvm import relay
 
-    input = relay.var("input", relay.TensorType((1, 28, 28), 'int8'))
-    requantize = relay.qnn.op.requantize(input,
-        relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
+    input = relay.var("input", relay.TensorType(shape, 'int8'))
+    requantize = relay.qnn.op.requantize(input, relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
     max_pool1d = relay.nn.max_pool1d(requantize, pool_size=3, layout="NWC")
-    requantize = relay.qnn.op.requantize(max_pool1d,
-        relay.const(0.00392157), relay.const(-128), relay.const(0.00392157), relay.const(0),
-        out_dtype='uint8')
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
+    requantize = relay.qnn.op.requantize(max_pool1d, relay.const(0.00392157), relay.const(-128), relay.const(0.00392157), relay.const(0), out_dtype='uint8')
+    return relay.Function([input], requantize)
+
+
+def _avgpool_1d_layer(shape):
+    from tvm import relay
+
+    input = relay.var("input", relay.TensorType(shape, 'int8'))
+    requantize = relay.qnn.op.requantize(input, relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
+    cast = relay.cast(requantize, 'int16')
+    avg_pool1d = relay.nn.avg_pool1d(cast, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    avg_pool1d = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
+    cast2 = relay.cast(avg_pool1d, 'int32')
+    requantize = relay.qnn.op.requantize(cast2, relay.const(0.133801), relay.const(37), relay.const(0.133801), relay.const(165), out_dtype='uint8')
 
     return relay.Function([input], requantize)
+
+
+def maxpool_1d_relay_mod():
+    import tvm
+
+    shape = (1, 64, 64)
+    relay_mod = tvm.IRModule.from_expr(_maxpool_1d_relay_func(shape))
+    input = ('input', shape, 'int8')
+    output = ('output', (1, 4, 64), 'uint8')
+    return (relay_mod, _generate_params(relay_mod), input, output)
+
+
+def avgpool_1d_relay_mod():
+    import tvm
+
+    shape = (1, 64, 64)
+    relay_mod = tvm.IRModule.from_expr(_avgpool_1d_layer(shape))
+    input = ('input', shape, 'int8')
+    output = ('output', (1, 64, 4), 'uint8')
+    return (relay_mod, _generate_params(relay_mod), input, output)
+
+
+def get_data():
+    import numpy as np
+
+    dataset = []
+    for i in range(10):
+        data = np.random.randint(low=-128, high=127, size=(1, 64, 64), dtype='int8')
+        dataset.append((str(i), data))
+
+    return dataset
 
 
 def read_relay_mod_from_txt(file_name):
@@ -51,50 +149,3 @@ def read_relay_mod_from_txt(file_name):
 
     return relay_mod, _generate_params(relay_mod)
 
-
-def _avgpool_1d_layer(shape):
-    from tvm import relay
-
-    input = relay.var("input", relay.TensorType(shape, 'int8'))
-    requantize = relay.qnn.op.requantize(input, relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
-    cast = relay.cast(requantize, 'int16')
-    avg_pool1d = relay.nn.avg_pool1d(cast, pool_size=3, layout="NCW")
-    avg_pool1d_2 = relay.nn.avg_pool1d(avg_pool1d, pool_size=3, layout="NCW")
-    avg_pool1d_3 = relay.nn.avg_pool1d(avg_pool1d_2, pool_size=6, layout="NCW")
-    avg_pool1d_4 = relay.nn.avg_pool1d(avg_pool1d_3, pool_size=7, layout="NCW")
-    avg_pool1d_5 = relay.nn.avg_pool1d(avg_pool1d_4, pool_size=8, layout="NCW")
-    cast2 = relay.cast(avg_pool1d_5, 'int32')
-    requantize = relay.qnn.op.requantize(cast2, relay.const(0.133801), relay.const(37), relay.const(0.133801), relay.const(165), out_dtype='uint8')
-
-    return relay.Function([input], requantize)
-
-
-def maxpool_1d_relay_mod():
-    import tvm
-
-
-    relay_mod = tvm.IRModule({tvm.relay.GlobalVar('main'): _maxpool_1d_relay_func()})
-    input = ('input', (1, 28, 28), 'int8')
-    output = ('output', (1, 4), 'uint8')
-    return (relay_mod, _generate_params(relay_mod), input, output)
-
-
-def avgpool_1d_relay_mod():
-    import tvm
-
-    shape = (1, 128, 128)
-    relay_mod = tvm.IRModule.from_expr(_avgpool_1d_layer(shape))
-    input = ('input', shape, 'int8')
-    output = ('output', (1, 128, 106), 'uint8')
-    return (relay_mod, _generate_params(relay_mod), input, output)
-
-
-def get_data():
-    import numpy as np
-
-    dataset = []
-    for i in range(10):
-        data = np.random.randint(low=-128, high=127, size=(1, 128, 128), dtype='int8')
-        dataset.append((str(i), data))
-
-    return dataset
