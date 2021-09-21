41956b669aae39aa1797a0c802f5885d6f083c39
Junru Shao <junrushao1994@gmail.com>
2021-09-21 09:40:11 -0700

    [Support] Add parallel_for_dynamic with dynamic schedules

--- src/support/parallel_for.cc
+++ src/support/parallel_for.cc
@@ -67,8 +67,8 @@ void parallel_for(int begin, int end, const std::function<void(int)>& f, int ste
   res_vec.reserve(run_partitions.size());
   for (const auto& run_partition : run_partitions) {
     std::packaged_task<void(const std::vector<int>&, const std::function<void(int)>&)> task(
-        [](const std::vector<int>& run_pattition, const std::function<void(int)>& f) {
-          for (const auto& i : run_pattition) {
+        [](const std::vector<int>& run_partition, const std::function<void(int)>& f) {
+          for (const auto& i : run_partition) {
             f(i);
           }
         });
@@ -87,6 +87,53 @@ void parallel_for(int begin, int end, const std::function<void(int)>& f, int ste
   try {
     for (auto&& i : res_vec) {
       i.get();
+    }
+  } catch (const std::exception& e) {
+    LOG(FATAL) << "Parallel_for error with " << e.what();
+  }
+}
+
+void parallel_for_dynamic(int begin, int end, int num_threads,
+                          const std::function<void(int thread_id, int task_id)>& f) {
+  // Step 1. Sanity checks
+  if (begin == end) {
+    return;
+  }
+  CHECK_LT(begin, end) << "ValueError: The interval [begin, end) requires `begin <= end`";
+  CHECK_GT(num_threads, 0) << "ValueError: `num_threads` should be positive";
+  // Step 2. Launch threads
+  // Step 2.1. Launch worker 1 to worker `num_threads - 1`
+  std::atomic<int> counter{begin};
+  std::vector<std::future<void>> futures;
+  std::vector<std::thread> threads;
+  futures.reserve(num_threads - 1);
+  threads.reserve(num_threads - 1);
+  auto worker = [end, &counter, &f](int thread_id) -> void {
+    for (int task_id; (task_id = counter++) < end;) {
+      f(thread_id, task_id);
+    }
+  };
+  for (int thread_id = 1; thread_id < num_threads; ++thread_id) {
+    std::packaged_task<void(int)> task(worker);
+    futures.emplace_back(task.get_future());
+    threads.emplace_back(std::move(task), thread_id);
+  }
+  // Step 2.2. Launch worker 0 inplace
+  try {
+    worker(0);
+  } catch (const std::exception& e) {
+    for (auto&& thread : threads) {
+      thread.join();
+    }
+    LOG(FATAL) << "Parallel_for error with " << e.what();
+  }
+  // Step 3. Join threads and check exceptions
+  for (auto&& thread : threads) {
+    thread.join();
+  }
+  try {
+    for (auto&& future : futures) {
+      future.get();
     }
   } catch (const std::exception& e) {
     LOG(FATAL) << "Parallel_for error with " << e.what();
