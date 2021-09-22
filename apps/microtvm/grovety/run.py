4cfcad080132e1400ec7c70f62d84a9c0200315c
Sergey Smirnov <Sergey.Smirnov@mir.dev>
2021-09-22 15:59:47 +0300

    more synth models for tests

--- apps/microtvm/grovety/run.py
+++ apps/microtvm/grovety/run.py
@@ -60,8 +60,9 @@ if __name__ == "__main__":
         dataset = sine.get_data()
     elif args.model == "synth":
         import test_models.synth as synth
-        relay_mod, params, input, output = synth.avgpool_2d_relay_mod()
-        dataset = synth.get_data()
+        relay_mod, params, input, output = synth.conv_1d_relay_mod()
+        _, input_shape, input_dtype = input
+        dataset = synth.get_data(input_shape, input_dtype=input_dtype)
     else:
         raise NotImplementedError
 
--- apps/microtvm/grovety/test_models/synth/__init__.py
+++ apps/microtvm/grovety/test_models/synth/__init__.py
@@ -27,9 +27,11 @@ def read_relay_mod_from_txt(file_name):
     return relay_mod, _generate_params(relay_mod)
 
 
-def _maxpool_1d_relay_func(shape):
+def maxpool_1d_relay_mod():
+    import tvm
     from tvm import relay
 
+    shape = (1, 64, 64)
     input = relay.var("input", relay.TensorType(shape, 'int8'))
     requantize = relay.qnn.op.requantize(input, relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
     max_pool1d = relay.nn.max_pool1d(requantize, pool_size=3, layout="NWC")
@@ -63,12 +65,18 @@ def _maxpool_1d_relay_func(shape):
     max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
     max_pool1d = relay.nn.max_pool1d(max_pool1d, pool_size=3, layout="NWC")
     requantize = relay.qnn.op.requantize(max_pool1d, relay.const(0.00392157), relay.const(-128), relay.const(0.00392157), relay.const(0), out_dtype='uint8')
-    return relay.Function([input], requantize)
+
+    relay_mod = tvm.IRModule.from_expr(relay.Function([input], requantize))
+    input = ('input', shape, 'int8')
+    output = ('output', (1, 4, 64), 'uint8')
+    return (relay_mod, _generate_params(relay_mod), input, output)
 
 
-def _avgpool_1d_layer(shape):
+def avgpool_1d_relay_mod():
+    import tvm
     from tvm import relay
 
+    shape = (1, 64, 64)
     input = relay.var("input", relay.TensorType(shape, 'int8'))
     requantize = relay.qnn.op.requantize(input, relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
     cast = relay.cast(requantize, 'int16')
@@ -105,11 +113,17 @@ def _avgpool_1d_layer(shape):
     cast2 = relay.cast(avg_pool1d, 'int32')
     requantize = relay.qnn.op.requantize(cast2, relay.const(0.133801), relay.const(37), relay.const(0.133801), relay.const(165), out_dtype='uint8')
 
-    return relay.Function([input], requantize)
+    relay_mod = tvm.IRModule.from_expr(relay.Function([input], requantize))
+    input = ('input', shape, 'int8')
+    output = ('output', (1, 64, 4), 'uint8')
+    return (relay_mod, _generate_params(relay_mod), input, output)
 
-def _avgpool_2d_layer(shape):
+
+def avgpool_2d_relay_mod():
+    import tvm
     from tvm import relay
 
+    shape = (1, 1, 64, 64)
     input = relay.var("input", relay.TensorType(shape, 'int8'))
     requantize = relay.qnn.op.requantize(input, relay.const(0.00392157), relay.const(0), relay.const(0.00392157), relay.const(-128))
     cast = relay.cast(requantize, 'int16')
@@ -145,45 +159,60 @@ def _avgpool_2d_layer(shape):
     avg_pool2d = relay.nn.avg_pool2d(avg_pool2d, pool_size=(3, 3), layout="NCHW")
     cast2 = relay.cast(avg_pool2d, 'int32')
     requantize = relay.qnn.op.requantize(cast2, relay.const(0.00392157), relay.const(-128), relay.const(0.00392157), relay.const(0), out_dtype='uint8')
-    return relay.Function([input], requantize)
 
-
-def maxpool_1d_relay_mod():
-    import tvm
-
-    shape = (1, 64, 64)
-    relay_mod = tvm.IRModule.from_expr(_maxpool_1d_relay_func(shape))
-    input = ('input', shape, 'int8')
-    output = ('output', (1, 4, 64), 'uint8')
-    return (relay_mod, _generate_params(relay_mod), input, output)
-
-
-def avgpool_1d_relay_mod():
-    import tvm
-
-    shape = (1, 64, 64)
-    relay_mod = tvm.IRModule.from_expr(_avgpool_1d_layer(shape))
-    input = ('input', shape, 'int8')
-    output = ('output', (1, 64, 4), 'uint8')
-    return (relay_mod, _generate_params(relay_mod), input, output)
-
-
-def avgpool_2d_relay_mod():
-    import tvm
-
-    shape = (1, 1, 64, 64)
-    relay_mod = tvm.IRModule.from_expr(_avgpool_2d_layer(shape))
+    relay_mod = tvm.IRModule.from_expr(relay.Function([input], requantize))
     input = ('input', shape, 'int8')
     output = ('output', (1, 1, 4, 4), 'uint8')
     return (relay_mod, _generate_params(relay_mod), input, output)
 
 
-def get_data():
+def conv_1d_relay_mod():
+    import tvm
+    from tvm import relay
+    import numpy as np
+
+    in_channels = 1
+    input = relay.var("input", relay.TensorType((1, 28, 28, in_channels), "int8"))
+    v1 = relay.reshape(input, (1, -1, in_channels))
+    v2 = relay.const(np.ones((3, in_channels, 12), dtype="int8"))
+    conv1 = relay.op.nn.conv1d(v1, v2, kernel_size=3, data_layout="NWC", kernel_layout="WIO", out_dtype="int32", channels=12)
+    v3 = relay.cast(conv1, dtype="int16")
+    v4 = relay.const(np.ones((3, 12, 24), dtype="int16"))
+    conv2 = relay.op.nn.conv1d(v3, v4, kernel_size=3, data_layout="NWC", kernel_layout="WIO", out_dtype="int32", channels=24)
+    relay_mod = tvm.IRModule.from_expr(relay.Function([input], conv2))
+
+    input = ('input', (1, 28, 28, in_channels), 'int8')
+    output = ('output', (1, 780, 24), 'int16')
+    return (relay_mod, _generate_params(relay_mod), input, output)
+
+
+def conv_1d_relay_mod_2():
+    import tvm
+    from tvm import relay
+    import numpy as np
+
+    in_channels = 1
+    input = relay.var("input", relay.TensorType((1, 28, 28, in_channels), "int8"))
+    v1 = relay.reshape(input, (1, -1, in_channels))
+    v2 = relay.const(np.ones((3, 12, in_channels), dtype="int8"))
+    conv1 = relay.op.nn.conv1d(v1, v2, kernel_size=3, data_layout="NWC", kernel_layout="WOI", out_dtype="int32", channels=12)
+    v3 = relay.cast(conv1, dtype="int16")
+    v4 = relay.const(np.ones((3, 24, 12), dtype="int16"))
+    conv2 = relay.op.nn.conv1d(v3, v4, kernel_size=3, data_layout="NWC", kernel_layout="WOI", out_dtype="int32", channels=24)
+    v5 = relay.cast(conv2, dtype="int16")
+    relay_mod = tvm.IRModule.from_expr(relay.Function([input], v5))
+
+    input = ('input', (1, 28, 28, in_channels), 'int8')
+    output = ('output', (1, 780, 24), 'int16')
+    return (relay_mod, _generate_params(relay_mod), input, output)
+
+
+def get_data(input_shape, input_dtype='int8'):
     import numpy as np
 
     dataset = []
     for i in range(10):
-        data = np.random.randint(low=-128, high=127, size=(1, 64, 64), dtype='int8')
+        data = np.random.randint(low=-128, high=127, size=input_shape, dtype=input_dtype)
         dataset.append((str(i), data))
 
     return dataset
