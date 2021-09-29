ad24256df9bf93909a24927c99bc3fb1b1cb990c
sunway <sunwayforever@gmail.com>
2021-09-29 08:28:11 +0800

    [BYOC] support arbitrary input dims for add/mul/relu of dnnl c_src codegen (#9127)
    * support arbitrary input dims for add/mul/relu of dnnl c_src codegen
    
    * fix lint
    
    * fix
    
    Co-authored-by: sunway <wei.sun@hexintek.com>

--- src/runtime/contrib/dnnl/dnnl_kernel.h
+++ src/runtime/contrib/dnnl/dnnl_kernel.h
@@ -26,6 +26,9 @@
 #define TVM_RUNTIME_CONTRIB_DNNL_DNNL_KERNEL_H_
 
 #include <tvm/runtime/c_runtime_api.h>
+#include <tvm/runtime/logging.h>
+
+#include <vector>
 
 #include "dnnl.hpp"
 
@@ -54,14 +57,14 @@ extern "C" TVM_DLL void dnnl_fused_conv2d_bias_relu(float* data, float* weights,
 extern "C" TVM_DLL void dnnl_dense(float* data, float* weight, float* out, int p_B_, int p_I_,
                                    int p_O_);
 
-extern "C" TVM_DLL void dnnl_relu(float* data, float* out, int p_N_, int p_C_, int p_H_, int p_W_);
+extern "C" TVM_DLL void dnnl_relu(float* data, float* out, std::vector<int64_t> shape);
 
 extern "C" TVM_DLL void dnnl_bn(float* data, float* gamma, float* beta, float* mean,
                                 float* variance, float* out, float* new_mean, float* new_variance,
                                 int p_n_, int p_c_, int p_h_, int p_w_, int p_e_);
 
-extern "C" TVM_DLL void dnnl_add(float* data, float* weight, float* out, int p_n_, int p_c_,
-                                 int p_h_, int p_w_);
+extern "C" TVM_DLL void dnnl_binary_op(float* data, float* weight, float* out, int binary_algo,
+                                       std::vector<int64_t> shape);
 
 }  // namespace contrib
 }  // namespace runtime
