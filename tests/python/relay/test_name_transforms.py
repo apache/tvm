181226faec7f428da77c2f800e8fa200f7aecce0
Christopher Sidebottom <chris.sidebottom@arm.com>
2021-09-30 19:07:56 +0100

    Introduce centralised name transformation functions (#9088)
    * Introduce centralised name transformation functions
    
    To address some of the concerns raised in https://github.com/apache/tvm/pull/8280 and https://github.com/apache/tvm/pull/8720 I've put together a series of functions to combine together names to re-use between these areas. These are meant to be a starting point to fix up the name generation to use the TVM C conventions and port the interface API header to C++.
    
    These functions will also be used for constructing the names in the C Device API (https://github.com/apache/tvm-rfcs/pull/31).
    
    * Improve error handling and error messages
    
    * Sanitize sanitise to sanitize
    
    This patch aims to sanitize uses of sanitise to the form of sanitize to be consistent with the overall codebases use of American English.

---
+++ tests/python/relay/test_name_transforms.py
@@ -0,0 +1,102 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License" you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from tvm import TVMError
+from tvm.relay.backend.name_transforms import (
+    to_c_function_style,
+    to_c_variable_style,
+    prefix_name,
+    prefix_generated_name,
+    sanitize_name,
+)
+import pytest
+
+
+def test_to_c_function_style():
+    assert to_c_function_style("TVM_Woof") == "TVMWoof"
+    assert to_c_function_style("TVM_woof") == "TVMWoof"
+    assert to_c_function_style("TVM_woof_woof") == "TVMWoofWoof"
+    assert to_c_function_style("TVMGen_woof_woof") == "TVMGenWoofWoof"
+
+    # Incorrect prefix
+    with pytest.raises(TVMError, match="Function not TVM prefixed"):
+        to_c_function_style("Cake_Bakery")
+    with pytest.raises(TVMError, match="Function name is empty"):
+        to_c_function_style("")
+
+
+def test_to_c_variable_style():
+    assert to_c_variable_style("TVM_Woof") == "tvm_woof"
+    assert to_c_variable_style("TVM_woof") == "tvm_woof"
+    assert to_c_variable_style("TVM_woof_Woof") == "tvm_woof_woof"
+
+    # Incorrect prefix
+    with pytest.raises(TVMError, match="Variable not TVM prefixed"):
+        to_c_variable_style("Cake_Bakery")
+    with pytest.raises(TVMError, match="Variable name is empty"):
+        to_c_variable_style("")
+
+
+def test_prefix_name():
+    assert prefix_name("Woof") == "TVM_Woof"
+    assert prefix_name(["Woof"]) == "TVM_Woof"
+    assert prefix_name(["woof"]) == "TVM_woof"
+    assert prefix_name(["woof", "moo"]) == "TVM_woof_moo"
+
+    with pytest.raises(TVMError, match="Name is empty"):
+        prefix_name("")
+    with pytest.raises(TVMError, match="Name segments empty"):
+        prefix_name([])
+    with pytest.raises(TVMError, match="Name segment is empty"):
+        prefix_name([""])
+
+
+def test_prefix_generated_name():
+    assert prefix_generated_name("Woof") == "TVMGen_Woof"
+    assert prefix_generated_name(["Woof"]) == "TVMGen_Woof"
+    assert prefix_generated_name(["Woof"]) == "TVMGen_Woof"
+    assert prefix_generated_name(["woof"]) == "TVMGen_woof"
+    assert prefix_generated_name(["woof", "moo"]) == "TVMGen_woof_moo"
+
+    with pytest.raises(TVMError, match="Name is empty"):
+        prefix_generated_name("")
+    with pytest.raises(TVMError, match="Name segments empty"):
+        prefix_generated_name([])
+    with pytest.raises(TVMError, match="Name segment is empty"):
+        prefix_generated_name([""])
+
+
+def test_sanitize_name():
+    assert sanitize_name("+_+ ") == "_"
+    assert sanitize_name("input+") == "input_"
+    assert sanitize_name("input-") == "input_"
+    assert sanitize_name("input++") == "input_"
+    assert sanitize_name("woof:1") == "woof_1"
+
+    with pytest.raises(TVMError, match="Name is empty"):
+        sanitize_name("")
+
+
+def test_combined_logic():
+    assert (
+        to_c_function_style(prefix_name(["Device", "target", "Invoke"])) == "TVMDeviceTargetInvoke"
+    )
+    assert to_c_function_style(prefix_generated_name(["model", "Run"])) == "TVMGenModelRun"
+    assert to_c_variable_style(prefix_name(["Device", "target", "t"])) == "tvm_device_target_t"
+    assert (
+        to_c_variable_style(prefix_generated_name(["model", "Devices"])) == "tvmgen_model_devices"
+    )
