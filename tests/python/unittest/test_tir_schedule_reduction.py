c11fed91f40e85fdef3ff83ba77c949de34c04a9
spectrometerHBH <spectrometer@sjtu.edu.cn>
2021-10-02 02:18:58 +0000

    rebase

--- tests/python/unittest/test_tir_schedule_reduction.py
+++ tests/python/unittest/test_tir_schedule_reduction.py
@@ -27,125 +27,125 @@ from tvm.tir.schedule.testing import verify_trace_roundtrip
 # pylint: disable=no-member,invalid-name,unused-variable,unexpected-keyword-arg
 
 
-@tvm.script.tir
-def rowsum_blockized(a: ty.handle, b: ty.handle) -> None:
-    B = tir.match_buffer(b, [32, 4])
-    A = tir.match_buffer(a, [32, 4, 128])
-    for i0, i2_0 in tir.grid(32, 16):
-        with tir.block([32, tir.reduce_axis(0, 16)], "blockized_B") as [io, ko]:
-            tir.bind(io, i0)
-            tir.bind(ko, i2_0)
-            with tir.init():
-                for i1 in tir.serial(0, 4):
-                    with tir.block([4], "B_init") as [ii_init]:
-                        tir.bind(ii_init, i1)
+@T.prim_func
+def rowsum_blockized(a: T.handle, b: T.handle) -> None:
+    B = T.match_buffer(b, [32, 4])
+    A = T.match_buffer(a, [32, 4, 128])
+    for i0, i2_0 in T.grid(32, 16):
+        with T.block([32, T.reduce_axis(0, 16)], "blockized_B") as [io, ko]:
+            T.bind(io, i0)
+            T.bind(ko, i2_0)
+            with T.init():
+                for i1 in T.serial(0, 4):
+                    with T.block([4], "B_init") as [ii_init]:
+                        T.bind(ii_init, i1)
                         B[io, ii_init] = 0.0
-            for i1_1, i2_1 in tir.grid(4, 8):
-                with tir.block([4, tir.reduce_axis(0, 128)], "B") as [ii, k]:
-                    tir.bind(ii, i1_1)
-                    tir.bind(k, ko * 8 + i2_1)
+            for i1_1, i2_1 in T.grid(4, 8):
+                with T.block([4, T.reduce_axis(0, 128)], "B") as [ii, k]:
+                    T.bind(ii, i1_1)
+                    T.bind(k, ko * 8 + i2_1)
                     B[io, ii] = B[io, ii] + A[io, ii, k]
 
 
-@tvm.script.tir
-def matmul(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
-    A = tir.match_buffer(a, [128, 128])
-    B = tir.match_buffer(b, [128, 128])
-    C = tir.match_buffer(c, [128, 128])
+@T.prim_func
+def matmul(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, [128, 128])
+    B = T.match_buffer(b, [128, 128])
+    C = T.match_buffer(c, [128, 128])
 
-    with tir.block([128, 128, tir.reduce_axis(0, 128)], "update") as [vi, vj, vk]:
-        with tir.init():
+    with T.block([128, 128, T.reduce_axis(0, 128)], "update") as [vi, vj, vk]:
+        with T.init():
             C[vi, vj] = 0.0
         C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vj, vk]
 
 
-@tvm.script.tir
-def matmul_decompose0(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
-    A = tir.match_buffer(a, [128, 128])
-    B = tir.match_buffer(b, [128, 128])
-    C = tir.match_buffer(c, [128, 128])
+@T.prim_func
+def matmul_decompose0(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, [128, 128])
+    B = T.match_buffer(b, [128, 128])
+    C = T.match_buffer(c, [128, 128])
 
-    with tir.block([128, 128], "init") as [vi, vj]:
+    with T.block([128, 128], "init") as [vi, vj]:
         C[vi, vj] = 0.0
 
-    with tir.block([128, 128, tir.reduce_axis(0, 128)], "update") as [vi, vj, vk]:
+    with T.block([128, 128, T.reduce_axis(0, 128)], "update") as [vi, vj, vk]:
         C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vj, vk]
 
 
-@tvm.script.tir
-def matmul_decompose1(a: ty.handle, b: ty.handle) -> None:
-    A = tir.match_buffer(a, [32, 4, 128], elem_offset=0, align=128, offset_factor=1)
-    B = tir.match_buffer(b, [32, 4], elem_offset=0, align=128, offset_factor=1)
+@T.prim_func
+def matmul_decompose1(a: T.handle, b: T.handle) -> None:
+    A = T.match_buffer(a, [32, 4, 128], elem_offset=0, align=128, offset_factor=1)
+    B = T.match_buffer(b, [32, 4], elem_offset=0, align=128, offset_factor=1)
 
-    for i0 in tir.serial(0, 32):
-        with tir.block([32], "blockized_B_init") as [io]:
-            for i1 in tir.serial(0, 4):
-                with tir.block([4], "B_init") as [ii]:
-                    B[io, ii] = tir.float32(0)
-    for i0, i2_o in tir.grid(32, 16):
-        with tir.block([32, tir.reduce_axis(0, 16)], "blockized_B_update") as [io, ko]:
-            for i1, i2_i in tir.grid(4, 8):
-                with tir.block([4, tir.reduce_axis(0, 128)], "B") as [ii, k]:
-                    tir.bind(ii, i1)
-                    tir.bind(k, ((ko * 8) + i2_i))
+    for i0 in T.serial(0, 32):
+        with T.block([32], "blockized_B_init") as [io]:
+            for i1 in T.serial(0, 4):
+                with T.block([4], "B_init") as [ii]:
+                    B[io, ii] = T.float32(0)
+    for i0, i2_o in T.grid(32, 16):
+        with T.block([32, T.reduce_axis(0, 16)], "blockized_B_update") as [io, ko]:
+            for i1, i2_i in T.grid(4, 8):
+                with T.block([4, T.reduce_axis(0, 128)], "B") as [ii, k]:
+                    T.bind(ii, i1)
+                    T.bind(k, ((ko * 8) + i2_i))
                     B[io, ii] = B[io, ii] + A[io, ii, k]
 
 
-@tvm.script.tir
-def matmul_decompose2(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
-    C = tir.match_buffer(c, [128, 128], elem_offset=0, align=128, offset_factor=1)
-    B = tir.match_buffer(b, [128, 128], elem_offset=0, align=128, offset_factor=1)
-    A = tir.match_buffer(a, [128, 128], elem_offset=0, align=128, offset_factor=1)
+@T.prim_func
+def matmul_decompose2(a: T.handle, b: T.handle, c: T.handle) -> None:
+    C = T.match_buffer(c, [128, 128], elem_offset=0, align=128, offset_factor=1)
+    B = T.match_buffer(b, [128, 128], elem_offset=0, align=128, offset_factor=1)
+    A = T.match_buffer(a, [128, 128], elem_offset=0, align=128, offset_factor=1)
 
-    for i0, i1 in tir.grid(128, 128):
-        with tir.block([128, 128], "update_init") as [vi_init, vj_init]:
-            C[vi_init, vj_init] = tir.float32(0)
-        for i2 in tir.serial(0, 128):
-            with tir.block([128, 128, tir.reduce_axis(0, 128)], "update_update") as [vi, vj, vk]:
+    for i0, i1 in T.grid(128, 128):
+        with T.block([128, 128], "update_init") as [vi_init, vj_init]:
+            C[vi_init, vj_init] = T.float32(0)
+        for i2 in T.serial(0, 128):
+            with T.block([128, 128, T.reduce_axis(0, 128)], "update_update") as [vi, vj, vk]:
                 C[vi, vj] = C[vi, vj] + (A[vi, vk] * B[vj, vk])
 
 
-@tvm.script.tir
-def matmul_decompose_fail3(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
-    A = tir.match_buffer(a, [128, 128])
-    B = tir.match_buffer(b, [128, 128])
-    C = tir.match_buffer(c, [128, 128])
+@T.prim_func
+def matmul_decompose_fail3(a: T.handle, b: T.handle, c: T.handle) -> None:
+    A = T.match_buffer(a, [128, 128])
+    B = T.match_buffer(b, [128, 128])
+    C = T.match_buffer(c, [128, 128])
 
-    for i, k, j in tir.grid(128, 128, 128):
-        with tir.block([128, 128, tir.reduce_axis(0, 128)], "update") as [vi, vj, vk]:
-            with tir.init():
+    for i, k, j in T.grid(128, 128, 128):
+        with T.block([128, 128, T.reduce_axis(0, 128)], "update") as [vi, vj, vk]:
+            with T.init():
                 C[vi, vj] = 0.0
-            tir.bind(vi, i)
-            tir.bind(vj, j)
-            tir.bind(vk, k)
+            T.bind(vi, i)
+            T.bind(vj, j)
+            T.bind(vk, k)
             C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vj, vk]
 
 
-@tvm.script.tir
-def matmul_decompose4(a: ty.handle, b: ty.handle, c: ty.handle) -> None:
-    C = tir.match_buffer(c, [128, 128], elem_offset=0, align=128, offset_factor=1)
-    B = tir.match_buffer(b, [128, 128], elem_offset=0, align=128, offset_factor=1)
-    A = tir.match_buffer(a, [128, 128], elem_offset=0, align=128, offset_factor=1)
+@T.prim_func
+def matmul_decompose4(a: T.handle, b: T.handle, c: T.handle) -> None:
+    C = T.match_buffer(c, [128, 128], elem_offset=0, align=128, offset_factor=1)
+    B = T.match_buffer(b, [128, 128], elem_offset=0, align=128, offset_factor=1)
+    A = T.match_buffer(a, [128, 128], elem_offset=0, align=128, offset_factor=1)
     # body
-    with tir.block([], "root"):
-        tir.reads([])
-        tir.writes([])
-        for i0_0 in tir.serial(0, 16):
-            for i0_1_init, i1_init in tir.grid(8, 128):
-                with tir.block([128, 128], "update_init") as [vi_init, vj_init]:
-                    tir.bind(vi_init, ((i0_0 * 8) + i0_1_init))
-                    tir.bind(vj_init, i1_init)
-                    C[vi_init, vj_init] = tir.float32(0)
-            for i0_1, i1, i2_0, i2_1 in tir.grid(8, 128, 19, 7):
-                with tir.block([128, 128, tir.reduce_axis(0, 128)], "update_update") as [
+    with T.block([], "root"):
+        T.reads([])
+        T.writes([])
+        for i0_0 in T.serial(0, 16):
+            for i0_1_init, i1_init in T.grid(8, 128):
+                with T.block([128, 128], "update_init") as [vi_init, vj_init]:
+                    T.bind(vi_init, ((i0_0 * 8) + i0_1_init))
+                    T.bind(vj_init, i1_init)
+                    C[vi_init, vj_init] = T.float32(0)
+            for i0_1, i1, i2_0, i2_1 in T.grid(8, 128, 19, 7):
+                with T.block([128, 128, T.reduce_axis(0, 128)], "update_update") as [
                     vi,
                     vj,
                     vk,
                 ]:
-                    tir.where((((i2_0 * 7) + i2_1) < 128))
-                    tir.bind(vi, ((i0_0 * 8) + i0_1))
-                    tir.bind(vj, i1)
-                    tir.bind(vk, ((i2_0 * 7) + i2_1))
+                    T.where((((i2_0 * 7) + i2_1) < 128))
+                    T.bind(vi, ((i0_0 * 8) + i0_1))
+                    T.bind(vj, i1)
+                    T.bind(vk, ((i2_0 * 7) + i2_1))
                     C[vi, vj] = C[vi, vj] + (A[vi, vk] * B[vj, vk])
 
 
