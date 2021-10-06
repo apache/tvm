64934c8fc6e15bef038f14f932beca1afd9c21ff
Grant Watson <grant.watson@arm.com>
2021-10-06 15:27:46 +0100

    Arm(R) Cortex(R)-M55 CPU and Arm(R) Ethos(TM)-U55 NPU Demo App
    Refactored convert_image.py
    
    Change-Id: I2e6e9dc6722f683182e2bd101c19b36f214c0151

--- apps/microtvm/ethosu/convert_image.py
+++ apps/microtvm/ethosu/convert_image.py
@@ -23,38 +23,23 @@ from PIL import Image
 import numpy as np
 
 
-def create_header_file(name, section, npy_data, output_path):
+def create_header_file(name, section, tensor_name, tensor_data, output_path):
     """
     This function generates a header file containing the data from the numpy array provided.
     """
     file_path = pathlib.Path(f"{output_path}/" + name).resolve()
-
     # Create header file with npy_data as a C array
     raw_path = file_path.with_suffix(".h").resolve()
     with open(raw_path, "w") as header_file:
-        header_file.write("#include <tvmgen_default.h>\n")
-        for tensor_name in npy_data.keys():
-            sanitized_tensor_name = re.sub(r"\W+", "_", tensor_name)
-            header_file.write(
-                f"const size_t {sanitized_tensor_name}_len = {npy_data[tensor_name].size};\n"
-            )
-
-            # Convert numpy data type to C data type
-            if npy_data[tensor_name].dtype == np.uint8:
-                c_type = "uint8_t"
-            elif npy_data[tensor_name].dtype == np.int8:
-                c_type = "int8_t"
-            else:
-                raise RuntimeError(f"Data type {str(npy_data[tensor_name].dtype)} not supported")
-
-            header_file.write(
-                f'{c_type} {sanitized_tensor_name}[] __attribute__((section("{section}"), aligned(16))) = "'
-            )
-
-            data_hexstr = npy_data[tensor_name].tobytes().hex()
-            for i in range(0, len(data_hexstr), 2):
-                header_file.write(f"\\x{data_hexstr[i:i+2]}")
-            header_file.write('";\n\n')
+        header_file.write(
+            "#include <tvmgen_default.h>\n"
+            + f"const size_t {tensor_name}_len = {tensor_data.size};\n"
+            + f'uint8_t {tensor_name}[] __attribute__((section("{section}"), aligned(16))) = "'
+        )
+        data_hexstr = tensor_data.tobytes().hex()
+        for i in range(0, len(data_hexstr), 2):
+            header_file.write(f"\\x{data_hexstr[i:i+2]}")
+        header_file.write('";\n\n')
 
 
 def create_headers(image_name):
@@ -71,14 +56,14 @@ def create_headers(image_name):
     img_data = np.transpose(img_data, (2, 0, 1))
 
     # Create input header file
-    input_data = {"input": img_data.astype(np.uint8)}
-    create_header_file("inputs", "ethosu_scratch", input_data, "./include")
-
+    input_data = img_data.astype(np.uint8)
+    create_header_file("inputs", "ethosu_scratch", "input", input_data, "./include")
     # Create output header file
-    output_data = {"output": np.zeros([1001], np.uint8)}
+    output_data = np.zeros([1001], np.uint8)
     create_header_file(
         "outputs",
         "output_data_sec",
+        "output",
         output_data,
         "./include",
     )
