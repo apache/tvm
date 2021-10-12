a273bc787097612dc8aeb160915fd7f79c2dad81
Christopher Sidebottom <chris.sidebottom@arm.com>
2021-10-12 16:26:59 +0100

    [TVMC] Split common tvmc test file into more specific files (#9206)
    The `test_tvmc_common.py` file was becoming a bit of a mixed bag of
    tests and as we now want to extend the `Target` processing logic it made
    sense to split each out into its own file to make it clearer what each
    does.
    
    `test_common.py` has also been renamed before we start using it for all the
    tests instead.

--- tests/python/driver/tvmc/test_pass_list.py
+++ tests/python/driver/tvmc/test_pass_list.py
@@ -14,12 +14,13 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 import argparse
 import pytest
 from tvm.driver import tvmc
 
 
-def test_common_parse_pass_list_str():
+def test_parse_pass_list_str():
     assert [""] == tvmc.common.parse_pass_list_str("")
     assert ["FoldScaleAxis", "FuseOps"] == tvmc.common.parse_pass_list_str("FoldScaleAxis,FuseOps")
 
