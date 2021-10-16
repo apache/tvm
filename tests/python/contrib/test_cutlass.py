dc01e3357f7ff04cc1c917d513d9e0c333cd81da
Masahiro Masuda <masahi129@gmail.com>
2021-10-16 17:57:08 +0900

    turned out the result match's TVM non-cutlass result. Numpy fp16 matmul is busted?

--- tests/python/contrib/test_cutlass.py
+++ tests/python/contrib/test_cutlass.py
@@ -14,20 +14,13 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import traceback
 import tvm
 from tvm import relay
 import numpy as np
-import time
 from tvm.contrib.cutlass import profile_and_build
 
 
-M = 1820
-N = 768
-K = 768
-
-
-def get_mod():
+def get_mod(M, N, K):
     data = relay.var("data", shape=(M, K), dtype="float16")
     weight = relay.var("weight", shape=(N, K), dtype="float16")
     bias = relay.var("bias", shape=(N,), dtype="float16")
@@ -39,48 +32,50 @@ def get_mod():
     return tvm.IRModule.from_expr(out)
 
 
-mod = get_mod()
+def get_ref_rt_mod(mod, params):
+    with tvm.transform.PassContext(opt_level=3):
+        lib = relay.build(mod, target="cuda", params=params)
+    ctx = tvm.gpu()
+    rt_mod = tvm.contrib.graph_executor.GraphModule(lib["default"](ctx))
+    return rt_mod, ctx
+
+
+def get_output(rt_mod, x):
+    rt_mod.set_input("data", x)
+    rt_mod.run()
+    return rt_mod.get_output(0).asnumpy()
+
+
+def get_output_numpy(np_data, np_weight):
+    np_out = np.dot(np_data, np_weight.T)
+    # np_out = np_out + np_bias
+    # np_out = np.dot(np_out, np_weight.T)
+    # np_out = np_out * (np_out > 0)
+    # np_out = np_out*(0.5+erf(np_out * np.sqrt(0.5)) * 0.5)
+    return np_out
+
+
+M = 1820
+N = 768
+K = 768
+
+mod = get_mod(M, N, K)
 
 np_data = np.random.uniform(-1, 1, (M, K)).astype("float16")
 np_weight = np.random.uniform(-1, 1, (N, K)).astype("float16")
 np_bias = np.random.uniform(-1, 1, (N,)).astype("float16")
 
-tvm_data = np_data
 tvm_weight = np_weight
 tvm_bias = np_bias
 
 params = {"weight": tvm_weight, "bias": tvm_bias}
 
-print("compiling...")
-sm = "86"
-rt_mod, ctx = profile_and_build(mod, params, sm)
-x = tvm.nd.array(tvm_data, device=ctx)
-rt_mod.set_input("data", x)
+rt_mod, ctx = profile_and_build(mod, params, "80")
+rt_mod_ref, ctx = get_ref_rt_mod(get_mod(M, N, K), params)
+x = tvm.nd.array(np_data, device=ctx)
 
-print("Running for the first time...")
-rt_mod.run()
-y = rt_mod.get_output(0)
+out = get_output(rt_mod, x)
+ref_out = get_output(rt_mod_ref, x)
+np_out = get_output_numpy(np_data, np_weight)
 
-print("np computing...")
-np_out = np.dot(np_data, np_weight.T)
-# np_out = np_out + np_bias
-# np_out = np.dot(np_out, np_weight.T)
-# np_out = np_out * (np_out > 0)
-# np_out = np_out*(0.5+erf(np_out * np.sqrt(0.5)) * 0.5)
-
-try:
-    np.testing.assert_allclose(y.asnumpy(), np_out, atol=1e-2, rtol=1e-2)
-    print("Accuracy test passed...")
-except:
-    traceback.print_exc()
-    print("Accuracy test failed...")
-
-
-times = []
-for i in range(100):
-    start = time.time()
-    rt_mod.run()
-    ctx.sync()  # wait for the device to finish
-    times.append(time.time() - start)
-print("Latency:", 1000.0 * np.mean(times), "ms")
-print("TFLOPS:", 2 * M * N * K / np.mean(times) / 1e12)
+np.testing.assert_allclose(out, ref_out, atol=1e-5, rtol=1e-5)
