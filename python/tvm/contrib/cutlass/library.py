50fc190307ed3e30db93e3d37d93633f452061a0
Masahiro Masuda <masahi129@gmail.com>
2021-10-18 12:37:39 +0900

    remove unused stuff from library.py

--- python/tvm/contrib/cutlass/library.py
+++ python/tvm/contrib/cutlass/library.py
@@ -14,62 +14,26 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-#
-# \file library.py
-#
-# \brief Generates the CUTLASS Library's instances
-#
-
 import re
-
-###################################################################################################
-
 import enum
-
-# The following block implements enum.auto() for Python 3.5 variants that don't include it such
-# as the default 3.5.2 on Ubuntu 16.04.
-#
-# https://codereview.stackexchange.com/questions/177309/reimplementing-pythons-enum-auto-for-compatibility
-
-try:
-    from enum import auto as enum_auto
-except ImportError:
-    __cutlass_library_auto_enum = 0
-
-    def enum_auto() -> int:
-        global __cutlass_library_auto_enum
-        i = __cutlass_library_auto_enum
-        __cutlass_library_auto_enum += 1
-        return i
+from enum import auto as enum_auto
 
 
-###################################################################################################
-
-#
 class GeneratorTarget(enum.Enum):
     Library = enum_auto()
 
 
-#
-GeneratorTargetNames = {GeneratorTarget.Library: "library"}
-#
-
-###################################################################################################
-
-#
 class DataType(enum.Enum):
     f16 = enum_auto()
     f32 = enum_auto()
-    invalid = enum_auto()
 
 
-#
 ShortDataTypeNames = {
     DataType.f16: "h",
     DataType.f32: "s",
 }
 
-#
+
 DataTypeNames = {
     DataType.f16: "f16",
     DataType.f32: "f32",
@@ -86,9 +50,6 @@ DataTypeSize = {
 }
 
 
-###################################################################################################
-
-#
 class MathOperation(enum.Enum):
     multiply_add = enum_auto()
 
@@ -97,58 +58,30 @@ MathOperationTag = {
     MathOperation.multiply_add: "cutlass::arch::OpMultiplyAdd",
 }
 
-###################################################################################################
 
-#
 class LayoutType(enum.Enum):
     ColumnMajor = enum_auto()
     RowMajor = enum_auto()
-    ColumnMajorInterleaved2 = enum_auto()
-    RowMajorInterleaved2 = enum_auto()
-    ColumnMajorInterleaved32 = enum_auto()
-    RowMajorInterleaved32 = enum_auto()
-    ColumnMajorInterleaved64 = enum_auto()
-    RowMajorInterleaved64 = enum_auto()
 
 
-#
 LayoutTag = {
     LayoutType.ColumnMajor: "cutlass::layout::ColumnMajor",
     LayoutType.RowMajor: "cutlass::layout::RowMajor",
-    LayoutType.ColumnMajorInterleaved2: "cutlass::layout::ColumnMajorInterleaved<2>",
-    LayoutType.RowMajorInterleaved2: "cutlass::layout::RowMajorInterleaved<2>",
-    LayoutType.ColumnMajorInterleaved32: "cutlass::layout::ColumnMajorInterleaved<32>",
-    LayoutType.RowMajorInterleaved32: "cutlass::layout::RowMajorInterleaved<32>",
-    LayoutType.ColumnMajorInterleaved64: "cutlass::layout::ColumnMajorInterleaved<64>",
-    LayoutType.RowMajorInterleaved64: "cutlass::layout::RowMajorInterleaved<64>",
 }
 
-#
+
 TransposedLayout = {
     LayoutType.ColumnMajor: LayoutType.RowMajor,
     LayoutType.RowMajor: LayoutType.ColumnMajor,
-    LayoutType.ColumnMajorInterleaved2: LayoutType.RowMajorInterleaved2,
-    LayoutType.RowMajorInterleaved2: LayoutType.ColumnMajorInterleaved2,
-    LayoutType.ColumnMajorInterleaved32: LayoutType.RowMajorInterleaved32,
-    LayoutType.RowMajorInterleaved32: LayoutType.ColumnMajorInterleaved32,
-    LayoutType.ColumnMajorInterleaved64: LayoutType.RowMajorInterleaved64,
-    LayoutType.RowMajorInterleaved64: LayoutType.ColumnMajorInterleaved64,
 }
 
-#
+
 ShortLayoutTypeNames = {
     LayoutType.ColumnMajor: "n",
-    LayoutType.ColumnMajorInterleaved2: "n2",
-    LayoutType.ColumnMajorInterleaved32: "n32",
-    LayoutType.ColumnMajorInterleaved64: "n64",
     LayoutType.RowMajor: "t",
-    LayoutType.RowMajorInterleaved2: "t2",
-    LayoutType.RowMajorInterleaved32: "t32",
-    LayoutType.RowMajorInterleaved64: "t64",
 }
 
-###################################################################################################
-#
+
 class OpcodeClass(enum.Enum):
     Simt = enum_auto()
     TensorOp = enum_auto()
@@ -167,35 +100,20 @@ OpcodeClassTag = {
     OpcodeClass.WmmaTensorOp: "cutlass::arch::OpClassWmmaTensorOp",
 }
 
-###################################################################################################
 
-#
 class OperationKind(enum.Enum):
     Gemm = enum_auto()
 
 
-#
 OperationKindNames = {
     OperationKind.Gemm: "gemm",
 }
 
-#
+
 class Target(enum.Enum):
     library = enum_auto()
 
 
-ArchitectureNames = {
-    50: "maxwell",
-    60: "pascal",
-    61: "pascal",
-    70: "volta",
-    75: "turing",
-    80: "ampere",
-}
-
-###################################################################################################
-
-#
 def SubstituteTemplate(template, values):
     text = template
     changed = True
@@ -210,18 +128,15 @@ def SubstituteTemplate(template, values):
     return text
 
 
-###################################################################################################
-#
 class GemmKind(enum.Enum):
     Gemm = enum_auto()
 
 
-#
 GemmKindNames = {
     GemmKind.Gemm: "gemm",
 }
 
-#
+
 class EpilogueFunctor(enum.Enum):
     LinearCombination = enum_auto()
     LinearCombinationRelu = enum_auto()
@@ -244,7 +159,6 @@ class SwizzlingFunctor(enum.Enum):
     Identity8 = enum_auto()
 
 
-#
 SwizzlingFunctorTag = {
     SwizzlingFunctor.Identity1: "cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<1>",
     SwizzlingFunctor.Identity2: "cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<2>",
@@ -252,9 +166,7 @@ SwizzlingFunctorTag = {
     SwizzlingFunctor.Identity8: "cutlass::gemm::threadblock::GemmIdentityThreadblockSwizzle<8>",
 }
 
-###################################################################################################
 
-#
 class MathInstruction:
     def __init__(
         self,
@@ -273,7 +185,6 @@ class MathInstruction:
         self.math_operation = math_operation
 
 
-#
 class TileDescription:
     def __init__(
         self, threadblock_shape, stages, warp_count, math_instruction, min_compute, max_compute
@@ -294,12 +205,8 @@ class TileDescription:
         )
 
 
-#
 class TensorDescription:
     def __init__(self, element, layout, alignment=1):
         self.element = element
         self.layout = layout
         self.alignment = alignment
-
-
-###################################################################################################
