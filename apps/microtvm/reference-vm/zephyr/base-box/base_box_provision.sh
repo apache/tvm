0bd16911de55cff1b35be59952a54ced637fc8b1
Mehrdad Hessar <mhessar@octoml.ai>
2021-10-25 11:19:13 -0700

    [microTVM] Add platform version check to template project (#9274)
    * add platform version to project template
    
    * fix arduino cli version on qemu
    
    * specify arduino/zephyr version everywhere
    
    * cleanup
    
    * address comments
    
    * fix warning message
    
    * fix typo
    
    * trigger
    
    * trigger
    
    * address comments
    
    * trigger
    
    * trigger

--- apps/microtvm/reference-vm/zephyr/base-box/base_box_provision.sh
+++ apps/microtvm/reference-vm/zephyr/base-box/base_box_provision.sh
@@ -28,7 +28,8 @@ source ~/.profile
 # Init Zephyr
 cd ~
 # Using most recent commit that passes all the tests.
-~/ubuntu_init_zephyr_project.sh ~/zephyr v2.5-branch --commit dabf23758417fd041fec2a2a821d8f526afac29d
+ZEPHYR_VERSION="v2.5-branch"
+~/ubuntu_init_zephyr_project.sh ~/zephyr ${ZEPHYR_VERSION} --commit dabf23758417fd041fec2a2a821d8f526afac29d
 
 # Cleanup
 rm -f *.sh
