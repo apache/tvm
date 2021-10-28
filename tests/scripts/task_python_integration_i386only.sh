e1cf96451a00332afce631f82ff5ebdf0fe3b70c
Mehrdad Hessar <mhessar@octoml.ai>
2021-10-28 10:06:56 -0700

    address comment

--- tests/scripts/task_python_integration_i386only.sh
+++ tests/scripts/task_python_integration_i386only.sh
@@ -15,6 +15,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+set -e
+set -u
 
 export TVM_INTEGRATION_I386_ONLY=1
 
