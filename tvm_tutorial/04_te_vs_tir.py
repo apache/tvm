10af1c7d3b1a2091d86887df3d08f4512568c16a
Paul Palomero Bernardo <paulpb@outlook.com>
2021-11-02 22:44:52 +0100

    Fix tutorial 04 and update enumeration

--- tvm_tutorial/04_te_vs_tir.py
+++ tvm_tutorial/04_te_vs_tir.py
@@ -67,9 +67,55 @@ def tir_conv1d(
 
 
 def main():
+    # Define parameters
     params = {"G": 1, "K": 8, "C": 8, "X": 20, "F_X": 9, "s": 1, "d": 1, "p": 0}
+    X_prime = (
+        params["X"] + 2 * params["p"] - params["F_X"] - (params["F_X"] - 1) * (params["d"] - 1)
+    ) // params["s"] + 1
+
+    i_torch = torch.rand((1, params["C"], params["X"]))
+    w_torch = torch.rand((params["K"], params["C"] // params["G"], params["F_X"]))
+    b_torch = torch.rand((params["K"]))
+
+    i_tvm = tvm.nd.array(i_torch.numpy())
+    w_tvm = tvm.nd.array(w_torch.numpy())
+    b_tvm = tvm.nd.array(b_torch.numpy())
+
+    # Compute torch_conv1d
+    torch_conv1d = F.conv1d(
+        i_torch,
+        w_torch,
+        bias=b_torch,
+        stride=params["s"],
+        padding=params["p"],
+        dilation=params["d"],
+        groups=params["G"],
+    )
+
+    # Compute te_conv1d
+    o_te = tvm.nd.array(
+        np.zeros((1, params["G"], params["K"] // params["G"], X_prime), dtype="float32")
+    )
+    i, w, b, o = te_conv1d(
+        params["G"],
+        params["K"],
+        params["C"],
+        params["X"],
+        params["F_X"],
+        params["s"],
+        params["d"],
+        params["p"],
+    )
+    schedule = te.create_schedule(o.op)
+    mod = tvm.lower(schedule, [i, w, b, o], simple_mode=False)
+    te_func = tvm.build(mod, [i, w, b, o], "llvm")
+    te_func(i_tvm, w_tvm, b_tvm, o_te)
+    o_te = o_te.asnumpy().reshape(1, params["K"], X_prime)
+
+    # Compute tir_conv1d
+    o_tir = tvm.nd.array(np.zeros((1, params["K"], X_prime), dtype="float32"))
     _, _, _, _, G, K, C, X, F_X, s, d, p = tir_conv1d.params
-    func = tir_conv1d.specialize(
+    tir_func = tir_conv1d.specialize(
         {
             G: params["G"],
             K: params["K"],
@@ -81,77 +127,13 @@ def main():
             p: params["p"],
         }
     )
-    sch = tir.Schedule(func, debug_mask="all")
-    g, k, c, f_x, x = sch.get_loops(sch.get_block("mac"))
-    k_0, k_1 = sch.split(k, factors=[2, 4])
-    sch.reorder(g, k_0, c, f_x, x, k_1)
-    print(sch.mod)
-
-    # input (i), weights (w)
-    i = torch.rand((1, params["C"], params["X"]))
-    w = torch.rand((params["K"], params["C"] // params["G"], params["F_X"]))
-    b = torch.rand((params["K"]))
-
-    conv = F.conv1d(
-        i,
-        w,
-        bias=b,
-        stride=params["s"],
-        padding=params["p"],
-        dilation=params["d"],
-        groups=params["G"],
-    )
-
-    i = tvm.nd.array(i.numpy())
-    w = tvm.nd.array(w.numpy())
-    b = tvm.nd.array(b.numpy())
-    o = tvm.nd.array(
-        np.zeros(
-            (
-                1,
-                params["K"],
-                (
-                    params["X"]
-                    + 2 * params["p"]
-                    - params["F_X"]
-                    - (params["F_X"] - 1) * (params["d"] - 1)
-                )
-                // params["s"]
-                + 1,
-            ),
-            dtype="float32",
-        )
-    )
+    sch = tir.Schedule(tir_func, debug_mask="all")
     mod = tvm.lower(sch.mod["main"])
-    print(mod)
-    tvm_conv1d = tvm.build(mod)
-    tvm_conv1d(i, w, b, o)
+    tir_func = tvm.build(mod)
+    tir_func(i_tvm, w_tvm, b_tvm, o_tir)
 
-    assert np.allclose(o.numpy(), conv.numpy())
-
-    # ii, ww, bb, oo = te_conv1d(
-    #     params["G"],
-    #     params["K"],
-    #     params["C"],
-    #     params["X"],
-    #     params["F_X"],
-    #     params["s"],
-    #     params["d"],
-    #     params["p"],
-    # )
-    # schedule = te.create_schedule(oo.op)
-    # # nn, gg, kk, xx = o.op.axis
-    # mod = tvm.lower(schedule, [ii, ww, bb, oo], simple_mode=False)
-    # func = tvm.build(mod, [ii, ww, bb, oo], "llvm")
-    # func(i, w, b, o)
-    # o = o.asnumpy().reshape(
-    #     1,
-    #     params["K"],
-    #     (params["X"] + 2 * params["p"] - params["F_X"] - (params["F_X"] - 1) * (params["d"] - 1))
-    #     // params["s"]
-    #     + 1,
-    # )
-    # print(o)
+    # Compare torch, te, tir
+    assert np.allclose(o_te, o_tir.numpy(), torch_conv1d.numpy())
 
 
 if __name__ == "__main__":
