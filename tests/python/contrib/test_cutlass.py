cb7316595fb4ce23d676b3d288ae081ffdd31d53
masahi <masahi129@gmail.com>
2021-11-03 10:16:49 +0900

    [CUTLASS] Initial support for dynamic shape dense (#9419)
    * dynamic dense branch import
    
    commit ee790ad64dfaec9d28f27726074a53a1daf3cf3e
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 21:32:32 2021 +0900
    
        dynamic dense working on ref build and exec
    
    commit 2449b667727e247c5b04e5fce12fa9d284444c38
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 21:27:46 2021 +0900
    
        add vm build API
    
    commit 57036806b0533e5b039cc17f8da81983fba3ef9c
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 19:57:33 2021 +0900
    
        fix test
    
    commit 4a7a5033b1f8c34b7f08b0a2882d6fa8bb5f44ab
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 19:55:33 2021 +0900
    
        fixed profile_all = False case
    
    commit fb915195c122e9036ea3283e7fc286fa19222709
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 19:34:15 2021 +0900
    
        add export_lib compile kwargs
    
    commit 3ad61e5379b486a0af010143ae8c7065b1c649d0
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 18:41:45 2021 +0900
    
        fixed hardcoded cutlass include path
    
    commit 1def1d34ad042f9859307fdbd1f732186115d556
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 18:36:19 2021 +0900
    
        add vm build and exec
    
    commit 570f9dd2cdc9b366348043ecc9c52a593ae4e273
    Author: Masahiro Masuda <masahi129@gmail.com>
    Date:   Fri Oct 29 18:15:01 2021 +0900
    
        adding test
    
    * start new jit impl for updated signature
    
    * generate signature that takes DLTensor as input
    
    * choose default kernel, hit shape func problem
    
    * dynamic dense ran but the result is a bit off
    
    * the result agrees with cublas
    
    * support dynamic N
    
    * run black
    
    * clang format
    
    * add doc for build_cutlass_kernels_vm
    
    * add doc to gen_gemm.py
    
    * add sm75 dense kernels
    
    * black
    
    * replace print with logging, add sm check for DEFAULT_KERNELS

--- tests/python/contrib/test_cutlass.py
+++ tests/python/contrib/test_cutlass.py
@@ -14,21 +14,46 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import logging
 import math
 import pytest
 import tvm
 from tvm import relay
 import numpy as np
+from tvm.runtime.vm import VirtualMachine
 from tvm.relay.op.contrib.cutlass import partition_for_cutlass
-from tvm.contrib.cutlass import tune_cutlass_kernels, build_cutlass_kernels
+from tvm.contrib.cutlass import (
+    tune_cutlass_kernels,
+    build_cutlass_kernels,
+    build_cutlass_kernels_vm,
+)
+
+logging.basicConfig(level=logging.INFO)
 
 
-def get_ref_rt_mod(mod, params):
+def has_cublas():
+    return tvm.get_global_func("tvm.contrib.cublas.matmul", True) != None
+
+
+def has_cutlass():
+    return tvm.get_global_func("relay.ext.cutlass", True) != None
+
+
+def get_ref_rt_mod(mod, params, target="cuda"):
     with tvm.transform.PassContext(opt_level=3):
-        lib = relay.build(mod, target="cuda", params=params)
-    dev = tvm.device("cuda", 0)
+        lib = relay.build(mod, target=target, params=params)
+    dev = tvm.device(target, 0)
     rt_mod = tvm.contrib.graph_executor.GraphModule(lib["default"](dev))
     return rt_mod, dev
+
+
+def get_ref_vm(mod, params, target="cuda"):
+    with tvm.transform.PassContext(opt_level=3):
+        vm_exec = relay.vm.compile(mod, target=target, params=params)
+        code, lib = vm_exec.save()
+    dev = tvm.device(target, 0)
+    vm_exec = tvm.runtime.vm.Executable.load_exec(code, lib)
+    return VirtualMachine(vm_exec, dev), dev
 
 
 def get_output(rt_mod, x):
@@ -37,10 +62,18 @@ def get_output(rt_mod, x):
     return rt_mod.get_output(0).asnumpy()
 
 
-def get_dense(M, N, K, out_dtype="float16"):
-    data = relay.var("data", shape=(M, K), dtype="float16")
-    weight = relay.var("weight", shape=(N, K), dtype="float16")
+def get_output_vm(vm, x):
+    return vm.invoke("main", data=x).numpy()
+
+
+def get_dense_with_shape(data_shape, weight_shape, out_dtype="float16"):
+    data = relay.var("data", shape=data_shape, dtype="float16")
+    weight = relay.var("weight", shape=weight_shape, dtype="float16")
     return relay.nn.dense(data, weight, out_dtype=out_dtype)
+
+
+def get_dense(M, N, K, out_dtype="float16"):
+    return get_dense_with_shape((M, K), (N, K), out_dtype)
 
 
 def get_dense_bias(M, N, K, out_dtype="float16"):
@@ -78,32 +111,61 @@ def profile_and_build(mod, params, sm, tmp_dir="./tmp", lib_path="compile.so"):
     return rt_mod, dev, num_cutlass_partition
 
 
-def verify(func, M, N, K, sm=80, atol=1e-5, rtol=1e-5, run_benchmark=False):
-    if not tvm.get_global_func("relay.ext.cutlass", True):
+def profile_and_build_vm(
+    mod, params, sm, tmp_dir="./tmp", lib_path="compile.so", vmcode_path="vmcode.ro"
+):
+    mod = partition_for_cutlass(mod)
+    mod, num_cutlass_partition = tune_cutlass_kernels(mod, sm, tmp_dir=tmp_dir)
+    with tvm.transform.PassContext(opt_level=3):
+        vm_exec = relay.vm.compile(mod, target="cuda", params=params)
+    vm_exec = build_cutlass_kernels_vm(vm_exec, sm, tmp_dir, lib_path, vmcode_path)
+    dev = tvm.device("cuda", 0)
+    return VirtualMachine(vm_exec, dev), dev, num_cutlass_partition
+
+
+def verify(func, M, N, K, ref_target="cuda", sm=80, atol=1e-5, rtol=1e-5, run_benchmark=False):
+    if not has_cutlass():
         return
     mod = tvm.IRModule.from_expr(func)
-    typ = relay.transform.InferType()(mod)
-    out_dtype = typ["main"].body.checked_type.dtype
+    typ = relay.transform.InferType()(mod)["main"].body.checked_type
+    out_dtype = typ.dtype
+    use_vm = any(isinstance(s, tvm.tir.Any) for s in typ.shape)
     np_data = np.random.uniform(-1, 1, (M, K)).astype("float16")
     np_weight = np.random.uniform(-1, 1, (N, K)).astype("float16")
     np_bias = np.random.uniform(-1, 1, (N,)).astype(out_dtype)
 
     params = {"weight": np_weight, "bias": np_bias}
 
-    rt_mod_ref, dev = get_ref_rt_mod(mod, params)
-    rt_mod, dev, num_partition = profile_and_build(mod, params, sm)
+    if use_vm:
+        if ref_target == "cuda" and out_dtype == "float16":
+            # Uncomment "return" below to see the accuracy difference of static vs dynamic TVM native fp16 dense
+            # The static one can use a tensorcore schedule, but the dynamic one cannot
+            rt_mod, dev = get_ref_vm(tvm.IRModule.from_expr(get_dense(M, N, K)), params)
+            num_partition = 1
+            logging.warning(
+                "The reference fp16 dense with dynamic shape using fp16 accumulation has accuracy issues."
+            )
+            return
+        else:
+            rt_mod, dev, num_partition = profile_and_build_vm(mod, params, sm)
+
+        rt_mod_ref, dev = get_ref_vm(mod, params, target=ref_target)
+        x = tvm.nd.array(np_data, device=dev)
+        out = get_output_vm(rt_mod, x)
+        ref_out = get_output_vm(rt_mod_ref, x)
+    else:
+        rt_mod_ref, dev = get_ref_rt_mod(mod, params, target=ref_target)
+        rt_mod, dev, num_partition = profile_and_build(mod, params, sm)
+        x = tvm.nd.array(np_data, device=dev)
+        out = get_output(rt_mod, x)
+        ref_out = get_output(rt_mod_ref, x)
+
     assert num_partition > 0
-
-    x = tvm.nd.array(np_data, device=dev)
-
-    out = get_output(rt_mod, x)
-    ref_out = get_output(rt_mod_ref, x)
-
     np.testing.assert_allclose(out, ref_out, atol=atol, rtol=rtol)
 
     if run_benchmark:
         print("CUTLASS:", rt_mod.benchmark(dev, number=1, repeat=600))
-        print("TVM Tensorcore (no tuning):", rt_mod_ref.benchmark(dev, number=1, repeat=600))
+        print("TVM with target %s:" % ref_target, rt_mod_ref.benchmark(dev, number=1, repeat=600))
 
 
 M = 1820
@@ -129,6 +191,31 @@ def test_dense_bias_relu():
 def test_dense_bias_gelu():
     verify(get_dense_bias_gelu(M, N, K), M, N, K, atol=1e-3, rtol=1e-3)
     verify(get_dense_bias_gelu(M, N, K, out_dtype="float32"), M, N, K, atol=1e-3, rtol=1e-3)
+
+
+def test_dense_dynamic():
+    data_shape = (relay.Any(), K)
+    weight_shape = (relay.Any(), K)
+
+    if has_cublas():
+        # TVM native fp16 dense (without tensorcore), using fp16 accum, seems to have accuracy issues
+        # Use cublas as a reference
+        verify(
+            get_dense_with_shape(data_shape, weight_shape),
+            M,
+            N,
+            K,
+            ref_target="cuda -libs=cublas",
+        )
+
+    verify(
+        get_dense_with_shape(data_shape, weight_shape, out_dtype="float32"),
+        M,
+        N,
+        K,
+        atol=1e-4,
+        rtol=1e-4,
+    )
 
 
 if __name__ == "__main__":
