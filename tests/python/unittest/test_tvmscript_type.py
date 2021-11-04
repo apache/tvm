3ea3edd6ee34268dcfb8ddf65e819a7af970b344
shingjan <yj@shingjan.me>
2021-11-04 01:29:21 -0700

    add test cases

--- tests/python/unittest/test_tvmscript_type.py
+++ tests/python/unittest/test_tvmscript_type.py
@@ -21,38 +21,68 @@ import tvm
 from tvm.script import tir as T
 
 """
-This module tests the type of
+This prim_func tests the type of
 T.prim_func, T.handle, T.match_buffer, T.block
 T.reads, T.writes, T.alloc_buffer, T.serial
-T.block_attr, T.float32
+T.block_attr, T.float32, T.axis.remap
 """
 
 
-@tvm.script.ir_module
-class Module:
-    @T.prim_func
-    def element_wise_storage_align(a: T.handle, c: T.handle) -> None:
-        C = T.match_buffer(c, [128, 128], elem_offset=0, align=128, offset_factor=1)
-        A = T.match_buffer(a, [128, 128], elem_offset=0, align=128, offset_factor=1)
-        # body
-        with T.block("root"):
-            T.reads([])
-            T.writes([])
-            B = T.alloc_buffer([128, 128], elem_offset=0, align=128, offset_factor=1)
-            for i0 in T.serial(0, 128):
-                for ax1 in T.serial(0, 128):
-                    with T.block("B"):
-                        vi, vj = T.axis.remap("SS", [i0, ax1])
-                        T.reads([A[vi, vj]])
-                        T.writes([B[vi, vj]])
-                        T.block_attr({"buffer_dim_align": [[0, 0, 128, 127]]})
-                        B[vi, vj] = A[vi, vj] * T.float32(2)
-                for i1 in T.serial(0, 128):
-                    with T.block("C"):
-                        vi_1, vj_1 = T.axis.remap("SS", [i0, i1])
-                        T.reads([B[vi_1, vj_1]])
-                        T.writes([C[vi_1, vj_1]])
-                        C[vi_1, vj_1] = B[vi_1, vj_1] + T.float32(1)
+@T.prim_func
+def element_wise_storage_align(a: T.handle, c: T.handle) -> None:
+    C = T.match_buffer(c, [128, 128], elem_offset=0, align=128, offset_factor=1)
+    A = T.match_buffer(a, [128, 128], elem_offset=0, align=128, offset_factor=1)
+    # body
+    with T.block("root"):
+        T.reads([])
+        T.writes([])
+        B = T.alloc_buffer([128, 128], elem_offset=0, align=128, offset_factor=1)
+        for i0 in T.serial(0, 128):
+            for ax1 in T.serial(0, 128):
+                with T.block("B"):
+                    vi, vj = T.axis.remap("SS", [i0, ax1])
+                    T.reads([A[vi, vj]])
+                    T.writes([B[vi, vj]])
+                    T.block_attr({"buffer_dim_align": [[0, 0, 128, 127]]})
+                    B[vi, vj] = A[vi, vj] * T.float32(2)
+            for i1 in T.serial(0, 128):
+                with T.block("C"):
+                    vi_1, vj_1 = T.axis.remap("SS", [i0, i1])
+                    T.reads([B[vi_1, vj_1]])
+                    T.writes([C[vi_1, vj_1]])
+                    C[vi_1, vj_1] = B[vi_1, vj_1] + T.float32(1)
+
+
+"""
+This prim_func tests the type of
+T.env_thread, T.launch_thread, T.thread_binding
+"""
+
+
+@T.prim_func
+def element_wise_env_thread_x(a: T.handle, b: T.handle, c: T.handle) -> None:
+    j1_0 = T.env_thread("threadIdx.x")
+    j0_0 = T.env_thread("threadIdx.x")
+    i = T.env_thread("blockIdx.x")
+    A = T.match_buffer(a, [128, 128])
+    B = T.match_buffer(b, [128, 128])
+    C = T.match_buffer(c, [128, 128])
+    T.launch_thread(i, 128)
+    T.launch_thread(j0_0, 4)
+    T.launch_thread(j1_0, 4)
+
+    for blockIdx_x in T.thread_binding(0, 128, "blockIdx.x"):
+        for threadIdx_x in T.thread_binding(0, 4, "threadIdx.x"):
+            for j0_1 in T.serial(0, 32):
+                with T.block(""):
+                    B[blockIdx_x, threadIdx_x * 32 + j0_1] = (
+                        A[blockIdx_x, threadIdx_x * 32 + j0_1] * 2.0
+                    )
+            for j1_1 in T.serial(0, 32):
+                with T.block(""):
+                    C[blockIdx_x, threadIdx_x * 32 + j1_1] = (
+                        B[blockIdx_x, threadIdx_x * 32 + j1_1] + 1.0
+                    )
 
 
 if __name__ == "__main__":
