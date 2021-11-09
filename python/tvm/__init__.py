f7455b54dc85d08f8652422650c797ca8328c72e
Matthew Brookhart <mbrookhart@octoml.ai>
2021-11-09 11:07:43 -0700

    try conditional for micro import

--- python/tvm/__init__.py
+++ python/tvm/__init__.py
@@ -23,7 +23,8 @@ import traceback
 
 # top-level alias
 # tvm._ffi
-from ._ffi.base import TVMError, __version__
+from ._ffi.base import TVMError, __version__, _RUNTIME_ONLY
+
 from ._ffi.runtime_ctypes import DataTypeCode, DataType
 from ._ffi import register_object, register_func, register_extension, get_global_func
 
@@ -68,7 +69,7 @@ from . import support
 # Contrib initializers
 from .contrib import rocm as _rocm, nvcc as _nvcc, sdaccel as _sdaccel
 
-if support.libinfo().get("USE_MICRO", "OFF") == "ON":
+if not _RUNTIME_ONLY and support.libinfo().get("USE_MICRO", "OFF") == "ON":
     from . import micro
 
 # NOTE: This file should be python2 compatible so we can
