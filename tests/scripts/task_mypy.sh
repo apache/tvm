89ef25132267ed70c2d1a8748045f61b2d995388
Yuanjing Shi <yj@shingjan.me>
2021-11-09 07:29:32 -0800

    [TIR] Add type hint for TIR  (#9432)
    * add init
    
    * get rid of span
    
    * afs header
    
    * update scope_handler
    
    * rm tir/__init__.pyi
    
    * fix linting
    
    * fix lint
    
    * new test case
    
    * add axis module
    
    * address comments
    
    * redefine ty types
    
    * lint
    
    * address comments
    
    * address comments
    
    * fix ci
    
    * add test cases
    
    * fix CI
    
    * address comments
    
    * add types
    
    * mypy --strict
    
    * comments
    
    * update test comments
    
    * linting fix
    
    * address comments
    
    * add pylint for tir type check
    
    * address comments
    
    * move doc string
    
    * comments
    
    * getter setter
    
    * add PrimExpr, IterVar and Var
    
    * add sequence
    
    * change for handle

--- tests/scripts/task_mypy.sh
+++ tests/scripts/task_mypy.sh
@@ -19,6 +19,7 @@
 set -e
 set -u
 set -o pipefail
+source tests/scripts/setup-pytest-env.sh
 
 echo "Checking MyPy Type defs in the TensorIR schedule package."
 mypy  --check-untyped-defs python/tvm/tir/schedule
@@ -31,6 +32,9 @@ mypy  --check-untyped-defs python/tvm/tir/analysis/
 
 echo "Checking MyPy Type defs in the transform package."
 mypy  --check-untyped-defs python/tvm/tir/transform/
+
+echo "Checking MyPy Type defs in the TIR package with unittest"
+MYPYPATH=$TVM_PATH/python mypy --check-untyped-defs tests/python/unittest/test_tvmscript_type.py
 
 #TODO(@mikepapadim): This is failing atm
 # echo "Checking MyPy Type defs in the tvm.relay.backend.contrib.ethosu package."
