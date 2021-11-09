59c5d3f3ad65d078eebe20be7a8a6fb1379cc1ef
lhutton1 <35535092+lhutton1@users.noreply.github.com>
2021-11-09 14:24:50 +0000

    [TVMC] Add test for quantized pytorch model (#9467)
    As a follow up to #9417 and now that #9362 is resolved, this PR adds a
    test to check quantized pytorch mobilenetv2 is converted correctly.
    
    Change-Id: Iaf2d38ce71c008e0141a4a2536bd54c2c9f3fe3d

--- tests/python/driver/tvmc/conftest.py
+++ tests/python/driver/tvmc/conftest.py
@@ -127,6 +127,23 @@ def pytorch_resnet18(tmpdir_factory):
 
 
 @pytest.fixture(scope="session")
+def pytorch_mobilenetv2_quantized(tmpdir_factory):
+    try:
+        import torch
+        import torchvision.models as models
+    except ImportError:
+        # Not all environments provide Pytorch, so skip if that's the case.
+        return ""
+    model = models.quantization.mobilenet_v2(quantize=True)
+    model_file_name = "{}/{}".format(tmpdir_factory.mktemp("data"), "mobilenet_v2_quantized.pth")
+    # Trace model into torchscript.
+    traced_cpu = torch.jit.trace(model, torch.randn(1, 3, 224, 224))
+    torch.jit.save(traced_cpu, model_file_name)
+
+    return model_file_name
+
+
+@pytest.fixture(scope="session")
 def onnx_resnet50():
     base_url = "https://github.com/onnx/models/raw/master/vision/classification/resnet/model"
     file_to_download = "resnet50-v2-7.onnx"
