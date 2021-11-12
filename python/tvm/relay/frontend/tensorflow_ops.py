6a4172f5e093e750fa71917b9b069c0c40f02452
AndrewZhaoLuo <andrew.zhao.luo@gmail.com>
2021-11-11 16:45:54 -0800

    [Topi][Op][PyTorch][Vitas] Fix inconsistent kernel layout conventions for conv2d_transpose (#9336)
    * fix a lot of initial tests
    
    * make pytorch tests pass
    
    * lint
    
    * add test
    
    * fix bug with layout transform
    
    * change layouts for conv2d_transpose too
    
    * fix vitis tests
    
    * fix qnn conv2d transpose tests
    
    * fix fake quantization pass
    
    * add todo
    
    * lint
    
    * undo just formatting changes
    
    * remove formatting only change
    
    * remove f2qi for later pr
    
    * more frontend tests fixes
    
    * fix a lot of initial tests
    
    * make pytorch tests pass
    
    * lint
    
    * add test
    
    * fix bug with layout transform
    
    * change layouts for conv2d_transpose too
    
    * fix vitis tests
    
    * fix qnn conv2d transpose tests
    
    * fix fake quantization pass
    
    * add todo
    
    * lint
    
    * undo just formatting changes
    
    * remove formatting only change
    
    * remove f2qi for later pr
    
    * more frontend tests fixes
    
    * jostle
    
    * fix keras
    
    * fix another frontend test
    
    * fix things
    
    * jostle ci

--- python/tvm/relay/frontend/tensorflow_ops.py
+++ python/tvm/relay/frontend/tensorflow_ops.py
@@ -461,8 +461,11 @@ def _conv(opname):
             raise tvm.error.OpAttributeInvalid(msg.format(attr["padding"]))
 
         if "kernel_layout" not in attr:
-            if opname in ["conv", "conv_transpose"]:
+            if opname == "conv":
                 attr["kernel_layout"] = "HWIO" if attr["data_format"] == "NHWC" else "OIHW"
+            elif opname == "conv_transpose":
+                # conv_transpose in TVM has weights be IOHW for NCHW
+                attr["kernel_layout"] = "HWIO" if attr["data_format"] == "NHWC" else "IOHW"
             else:
                 attr["kernel_layout"] = "HWOI" if attr["data_format"] == "NHWC" else "OIHW"
 
