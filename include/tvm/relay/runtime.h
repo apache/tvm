34ff4e98079c823e594e75523e53026cba666077
wrongtest <wrongtest0@gmail.com>
2021-11-15 23:46:54 +0800

    remove compile warning complained by macos clang-13.0

--- include/tvm/relay/runtime.h
+++ include/tvm/relay/runtime.h
@@ -159,9 +159,6 @@ class Runtime : public ObjectRef {
  */
 class RuntimeRegEntry {
  public:
-  /*! \brief Set name of the Runtime to be the same as registry if it is empty */
-  inline RuntimeRegEntry& set_name();
-
   /*!
    * \brief Register a valid configuration option and its ValueType for validation
    * \param key The configuration key
@@ -218,13 +215,6 @@ class RuntimeRegEntry {
   friend class Runtime;
 };
 
-inline RuntimeRegEntry& RuntimeRegEntry::set_name() {
-  if (name.empty()) {
-    name = name;
-  }
-  return *this;
-}
-
 template <typename ValueType>
 inline RuntimeRegEntry& RuntimeRegEntry::add_attr_option(const String& key) {
   ICHECK(!key2vtype_.count(key)) << "AttributeError: add_attr_option failed because '" << key
@@ -269,7 +259,7 @@ inline RuntimeRegEntry& RuntimeRegEntry::add_attr_option(const String& key,
  */
 #define TVM_REGISTER_RUNTIME(RuntimeName)                     \
   TVM_STR_CONCAT(TVM_RUNTIME_REGISTER_VAR_DEF, __COUNTER__) = \
-      ::tvm::relay::RuntimeRegEntry::RegisterOrGet(RuntimeName).set_name()
+      ::tvm::relay::RuntimeRegEntry::RegisterOrGet(RuntimeName)
 }  // namespace relay
 }  // namespace tvm
 
