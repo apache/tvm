948641c7f4abef6376094d5eddcc52e79521d425
Christopher Sidebottom <chris.sidebottom@arm.com>
2021-11-16 08:09:00 +0000

    [2/3][AOT][DeviceAPI] Add Hooks for Activate/Deactivate/Open/Close (#9500)
    * [AOT][DeviceAPI] Add Hooks for Activate/Deactivate/Open/Close
    
    This adds the relevant hooks into their starting places in the code
    generation. As per the [C Device API
    RFC](https://github.com/apache/tvm-rfcs/blob/main/rfcs/0031-devices-api.md)
    
    * Standardise on `lowered_ir_mods` and correct device_hook variable name

--- python/tvm/relay/backend/executor_factory.py
+++ python/tvm/relay/backend/executor_factory.py
@@ -75,6 +75,8 @@ class AOTExecutorFactoryModule(ExecutorFactoryModule):
     ----------
     ir_mod : :py:class:`~tvm.IRModule`
         The IR module to build.
+    lowered_ir_mods : dict[Target, IRModule]
+        The IR modules lowered per Target.
     target : tvm.Target
         The Target used to build this module.
     libmod : tvm.Module
@@ -89,8 +91,19 @@ class AOTExecutorFactoryModule(ExecutorFactoryModule):
         List of devices used in the module
     """
 
-    def __init__(self, ir_mod, target, libmod, libmod_name, params, function_metadata, devices):
+    def __init__(
+        self,
+        ir_mod,
+        lowered_ir_mods,
+        target,
+        libmod,
+        libmod_name,
+        params,
+        function_metadata,
+        devices,
+    ):
         self.ir_mod = ir_mod
+        self.lowered_ir_mods = lowered_ir_mods
         self.target = target
         self.lib = libmod
         self.libmod_name = libmod_name
@@ -136,7 +149,14 @@ class GraphExecutorFactoryModule(ExecutorFactoryModule):
     """
 
     def __init__(
-        self, ir_mod, target, graph_json_str, libmod, libmod_name, params, function_metadata
+        self,
+        ir_mod,
+        target,
+        graph_json_str,
+        libmod,
+        libmod_name,
+        params,
+        function_metadata,
     ):
         assert isinstance(graph_json_str, string_types)
         fcreate = get_global_func("tvm.graph_executor_factory.create")
