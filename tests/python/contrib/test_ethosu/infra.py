399fada7a6377c3b2a450d655dfab1c4919882ff
Elen Kalda <elen.kalda@arm.com>
2021-11-16 17:24:59 +0000

    Add the Arm(R) Ethos(TM)-U NPU identity operator (#9457)
    * Add the Arm(R) Ethos(TM)-U NPU identity operator
    
    * Add the ethosu.identity operator which returns the input tensor
    * Add an opportunity to requantize the tensor
    * Add legalization for reshape and strided slice
    * Add a pass that puts an indentity op after a no-op
    
    Change-Id: I0adb5ca269f8529c79e0e7681ca4b5147d8f53c8
    
    * Fix the pylint errors
    
    Change-Id: Icc9b6507f164681a5d6b1fcff2ae4a5051d44734
    
    * Changes in response to review comments
    
    Change-Id: I63f30f84ad481789fc047ad8c2107f5313562f7f

--- tests/python/contrib/test_ethosu/infra.py
+++ tests/python/contrib/test_ethosu/infra.py
@@ -308,6 +308,33 @@ def generate_ref_data_tflite(model):
     return input_data, expected_output_data
 
 
+def make_partitioned_function(relay_op):
+
+    ifm0 = relay.analysis.free_vars(relay_op)
+    ifm_shape = ifm0[0].type_annotation.shape
+
+    ifm = relay.var("ifm", shape=ifm_shape, dtype="int8")
+
+    glb_ethosu = relay.GlobalVar("tvmgen_default_ethosu_main_0")
+
+    func = (
+        relay.Function(ifm0, relay_op)
+        .with_attr("Inline", 1)
+        .with_attr("Compiler", "ethos-u")
+        .with_attr("global_symbol", "tvmgen_default_ethosu_main_0")
+        .with_attr("Primitive", 1)
+    )
+    mod = tvm.IRModule()
+    mod[glb_ethosu] = func
+    mod = relay.transform.InferType()(mod)
+
+    call = relay.Call(glb_ethosu, [ifm])
+    mod["main"] = relay.Function([ifm], call)
+    mod = relay.transform.InferType()(mod)
+
+    return mod
+
+
 def generate_weights_data(shape, dtype):
     size = 1
     for dim in shape:
@@ -567,3 +594,24 @@ def make_ethosu_binary_elementwise(
         ofm_layout=ofm_layout,
     )
     return ethosu_binary_elementwise
+
+
+def make_ethosu_identity(
+    ifm,
+    lut=relay.const([], dtype="int8"),
+    ifm_scale=1,
+    ifm_zero_point=0,
+    ofm_scale=1,
+    ofm_zero_point=0,
+    activation="NONE",
+):
+    identity = ethosu_ops.ethosu_identity(
+        ifm,
+        lut=lut,
+        ifm_scale=ifm_scale,
+        ifm_zero_point=ifm_zero_point,
+        ofm_scale=ofm_scale,
+        ofm_zero_point=ofm_zero_point,
+        activation=activation,
+    )
+    return identity
--- tests/python/contrib/test_ethosu/test_type_inference.py
+++ tests/python/contrib/test_ethosu/test_type_inference.py
@@ -19,12 +19,12 @@ import pytest
 pytest.importorskip("ethosu.vela")
 
 from tvm import relay, TVMError
-from tvm import relay
 from tvm.relay.testing import run_opt_pass
 from .infra import make_ethosu_conv2d
 from .infra import make_ethosu_depthwise_conv2d
 from .infra import make_ethosu_pooling
 from .infra import make_ethosu_binary_elementwise
+from .infra import make_ethosu_identity
 
 
 @pytest.mark.parametrize(
@@ -338,6 +338,38 @@ def test_ethosu_binary_elementwise_shift_invalid_data_type(invalid_dtype, operat
         invalid_dtype,
     )
     func = relay.Function([ifm, ifm2], binary_elementwise)
+    with pytest.raises(TVMError):
+        run_opt_pass(func, relay.transform.InferType())
+
+
+@pytest.mark.parametrize("shape", [(1, 56, 72, 55), (241, 7, 755), (28, 44), (5003,)])
+def test_ethosu_identity_type_inference(shape):
+    dtype = "int8"
+    ifm = relay.var("ifm", shape=shape, dtype=dtype)
+    identity = make_ethosu_identity(ifm)
+    func = relay.Function([ifm], identity)
+    func = run_opt_pass(func, relay.transform.InferType())
+    assert tuple(func.body.checked_type.shape) == shape
+    assert func.body.checked_type.dtype == dtype
+
+
+def test_ethosu_identity_invalid_shape():
+    invalid_shape = [1, 2, 3, 4, 5]
+    dtype = "int8"
+    ifm = relay.var("ifm", shape=invalid_shape, dtype=dtype)
+
+    identity = make_ethosu_identity(ifm)
+    func = relay.Function([ifm], identity)
+    with pytest.raises(TVMError):
+        run_opt_pass(func, relay.transform.InferType())
+
+
+def test_ethosu_invalid_dtype():
+    invalid_dtype = "int32"
+    ifm = relay.var("ifm", shape=[6000], dtype=invalid_dtype)
+
+    identity = make_ethosu_identity(ifm)
+    func = relay.Function([ifm], identity)
     with pytest.raises(TVMError):
         run_opt_pass(func, relay.transform.InferType())
 
