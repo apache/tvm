093f3e1d22b6c0e09e5fbec70aeb3702245618f5
Ashutosh Parkhi <ashutosh.parkhi@arm.com>
2021-11-26 17:20:48 +0000

    Code generation for fully connected layer via CMSIS-NN
    Change-Id: I67f65e69dfa2c6d31c2ea928d92a827df80bc51a

--- python/tvm/relay/op/contrib/cmsisnn.py
+++ python/tvm/relay/op/contrib/cmsisnn.py
@@ -145,6 +145,49 @@ def pattern_table():
             and (not is_depthwise or bias_add is not None)
         )
 
+    def qnn_fully_connected_pattern():
+        """Create pattern for qnn.dense with optional relu."""
+        qnn_fc = is_op("qnn.dense")(
+            wildcard(), is_constant(), is_constant(), is_constant(), is_constant(), is_constant()
+        )
+        bias_add = is_op("nn.bias_add")(qnn_fc, is_constant())
+        req = is_op("qnn.requantize")(
+            qnn_fc | bias_add, is_constant(), is_constant(), is_constant(), is_constant()
+        )
+        clip_or_req = req.optional(is_op("clip"))
+        return clip_or_req
+
+    def check_qnn_fully_connected(pattern):
+        """Check if the fully connected is supported by CMSIS-NN."""
+        if str(pattern.op.name) == "clip":
+            relu = pattern
+            requantize = relu.args[0]
+        else:
+            requantize = pattern
+        requantize_input = requantize.args[0]
+        bias_add = None
+        bias_dtype = "int32"
+        if str(requantize_input.op.name) == "nn.bias_add":
+            bias_add = requantize_input
+            fc = bias_add.args[0]
+            bias_dtype = bias_add.args[1].checked_type.dtype
+        else:
+            fc = requantize_input
+        fc_input = fc.args[0]
+        fc_weight = fc.args[1]
+
+        # kernel zero_point should be 0
+        kernel_zp = fc.args[3].data.numpy().item(0)
+
+        return (
+            fc.attrs.out_dtype == "int32"
+            and fc_input.checked_type.dtype == "int8"
+            and fc_weight.checked_type.dtype == "int8"
+            and pattern.checked_type.dtype == "int8"
+            and bias_dtype == "int32"
+            and kernel_zp == 0
+        )
+
     def binary_op_pattern(op):
         """Matches QNN binary operation"""
         return is_op(f"qnn.{op}")(
@@ -166,8 +209,9 @@ def pattern_table():
         )
 
     return [
-        ("cmsis-nn.qnn_softmax", qnn_softmax_pattern(), check_qnn_softmax),
         ("cmsis-nn.qnn_conv2d", qnn_conv2d_pattern(), check_qnn_conv2d),
+        ("cmsis-nn.qnn_fully_connected", qnn_fully_connected_pattern(), check_qnn_fully_connected),
         ("cmsis-nn.qnn_mul", binary_op_pattern("mul"), check_qnn_binary_op),
         ("cmsis-nn.qnn_add", binary_op_pattern("add"), check_qnn_binary_op),
+        ("cmsis-nn.qnn_softmax", qnn_softmax_pattern(), check_qnn_softmax),
     ]
