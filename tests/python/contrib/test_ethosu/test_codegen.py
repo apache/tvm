1abac94dbfa69bcc8c3e7e6d35e19f25edaa1c0d
Elen Kalda <elen.kalda@arm.com>
2021-11-27 18:40:36 +0000

    [microNPU] Add support for SPLIT and SPLIT_V
    Both, SPLIT and SPLIT_V get lowered to relay.split and in the
    legalization the Relay split gets turned into strided slices. This
    patch adds the pattern and legalizer to enable offloading the TFLite's
    splits to the NPU.

--- tests/python/contrib/test_ethosu/test_codegen.py
+++ tests/python/contrib/test_ethosu/test_codegen.py
@@ -856,5 +856,83 @@ def test_tflite_sigmoid(accel_type):
     _compare_tvm_with_tflite(sigmoid_function, [ifm_shape], accel_type)
 
 
+# This codegen test checks both, split and split_v
+@pytest.mark.parametrize("accel_type", ACCEL_TYPES)
+@pytest.mark.parametrize(
+    "ifm_shape, num_or_size_splits, axis",
+    [
+        ((1, 4, 6, 8), (1, 3, 4), 3),
+        ((4, 6, 8), 2, 0),
+        ((50,), 25, 0),
+        ((5, 11), 1, 1),
+        ((13,), (13,), 0),
+        ((22, 7), (4, -1), 1),
+    ],
+)
+def test_tflite_split(accel_type, ifm_shape, num_or_size_splits, axis):
+    dtype = "int8"
+
+    def get_tflite_graph():
+        class Model(tf.Module):
+            @tf.function
+            def tf_function(self, x, num_or_size_splits, axis):
+                op = tf.split(x, num_or_size_splits, axis=axis)
+                return op
+
+        model = Model()
+
+        concrete_func = model.tf_function.get_concrete_function(
+            tf.TensorSpec(ifm_shape, dtype=tf.float32), num_or_size_splits, axis
+        )
+
+        # Convert the model
+        def representative_dataset():
+            for _ in range(100):
+                data = np.random.rand(*tuple(ifm_shape))
+                yield [data.astype(np.float32)]
+
+        converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter.representative_dataset = representative_dataset
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter.inference_input_type = tf.int8
+        converter.inference_output_type = tf.int8
+        tflite_model = converter.convert()
+        return tflite_model
+
+    tflite_graph = get_tflite_graph()
+    tflite_model = tflite.Model.Model.GetRootAsModel(tflite_graph, 0)
+
+    relay_module, params = relay.frontend.from_tflite(
+        tflite_model,
+        shape_dict={"input": ifm_shape},
+        dtype_dict={"input": dtype},
+    )
+    mod = partition_for_ethosu(relay_module, params)
+
+    # Generate reference data
+    input_data, output_data = infra.generate_ref_data_tflite(tflite_graph)
+
+    compiled_models = infra.build_source(
+        mod,
+        input_data,
+        output_data,
+        accel_type,
+    )
+
+    # Assumes only two runtime.Modules are created -- i.e. single offload module
+    imported_modules = compiled_models[0].executor_factory.lib.imported_modules
+    assert len(imported_modules) == 2
+    ethosu_module = imported_modules[0]
+
+    # Verify generated C source
+    get_cs = tvm._ffi.get_global_func("runtime.module.ethos-u.getcs")
+    cmms = get_cs(ethosu_module)
+    cmms = bytes.fromhex(cmms)
+
+    infra.print_payload(cmms)
+    infra.verify_source(compiled_models, accel_type)
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
