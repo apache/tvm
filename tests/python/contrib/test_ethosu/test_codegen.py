18ee95b655f32977dac23f4786786eb4daac61d2
Luke Hutton <luke.hutton@arm.com>
2021-12-01 16:50:52 +0000

    fix codegen tests after migration to target hooks
    Change-Id: Idcab5f71f3c5a971831b96fe8f9a5c8c73b267da

--- tests/python/contrib/test_ethosu/test_codegen.py
+++ tests/python/contrib/test_ethosu/test_codegen.py
@@ -593,15 +593,12 @@ def test_mean(accel_type, ifm_shape, axis, keep_dims, use_same_quantization):
     )
 
     # Assumes only two runtime.Modules are created -- i.e. single offload module
-    imported_modules = compiled_models[0].executor_factory.lib.imported_modules
-    assert len(imported_modules) == 2
-    ethosu_module = imported_modules[0]
+    ethosu_module = compiled_models[0].executor_factory.lib.imported_modules[0].imported_modules[0]
 
     # Verify generated C source
-    get_cs = tvm._ffi.get_global_func("runtime.module.ethos-u.getcs")
-    cmms = get_cs(ethosu_module)
-    cmms = bytes.fromhex(cmms)
-
+    get_artifacts = tvm._ffi.get_global_func("runtime.module.ethos-u.get_artifacts")
+    compilation_artifacts = get_artifacts(ethosu_module)
+    cmms = bytes.fromhex(compilation_artifacts[0].command_stream)
     infra.print_payload(cmms)
     infra.verify_source(compiled_models, accel_type)
 
