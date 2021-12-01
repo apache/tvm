34d25619fe8de0783c92ab8d4a5e839543d60ba0
Matthew Barrett <55580676+mbaret@users.noreply.github.com>
2021-12-01 13:00:34 +0000

    [microNPU][1] Add affine analysis structures for the cascader (#9458)
    * [ETHOSU][1] Add affine analysis structures for the cascader
    
    The cascader relies heavily on being able to determine
    data dependencies between operators. This is so that it
    can calculate how stripes should be propagated through a
    cascade.
    
    To do this, two data structures are defined: StripeConfig
    and Propagator. StripeConfig stores information for how a
    tensor should be broken up into stripes and executed.
    Propagator transforms a StripeConfig using an affine
    transform matrix, allowing an input StripeConfig for an
    operator to be determined by 'propagating' the output
    StripeConfig.
    
    By chaining together Propagators, we can analyse how
    data dependencies vary throughout a cascade and therefore
    calculate the memory requirements (and approximate the
    performance).
    
    Change-Id: If7176fea961c631be4a6c195303da536030d957b
    
    * Add test guards
    
    Change-Id: I1d7633e20daab33642fa5c4a12e474a4def4d8b8
    
    * Address review comments
    
    Change-Id: Iff5f1effa08e0628de91f5577487d0cecebec824
    
    * Improve docs
    
    Change-Id: I508809d8c1a08d231e3a9b0fd9b3f2639cc2f0e3

--- python/tvm/contrib/ethosu/cascader/_ffi_api.py
+++ python/tvm/contrib/ethosu/cascader/_ffi_api.py
@@ -14,10 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""FFI APIs for the NPU cascader."""
+import tvm._ffi
 
-if(USE_ETHOSU)
-  file(GLOB COMPILER_ETHOSU_SRCS
-       CONFIGURE_DEPENDS src/relay/backend/contrib/ethosu/*
-       CONFIGURE_DEPENDS src/contrib/ethosu/cascader/*)
-  list(APPEND COMPILER_SRCS ${COMPILER_ETHOSU_SRCS})
-endif(USE_ETHOSU)
+
+tvm._ffi._init_api("contrib.ethosu.cascader", __name__)
