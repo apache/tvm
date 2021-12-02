9bf946fad0826d77fa0ca07fa5c947f157463637
CircleSpin <jshiue@octoml.ai>
2021-12-02 16:24:30 -0500

    Update gallery/how_to/use_tvms_python_api/tvmc_python.py
    Co-authored-by: Leandro Nunes <leandro.nunes@arm.com>

--- gallery/how_to/use_tvms_python_api/tvmc_python.py
+++ gallery/how_to/use_tvms_python_api/tvmc_python.py
@@ -38,7 +38,7 @@ Hi! Here we explain the scripting tool designed for the complete TVM beginner. ð
 # --------------------
 
 # Let's import our model into tvmc. This step converts a machine learning model from 
-# a supported framework into tvm's high level graph representation language called relay. 
+# a supported framework into TVM's high level graph representation language called relay. 
 # This is to have a unified starting point for all models in tvm. The frameworks we currently 
 # support are: Keras, Onnx, Tensorflow, TFLite, and Pytorch.
 # 
