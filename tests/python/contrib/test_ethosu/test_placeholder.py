6a58c9b6ed62c1744206a3644df729b02eed0985
Manupa Karunaratne <manupa.karunaratne@arm.com>
2021-12-02 02:05:01 +0000

    [CI][microNPU]Running tests parallel using pytest-xdist
    * fixing a typo
    
    Change-Id: Iba8bed3d86b6aab64ba611a67e4751fa6b6b96c7

--- tests/python/contrib/test_ethosu/test_placeholder.py
+++ tests/python/contrib/test_ethosu/test_placeholder.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""This files contain a placeholder test that always run"""
+"""This file contains a placeholder test that always run"""
 
 
 def test_placeholder():
