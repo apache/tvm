1950d8b73f4bf6ede37dfe2d728f50d032a80422
Andrew Reusch <areusch@octoml.ai>
2021-12-08 15:18:28 -0800

    Add a runtime Module to mux between .text Metadata and live Metadata.

--- src/runtime/meta_data.h
+++ src/runtime/meta_data.h
@@ -36,6 +36,8 @@
 #include <utility>
 #include <vector>
 
+#include <tvm/runtime/metadata.h>
+
 #include "runtime_base.h"
 
 namespace tvm {
@@ -48,6 +50,15 @@ inline String get_name_mangled(const String& module_name, const String& name) {
   ss << module_name << "_" << name;
   return ss.str();
 }
+
+/*!
+ * \brief Create a metadata module object.
+ *
+ * \param metadata Exported metadata structure.
+ *
+ * \return The created metadata module.
+ */
+Module MetadataModuleCreate(metadata::Metadata metadata);
 
 /*! \brief A tag to specify whether or not dynamic shared memory is used */
 constexpr const char* kUseDynamicSharedMemoryTag = "tir.use_dyn_shared_memory";
