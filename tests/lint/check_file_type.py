f2c7eb050ef9b3fc9fc73e14c3d4548b0178d067
Mehrdad Hessar <mhessar@octoml.ai>
2021-12-08 10:43:01 -0800

    [Hexagon] Add RPC Mechanism for Hexagon (#9631)
    * Add Hexagon RPC
    
    * removed android remote and updated Readme
    
    * Add check for workspace size
    
    * Make libtvm_runtime consistent for Android
    
    * Remove root access
    
    * Fix some docstrings
    
    * Make stack remote size as parameter
    
    * add documentation
    
    * Refactor test conftest
    
    * clang format
    
    * Decoupled USE_HEXAGON_RPC
    
    * fix creation of test base directory on android
    
    * Address global variable
    
    * Fix format and Cleanup cmake
    
    * Fix build for other targets

--- tests/lint/check_file_type.py
+++ tests/lint/check_file_type.py
@@ -150,6 +150,8 @@ ALLOW_SPECIFIC_FILE = {
     "apps/microtvm/reference-vm/arduino/base-box/Vagrantfile.packer-template",
     "apps/microtvm/reference-vm/zephyr/Vagrantfile",
     "apps/microtvm/reference-vm/zephyr/base-box/Vagrantfile.packer-template",
+    # Hexagon
+    "src/runtime/hexagon/rpc/android_bash.sh.template",
 }
 
 
--- tests/python/contrib/test_hexagon/proxy_rpc/test_matmul.py
+++ tests/python/contrib/test_hexagon/proxy_rpc/test_matmul.py
@@ -16,104 +16,18 @@
 # under the License.
 
 import os
-import sys
 
 import tvm
 import tvm.testing
 from tvm import te
-import tvm.contrib.hexagon as hexagon
+import tvm.contrib.hexagon.hexagon as hexagon
 from tvm.contrib import utils
-from tvm import rpc
 import numpy as np
 
-import pytest
-
-HEXAGON_TOOLCHAIN = "HEXAGON_TOOLCHAIN"
-TVM_TRACKER_HOST = "TVM_TRACKER_HOST"
-TVM_TRACKER_PORT = "TVM_TRACKER_PORT"
-ANDROID_TRACKER_KEY = "ANDROID_TRACKER_KEY"
-ANDROID_REMOTE_DIR = "ANDROID_REMOTE_DIR"
+from ..conftest import requires_hexagon_toolchain, requires_rpc_tracker_and_android_key
 
 
-def _compose(args, decs):
-    """Helper to apply multiple markers"""
-    if len(args) > 0:
-        f = args[0]
-        for d in reversed(decs):
-            f = d(f)
-        return f
-    return decs
-
-
-def requires_hexagon_toolchain(*args):
-    _requires_rpc_tracker = [
-        *tvm.testing.requires_rpc(),
-        pytest.mark.skipif(
-            os.environ.get(HEXAGON_TOOLCHAIN) == None,
-            reason="HEXAGON_TOOLCHAIN environment variable is required to run Hexagon proxy rpc tests",
-        ),
-    ]
-
-    return _compose(args, _requires_rpc_tracker)
-
-
-def requires_rpc_tracker(*args):
-    """Mark a test as requiring an RPC tracker to exist in
-    the host environment to run."""
-    _requires_rpc_tracker = [
-        *tvm.testing.requires_rpc(),
-        pytest.mark.skipif(
-            os.environ.get(TVM_TRACKER_HOST) == None,
-            reason="Missing environment variable, TVM_TRACKER_HOST",
-        ),
-        pytest.mark.skipif(
-            os.environ.get(TVM_TRACKER_PORT) == None,
-            reason="Missing environment variable, TVM_TRACKER_PORT",
-        ),
-        pytest.mark.skipif(
-            os.environ.get(ANDROID_TRACKER_KEY) == None,
-            reason="Missing environment variable, ANDROID_TRACKER_KEY",
-        ),
-        pytest.mark.skipif(
-            os.environ.get(ANDROID_REMOTE_DIR) == None,
-            reason="Missing environment variable, ANDROID_REMOTE_DIR",
-        ),
-    ]
-
-    return _compose(args, _requires_rpc_tracker)
-
-
-@tvm.testing.fixture
-def android_tracker_key():
-    return os.environ["ANDROID_TRACKER_KEY"]
-
-
-@tvm.testing.fixture
-def tvm_tracker_host():
-    return os.environ["TVM_TRACKER_HOST"]
-
-
-@tvm.testing.fixture
-def tvm_tracker_port():
-    return int(os.environ["TVM_TRACKER_PORT"])
-
-
-@tvm.testing.fixture
-def remote_path():
-    dso_binary = "test_binary.so"
-    return os.path.join(os.environ["ANDROID_REMOTE_DIR"], dso_binary)
-
-
-@tvm.testing.fixture
-def rpc_sess(android_tracker_key, tvm_tracker_host, tvm_tracker_port):
-    from tvm import rpc
-
-    tracker = rpc.connect_tracker(tvm_tracker_host, tvm_tracker_port)
-    remote = tracker.request(android_tracker_key, priority=0, session_timeout=600)
-    return remote
-
-
-@requires_rpc_tracker
+@requires_rpc_tracker_and_android_key
 @requires_hexagon_toolchain
 class TestMatMul:
     M = tvm.testing.parameter(32)
