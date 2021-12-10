2bd16012c0c0f278dcb6fe405e2afad65caf65ae
Elen Kalda <elen.kalda@arm.com>
2021-12-10 16:38:29 +0000

    [microNPU] Add support for SPLIT and SPLIT_V (#9621)
    Both, SPLIT and SPLIT_V get lowered to relay.split and in the
    legalization the Relay split gets turned into strided slices. This
    patch adds the pattern and legalizer to enable offloading the TFLite's
    splits to the NPU.

--- tests/python/contrib/test_ethosu/test_legalize.py
+++ tests/python/contrib/test_ethosu/test_legalize.py
@@ -1344,5 +1344,163 @@ def test_tflite_sigmoid_legalize():
     assert tuple(func_body.args[1].checked_type.shape) == (256,)
 
 
+@pytest.mark.parametrize(
+    "ifm_shape, num_or_size_splits, axis",
+    [
+        ((1, 4, 6, 8), 3, 2),
+        ((4, 6, 8), 2, 0),
+        ((5, 15), 3, 1),
+        ((3, 7), 1, 1),
+        ((100,), 25, 0),
+    ],
+)
+def test_tflite_split_legalize(ifm_shape, num_or_size_splits, axis):
+    dtype = "int8"
+
+    def create_tflite_graph():
+        class Model(tf.Module):
+            @tf.function
+            def tf_function(self, x, num_or_size_splits, axis):
+                op = tf.split(x, num_or_size_splits, axis=axis)
+                return op
+
+        model = Model()
+        concrete_func = model.tf_function.get_concrete_function(
+            tf.TensorSpec(ifm_shape, tf.float32), num_or_size_splits, axis
+        )
+
+        def representative_dataset():
+            for _ in range(100):
+                data = np.random.rand(*tuple(ifm_shape))
+                yield [data.astype(np.float32)]
+
+        converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter.representative_dataset = representative_dataset
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter.inference_input_type = tf.int8
+        converter.inference_output_type = tf.int8
+        tflite_model = converter.convert()
+
+        return tflite_model
+
+    def verify(ext_func):
+        # dig out the split
+        single_output_split = num_or_size_splits == 1
+        split = (
+            ext_func.body.tuple_value
+            if single_output_split
+            else ext_func.body.args[0][0].args[0].tuple_value
+        )
+        assert split.op.name == "split"
+
+        # Split is specified by number of equal chunks
+        assert split.attrs.indices_or_sections == num_or_size_splits
+
+        assert split.attrs.axis == axis
+
+    tflite_graph = create_tflite_graph()
+    tflite_model = tflite.Model.Model.GetRootAsModel(tflite_graph, 0)
+
+    mod, _ = relay.frontend.from_tflite(
+        tflite_model,
+        shape_dict={"input": ifm_shape},
+        dtype_dict={"input": dtype},
+    )
+    mod = ethosu.partition_for_ethosu(mod)
+
+    mod["tvmgen_default_ethos_u_main_0"] = dataflow_pattern.rewrite(
+        legalize.PartitionedSplitRewriter(), mod["tvmgen_default_ethos_u_main_0"]
+    )
+
+    mod["tvmgen_default_ethos_u_main_0"] = relay.transform.InferType()(mod)[
+        "tvmgen_default_ethos_u_main_0"
+    ]
+
+    verify(mod["tvmgen_default_ethos_u_main_0"])
+
+
+@pytest.mark.parametrize(
+    "ifm_shape, num_or_size_splits, axis",
+    [
+        ((1, 4, 6, 8), (1, 3, 4), 3),
+        ((10, 18, 4), (1, 4, 3, 2), 0),
+        ((22, 7), (4, -1), 1),
+        ((25,), (25,), 0),
+    ],
+)
+def test_tflite_split_v_legalize(ifm_shape, num_or_size_splits, axis):
+    dtype = "int8"
+
+    def create_tflite_graph():
+        class Model(tf.Module):
+            @tf.function
+            def tf_function(self, x, num_or_size_splits, axis):
+                # TF split gets converted into TFLite's split_v
+                op = tf.split(x, num_or_size_splits, axis=axis)
+                return op
+
+        model = Model()
+        concrete_func = model.tf_function.get_concrete_function(
+            tf.TensorSpec(ifm_shape, tf.float32), num_or_size_splits, axis
+        )
+
+        def representative_dataset():
+            for _ in range(100):
+                data = np.random.rand(*tuple(ifm_shape))
+                yield [data.astype(np.float32)]
+
+        converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        converter.representative_dataset = representative_dataset
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+        converter.inference_input_type = tf.int8
+        converter.inference_output_type = tf.int8
+        tflite_model = converter.convert()
+
+        return tflite_model
+
+    def verify(ext_func):
+        # dig out the split
+        single_output_split = len(num_or_size_splits) == 1
+        split = (
+            ext_func.body.tuple_value
+            if single_output_split
+            else ext_func.body.args[0][0].args[0].tuple_value
+        )
+        assert split.op.name == "split"
+
+        # Split is specified by the size of sections, so converting num_or_size_splits
+        # into the indices where the tensor is split at since this is how split is represented
+        # in Relay
+        split_sections = [] if single_output_split else [num_or_size_splits[0]]
+        for split_size in num_or_size_splits[1:-1]:
+            sec = split_sections[-1] + split_size
+            split_sections.append(sec)
+        assert list(split.attrs.indices_or_sections) == split_sections
+
+        assert split.attrs.axis == axis
+
+    tflite_graph = create_tflite_graph()
+    tflite_model = tflite.Model.Model.GetRootAsModel(tflite_graph, 0)
+
+    mod, _ = relay.frontend.from_tflite(
+        tflite_model,
+        shape_dict={"input": ifm_shape},
+        dtype_dict={"input": dtype},
+    )
+    mod = ethosu.partition_for_ethosu(mod)
+
+    mod["tvmgen_default_ethos_u_main_0"] = dataflow_pattern.rewrite(
+        legalize.PartitionedSplitRewriter(), mod["tvmgen_default_ethos_u_main_0"]
+    )
+
+    mod["tvmgen_default_ethos_u_main_0"] = relay.transform.InferType()(mod)[
+        "tvmgen_default_ethos_u_main_0"
+    ]
+
+    verify(mod["tvmgen_default_ethos_u_main_0"])
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
