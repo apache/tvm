5d47efff24aaa25d29a22d5f892e1a9ed6d97373
Wuwei Lin <wuwei@apache.org>
2021-12-15 00:28:12 -0500

    [TIR] Add 'global_symbol' and 'tir.noalias' as default attributes in script auto completion (#9744)
    * [TVMScript] Update default TIR prefix to T
    
    * [TIR] Add 'global_symbol' and 'tir.noalias' as default attributes in script auto completion
    
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>
    Co-authored-by: Xiyou Zhou <xiyou@octoml.ai>
    
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Xiyou Zhou <xiyou@octoml.ai>

--- src/printer/text_printer.h
+++ src/printer/text_printer.h
@@ -409,7 +409,7 @@ class TIRTextPrinter : public StmtFunctor<Doc(const Stmt&)>,
   Doc PrintBody(const Stmt& body, bool indent = true);
 };
 
-String AsTVMScript(const ObjectRef& mod, const String& tir_prefix = "tir", bool show_meta = false);
+String AsTVMScript(const ObjectRef& mod, const String& tir_prefix = "T", bool show_meta = false);
 
 String AsTVMScriptWithDiagnostic(const ObjectRef& mod, const String& tir_prefix, bool show_meta,
                                  runtime::TypedPackedFunc<std::string(Stmt)> annotate);
