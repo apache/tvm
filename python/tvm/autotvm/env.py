9bebac1fa2430297876722d0760900c1260faec2
pansn1995 <62982030+pansn1995@users.noreply.github.com>
2021-12-18 17:10:52 +0800

    Update env.py

--- python/tvm/autotvm/env.py
+++ python/tvm/autotvm/env.py
@@ -41,4 +41,4 @@ def reset_global_scope(global_scope):
     """Reset global autotvm state. This is needed to initialize PopenPool workers."""
     global GLOBAL_SCOPE
     GLOBAL_SCOPE.deepcopy(global_scope)
-    AutotvmGlobalScope.current = GLOBAL_SCOPE
+    AutotvmGlobalScope.current = global_scope
