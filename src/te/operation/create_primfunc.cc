8352f2fc0dfe3062b9cbbefba9119e4b650a50bb
Siyuan Feng <Hzfengsy@sjtu.edu.cn>
2021-12-20 10:14:36 +0800

    [TE][TensorIR] fix tensor attr in create_prim_func (#9764)
    * [TE][TensorIR] fix tensor attr in create_prim_func
    
    * Update src/te/operation/create_primfunc.cc
    
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>

--- src/te/operation/create_primfunc.cc
+++ src/te/operation/create_primfunc.cc
@@ -25,6 +25,7 @@
 #include <algorithm>
 #include <unordered_set>
 
+#include "../../tir/ir/functor_common.h"
 #include "../schedule/graph.h"
 
 namespace tvm {
@@ -144,7 +145,27 @@ BlockRealize GenerateBlockFromTensor(const te::ComputeOp& compute_op, const te::
   }
 
   // Step 6. Add script_parsing_detect_access attr for auto complete the whole IR.
-  Map<String, ObjectRef> annotations = compute_op->attrs;
+  Map<String, ObjectRef> annotations;
+  auto mutate_attr = [&info](const ObjectRef& value) -> ObjectRef {
+    if (const auto* tensor_value = value.as<te::TensorNode>()) {
+      return info->tensor2buffers.at(GetRef<te::Tensor>(tensor_value));
+    } else {
+      return value;
+    }
+  };
+
+  for (const auto& pair : compute_op->attrs) {
+    const String& key = pair.first;
+    const ObjectRef& value = pair.second;
+    // TensorIR will not allow Tensor data structure
+    if (value->IsInstance<ArrayNode>()) {
+      const auto array_value = Downcast<Array<ObjectRef>>(value);
+      annotations.Set(key, MutateArray(array_value, mutate_attr));
+    } else {
+      annotations.Set(key, mutate_attr(value));
+    }
+  }
+  // Set script_parsing_detect_access
   annotations.Set(tir::attr::script_parsing_detect_access, IntImm(DataType::Int(32), 3));
 
   // Step 7. Create Block and BlockRealize.
