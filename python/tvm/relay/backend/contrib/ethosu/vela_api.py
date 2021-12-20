55567271d37526281d630d6f34bf983b808ea5ed
Jacob Bohlin <jacob.bohlin@arm.com>
2021-12-20 18:53:50 +0000

    [microNPU] Added checks for out of range shifts (#9707)
    * [microNPU] Added checks for out of range shifts
    
    * Added testcase
    
    * Addressed comments

--- python/tvm/relay/backend/contrib/ethosu/vela_api.py
+++ python/tvm/relay/backend/contrib/ethosu/vela_api.py
@@ -316,7 +316,10 @@ def _quantize_scale(scale: float) -> Tuple[int, int]:
     mantissa_scaled = mantissa * (1 << 31)
     mantissa_scaled = int(util.round_away_zero(mantissa_scaled))
     required_shift = 31 - exponent
-    assert 0 <= required_shift < (1 << 6)
+    if required_shift < 0 or required_shift >= (1 << 6):
+        # Shift outside of valid range, set scale to 0
+        return 0, 16
+
     return mantissa_scaled, required_shift
 
 
@@ -332,6 +335,11 @@ def _reduced_quantize_scale(scale: float) -> Tuple[int, int]:
     else:
         reduced_mantissa_scaled = (mantissa_scaled + (1 << 15)) >> 16
     reduced_shift = required_shift - 16
+
+    if required_shift < 0 or required_shift >= (1 << 6):
+        # Shift outside of valid range, set scale to 0
+        return 0, 16
+
     return reduced_mantissa_scaled, reduced_shift
 
 
