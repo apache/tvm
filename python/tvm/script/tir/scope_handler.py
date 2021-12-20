40d97188b0330c8d88f1aee9fae78b3b8cf8892b
wrongtest <wrongtest0@gmail.com>
2021-12-20 18:47:17 +0800

    [TIRScript] fix parse StringImm value in for loop annotations (#9755)
    * fix parse strimm value in for annotations
    
    * flatten buffer allow runtime.String attr value
    
    * remove unused import
    
    * rebase and ensure flattened attr order

--- python/tvm/script/tir/scope_handler.py
+++ python/tvm/script/tir/scope_handler.py
@@ -20,7 +20,7 @@ from typing import Tuple, Any, Callable, Optional, List, Union, Mapping
 
 import synr
 import tvm.tir
-from tvm.runtime import Object, String
+from tvm.runtime import Object
 from tvm.ir import Span, Range
 from tvm.tir import Stmt, PrimExpr, IterVar, Var, Buffer, BufferRegion, ForKind
 
@@ -483,14 +483,8 @@ class ForScopeHandler(ScopeHandler):
         """
         assert self.context and self.node, "call 'exit_scope' before 'enter_scope'"
         extent = end if begin == 0 else self.context.analyzer.simplify(end - begin)
-        self.annotations: Mapping[str, Object] = {}
-        if annotations is not None:
-            self.annotations = {
-                key: String(val) if isinstance(val, str) else val
-                for key, val in annotations.items()
-            }
-
-        self.loop_info.append(LoopInfo(begin, extent, kind, thread_binding, annotations))
+        self.annotations = annotations
+        self.loop_info.append(LoopInfo(begin, extent, kind, thread_binding, self.annotations))
 
 
 @register
