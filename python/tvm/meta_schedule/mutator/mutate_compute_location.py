985f4ccb217729dd287f4dcc63fc422f37ad366a
Siyuan Feng <Hzfengsy@sjtu.edu.cn>
2022-01-23 18:47:34 +0800

    [MetaSchedule] Mutator: Mutate compute location (#10028)
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Xiyou Zhou <xiyou@octoml.ai>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>
    
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Xiyou Zhou <xiyou@octoml.ai>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>

--- python/tvm/meta_schedule/mutator/mutate_compute_location.py
+++ python/tvm/meta_schedule/mutator/mutate_compute_location.py
@@ -14,10 +14,18 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""
-The tvm.meta_schedule.mutator package.
-Meta Schedule mutator that mutates the trace to explore the
-design space.
-"""
-from .mutator import Mutator, PyMutator
-from .mutate_compute_location import MutateComputeLocation
+"""A mutator that mutates the compute-at location decision of SampleComputeLocation"""
+from tvm._ffi.registry import register_object
+
+from .. import _ffi_api
+from .mutator import Mutator
+
+
+@register_object("meta_schedule.MutateComputeLocation")
+class MutateComputeLocation(Mutator):
+    """A mutator that mutates the compute-at location decision of SampleComputeLocation"""
+
+    def __init__(self) -> None:
+        self.__init_handle_by_constructor__(
+            _ffi_api.MutatorMutateComputeLocation,  # type: ignore # pylint: disable=no-member
+        )
