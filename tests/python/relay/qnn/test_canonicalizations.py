e60f2b4dd418697b15a44d73ebcfd24e5b7d916a
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-01-24 13:33:35 -0800

    clean up existing tests

--- tests/python/relay/qnn/test_canonicalizations.py
+++ tests/python/relay/qnn/test_canonicalizations.py
@@ -7,9 +7,6 @@ from tvm.relay.qnn.op import canonicalizations
 class TestIntegerTableLookupTable:
     """Consists of tests testing functionality of creating lookup tables for integer operations."""
 
-    # def __init__(self) -> None:
-    #     self.input = np.arange(start=0, stop=256, dtype="uint8")
-
     def fake_identity_func_numpy(self, arr: np.ndarray):
         return arr.astype("float32")
 
@@ -47,40 +44,85 @@ class TestIntegerTableLookupTable:
     def dequantize_numpy(self, np_arr, np_scale=1.0, np_zero_point=0):
         return (np_arr.astype("int32") - np_zero_point) * np_scale
 
-    def test_int8_to_int8(self):
-        relay_lookup, input_arg = self.fake_identity_func_relay(in_dtype="int8", out_dtype="int8")
+    def run_identity_function_test(
+        self,
+        in_scale: float,
+        in_zero_point: int,
+        out_scale: float,
+        out_zero_point: int,
+        in_dtype: str,
+        out_dtype: str,
+        rtol=1e-7,
+        atol=0,
+    ):
+        relay_lookup, input_arg = self.fake_identity_func_relay(
+            in_scale=relay.const(in_scale, "float32"),
+            in_zero_point=relay.const(in_zero_point, "int32"),
+            out_scale=relay.const(out_scale, "float32"),
+            out_zero_point=relay.const(out_zero_point, "int32"),
+            in_dtype=in_dtype,
+            out_dtype=out_dtype,
+        )
         result = canonicalizations.run_const_expr(relay_lookup)
-        assert np.allclose(self.dequantize_numpy(input_arg), self.dequantize_numpy(result))
+        np.testing.assert_allclose(
+            self.dequantize_numpy(input_arg, np_scale=in_scale, np_zero_point=in_zero_point),
+            self.dequantize_numpy(result, np_scale=out_scale, np_zero_point=out_zero_point),
+            atol=atol,
+            rtol=rtol,
+        )
+
+    def test_int8_to_int8(self):
+        """Test int8 input to int8 output mapping workings"""
+        self.run_identity_function_test(
+            in_scale=1.0,
+            in_zero_point=0,
+            out_scale=1.0,
+            out_zero_point=0,
+            in_dtype="int8",
+            out_dtype="int8",
+        )
 
     def test_uint8_to_uint8(self):
-        relay_lookup, input_arg = self.fake_identity_func_relay(in_dtype="uint8", out_dtype="uint8")
-        result = canonicalizations.run_const_expr(relay_lookup)
-        assert np.allclose(self.dequantize_numpy(input_arg), self.dequantize_numpy(result))
+        self.run_identity_function_test(
+            in_scale=1.0,
+            in_zero_point=128,
+            out_scale=1.0,
+            out_zero_point=128,
+            in_dtype="uint8",
+            out_dtype="uint8",
+        )
 
     def test_int8_to_uint8(self):
-        relay_lookup, input_arg = self.fake_identity_func_relay(
-            out_scale=relay.const(1.0, dtype="float32"),
-            out_zero_point=relay.const(128, dtype="int32"),
+        self.run_identity_function_test(
+            in_scale=1.0,
+            in_zero_point=0,
+            out_scale=1.0,
+            out_zero_point=128,
             in_dtype="int8",
             out_dtype="uint8",
         )
-        result = canonicalizations.run_const_expr(relay_lookup)
-        assert np.allclose(
-            self.dequantize_numpy(input_arg),
-            self.dequantize_numpy(result, np_scale=1.0, np_zero_point=128),
-        )
 
     def test_uint8_to_int8(self):
-        relay_lookup, input_arg = self.fake_identity_func_relay(
-            in_scale=relay.const(1.0, dtype="float32"),
-            in_zero_point=relay.const(128, dtype="int32"),
+        self.run_identity_function_test(
+            in_scale=1.0,
+            in_zero_point=128,
+            out_scale=1.0,
+            out_zero_point=0,
             in_dtype="uint8",
             out_dtype="int8",
         )
-        result = canonicalizations.run_const_expr(relay_lookup)
-        assert np.allclose(
-            self.dequantize_numpy(input_arg, np_scale=1.0, np_zero_point=128),
-            self.dequantize_numpy(result),
+
+    def test_different_in_out_qparams(self):
+        """Test mapping with different in/out qparams works."""
+        self.run_identity_function_test(
+            in_scale=1.0,
+            in_zero_point=128,
+            out_scale=1.0,
+            out_zero_point=128,
+            in_dtype="uint8",
+            out_dtype="uint8",
+            atol=1,  # numbers range from -128 -> 128 so not that big error
+            rtol=0,
         )
 
 
