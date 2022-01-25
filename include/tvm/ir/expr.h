42107f55188a2a9eefa532b4f332606f4777abe5
Alexey Voronov <avoronov.icemist@gmail.com>
2022-01-25 13:58:16 +0300

    Fix warning "unsafe mix of type 'const int64_t' and type 'bool' in operation" occurring in tvm::tir::HasAnn

--- include/tvm/ir/expr.h
+++ include/tvm/ir/expr.h
@@ -374,6 +374,12 @@ inline Bool operator&&(const Bool& a, const Bool& b) {
   return Bool(a.operator bool() && b.operator bool());
 }
 
+inline Bool operator==(const Bool& a, bool b) { return Bool(a.operator bool() == b); }
+inline Bool operator==(bool a, const Bool& b) { return Bool(a == b.operator bool()); }
+inline Bool operator==(const Bool& a, const Bool& b) {
+  return Bool(a.operator bool() == b.operator bool());
+}
+
 /*!
  * \brief Container of constant int that adds more constructors.
  *
