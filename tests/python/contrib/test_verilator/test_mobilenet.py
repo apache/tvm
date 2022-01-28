9ebc3a6bb4be85e22695d1db4f18a9979210797e
Michal Piszczek <imichaljp@gmail.com>
2022-01-28 13:45:34 -0800

    -mFix skipping of verilator tests

--- tests/python/contrib/test_verilator/test_mobilenet.py
+++ tests/python/contrib/test_verilator/test_mobilenet.py
@@ -215,7 +215,6 @@ def is_tflite_available():
         return False
 
 
-@pytest.mark.skipif(skip_test(), reason="Skip because Verilator codegen is not available")
 def tmobilenet(lanes):
     """Mobilenet test template.
     Paramters
@@ -223,8 +222,6 @@ def tmobilenet(lanes):
     lanes : Int
         The number of vector lanes.
     """
-    if skip_test():
-        return
     if not is_tflite_available():
         return
     model = get_mobilenet_model()
@@ -239,6 +236,7 @@ def tmobilenet(lanes):
     print_test_info(lanes, values["cycle_counter"])
 
 
+@pytest.mark.skipif(skip_test(), reason="Skip because Verilator codegen is not available")
 def test_mobilenet():
     """Mobilenet tests."""
     tmobilenet(4)
--- tests/python/contrib/test_verilator/test_verilator_ops.py
+++ tests/python/contrib/test_verilator/test_verilator_ops.py
@@ -139,7 +139,6 @@ def print_test_info(test, lanes, cycles):
     print("test:{} vector-lanes:{} number of cycles:{}".format(test, lanes, cycles))
 
 
-@pytest.mark.skipif(skip_test(), reason="Skip because Verilator codegen is not available")
 def tadd(lanes):
     """Print counter
 
@@ -148,8 +147,6 @@ def tadd(lanes):
     lanes : Int
         The number of vector lanes.
     """
-    if skip_test():
-        return
     dtype = "int32"
     shape = (8, 4)
     mod = create_module_add(shape, dtype)
@@ -160,7 +157,6 @@ def tadd(lanes):
     print_test_info("add", lanes, cycles)
 
 
-@pytest.mark.skipif(skip_test(), reason="Skip because Verilator codegen is not available")
 def tbias(lanes):
     """Print counter
 
@@ -169,8 +165,6 @@ def tbias(lanes):
     lanes : Int
         The number of vector lanes.
     """
-    if skip_test():
-        return
     dtype = "int32"
     xshape = (1, 112, 112, 32)
     yshape = (32,)
@@ -182,12 +176,14 @@ def tbias(lanes):
     print_test_info("nn.bias_add", lanes, cycles)
 
 
+@pytest.mark.skipif(skip_test(), reason="Skip because Verilator codegen is not available")
 def test_add():
     """add tests."""
     tadd(1)
     tadd(4)
 
 
+@pytest.mark.skipif(skip_test(), reason="Skip because Verilator codegen is not available")
 def test_bias_add():
     """bias_add tests."""
     tbias(1)
