6e86ed38c8f58289182bd6178581ca28ef0eef0d
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-01-27 18:13:19 -0800

    add tests to qnn

--- tests/python/relay/test_op_qnn_unary_elementwise.py
+++ tests/python/relay/test_op_qnn_unary_elementwise.py
@@ -19,6 +19,7 @@ from typing import Callable, List
 
 import numpy as np
 import pytest
+import scipy.special
 import tvm
 from tvm import relay
 
@@ -28,13 +29,15 @@ def dequantize(data, scale, zp):
 
 
 def generate_golden_output(
-    floating_point_golden_func, dequantized_x, output_scale, output_zero_point
+    floating_point_golden_func, dequantized_x, output_scale, output_zero_point, dtype
 ):
     output = floating_point_golden_func(dequantized_x)
     output = np.around(output / output_scale + output_zero_point)
 
-    q_min = np.iinfo(np.uint8).min
-    q_max = np.iinfo(np.uint8).max
+    np_dtype = {"int8": np.int8, "uint8": np.uint8}[dtype]
+
+    q_min = np.iinfo(np_dtype).min
+    q_max = np.iinfo(np_dtype).max
     return np.clip(output, q_min, q_max)
 
 
@@ -51,13 +54,14 @@ def run_qnn_func(func: relay.Function, args: List[relay.Expr]):
 
 def create_qnn_func(
     qnn_op: Callable[[relay.Expr, relay.Expr, relay.Expr, relay.Expr, relay.Expr], relay.Call],
+    x_data: np.ndarray,
     input_scale: float,
     input_zero_point: int,
     output_scale: float,
     output_zero_point: int,
     input_dtype: str = "uint8",
 ):
-    x = relay.var("x", shape=(1, 4), dtype=input_dtype)
+    x = relay.var("x", shape=x_data.shape, dtype=input_dtype)
     y = qnn_op(
         x=x,
         scale=relay.const(input_scale, "float32"),
@@ -80,6 +84,7 @@ def run_condition(
 ):
     func = create_qnn_func(
         qnn_op,
+        x_data,
         input_scale=input_scale,
         input_zero_point=input_zero_point,
         output_scale=output_scale,
@@ -87,15 +92,36 @@ def run_condition(
         input_dtype=input_dtype,
     )
 
-    x_data = np.array((255, 133, 0, 9)).reshape((1, 4))
     x_dequantized = dequantize(x_data, input_scale, input_zero_point)
     golden_output = generate_golden_output(
-        floating_point_golden_func, x_dequantized, output_scale, output_zero_point
+        floating_point_golden_func,
+        x_dequantized,
+        output_scale,
+        output_zero_point,
+        dtype=input_dtype,
     )
 
     op_res = run_qnn_func(func, [x_data])
+    np.testing.assert_equal(op_res, golden_output.astype(input_dtype))
 
-    np.testing.assert_equal(op_res, np.uint8(golden_output))
+
+def generic_test(
+    qnn_op: Callable[[relay.Expr, relay.Expr, relay.Expr, relay.Expr, relay.Expr], relay.Call],
+    floating_point_golden_func: Callable[[np.ndarray], np.ndarray],
+    input_dtype: str = "uint8",
+    x_data: np.ndarray = np.arange(0, 256, dtype="uint8"),
+):
+    x_data = x_data.view(input_dtype)
+    return run_condition(
+        qnn_op,
+        floating_point_golden_func,
+        x_data,
+        input_scale=0.125,
+        input_zero_point=0,
+        output_scale=0.125,
+        output_zero_point=0,
+        input_dtype=input_dtype,
+    )
 
 
 class TestRSqrt:
@@ -124,6 +150,62 @@ class TestRSqrt:
             output_zero_point=0,
             input_dtype="uint8",
         )
+
+    def test_all_numbers_uint8(self):
+        generic_test(relay.qnn.op.rsqrt, lambda x: 1 / np.sqrt(x), input_dtype="uint8")
+
+    def test_all_numbers_int8(self):
+        generic_test(
+            relay.qnn.op.rsqrt,
+            lambda x: 1 / np.sqrt(x),
+            input_dtype="int8",
+            x_data=np.arange(1, 128, dtype="int8"),
+        )
+
+
+class Sqrt:
+    def test_all_numbers_uint8(self):
+        generic_test(relay.qnn.op.sqrt, np.sqrt, input_dtype="uint8")
+
+    def test_all_numbers_int8(self):
+        generic_test(
+            relay.qnn.op.sqrt,
+            np.sqrt,
+            input_dtype="int8",
+            x_data=np.arange(1, 128, dtype="int8"),
+        )
+
+
+class TestExp:
+    def test_all_numbers_uint8(self):
+        generic_test(relay.qnn.op.exp, np.exp, input_dtype="uint8")
+
+    def test_all_numbers_int8(self):
+        generic_test(relay.qnn.op.exp, np.exp, input_dtype="int8")
+
+
+class TestTanh:
+    def test_all_numbers_uint8(self):
+        generic_test(relay.qnn.op.tanh, np.tanh, input_dtype="uint8")
+
+    def test_all_numbers_int8(self):
+        generic_test(relay.qnn.op.tanh, np.tanh, input_dtype="int8")
+
+
+class TestErf:
+    def test_all_numbers_uint8(self):
+        generic_test(relay.qnn.op.erf, scipy.special.erf, input_dtype="uint8")
+
+    def test_all_numbers_int8(self):
+        generic_test(relay.qnn.op.erf, scipy.special.erf, input_dtype="int8")
+
+
+class TestSigmoid:
+    def test_all_numbers_uint8(self):
+        generic_test(relay.qnn.op.sigmoid, lambda x: 1 / (1 + np.exp(-x)), input_dtype="uint8")
+
+    def test_all_numbers_int8(self):
+        generic_test(relay.qnn.op.sigmoid, lambda x: 1 / (1 + np.exp(-x)), input_dtype="int8")
 
 
 if __name__ == "__main__":
