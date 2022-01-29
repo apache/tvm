538347e49f89d345b48d68382a15fa5ffca698b2
Siyuan Feng <Hzfengsy@sjtu.edu.cn>
2022-01-30 05:59:09 +0800

    [MetaSchedule] postproc: rewrite_cooperative_fetch (#10081)
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Xiyou Zhou <xiyou@octoml.ai>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>
    
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Xiyou Zhou <xiyou@octoml.ai>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>

--- python/tvm/meta_schedule/postproc/rewrite_cooperative_fetch.py
+++ python/tvm/meta_schedule/postproc/rewrite_cooperative_fetch.py
@@ -14,11 +14,21 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""The tvm.meta_schedule.postproc package."""
-from .postproc import Postproc, PyPostproc
-from .disallow_dynamic_loop import DisallowDynamicLoop
-from .rewrite_cooperative_fetch import RewriteCooperativeFetch
-from .rewrite_parallel_vectorize_unroll import RewriteParallelVectorizeUnroll
-from .rewrite_reduction_block import RewriteReductionBlock
-from .rewrite_unbound_block import RewriteUnboundBlock
-from .verify_gpu_code import VerifyGPUCode
+"""A postprocessor that rewrites the cooperative fetch annotation to actual
+vectorized cooperative fetching in loop bindings."""
+
+from tvm._ffi.registry import register_object
+from .. import _ffi_api
+from .postproc import Postproc
+
+
+@register_object("meta_schedule.RewriteCooperativeFetch")
+class RewriteCooperativeFetch(Postproc):
+    """A postprocessor that rewrites the cooperative fetch annotation to actual vectorized
+    cooperative fetching in loop bindings.
+    """
+
+    def __init__(self) -> None:
+        self.__init_handle_by_constructor__(
+            _ffi_api.PostprocRewriteCooperativeFetch,  # type: ignore # pylint: disable=no-member
+        )
