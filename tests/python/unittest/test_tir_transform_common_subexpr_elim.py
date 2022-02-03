6f5fc37947773e5920fe29eaffad8d8d6c457301
Franck Slama <quic_fslama@quicinc.com>
2022-02-03 11:23:38 -0600

    Fixing style of the python test files

--- tests/python/unittest/test_tir_transform_common_subexpr_elim.py
+++ tests/python/unittest/test_tir_transform_common_subexpr_elim.py
@@ -33,25 +33,38 @@ def test_cse():
     # Test prog :
     # let z1=1 in let z2=2 in
     #   Mem[i1] = z1+z2;
-    #   let x = 1 in let y = 1 in 
+    #   let x = 1 in let y = 1 in
     #     let a = (x+y) + (z1+z2) in
     #       let b = (x+y) + z3 in
     #         Mem[i2] = a+b;
-    body = tvm.tir.LetStmt(z1, 1, 
-                            tvm.tir.LetStmt(z2, 2, 
-                                            tvm.tir.SeqStmt([tvm.tir.Store(buffer.data, z1+z2, i1),
-                                                            tvm.tir.LetStmt(x, 1, 
-                                                                            tvm.tir.LetStmt(y, 1, 
-                                                                                            tvm.tir.LetStmt(a, (x+y) + (z1+z2), 
-                                                                                                            tvm.tir.LetStmt(b, (x+y) + z3, 
-                                                                                                            tvm.tir.Store(buffer.data, a+b, i2)
-                                                                                                          )
-                                                                                            )
-                                                                              )
-                                                                            )
-                                                          ])
-                                            )
-                            )
+    body = tvm.tir.LetStmt(
+        z1,
+        1,
+        tvm.tir.LetStmt(
+            z2,
+            2,
+            tvm.tir.SeqStmt(
+                [
+                    tvm.tir.Store(buffer.data, z1+z2, i1),
+                    tvm.tir.LetStmt(
+                        x,
+                        1,
+                        tvm.tir.LetStmt(
+                            y,
+                            1,
+                            tvm.tir.LetStmt(
+                                a,
+                                (x+y) + (z1+z2),
+                                tvm.tir.LetStmt(
+                                    b, (x+y) + z3, tvm.tir.Store(buffer.data, a+b, i2)
+                                ),
+                            ),
+                        ),
+                    ),
+                ]
+            ),
+        ),
+    )
     # This test program gives the opportunity to introduce two new variables, at two different levels
     # and to perform replacements in the value of "a" and "b", using these new variables
     # We will check all of that underneath and more, making also sure that nothing else has been changed
@@ -61,7 +74,7 @@ def test_cse():
 
     tvm.transform.PrintIR()(body)
 
-    body = body["main"].body # Gets the body of the main, i.e. the full statement
+    body = body["main"].body  # Gets the body of the main, i.e. the full statement
 
     assert body.var.name == "z1"
     assert body.value == 1
@@ -76,9 +89,9 @@ def test_cse():
     body = body.body
 
     # And this is the name and value of this variable
-    cse_var_1 = body.var # Keep the variable accessible for later checking the replacements
+    cse_var_1 = body.var  # Keep the variable accessible for later checking the replacements
     assert body.var.name == "cse_var_1"
-    assert tvm.ir.structural_equal(body.value, z1+z2)
+    assert tvm.ir.structural_equal(body.value, z1 + z2)
     assert isinstance(body.body, tvm.tir.SeqStmt)
 
     body = body.body
@@ -101,21 +114,21 @@ def test_cse():
     body = body.body
 
     # And this is the name and value of this variable
-    cse_var_2 = body.var # Keep the variable accessible for later checking the replacements
+    cse_var_2 = body.var  # Keep the variable accessible for later checking the replacements
     assert body.var.name == "cse_var_2"
-    assert tvm.ir.structural_equal(body.value, x+y)
+    assert tvm.ir.structural_equal(body.value, x + y)
 
     body = body.body
 
     body.var.name == "a"
     # Check that the replacement has been done correctly!
-    assert tvm.ir.structural_equal(body.value, cse_var_2+cse_var_1)
+    assert tvm.ir.structural_equal(body.value, cse_var_2 + cse_var_1)
     
     body = body.body
 
     body.var.name == "b"
     # Check that the replacement has been done correctly!
-    assert tvm.ir.structural_equal(body.value, cse_var_2+z3)
+    assert tvm.ir.structural_equal(body.value, cse_var_2 + z3)
 
     assert isinstance(body.body, tvm.tir.Store)
 
@@ -124,97 +137,114 @@ def test_cse():
 # In this case, the CSE pass should introduce the redundant computation at the top if the Then branch, not before the whole If
 # (otherwise that would lead to some computations being computed for nothing when it is the Else branch that is executed).
 def test_cse_ifNode_1():
-  b = te.var("b")
-  i1 = te.var("i1")
-  i2 = te.var("i2")
-  i3 = te.var("i3")
-  y = te.var("y")
-  z = te.var("z")
-  dtype = "int32"
-  buffer = tvm.tir.decl_buffer((50,), dtype)
-  # Test prog :
-  # let b=1 in
-  #   if(b)
-  #		{
-  #		  Mem[i1] = y+z
-  #		  Mem[i2] = y+z
-  #   }
-  #   else
-  #	  {
-  #		  Mem[i3] = y
-  # 	}
-  body = tvm.tir.LetStmt(b, 1, tvm.tir.IfThenElse(b,
-                          tvm.tir.SeqStmt([tvm.tir.Store(buffer.data, y+z, i1),
-                                            tvm.tir.Store(buffer.data, y+z, i2)]),
-                          tvm.tir.Store(buffer.data, y, i3)))
+    b = te.var("b")
+    i1 = te.var("i1")
+    i2 = te.var("i2")
+    i3 = te.var("i3")
+    y = te.var("y")
+    z = te.var("z")
+    dtype = "int32"
+    buffer = tvm.tir.decl_buffer((50,), dtype)
+    # Test prog :
+    # let b=1 in
+    #   if(b)
+    #	  {
+    #	    Mem[i1] = y+z
+    #     Mem[i2] = y+z
+    #   }
+    #   else
+    #   {
+    #	    Mem[i3] = y
+    #   }
+    body = tvm.tir.LetStmt(
+        b,
+        1,
+        tvm.tir.IfThenElse(
+            b,
+            tvm.tir.SeqStmt(
+                [tvm.tir.Store(buffer.data, y + z, i1), tvm.tir.Store(buffer.data, y + z, i2)]
+            ),
+            tvm.tir.Store(buffer.data, y, i3),
+        ),
+    )
 
-  mod = tvm.IRModule.from_expr(tvm.tir.PrimFunc([i1,i2,i3,y,z], body))
-  body = tvm.tir.transform.CommonSubexprElimTIR()(mod)
+    mod = tvm.IRModule.from_expr(tvm.tir.PrimFunc([i1, i2, i3, y, z], body))
+    body = tvm.tir.transform.CommonSubexprElimTIR()(mod)
 
-  tvm.transform.PrintIR()(body)
+    tvm.transform.PrintIR()(body)
 
-  body = body["main"].body # Gets the body of the main, i.e. the full statement
+    body = body["main"].body  # Gets the body of the main, i.e. the full statement
 
-  assert body.var.name == "b"
-  assert body.value == 1
-  assert isinstance(body.body, tvm.tir.IfThenElse)
+    assert body.var.name == "b"
+    assert body.value == 1
+    assert isinstance(body.body, tvm.tir.IfThenElse)
 
-  body = body.body
+    body = body.body
 
-  assert isinstance(body.then_case, tvm.tir.LetStmt)
+    assert isinstance(body.then_case, tvm.tir.LetStmt)
 
-  body = body.then_case
+    body = body.then_case
 
-  # The let-in introduced by the CSE should appear now, inside the Then branch of the If node
-  assert body.var.name == "cse_var_1"
-  # and it should contain the expression (y+z) that was redundant
-  assert tvm.ir.structural_equal(body.value, y+z)
+    # The let-in introduced by the CSE should appear now, inside the Then branch of the If node
+    assert body.var.name == "cse_var_1"
+    # and it should contain the expression (y+z) that was redundant
+    assert tvm.ir.structural_equal(body.value, y + z)
 
 
 # Second test for if nodes : Some duplicated computations appear in both the Then and the Else branch.
 # In this case, the CSE pass should introduce the redundant computation before the whole If node, because
 # regardless of the execution path, it is going to be computed.
 def test_cse_ifNode_2():
-  b = te.var("b")
-  i1 = te.var("i1")
-  i2 = te.var("i2")
-  i3 = te.var("i3")
-  y = te.var("y")
-  z = te.var("z")
-  dtype = "int32"
-  buffer = tvm.tir.decl_buffer((50,), dtype)
-  # Test prog :
-  # let b=1 in
-  #   if(b)
-  #	  {
-  #		  Mem[i1] = y+z
-  #		  Mem[i2] = y
-  #   }
-  #   else
-  #	  {
-  #		  Mem[i3] = y+z
-  # 	}
-  body = tvm.tir.LetStmt(b, 1, tvm.tir.IfThenElse(b,
-                          tvm.tir.SeqStmt([tvm.tir.Store(buffer.data, y+z, i1), # (y+z)is present in the Then branch
-                                            tvm.tir.Store(buffer.data, y, i2)]),
-                          tvm.tir.Store(buffer.data, y+z, i3)))                 # and (y+z) is also present in the Else branch
+    b = te.var("b")
+    i1 = te.var("i1")
+    i2 = te.var("i2")
+    i3 = te.var("i3")
+    y = te.var("y")
+    z = te.var("z")
+    dtype = "int32"
+    buffer = tvm.tir.decl_buffer((50,), dtype)
+    # Test prog :
+    # let b=1 in
+    #   if(b)
+    #   {
+    #	    Mem[i1] = y+z
+    #	    Mem[i2] = y
+    #   }
+    #   else
+    #   {
+    #     Mem[i3] = y+z
+    #   }
+    body = tvm.tir.LetStmt(
+        b,
+        1,
+        tvm.tir.IfThenElse(
+            b,
+            tvm.tir.SeqStmt(
+                [
+                    tvm.tir.Store(buffer.data, y + z, i1),  # (y+z) is present in the Then branch
+                    tvm.tir.Store(buffer.data, y, i2),
+                ]
+            ),
+            tvm.tir.Store(buffer.data, y + z, i3),  # and also present in the Else branch
+        ),
+    )
 
-  mod = tvm.IRModule.from_expr(tvm.tir.PrimFunc([i1,i2,i3,y,z], body))
-  body = tvm.tir.transform.CommonSubexprElimTIR()(mod)
+    mod = tvm.IRModule.from_expr(tvm.tir.PrimFunc([i1, i2, i3, y, z], body))
+    body = tvm.tir.transform.CommonSubexprElimTIR()(mod)
 
-  tvm.transform.PrintIR()(body)
+    tvm.transform.PrintIR()(body)
 
-  body = body["main"].body # Gets the body of the main, i.e. the full statement
+    body = body["main"].body  # Gets the body of the main, i.e. the full statement
 
-  assert isinstance(body, tvm.tir.LetStmt)
+    assert isinstance(body, tvm.tir.LetStmt)
 
-  # The let-in introduced by the CSE should appear now, at the toplevel (i.e. before the If)
-  assert body.var.name == "cse_var_1"
-  # and it should contain the expression (y+z) that was redundant
-  assert tvm.ir.structural_equal(body.value, y+z)
+    # The let-in introduced by the CSE should appear now, at the toplevel (i.e. before the If)
+    assert body.var.name == "cse_var_1"
+    # and it should contain the expression (y+z) that was redundant
+    assert tvm.ir.structural_equal(body.value, y + z)
 
 
 if __name__ == "__main__":
-  test_cse()
-  test_cse_ifNode_1()
-  test_cse_ifNode_2()
+    test_cse()
+    test_cse_ifNode_1()
+    test_cse_ifNode_2()
