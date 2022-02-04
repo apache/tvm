4775f897469000799c8af3439aa70148abe51877
Eric Lunderberg <elunderberg@octoml.ai>
2022-02-04 13:28:44 -0600

    Fixup, bound checker vectorize test.

--- tests/python/unittest/test_tir_transform_instrument_bound_checkers.py
+++ tests/python/unittest/test_tir_transform_instrument_bound_checkers.py
@@ -94,7 +94,7 @@ def test_out_of_bounds_vectorize_llvm(nn, index_a, index_b):
 @tvm.testing.requires_llvm
 def test_in_bounds_vectorize_llvm():
     n = 512
-    lanes = 2
+    lanes = 1
     A = te.placeholder((n,), name="A", dtype="float32x%d" % lanes)
     B = te.compute((n,), lambda i: A[i], name="B")
     C = te.compute((n,), lambda i: B[i] + tvm.tir.const(1, A.dtype), name="C")
@@ -111,7 +111,9 @@ def test_in_bounds_vectorize_llvm():
     f = tvm.build(s, [A, C], "llvm")
     dev = tvm.cpu(0)
     # launch the kernel.
-    a = tvm.nd.empty((n,), A.dtype).copyfrom(np.random.uniform(size=(n, lanes)))
+    a = tvm.nd.empty((n,), A.dtype).copyfrom(
+        np.random.uniform(size=[n] + ([] if lanes == 1 else [lanes]))
+    )
     c = tvm.nd.empty((n,), C.dtype, dev)
     f(a, c)
     tvm.testing.assert_allclose(c.numpy(), a.numpy() + 1)
