a0969891daeac3a3ed03617d6c6a12d4b2fece3e
Eric Lunderberg <elunderberg@octoml.ai>
2022-02-09 09:16:25 -0600

    Updated test_meta_schedule_postproc_verify_gpu_code.py TIR
    Replaced Load/Store with BufferLoad/BufferStore.

--- tests/python/unittest/test_meta_schedule_postproc_verify_gpu_code.py
+++ tests/python/unittest/test_meta_schedule_postproc_verify_gpu_code.py
@@ -59,8 +59,8 @@ class Conv2dCuda0:
         blockIdx_x = T.env_thread("blockIdx.x")
         blockIdx_y = T.env_thread("blockIdx.y")
         blockIdx_z = T.env_thread("blockIdx.z")
-        A = T.match_buffer(a, [14, 14, 256, 256], dtype="float32")
-        B = T.match_buffer(b, [14, 14, 512, 256], dtype="float32")
+        A = T.match_buffer(a, [14*14*256*256], dtype="float32")
+        B = T.match_buffer(b, [14*14*512*256], dtype="float32")
         # body
         T.launch_thread(blockIdx_z, 196)
         B_local = T.allocate([64], "float32", "local")
@@ -71,17 +71,22 @@ class Conv2dCuda0:
         T.launch_thread(threadIdx_y, 8)
         T.launch_thread(threadIdx_x, 8)
         for ff_c_init, nn_c_init in T.grid(8, 8):
-            T.store(B_local, ff_c_init * 8 + nn_c_init, T.float32(0), True)
+            B_local[ff_c_init * 8 + nn_c_init] = T.float32(0)
         for rc_outer, ry, rx in T.grid(32, 3, 3):
             for ax3_inner_outer in T.serial(0, 2):
-                T.store(Apad_shared, T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4), T.if_then_else(1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15, T.load("float32x4", A.data, T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4), T.broadcast(True, 4)), T.broadcast(T.float32(0), 4), dtype="float32x4"), T.broadcast(True, 4))
+                Apad_shared[T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4)] = T.if_then_else(
+                    1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15,
+                    A[T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4)],
+                    T.broadcast(T.float32(0), 4),
+                    dtype="float32x4",
+                )
             for rc_inner in T.serial(0, 8):
                 for ax3 in T.serial(0, 8):
-                    T.store(Apad_shared_local, ax3, T.load("float32", Apad_shared, rc_inner * 64 + threadIdx_x * 8 + ax3), True)
+                    Apad_shared_local[ax3] = Apad_shared[rc_inner * 64 + threadIdx_x * 8 + ax3]
                 for ff_c, nn_c in T.grid(8, 8):
-                    T.store(B_local, ff_c * 8 + nn_c, T.load("float32", B_local, ff_c * 8 + nn_c) + T.load("float32", Apad_shared_local, nn_c), True)
+                    B_local[ff_c * 8 + nn_c] = B_local[ff_c * 8 + nn_c] + Apad_shared_local[nn_c]
         for ff_inner_inner_inner, nn_inner_inner_inner in T.grid(8, 8):
-            T.store(B.data, blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner, T.load("float32", B_local, ff_inner_inner_inner * 8 + nn_inner_inner_inner), True)# fmt: on
+            B[blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner] = B_local[ff_inner_inner_inner * 8 + nn_inner_inner_inner] # fmt: on
 
 
 @tvm.script.ir_module
@@ -96,8 +101,8 @@ class Conv2dCuda1:
         blockIdx_x = T.env_thread("blockIdx.x")
         blockIdx_y = T.env_thread("blockIdx.y")
         blockIdx_z = T.env_thread("blockIdx.z")
-        A = T.match_buffer(a, [14, 14, 256, 256], dtype="float32")
-        B = T.match_buffer(b, [14, 14, 512, 256], dtype="float32")
+        A = T.match_buffer(a, [14*14*256*256], dtype="float32")
+        B = T.match_buffer(b, [14*14*512*256], dtype="float32")
         # body
         T.launch_thread(blockIdx_z, 196)
         B_local = T.allocate([6400000], "float32", "local")
@@ -108,17 +113,22 @@ class Conv2dCuda1:
         T.launch_thread(threadIdx_y, 8)
         T.launch_thread(threadIdx_x, 8)
         for ff_c_init, nn_c_init in T.grid(8, 8):
-            T.store(B_local, ff_c_init * 8 + nn_c_init, T.float32(0), True)
+            B_local[ff_c_init * 8 + nn_c_init] = T.float32(0)
         for rc_outer, ry, rx in T.grid(32, 3, 3):
             for ax3_inner_outer in T.serial(0, 2):
-                T.store(Apad_shared, T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4), T.if_then_else(1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15, T.load("float32x4", A.data, T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4), T.broadcast(True, 4)), T.broadcast(T.float32(0), 4), dtype="float32x4"), T.broadcast(True, 4))
+                Apad_shared[T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4)] = T.if_then_else(
+                    1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15,
+                    A[T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4)],
+                    T.broadcast(T.float32(0), 4),
+                    dtype="float32x4",
+                )
             for rc_inner in T.serial(0, 8):
                 for ax3 in T.serial(0, 8):
-                    T.store(Apad_shared_local, ax3, T.load("float32", Apad_shared, rc_inner * 64 + threadIdx_x * 8 + ax3), True)
+                    Apad_shared_local[ax3] = Apad_shared[rc_inner * 64 + threadIdx_x * 8 + ax3]
                 for ff_c, nn_c in T.grid(8, 8):
-                    T.store(B_local, ff_c * 8 + nn_c, T.load("float32", B_local, ff_c * 8 + nn_c) + T.load("float32", Apad_shared_local, nn_c), True)
+                    B_local[ff_c * 8 + nn_c] = B_local[ff_c * 8 + nn_c] + Apad_shared_local[nn_c]
         for ff_inner_inner_inner, nn_inner_inner_inner in T.grid(8, 8):
-            T.store(B.data, blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner, T.load("float32", B_local, ff_inner_inner_inner * 8 + nn_inner_inner_inner), True)# fmt: on
+            B[blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner] = B_local[ff_inner_inner_inner * 8 + nn_inner_inner_inner]# fmt: on
 
 
 @tvm.script.ir_module
@@ -133,8 +143,8 @@ class Conv2dCuda2:
         blockIdx_x = T.env_thread("blockIdx.x")
         blockIdx_y = T.env_thread("blockIdx.y")
         blockIdx_z = T.env_thread("blockIdx.z")
-        A = T.match_buffer(a, [14, 14, 256, 256], dtype="float32")
-        B = T.match_buffer(b, [14, 14, 512, 256], dtype="float32")
+        A = T.match_buffer(a, [14*14*256*256], dtype="float32")
+        B = T.match_buffer(b, [14*14*512*256], dtype="float32")
         # body
         T.launch_thread(blockIdx_z, 196)
         B_local = T.allocate([64], "float32", "local")
@@ -145,17 +155,22 @@ class Conv2dCuda2:
         T.launch_thread(threadIdx_y, 8)
         T.launch_thread(threadIdx_x, 8)
         for ff_c_init, nn_c_init in T.grid(8, 8):
-            T.store(B_local, ff_c_init * 8 + nn_c_init, T.float32(0), True)
+            B_local[ff_c_init * 8 + nn_c_init] = T.float32(0)
         for rc_outer, ry, rx in T.grid(32, 3, 3):
             for ax3_inner_outer in T.serial(0, 2):
-                T.store(Apad_shared, T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4), T.if_then_else(1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15, T.load("float32x4", A.data, T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4), T.broadcast(True, 4)), T.broadcast(T.float32(0), 4), dtype="float32x4"), T.broadcast(True, 4))
+                Apad_shared[T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4)] = T.if_then_else(
+                    1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15,
+                    A[T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4)],
+                    T.broadcast(T.float32(0), 4),
+                    dtype="float32x4",
+                )
             for rc_inner in T.serial(0, 8):
                 for ax3 in T.serial(0, 8):
-                    T.store(Apad_shared_local, ax3, T.load("float32", Apad_shared, rc_inner * 64 + threadIdx_x * 8 + ax3), True)
+                    Apad_shared_local[ax3] = Apad_shared[rc_inner * 64 + threadIdx_x * 8 + ax3]
                 for ff_c, nn_c in T.grid(8, 8):
-                    T.store(B_local, ff_c * 8 + nn_c, T.load("float32", B_local, ff_c * 8 + nn_c) + T.load("float32", Apad_shared_local, nn_c), True)
+                    B_local[ff_c * 8 + nn_c] = B_local[ff_c * 8 + nn_c] + Apad_shared_local[nn_c]
         for ff_inner_inner_inner, nn_inner_inner_inner in T.grid(8, 8):
-            T.store(B.data, blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner, T.load("float32", B_local, ff_inner_inner_inner * 8 + nn_inner_inner_inner), True)# fmt: on
+            B[blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner] = B_local[ff_inner_inner_inner * 8 + nn_inner_inner_inner]# fmt: on
 
 
 @tvm.script.ir_module
@@ -170,8 +185,8 @@ class Conv2dCuda3:
         blockIdx_x = T.env_thread("blockIdx.x")
         blockIdx_y = T.env_thread("blockIdx.y")
         blockIdx_z = T.env_thread("blockIdx.z")
-        A = T.match_buffer(a, [14, 14, 256, 256], dtype="float32")
-        B = T.match_buffer(b, [14, 14, 512, 256], dtype="float32")
+        A = T.match_buffer(a, [14*14*256*256], dtype="float32")
+        B = T.match_buffer(b, [14*14*512*256], dtype="float32")
         # body
         T.launch_thread(blockIdx_z, 196)
         B_local = T.allocate([64], "float32", "local")
@@ -182,17 +197,22 @@ class Conv2dCuda3:
         T.launch_thread(threadIdx_y, 8)
         T.launch_thread(threadIdx_x, 800000)
         for ff_c_init, nn_c_init in T.grid(8, 8):
-            T.store(B_local, ff_c_init * 8 + nn_c_init, T.float32(0), True)
+            B_local[ff_c_init * 8 + nn_c_init] = T.float32(0)
         for rc_outer, ry, rx in T.grid(32, 3, 3):
             for ax3_inner_outer in T.serial(0, 2):
-                T.store(Apad_shared, T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4), T.if_then_else(1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15, T.load("float32x4", A.data, T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4), T.broadcast(True, 4)), T.broadcast(T.float32(0), 4), dtype="float32x4"), T.broadcast(True, 4))
+                Apad_shared[T.ramp(threadIdx_y * 64 + threadIdx_x * 8 + ax3_inner_outer * 4, 1, 4)] = T.if_then_else(
+                    1 <= blockIdx_z // 14 + ry and blockIdx_z // 14 + ry < 15 and 1 <= rx + blockIdx_z % 14 and rx + blockIdx_z % 14 < 15,
+                    A[T.ramp(ry * 917504 + blockIdx_z * 65536 + rx * 65536 + rc_outer * 2048 + threadIdx_y * 256 + blockIdx_x * 64 + threadIdx_x * 8 + ax3_inner_outer * 4 - 983040, 1, 4)],
+                    T.broadcast(T.float32(0), 4),
+                    dtype="float32x4",
+                )
             for rc_inner in T.serial(0, 8):
                 for ax3 in T.serial(0, 8):
-                    T.store(Apad_shared_local, ax3, T.load("float32", Apad_shared, rc_inner * 64 + threadIdx_x * 8 + ax3), True)
+                    Apad_shared_local[ax3] = Apad_shared[rc_inner * 64 + threadIdx_x * 8 + ax3]
                 for ff_c, nn_c in T.grid(8, 8):
-                    T.store(B_local, ff_c * 8 + nn_c, T.load("float32", B_local, ff_c * 8 + nn_c) + T.load("float32", Apad_shared_local, nn_c), True)
+                    B_local[ff_c * 8 + nn_c] = B_local[ff_c * 8 + nn_c] + Apad_shared_local[nn_c]
         for ff_inner_inner_inner, nn_inner_inner_inner in T.grid(8, 8):
-            T.store(B.data, blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner, T.load("float32", B_local, ff_inner_inner_inner * 8 + nn_inner_inner_inner), True)# fmt: on
+            B[blockIdx_z * 131072 + blockIdx_y * 16384 + threadIdx_y * 2048 + ff_inner_inner_inner * 256 + blockIdx_x * 64 + threadIdx_x * 8 + nn_inner_inner_inner] = B_local[ff_inner_inner_inner * 8 + nn_inner_inner_inner]# fmt: on
 
 
 # fmt: on
