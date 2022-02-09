985cec5f21d7e18a6029a35d0f9f4864d09c83a4
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-02-09 10:53:22 +0100

    USMP: Integrate hill_climb algo in test_crt_aot_usmp

--- tests/python/relay/aot/test_crt_aot_usmp.py
+++ tests/python/relay/aot/test_crt_aot_usmp.py
@@ -212,6 +212,7 @@ MOBILENET_V1_URL = (
     [
         (MOBILENET_V1_URL, "greedy_by_size", 4845696),
         (MOBILENET_V1_URL, "greedy_by_conflicts", 4444288),
+        (MOBILENET_V1_URL, "hill_climb", 3240064),
     ],
 )
 def test_tflite_model(model_url, usmp_algo, workspace_size):
