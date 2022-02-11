7840a9e1f3d803aa464309e8dd43a6c9c11b83eb
Masahiro Masuda <masahi129@gmail.com>
2022-02-12 08:10:35 +0900

    skip on ci

--- tests/python/relay/test_op_level1.py
+++ tests/python/relay/test_op_level1.py
@@ -634,6 +634,7 @@ def test_bitserial_dense():
     assert yy.checked_type == relay.TensorType((m, 32), "int16")
 
 
+@pytest.mark.skip("Requires cascadelake")
 def test_dense_vnni():
     data_shape = (32, 96)
     weight_shape = (128, 96)
@@ -668,5 +669,4 @@ def test_dense_vnni():
 
 
 if __name__ == "__main__":
-    # pytest.main([__file__])
-    test_dense_vnni()
+    pytest.main([__file__])
