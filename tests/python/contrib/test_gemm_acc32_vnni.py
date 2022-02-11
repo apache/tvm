4be2c8ab8707f7f6bd3c8199a1339260a13337f7
Masahiro Masuda <masahi129@gmail.com>
2022-02-11 19:10:28 +0900

    restore original test

--- tests/python/contrib/test_gemm_acc32_vnni.py
+++ tests/python/contrib/test_gemm_acc32_vnni.py
@@ -25,40 +25,6 @@ from tvm.topi.x86.tensor_intrin import dot_16x1x16_uint8_int8_int32
 import pytest
 
 
-def dense_vnni_compute(X, packedW):
-    m, k = X.shape
-    n_o, _, n_i, _ = packedW.shape
-    ak = te.reduce_axis((0, k), name="k")
-
-    return te.compute(
-        (m, n_o * n_i),
-        lambda i, j: te.sum(
-            X[i, ak].astype("int32")
-            * packedW[tvm.tir.indexdiv(j, 16), tvm.tir.indexdiv(ak, 4),  j % 16, ak % 4].astype("int32"),
-            axis=ak,
-        ),
-        name="F",
-    )
-
-
-def dense_vnni_schedule(t_fc):
-    t_sch = te.create_schedule(t_fc.op)
-    a_x, a_y = t_fc.op.axis
-    (a_k,) = t_fc.op.reduce_axis
-
-    a_yo, a_yi = t_sch[t_fc].split(a_y, factor=16)
-    a_ko, a_ki = t_sch[t_fc].split(a_k, factor=4)
-    a_xo, a_xi = t_sch[t_fc].split(a_x, factor=32)
-
-    t_sch[t_fc].reorder(a_yo, a_xo, a_xi, a_ko, a_yi, a_ki)
-
-    pc = dot_16x1x16_uint8_int8_int32_cascadelake()
-    t_sch[t_fc].tensorize(a_yi, pc)
-
-    return t_sch
-
-
-
 @tvm.testing.requires_llvm
 @pytest.mark.skip("skip because feature not enabled")
 def test_fc_int8_acc32():
@@ -67,8 +33,7 @@ def test_fc_int8_acc32():
     k = 1024
 
     X = te.placeholder((m, k), name="X", dtype="uint8")
-    packedW = te.placeholder((n // 16, k // 4, 16, 4), name="packedW", dtype="int8")
-
+    W = te.placeholder((n, k), name="W", dtype="int8")
 
     peak = 280
     print("Peak {} Gops/s".format(peak))
@@ -83,28 +48,48 @@ def test_fc_int8_acc32():
             print("skip because %s is not enabled..." % target)
             return
 
-        t_fc = dense_vnni_compute(X, packedW)
-        t_sch = dense_vnni_schedule(t_fc)
+        dev = tvm.device(target, 0)
+        pc = dot_16x1x16_uint8_int8_int32_cascadelake()
+        ak = te.reduce_axis((0, k), name="k")
+        packedW = te.placeholder((n // 16, 16 * (k // 4), 4), name="packedW", dtype="int8")
 
-        print(tvm.lower(t_sch, [X, packedW, t_fc]))
+        t_fc = te.compute(
+            (m, n),
+            lambda i, j: te.sum(
+                X[i, ak].astype("int32")
+                * packedW[tvm.tir.indexdiv(j, 16), tvm.tir.indexdiv(ak,  4) * 16 + j % 16, ak % 4].astype("int32"),
+                axis=ak,
+            ),
+            name="F",
+        )
+        t_sch = te.create_schedule(t_fc.op)
+        a_x, a_y = t_fc.op.axis
+        (a_k,) = t_fc.op.reduce_axis
+
+        a_yo, a_yi = t_sch[t_fc].split(a_y, factor=16)
+        a_xo, a_xi = t_sch[t_fc].split(a_x, factor=32)
+        a_ko, a_ki = t_sch[t_fc].split(a_k, factor=4)
+        a_koo, a_koi = t_sch[t_fc].split(a_ko, factor=4)
+        t_sch[t_fc].reorder(a_yo, a_xo, a_xi, a_koo, a_koi, a_yi, a_ki)
+
+        t_sch[t_fc].unroll(a_koi)
+        t_sch[t_fc].tensorize(a_yi, pc)
 
         t_func = tvm.build(t_sch, [X, packedW, t_fc], target, name="intrinsic")
-
-        dev = tvm.device(target, 0)
         t_evaluator = t_func.time_evaluator(t_func.entry_name, dev, number=10)
-        # print(t_func.get_source("asm"))
 
         # generate the plain data
         a_ = np.random.uniform(1, 10, size=(m, k)).astype("uint8")
         b_ = np.random.uniform(1, 10, size=(n, k)).astype("int8")
 
-        packW = np.random.uniform(1, 10, size=(n // 16, (k // 4), 16, 4)).astype("int8")
+        packW = np.random.uniform(1, 10, size=(n // 16, 16 * (k // 4), 4)).astype("int8")
         # This occurs in pre_compute stage
         for r_idx in range(n // 16):
-            for ko in range(k // 4):
-                for s_idx in range(16):
-                    for t_idx in range(4):
-                        packW[r_idx][ko][s_idx][t_idx] = b_[r_idx * 16 + s_idx][ko * 4 + t_idx]
+            for s_idx in range(16 * (k // 4)):
+                for t_idx in range(4):
+                    packW[r_idx][s_idx][t_idx] = b_[r_idx * 16 + s_idx % 16][
+                        (s_idx // 16) * 4 + t_idx
+                    ]
 
         x = tvm.nd.array(a_, dev)
         w = tvm.nd.array(packW, dev)
@@ -128,4 +113,5 @@ if __name__ == "__main__":
     # The test requires Cascade Lake and newer Intel machines to generate the
     # correct AVX512 VNNI instruction. So, disabling the test.
 
-    test_fc_int8_acc32()
+    # test_fc_int8_acc32()
+    pass
