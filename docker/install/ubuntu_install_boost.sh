c7b5b4a6731af897e8e60c637a831d89bebf45c5
driazati <driazati@users.noreply.github.com>
2022-02-15 12:53:51 -0800

    Address comments: pip hash, cleanup

--- docker/install/ubuntu_install_boost.sh
+++ docker/install/ubuntu_install_boost.sh
@@ -18,7 +18,15 @@
 
 set -euxo pipefail
 
-curl -LO https://boostorg.jfrog.io/artifactory/main/release/1.67.0/source/boost_1_67_0.tar.gz
+cleanup() {
+  rm -rf /boost_1_67_0 /boost_1_67_0.tar.gz
+}
+
+trap cleanup 0
+
+curl -O https://boostorg.jfrog.io/artifactory/main/release/1.67.0/source/boost_1_67_0.tar.gz
+BOOST_HASH=cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
+echo "$BOOST_HASH" boost_1_67_0.tar.gz | sha512sum -c
 tar -xf boost_1_67_0.tar.gz
 
 pushd boost_1_67_0
