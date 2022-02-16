0f1635c3a9f5c20a22323d7815dc6b3e406f25ed
Tristan Konolige <tkonolige@octoml.ai>
2022-02-16 10:45:34 -0800

    add groups argument to conv3d_ncdhw_winograd_without_weight_transform

--- python/tvm/topi/cuda/conv3d_winograd.py
+++ python/tvm/topi/cuda/conv3d_winograd.py
@@ -652,9 +652,10 @@ def schedule_conv3d_ncdhw_winograd(cfg, outs):
 
 @autotvm.register_topi_compute("conv3d_ncdhw_winograd_without_weight_transform.cuda")
 def conv3d_ncdhw_winograd_without_weight_transform(
-    cfg, data, kernel, strides, padding, dilation, out_dtype
+    cfg, data, kernel, strides, padding, dilation, groups, out_dtype
 ):
     """Conv3d NCDHW winograd without weight transform."""
+    assert groups == 1, "conv3d_ncdhw_winograd_without_weight_transform does not support more than one group"
     A, B, C, _, _ = get_const_tuple(kernel.shape)
     # Check if we can transform depth.
     if A == B == C:
