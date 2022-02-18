2c0a7c2a7e1df8203c14e4f0326a99aeaceb967a
Tristan Konolige <tkonolige@octoml.ai>
2022-02-18 14:18:01 -0700

    [TOPI] Add support for groupped conv3d (#9873)
    * [TOPI] Add support for groupped conv3d
    
    Change conv3d to use generic conv implementation which supports groupped
    convolutions. Also, remove support for non-float16 tensorcore operations
    as they cause large degradation in accuracy. Generic conv now supports
    autoscheduler.
    
    * correct none check
    
    * add tests for floordiv simplification
    
    * fixed incorrect test for autoscheduler
    
    * formatting
    
    * add groups to winograd
    
    * fix tensorcore
    
    * manually simplify index instead of relying on simplifier
    
    * formatting
    
    * add groups argument to conv3d_ncdhw_winograd_without_weight_transform
    
    * formatting

--- python/tvm/topi/cuda/conv3d_winograd.py
+++ python/tvm/topi/cuda/conv3d_winograd.py
@@ -620,7 +620,9 @@ def schedule_winograd_no_depth_cuda(cfg, s, output, pre_computed):
 
 
 @autotvm.register_topi_compute("conv3d_ncdhw_winograd.cuda")
-def conv3d_ncdhw_winograd(cfg, data, kernel, strides, padding, dilation, out_dtype):
+def conv3d_ncdhw_winograd(cfg, data, kernel, strides, padding, dilation, groups, out_dtype):
+    """Conv3d NCDHW using winograd optimization"""
+    assert groups == 1, "conv3d_ncdhw_winograd only supports a single group"
     CO, CI, KD, KH, KW = get_const_tuple(kernel.shape)
     # Check if we can transform depth.
     if 2 < KD < 8 and KD == KH:
@@ -650,9 +652,12 @@ def schedule_conv3d_ncdhw_winograd(cfg, outs):
 
 @autotvm.register_topi_compute("conv3d_ncdhw_winograd_without_weight_transform.cuda")
 def conv3d_ncdhw_winograd_without_weight_transform(
-    cfg, data, kernel, strides, padding, dilation, out_dtype
+    cfg, data, kernel, strides, padding, dilation, groups, out_dtype
 ):
     """Conv3d NCDHW winograd without weight transform."""
+    assert (
+        groups == 1
+    ), "conv3d_ncdhw_winograd_without_weight_transform does not support more than one group"
     A, B, C, _, _ = get_const_tuple(kernel.shape)
     # Check if we can transform depth.
     if A == B == C:
