3ffef3dcafcfd68ffb816c6151d3082d4e8dc6a5
David Riazati <9407960+driazati@users.noreply.github.com>
2022-02-21 02:24:37 -0800

    [docker] Update CI to Python 3.7 and Ubuntu 18 (#10247)
    This updates the docker image build to use Python 3.7 and Ubuntu 18 as discussed in #9703. The update is mostly straightforward except that the `apt` boost isn't built with 3.7 so we must now build it from source.

--- docker/install/ubuntu1804_install_python.sh
+++ docker/install/ubuntu1804_install_python.sh
@@ -20,13 +20,27 @@ set -e
 set -u
 set -o pipefail
 
-# install python and pip, don't modify this, modify install_python_package.sh
+
+cleanup() {
+  rm -rf base-requirements.txt
+}
+
+trap cleanup 0
+
+
+# Install python and pip. Don't modify this to add Python package dependencies,
+# instead modify install_python_package.sh
 apt-get update
 apt-get install -y software-properties-common
-apt-get install -y python3-dev python3-setuptools
-
-# Install pip
-cd /tmp && wget -q https://bootstrap.pypa.io/pip/3.6/get-pip.py && python3 get-pip.py
+apt-get install -y python3.7 python3.7-dev python3-pip
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
 
 # Pin pip and setuptools versions
-pip3 install pip==19.3.1 setuptools==58.4.0
+# Hashes generated via:
+#   $ pip download <package>==<version>
+#   $ pip hash --algorithm sha512 <package>.whl
+cat <<EOF > base-requirements.txt
+pip==22.0.3 --hash=sha512:12ca75130a1ce9807060a66dd2341afc7c7e663357ca4b937868dbc733634e11bae49ffff96acb0f5f3fb16cb14f680b9b6d185155a711c6098eda5cfbf2f8f5
+setuptools==60.9.1 --hash=sha512:24c21006f0650209e6a934a5366614e32a98fbdf11bc0e941419731aa3c8cb6d217e486608b4834b8f89c14dc36b3bafa30e795c87b58705be2b156f78acc3c5
+EOF
+pip3 install -r base-requirements.txt
