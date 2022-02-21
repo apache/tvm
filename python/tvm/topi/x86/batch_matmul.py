3621410a26387e9dcd628b4320c658d3a9dc8f3b
Masahiro Masuda <masahi129@gmail.com>
2022-02-21 18:33:08 +0900

    try an alternative approach to scheduling layout transform

--- python/tvm/topi/x86/batch_matmul.py
+++ python/tvm/topi/x86/batch_matmul.py
@@ -24,7 +24,6 @@ from tvm.contrib import cblas, mkl
 from .. import generic, nn
 from ..transform import layout_transform
 from ..utils import traverse_inline, get_const_tuple, get_max_power2_factor
-from .injective import schedule_injective_from_existing
 from .utils import target_has_vnni
 from .dense import dense_vnni_schedule
 
@@ -55,7 +54,7 @@ def batch_matmul_vnni_compute(cfg, x, y):
     return z
 
 
-def batch_matmul_vnni_schedule(cfg, s, C, O):
+def batch_matmul_vnni_schedule(cfg, s, C, O, layout_trans):
     """Schedule batch_matmul compute using VNNI vpdpbusd instruction"""
     # C: The output of batched GEMM
     # O: The output of the fused op
@@ -65,6 +64,12 @@ def batch_matmul_vnni_schedule(cfg, s, C, O):
     # Parallelize over batch
     fused = s[O].fuse(O.op.axis[0], fused_inner)
     s[O].parallel(fused)
+
+    s[layout_trans].compute_at(s[O], fused)
+    _, _, _, ni, ki = s[layout_trans].op.axis
+    s[layout_trans].vectorize(ki)
+    s[layout_trans].unroll(ni)
+
     return s
 
 
@@ -158,12 +163,8 @@ def schedule_batch_matmul(cfg, outs):
 
     def _callback(op):
         if "batch_matmul_vnni" in op.tag:
-            # Schedule layout transform
             layout_trans = op.input_tensors[1]
-            s[layout_trans].compute_root()
-            schedule_injective_from_existing(s, layout_trans)
-            # Schedule batch matmul
-            batch_matmul_vnni_schedule(cfg, s, op.output(0), outs[0])
+            batch_matmul_vnni_schedule(cfg, s, op.output(0), outs[0], layout_trans)
         elif "batch_matmul" in op.tag:
             C = op.output(0)
             A, B = op.input_tensors
