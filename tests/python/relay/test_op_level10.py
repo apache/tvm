8ea151e35b5260e43540a779abdebeeb80751c1e
Masahiro Masuda <masahi129@gmail.com>
2022-02-21 16:31:48 +0900

    add test

--- tests/python/relay/test_op_level10.py
+++ tests/python/relay/test_op_level10.py
@@ -16,6 +16,9 @@
 # under the License.
 """ Support level10 operator test cases.
 """
+import sys
+import pytest
+
 import numpy as np
 import tvm
 import tvm.testing
@@ -388,6 +391,37 @@ def test_batch_matmul():
     verify_batch_matmul_with_inputs(x, x, x_np, x_np, (10, 27, 27))
 
 
+@pytest.mark.skip("Requires cascadelake")
+def test_batch_matmul_vnni():
+    x_shape = (16, 32, 96)
+    y_shape = (16, 128, 96)
+
+    x = relay.var("x", shape=x_shape, dtype="uint8")
+    y = relay.var("y", shape=y_shape, dtype="int8")
+    bmm = relay.nn.batch_matmul(x, y, out_dtype="int32")
+    mod = tvm.IRModule.from_expr(bmm)
+
+    target = "llvm -mcpu=cascadelake"
+    with tvm.transform.PassContext(opt_level=3):
+        lib = relay.build(mod, target=target)
+
+    dev = tvm.device(target, 0)
+    runtime = tvm.contrib.graph_executor.GraphModule(lib["default"](dev))
+
+    x_np = np.random.uniform(1, 10, size=x_shape).astype("uint8")
+    y_np = np.random.uniform(1, 10, size=y_shape).astype("int8")
+
+    runtime.set_input("x", x_np)
+    runtime.set_input("y", y_np)
+    runtime.run()
+
+    out = runtime.get_output(0).numpy()
+    ref = tvm.topi.testing.batch_matmul(x_np, y_np, out_dtype="int32")
+
+    np.testing.assert_equal(out, ref)
+    print("ok")
+
+
 @tvm.testing.uses_gpu
 def test_shape_of():
     shape = (10, 5, 12)
@@ -623,7 +657,5 @@ def test_nll_loss(dev, target):
 
 
 if __name__ == "__main__":
-    import sys
-    import pytest
-
-    sys.exit(pytest.main([__file__] + sys.argv[1:]))
+    # sys.exit(pytest.main([__file__] + sys.argv[1:]))
+    test_batch_matmul_vnni()
