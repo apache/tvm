apache/tvm

commit 122be3fb18902bf2317797fedfa867dcf9607ef9

Add group_conv2d_transpose_nchw to CUDA backend (#10423)
* add group_conv2d_transpose_nchw to CUDA backend

* simplify significantly, just add groups argument to conv2d_transpose_nchw

python/tvm/topi/cuda/conv2d_transpose.py
@@ -27,7 +27,7 @@ from ..utils import get_const_tuple, traverse_inline
 
 
 @autotvm.register_topi_compute("conv2d_transpose_nchw.cuda")
-def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_padding):
+def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_padding, groups=1):
     """Transposed 2D convolution nchw forward operator.
 
     Parameters
@@ -46,6 +46,8 @@ def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_
         The output type. This is used in mixed precision
     output_padding : tuple of two ints
         Used to disambiguate output shape.
+    groups : int
+        number of groups
 
     Returns
     -------
@@ -57,6 +59,7 @@ def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_
     stride_height, stride_width = stride
     outpad_height, outpad_width = output_padding
     assert outpad_height < stride_height and outpad_width < stride_width
+    assert inp_channels % groups == 0, f"input channels {inp_channels} must divide group size {groups}"
     cfg.stride = stride
     pad_top, pad_left, pad_bottom, pad_right = nn.get_pad_tuple(
         padding, (kernel_height, kernel_width)
@@ -103,14 +106,21 @@ def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_
     )
 
     # compute transposed conv
-    dc = te.reduce_axis((0, inp_channels), name="dc")
+    dc = te.reduce_axis((0, inp_channels // groups), name="dc")
     dh = te.reduce_axis((0, kernel_height), name="dh")
     dw = te.reduce_axis((0, kernel_width), name="dw")
     data_out = te.compute(
-        (batch, out_channels, out_height, out_width),
+        (batch, out_channels * groups, out_height, out_width),
         lambda b, c, h, w: te.sum(
-            data[b, dc, h + dh, w + dw].astype(out_dtype)
-            * kernel[dc, c, kernel_height - 1 - dh, kernel_width - 1 - dw].astype(out_dtype),
+            data[
+                b,  c // out_channels * (inp_channels // groups) + dc, h + dh, w + dw
+            ].astype(out_dtype)
+            * kernel[
+                c // out_channels * (inp_channels // groups) + dc,
+                c % out_channels,
+                kernel_height - 1 - dh,
+                kernel_width - 1 - dw
+            ].astype(out_dtype),
             axis=[dc, dh, dw],
         ),
         tag="conv2d_transpose_nchw",

python/tvm/relay/op/strategy/cuda.py
@@ -593,9 +593,9 @@ def conv2d_transpose_strategy_cuda(attrs, inputs, out_type, target):
     strategy = _op.OpStrategy()
     num_strategies = 0
 
-    if layout == "NCHW" and groups == 1:
+    if layout == "NCHW":
         strategy.add_implementation(
-            wrap_compute_conv2d_transpose(topi.cuda.conv2d_transpose_nchw),
+            wrap_compute_conv2d_transpose(topi.cuda.conv2d_transpose_nchw, has_groups=True),
             wrap_topi_schedule(topi.cuda.schedule_conv2d_transpose_nchw),
             name="conv2d_transpose_nchw.cuda",
         )

tests/python/topi/python/test_topi_group_conv2d_transpose.py
@@ -25,10 +25,8 @@ from tvm.contrib.pickle_memoize import memoize
 from tvm.topi.utils import get_const_tuple
 
 _group_conv2d_nchw_implement = {
-    "generic": (
-        topi.nn.group_conv2d_transpose_nchw,
-        topi.generic.schedule_group_conv2d_transpose_nchw,
-    ),
+    "generic": (topi.nn.group_conv2d_transpose_nchw, topi.generic.schedule_group_conv2d_transpose_nchw),
+    "cuda": (topi.cuda.conv2d_transpose_nchw, topi.cuda.schedule_conv2d_transpose_nchw),
 }
 
 
@@ -92,16 +90,23 @@ def verify_group_conv2d_transpose_nchw(
             s,
             [A, W, C],
             target,
-            name="group_conv2d_transpose_%d_%d_%s_%d_%s_%s_%s_%s_%d"
+            name="group_conv2d_transpose_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d"
             % (
                 batch,
                 in_channel,
-                in_size,
+                in_size[0],
+                in_size[1],
                 num_filter,
-                kernel,
-                stride,
-                padding,
-                output_padding,
+                kernel[0],
+                kernel[1],
+                stride[0],
+                stride[1],
+                padding[0],
+                padding[1],
+                padding[2],
+                padding[3],
+                output_padding[0],
+                output_padding[1],
                 groups,
             ),
         )
@@ -110,46 +115,26 @@ def verify_group_conv2d_transpose_nchw(
         for measurement, reference in zip(c, c_np):
             tvm.testing.assert_allclose(measurement, reference, rtol=1e-5)
 
-    for target in ["llvm"]:
+    for target in ["llvm", "cuda"]:
         check_target(target)
 
 
 @tvm.testing.uses_gpu
 def test_group_conv2d_transpose_nchw():
-    verify_group_conv2d_transpose_nchw(1, 1, (224, 224), 1, (1, 1), (1, 1), (0, 0, 0, 0), (0, 0), 1)
-    verify_group_conv2d_transpose_nchw(
-        1, 3, (224, 224), 32, (3, 3), (1, 1), (0, 0, 0, 0), (0, 0), 1
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 3, (224, 224), 32, (3, 3), (3, 3), (0, 0, 0, 0), (0, 0), 1
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 3, (224, 224), 32, (3, 3), (1, 1), (0, 0, 0, 0), (0, 0), 1
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 3, (224, 224), 32, (3, 3), (2, 2), (1, 1, 1, 1), (0, 0), 1
-    )
     verify_group_conv2d_transpose_nchw(1, 4, (32, 32), 4, (5, 5), (1, 1), (0, 0, 0, 0), (0, 0), 2)
     verify_group_conv2d_transpose_nchw(1, 9, (32, 32), 9, (5, 5), (1, 1), (0, 0, 0, 0), (0, 0), 3)
     verify_group_conv2d_transpose_nchw(1, 4, (32, 32), 16, (5, 5), (2, 2), (1, 1, 1, 1), (0, 0), 4)
-    verify_group_conv2d_transpose_nchw(
-        1, 32, (8192, 1), 8, (31, 1), (2, 1), (14, 0, 15, 0), (0, 0), 2
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 512, (8, 1), 256, (31, 1), (2, 1), (14, 0, 15, 0), (0, 0), 16
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 512, (8, 1), 256, (31, 1), (2, 1), (14, 0, 15, 0), (1, 0), 16
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 64, (64, 64), 64, (4, 4), (1, 1), (0, 0, 0, 0), (0, 0), 64
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 128, (32, 32), 128, (4, 4), (1, 1), (0, 0, 0, 0), (0, 0), 128
-    )
-    verify_group_conv2d_transpose_nchw(
-        1, 256, (16, 16), 256, (4, 4), (1, 1), (0, 0, 0, 0), (0, 0), 256
-    )
+    verify_group_conv2d_transpose_nchw(1, 32, (8192, 1), 8, (31, 1), (2, 1), (14, 0, 15, 0), (0, 0), 2)
+    verify_group_conv2d_transpose_nchw(1, 512, (8, 1), 256, (31, 1), (2, 1), (14, 0, 15, 0), (0, 0), 16)
+    verify_group_conv2d_transpose_nchw(1, 512, (8, 1), 256, (31, 1), (2, 1), (14, 0, 15, 0), (1, 0), 16)
+    verify_group_conv2d_transpose_nchw(1, 64, (64, 64), 64, (4, 4), (1, 1), (0, 0, 0, 0), (0, 0), 64)
+    verify_group_conv2d_transpose_nchw(1, 128, (32, 32), 128, (4, 4), (1, 1), (0, 0, 0, 0), (0, 0), 128)
+    verify_group_conv2d_transpose_nchw(1, 256, (16, 16), 256, (4, 4), (1, 1), (0, 0, 0, 0), (0, 0), 256)
+    verify_group_conv2d_transpose_nchw(1, 1, (224, 224), 1, (1, 1), (1, 1), (0, 0, 0, 0), (0, 0), 1)
+    verify_group_conv2d_transpose_nchw(1, 3, (224, 224), 32, (3, 3), (1, 1), (0, 0, 0, 0), (0, 0), 1)
+    verify_group_conv2d_transpose_nchw(1, 3, (224, 224), 32, (3, 3), (3, 3), (0, 0, 0, 0), (0, 0), 1)
+    verify_group_conv2d_transpose_nchw(1, 3, (224, 224), 32, (3, 3), (1, 1), (0, 0, 0, 0), (0, 0), 1)
+    verify_group_conv2d_transpose_nchw(1, 3, (224, 224), 32, (3, 3), (2, 2), (1, 1, 1, 1), (0, 0), 1)
 
 
 if __name__ == "__main__":

commit a82a7412e7ff36401ad9e95ff3fbf7a4cd172264

[skip ci][CI][Fix] Fixing lint
A linting issue was introduced in #10423, fixing this up.

Change-Id: I06c518194e30dcaa755005f06b8b7280c237d386

python/tvm/topi/cuda/conv2d_transpose.py
@@ -59,7 +59,9 @@ def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_
     stride_height, stride_width = stride
     outpad_height, outpad_width = output_padding
     assert outpad_height < stride_height and outpad_width < stride_width
-    assert inp_channels % groups == 0, f"input channels {inp_channels} must divide group size {groups}"
+    assert (
+        inp_channels % groups == 0
+    ), f"input channels {inp_channels} must divide group size {groups}"
     cfg.stride = stride
     pad_top, pad_left, pad_bottom, pad_right = nn.get_pad_tuple(
         padding, (kernel_height, kernel_width)
@@ -112,14 +114,14 @@ def conv2d_transpose_nchw(cfg, data, kernel, stride, padding, out_dtype, output_
     data_out = te.compute(
         (batch, out_channels * groups, out_height, out_width),
         lambda b, c, h, w: te.sum(
-            data[
-                b,  c // out_channels * (inp_channels // groups) + dc, h + dh, w + dw
-            ].astype(out_dtype)
+            data[b, c // out_channels * (inp_channels // groups) + dc, h + dh, w + dw].astype(
+                out_dtype
+            )
             * kernel[
                 c // out_channels * (inp_channels // groups) + dc,
                 c % out_channels,
                 kernel_height - 1 - dh,
-                kernel_width - 1 - dw
+                kernel_width - 1 - dw,
             ].astype(out_dtype),
             axis=[dc, dh, dw],
         ),