apache/tvm

commit 8fe75fd42c114b6e864862b1745ab388bc8eb734

[ci] Skip CI based on globs (#10456)
Co-authored-by: driazati <driazati@users.noreply.github.com>

Jenkinsfile
@@ -132,6 +132,14 @@ def cancel_previous_build() {
 }
 
 def should_skip_ci(pr_number) {
+  glob_skip_ci_code = sh (
+    returnStatus: true,
+    script: "./tests/scripts/git_skip_ci_globs.py",
+    label: 'Check if CI should be skipped due to changed files',
+  )
+  if (glob_skip_ci_code == 0) {
+    return true
+  }
   withCredentials([string(
     credentialsId: 'tvm-bot-jenkins-reader',
     variable: 'TOKEN',
@@ -143,7 +151,7 @@ def should_skip_ci(pr_number) {
       script: "./tests/scripts/git_skip_ci.py --pr '${pr_number}'",
       label: 'Check if CI should be skipped',
     )
-    }
+  }
   return git_skip_ci_code == 0
 }
 

tests/python/unittest/test_ci.py
@@ -263,6 +263,41 @@ def test_skip_ci(tmpdir_factory):
     )
 
 
+def test_skip_globs(tmpdir_factory):
+    script = REPO_ROOT / "tests" / "scripts" / "git_skip_ci_globs.py"
+
+    def run(files, should_skip):
+        git = TempGit(tmpdir_factory.mktemp("tmp_git_dir"))
+        # Jenkins git is too old and doesn't have 'git init --initial-branch'
+        git.run("init")
+        git.run("checkout", "-b", "main")
+        git.run("remote", "add", "origin", "https://github.com/apache/tvm.git")
+
+        proc = subprocess.run(
+            [
+                str(script),
+                "--files",
+                ",".join(files),
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf-8",
+            cwd=git.cwd,
+        )
+
+        if should_skip:
+            assert proc.returncode == 0
+        else:
+            assert proc.returncode == 1
+
+    run([], should_skip=True)
+    run(["README.md"], should_skip=True)
+    run(["test.c"], should_skip=False)
+    run(["test.c", "README.md"], should_skip=False)
+    run(["src/autotvm/feature_visitor.cc", "README.md"], should_skip=False)
+    run([".asf.yaml", "docs/README.md"], should_skip=True)
+
+
 def test_ping_reviewers(tmpdir_factory):
     reviewers_script = REPO_ROOT / "tests" / "scripts" / "ping_reviewers.py"
 

tests/scripts/git_change_docs.sh
@@ -20,23 +20,19 @@
 set -eux
 
 FOUND_ONE_FILE=0
-DOCS_DIR=0
-OTHER_DIR=0
-DOC_DIR="docs/"
+SAW_NON_DOC_CHANGES=0
 
-changed_files=`git diff --no-commit-id --name-only -r origin/main`
+changed_files=$(git diff --no-commit-id --name-only -r origin/main)
 
 for file in $changed_files; do
     FOUND_ONE_FILE=1
-    if grep -q "$DOC_DIR" <<< "$file"; then
-        DOCS_DIR=1
-    else
-        OTHER_DIR=1
+    if ! grep -q "docs/" <<< "$file"; then
+        SAW_NON_DOC_CHANGES=1
         break
     fi
 done
 
-if [ ${FOUND_ONE_FILE} -eq 0 -o ${OTHER_DIR} -eq 1 ]; then
+if [ ${FOUND_ONE_FILE} -eq 0 ] || [ ${SAW_NON_DOC_CHANGES} -eq 1 ]; then
     exit 0
 else
     exit 1

tests/scripts/git_skip_ci_globs.py
@@ -1,0 +1,76 @@
+#!/usr/bin/env python3
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import argparse
+import fnmatch
+from typing import Optional
+
+from git_utils import git
+
+
+globs = [
+    "*.md",
+    "docker/*",
+    "conda/*",
+    ".github/*",
+    ".asf.yaml",
+    "LICENSE",
+    "NOTICE",
+    "KEYS",
+]
+
+
+def match_any(f: str) -> Optional[str]:
+    for glob in globs:
+        if fnmatch.fnmatch(f, glob):
+            return glob
+    return None
+
+
+if __name__ == "__main__":
+    help = "Exits with code 1 if a change only touched files, indicating that CI could be skipped for this changeset"
+    parser = argparse.ArgumentParser(description=help)
+    parser.add_argument("--files", help="(testing only) comma separated list of files to check")
+    args = parser.parse_args()
+    print(args)
+    if args.files is not None:
+        diff = [x for x in args.files.split(",") if x.strip() != ""]
+    else:
+        diff = git(["diff", "--no-commit-id", "--name-only", "-r", "origin/main"])
+        diff = diff.split("\n")
+        diff = [d.strip() for d in diff]
+        diff = [d for d in diff if d != ""]
+
+    print(f"Changed files:\n{diff}")
+
+    if len(diff) == 0:
+        print("Found no changed files, skipping CI")
+        exit(0)
+
+    print(f"Checking with globs:\n{globs}")
+
+    for file in diff:
+        match = match_any(file)
+        if match is None:
+            print(f"{file} did not match any globs, running CI")
+            exit(1)
+        else:
+            print(f"{file} matched glob {match}")
+
+    print("All files matched a glob, skipping CI")
+    exit(0)

commit 619d7a127d3ef5be420d2ae8e8284e4e100c8e67

[skip ci][ci] hotfix Jenkinsfile
This was broken by #10456 since that did not have a case for ignoring the `main` and staging branches. This adds it at the Jenkins level so the further scripts won't even be run if not necessary. This is marked `skip ci` but since the changes all happen before lint this is being tested.

commit-id:1655b8da

Jenkinsfile
@@ -132,6 +132,10 @@ def cancel_previous_build() {
 }
 
 def should_skip_ci(pr_number) {
+  if (!env.BRANCH_NAME.startsWith('PR-')) {
+    // never skip CI on build sourced from a branch
+    return false
+  }
   glob_skip_ci_code = sh (
     returnStatus: true,
     script: "./tests/scripts/git_skip_ci_globs.py",