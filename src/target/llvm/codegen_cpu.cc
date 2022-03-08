67beceeda9a0e955ddf67008f8ca63b3c902f618
Eric Lunderberg <elunderberg@octoml.ai>
2022-03-08 09:45:12 -0600

    Use name parameter in CreateLoad.

--- src/target/llvm/codegen_cpu.cc
+++ src/target/llvm/codegen_cpu.cc
@@ -606,8 +606,8 @@ void CodeGenCPU::UnpackClosureData(TypedPointer cdata, const Array<Var>& vfields
     llvm::Type* field_type = cdata.type->getStructElementType(i);
     llvm::Value* field_addr =
         builder_->CreateInBoundsGEP(cdata.type, cdata.addr, {ConstInt32(0), ConstInt32(i)});
-    llvm::Value* load = builder_->CreateLoad(field_type, field_addr);
-    load->setName(std::string(vfields[i]->name_hint));
+    llvm::Value* load =
+        builder_->CreateLoad(field_type, field_addr, std::string(vfields[i]->name_hint));
     (*vmap)[vfields[i].get()] = load;
   }
 }
@@ -646,11 +646,10 @@ void CodeGenCPU::CreateParallelLaunch(const Stmt& body, int num_task, std::strin
   par_env.task_id = Var("task_id", DataType::Int(32));
   par_env.num_task = Var("num_task", DataType::Int(32));
   new_vmap[par_env.task_id.get()] = task_id;
-  llvm::Value* num_task_value = builder_->CreateLoad(
+  new_vmap[par_env.num_task.get()] = builder_->CreateLoad(
       t_int32_,
-      builder_->CreateInBoundsGEP(t_tvm_parallel_group_env_, penv, {ConstInt32(0), ConstInt32(1)}));
-  num_task_value->setName("num_task");
-  new_vmap[par_env.num_task.get()] = num_task_value;
+      builder_->CreateInBoundsGEP(t_tvm_parallel_group_env_, penv, {ConstInt32(0), ConstInt32(1)}),
+      "num_task");
   par_env.penv = penv;
   auto new_analyzer = std::make_unique<arith::Analyzer>();
   std::swap(function_, f);
