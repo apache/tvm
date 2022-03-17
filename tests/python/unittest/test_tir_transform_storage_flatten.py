apache/tvm

commit 174d09ee2cef1ea2caab4c84e0bd58d90c09178f

[TE][TIR] Implement layout transformations, non-flat memory buffers (#9727)
* [TIR] Added BufferLoadNode::LegalizeDtype

When modifying a BufferLoad object, the return dtype must also be
updated.  This exposes the legalization function, so that passes that
use `BufferLoad::CopyOnWrite` to modify the buffer/indices don't need
to repeat the logic to update the dtype returned.

* Replacing Store/Load in Stmt/Expr Visitor/Mutator

* Removing Store/Load from optimization passes

- UpdatePointerStorageScope
- UnrollLoop
- ThreadSync
- LinearAccessPatternFinder
- StoragePlanRewriter
- VectorTypeRewriter
- VectorTypeAccessChecker
- NarrowDataType
- IRConvertSSA
- CompactBufferRegion

* Removing Store/Load from examples

- ConvertAddToSubtract

* Replacing Store/Load in StorageFlatten

Now, outputs BufferLoad/BufferStore with a flattened buffer object.

temp commit, replacing Store/Load, BufferBindUnwrapper

temp commit, replacing Store/Load, StorageFlattener

* Replacing Store/Load in utility passes.

- StmtSimplifier
- IRSubstitute
- BaseInliner
- FeatureVisitor

* Replacing Store/Load in analysis functions

- StorageAccessVisitor
- VarTouchedAnalysis
- MemoryAccessVerifier
- InplaceOpVerifier
- GPUCodeVerifier
- VarTouchVisitor
- LCADetector
- BlockReadWriteDetector
- InstrumentBoundCheckers

* Replacing Store/Load in lowering/legalization passes.

- MakeCrossThreadReduction
- CacheReadRewriter/CacheWriteRewriter
- InjectVirtualThread
- InjectDoubleBuffer
- InjectCopyIntrin
- LowerWarpMemory
- LowerThreadAllreduce
- LowerThreadAllreduce
- LowerCustomDatatypes
- LowerTVMBuiltin
- CoProcSync
- MergeDynamicSharedMemAllocations
- VectorizeLoop
- BF16Legalize

* Replacing Load/Store in codegens.

- Device code generators
  - CodegenC
  - CodegenLLVM
  - CodeGenOpenCL

- Utilities used during codegen
  - ArgBinder
  - MakePackedAPI
  - ReturnRewriter
  - SplitHostDevice

tests/python/unittest/test_tir_transform_storage_flatten.py
@@ -78,28 +78,25 @@ def test_flatten_storage_align():
 
 
 def test_flatten_double_buffer():
-    dtype = "int64"
-    n = 100
-    m = 4
-    tx = te.thread_axis("threadIdx.x")
-    ib = tvm.tir.ir_builder.create()
-    A = ib.pointer("float32", name="A")
-    C = ib.pointer("float32", name="C")
-    ib.scope_attr(tx, "thread_extent", 1)
-    with ib.for_range(0, n) as i:
-        B = ib.allocate("float32", m, name="B", scope="shared")
-        with ib.new_scope():
-            ib.scope_attr(B.asobject(), "double_buffer_scope", 1)
-            with ib.for_range(0, m) as j:
-                B[j] = A[i * 4 + j]
-        with ib.for_range(0, m) as j:
-            C[j] = B[j] + 1
+    @tvm.script.ir_module
+    class ModFromScript:
+        @T.prim_func
+        def main(A_param: T.handle, C_param: T.handle):
+            A = T.match_buffer(A_param, (400,), "float32", strides=[1])
+            C = T.match_buffer(C_param, (4,), "float32", strides=[1])
+            T.func_attr({"from_legacy_te_schedule": True})
+            threadIdx_x = T.env_thread("threadIdx.x")
+            T.launch_thread(threadIdx_x, 1)
+            for i in T.serial(0, 100):
+                B = T.allocate([4], "float32", scope="shared", strides=[1])
+                with T.attr(B.data, "double_buffer_scope", 1):
+                    for j in T.serial(0, 4):
+                        B[j] = A[4 * i + j]
 
-    stmt = ib.get()
+                for j in T.serial(0, 4):
+                    C[j] = B[j] + 1.0
 
-    mod = tvm.IRModule.from_expr(
-        tvm.tir.PrimFunc([A, C], stmt).with_attr("from_legacy_te_schedule", True)
-    )
+    mod = ModFromScript
 
     with tvm.transform.PassContext(config={"tir.InjectDoubleBuffer": {"split_loop": 2}}):
         mod = tvm.transform.Sequential(
@@ -112,10 +109,10 @@ def test_flatten_double_buffer():
 
     stmt = mod["main"].body
     assert isinstance(stmt.body, tvm.tir.Allocate)
-    assert stmt.body.extents[0].value == 2
+    assert list(stmt.body.extents) == [8]
 
-    mod = tvm.IRModule.from_expr(tvm.tir.PrimFunc([A, C], stmt).with_attr("global_symbol", "db"))
-    f = tvm.tir.transform.ThreadSync("shared")(mod)["db"]
+    mod = tvm.tir.transform.ThreadSync("shared")(mod)
+    f = mod["main"]
 
     count = [0]
 

commit d66ec9308370c08be8a974cde8eea62a713adb38

[TIR] Bugfix in StorageFlatten, index flattening in PrefetchNode
This resolves a bug introduced in
https://github.com/apache/tvm/pull/9727, and adds a test to catch this
failure mode.  This bug occurred because StorageFlatten's visitor for
PrefetchNode inserted additional pre-flattened `BufferLoad` nodes
after visiting the body of the Prefetch, preventing those `BufferLoad`
nodes from being flattened.  Moving this visit to after the insertion
of the `BufferLoad` nodes allows the usual buffer flattening to apply
to the newly inserted nodes.

src/tir/transforms/storage_flatten.cc
@@ -1489,10 +1489,6 @@ class StorageFlattener : public StmtExprMutator {
   }
 
   Stmt VisitStmt_(const PrefetchNode* op) final {
-    Stmt stmt = StmtExprMutator::VisitStmt_(op);
-    op = stmt.as<PrefetchNode>();
-    ICHECK(op != nullptr);
-
     const BufferEntry& e = GetBufferEntry(op->buffer);
 
     ICHECK(e.in_scope) << "Cannot prefetch " << op->buffer << ", out of scope.";
@@ -1524,6 +1520,8 @@ class StorageFlattener : public StmtExprMutator {
       vars.push_back(Var("prefetch." + func_name + "." + std::to_string(i), DataType::Int(32)));
       args.push_back(vars.back() + op->bounds[i]->min);
     }
+
+    Stmt stmt = GetRef<Stmt>(op);
     for (int i = starts; i >= 0; --i) {
       if (i < starts) {
         stmt = For(vars[i], 0, op->bounds[i]->extent, ForKind::kSerial, stmt);
@@ -1536,7 +1534,7 @@ class StorageFlattener : public StmtExprMutator {
         stmt = For(vars[i], 0, extent, ForKind::kSerial, stmt);
       }
     }
-    return stmt;
+    return this->VisitStmt(stmt);
   }
 
   PrimExpr VisitExpr_(const ProducerLoadNode* op) final {

tests/python/unittest/test_tir_transform_storage_flatten.py
@@ -57,6 +57,12 @@ def test_flatten_prefetch():
     assert isinstance(stmt.body, tvm.tir.For)
     assert stmt.body.extent.value == 2
 
+    def assert_flat_loads(stmt):
+        if isinstance(stmt, tvm.tir.BufferLoad):
+            assert len(stmt.indices) == 1, "All prefetch indices should be flattened"
+
+    tvm.tir.stmt_functor.post_order_visit(stmt, assert_flat_loads)
+
 
 def test_flatten_storage_align():
     m = 8