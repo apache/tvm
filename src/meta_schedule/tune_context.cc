2789e669b59abf9f440504f66fe264203978bb34
Junru Shao <junrushao1994@gmail.com>
2022-03-29 00:20:58 -0700

    [MetaSchedule] Support grouping in the cost model

--- src/meta_schedule/tune_context.cc
+++ src/meta_schedule/tune_context.cc
@@ -87,5 +87,8 @@ TVM_REGISTER_GLOBAL("meta_schedule.TuneContext")
       return TuneContext(mod, target, space_generator, search_strategy, sch_rules, postprocs,
                          mutator_probs, task_name, rand_state, num_threads);
     });
+
+TVM_REGISTER_GLOBAL("meta_schedule._SHash2Hex").set_body_typed(SHash2Hex);
+
 }  // namespace meta_schedule
 }  // namespace tvm
