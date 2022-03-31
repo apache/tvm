4322ebc38797d8d76838db8be5918459ac3d0958
Ivy Zhang <yan3.zhang@intel.com>
2022-04-01 03:53:36 +0800

    [BYOC-DNNL] Enhance GetRootCall function (#10836)
    * enhance dnnl codegen to support different topology of partition graph
    
    * fix lint
    
    * add test case

--- src/relay/backend/utils.h
+++ src/relay/backend/utils.h
@@ -449,8 +449,12 @@ inline const CallNode* GetRootCall(const CallNode* current_call, int depth,
   }
 
   ICHECK_GT(current_call->args.size(), 0);
-
-  const auto* next_call = current_call->args[0].as<CallNode>();
+  size_t valid_node_idx = 0;
+  while (valid_node_idx < current_call->args.size() &&
+         current_call->args[valid_node_idx].as<VarNode>()) {
+    valid_node_idx++;
+  }
+  const auto* next_call = current_call->args[valid_node_idx].as<CallNode>();
   return GetRootCall(next_call, depth - 1, expected_op_names);
 }
 
