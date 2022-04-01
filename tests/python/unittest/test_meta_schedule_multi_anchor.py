86d70607e72b64455941e641faf53870d6949cab
Masahiro Masuda <masahi129@gmail.com>
2022-04-02 06:26:17 +0900

    remove unused import

--- tests/python/unittest/test_meta_schedule_multi_anchor.py
+++ tests/python/unittest/test_meta_schedule_multi_anchor.py
@@ -14,9 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import os
-import tempfile
-
 import numpy as np
 
 import tvm
