98f735963de898b346aa0958e2a60c70c9a3a8cc
Masahiro Masuda <masahi129@gmail.com>
2022-04-14 16:53:34 +0900

    add conv2d relay test

--- tests/python/relay/test_op_level2.py
+++ tests/python/relay/test_op_level2.py
@@ -1582,18 +1582,24 @@ def _test_upsampling3d(layout, method, coordinate_transformation_mode="half_pixe
 
     def get_shape():
         if layout == "NCDHW":
-            return (c, d, h, w), (
-                c,
-                int(round(d * scale_d)),
-                int(round(h * scale_h)),
-                int(round(w * scale_w)),
+            return (
+                (c, d, h, w),
+                (
+                    c,
+                    int(round(d * scale_d)),
+                    int(round(h * scale_h)),
+                    int(round(w * scale_w)),
+                ),
             )
         else:
-            return (d, h, w, c), (
-                int(round(d * scale_d)),
-                int(round(h * scale_h)),
-                int(round(w * scale_w)),
-                c,
+            return (
+                (d, h, w, c),
+                (
+                    int(round(d * scale_d)),
+                    int(round(h * scale_h)),
+                    int(round(w * scale_w)),
+                    c,
+                ),
             )
 
     ishape, oshape = get_shape()
@@ -1942,6 +1948,56 @@ def test_correlation():
         padding=2,
         is_multiply=False,
     )
+
+
+@pytest.mark.skip("Requires GFX10 AMDGPU")
+def test_conv2d_rocm_sdot4():
+    d_shape = (1, 64, 56, 56)
+    w_shape = (64, 64, 3, 3)
+    padding = (1, 1)
+    strides = (1, 1)
+    data_dtype = "int8"
+    weight_dtype = "int8"
+    out_dtype = "int32"
+
+    data = relay.var("data", shape=d_shape, dtype=data_dtype)
+    weight = relay.var("weight", shape=w_shape, dtype=weight_dtype)
+    out_channel = w_shape[0]
+    conv2d = relay.nn.conv2d(
+        data=data,
+        weight=weight,
+        kernel_size=w_shape[2:],
+        channels=out_channel,
+        padding=padding,
+        strides=strides,
+        out_dtype=out_dtype,
+    )
+
+    mod = tvm.IRModule.from_expr(conv2d)
+
+    data_np = np.random.uniform(1, 10, d_shape).astype("int8")
+    weight_np = np.random.uniform(1, 10, size=w_shape).astype("int8")
+
+    target = "rocm -mattr=+dotprod"
+    with tvm.transform.PassContext(opt_level=3):
+        lib = relay.build(mod, target=target, params={"weight": weight_np})
+
+    asm = lib.lib.imported_modules[0].get_source("asm")
+    assert "v_dot4_i32_i8" in asm
+
+    dev = tvm.device(target, 0)
+    runtime = tvm.contrib.graph_executor.GraphModule(lib["default"](dev))
+
+    runtime.set_input("data", data_np)
+    runtime.run()
+
+    out = runtime.get_output(0).numpy()
+
+    ref = tvm.topi.testing.conv2d_nchw_python(
+        data_np.astype("int32"), weight_np.astype("int32"), strides, padding
+    )
+
+    np.testing.assert_equal(out, ref)
 
 
 if __name__ == "__main__":
