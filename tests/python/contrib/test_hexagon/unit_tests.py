262dc73e53469f069d3794bfa4a6fe2d023e861f
adstraw <astraw@octoml.ai>
2022-04-22 08:57:24 -0700

    add gtest init with arguments

--- tests/python/contrib/test_hexagon/unit_tests.py
+++ tests/python/contrib/test_hexagon/unit_tests.py
@@ -1,11 +1,18 @@
 import pytest
 import numpy as np
 from tvm.contrib.hexagon.build import HexagonLauncher
-#import tvm.contrib.hexagon as hexagon
 from .conftest import requires_hexagon_toolchain
+
 
 @requires_hexagon_toolchain
 def test_cache_read_write_2d(hexagon_session):
+    # arguments to pass to gtest
+    # e.g.
+    # 1) to run all tests use
+    # gtest_args = ""
+    # 2) to run all tests with "foo" in their name twice use:
+    # gtest_args = "--gtest_repeat=2 --gtest_filter=*foo*"
+    gtest_args = "--gtest_repeat=2 --gtest_filter=*micro*"
     func = hexagon_session._rpc.get_function("hexagon.run_all_tests")
-    x = func()
-    np.testing.assert_equal(x, 0)
+    result = func(gtest_args)
+    np.testing.assert_equal(result, 0)
