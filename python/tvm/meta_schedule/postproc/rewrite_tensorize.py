68464841ea617f9c859c1f7c546b29b611428186
Masahiro Masuda <masahi129@gmail.com>
2022-04-27 02:17:44 +0900

    [Metaschedule] Auto tensorization for CPU / GPU dot product (#11088)
    * [Metaschedule] Auto-tensorization for CPU / GPU dot product
    
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>
    
    * doc update
    
    * add vnni conv2d test
    
    * add dp4a test
    
    * adding tests for rewrite_tensorize
    
    * add rewrite_tensorize test
    
    * add missing pydoc
    
    * black
    
    * more doc
    
    * adding auto tensorize integration test
    
    * add dp4a test
    
    * fix target name
    
    * fix dtype in test
    
    * skip bert test
    
    * replace hard-coded llvm intrinsic id in test with look up
    
    * remove unnecessary include, add doc for the rest of params
    
    * update postproc.h
    
    * update doc
    
    * fix shape in te matmul workload
    
    * fix newline in cppdoc
    
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Ruihang Lai <lairuihangdongdong@qq.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>

--- python/tvm/meta_schedule/postproc/rewrite_tensorize.py
+++ python/tvm/meta_schedule/postproc/rewrite_tensorize.py
@@ -14,12 +14,25 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""The tvm.meta_schedule.postproc package."""
-from .postproc import Postproc, PyPostproc
-from .disallow_dynamic_loop import DisallowDynamicLoop
-from .rewrite_cooperative_fetch import RewriteCooperativeFetch
-from .rewrite_parallel_vectorize_unroll import RewriteParallelVectorizeUnroll
-from .rewrite_reduction_block import RewriteReductionBlock
-from .rewrite_unbound_block import RewriteUnboundBlock
-from .verify_gpu_code import VerifyGPUCode
-from .rewrite_tensorize import RewriteTensorize
+"""A postprocessor that tensorize related components."""
+
+from tvm._ffi.registry import register_object
+from .. import _ffi_api
+from .postproc import Postproc
+
+
+@register_object("meta_schedule.RewriteTensorize")
+class RewriteTensorize(Postproc):
+    """A postprocessor that applies tensorization to annotated blocks.
+
+    Parameters
+    ----------
+    vectorize_init_loop : bool
+       Whether or not vectorize the initialization loop produced by DecomposeReduction
+    """
+
+    def __init__(self, vectorize_init_loop=False) -> None:
+        self.__init_handle_by_constructor__(
+            _ffi_api.PostprocRewriteTensorize,  # type: ignore # pylint: disable=no-member
+            vectorize_init_loop,
+        )
