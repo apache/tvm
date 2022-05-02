84e1aa8addde5f37e89ad25485ef8047e9bfc0f6
Krzysztof Parzyszek <kparzysz@quicinc.com>
2022-05-02 10:37:50 -0500

    Remove support for run-time linked-params from codegen (#11144)
    Linking parameters via a runtime lookup function no longer happens after
    commit b5f1dabce4 (PR#8509): "Tir constants integration into compilation
    pipeline". Now, in cases where the runtime lookup would have happened in
    the past, the parameters are embedded into TIR, removing the need for a
    runtime lookup.
    
    There is still plenty of code around that implemented the original runtime
    lookup. This patch removes the unnecessary leftovers from TVM's codegen.

--- src/target/llvm/codegen_llvm.h
+++ src/target/llvm/codegen_llvm.h
@@ -126,18 +126,6 @@ class CodeGenLLVM : public ExprFunctor<llvm::Value*(const PrimExpr&)>,
    */
   void AddLinkModule(std::unique_ptr<llvm::Module>&& mod);
   /*!
-   * \brief Link parameters into the module so they don't need to be supplied at runtime.
-   * Parameters can be linked into the module so that the generated code is easier to use, or so
-   * that RAM space doesn't need to be allocated for them. This function adds the given parameters
-   * to the generated LLVM module.
-   * \param storage_id_offset Offset added to the index of each entry in params_by_sid to form the
-   *     storage_id of that parameter. Storage ids for parameters are expected to be contiguous.
-   * \param params_by_sid Array of NDArray. Each entry is a parameter. The index of the array (added
-   *     to sid_offset) is the storage_id of the param.
-   * \param param_names Array containing the name for each param in params_by_sid.
-   */
-  void LinkParameters(const Map<String, LinkedParam> params);
-  /*!
    * \brief Create Value for expression e
    * \param e The expression to be created value for.
    * \return created value.
@@ -349,14 +337,6 @@ class CodeGenLLVM : public ExprFunctor<llvm::Value*(const PrimExpr&)>,
    */
   llvm::Function* GetIntrinsicDecl(llvm::Intrinsic::ID id, llvm::Type* ret_type,
                                    llvm::ArrayRef<llvm::Type*> arg_types);
-  /*!
-   * \brief Lookup or create a GlobalVariable whose content is the data field of a DLTensor for a
-   * given linked_param() CallNode.
-   * \param param_name Parameter name (e.g. unmangled, from lookup_param node).
-   * \return the GlobalVariable indicated in the brief.
-   */
-  llvm::GlobalVariable* GetLinkedParamSymbol(const ::std::string& param_name,
-                                             llvm::ConstantArray* array);
   /*!
    * \brief Get the number of elements in the given vector value.
    * \param vec The value, must be of a vector type.
