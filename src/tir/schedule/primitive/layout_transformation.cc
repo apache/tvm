5007033086fbd7be66259740e330cd750358f351
Wuwei Lin <wuwei@apache.org>
2022-05-04 21:46:06 -0700

    [TIR] Fix printing enum in TransformLayout::AsPython (#11211)
    After this PR, `as_python` can handle `transform_layout` correctly. The result will be like
    
    ```python
    sch.transform_layout(..., buffer_index_type="read", ...)
    ```
    
    Previously `buffer_index_type` was printed unquoted.

--- src/tir/schedule/primitive/layout_transformation.cc
+++ src/tir/schedule/primitive/layout_transformation.cc
@@ -207,8 +207,10 @@ struct TransformLayoutTraits : public UnpackedInstTraits<TransformLayoutTraits> 
     PythonAPICall py("transform_layout");
     py.Input("block", block_rv);
     py.Input("buffer_index", buffer_index);
-    py.Input("buffer_index_type",
-             BufferIndexType2Str(static_cast<BufferIndexType>(buffer_index_type->value)));
+    py.Input("buffer_index_type", '"' +
+                                      std::string(BufferIndexType2Str(
+                                          static_cast<BufferIndexType>(buffer_index_type->value))) +
+                                      '"');
     py.Input("index_map", index_map->ToPythonString());
     return py.Str();
   }
