9e5e12f387e7456cfa2d642e6db1411a5d72b0e9
Mohamad Katanbaf <mtkatanbaf@gmail.com>
2022-05-12 11:48:30 -0700

    [rpc] Implemented rpc logging (#11232)
    * Implemented rpc logging
    
    * fixing windows build issue
    
    * trigger
    
    Co-authored-by: Mohamad <mkatanbaf@users.noreply.github.com>

--- src/runtime/crt/microtvm_rpc_server/rpc_server.cc
+++ src/runtime/crt/microtvm_rpc_server/rpc_server.cc
@@ -193,8 +193,6 @@ class MicroRPCServer {
 }  // namespace runtime
 }  // namespace tvm
 
-void* operator new[](size_t count, void* ptr) noexcept { return ptr; }
-
 extern "C" {
 
 static microtvm_rpc_server_t g_rpc_server = nullptr;
