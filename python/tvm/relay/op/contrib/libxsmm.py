a52376aef5c2245f36e2f1424206ac4978f40f51
wenxizhu <wenxizhu@tencent.com>
2022-05-16 14:53:16 +0800

    Add test cases for libxsmm BYOC disabled situations.

--- python/tvm/relay/op/contrib/libxsmm.py
+++ python/tvm/relay/op/contrib/libxsmm.py
@@ -1,32 +1,44 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# pylint: disable=invalid-name
+"""Patterns supported LIBXSMM."""
+import numpy as np
+
 import tvm
 from tvm import relay
 from tvm.ir.transform import Sequential
 from tvm.relay import transform
+
 from ...dataflow_pattern import wildcard, is_op, is_constant
 from .register import register_pattern_table
-import numpy as np
 
+
+def use_libxsmm(m, n, k):
+  """Conditions to enable libxsmm BYOC.
+     Currently we enable libxsmm when cube_root(m * n * k ) <= 256 since it has significant performance improvement."""
+  return bool(np.cbrt(m * n * k) <= 256)
 
 @tvm.ir.register_op_attr("nn.dense", "target.libxsmm")
 def dense(expr):
     args = expr.args
-    print("args[0].shape", args[0].checked_type.shape)
-    print("args[1].shape", args[1].checked_type.shape)
     m = int(args[0].checked_type.shape[0])
     n = int(args[1].checked_type.shape[0])
     k = int(args[0].checked_type.shape[1])
-    return bool(np.cbrt(m * n * k) <= 256)
-
-def _register_external_op_helper(op_name, supported=True):
-
-  @tvm.ir.register_op_attr(op_name, "target.libxsmm")
-  def _func_wrapper(expr):
-    return supported
-
-  return _func_wrapper
-
-#_register_external_op_helper("nn.bias_add")
-#_register_external_op_helper("nn.relu")
+    return use_libxsmm(m, n, k)
 
 def get_root_call(call, root_op_name):
   if not isinstance(call, relay.Call):
@@ -36,15 +48,13 @@ def get_root_call(call, root_op_name):
   return get_root_call(call.args[0], root_op_name)
 
 def check_dense_shape(call):
-  dense = get_root_call(call, "nn.dense") 
+  dense = get_root_call(call, "nn.dense")
   data = dense.args[0].checked_type
   weight = dense.args[1].checked_type
   m = int(data.shape[0])
   n = int(weight.shape[0])
   k = int(data.shape[1])
-  print("m:{}, n:{}, k:{}".format(m, n, k))
-  print("(m * n * k) ** (1/3): {}".format((m * n * k) ** (1/3) <= 256))
-  return  (m * n * k) ** (1/3) <= 256
+  return use_libxsmm(m, n, k)
 
 
 def make_dense_pattern(with_bias=False, eltwise=None):
@@ -66,7 +76,7 @@ def make_dense_pattern(with_bias=False, eltwise=None):
 
 @register_pattern_table("libxsmm")
 def pattern_table():
-    elt_list = ["nn.relu", "sigmoid", None]
+    elt_list = ["nn.relu", None]
     libxsmm_patterns = []
     for with_bias in [True, False]:
         for elt in elt_list:
--- tests/python/contrib/test_libxsmm.py
+++ tests/python/contrib/test_libxsmm.py
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 import pytest
 import numpy as np
 
@@ -9,43 +25,39 @@ from tvm.contrib import graph_runtime
 from tvm import testing
 
 
-def run_and_verify(mod, input_dict, params, target):
-    dev = tvm.cpu()
+def check_libxsmm_used(mod, libxsmm_enabled):
+    num_libxsmm_subgraphs = sum([1 if "libxsmm" in gv.name_hint else 0 for gv in mod.get_global_vars()])
+    assert num_libxsmm_subgraphs == 1 if libxsmm_enabled else num_libxsmm_subgraphs == 0
 
-    #print("input_dict:", input_dict)
-    
+def run_and_verify(mod, input_dict, params, target, libxsmm_enabled=True):
+    dev = tvm.cpu()
     result_dict = {}
     for r in ("origin", "libxsmm"):
-    #for r in ("origin", ):
-    #for r in ("libxsmm", ):
-      if r == "libxsmm":
-        mod = partition_for_libxsmm(mod)
+        if r == "libxsmm":
+            mod = partition_for_libxsmm(mod)
+            check_libxsmm_used(mod, libxsmm_enabled)
 
-      print("mod:", mod)
-
-      json, lib, param = relay.build(mod, target="llvm", params=params)
-      runtime_module = tvm.contrib.graph_runtime.create(json, lib, device=dev)
-      for k, v in input_dict.items():
-        runtime_module.set_input(k, v)
-      runtime_module.load_params(tvm.runtime.save_param_dict(param))
-      runtime_module.run()
-      result_dict[r] = runtime_module.get_output(0).asnumpy()
-      #print("{}: {}".format(r,result_dict[r]))
+        json, lib, param = relay.build(mod, target="llvm", params=params)
+        runtime_module = tvm.contrib.graph_runtime.create(json, lib, device=dev)
+        for k, v in input_dict.items():
+            runtime_module.set_input(k, v)
+        runtime_module.load_params(tvm.runtime.save_param_dict(param))
+        runtime_module.run()
+        result_dict[r] = runtime_module.get_output(0).asnumpy()
 
     tvm.testing.assert_allclose(result_dict["origin"], result_dict["libxsmm"], rtol=1e-5, atol=1e-5)
-    
 
-def run_and_verify_func(config, dtype):
+
+def run_and_verify_func(config, dtype, libxsmm_enabled=True):
     f, input_shapes, param_list = config
     params = {x: np.random.uniform(-1, 1, input_shapes[x]).astype(dtype) for x in param_list}
     input_dict = {
         k: np.random.uniform(-1, 1, v).astype(dtype)
-        #k : np.arange(np.prod(v)).reshape(v)
         for k, v in input_shapes.items()
         if k not in param_list
     }
-    #input_dict["kernel"] = np.arange(8, 8 + np.prod(input_shapes["kernel"])).reshape(input_shapes["kernel"])
-    run_and_verify(f, input_dict, params, target="llvm")
+    run_and_verify(f, input_dict, params, target="llvm", libxsmm_enabled=libxsmm_enabled)
+
 
 def get_dense(x_shape, k_shape, dtype="float32"):
     x = relay.var("x", shape=(x_shape), dtype=dtype)
@@ -53,7 +65,6 @@ def get_dense(x_shape, k_shape, dtype="float32"):
     out = relay.nn.dense(x, kernel, units=k_shape[0])
     dic = {"x": x_shape, "kernel": k_shape}
     param_list = ["kernel"]
-    #param_list = []
     return out, dic, param_list
 
 
@@ -61,7 +72,7 @@ def get_dense_with_bias(x_shape, k_shape, dtype="float32"):
     dense, dic, param_list = get_dense(x_shape, k_shape, dtype)
     bias = relay.var("bias", shape=(k_shape[0],), dtype=dtype)
     out = relay.nn.bias_add(dense, bias)
-    dic["bias"] = (k_shape[0], )
+    dic["bias"] = (k_shape[0],)
     param_list += ["bias"]
 
     return out, dic, param_list
@@ -71,23 +82,28 @@ def test_dense():
     dtype = "float32"
     x_shape = (16, 32)
     k_shape = (64, 32)
-    #x_shape = (2, 2)
-    #k_shape = (2, 2)
-    #x_shape = (1024, 2048)
-    #k_shape = (4096, 2048)
 
     dense, dic, param_list = get_dense(x_shape, k_shape, dtype=dtype)
     dense = tvm.IRModule.from_expr(dense)
     config = dense, dic, param_list
     run_and_verify_func(config, dtype=dtype)
- 
+
+
+def test_dense_libxsmm_not_enabled():
+    dtype = "float32"
+    x_shape = (1024, 1024)
+    k_shape = (1024, 1024)
+
+    dense, dic, param_list = get_dense(x_shape, k_shape, dtype=dtype)
+    dense = tvm.IRModule.from_expr(dense)
+    config = dense, dic, param_list
+    run_and_verify_func(config, dtype=dtype, libxsmm_enabled=False)
+
 
 def test_dense_with_bias():
     dtype = "float32"
     x_shape = (16, 32)
     k_shape = (64, 32)
-    #x_shape = (1024, 1024)
-    #k_shape = (1024, 1024)
 
     dense_with_bias, dic, param_list = get_dense_with_bias(x_shape, k_shape, dtype=dtype)
     dense_with_bias = tvm.IRModule.from_expr(dense_with_bias)
@@ -95,24 +111,43 @@ def test_dense_with_bias():
     run_and_verify_func(config, dtype=dtype)
 
 
+def test_dense_with_bias_libxsmm_not_enabled():
+    dtype = "float32"
+    x_shape = (256, 512)
+    k_shape = (1024, 512)
+
+    dense_with_bias, dic, param_list = get_dense_with_bias(x_shape, k_shape, dtype=dtype)
+    dense_with_bias = tvm.IRModule.from_expr(dense_with_bias)
+    config = dense_with_bias, dic, param_list
+    run_and_verify_func(config, dtype=dtype, libxsmm_enabled=False)
+
+
 def test_dense_with_relu():
     dtype = "float32"
     x_shape = (16, 32)
     k_shape = (64, 32)
-    #x_shape = (1024, 2048)
-    #k_shape = (4096, 2048)
     dense, dic, param_list = get_dense(x_shape, k_shape, dtype=dtype)
     dense_with_relu = relay.nn.relu(dense)
     dense_with_relu = tvm.IRModule.from_expr(dense_with_relu)
     config = dense_with_relu, dic, param_list
     run_and_verify_func(config, dtype=dtype)
 
+
+def test_dense_with_relu_libxsmm_not_enabled():
+    dtype = "float32"
+    x_shape = (257, 256)
+    k_shape = (256, 256)
+    dense, dic, param_list = get_dense(x_shape, k_shape, dtype=dtype)
+    dense_with_relu = relay.nn.relu(dense)
+    dense_with_relu = tvm.IRModule.from_expr(dense_with_relu)
+    config = dense_with_relu, dic, param_list
+    run_and_verify_func(config, dtype=dtype, libxsmm_enabled=False)
+
+
 def test_dense_with_bias_and_relu():
     dtype = "float32"
     x_shape = (16, 32)
     k_shape = (64, 32)
-    #x_shape = (1024, 1024)
-    #k_shape = (1024, 1024)
 
     dense_with_bias, dic, param_list = get_dense_with_bias(x_shape, k_shape, dtype=dtype)
     dense_with_bias_and_relu = relay.nn.relu(dense_with_bias)
@@ -120,11 +155,18 @@ def test_dense_with_bias_and_relu():
     config = dense_with_bias_and_relu, dic, param_list
     run_and_verify_func(config, dtype=dtype)
 
+
+def test_dense_with_bias_and_relu_libxsmm_not_enabled():
+    dtype = "float32"
+    x_shape = (256, 257)
+    k_shape = (256, 257)
+
+    dense_with_bias, dic, param_list = get_dense_with_bias(x_shape, k_shape, dtype=dtype)
+    dense_with_bias_and_relu = relay.nn.relu(dense_with_bias)
+    dense_with_bias_and_relu = tvm.IRModule.from_expr(dense_with_bias_and_relu)
+    config = dense_with_bias_and_relu, dic, param_list
+    run_and_verify_func(config, dtype=dtype, libxsmm_enabled=False)
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
-    
-    #test_dense()
-    #test_dense_with_bias()
-    #test_dense_with_relu()
-    #test_dense_with_bias_and_relu()
-
