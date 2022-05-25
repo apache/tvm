7519947f713944e33d0576bb70f8fdb4e8389fb3
tqchen <tianqi.tchen@gmail.com>
2022-05-25 16:10:37 -0400

    [FFI][CYTHON] Release GIL when calling into long running functions
    Unlike ctypes, Cython by default do not release GIL when
    calling into C API functions. This causes problems when the
    function is long running. As the particular calling thread will
    block other python threads by holding the GIL.
    
    This PR explicitly releases GIL when calling into possible
    long running functions. It fixes the timeout issue in
    PopenPool which previously relied on another python thread
    for timeout.
    
    Added a regression test-case by changing sleep to sleep
    in FFI, which previously will indefinitely block the popen tests.

--- src/support/ffi_testing.cc
+++ src/support/ffi_testing.cc
@@ -28,6 +28,7 @@
 #include <tvm/te/tensor.h>
 #include <tvm/tir/expr.h>
 
+#include <chrono>
 #include <thread>
 
 namespace tvm {
@@ -158,5 +159,10 @@ runtime::Module NewFrontendTestModule() {
 }
 
 TVM_REGISTER_GLOBAL("testing.FrontendTestModule").set_body_typed(NewFrontendTestModule);
+
+TVM_REGISTER_GLOBAL("testing.sleep_in_ffi").set_body_typed([](double timeout) {
+  std::chrono::duration<int64_t, std::nano> duration(static_cast<int64_t>(timeout * 1e9));
+  std::this_thread::sleep_for(duration);
+});
 
 }  // namespace tvm
