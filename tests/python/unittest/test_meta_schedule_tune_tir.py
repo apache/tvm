add70cc35a0cad90e87fecd76a0eb392e3fa03ad
YJ Shi <yuanjing@octoml.ai>
2022-05-25 12:54:08 -0700

    add test for run_module_via_rpc

--- tests/python/unittest/test_meta_schedule_tune_tir.py
+++ tests/python/unittest/test_meta_schedule_tune_tir.py
@@ -17,12 +17,20 @@
 # pylint: disable=missing-docstring
 import logging
 import tempfile
+import time
+import numpy as np
 
 import pytest
+import tvm
+
+from tvm import meta_schedule as ms
 from tvm.meta_schedule import TuneConfig, tune_tir
+from tvm.meta_schedule.testing.custom_builder_runner import run_module_via_rpc
 from tvm.script import tir as T
 from tvm.target import Target
 from tvm.tir import Schedule
+from tvm.rpc.tracker import Tracker
+from tvm.rpc.server import Server
 
 logging.basicConfig()
 logging.getLogger("tvm.meta_schedule").setLevel(logging.DEBUG)
@@ -89,6 +97,63 @@ def test_tune_matmul_cuda():
             print(sch.trace)
 
 
+def test_tune_run_module_via_rpc():
+    # pylint: disable=import-outside-toplevel
+
+    tracker = Tracker(host="127.0.0.1", port=9000, port_end=10000, silent=True)
+    device_key = "$local$device$%d" % tracker.port
+    server = Server(
+        port=tracker.port,
+        port_end=10000,
+        key=device_key,
+        silent=True,
+        tracker_addr=("127.0.0.1", tracker.port),
+    )
+    rpc_config = ms.runner.RPCConfig(
+        tracker_host=tracker.host,
+        tracker_port=tracker.port,
+        tracker_key=device_key,
+        session_timeout_sec=3600,
+    )
+    # Wait for the processes to start
+    time.sleep(0.5)
+
+    target = tvm.target.Target("llvm")
+    rt_mod = tvm.build(matmul, target)
+
+    # construct the input
+    input_data = []
+    input_shape = (128, 128)
+    input_dtype = "float32"
+    dev = tvm.cpu()
+    a_np = np.random.uniform(size=input_shape).astype(input_dtype)
+    b_np = np.random.uniform(size=input_shape).astype(input_dtype)
+    c_np = np.zeros(input_shape).astype(input_dtype)
+    for i in range(128):
+        for j in range(128):
+            for k in range(128):
+                c_np[i, j] = c_np[i, j] + a_np[i, k] * b_np[j, k]
+    input_data.append(tvm.nd.array(a_np, dev))
+    input_data.append(tvm.nd.array(b_np, dev))
+    input_data.append(tvm.nd.array(np.zeros(input_shape).astype(input_dtype), dev))
+
+    def f_timer(rt_mod, dev, input_data):
+        rt_mod(input_data[0], input_data[1], input_data[2])
+        return input_data[2]
+
+    result = run_module_via_rpc(
+        rpc_config=rpc_config,
+        lib=rt_mod,
+        dev_type=target.kind.name,
+        args=input_data,
+        continuation=f_timer,
+    )
+    tvm.testing.assert_allclose(result.numpy(), c_np, rtol=1e-3)
+    tracker.terminate()
+    server.terminate()
+
+
 if __name__ == """__main__""":
     test_tune_matmul_cpu()
     test_tune_matmul_cuda()
+    test_tune_run_module_via_rpc()
