c84b30e0423ee642c7a838c4a3ef2dbb97f04145
uhj1lud <ulrik.hjort@se.bosch.com>
2022-05-30 17:14:23 +0200

    Constant name prefix added

--- src/relay/backend/te_compiler_cache.cc
+++ src/relay/backend/te_compiler_cache.cc
@@ -193,7 +193,7 @@ class LowerToTECompute : public backend::MemoizedExprTranslator<Array<te::Tensor
     } else {
       const auto* ttype = op->checked_type().as<TensorTypeNode>();
       std::stringstream ss;
-      ss << "constant_" << const_index++;
+      ss << "cc_constant_" << const_index++;
       tvm::te::Tensor tensor = tvm::te::placeholder(GetShape(ttype->shape), ttype->dtype, ss.str());
       constant_tensors_[op] = tensor;
       return {tensor};
