f5f839b127262f5ac6251ecda048f3a472780184
Altan Haan <ahaan@octoml.ai>
2022-06-01 16:26:15 -0700

    lint

--- python/tvm/topi/nn/lstm.py
+++ python/tvm/topi/nn/lstm.py
@@ -14,6 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# pylint: disable=invalid-name
+"""General LSTM implementation using TE scan."""
 from tvm import te, tir
 
 
--- tests/python/topi/python/test_topi_lstm.py
+++ tests/python/topi/python/test_topi_lstm.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# pylint: disable=invalid-name
 """Test code for LSTM."""
 import numpy as np
 from rsa import verify
