ec991acdc5199ed21824030174dd0872c0b713e9
An Wang <anwang2009@gmail.com>
2022-06-07 13:25:19 -0700

    add more dynamism robustness to eyelike onnx importer

--- tests/python/frontend/onnx/test_forward.py
+++ tests/python/frontend/onnx/test_forward.py
@@ -5072,18 +5072,22 @@ def test_cumsum(target, dev):
 
 @tvm.testing.parametrize_targets
 def test_eyelike(target, dev):
-    if target != "llvm":
-        pytest.skip()
-
     def verify_eyelike(indata, dynamic=False):
         node_list = []
         eyelike_inputs = ["X"]
-        if dynamic:
-            shape_node = helper.make_node("Shape", ["X"], ["shape"])
-            reshape_node = helper.make_node("Reshape", ["X", "shape"], ["X_dyn"])
+        input_node_list = [
+            helper.make_tensor_value_info("X", TensorProto.FLOAT, list(indata.shape))
+        ]
+        input_list = [indata]
 
+        if dynamic:
+            input_node_list.append(
+                helper.make_tensor_value_info("shape", TensorProto.INT64, [len(indata.shape)])
+            )
+            input_list.append(np.asarray(indata.shape))
+            reshape_node = helper.make_node("Reshape", ["X", "shape"], ["X_dyn"])
             eyelike_inputs[0] = "X_dyn"
-            node_list += [shape_node, reshape_node]
+            node_list += [reshape_node]
 
         node = helper.make_node(
             "EyeLike",
@@ -5095,14 +5099,13 @@ def test_eyelike(target, dev):
         graph = helper.make_graph(
             node_list,
             "eyelike_test",
-            inputs=[helper.make_tensor_value_info("X", TensorProto.FLOAT, list(indata.shape))],
+            inputs=input_node_list,
             outputs=[helper.make_tensor_value_info("Y", TensorProto.FLOAT, list(indata.shape))],
         )
 
         model = helper.make_model(graph, producer_name="eyelike_test")
-
         verify_with_ort_with_inputs(
-            model, [indata], dtype="float32", opset=9, target=target, dev=dev
+            model, input_list, dtype="float32", opset=9, target=target, dev=dev, use_vm=True
         )
 
     input_data = np.zeros((5, 5), dtype=np.float32)
