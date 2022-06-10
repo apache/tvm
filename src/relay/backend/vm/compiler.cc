0b46efa33e045ab4d892e40c1f5084dd6651c8b6
Mark Shields <87091372+mbs-octoml@users.noreply.github.com>
2022-06-09 18:50:06 -0700

    [BUG] Disable second PlanDevices pass (#11662)
    Though started with the best of intentions, the second
    PlanDevices pass to account for memory scope's introduced
    by lowering is buggy and not ready for prime time. It
    has caused an ICHECK fail since for some reason the new
    constraints are not flowing into device_copies.

--- src/relay/backend/vm/compiler.cc
+++ src/relay/backend/vm/compiler.cc
@@ -244,6 +244,7 @@ class VMFunctionCompiler : DeviceAwareExprFunctor<void(const Expr& n)> {
         host_virtual_device_(std::move(host_virtual_device)) {}
 
   VMFunction Compile(const GlobalVar& var, const Function& func) {
+    VLOG(1) << "Compiling:" << std::endl << PrettyPrint(func);
     std::vector<Index> param_device_indexes;
     if (IsClosure(func)) {
       // After lifting we'll have functions of the form:
@@ -1102,9 +1103,8 @@ IRModule VMCompiler::OptimizeModuleImpl(IRModule mod) {
   // let-bound functions.
   pass_seqs.push_back(DeadCodeElimination(/*inline_once=*/false));
 
-  // Now that we have PrimFuncs, flow and solve VirtualDevice constraints again to account for
-  // any memory scopes which lowering has settled on.
-  pass_seqs.push_back(transform::PlanDevices(config_));
+  // At this point it's possible to run PlanDevices again to pick up any additional constraints
+  // introduced during lowering. However we'll not do this until more testing has been done.
 
   // Inline the functions that are lifted to the module scope. We perform this
   // pass after all other optimization passes but before the memory allocation
