9d98da27361429cb558930032f074172bc99b7c3
Jyotsna Verma <73191103+jverma-quic@users.noreply.github.com>
2022-06-15 12:40:37 -0500

    [Hexagon] Implement avg_pool2d slice op (#11417)
    * Implement avg_pool2d slice op
    
    * Address review comments and fix the STIR schedule
    
    * Fix formatting issues
    
    * Address pylint errors
    
    * Additional formatting issues
    
    * more pylint fixes
    
    * Changed arch version to v68 for now
    
    * Changing arch version back to v69
    
    * Move the test to tests/python/contrib/test_hexagon/topi

--- tests/python/contrib/test_hexagon/infrastructure.py
+++ tests/python/contrib/test_hexagon/infrastructure.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+# pylint: disable=invalid-name
 
 """ Hexagon testing infrastructure """
 
@@ -228,3 +229,22 @@ def conv2d_compute(X, filt, pad, stride, dilation):
         )
 
     return output_shape, compute
+
+
+def transform_numpy(arr_np, current_layout: str, new_layout: str):
+    """Reshape and transpose numpy array according to the specified layout"""
+    if current_layout == "nhwc":
+        if new_layout == "nhwc":
+            return arr_np
+        if new_layout in ["nhwc-8h2w32c2w-2d", "nhwc-8h2w32c2w-1d"]:
+            n, h, w, c = arr_np.shape
+            return arr_np.reshape([n, h // 8, 8, w // 4, 2, 2, c // 32, 32]).transpose(
+                0, 1, 3, 6, 2, 4, 7, 5
+            )
+        if new_layout in ["n11c-1024c-2d", "n11c-1024c-1d"]:
+            n, h, w, c = arr_np.shape
+            assert h == 1 and w == 1, "The size of h and w must be 1"
+            return arr_np.reshape([n, 1, 1, c // 1024, 1024])
+
+        raise RuntimeError(f"Unexpected new_layout '{new_layout}'")
+    raise RuntimeError(f"Unexpected current_layout '{current_layout}'")
