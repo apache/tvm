9b95eded2da3b7194ed4efde9a0e54da66fd7580
Ivy <yan3.zhang@intel.com>
2022-06-20 14:29:52 +0800

    support dnnl.global_avg_pooling2d

--- python/tvm/relay/op/contrib/dnnl.py
+++ python/tvm/relay/op/contrib/dnnl.py
@@ -88,6 +88,7 @@ _register_external_op_helper("nn.conv3d_transpose")
 _register_external_op_helper("nn.dense")
 _register_external_op_helper("nn.max_pool2d")
 _register_external_op_helper("nn.avg_pool2d")
+_register_external_op_helper("nn.global_avg_pool2d")
 _register_external_op_helper("nn.max_pool3d")
 _register_external_op_helper("nn.avg_pool3d")
 _register_external_op_helper("abs")
@@ -551,6 +552,7 @@ class IsComputeIntensiveGraph(ExprVisitor):
                 "nn.conv3d_transpose",
                 "nn.dense",
                 "nn.layer_norm",
+                "nn.global_avg_pool2d",
             ]
         )
         if isinstance(call.op, tvm.tir.op.Op):
