0cf333285a7aac1af9365890acccd3a31d9471cd
Michael J. Klaiber <michael.klaiber@de.bosch.com>
2022-06-27 08:42:24 +0000

    [UMA] refactoring of test_uma_lowering_with_umalower.py

---
+++ tests/python/unittest/test_uma_lowering_with_umalower.py
@@ -0,0 +1,88 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import tvm
+from tests.python.unittest.test_uma_utils import _create_schedule, _generate_io_arrays, conv2d_c_code
+from tvm import topi, IRModule
+from tvm.relay.backend.contrib.uma._template.passes import my_ai_hw_conv2d_pass
+import tvm.testing
+from tvm import te
+from tvm.relay.backend.contrib.uma.api.lower import UMALower
+from tvm.relay.backend.contrib.uma.api.utils import PassPhase
+
+
+def _conv2d_te_definition(shapes: dict) -> list:
+    n, w, h, ci, kw, kh, co = shapes["n"], shapes["w"], shapes["h"], shapes["ci"], shapes["kw"], shapes["kh"], shapes["co"],
+    ifmap = te.placeholder((n, ci, w, h), dtype="float32", name="ifmap")
+    weights = te.placeholder((co, ci, kw, kh), dtype="float32", name="weights")
+    result = topi.nn.conv2d_nchw(ifmap, weights, stride=1, padding=1, dilation=1)
+    return [ifmap, weights, result]
+
+
+def _pepare_conv2d_schedule(shapes, use_external_conv2d_impl=True, ):
+    placeholders = _conv2d_te_definition(shapes)
+    sch_tir = _create_schedule(placeholders, conv2d_c_code, use_external_conv2d_impl=use_external_conv2d_impl)
+    return placeholders, sch_tir
+
+
+def _run_external_conv2d(dut_io_arrays, conv2d_shapes, target):
+    # Run conv2d with external function
+    placeholders, schedule = _pepare_conv2d_schedule(conv2d_shapes)
+
+    uma_lower = UMALower("lower_test")
+    uma_lower._tir_passes.append((PassPhase.TIR_PHASE_0, my_ai_hw_conv2d_pass()))
+    with tvm.transform.PassContext():
+        tir_mod = uma_lower._lower_stir_to_nstir(schedule.mod["main"])
+
+    ifmap_data, weight_data, result_data = dut_io_arrays
+
+    llvm_conv2d_mod = tvm.build(tir_mod, placeholders, target=target, name="test_external_conv2d")
+    llvm_conv2d_mod(ifmap_data, weight_data, result_data)
+
+
+def _run_reference_conv2d(reference_io_arrays, conv2d_shapes, target):
+    placeholders, schedule = _pepare_conv2d_schedule(conv2d_shapes)
+    ref_mod = tvm.build(schedule.mod, placeholders, target=target, name="test_reference_conv2d")
+    ifmap, weights, result = reference_io_arrays
+    ref_mod(ifmap, weights, result)
+
+
+def test_lower_with_uma():
+    target = tvm.target.Target(target="llvm", host="llvm")
+    dev = tvm.device(target.kind.name, 0)
+    conv2d_shapes = dict(n=1, w=224, h=224, ci=3, kw=3, kh=3, co=1)
+
+    dut_io_arrays, reference_io_arrays = _prepare_io_arrays(conv2d_shapes, dev)
+
+    _run_external_conv2d(dut_io_arrays, conv2d_shapes, target)
+    _run_reference_conv2d(reference_io_arrays, conv2d_shapes, target)
+
+    # compare results
+    dut_results = dut_io_arrays[2].numpy()
+    ref_results = reference_io_arrays[2].numpy()
+    tvm.testing.assert_allclose(dut_results, ref_results, rtol=1e-5)
+
+
+def _prepare_io_arrays(conv2d_shapes, dev):
+    dut_io_arrays = _generate_io_arrays(conv2d_shapes, dev)
+    _, _, ref_result = _generate_io_arrays(conv2d_shapes, dev)
+    reference_io_arrays = [dut_io_arrays[0], dut_io_arrays[1], ref_result]
+    return dut_io_arrays, reference_io_arrays
+
+
+if __name__ == "__main__":
+    test_lower_with_uma()
---
+++ tests/python/unittest/test_uma_utils.py
@@ -0,0 +1,130 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+import tvm
+from tvm import topi, IRModule
+import numpy as np
+from tvm.contrib import utils, clang
+import tvm.testing
+from tvm import te
+
+conv2d_c_code = """
+extern "C" int my_hw_ai_conv2dnchw(float* ifmap, float*  weights, float*  result) {
+  result[0] = 42.0;
+  result[1] = 3.14;  // int iw = 7;
+  // int ih = 7;
+  // int ic = 512;
+  // int oc = 42;
+  // int kh = 3;
+  // int kw = 3;
+
+
+  int ic = 3;
+  int oc = 1;
+  int iw = 224;
+  int ih = 224;
+  int kh = 3;
+  int kw = 3;
+
+  int kw_low = kw / 2;
+  int kh_low = kh / 2;
+  int kw_high = iw + kw / 2;
+  int kh_high = ih + kh / 2;
+
+  int padded_iw = iw + 2 * kw_low; 
+  int padded_ih = ih + 2 * kh_low;
+
+  float* pad_temp = new float[(((ic * padded_iw * padded_ih) + (padded_ih * padded_iw)) + padded_iw)];
+
+  if (pad_temp == nullptr) {
+    return -1;
+  }
+
+  int shift = padded_iw * kh_low + kw_low;
+
+  for (int i1 = 0; i1 < ic; ++i1) {
+    for (int i2 = 0; i2 < padded_ih; ++i2) {
+      for (int i3 = 0; i3 < padded_iw; ++i3) {
+        ((float*)pad_temp)[(((i1 * padded_iw * padded_ih) + (i2 * padded_iw)) + i3)] = 
+           (((((kh_low <= i2) && (i2 < kh_high)) && (kw_low <= i3)) && (i3 < kw_high)) ? ifmap[((((i1 * iw * ih) + (i2 * iw)) + i3) - shift)] : 0.000000e+00f);
+      }
+    }
+  }
+  for (int i11 = 0; i11 < oc; ++i11) { 
+    for (int i21 = 0; i21 < ih; ++i21) { 
+      for (int i31 = 0; i31 < iw; ++i31) { 
+        for (int i4 = 0; i4 < ic; ++i4) { 
+          for (int i5 = 0; i5 < kh; ++i5) { 
+            for (int i6 = 0; i6 < kw; ++i6) { 
+              int cse_var_1 = (((i11 * iw*ih) + (i21 * iw)) + i31);
+              if (((i4 == 0) && (i5 == 0)) && (i6 == 0)) {
+                result[cse_var_1] = 0.000000e+00f;
+              }
+              result[cse_var_1] = (result[cse_var_1] 
+              + (((float*)pad_temp)[i4 * padded_iw * padded_ih + (i21+i5) * padded_iw + i31 + i6] 
+              * weights[((((i11 * ic * kh * kw) + (i4 * kh * kw)) + (i5 * kw)) + i6)]));
+            }
+          }
+        }
+      }
+    }
+  }
+  delete[] pad_temp;
+  return 0;
+}
+"""
+
+
+def _create_schedule(placeholder: list, c_code_str: str = "", use_external_conv2d_impl: bool = True):
+    # How to do the same with TE
+    # Add pragma TE
+    # s = te.create_schedule(result.op)
+    # axis = result.op.axis
+    # s[result].pragma(axis[0], "import_llvm", c_to_llvm())
+    # with tvm.transform.PassContext(config={"tir.add_lower_pass": [(1, my_ai_hw_conv2d_pass)]}):
+    #     mod = tvm.lower(s, [ifmap, weights, result], simple_mode=True)
+    #
+    # llvm_mod = tvm.build(mod, [ifmap, weights, result], target=target, name="test_external_conv2d")
+    # llvm_mod(ifmap_data, weight_data, result_data)
+
+    assert use_external_conv2d_impl and c_code_str != "" \
+           or not use_external_conv2d_impl and c_code_str == ""
+
+    def _c_to_llvm(c_code: str) -> str:
+        temp = utils.tempdir()
+        ll_path = temp.relpath("conv2d.ll")
+        ll_code = clang.create_llvm([c_code], output=ll_path)
+        return ll_code
+
+    func_tir = te.create_prim_func(placeholder)
+    ir_module_from_te = IRModule({"main": func_tir})
+    sch_tir = tvm.tir.Schedule(ir_module_from_te)
+    if use_external_conv2d_impl:
+        conv2d_b = sch_tir.get_block("conv2d_nchw")
+        conv2d_l = sch_tir.get_loops(conv2d_b)
+        sch_tir.annotate(conv2d_l[0], "pragma_import_llvm", _c_to_llvm(c_code_str))
+    return sch_tir
+
+
+def _generate_io_arrays(shapes: dict, dev):
+    n, w, h, ci, kw, kh, co = shapes["n"], shapes["w"], shapes["h"], shapes["ci"], shapes["kw"], shapes["kh"], shapes["co"],
+
+    ifmap_data = tvm.nd.array(np.random.uniform(size=(n, ci, w, h)).astype("float32"), dev)
+    weight_data = tvm.nd.array(np.random.uniform(size=(co, ci, kh, kw)).astype("float32"), dev)
+    result_data = tvm.nd.array(np.zeros((n, co, w, h)).astype("float32"), dev)
+    return ifmap_data, weight_data, result_data
