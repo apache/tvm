6e10636255c45e2edd25d0bca5258aa5e08e2563
Josh Herrera <joshherr@quicinc.com>
2022-06-28 13:32:21 -0700

    Update for llvm api

--- src/target/llvm/codegen_hexagon.cc
+++ src/target/llvm/codegen_hexagon.cc
@@ -80,7 +80,9 @@ class CodeGenHexagon final : public CodeGenCPU {
   llvm::Module* GetModulePtr() const { return module_.get(); }
 
   uint64_t GetTypeSizeInBits(llvm::Type* type) const {
-#if TVM_LLVM_VERSION >= 100
+#if TVM_LLVM_VERSION >= 110
+    return data_layout_->getTypeSizeInBits(type).getFixedSize();
+#elif TVM_LLVM_VERSION >= 100
     return data_layout_->getTypeSizeInBits(type).getFixedValue();
 #else
     return data_layout_->getTypeSizeInBits(type);
