1b9d74a51cd008b960c914e9b28a307398c192d5
Andrew Luo <andrew.zhao.luo@gmail.com>
2022-06-28 14:35:58 -0700

    fix

--- tests/python/all_platform_minimal_test/test_runtime_ndarray.py
+++ tests/python/all_platform_minimal_test/test_runtime_ndarray.py
@@ -16,41 +16,46 @@
 # under the License.
 """Basic runtime enablement test."""
 
-import tvm
-from tvm import te
 import numpy as np
+import tvm
 import tvm.testing
+from tvm import te
 
 
 @tvm.testing.uses_gpu
 def test_nd_create():
-    for target, dev in tvm.testing.enabled_targets():
+    """Test creating an array in TVM."""
+    for _, dev in tvm.testing.enabled_targets():
         for dtype in ["uint8", "int8", "uint16", "int16", "uint32", "int32", "float32"]:
-            x = np.random.randint(0, 10, size=(3, 4))
-            x = np.array(x, dtype=dtype)
-            y = tvm.nd.array(x, device=dev)
-            z = y.copyto(dev)
-            assert y.dtype == x.dtype
-            assert y.shape == x.shape
-            assert isinstance(y, tvm.nd.NDArray)
-            np.testing.assert_equal(x, y.numpy())
-            np.testing.assert_equal(x, z.numpy())
+            main_data = np.random.randint(0, 10, size=(3, 4))
+            np_array = np.array(main_data, dtype=dtype)
+            tvm_array = tvm.nd.array(np_array, device=dev)
+            tvm_array_device = tvm_array.copyto(dev)
+            assert tvm_array.dtype == np_array.dtype
+            assert tvm_array.shape == np_array.shape
+            assert isinstance(tvm_array, tvm.nd.NDArray)
+            np.testing.assert_equal(np_array, tvm_array.numpy())
+            np.testing.assert_equal(np_array, tvm_array_device.numpy())
         # no need here, just to test usablity
         dev.sync()
 
 
 def test_fp16_conversion():
-    n = 100
+    """Test converting to and from FP16."""
+    arr_size = 100
+
+    def get_compute_func(placeholder_a, dst):
+        return lambda i: placeholder_a[i].astype(dst)
 
     for (src, dst) in [("float32", "float16"), ("float16", "float32")]:
-        A = te.placeholder((n,), dtype=src)
-        B = te.compute((n,), lambda i: A[i].astype(dst))
+        placeholder_a = te.placeholder((arr_size,), dtype=src)
+        result_b = te.compute((arr_size,), get_compute_func(placeholder_a, dst))
 
-        s = te.create_schedule([B.op])
-        func = tvm.build(s, [A, B], "llvm")
+        schedule = te.create_schedule([result_b.op])
+        func = tvm.build(schedule, [placeholder_a, result_b], "llvm")
 
-        x_tvm = tvm.nd.array(100 * np.random.randn(n).astype(src) - 50)
-        y_tvm = tvm.nd.array(100 * np.random.randn(n).astype(dst) - 50)
+        x_tvm = tvm.nd.array(100 * np.random.randn(arr_size).astype(src) - 50)
+        y_tvm = tvm.nd.array(100 * np.random.randn(arr_size).astype(dst) - 50)
 
         func(x_tvm, y_tvm)
 
