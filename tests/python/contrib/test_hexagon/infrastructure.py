7323da276c0206ddf5c51e8297d6c3c088a66abe
arangasa <76030063+arangasa@users.noreply.github.com>
2022-06-30 02:52:35 +0530

    [TOPI][Hexagon] Implement Argmax Slice Op (#11847)
    * [TOPI][Hexagon] Implement Argmax Slice Op
    
    * run through black
    
    * Address initial review comments
    
    * Fix variable names in tests
    
    * Fix lint issue
    
    Co-authored-by: arangasa (generated by with_the_same_user script) <arangasa@hu-arangasa-hyd.qualcomm.com>

--- tests/python/contrib/test_hexagon/infrastructure.py
+++ tests/python/contrib/test_hexagon/infrastructure.py
@@ -247,4 +247,12 @@ def transform_numpy(arr_np, current_layout: str, new_layout: str):
             return arr_np.reshape([n, 1, 1, c // 1024, 1024])
 
         raise RuntimeError(f"Unexpected new_layout '{new_layout}'")
+
+    if current_layout == "nhw":
+        if new_layout in ["nhw-32h16w-2d"]:
+            n, h, w = arr_np.shape
+            return arr_np.reshape([n, h // 32, 32, w // 16, 16]).transpose(0, 1, 3, 2, 4)
+
+        raise RuntimeError(f"Unexpected new_layout '{new_layout}'")
+
     raise RuntimeError(f"Unexpected current_layout '{current_layout}'")
