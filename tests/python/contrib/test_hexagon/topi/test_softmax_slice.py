f91ad9177b5ad48b4a4022e9c88bf40551e9cda9
Mehrdad Hessar <mhessar@octoml.ai>
2022-06-29 19:18:15 -0700

    moved test

--- tests/python/contrib/test_hexagon/topi/test_softmax_slice.py
+++ tests/python/contrib/test_hexagon/topi/test_softmax_slice.py
@@ -22,7 +22,7 @@ import tvm
 from tvm import te
 from tvm.topi.testing import softmax_python
 import tvm.topi.hexagon.slice_ops as sl
-from .infrastructure import allocate_hexagon_array
+from ..infrastructure import allocate_hexagon_array
 
 
 def transform_numpy(arr_np, layout):
