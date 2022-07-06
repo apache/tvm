070dccac97ac028d5ecbb1dbddb871e5d64bb5cc
Anirudh Sundar <quic_sanirudh@quicinc.com>
2022-07-06 20:22:44 +0530

    [Topi] [Hexagon] Conv2d slice op initial version (#11489)

--- python/tvm/topi/hexagon/slice_ops/__init__.py
+++ python/tvm/topi/hexagon/slice_ops/__init__.py
@@ -23,3 +23,4 @@ from .argmax import argmax_compute, argmax_schedule
 from .batch_flatten import batch_flatten_compute, batch_flatten_stir_schedule
 from .softmax_slice import *
 from .clip import *
+from .conv2d import *
