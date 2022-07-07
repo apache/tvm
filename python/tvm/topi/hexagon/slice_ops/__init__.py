c76d8e2bdb0a11cbdc30bdfd631963ba9813662a
abhikran-quic <63697863+abhikran-quic@users.noreply.github.com>
2022-07-07 23:49:01 +0530

    [TOPI] [Hexagon] Reshape slice op (#11983)
    * Reshape slice op. This patch adds the initial python implementation reshape slice op for hexagon.
    
    * Add tests for reshape op

--- python/tvm/topi/hexagon/slice_ops/__init__.py
+++ python/tvm/topi/hexagon/slice_ops/__init__.py
@@ -24,3 +24,4 @@ from .batch_flatten import batch_flatten_compute, batch_flatten_stir_schedule
 from .softmax_slice import *
 from .clip import *
 from .conv2d import *
+from .reshape import reshape_compute, reshape_stir_schedule
