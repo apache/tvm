6caca21e7deb0abd41784217b305d0f2391ab555
Leandro Nunes <leandro.nunes@arm.com>
2022-07-08 16:17:19 +0100

    [docs][tvmc] Fix ResNet50 model URL
    Fix the ResNet50 Models in both tvmc tutorials so that the commands
    suggested will work fine.
    
    Co-Authored-By: Liam Sturge <Liam.Sturge@arm.com>

--- gallery/tutorial/tvmc_python.py
+++ gallery/tutorial/tvmc_python.py
@@ -29,7 +29,7 @@ Follow the steps to download a resnet model via the terminal:
 
      mkdir myscripts
      cd myscripts
-     wget https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet50-v2-7.onnx
+     wget https://github.com/onnx/models/raw/b9a54e89508f101a1611cd64f4ef56b9cb62c7cf/vision/classification/resnet/model/resnet50-v2-7.onnx
      mv resnet50-v2-7.onnx my_model.onnx
      touch tvmcpythonintro.py
 
