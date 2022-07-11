f8f7c72874aef674af6ae57d9d35602d6c7ca0cf
Alan MacDonald <alanmacd@users.noreply.github.com>
2022-07-11 15:24:29 -0700

    update pylint.sh with new filenames

--- tests/lint/pylint.sh
+++ tests/lint/pylint.sh
@@ -19,8 +19,29 @@ set -euxo pipefail
 
 python3 -m pylint python/tvm --rcfile="$(dirname "$0")"/pylintrc
 python3 -m pylint vta/python/vta --rcfile="$(dirname "$0")"/pylintrc
-python3 -m pylint tests/python/unittest/test_tvmscript_type.py --rcfile="$(dirname "$0")"/pylintrc
 python3 -m pylint tests/python/contrib/test_cmsisnn --rcfile="$(dirname "$0")"/pylintrc
 python3 -m pylint tests/python/relay/aot/*.py --rcfile="$(dirname "$0")"/pylintrc
 python3 -m pylint tests/python/ci --rcfile="$(dirname "$0")"/pylintrc
 python3 -m pylint tests/python/integration/ --rcfile="$(dirname "$0")"/pylintrc
+
+# tests/python/unitest/
+python3 -m pylint tests/python/unittest/test_crt.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_micro_model_library_format.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_micro_project_api.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_micro_transport.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_node_reflection.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_container.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_error.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_extension.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_graph.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_graph_cuda_graph.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_graph_debug.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_measure.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_module_based_interface.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_module_export.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_module_load.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_profiling.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_rpc.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_trace.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_runtime_vm_profiler.py --rcfile="$(dirname "$0")"/pylintrc
+python3 -m pylint tests/python/unittest/test_tvmscript_type.py --rcfile="$(dirname "$0")"/pylintrc
