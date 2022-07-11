9d0828051109455b0d579c7bc5a147f8442c8826
Alan MacDonald <alanmacd@users.noreply.github.com>
2022-07-11 15:00:04 -0700

    pylint test_runtime_vm_profiler.py

--- tests/python/unittest/test_runtime_vm_profiler.py
+++ tests/python/unittest/test_runtime_vm_profiler.py
@@ -14,6 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""Test runtime VM profiler"""
+
 import numpy as np
 
 import tvm
@@ -25,6 +27,7 @@ from tvm.relay.testing import mlp
 
 @tvm.testing.parametrize_targets
 def test_basic(dev, target):
+    """Test basic"""
     mod, params = mlp.get_workload(batch_size=1)
     if not profiler_vm.enabled():
         return
@@ -32,15 +35,15 @@ def test_basic(dev, target):
     exe = relay.vm.compile(mod, target, params=params)
     code, lib = exe.save()
     des_exe = tvm.runtime.vm.Executable.load_exec(code, lib)
-    vm = profiler_vm.VirtualMachineProfiler(des_exe, dev)
+    vmp = profiler_vm.VirtualMachineProfiler(des_exe, dev)
 
     data = np.random.rand(1, 1, 28, 28).astype("float32")
-    res = vm.profile(tvm.nd.array(data), func_name="main")
+    res = vmp.profile(tvm.nd.array(data), func_name="main")
     assert "softmax" in str(res)
 
 
 def test_vm_reshape_and_copy():
-    target = "llvm"
+    """Test VM reshape and copy"""
     dev = tvm.gpu()
     x_np = np.random.uniform(size=(8, 16)).astype("float32")
     x = relay.var("x", shape=(8, 16), dtype="float32")
@@ -48,10 +51,10 @@ def test_vm_reshape_and_copy():
     mod = tvm.IRModule()
     mod["main"] = relay.Function([x], y)
     with tvm.transform.PassContext(opt_level=3):
-        exec = relay.vm.compile(mod, "llvm")
-    assert "reshape_tensor" in exec.bytecode
-    vm = profiler_vm.VirtualMachineProfiler(exec, dev)
-    vm.profile(tvm.nd.array(x_np))
+        exc = relay.vm.compile(mod, "llvm")
+    assert "reshape_tensor" in exc.bytecode
+    vmp = profiler_vm.VirtualMachineProfiler(exc, dev)
+    vmp.profile(tvm.nd.array(x_np))
 
 
 if __name__ == "__main__":
