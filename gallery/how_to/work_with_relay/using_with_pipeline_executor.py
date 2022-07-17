873e0273a40e67cb6e9e15d9fc7342f64938eb8e
huajsj <huaj@xilinx.com>
2022-07-17 15:03:50 -0700

    fix doc convention issue

--- gallery/how_to/work_with_relay/using_with_pipeline_executor.py
+++ gallery/how_to/work_with_relay/using_with_pipeline_executor.py
@@ -117,12 +117,12 @@ testing.utils.install_request_hook(depth=3)
 # sphinx_gallery_end_ignore
 #########################################
 # Build the subgraph with cutlass target.
-# ---------------------------------------
-#########################################
+#----------------------------------------
+
 cutlass = tvm.target.Target(
     {
         "kind": "cutlass",
-        "sm": 80,
+        "sm": int(tvm.target.Target("cuda").arch.split("_")[1]),
         "use_3xtf32": True,
         "split_k_slices": [1],
         "profile_all_alignments": False,
@@ -149,131 +149,127 @@ def cutlass_build(mod, target, params=None, target_host=None, mod_name="default"
 # Define a function to do all the codegen and pipeline executor works.
 # To run pipeline executor with dnnl, USE_PIPELINE_EXECUTOR need to get set as ON.
 # and the 'USE_CUTLASS' should set as ON in config.cmake.
-def run_pipeline():
-    from tvm.contrib import graph_executor, pipeline_executor, pipeline_executor_build
+from tvm.contrib import graph_executor, pipeline_executor, pipeline_executor_build
 
-    #########################################
-    # Create subgraph pipeline configuration.
-    # Associate the subgraph module with a target.
-    # Using BYOC to set the codegen of the second subgraph module.
-    # To use cutlass the 'USE_CUTLASS' should set as ON.
-    mod0, mod1 = subgraphs[0], subgraphs[1]
-    # Apply cutlass as the codegen.
-    mod1 = partition_for_cutlass(mod1)
-    #################################################
-    # Get the pipeline executor configuration object.
-    pipe_config = pipeline_executor_build.PipelineConfig()
-    ###########################################################################
-    # Set the compile target of the second subgraph module for example as LLVM.
-    pipe_config[mod0].target = "llvm"
-    pipe_config[mod0].dev = tvm.cpu(0)
-    ###############################################################################
-    # Set the cpu afinity for control flow, for example using cpu 0 for control flow.
-    pipe_config[mod1].cpu_affinity = "0"
-    pipe_config[mod1].export_cc = None
-    ##############################################################
-    # Set the compile target of the second subgraph module as LLVM.
-    pipe_config[mod1].target = "cuda"  # tvm.target.Target("cuda", host=tvm.target.Target("llvm"))
-    pipe_config[mod1].dev = tvm.device("cuda", 0)
-    pipe_config[mod1].build_func = cutlass_build
-    pipe_config[mod1].export_cc = "nvcc"
-    #################################################################################
-    # Set the cpu afinity for control flow, for example using cpu 1 for control flow.
-    pipe_config[mod1].cpu_affinity = "1"
-    pipe_config["input"]["data"].connect(pipe_config[mod0]["input"]["data"])
-    pipe_config[mod0]["output"][0].connect(pipe_config[mod1]["input"]["data_n_0"])
-    pipe_config[mod1]["output"]["0"].connect(pipe_config["output"][0])
-    ######################################
-    # The pipeline configuration as below.
-    """
-    print(pipe_config)
-     Inputs
-      |data: mod0:data
+#########################################
+# Create subgraph pipeline configuration.
+# Associate the subgraph module with a target.
+# Using BYOC to set the codegen of the second subgraph module.
+# To use cutlass the 'USE_CUTLASS' should set as ON.
+mod0, mod1 = subgraphs[0], subgraphs[1]
+# Use cutlass as the codegen.
+mod1 = partition_for_cutlass(mod1)
+#################################################
+# Get the pipeline executor configuration object.
+pipe_config = pipeline_executor_build.PipelineConfig()
+###########################################################################
+# Set the compile target of the second subgraph module for example as LLVM.
+pipe_config[mod0].target = "llvm"
+pipe_config[mod0].dev = tvm.cpu(0)
+###############################################################################
+# Set the cpu afinity for control flow, for example using cpu 0 for control flow.
+pipe_config[mod1].cpu_affinity = "0"
+pipe_config[mod1].export_cc = None
+##############################################################
+# Set the compile target of the second subgraph module as LLVM.
+pipe_config[mod1].target = "cuda"  # tvm.target.Target("cuda", host=tvm.target.Target("llvm"))
+pipe_config[mod1].dev = tvm.device("cuda", 0)
+pipe_config[mod1].build_func = cutlass_build
+pipe_config[mod1].export_cc = "nvcc"
+#################################################################################
+# Set the cpu afinity for control flow, for example using cpu 1 for control flow.
+pipe_config[mod1].cpu_affinity = "1"
+pipe_config["input"]["data"].connect(pipe_config[mod0]["input"]["data"])
+pipe_config[mod0]["output"][0].connect(pipe_config[mod1]["input"]["data_n_0"])
+pipe_config[mod1]["output"]["0"].connect(pipe_config["output"][0])
+######################################
+# The pipeline configuration as below.
+"""
+print(pipe_config)
+ Inputs
+  |data: mod0:data
 
-     output
-      |output(0) : mod1.output(0)
+ output
+  |output(0) : mod1.output(0)
 
-     connections
-      |mod0.output(0)-> mod1.data_n_0
-    """
+ connections
+  |mod0.output(0)-> mod1.data_n_0
+"""
 
-    # sphinx_gallery_start_ignore
-    from tvm import testing
+# sphinx_gallery_start_ignore
+from tvm import testing
 
-    # testing.utils.install_request_hook(depth=3)
-    # sphinx_gallery_end_ignore
-    ##############################
-    # Build the pipeline executor.
-    # ----------------------------
-    with tvm.transform.PassContext(opt_level=3):
-        pipeline_mod_factory = pipeline_executor_build.build(pipe_config)
-    ###############################################
-    # Export the parameter configuration to a file.
-    directory_path = tvm.contrib.utils.tempdir().temp_dir
-    #############################################
-    # If the directory does not exist, create it.
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
-    config_file_name = pipeline_mod_factory.export_library(directory_path)
-    ################################################################
-    # Use the load function to create and initialize PipelineModule.
-    # --------------------------------------------------------------
-    pipeline_module = pipeline_executor.PipelineModule.load_library(config_file_name)
+# testing.utils.install_request_hook(depth=3)
+# sphinx_gallery_end_ignore
+##############################
+# Build the pipeline executor.
+# ----------------------------
+with tvm.transform.PassContext(opt_level=3):
+    pipeline_mod_factory = pipeline_executor_build.build(pipe_config)
+###############################################
+# Export the parameter configuration to a file.
+directory_path = tvm.contrib.utils.tempdir().temp_dir
+#############################################
+# If the directory does not exist, create it.
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+config_file_name = pipeline_mod_factory.export_library(directory_path)
+################################################################
+# Use the load function to create and initialize PipelineModule.
+# --------------------------------------------------------------
+pipeline_module = pipeline_executor.PipelineModule.load_library(config_file_name)
 
-    ############################
-    # Run the pipeline executor.
-    # --------------------------
-    # Allocated a input data.
-    data = np.random.uniform(-1, 1, size=data_shape).astype("float16")
-    pipeline_module.set_input("data", tvm.nd.array(data))
-    ##########################################################################
-    # Run the two subgraph in pipeline mode and get the output asynchronously.
-    pipeline_module.run()
-    outputs = []
-    while not outputs:
-        outputs = pipeline_module.get_output()
-        time.sleep(0.001)
-    ######################################
-    # Use graph_executor for verification.
-    # ------------------------------------
-    # Run these two subgraphs in sequence with graph_executor to get the output.
-    target = "llvm"
-    dev0 = tvm.device(target, 0)
-    lib0 = relay.build_module.build(mod0, target, params=params)
-    module0 = runtime.GraphModule(lib0["default"](dev0))
-    cutlass = tvm.target.Target(
-        {
-            "kind": "cutlass",
-            "sm": 75,
-            "use_3xtf32": True,
-            "split_k_slices": [1],
-            "profile_all_alignments": False,
-            "find_first_valid": True,
-            "use_multiprocessing": True,
-            "use_fast_math": False,
-            "tmp_dir": "./tmp",
-        },
-        host=tvm.target.Target("llvm"),
-    )
-    cuda = tvm.target.Target("cuda", host=tvm.target.Target("llvm"))
-    lib1 = relay.build_module.build(mod1, [cuda, cutlass], params=params)
-    lib1 = finalize_modules(lib1, "compile.so", "./tmp")
+############################
+# Run the pipeline executor.
+# --------------------------
+# Allocated a input data.
+data = np.random.uniform(-1, 1, size=data_shape).astype("float16")
+pipeline_module.set_input("data", tvm.nd.array(data))
+##########################################################################
+# Run the two subgraph in pipeline mode and get the output asynchronously.
+pipeline_module.run()
+outputs = []
+while not outputs:
+    outputs = pipeline_module.get_output()
+    time.sleep(0.001)
+######################################
+# Use graph_executor for verification.
+# ------------------------------------
+# Run these two subgraphs in sequence with graph_executor to get the output.
+target = "llvm"
+dev0 = tvm.device(target, 0)
+lib0 = relay.build_module.build(mod0, target, params=params)
+module0 = runtime.GraphModule(lib0["default"](dev0))
+cutlass = tvm.target.Target(
+    {
+        "kind": "cutlass",
+        "sm": 75,
+        "use_3xtf32": True,
+        "split_k_slices": [1],
+        "profile_all_alignments": False,
+        "find_first_valid": True,
+        "use_multiprocessing": True,
+        "use_fast_math": False,
+        "tmp_dir": "./tmp",
+    },
+    host=tvm.target.Target("llvm"),
+)
+cuda = tvm.target.Target("cuda", host=tvm.target.Target("llvm"))
+lib1 = relay.build_module.build(mod1, [cuda, cutlass], params=params)
+lib1 = finalize_modules(lib1, "compile.so", "./tmp")
 
-    dev1 = tvm.device("cuda", 0)
+dev1 = tvm.device("cuda", 0)
 
-    module1 = runtime.GraphModule(lib1["default"](dev1))
+module1 = runtime.GraphModule(lib1["default"](dev1))
 
-    module0.set_input("data", data)
-    module0.run()
-    out_shape = (1, 16, img_size, img_size)
-    out = module0.get_output(0, tvm.nd.empty(out_shape, "float16"))
-    module1.set_input("data_n_0", out)
-    module1.run()
-    out_shape = (1, 1)
-    out = module1.get_output(0, tvm.nd.empty(out_shape, "float16"))
-    ####################
-    # Verify the result.
-    tvm.testing.assert_allclose(outputs[0].numpy(), out.numpy())
-
-
-run_pipeline()
+module0.set_input("data", data)
+module0.run()
+out_shape = (1, 16, img_size, img_size)
+out = module0.get_output(0, tvm.nd.empty(out_shape, "float16"))
+module1.set_input("data_n_0", out)
+module1.run()
+out_shape = (1, 1)
+out = module1.get_output(0, tvm.nd.empty(out_shape, "float16"))
+####################
+# Verify the result.
+tvm.testing.assert_allclose(outputs[0].numpy(), out.numpy())
