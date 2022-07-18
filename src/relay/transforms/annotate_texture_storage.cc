94ad477c0f3189aca09c6a19e67cb3705074a7f0
Andrey Malyshev <elvin.nnov@gmail.com>
2022-07-18 13:12:17 +0300

    fix clang

--- src/relay/transforms/annotate_texture_storage.cc
+++ src/relay/transforms/annotate_texture_storage.cc
@@ -326,7 +326,7 @@ class StorageInfo : private transform::DeviceAwareExprVisitor {
       }
     } else if (auto attrs = call->attrs.as<Conv2DWinogradAttrs>()) {
       if ((attrs->data_layout == "NCHW4c" || attrs->data_layout == "NHWC4c") &&
-         (attrs->kernel_layout == "OIHW4o" || attrs->kernel_layout == "HWIO4o")) {
+          (attrs->kernel_layout == "OIHW4o" || attrs->kernel_layout == "HWIO4o")) {
         supports_texture_storage = true;
       }
     } else if (auto attrs = call->attrs.as<GlobalPool2DAttrs>()) {
