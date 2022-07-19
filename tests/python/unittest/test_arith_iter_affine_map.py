eb7cf7051dd239ef86db13875687ef6d5ebb9418
Florin Blanaru <florin.blanaru96@gmail.com>
2022-07-20 00:36:38 +0100

    Revert "support overlapped itersum (#12039)" (#12137)
    This reverts commit 3e7a2ad9568a79fb775c0ca9d09a3fa2f51f792f.

--- tests/python/unittest/test_arith_iter_affine_map.py
+++ tests/python/unittest/test_arith_iter_affine_map.py
@@ -61,6 +61,7 @@ def assert_iter_sum_pattern(
     )
     indices = res.indices
     assert len(indices) == len(keys), res.errors
+    print(indices)
     for i, input_iter in enumerate(keys):
         spec = expect_dict[input_iter]
         (
@@ -443,13 +444,6 @@ def test_predicate():
         {xo * 129 + xi: (128, 0), y: (128, 0)},
         var_dom([(xo, 1), (xi, 129), (y, 128)]),
         predicate=xo * 129 + xi < 128,
-    )
-
-    # strided iteration predicate
-    assert_iter_sum_pattern(
-        {xo * 16 + xi * 4: (10, 0, 4)},
-        var_dom([(xo, 3), (xi, 4)]),
-        predicate=xo * 4 + xi < 10,
     )
 
 
@@ -1014,56 +1008,6 @@ def test_padding():
     # original extent is smaller than the divident
     # it is not surjective wrt to the region [0, 16)
     assert_iter_sum_failure({flm(x, 16)}, var_dom([(x, 3)]))
-
-
-def test_overlapped_fuse():
-    x = tvm.tir.Var("x", "int32")
-    y = tvm.tir.Var("y", "int32")
-    z = tvm.tir.Var("z", "int32")
-    a = tvm.tir.Var("x", "int32")
-    b = tvm.tir.Var("y", "int32")
-
-    # non-bijective fuse of two
-    assert_iter_sum_pattern(
-        {
-            x * 7 + y: (22, 0, 1),
-        },
-        var_dom([(x, 3), (y, 8)]),
-        check_level="surjective",
-    )
-    assert_iter_sum_failure([x * 7 + y], var_dom([(x, 3), (y, 8)]), check_level="bijective")
-
-    # non-bijective fuse of three
-    assert_iter_sum_pattern(
-        {
-            x * 18 + y * 7 + z: (40, 0, 1),
-        },
-        var_dom([(x, 2), (y, 3), (z, 8)]),
-        check_level="surjective",
-    )
-    assert_iter_sum_failure([x * 7 + y], var_dom([(x, 2), (y, 3), (z, 8)]), check_level="bijective")
-
-    # negative scale fusion is not allowed
-    assert_iter_sum_failure([x * -7 + y], var_dom([(x, 3), (y, 8)]), check_level="surjective")
-    assert_iter_sum_failure([x * 7 - y], var_dom([(x, 3), (y, 8)]), check_level="surjective")
-
-    # with predicate
-    assert_iter_sum_pattern(
-        {
-            a * 40 + b * 20 + x * 18 + y * 3 + z: (125, 6, 1),
-        },
-        var_dom([(a, 3), (b, 2), (x, 2), (y, 6), (z, 8)]),
-        predicate=tvm.tir.all(z < 4, 1 < x * 6 + y, x * 6 + y < 10),
-        check_level="surjective",
-    )
-
-    # stride=1 kernel
-    assert_iter_sum_pattern(
-        {x + a: (230, 0, 1)}, var_dom([(x, 224), (a, 7)]), check_level="surjective"
-    )
-
-    # do not allow both strided and overlapped
-    assert_iter_sum_failure([5 * x + 2 * y], var_dom([(x, 4), (y, 3)]), check_level="surjective")
 
 
 if __name__ == "__main__":
