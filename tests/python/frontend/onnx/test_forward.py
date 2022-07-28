dff98bad9a73b9adb7b755151afd35a9dd109b73
blackkker <823036806@qq.com>
2022-07-28 10:34:12 +0000

    Disable unused-argument is for some unused-argument in function can not be removed

--- tests/python/frontend/onnx/test_forward.py
+++ tests/python/frontend/onnx/test_forward.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=import-self, invalid-name, unused-argument, unused-variable, missing-function-docstring, redefined-builtin, consider-using-f-string
+# pylint: disable=unused-argument
 """
 ONNX testcases
 ================
