00efafc000131cb1547d3a1066626a1d3e733d8a
blackkker <823036806@qq.com>
2022-07-29 09:45:27 +0000

    Fix typo errors

--- tests/python/frontend/tflite/test_forward.py
+++ tests/python/frontend/tflite/test_forward.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=unused-argument, disable=import-outside-toplevel, inconsistent-return-statements
+# pylint: disable=unused-argument, import-outside-toplevel, inconsistent-return-statements
 """
 TFLite testcases
 ================
