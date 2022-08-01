06aeef5160c3274f62ec493b37fdcbccec00dcec
MoebiusMeow <MoebiusMeow@qq.com>
2022-08-01 19:38:32 +0800

    upd subwarp unittest

--- tests/python/unittest/test_subwarp_reduction_cuda.py
+++ tests/python/unittest/test_subwarp_reduction_cuda.py
@@ -36,7 +36,7 @@ def reduce(a: T.handle, b: T.handle, d1: T.int32, d2: T.int32, d3: T.int32) -> N
 @tvm.testing.requires_gpu
 @tvm.testing.requires_cuda
 def test_cuda_subwarp_reduction():
-    def check(d1: int, d2: int, d3: int):
+    def check_sum(d1: int, d2: int, d3: int):
         _, _, _d1, _d2, _d3 = reduce.params
         mod = reduce.specialize({_d1: d1, _d2: d2, _d3: d3})
         sch = tvm.tir.Schedule(mod)
@@ -58,10 +58,33 @@ def test_cuda_subwarp_reduction():
         f(a, b)
         tvm.testing.assert_allclose(b.numpy(), b_np, rtol=1e-6, atol=1e-6)
 
+    def check_max(d1: int, d2: int, d3: int):
+        _, _, _d1, _d2, _d3 = reduce.params
+        mod = reduce.specialize({_d1: d1, _d2: d2, _d3: d3})
+        sch = tvm.tir.Schedule(mod)
+        blk = sch.get_block("reduce")
+        i, j, k, l = sch.get_loops(blk)
+        sch.bind(i, "blockIdx.x")
+        sch.bind(j, "threadIdx.z")
+        sch.bind(k, "threadIdx.y")
+        sch.bind(l, "threadIdx.x")
+        f = tvm.build(sch.mod["main"], target="cuda")
+
+        # prepare input and output array
+        a_np = np.random.randn(1, d1, d2, d3).astype("float32")
+        b_np = a_np.max(axis=-1).astype("float32")
+        a = tvm.nd.array(a_np, tvm.cuda(0))
+        b = tvm.nd.array(np.zeros_like(b_np), tvm.cuda(0))
+
+        # launch kernel
+        f(a, b)
+        tvm.testing.assert_allclose(b.numpy(), b_np, rtol=1e-6, atol=1e-6)
+
     for d1 in range(1, 5):
         for d2 in range(1, 5):
             for d3 in range(2, 33):
-                check(d1, d2, d3)
+                check_sum(d1, d2, d3)
+                check_max(d1, d2, d3)
 
 
 if __name__ == "__main__":
