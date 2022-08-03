4a244a74f5460c8cdb676cc02baca7f1282030aa
Sevin F. Varoglu <sfvaroglu@octoml.ai>
2022-08-03 14:23:20 +0300

    Add attrs to FQ2I adaptive_avg_pool1d

--- python/tvm/relay/transform/fake_quantization_to_integer.py
+++ python/tvm/relay/transform/fake_quantization_to_integer.py
@@ -129,7 +129,7 @@ def adaptive_avgpool1d(expr, type_map):
     arg = expr.args[0]
     t = type_map[arg]
     arg = relay.op.cast(arg, "int32")
-    out = relay.op.nn.adaptive_avg_pool1d(arg)
+    out = relay.op.nn.adaptive_avg_pool1d(arg, **expr.attrs)
     out = relay.op.cast(out, t.dtype)
     return [out, t]
 
