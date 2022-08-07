1ef595fda205f58dd1820559ab6bdb1577648b8d
Chris Sidebottom <chris.sidebottom@arm.com>
2022-08-07 21:37:42 +0100

    Skip another docs warning

--- tests/scripts/task_python_docs.sh
+++ tests/scripts/task_python_docs.sh
@@ -82,6 +82,7 @@ IGNORED_WARNINGS=(
     'strategy:conv2d NHWC layout is not optimized for x86 with autotvm.'
     'strategy:depthwise_conv2d NHWC layout is not optimized for x86 with autotvm.'
     'strategy:depthwise_conv2d with layout NHWC is not optimized for arm cpu.'
+    'strategy:dense is not optimized for arm cpu.'
     'autotvm:Cannot find config for target=llvm -keys=cpu -link-params=0'
     'autotvm:One or more operators have not been tuned. Please tune your model for better performance. Use DEBUG logging level to see more details.'
     'autotvm:Cannot find config for target=cuda -keys=cuda,gpu'
