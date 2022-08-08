6285e2236dd41c51992c119e7e6f01cb88c83965
Lite Ye <yelite958@gmail.com>
2022-08-08 10:01:24 -0400

    Fix include style

--- tests/cpp/tvmscript_printer_traced_object_functor_test.cc
+++ tests/cpp/tvmscript_printer_traced_object_functor_test.cc
@@ -19,13 +19,12 @@
 
 #include <dmlc/logging.h>
 #include <gtest/gtest.h>
+#include <tvm/node/object_path.h>
 #include <tvm/node/repr_printer.h>
 #include <tvm/runtime/container/map.h>
+#include <tvm/runtime/packed_func.h>
+#include <tvm/script/printer/traced_object.h>
 #include <tvm/script/printer/traced_object_functor.h>
-
-#include "tvm/node/object_path.h"
-#include "tvm/runtime/packed_func.h"
-#include "tvm/script/printer/traced_object.h"
 
 using namespace tvm;
 using namespace tvm::script::printer;
