258dc72c6519c10ffa3a6dd3be0baa2eca2f9e25
blackkker <823036806@qq.com>
2022-08-11 07:07:52 +0000

    [CI] Apply linting rules to caffe tests

--- tests/python/frontend/caffe/test_forward.py
+++ tests/python/frontend/caffe/test_forward.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=import-self, invalid-name, unused-argument
+# pylint: disable=import-self, invalid-name, unused-argument, unspecified-encoding
 """
 Caffe testcases
 ====================
