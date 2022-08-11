4e7e459e3f9b5a70faab38edacb7e02616e85a90
Lite Ye <yelite958@gmail.com>
2022-08-11 17:08:32 -0400

    Add Python function to get type index by class

--- python/tvm/_ffi/__init__.py
+++ python/tvm/_ffi/__init__.py
@@ -27,4 +27,4 @@ and have a ctypes fallback implementation.
 from . import _pyversion
 from .base import register_error
 from .registry import register_object, register_func, register_extension
-from .registry import _init_api, get_global_func
+from .registry import _init_api, get_global_func, get_object_type_index
