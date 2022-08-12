f82cfd972c19a6dbcef95df661c7b10aa19c7f95
Egor Churaev <egor.churaev@gmail.com>
2022-08-12 13:35:25 +0300

    [HotFix] Op is not bound to any variables
    After PR #12349 inference of some Adreno networks was broken. In the
    output it was the following error:
    ```
    TVMError: Not all Vars are passed in api_args: 'compute' is not bound
    to any variables
    ```

--- src/tir/transforms/storage_rewrite.cc
+++ src/tir/transforms/storage_rewrite.cc
@@ -1463,7 +1463,9 @@ class VectorTypeRewriter : public StmtExprMutator {
 
   Stmt VisitStmt_(const LetStmtNode* op) final {
     auto it = rewrite_map_.find(op->var.get());
-    if (it == rewrite_map_.end()) {
+    PrimExpr value = this->VisitExpr(op->value);
+    Stmt body = this->VisitStmt(op->body);
+    if (value.same_as(op->value) && body.same_as(op->body) && it == rewrite_map_.end()) {
       return GetRef<Stmt>(op);
     }
     return LetStmt(it->second.new_buffer_var, op->value, op->body);
