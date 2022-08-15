85c1b1c62d005a5ae7544e8b420791e3ff6ef91d
Yaxing Cai <caiyaxing666@gmail.com>
2022-08-14 17:48:07 -0700

    Expose `shift_left` and `shift_right` to Python
    `op` list:
    `shift_left`
    `shift_right`

--- python/tvm/tir/__init__.py
+++ python/tvm/tir/__init__.py
@@ -58,7 +58,7 @@ from .op import trunc, abs, round, nextafter, nearbyint, power, popcount, fmod, 
 from .op import isnan, isfinite, isinf, copysign
 from .op import div, indexdiv, indexmod, truncdiv, truncmod, floordiv, floormod, ceildiv
 from .op import comm_reducer, min, max, sum
-from .op import q_multiply_shift
+from .op import q_multiply_shift, shift_left, shift_right
 
 from .schedule import StmtSRef, BlockScope, ScheduleState, Schedule, ScheduleError
 
