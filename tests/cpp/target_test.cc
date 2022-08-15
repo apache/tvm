5e93e3ed30e5c17322a008b3f155151b3da36465
Mehrdad Hessar <mhessar@octoml.ai>
2022-08-15 10:39:35 -0700

    fix error

--- tests/cpp/target_test.cc
+++ tests/cpp/target_test.cc
@@ -334,7 +334,7 @@ TEST(TargetCreation, DeduplicateKeys) {
   ICHECK_EQ(target->keys.size(), 2U);
   ICHECK_EQ(target->keys[0], "cpu");
   ICHECK_EQ(target->keys[1], "arm_cpu");
-  ICHECK_EQ(target->attrs.size(), 2U);
+  ICHECK_EQ(target->attrs.size(), 1U);
   ICHECK_EQ(target->GetAttr<String>("device"), "arm_cpu");
 }
 
