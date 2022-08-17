11ebf54d17c9959395ce060441a24a798ca4c306
Krzysztof Parzyszek <kparzysz@quicinc.com>
2022-08-16 20:13:05 -0500

    More c++14 -> c++17...

--- python/tvm/rpc/minrpc.py
+++ python/tvm/rpc/minrpc.py
@@ -67,7 +67,7 @@ def with_minrpc(compile_func, server="posix_popen_server", runtime="libtvm"):
     runtime_path = libinfo.find_lib_path([runtime, runtime + ".so", runtime + ".dylib"])[0]
 
     runtime_dir = os.path.abspath(os.path.dirname(runtime_path))
-    options = ["-std=c++14"]
+    options = ["-std=c++17"]
     # Make sure the rpath to the libtvm is set so we can do local tests.
     # Note that however, this approach won't work on remote.
     # Always recommend to to link statically.
