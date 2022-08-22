19a115367410832287b1667aa26b8b13fb1cd8e8
valmat07 <valmat07@yandex.ru>
2022-08-22 20:28:20 +0300

    Remove OpenCL gtest variable

--- tests/scripts/task_config_build_gpu.sh
+++ tests/scripts/task_config_build_gpu.sh
@@ -28,7 +28,6 @@ echo set\(USE_CUDNN ON\) >> config.cmake
 echo set\(USE_CUDA ON\) >> config.cmake
 echo set\(USE_VULKAN ON\) >> config.cmake
 echo set\(USE_OPENGL ON\) >> config.cmake
-echo set\(USE_OPENCL_GTEST "${HEXAGON_SDK_ROOT}/utils/googletest/gtest"\) >> config.cmake
 echo set\(USE_OPENCL ON\) >> config.cmake
 echo set\(USE_CPP_RPC ON\) >> config.cmake
 echo set\(USE_MICRO ON\) >> config.cmake
