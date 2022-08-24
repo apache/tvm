b17219d6ea917f135904ae430149d6cc3e5c0593
valmat07 <valmat07@yandex.ru>
2022-08-24 14:22:57 +0300

    Fix opencl gtests include directory

--- tests/scripts/task_config_build_gpu.sh
+++ tests/scripts/task_config_build_gpu.sh
@@ -30,7 +30,7 @@ echo set\(USE_VULKAN ON\) >> config.cmake
 echo set\(USE_OPENGL ON\) >> config.cmake
 echo set\(USE_OPENCL ON\) >> config.cmake
 echo set\(USE_CPP_RPC ON\) >> config.cmake
-echo set\(USE_OPENCL_GTEST /usr/src/gtest\) >> config.cmake
+echo set\(USE_OPENCL_GTEST /usr/src/googletest\) >> config.cmake
 echo set\(USE_MICRO ON\) >> config.cmake
 echo set\(USE_MICRO_STANDALONE_RUNTIME ON\) >> config.cmake
 echo set\(USE_LLVM \"/usr/bin/llvm-config-9 --link-static\"\) >> config.cmake
