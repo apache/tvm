efed986b4ac903fcc12b5bc9c75cf399fdbf8d60
Robert Kimball <bobkimball@gmail.com>
2022-08-31 20:12:13 +0000

    Add another tests for python API to access new load/save functions where there are no constants

--- tests/python/relay/test_vm.py
+++ tests/python/relay/test_vm.py
@@ -1453,5 +1453,37 @@ def test_load_and_save_constants_via_map():
     tvm.testing.assert_allclose(expected, actual.numpy())
 
 
+def test_load_late_bound_consts_via_map_with_no_late_bound_consts():
+    """Check that load_late_bound_consts handles a model with no late bound consts."""
+    target = tvm.target.Target("llvm")
+    dev = tvm.cpu()
+
+    const_data = np.random.rand(1).astype("float64")
+    x = relay.var("x", shape=(1,), dtype="float64")
+    const = relay.const(const_data, dtype="float64")
+
+    func = relay.Function([x], relay.op.add(x, const))
+    mod = tvm.IRModule.from_expr(func)
+
+    vm_exec = vm.compile(mod, target=target)
+
+    temp = utils.tempdir()
+    path_dso = temp.relpath("lib.so")
+
+    # Ensure const_data is below the byte threshold for a late-bound const.
+    byte_limit = len(const_data.tobytes()) + 1
+    consts_map = vm_exec.get_late_bound_consts(byte_limit=byte_limit)
+    vm_exec.mod.export_library(path_dso)
+
+    mod = runtime.load_module(path_dso)
+    mod["load_late_bound_consts_from_map"](consts_map)
+
+    x_data = np.random.rand(1).astype("float64")
+    loaded_vm = runtime.vm.VirtualMachine(mod, dev)
+    actual = loaded_vm.invoke("main", x_data)
+    expected = x_data + const_data
+    tvm.testing.assert_allclose(expected, actual.numpy())
+
+
 if __name__ == "__main__":
     tvm.testing.main()
