82f0c4d31bff370964e572fb1eefdb6162ea0c20
Luke Hutton <luke.hutton@arm.com>
2022-09-02 14:09:40 +0000

    [ETHOSN] Use pytest parameterization for integration tests
    Using pytest parameterization helps identify the particular parameter
    combinations that are failing for a given test. Additionally, it can be
    useful when parallelizing the tests. This commit makes sure that
    "trials" have been replaced by parameterization as well as completing a
    general cleanup.
    
    Change-Id: Iaabefaa8a2f316c9f403ac793fbd42b98edf9033

--- tests/python/contrib/test_ethosn/test_depth_to_space.py
+++ tests/python/contrib/test_ethosn/test_depth_to_space.py
@@ -33,37 +33,35 @@ def _get_model(shape, block, dtype, layout):
 
 @requires_ethosn
 @pytest.mark.parametrize("dtype", ["uint8", "int8"])
-def test_depth_to_space(dtype):
-    """Compare Depth To Space output with TVM."""
-
-    trials = [
+@pytest.mark.parametrize(
+    "shape",
+    [
         (1, 16, 16, 16),
         (1, 64, 32, 16),
-    ]
-
+    ],
+)
+def test_depth_to_space(dtype, shape):
+    """Compare Depth To Space output with TVM."""
     np.random.seed(0)
-    for shape in trials:
-        inputs = {
-            "a": tvm.nd.array(
-                np.random.randint(
-                    np.iinfo(dtype).min, np.iinfo(dtype).max + 1, size=shape, dtype=dtype
-                )
-            )
-        }
-        outputs = []
-        for npu in [False, True]:
-            model = _get_model(shape, 2, dtype, "NHWC")
-            mod = tei.make_module(model, {})
-            outputs.append(tei.build_and_run(mod, inputs, 1, {}, npu=npu))
 
-        tei.verify(outputs, dtype, 1)
+    inputs = {
+        "a": tvm.nd.array(
+            np.random.randint(np.iinfo(dtype).min, np.iinfo(dtype).max + 1, size=shape, dtype=dtype)
+        )
+    }
+    outputs = []
+    for npu in [False, True]:
+        model = _get_model(shape, 2, dtype, "NHWC")
+        mod = tei.make_module(model, {})
+        outputs.append(tei.build_and_run(mod, inputs, 1, {}, npu=npu))
+
+    tei.verify(outputs, dtype, 1)
 
 
 @requires_ethosn
-def test_depth_to_space_failure():
-    """Check Depth To Space error messages."""
-
-    trials = [
+@pytest.mark.parametrize(
+    "shape,block,dtype,layout,err_msg",
+    [
         ((2, 16, 16, 16), 2, "uint8", "NHWC", "batch size=2, batch size must = 1"),
         (
             (1, 16, 16, 16),
@@ -74,9 +72,10 @@ def test_depth_to_space_failure():
         ),
         ((1, 16, 16, 16), 4, "uint8", "NHWC", "Only block size of 2 is supported"),
         ((1, 16, 16, 16), 2, "uint8", "NCHW", "Input layer must be NHWC or NHWCB"),
-    ]
-
-    for shape, block, dtype, layout, err_msg in trials:
-        model = _get_model(shape, block, dtype, layout)
-        mod = tei.make_ethosn_partition(model)
-        tei.test_error(mod, {}, err_msg)
+    ],
+)
+def test_depth_to_space_failure(shape, block, dtype, layout, err_msg):
+    """Check Depth To Space error messages."""
+    model = _get_model(shape, block, dtype, layout)
+    mod = tei.make_ethosn_partition(model)
+    tei.test_error(mod, {}, err_msg)
