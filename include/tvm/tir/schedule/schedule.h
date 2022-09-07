5a1e63fe5f571d2d69c094874d0dbc135b1c4b7c
Eric Lunderberg <elunderberg@octoml.ai>
2022-09-07 08:44:26 -0500

    Add default value of NullOpt for pad_value

--- include/tvm/tir/schedule/schedule.h
+++ include/tvm/tir/schedule/schedule.h
@@ -605,7 +605,7 @@ class ScheduleNode : public runtime::Object {
    */
   virtual void TransformLayout(const BlockRV& block_rv, int buffer_index,
                                BufferIndexType buffer_index_type, const IndexMap& index_map,
-                               const Optional<PrimExpr>& pad_value) = 0;
+                               const Optional<PrimExpr>& pad_value = NullOpt) = 0;
 
   /*!
    * \brief Apply a transformation represented by IndexMap to block
