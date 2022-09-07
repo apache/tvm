307ba3cde5e88adea07df83530dd05597209b30b
valmat07 <valmat07@yandex.ru>
2022-09-08 02:07:53 +0300

    fix pytest skipif warnings

--- tests/python/relay/opencl_texture/test_conv2d_nchw_texture.py
+++ tests/python/relay/opencl_texture/test_conv2d_nchw_texture.py
@@ -479,7 +479,7 @@ def test_conv2d_winograd_conv(target, dtype):
 
 @tvm.testing.requires_opencl
 @tvm.testing.parametrize_targets("opencl -device=adreno")
-@pytest.mark.skipIf(tvm.testing.utils.IS_IN_CI, reason="failed due to nvidia libOpencl")
+@pytest.mark.skipif(tvm.testing.utils.IS_IN_CI, reason="failed due to nvidia libOpencl in the CI")
 def test_residual_block(target, dtype):
     """
     - some kind of residual block followed by convolution to have texture after residual block
