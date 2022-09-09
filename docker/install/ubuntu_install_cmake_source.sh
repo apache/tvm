1c5ffc67ad2497a2d34509e0599b3a787fcd464d
driazati <9407960+driazati@users.noreply.github.com>
2022-09-09 00:07:09 -0700

    [ci][docker] Use CMake 3.20.0 for cortexm (#12744)
    The Zephyr project builds require 3.20.0 to work correctly
    
    Co-authored-by: driazati <driazati@users.noreply.github.com>

--- docker/install/ubuntu_install_cmake_source.sh
+++ docker/install/ubuntu_install_cmake_source.sh
@@ -20,13 +20,19 @@ set -e
 set -u
 set -o pipefail
 
-v=3.18
-version=3.18.4
+if [ -z ${1+x} ]; then
+    version=3.18.4
+else
+    version=$1
+fi
+
+v=$(echo $version | sed 's/\(.*\)\..*/\1/g')
+echo "Installing cmake $version ($v)"
 wget https://cmake.org/files/v${v}/cmake-${version}.tar.gz
 tar xvf cmake-${version}.tar.gz
 cd cmake-${version}
 ./bootstrap
-make -j$(nproc)
+make -j"$(nproc)"
 make install
 cd ..
 rm -rf cmake-${version} cmake-${version}.tar.gz
