7e432e8d905ee73dc879486dc966a5fcc245670c
Federico Peccia <peccia@fzi.de>
2022-09-16 14:50:20 +0200

    lint fix

--- python/tvm/micro/model_library_format.py
+++ python/tvm/micro/model_library_format.py
@@ -294,7 +294,8 @@ def _build_function_memory_map(function_metadata):
             main_func_metadata.io_sizes[target]
         )
 
-        # Now, we also add the information about the size of each input and output of the main function (in bytes)
+        # Now, we also add the information about the size of each input and output of the main 
+        # function (in bytes)
         input_dict = {}
         for input_param in main_func_metadata.relay_primfuncs[target].params:
             if hasattr(input_param, "checked_type"):
