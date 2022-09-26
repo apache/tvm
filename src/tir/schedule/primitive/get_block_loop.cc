3bfbd009886f425c6e0ce79fa52ca3bb3dd89979
Wuwei Lin <wuwei@apache.org>
2022-09-26 11:01:31 -0700

    [TIR] Fix GetProducer/Consumer for duplicating dep edges

--- src/tir/schedule/primitive/get_block_loop.cc
+++ src/tir/schedule/primitive/get_block_loop.cc
@@ -80,27 +80,27 @@ Array<StmtSRef> GetChildBlocks(const ScheduleState& self, const StmtSRef& parent
 Array<StmtSRef> GetProducers(const ScheduleState& self, const StmtSRef& block_sref) {
   StmtSRef scope_root = GetScopeRoot(self, block_sref, /*require_stage_pipeline=*/false);
   Array<Dependency> edges = self->GetBlockScope(scope_root)->GetDepsByDst(block_sref);
-  Array<StmtSRef> results;
+  std::unordered_set<StmtSRef, ObjectPtrHash, ObjectPtrEqual> results;
   results.reserve(edges.size());
   for (const Dependency& edge : edges) {
     if (edge->kind == DepKind::kRAW || edge->kind == DepKind::kWAW) {
-      results.push_back(edge->src);
+      results.emplace(edge->src);
     }
   }
-  return results;
+  return Array<StmtSRef>(results.begin(), results.end());
 }
 
 Array<StmtSRef> GetConsumers(const ScheduleState& self, const StmtSRef& block_sref) {
   StmtSRef scope_root = GetScopeRoot(self, block_sref, /*require_stage_pipeline=*/false);
   Array<Dependency> edges = self->GetBlockScope(scope_root)->GetDepsBySrc(block_sref);
-  Array<StmtSRef> results;
+  std::unordered_set<StmtSRef, ObjectPtrHash, ObjectPtrEqual> results;
   results.reserve(edges.size());
   for (const Dependency& edge : edges) {
     if (edge->kind == DepKind::kRAW || edge->kind == DepKind::kWAW) {
-      results.push_back(edge->dst);
+      results.emplace(edge->dst);
     }
   }
-  return results;
+  return Array<StmtSRef>(results.begin(), results.end());
 }
 
 /******** InstructionKind Registration ********/
