43689b18a75a70449adef69165a1b97538a46fe0
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2022-09-27 23:12:00 -0700

    injective type

--- src/relay/op/nn/nn.cc
+++ src/relay/op/nn/nn.cc
@@ -1025,7 +1025,7 @@ RELAY_REGISTER_OP("nn.layer_norm")
                                    NormalizationInferCorrectLayout<LayerNormAttrs>)
     .set_support_level(1)
     .add_type_rel("LayerNorm", LayerNormRel)
-    .set_attr<TOpPattern>("TOpPattern", kOpaque);
+    .set_attr<TOpPattern>("TOpPattern", kInjective);
 
 // group_norm
 TVM_REGISTER_NODE_TYPE(GroupNormAttrs);
