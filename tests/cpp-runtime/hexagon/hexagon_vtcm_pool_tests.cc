bf284d1b66d0b4501b004675c6b395968abb8b9e
Janet Schneider <janetsc@octoml.ai>
2022-09-29 11:49:29 -0700

    additional tests

--- tests/cpp-runtime/hexagon/hexagon_vtcm_pool_tests.cc
+++ tests/cpp-runtime/hexagon/hexagon_vtcm_pool_tests.cc
@@ -41,23 +41,76 @@ TEST_F(HexagonVtcmPoolTest, basic) {
   size_t max_bytes = vtcm_pool->TotalBytes();
   size_t two_k_block = 2048;
   size_t one_k_block = 1024;
+  size_t one_byte_block = 1;
   ptr = vtcm_pool->Allocate(max_bytes);
   vtcm_pool->Free(ptr, max_bytes);
   ptr = vtcm_pool->Allocate(two_k_block);
   vtcm_pool->Free(ptr, two_k_block);
   ptr = vtcm_pool->Allocate(one_k_block);
   vtcm_pool->Free(ptr, one_k_block);
+  ptr = vtcm_pool->Allocate(one_byte_block);
+  vtcm_pool->Free(ptr, one_byte_block);
 }
 
-// TEST_F(HexagonVtcmPoolTest, basic) {
-//   void* ptr;
-//   size_t max_bytes = 1024*1024;
-//   size_t two_k_block = 2048;
-//   size_t one_k_block = 1024;
-//   ptr = vtcm_pool->Allocate(max_bytes);
-//   vtcm_pool->Free(ptr, max_bytes);
-//   ptr = vtcm_pool->Allocate(two_k_block);
-//   vtcm_pool->Free(ptr, two_k_block);
-//   ptr = vtcm_pool->Allocate(one_k_block);
-//   vtcm_pool->Free(ptr, one_k_block);
-// }
+TEST_F(HexagonVtcmPoolTest, no_free_vtcm) {
+  void* ptr;
+  size_t max_bytes = vtcm_pool->TotalBytes();
+  ptr = vtcm_pool->Allocate(max_bytes);
+  EXPECT_THROW(vtcm_pool->Allocate(1), InternalError);
+  vtcm_pool->Free(ptr, max_bytes);
+}
+
+TEST_F(HexagonVtcmPoolTest, not_enough_free_vtcm) {
+  void* ptr;
+  size_t max_bytes = vtcm_pool->TotalBytes();
+  size_t two_k_block = 2048;
+  ptr = vtcm_pool->Allocate(max_bytes-two_k_block);
+  EXPECT_THROW(vtcm_pool->Allocate(two_k_block*2), InternalError);
+  vtcm_pool->Free(ptr, max_bytes-two_k_block);
+}
+
+TEST_F(HexagonVtcmPoolTest, free_with_wrong_size) {
+  void* ptr;
+  size_t two_k_block = 2048;
+  ptr = vtcm_pool->Allocate(two_k_block*2);
+  EXPECT_THROW(vtcm_pool->Free(ptr, two_k_block), InternalError);
+  vtcm_pool->Free(ptr, two_k_block*2);
+}
+
+TEST_F(HexagonVtcmPoolTest, free_alloc_in_middle) {
+  void* ptr1;
+  void* ptr2;
+  void* ptr3;
+  void* ptr4;
+  void* new_ptr;
+  size_t two_k_block = 2048;
+  size_t max_less_3_blocks = vtcm_pool->TotalBytes()-(3*two_k_block);
+  ptr1 = vtcm_pool->Allocate(two_k_block);
+  ptr2 = vtcm_pool->Allocate(two_k_block);
+  ptr3 = vtcm_pool->Allocate(two_k_block);
+  ptr4 = vtcm_pool->Allocate(max_less_3_blocks);
+
+  // Make sure pointers are 2k apart from each other
+  CHECK((char*)ptr1+two_k_block == (char*)ptr2);
+  CHECK((char*)ptr2+two_k_block == (char*)ptr3);
+  CHECK((char*)ptr3+two_k_block == (char*)ptr4);
+
+  // Free 2, realloc it, make sure it is the same as before
+  vtcm_pool->Free(ptr2, two_k_block);
+  new_ptr = vtcm_pool->Allocate(two_k_block);
+  CHECK(new_ptr==ptr2);
+
+  // Free 1 and 2, re-alloc and make sure they are the same
+  vtcm_pool->Free(ptr1, two_k_block);
+  vtcm_pool->Free(ptr2, two_k_block);
+  new_ptr = vtcm_pool->Allocate(two_k_block);
+  CHECK(new_ptr==ptr1);
+  new_ptr = vtcm_pool->Allocate(two_k_block);
+  CHECK(new_ptr==ptr2);
+
+  // Free in order to exercise different deletion scenarios
+  vtcm_pool->Free(ptr2, two_k_block);
+  vtcm_pool->Free(ptr3, two_k_block);
+  vtcm_pool->Free(ptr4, max_less_3_blocks);
+  vtcm_pool->Free(ptr1, two_k_block);
+}
