4797faebe323138c9d08a95bb626fb1d60841376
Tristan Konolige <tkonolige@octoml.ai>
2022-09-30 14:52:28 -0700

    cant use papi in CI because performance counters arent available

--- tests/scripts/task_config_build_cpu.sh
+++ tests/scripts/task_config_build_cpu.sh
@@ -37,7 +37,6 @@ echo set\(CMAKE_CXX_FLAGS -Werror\) >> config.cmake
 echo set\(HIDE_PRIVATE_SYMBOLS ON\) >> config.cmake
 echo set\(USE_VTA_TSIM ON\) >> config.cmake
 echo set\(USE_VTA_FSIM ON\) >> config.cmake
-echo set\(USE_PAPI ON\) >> config.cmake
 
 # This conditional is just to support the transition to cope
 # with the change in the way TFLite is built. It can be
