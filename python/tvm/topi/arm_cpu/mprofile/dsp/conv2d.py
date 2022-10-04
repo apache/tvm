058cb3485ec39987e08b4dc04aeeab2e74aef496
Gavin Uberti <gavin.uberti@gmail.com>
2022-10-04 06:30:34 -0700

    Separate into new tensordot conv2d schedule

--- python/tvm/topi/arm_cpu/mprofile/dsp/conv2d.py
+++ python/tvm/topi/arm_cpu/mprofile/dsp/conv2d.py
@@ -17,20 +17,37 @@
 # pylint: disable=invalid-name, no-value-for-parameter
 """Direct implementation of conv2d."""
 
-import random
-import string
-
-from tvm import autotvm, te, tir
+from tvm import autotvm
 from tvm.autotvm.task import deserialize_args
+from tvm import te
 from tvm.topi.utils import simplify, traverse_inline
 from tvm.topi.nn.pad import pad
 from tvm.topi.nn.utils import get_pad_tuple
 from tvm.tir.expr import Mul
 
-from .micro_kernel.tensordot import (
-    make_intrin_tensordot,
-    tensordot_impl,
+from .micro_kernel.gemm import (
+    intrin_gemm_MxKxN,
+    gemm_MxKxN_impl,
 )
+
+
+def conv2d_nhwc_dsp(*args, **kwargs):
+    """Defines the v7e-m DSP instructions of conv2d."""
+    assert not kwargs, "Do not support kwargs in template function call"
+    args = deserialize_args(args)
+    data, kernel = args[:2]
+    layout = args[-2]
+    cfg = autotvm.get_config()
+    args = [cfg] + args
+    assert layout == "NHWC"
+    conv = conv2d_nhwc_dsp_compute(*args)
+    sched = conv2d_nhwc_dsp_schedule(cfg, [data, kernel, conv])
+    return sched, [data, kernel, conv]
+
+
+conv2d_nhwc_dsp.template_key = "dsp"
+conv2d_nhwc_dsp.default_data_layout = "NHWC"
+conv2d_nhwc_dsp.default_kernel_layout = "HWOI"
 
 
 def conv2d_nhwc_dsp_compute(cfg, data, kernel, strides, padding, dilation, out_dtype):
@@ -43,134 +60,135 @@ def conv2d_nhwc_dsp_compute(cfg, data, kernel, strides, padding, dilation, out_d
     else:
         stride_h, stride_w = strides
 
-    # Dilation prevents us from using DSP instructions, so this schedule can't work (aside from the
-    # niche case where dilation_h == stride_h and dilation_w == stride_w, which is rare enough we
-    # probably don't need to support it).
     if isinstance(dilation, int):
         dilation_h = dilation_w = dilation
     else:
         dilation_h, dilation_w = dilation
-    assert dilation_h == dilation_w == 1
 
     batch_size, in_height, in_width, in_channels = data.shape
-    out_channels, kernel_h, kernel_w, _ = kernel.shape
-    assert kernel.shape[3] == in_channels
+    kernel_h, kernel_w, out_channels, _ = kernel.shape
 
-    # Compute and apply padding
-    assert isinstance(padding, tuple)
-    if len(padding) == 2:
-        (pad_up, pad_down), (pad_left, pad_right) = padding
-    else:
-        pad_up, pad_left, pad_down, pad_right = padding
+    # compute the output shape
+    dilated_kernel_h = (kernel_h - 1) * dilation_h + 1
+    dilated_kernel_w = (kernel_w - 1) * dilation_w + 1
+    pad_top, pad_left, pad_down, pad_right = get_pad_tuple(
+        padding, (dilated_kernel_h, dilated_kernel_w)
+    )
+    out_height = simplify((in_height - dilated_kernel_h + pad_top + pad_down) // stride_h + 1)
+    out_width = simplify((in_width - dilated_kernel_w + pad_left + pad_right) // stride_w + 1)
 
-    if pad_up or pad_left or pad_down or pad_right:
-        padded_data = pad(
-            data,
-            [0, pad_up, pad_left, 0],
-            [0, pad_down, pad_right, 0],
-            name="padded_data"
-        )
-    else:
-        padded_data = data
+    pad_before = [0, pad_top, pad_left, 0]
+    pad_after = [0, pad_down, pad_right, 0]
+    padded_data = pad(data, pad_before, pad_after, name="padded_data")
 
-    # Compute output dimensions
-    output_h = (in_height - kernel_h + pad_up + pad_down) // stride_h + 1
-    output_w = (in_width - kernel_w + pad_left + pad_right) // stride_w + 1
-
-    # Offsets to "prefer" the bottom right corner. This is done to match Tensorflow's convention,
-    # but does NOT match the other TVM schedules.
-    y_offset = (in_height + pad_up + pad_down - kernel_h) % stride_h
-    x_offset = (in_width + pad_left + pad_right - kernel_w) % stride_w
-
+    rc = te.reduce_axis((0, in_channels), name="rc")
     ry = te.reduce_axis((0, kernel_h), name="ry")
     rx = te.reduce_axis((0, kernel_w), name="rx")
-    rc = te.reduce_axis((0, in_channels), name="rc")
-    return te.compute(
-        (batch_size, output_h, output_w, out_channels),
+
+    conv = te.compute(
+        (batch_size, out_height, out_width, out_channels),
         lambda nn, yy, xx, ff: te.sum(
             padded_data[
-                nn, y_offset + yy * stride_h + ry * dilation_h, x_offset + xx * stride_w + rx * dilation_w, rc
+                nn, yy * stride_h + ry * dilation_h, xx * stride_w + rx * dilation_w, rc
             ].astype(out_dtype)
-            * kernel[ff, ry, rx, rc].astype(out_dtype),
+            * kernel[ry, rx, ff, rc].astype(out_dtype),
             axis=[ry, rx, rc],
         ),
         name="conv2d",
         tag="conv2d_nhwc",
     )
 
-
-def _make_tensorization(padded_data, kernel):
-    _, padded_h, padded_w, in_channels = padded_data.shape
-    _, kernel_h, kernel_w, _ = kernel.shape
-    in_dtype = padded_data.dtype
-    suffix = "".join(random.choices(string.ascii_uppercase, k=8))
-    assert in_dtype == kernel.dtype
-
-    data_slice = te.placeholder((kernel_h, kernel_w, in_channels), name="a", dtype=in_dtype)
-    kernel_slice = te.placeholder((kernel_h, kernel_w, in_channels), name="b", dtype=in_dtype)
-
-    kh_i = te.reduce_axis((0, kernel_h), name="kh_i")
-    kw_i = te.reduce_axis((0, kernel_w), name="kw_i")
-    kc_i = te.reduce_axis((0, in_channels), name="kc_i")
-
-    output_slice = te.compute((1,),
-        lambda k: te.sum(
-            data_slice[kh_i, kw_i, kc_i].astype("int32")
-            * kernel_slice[kh_i, kw_i, kc_i].astype("int32"),
-            axis=[kh_i, kw_i, kc_i],
-        ),
-        name="c",
+    ###########################
+    # Config Space Definition #
+    ###########################
+    n, oh, ow, co = (
+        cfg.axis(batch_size.value),
+        cfg.axis(out_height.value),
+        cfg.axis(out_width.value),
+        cfg.axis(out_channels.value),
+    )
+    kh, kw, ci = (
+        cfg.reduce_axis(kernel_h.value),
+        cfg.reduce_axis(kernel_w.value),
+        cfg.reduce_axis(in_channels.value),
     )
 
-    # TVM has a really strange bug where the outer reduction axis (kh_i) having length 1 causes the
-    # decl_buffer strides check to fail. height_stride is a dark magic workaround for this.
-    height_stride = in_channels * padded_w if kernel_h > 1 else in_channels
-    print(f"Using strides {[height_stride, in_channels, 1]}")
-    data_buf = tir.decl_buffer(
-        data_slice.shape, data_slice.dtype, name="foofoomcbar", offset_factor=1,
-        strides=[height_stride, in_channels, 1],
+    owo, owi = cfg.define_split("tile_ow", ow, policy="factors", num_outputs=2)
+    cio, cii = cfg.define_split(
+        "tile_ci",
+        ci,
+        policy="factors",
+        num_outputs=2,
+        # TODO: check case with in_channels.value % 4 != 0 with AutoTVM
+        filter=None if cfg.is_fallback else lambda x: x.size[-1] % 4 == 0,
     )
-    kernel_buf = tir.decl_buffer(
-        kernel_slice.shape, kernel_slice.dtype, name="kernel", offset_factor=1,
-        strides=[kernel_w * in_channels, in_channels, 1]
-    )
-    output_buf = tir.decl_buffer(
-        output_slice.shape, output_slice.dtype, name="output", offset_factor=1, strides=[1]
+    coo, coi = cfg.define_split("tile_co", co, policy="factors", num_outputs=2)
+
+    cfg.define_reorder(
+        "reorder_0_simd",
+        [n, oh, owo, owi, coo, coi, kh, kw, cio, cii],
+        policy="candidate",
+        candidate=[
+            [n, oh, kh, kw, owo, coo, cio, owi, coi, cii],
+            [n, oh, kh, kw, coo, owo, cio, owi, coi, cii],
+            [n, kh, kw, oh, owo, coo, cio, owi, coi, cii],
+            [n, kh, kw, oh, coo, owo, cio, owi, coi, cii],
+        ],
     )
 
-    jump = (padded_w - kernel_w) * in_channels
-    tensordot_params = (in_dtype, kernel_h, jump, kernel_w * in_channels, suffix)
+    cfg.define_knob("auto_unroll_max_step", [0, 2, 4, 8, 16, 32])
+    cfg.define_knob("unroll_explicit", [0, 1])
 
-    intrin_tensordot = make_intrin_tensordot(
-        output_slice.op,
-        {data_slice: data_buf, kernel_slice: kernel_buf, output_slice: output_buf},
-        tensordot_params
-    )
+    if cfg.is_fallback:
+        cfg.fallback_split("tile_ow", [-1, out_width.value])
+        cfg.fallback_split("tile_ci", [-1, in_channels.value])
+        cfg.fallback_split("tile_co", [-1, out_channels.value])
 
-    tensordot_code = tensordot_impl(*tensordot_params)
-    return (intrin_tensordot, tensordot_code)
+    return conv
 
 
 def conv2d_nhwc_dsp_schedule(cfg, outs):
     """Schedule function for v7e-m DSP instructions of conv2d."""
-    schedule = te.create_schedule([x.op for x in outs])
+    sched = te.create_schedule([x.op for x in outs])
 
-    def _callback(operator):
-        if "conv2d_nhwc" not in operator.tag:
+    def _callback(op):
+        if "conv2d_nhwc" not in op.tag:
             return
 
         # extract tensors
-        output = operator.output(0)
-        padded_data = output.op.input_tensors[0]
-        kernel = output.op.input_tensors[1]
+        output = op.output(0)
+        conv = op
+        data_vec = conv.input_tensors[0]
+        kernel = conv.input_tensors[1]  # pylint: disable=unused-variable
+        last = outs[0]  # pylint: disable=unused-variable
 
-        b_ax, y_ax, x_ax, co_ax = schedule[output].op.axis
-        kh_ax, kw_ax, ci_ax = schedule[output].op.reduce_axis
-        schedule[output].reorder(b_ax, y_ax, x_ax, co_ax, kh_ax, kw_ax, ci_ax)
+        source_index_w = output.op.body[0].source[0].a.value.indices[2].a
+        stride_w = source_index_w.b.value if isinstance(source_index_w, Mul) else 1
 
-        intrin, code = _make_tensorization(padded_data, kernel)
-        schedule[output].tensorize(kh_ax, intrin)
-        schedule[output].pragma(b_ax, "import_c", code)
+        # tile reduction axes
+        n, oh, ow, co = sched[conv].op.axis
+        kh, kw, ci = sched[conv].op.reduce_axis
 
-    traverse_inline(schedule, outs[-1].op, _callback)
-    return schedule
+        M = cfg["tile_ow"].size[-1]
+        K = cfg["tile_ci"].size[-1]
+        N = cfg["tile_co"].size[-1]
+
+        owo, owi = cfg["tile_ow"].apply(sched, conv, ow)
+        cio, cii = cfg["tile_ci"].apply(sched, conv, ci)
+        coo, coi = cfg["tile_co"].apply(sched, conv, co)
+
+        cfg["reorder_0_simd"].apply(sched, conv, [n, oh, owo, owi, coo, coi, kh, kw, cio, cii])
+
+        gemm, uniq_id = intrin_gemm_MxKxN(M, K, N, data_vec.dtype, output.dtype, stride_w)
+        sched[output].tensorize(owi, gemm)
+        sched[output].pragma(n, "import_c", gemm_MxKxN_impl(M, K, N, uniq_id))
+
+        # this is the scope to attach global config inside this kernel
+        kernel_scope = n
+
+        # tune unroll
+        sched[output].pragma(kernel_scope, "auto_unroll_max_step", cfg["auto_unroll_max_step"].val)
+        sched[output].pragma(kernel_scope, "unroll_explicit", cfg["unroll_explicit"].val)
+
+    traverse_inline(sched, outs[-1].op, _callback)
+    return sched
---
+++ python/tvm/topi/arm_cpu/mprofile/dsp/tensordot_conv2ds.py
@@ -0,0 +1,228 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# pylint: disable=invalid-name, no-value-for-parameter
+"""Direct implementation of conv2d."""
+
+import random
+import string
+
+from tvm import te, tir
+from tvm.tir import indexdiv, indexmod
+from tvm.topi.utils import traverse_inline
+from tvm.topi.nn.pad import pad
+
+from .micro_kernel.tensordot import (
+    make_intrin_tensordot,
+    tensordot_impl,
+)
+
+
+# Dilation prevents us from using DSP instructions, so this schedule can't work (aside from the
+# niche case where dilation_h == stride_h and dilation_w == stride_w, which is rare enough we
+# probably don't need to support it).
+def _check_no_dilation(dilation):
+    assert isinstance(dilation, int) or len(dilation) == 2
+    if isinstance(dilation, int):
+        dilation_h = dilation_w = dilation
+    else:
+        dilation_h, dilation_w = dilation
+    assert dilation_h == dilation_w == 1
+
+
+def _unpack_strides(strides):
+    assert isinstance(strides, int) or len(strides) == 2
+    if isinstance(strides, int):
+        return (strides, strides)
+    else:
+        return strides
+
+
+def _unpack_padding(padding):
+    assert isinstance(padding, tuple)
+    if len(padding) == 2:
+        (pad_up, pad_down), (pad_left, pad_right) = padding
+    else:
+        pad_up, pad_left, pad_down, pad_right = padding
+    return pad_up, pad_left, pad_down, pad_right
+
+
+# We only care about tuples here - "VALID" and "SAME" padding will be converted by the importer.
+def _pad_if_needed(data, unpacked_padding):
+    pad_up, pad_left, pad_down, pad_right = unpacked_padding
+    if pad_up or pad_left or pad_down or pad_right:
+        return pad(
+            data,
+            [0, pad_up, pad_left, 0],
+            [0, pad_down, pad_right, 0],
+            name="padded_data"
+        )
+    else:
+        return data
+
+
+def _compute_output_dim(data_dim, kernel_dim, pad_before, pad_after, stride):
+    return (data_dim - kernel_dim + pad_before + pad_after) // stride + 1
+
+
+# Offsets to "prefer" the bottom right corner. This is done to match TensorFlow's convention, but
+# does NOT match the other TVM schedules. We violate this convention because it improves accuracy on
+# models imported from TensorFlow.
+def _compute_offset(data_dim, kernel_dim, pad_before, pad_after, stride):
+    return (data_dim - kernel_dim + pad_before + pad_after) % stride
+
+
+def conv2d_nhwc_ohwi_dsp_compute(cfg, data, kernel, strides, padding, dilation, out_dtype):
+    stride_h, stride_w = _unpack_strides(strides)
+    pad_up, pad_left, pad_down, pad_right = _unpack_padding(padding)
+    _check_no_dilation(dilation)
+
+    batch_size, data_h, data_w, in_channels = data.shape
+    out_channels, kernel_h, kernel_w, _ = kernel.shape
+    assert kernel.shape[3] == in_channels
+
+    output_h = _compute_output_dim(data_h, kernel_h, pad_up, pad_down, stride_h)
+    output_w = _compute_output_dim(data_w, kernel_w, pad_left, pad_right, stride_w)
+    y_offset = _compute_offset(data_h, kernel_h, pad_up, pad_down, stride_h)
+    x_offset = _compute_offset(data_w, kernel_w, pad_left, pad_right, stride_w)
+
+    ry = te.reduce_axis((0, kernel_h), name="ry")
+    rx = te.reduce_axis((0, kernel_w), name="rx")
+    rc = te.reduce_axis((0, in_channels), name="rc")
+
+    padded_data = _pad_if_needed(data, unpacked_padding)
+    return te.compute(
+        (batch_size, output_h, output_w, out_channels),
+        lambda n, y, x, c: te.sum(
+            padded_data[
+                n, y_offset + y * stride_h + ry, x_offset + x * stride_w + rxh, rc
+            ].astype(out_dtype)
+            * kernel[c, ry, rx, rc].astype(out_dtype),
+            axis=(ry, rx, rc),
+        ),
+        name="conv2d",
+        tag="conv2d_nhwc_ohwi_dsp",
+    )
+
+
+def depthwise_conv2d_nchw_oihw_dsp_compute(cfg, data, kernel, strides, padding, dilation, out_dtype):
+    stride_h, stride_w = _unpack_strides(strides)
+    pad_up, pad_left, pad_down, pad_right = _unpack_padding(padding)
+    _check_no_dilation(dilation)
+
+    batch_size, in_channels, data_h, data_w = data.shape
+    channel_multiplier, _, kernel_h, kernel_w = kernel.shape
+    assert kernel.shape[1] == in_channels
+
+    output_h = _compute_output_dim(data_h, kernel_h, pad_up, pad_down, stride_h)
+    output_w = _compute_output_dim(data_w, kernel_w, pad_left, pad_right, stride_w)
+    y_offset = _compute_offset(data_h, kernel_h, pad_up, pad_down, stride_h)
+    x_offset = _compute_offset(data_w, kernel_w, pad_left, pad_right, stride_w)
+
+    ry = te.reduce_axis((0, kernel_h), name="ry")
+    rx = te.reduce_axis((0, kernel_w), name="rx")
+
+    padded_data = _pad_if_needed(data, unpacked_padding)
+    return te.compute(
+        (batch_size, output_h, output_w, out_channels),
+        lambda n, y, x, c: te.sum(
+            padded_data[
+                n, idxdiv(c, c_mul), y_offset + y * stride_h + ry, x_offset + x * stride_w + rx,
+            ].astype(out_dtype)
+            * kernel[idxmod(c, c_mul), idxdiv(c, c_mul), ry, rx].astype(out_dtype),
+            axis=(ry, rx),
+        ),
+        name="depthwise_conv2d",
+        tag="depthwise_conv2d_nchw_oihw_dsp",
+    )
+
+def _make_tensorization(padded_data, kernel):
+    _, padded_h, padded_w, in_channels = padded_data.shape
+    _, kernel_h, kernel_w, _ = kernel.shape
+    in_dtype = padded_data.dtype
+    suffix = "".join(random.choices(string.ascii_uppercase, k=8))
+    assert in_dtype == kernel.dtype
+
+    data_slice = te.placeholder((kernel_h, kernel_w, in_channels), name="a", dtype=in_dtype)
+    kernel_slice = te.placeholder((kernel_h, kernel_w, in_channels), name="b", dtype=in_dtype)
+
+    kh_i = te.reduce_axis((0, kernel_h), name="kh_i")
+    kw_i = te.reduce_axis((0, kernel_w), name="kw_i")
+    kc_i = te.reduce_axis((0, in_channels), name="kc_i")
+
+    output_slice = te.compute((1,),
+        lambda k: te.sum(
+            data_slice[kh_i, kw_i, kc_i].astype("int32")
+            * kernel_slice[kh_i, kw_i, kc_i].astype("int32"),
+            axis=[kh_i, kw_i, kc_i],
+        ),
+        name="c",
+    )
+
+    # TVM has a really strange bug where the outer reduction axis (kh_i) having length 1 causes the
+    # decl_buffer strides check to fail. height_stride is a dark magic workaround for this.
+    height_stride = in_channels * padded_w if kernel_h > 1 else in_channels
+    data_buf = tir.decl_buffer(
+        data_slice.shape, data_slice.dtype, name="foofoomcbar", offset_factor=1,
+        strides=[height_stride, in_channels, 1],
+    )
+    kernel_buf = tir.decl_buffer(
+        kernel_slice.shape, kernel_slice.dtype, name="kernel", offset_factor=1,
+        strides=[kernel_w * in_channels, in_channels, 1]
+    )
+    output_buf = tir.decl_buffer(
+        output_slice.shape, output_slice.dtype, name="output", offset_factor=1, strides=[1]
+    )
+
+    jump = (padded_w - kernel_w) * in_channels
+    tensordot_params = (in_dtype, kernel_h, jump, kernel_w * in_channels, suffix)
+
+    intrin_tensordot = make_intrin_tensordot(
+        output_slice.op,
+        {data_slice: data_buf, kernel_slice: kernel_buf, output_slice: output_buf},
+        tensordot_params
+    )
+
+    tensordot_code = tensordot_impl(*tensordot_params)
+    return (intrin_tensordot, tensordot_code)
+
+
+def tensordot_conv2ds_schedule(cfg, outs):
+    schedule = te.create_schedule([x.op for x in outs])
+
+    def _callback(operator):
+        if "conv2d" in operator.tag:
+            output = operator.output(0)
+            padded_data = output.op.input_tensors[0]
+            kernel = output.op.input_tensors[1]
+
+            if tag == "conv2d_nhwc_ohwi_dsp":
+                b_ax, y_ax, x_ax, co_ax = schedule[output].op.axis
+                kh_ax, kw_ax, ci_ax = schedule[output].op.reduce_axis
+                schedule[output].reorder(b_ax, y_ax, x_ax, co_ax, kh_ax, kw_ax, ci_ax)
+                intrin, code = _make_tensorization(padded_data, kernel)
+
+            elif tag == "depthwise_conv2d_nchw_oihw_dsp"
+                b_ax, y_ax, x_ax, co_ax = schedule[output].op.axis
+                kh_ax, kw_ax = schedule[output].op.reduce_axis
+                schedule[output].reorder(b_ax, co_ax, y_ax, x_ax, kh_ax, kw_ax)
+                intrin, code = _make_tensorization(padded_data, kernel)
+
+            schedule[output].tensorize(kh_ax, intrin)
+            schedule[output].pragma(b_ax, "import_c", code)
+
+    traverse_inline(schedule, outs[-1].op, _callback)
+    return schedule
