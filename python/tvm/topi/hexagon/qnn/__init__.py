21ae90473606bc699cc3373d8818fef457b2a4e0
Gayatri Panchapakesan Kumari <quic_gpk@quicinc.com>
2022-10-17 16:38:22 +0530

    [TOPI][Hexagon] Implement quantized depthwise conv2d

--- python/tvm/topi/hexagon/qnn/__init__.py
+++ python/tvm/topi/hexagon/qnn/__init__.py
@@ -26,3 +26,4 @@ from .dequantize import (
 
 from .quantize import quantize_compute, tir_quantize_schedule
 from .nn import *
+from .qdepthwise_conv2d_slice import qdepthwise_conv2d_compute, qdepthwise_conv2d_schedule
