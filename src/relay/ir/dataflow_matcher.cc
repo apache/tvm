73590bd3e9c91c0972244f6f49b34fef1875a0d4
Lei Wen <wenlei03@qiyi.com>
2022-11-02 20:47:31 +0800

    [Relay] Enhancement for fold_scale_axis and simplify_expr
    * add(%1, %1) convert to multiply(%1, 2f)
    
    * enhance fold_scale_axis to fold multiply(%1, 2f) into conv
    
    Signed-off-by: Lei Wen <wenlei03@qiyi.com>

--- src/relay/ir/dataflow_matcher.cc
+++ src/relay/ir/dataflow_matcher.cc
@@ -427,16 +427,6 @@ bool DFPatternMatcher::VisitDFPattern_(const LetPatternNode* op, const Expr& exp
   return false;
 }
 
-Expr InferType(const Expr& expr) {
-  auto mod = IRModule::FromExpr(expr);
-  mod = transform::InferType()(mod);
-  if (expr.as<FunctionNode>()) {
-    return mod->Lookup("main");
-  } else {
-    return mod->Lookup("main").as<FunctionNode>()->body;
-  }
-}
-
 Expr InferTypeWithModule(const Expr& expr, const IRModule& m) {
   IRModule mod(m->functions, m->type_definitions, m->Imports());
   GlobalVarSupply global_var_supply = GlobalVarSupply(mod);
