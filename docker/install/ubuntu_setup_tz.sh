62beb63c90ecf677a1e7f86af690a78b636e1125
Mehrdad Hessar <mhessar@octoml.ai>
2022-11-09 14:26:21 -0800

    refactor timezone setup

--- docker/install/ubuntu_setup_tz.sh
+++ docker/install/ubuntu_setup_tz.sh
@@ -22,25 +22,6 @@ set -u
 set -x
 set -o pipefail
 
-# install libraries for building c++ core on ubuntu
-apt-get update && apt-install-and-clear -y --no-install-recommends \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    g++ \
-    gdb \
-    git \
-    graphviz \
-    libcurl4-openssl-dev \
-    libopenblas-dev \
-    libssl-dev \
-    libtinfo-dev \
-    libz-dev \
-    lsb-core \
-    make \
-    ninja-build \
-    parallel \
-    pkg-config \
-    sudo \
-    unzip \
-    wget \
+export TZ=Etc/UTC
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
