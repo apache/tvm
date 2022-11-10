1a0b312b41a974b11284a6deba0f242069db72d5
trahman-quic <trahman@quicinc.com>
2022-11-10 15:04:54 -0600

    Fix lint issues in test

--- tests/python/contrib/test_hexagon/topi/test_adaptive_avg_pool1d.py
+++ tests/python/contrib/test_hexagon/topi/test_adaptive_avg_pool1d.py
@@ -15,6 +15,8 @@
 # specific language governing permissions and limitations
 # under the License.
 
+"""Test code for specialized case of adaptive_avg_pool1d."""
+
 import numpy as np
 
 import tvm
@@ -24,78 +26,15 @@ import tvm.topi.hexagon.qnn as s1
 from ..infrastructure import allocate_hexagon_array, transform_numpy, quantize_np
 
 
-@tvm.testing.fixture
-def expected_output_np(
-    input_np,
-    output_size,
-    pool_type,
-    layout,
-):
-    out_width = output_size[0]
-
-    ref_np = adaptive_pool(
-        input_np,
-        out_width,
-        pool_type,
-        layout,
-    )
-    return ref_np
-
-
-@tvm.testing.fixture
-def input_np(input_shape, dtype):
-    if dtype in ("uint8", "int8"):
-        dtype = "float32"
-    return np.random.random(input_shape).astype(dtype)
-
-
-@tvm.testing.fixture
-def quantize_input_np(input_np, dtype):
-    if dtype in ("uint8", "int8"):
-        global zero_point_val, scale_val
-        input_np_quantized, scale_val, zero_point_val = quantize_np(input_np, dtype)
-        return input_np_quantized
-
-
-@tvm.testing.fixture
-def transformed_input_np(input_np, quantize_input_np, input_layout, layout, dtype):
-    if dtype in ("uint8", "int8"):
-        return transform_numpy(quantize_input_np, layout.lower(), input_layout)
-
-    raise RuntimeError(f"Unsupported data type '{dtype}'")
-
-
-@tvm.testing.fixture
-def quantize_expected_output_np(expected_output_np, output_layout, layout, dtype):
-    if dtype in ("uint8", "int8"):
-        global zero_point_M_val, scale_M_val
-        out_ref_quantized, scale_M_val, zero_point_M_val = quantize_np(expected_output_np, dtype)
-
-        # Since output_layout is ncw, no transformation is needed.
-        return out_ref_quantized
-
-    raise RuntimeError(f"Unsupported data type '{dtype}'")
-
-
-# Fixed chunk layout is set as ncw-32c64w-2d for now.
-# The adaptive_avg_pool1d implementation only handles specialized case
-# where output_size is 1 as it appears on quantized distilbert model.
-# Since output size won't be a multiple of fixed-chunk,
-# output_layout is ncw.
-# For optimization, it might get changed later.
-input_layout, output_layout, pool_type, layout, output_size, dtype, = tvm.testing.parameters(
-    (
-        "ncw-32c64w-2d",
-        "ncw",
-        "avg",
-        "NCW",
-        [1],
-        "uint8",
-    )
-)
+SCALE_M_VAL = None
+ZERO_POINT_M_VAL = None
+SCALE_VAL = None
+ZERO_POINT_VAL = None
 
 
 class TestAdaptivePool1D:
+    """Test specialized case of adaptive_avg_pool1d."""
+
     (input_shape,) = tvm.testing.parameters(
         ([1, 128, 128],),
         ([1, 64, 64],),
@@ -103,6 +42,78 @@ class TestAdaptivePool1D:
         ([1, 32, 64],),
         ([1, 128, 768],),
     )
+
+    # Fixed chunk layout is set as ncw-32c64w-2d for now.
+    # The adaptive_avg_pool1d implementation only handles specialized case
+    # where output_size is 1 as it appears on quantized distilbert model.
+    # Since output size won't be a multiple of fixed-chunk,
+    # output_layout is ncw.
+    # For optimization, it might get changed later.
+    input_layout, output_layout, pool_type, layout, output_size, dtype, = tvm.testing.parameters(
+        (
+            "ncw-32c64w-2d",
+            "ncw",
+            "avg",
+            "NCW",
+            [1],
+            "uint8",
+        )
+    )
+
+    @tvm.testing.fixture
+    def expected_output_np(
+        self,
+        input_np,
+        output_size,
+        pool_type,
+        layout,
+    ):
+        """Generate expected output."""
+        out_width = output_size[0]
+
+        ref_np = adaptive_pool(
+            input_np,
+            out_width,
+            pool_type,
+            layout,
+        )
+        return ref_np
+
+    @tvm.testing.fixture
+    def input_np(self, input_shape, dtype):
+        if dtype in ("uint8", "int8"):
+            dtype = "float32"
+        return np.random.random(input_shape).astype(dtype)
+
+    @tvm.testing.fixture
+    def quantize_input_np(self, input_np, dtype):
+        if dtype in ("uint8", "int8"):
+            global ZERO_POINT_VAL, SCALE_VAL
+            input_np_quantized, SCALE_VAL, ZERO_POINT_VAL = quantize_np(input_np, dtype)
+            return input_np_quantized
+
+        raise RuntimeError(f"Unsupported data type '{dtype}'")
+
+    @tvm.testing.fixture
+    def transformed_input_np(self, quantize_input_np, input_layout, layout, dtype):
+        if dtype in ("uint8", "int8"):
+            return transform_numpy(quantize_input_np, layout.lower(), input_layout)
+
+        raise RuntimeError(f"Unsupported data type '{dtype}'")
+
+    @tvm.testing.fixture
+    def quantize_expected_output_np(self, expected_output_np, dtype):
+        """Generate expected output."""
+        if dtype in ("uint8", "int8"):
+            global ZERO_POINT_M_VAL, SCALE_M_VAL
+            out_ref_quantized, SCALE_M_VAL, ZERO_POINT_M_VAL = quantize_np(
+                expected_output_np, dtype
+            )
+
+            # Since output_layout is ncw, no transformation is needed.
+            return out_ref_quantized
+
+        raise RuntimeError(f"Unsupported data type '{dtype}'")
 
     @tvm.testing.requires_hexagon
     def test_pool1d(
@@ -116,34 +127,37 @@ class TestAdaptivePool1D:
         quantize_expected_output_np,
         hexagon_session,
     ):
+        """Test adaptive_avg_pool1d."""
         target_hexagon = tvm.target.hexagon("v69")
-        A = te.placeholder(input_shape, name="A", dtype=dtype)
+        a_tensor = te.placeholder(input_shape, name="a_tensor", dtype=dtype)
 
-        M = s1.adaptive_avg_pool1d(
-            A,
+        m_tensor = s1.adaptive_avg_pool1d(
+            a_tensor,
             output_size,
             dtype,
-            zero_point_val,
-            scale_val,
-            zero_point_M_val,
-            scale_M_val,
+            ZERO_POINT_VAL,
+            SCALE_VAL,
+            ZERO_POINT_M_VAL,
+            SCALE_M_VAL,
         )
 
-        tir_schedule = s1.tir_adaptive_avg_pool1d_schedule(M, A, output_layout, input_layout)
+        tir_schedule = s1.tir_adaptive_avg_pool1d_schedule(
+            m_tensor, a_tensor, output_layout, input_layout
+        )
 
         sch = tir_schedule.mod
 
         with tvm.transform.PassContext(opt_level=3):
             func = tvm.build(
                 sch,
-                [A, M],
+                [a_tensor, m_tensor],
                 tvm.target.Target(target_hexagon, host=target_hexagon),
                 name="adaptive_pool1d",
             )
 
         input_axis_separator = [3]
 
-        A_data_nd = allocate_hexagon_array(
+        a_data_nd = allocate_hexagon_array(
             hexagon_session.device,
             data=transformed_input_np,
             dtype=dtype,
@@ -151,19 +165,19 @@ class TestAdaptivePool1D:
             mem_scope="global.vtcm",
         )
 
-        M_data_nd = allocate_hexagon_array(
+        m_data_nd = allocate_hexagon_array(
             hexagon_session.device,
             quantize_expected_output_np.shape,
             dtype=dtype,
         )
 
         mod = hexagon_session.load_module(func)
-        mod(A_data_nd, M_data_nd)
+        mod(a_data_nd, m_data_nd)
 
         # Convert nd to np
-        M_data_np = M_data_nd.numpy()
+        m_data_np = m_data_nd.numpy()
 
-        np.testing.assert_allclose(quantize_expected_output_np, M_data_np, atol=2)
+        np.testing.assert_allclose(quantize_expected_output_np, m_data_np, atol=2)
 
 
 if __name__ == "__main__":
