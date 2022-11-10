709e1d70eb64f1539d8aeb1a6b6068f4a38a6c26
Masahiro Masuda <masahi129@gmail.com>
2022-11-11 06:34:33 +0900

    Fuse shared to global store loops in MultiLevelTilingTensorCore

--- src/meta_schedule/schedule_rule/multi_level_tiling.cc
+++ src/meta_schedule/schedule_rule/multi_level_tiling.cc
@@ -269,8 +269,8 @@ std::vector<State> MultiLevelTilingNode::AddReadReuse(State state) const {
       sch->ComputeAt(cache_read_block, loop_rv, true);
       // Fuse the iterators of the cache_read
       Array<LoopRV> buffer_loops = sch->GetLoops(cache_read_block);
-      LoopRV fused = sch->Fuse(Array<LoopRV>{buffer_loops.end() - buffer_ndim,  //
-                                             buffer_loops.end()});
+      sch->Fuse(Array<LoopRV>{buffer_loops.end() - buffer_ndim,  //
+                              buffer_loops.end()});
       AnnotateCooperativeFetching(&sch, cache_read_block);
       new_state->read_reuse.emplace(i, cache_read_block);
     }
