8eff158470894fa495710cbfacba7f1984de138b
krishnaraj36 <45380557+krishnaraj36@users.noreply.github.com>
2022-11-19 01:55:12 +0530

    [RUNTIME][CLML] Add fixes to clml runtime api (#13426)
    * [RUNTIME][CLML] Add fixes to clml runtime
    
    Handled unsupported configuration for ops in clml pattern table
    and updated Dense op runtime api with conv 1x1 kernel api invocation.
    
    * [RUNTIME][CLML] Add fixes to clml runtime
    
    Handled unsupported configuration for ops in clml pattern table and
    updated Dense op runtime api with conv 1x1 kernel api invocation.
    
    * Fix the lint error for clml.py
    
    * Fix the lint error for whitespace
    
    * Fix the lint error for space
    
    * Fix the lint error for if condition
    
    * Fixes for lint error of whitespace
    
    * Fixes for lint error of trailing space
    
    * Fixes for lint error of trailing space
    
    * Fixes for lint error of whitespace
    
    * Fixes for lint error of trailingspace
    
    Co-authored-by: kvegiraj <kvegiraj@qti.qualcomm.com>

--- python/tvm/relay/op/contrib/clml.py
+++ python/tvm/relay/op/contrib/clml.py
@@ -137,11 +137,12 @@ def clml_pattern_table():
         pattern = pattern.optional(lambda x: is_op("nn.bias_add")(x, is_constant()))
         pattern = pattern.optional(lambda x: is_op("add")(x, is_constant()))
         pattern = pattern.optional(
-            lambda x: is_op("nn.batch_norm")(
-                x, is_constant(), is_constant(), is_constant(), is_constant()
+            lambda x: is_tuple_get_item(
+                is_op("nn.batch_norm")(
+                    x, is_constant(), is_constant(), is_constant(), is_constant()
+                )
             )
         )
-        pattern = pattern.optional(is_tuple_get_item)
         pattern = pattern.optional(is_op("nn.relu"))
         pattern = pattern.optional(is_op("clip"))
         return pattern
@@ -176,12 +177,13 @@ def clml_pattern_table():
 
     def pad_pattern():
         """Create a pad pattern."""
-        pattern = is_op("nn.pad")(wildcard(), wildcard())
+        pattern = is_op("nn.pad")(wildcard(), is_constant())
         return pattern
 
     def check_conv(extract):
         """Check conv pattern is supported by CLML."""
         call = extract
+        clip_found = False
         if isinstance(call, tvm.relay.expr.TupleGetItem):
             call = call.tuple_value
         elif call.op.name == "nn.relu":
@@ -189,6 +191,7 @@ def clml_pattern_table():
             if isinstance(call, tvm.relay.expr.TupleGetItem):
                 call = call.tuple_value
         elif call.op.name == "clip":
+            clip_found = True
             if call.attrs["a_min"] != 0.0 or call.attrs["a_max"] != 6.0:
                 return False
             call = call.args[0]
@@ -199,6 +202,14 @@ def clml_pattern_table():
             call = call.args[0]
         attrs, args = call.attrs, call.args
         if attrs.data_layout != "NCHW":
+            return False
+        if (
+            (not clip_found)
+            and (attrs.kernel_size[0] == 3)
+            and (attrs.dilation[0] != 1)
+            and (attrs.groups != 1)
+            and (attrs.channels == attrs.groups)
+        ):
             return False
         data_typ = args[0].checked_type
         kernel_typ = args[1].checked_type
@@ -213,12 +224,44 @@ def clml_pattern_table():
             return False
         return True
 
+    def check_binary_op(extract):
+        call = extract
+        if len(call.args[1].checked_type.shape) > 0:
+            return True
+        return False
+
+    def check_pad_op(extract):
+        call = extract
+        if len(call.attrs["pad_width"]) != 4:
+            return False
+        return True
+
+    def check_softmax_op(extract):
+        call = extract
+        if len(call.args[0].checked_type.shape) > 2:
+            return False
+        return True
+
+    def check_default_op(extract):
+        return True
+
     return [
         ("clml.conv2d", conv_pattern(), check_conv),
-        ("clml.dense", dense_pattern()),
-        ("clml.pad", pad_pattern()),
-        ("clml.concat", concat_pattern()),
-        ("clml.batch_norm", batch_norm_pattern()),
+        ("clml.dense", dense_pattern(), check_default_op),
+        ("clml.pad", pad_pattern(), check_pad_op),
+        ("clml.concat", concat_pattern(), check_default_op),
+        ("clml.batch_norm", batch_norm_pattern(), check_default_op),
+        ("clml.add", is_op("add")(wildcard(), wildcard()), check_binary_op),
+        ("clml.subtract", is_op("subtract")(wildcard(), wildcard()), check_binary_op),
+        ("clml.multiply", is_op("multiply")(wildcard(), wildcard()), check_binary_op),
+        ("clml.softmax", is_op("nn.softmax")(wildcard()), check_softmax_op),
+        ("clml.reshape", is_op("reshape")(wildcard()), check_default_op),
+        ("clml.avg_pool2d", is_op("nn.avg_pool2d")(wildcard()), check_default_op),
+        ("clml.max_pool2d", is_op("nn.max_pool2d")(wildcard()), check_default_op),
+        ("clml.global_avg_pool2d", is_op("nn.global_avg_pool2d")(wildcard()), check_default_op),
+        ("clml.global_max_pool2d", is_op("nn.global_max_pool2d")(wildcard()), check_default_op),
+        ("clml.relu", is_op("nn.relu")(wildcard()), check_default_op),
+        ("clml.clip", is_op("clip")(wildcard()), check_default_op),
     ]
 
 
@@ -230,17 +273,6 @@ def _register_external_op_helper(op_name, supported=True):
     return _func_wrapper
 
 
-_register_external_op_helper("clip")
-_register_external_op_helper("nn.relu")
-_register_external_op_helper("nn.global_avg_pool2d")
-_register_external_op_helper("nn.global_max_pool2d")
-_register_external_op_helper("nn.avg_pool2d")
-_register_external_op_helper("nn.max_pool2d")
-_register_external_op_helper("nn.softmax")
-_register_external_op_helper("reshape")
-_register_external_op_helper("add")
-_register_external_op_helper("subtract")
-_register_external_op_helper("multiply")
 _register_external_op_helper("minimum")
 _register_external_op_helper("maximum")
 
