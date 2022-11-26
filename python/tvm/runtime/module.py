0fe51d016f6e4191bcb762441a8577197a845355
jinhongyi <3231950289@qq.com>
2022-11-25 20:21:08 -0800

    add assert

--- python/tvm/runtime/module.py
+++ python/tvm/runtime/module.py
@@ -519,6 +519,7 @@ class Module(object):
 
         if self.imported_modules:
             if enabled("llvm") and llvm_target_string:
+                assert global_object_format is not None
                 path_obj = os.path.join(workspace_dir, f"devc.{global_object_format}")
                 m = _ffi_api.ModulePackImportsToLLVM(self, is_system_lib, llvm_target_string)
                 m.save(path_obj)
