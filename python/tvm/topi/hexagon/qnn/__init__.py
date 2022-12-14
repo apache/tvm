6dc26a91df1c2c0317535018625da2e779d73acd
trahman-quic <trahman@quicinc.com>
2022-12-14 13:27:47 -0600

    [TOPI][Hexagon] Implement global_avg_pool2d for hexagon

--- python/tvm/topi/hexagon/qnn/__init__.py
+++ python/tvm/topi/hexagon/qnn/__init__.py
@@ -28,3 +28,4 @@ from .quantize import quantize_compute, tir_quantize_schedule
 from .nn import *
 from .qdepthwise_conv2d_slice import qdepthwise_conv2d_compute, qdepthwise_conv2d_schedule
 from .adaptive_avg_pool1d import *
+from .global_avg_pool2d import *
--- python/tvm/topi/hexagon/slice_ops/__init__.py
+++ python/tvm/topi/hexagon/slice_ops/__init__.py
@@ -36,3 +36,4 @@ from .relu import relu_compute, relu_stir_schedule
 from .tanh import tanh_te_compute, tanhf16_schedule
 from .dwconv2d import *
 from .depth_to_space import d2s_compute, d2s_schedule
+from .global_avg_pool2d import *
