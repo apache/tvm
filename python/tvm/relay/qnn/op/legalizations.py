57fbc5ea5ed0a63471c663bbc2f5c09b6263afc4
Valery Chernov <valery.chernov@deelvin.com>
2022-12-15 17:05:43 +0300

    fix misprinting

--- python/tvm/relay/qnn/op/legalizations.py
+++ python/tvm/relay/qnn/op/legalizations.py
@@ -248,7 +248,7 @@ def helper_change_dtypes_to_uint8_int8(attrs, inputs, types, relay_op):
     Replacing QA + 128 with QA' and (zp_a + 128) with zp_a'
     We get our new quantized uint8 tensor - scale * (QA' - zp_a')
 
-    Similarly we can convert from int8 to uint8.
+    Similarly we can convert from uint8 to int8.
 
     Parameters
     ----------
