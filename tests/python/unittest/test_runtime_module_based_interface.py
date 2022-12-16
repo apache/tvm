710528cfe7215f3527265197bf707d8782167c84
YJ Shi <yuanjing@octoml.ai>
2022-12-16 12:39:55 -0800

    remove pytest

--- tests/python/unittest/test_runtime_module_based_interface.py
+++ tests/python/unittest/test_runtime_module_based_interface.py
@@ -15,7 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 import numpy as np
-import pytest
 import os
 from tvm import relay, runtime
 from tvm.relay import testing
