7e2b8dd2aca49a60819d891359618c6c012c7131
Luke Hutton <luke.hutton@arm.com>
2022-12-20 14:26:45 +0000

    [CI] Fix android build by constraining numpy version (#13648)
    Temporarily constrain the version of numpy to workaround the
    deprecated value used in mxnet. See #13647.
    
    Change-Id: Ib271c223447c76b855fe35cc8a1e77411a3fa441

--- python/gen_requirements.py
+++ python/gen_requirements.py
@@ -251,7 +251,8 @@ CONSTRAINTS = [
     ("h5py", "==2.10.0"),
     ("image", None),
     ("matplotlib", None),
-    ("numpy", None),
+    # Workaround, see https://github.com/apache/tvm/issues/13647
+    ("numpy", "<=1.23.*"),
     ("onnx", None),
     ("onnxoptimizer", None),
     ("onnxruntime", None),
