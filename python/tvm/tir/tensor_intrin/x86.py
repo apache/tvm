582caa9fb8ffa753214d753f8ca20b94c28ba4e0
Valery Chernov <valery.chernov@deelvin.com>
2022-12-23 09:00:55 +0300

    test workaround for const alloc in tir

--- python/tvm/tir/tensor_intrin/x86.py
+++ python/tvm/tir/tensor_intrin/x86.py
@@ -16,6 +16,7 @@
 # under the License.
 # pylint: disable=invalid-name,missing-function-docstring
 """Intrinsics for x86 tensorization."""
+import tvm
 from tvm.script import tir as T
 from .. import TensorIntrin
 
@@ -103,9 +104,10 @@ def dot_product_16x4_u8i8i32_avx512(
             dtype="int16x32",
         )
 
-        data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        One = T.allocate_const(data, "int16", [32])
-        One_int16x32 = T.reinterpret(One, dtype="int16x32")
+        One_int16x32 = tvm.tir.call_intrin("int16x32", "tir.const", 1)
+        # data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        # One = T.allocate_const(data, "int16", [32])
+        # One_int16x32 = T.reinterpret(One, dtype="int16x32")
 
         C[T.ramp(T.int32(0), 1, 16)] += T.call_llvm_pure_intrin(
             T.llvm_lookup_intrinsic_id("llvm.x86.avx512.pmaddw.d.512"),
