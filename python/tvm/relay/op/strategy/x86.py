62d985530bce09adfb9f5f1cd825576783f18567
Jiang, Qianshui <qianshui.jiang@intel.com>
2023-01-10 21:09:31 +0000

    remove unused import

--- python/tvm/relay/op/strategy/x86.py
+++ python/tvm/relay/op/strategy/x86.py
@@ -23,7 +23,6 @@ from tvm import tir, topi
 from tvm.auto_scheduler import is_auto_scheduler_enabled
 from tvm.meta_schedule import is_meta_schedule_enabled
 from tvm.relay.ty import is_dynamic
-from tvm.target import Target
 from tvm.te import SpecializedCondition
 
 from .. import op as _op
