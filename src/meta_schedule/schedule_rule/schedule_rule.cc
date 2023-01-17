2d772f6f4d18cc3242a5ceb93e99861fabc34906
Valery Chernov <valery.chernov@deelvin.com>
2023-01-17 12:08:24 +0300

    lint fix

--- src/meta_schedule/schedule_rule/schedule_rule.cc
+++ src/meta_schedule/schedule_rule/schedule_rule.cc
@@ -86,10 +86,8 @@ Array<ScheduleRule> ScheduleRule::DefaultLLVM() {
 }
 
 Array<ScheduleRule> ScheduleRule::DefaultX86(const String& type) {
-  static const Map<String, String> intrins = {
-    {"vnni", "dot_16x4_vnni"},
-    {"avx512", "dot_16x4_avx512"}
-  };
+  static const Map<String, String> intrins = {{"vnni", "dot_16x4_vnni"},
+                                              {"avx512", "dot_16x4_avx512"}};
   return {
       ScheduleRule::ApplyCustomRule(),
       ScheduleRule::InlineConstantScalars(),
