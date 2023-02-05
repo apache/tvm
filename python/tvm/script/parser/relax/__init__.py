5dead194c3b87110c4f43a6199f12dcc0207ff5c
Yuchen Jin <yuchenj@cs.washington.edu>
2023-02-05 00:51:08 -0500

    [Unity] Basic StructInfo Analysis and Expr construction (#13916)
    [Unity] Basic StructInfo Analysis and Expr construction.
    
    This PR adds struct info analysis and expr support.
    These are logics to construct the IR node and perform
    struct info related analysis.
    
    Testcases are added to cover the IR node construction
    and related struct info analysis checks.
    
    Co-authored-by: Tianqi Chen <tianqi.tchen@gmail.com>
    Co-authored-by: Altan Haan <altanh@cs.washington.edu>
    Co-authored-by: Andrew Liu <andrewlliu@gmail.com>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Jiawei Liu <jaway.liu@gmail.com>
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
    Co-authored-by: masahi <masahi129@gmail.com>
    Co-authored-by: Prakalp Srivastava <prakalp@octoml.ai>
    Co-authored-by: Ruihang Lai <ruihangl@cs.cmu.edu>
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-authored-by: Steven S. <Lyubomirsky slyubomirsky@octoml.ai>
    Co-authored-by: Sunghyun Park <49998730+sunggg@users.noreply.github.com>
    Co-authored-by: Yixin Dong <ubospica@gmail.com>
    Co-authored-by: Yong Wu <yongcale@gmail.com>
    Co-authored-by: Ziheng Jiang <ziheng@apache.org>

--- python/tvm/script/parser/relax/__init__.py
+++ python/tvm/script/parser/relax/__init__.py
@@ -14,8 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""TVM Script APIs of TVM Python Package"""
-from .parser import ir, ir_module
-from .parser import parse as from_source
-from .parser import tir
-from .parser import relax
+"""Initial impl of relax parser for sugars"""
+from tvm.relax import TensorStructInfo, ShapeStructInfo
+
+Tensor = TensorStructInfo
+Shape = ShapeStructInfo
