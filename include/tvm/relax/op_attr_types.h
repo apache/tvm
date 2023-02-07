58f12c4e4328b12dc8a3e2693e50e78c37624071
Yuchen Jin <yuchenj@cs.washington.edu>
2023-02-07 01:30:11 -0500

    [Unity] Relax BlockBuilder and ExprMutator (#13926)
    This PR adds BlockBuilder: the core data structure to construct Relax AST, and ExprMutator: performs AST mutation for implementing transformation passes.
    
    Co-Authored-by: Tianqi Chen <tianqi.tchen@gmail.com>
    Co-Authored-by: Altan Haan <altanh@cs.washington.edu>
    Co-Authored-by: Andrew Liu <andrewlliu@gmail.com>
    Co-Authored-by: Hongyi Jin <3231950289@qq.com>
    Co-Authored-by: Jiawei Liu <jaway.liu@gmail.com>
    Co-Authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-Authored-by: Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
    Co-Authored-by: masahi <masahi129@gmail.com>
    Co-Authored-by: Prakalp Srivastava <prakalp@octoml.ai>
    Co-Authored-by: Ruihang Lai <ruihangl@cs.cmu.edu>
    Co-Authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-Authored-by: Steven S. <Lyubomirsky slyubomirsky@octoml.ai>
    Co-Authored-by: Sunghyun Park <49998730+sunggg@users.noreply.github.com>
    Co-Authored-by: Yixin Dong <ubospica@gmail.com>
    Co-Authored-by: Yong Wu <yongcale@gmail.com>
    Co-Authored-by: Ziheng Jiang <ziheng@apache.org>

---
+++ include/tvm/relax/op_attr_types.h
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file tvm/relax/op_attr_types.h
+ * \brief Data structures that can appear in operator attributes.
+ */
+#ifndef TVM_RELAX_OP_ATTR_TYPES_H_
+#define TVM_RELAX_OP_ATTR_TYPES_H_
+
+#include <tvm/relax/expr.h>
+#include <tvm/relax/struct_info.h>
+#include <tvm/te/schedule.h>
+#include <tvm/te/tensor.h>
+
+#include <string>
+
+namespace tvm {
+namespace relax {
+
+/*!
+ * \brief Infer output struct info given the call
+ *
+ * \param call The call expression to be derived.
+ * \param ctx The builder context.
+ */
+using FInferStructInfo =
+    runtime::TypedPackedFunc<StructInfo(const Call& call, const BlockBuilder& ctx)>;
+
+/*!
+ * \brief Packed function implementation for operators. The relax operator will be lowered to
+ * this packed function call during codegen.
+ */
+using FCallPacked = String;
+
+struct PrintAttrs : public tvm::AttrsNode<PrintAttrs> {
+  std::string format;
+  TVM_DECLARE_ATTRS(PrintAttrs, "relax.attrs.PrintAttrs") {
+    TVM_ATTR_FIELD(format)
+        .describe("Python-style format string to use for displaying the input. Ignored if empty.")
+        .set_default("");
+  }
+};
+
+struct AssertOpAttrs : public tvm::AttrsNode<AssertOpAttrs> {
+  std::string format;
+  TVM_DECLARE_ATTRS(AssertOpAttrs, "relax.attrs.AssertOpAttrs") {
+    TVM_ATTR_FIELD(format)
+        .describe(
+            "Python-style format string to use for displaying "
+            "an error message if the assert fails. "
+            "Ignored if empty.")
+        .set_default("");
+  }
+};
+
+}  // namespace relax
+}  // namespace tvm
+#endif  // TVM_RELAX_OP_ATTR_TYPES_H_
