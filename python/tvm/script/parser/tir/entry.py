a355ecb978ddffa0f0d1ffa10d2bda543a935167
Siyuan Feng <Hzfengsy@sjtu.edu.cn>
2023-02-08 22:31:47 +0800

    [Unity] Relax TVMScript Parser. (#13932)
    This PR adds the TVMScript parser/ir_builder support based on the blockbuilder.
    
    Co-authored-by: Ruihang Lai <ruihangl@cs.cmu.edu>
    Co-authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-authored-by: Tianqi Chen <tianqi.tchen@gmail.com>
    Co-authored-by: Yuchen Jin <yuchenj@cs.washington.edu>
    Co-authored-by: Steven S. Lyubomirsky <slyubomirsky@gmail.com>
    Co-authored-by: Yong Wu <yongcale@gmail.com>

--- python/tvm/script/parser/tir/entry.py
+++ python/tvm/script/parser/tir/entry.py
@@ -85,7 +85,7 @@ class BufferProxy:
             return self(keys)
         if len(keys) >= 2 and not isinstance(keys[1], str):
             return self(keys)
-        return self(*keys)  # pylint: disable=no-member # type: ignore
+        return self(*keys)  # type: ignore[attr-defined] # pylint: disable=no-member
 
 
 class PtrProxy:
@@ -96,7 +96,7 @@ class PtrProxy:
     def __call__(self, dtype, storage_scope="global"):
         if callable(dtype):
             dtype = dtype().dtype
-        return ptr(dtype, storage_scope)  # pylint: disable=no-member # type: ignore
+        return ptr(dtype, storage_scope)  # type: ignore[attr-defined] # pylint: disable=no-member
 
     @deprecated("T.Ptr(...)", "T.Ptr(...)")
     def __getitem__(self, keys):
