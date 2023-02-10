45a9bfd1df25b9bd0bb7d39446a7d7c974771c2b
Junru Shao <junrushao@apache.org>
2023-02-10 07:41:11 -0800

    [Unity] Relax TVMScript Printer (#13944)
    This PR introduces Relax as a dialect supported by the TVMScript
    Printer. Some caveats:
    - Needs to rebase to mainline before merging.
    - Some tests are skiped because some operators are not upstreamed to
      the unity branch yet.
    
    Co-authored-by: Tianqi Chen <tianqi.tchen@gmail.com>
    Co-authored-by: Yuchen Jin <yuchenj@cs.washington.edu>
    Co-authored-by: Steven S. Lyubomirsky <slyubomirsky@gmail.com>
    Co-authored-by: Yong Wu <yongcale@gmail.com>
    Co-authored-by: Prakalp Srivastava <prakalp@octoml.ai>
    Co-authored-by: Sunghyun Park <49998730+sunggg@users.noreply.github.com>
    Co-authored-by: Ruihang Lai <ruihangl@cs.cmu.edu>
    Co-authored-by: Hongyi Jin <3231950289@qq.com>
    Co-authored-by: Bohan Hou <32121147+spectrometerHBH@users.noreply.github.com>
    Co-authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>

---
+++ src/script/printer/relax/function.cc
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+#include "./utils.h"
+
+namespace tvm {
+namespace script {
+namespace printer {
+
+TVM_REGISTER_NODE_TYPE(RelaxFrameNode);
+
+TVM_STATIC_IR_FUNCTOR(IRDocsifier, vtable)
+    .set_dispatch<relax::Function>("", [](relax::Function n, ObjectPath n_p, IRDocsifier d) -> Doc {
+      std::unordered_set<const tir::VarNode*> func_vars;
+      With<RelaxFrame> f(d);
+      (*f)->AddDispatchToken(d, "relax");
+      (*f)->is_func = true;
+      (*f)->func_vars = &func_vars;
+      // Step 1. Print the return type
+      Optional<ExprDoc> ret_type = NullOpt;
+      if (const auto& func_sinfo = relax::MatchStructInfo<relax::FuncStructInfo>(n)) {
+        ret_type = d->AsDoc<ExprDoc>(func_sinfo.value()->ret,  //
+                                     n_p->Attr("struct_info_")->Attr("ret"));
+      }
+      // Step 2. Print params
+      Array<AssignDoc> params;
+      {
+        ObjectPath params_p = n_p->Attr("params");
+        for (int i = 0, l = n->params.size(); i < l; ++i) {
+          params.push_back(AssignDoc(
+              /*lhs=*/DefineVar(n->params[i], *f, d),
+              /*rhs=*/NullOpt, StructInfoAsAnn(n->params[i], params_p->ArrayIndex(i), d, NullOpt)));
+        }
+      }
+      // Step 3. Clean up func variables
+      (*f)->func_vars = nullptr;
+      // Step 4. Print attributes
+      if (n->attrs.defined() && !n->attrs->dict.empty()) {
+        (*f)->stmts.push_back(
+            ExprStmtDoc(Relax(d, "func_attr")  //
+                            ->Call({d->AsDoc<ExprDoc>(n->attrs, n_p->Attr("attrs"))})));
+      }
+      // Step 5. Print body
+      Array<StmtDoc> body =
+          PrintSeqExpr(Downcast<relax::SeqExpr>(n->body), n_p->Attr("body"), d, /*use_ret=*/true);
+      (*f)->stmts.insert((*f)->stmts.end(), body.begin(), body.end());
+      return HeaderWrapper(d, FunctionDoc(IdDoc(FindFunctionName(d, n).value_or("main")), params,
+                                          {Relax(d, "function")}, ret_type, (*f)->stmts));
+    });
+
+TVM_STATIC_IR_FUNCTOR(IRDocsifier, vtable)
+    .set_dispatch<relax::ExternFunc>(  //
+        "", [](relax::ExternFunc n, ObjectPath n_p, IRDocsifier d) -> Doc {
+          // TODO(@junrushao): print more information out of extern function.
+          return ExprStmtDoc(LiteralDoc::Str(n->global_symbol, n_p));
+        });
+
+TVM_SCRIPT_REPR(relax::FunctionNode, ReprPrintRelax);
+TVM_SCRIPT_REPR(relax::ExternFuncNode, ReprPrintRelax);
+
+}  // namespace printer
+}  // namespace script
+}  // namespace tvm
---
+++ src/script/printer/relax/utils.h
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+#ifndef TVM_SCRIPT_PRINTER_RELAX_UTILS_H_
+#define TVM_SCRIPT_PRINTER_RELAX_UTILS_H_
+
+#include <tvm/relax/struct_info.h>
+#include <tvm/relax/type.h>
+#include <tvm/script/printer/ir_docsifier.h>
+
+#include <string>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "../utils.h"
+
+namespace tvm {
+namespace script {
+namespace printer {
+
+class RelaxFrameNode : public FrameNode {
+ public:
+  bool is_func = false;
+  std::unordered_set<const tir::VarNode*>* func_vars = nullptr;
+
+  void VisitAttrs(AttrVisitor* v) {
+    FrameNode::VisitAttrs(v);
+    v->Visit("is_global_func", &is_func);
+    // `func_var_to_define` is not visited
+  }
+
+  static constexpr const char* _type_key = "script.printer.RelaxFrame";
+  TVM_DECLARE_FINAL_OBJECT_INFO(RelaxFrameNode, FrameNode);
+};
+
+class RelaxFrame : public Frame {
+ public:
+  explicit RelaxFrame(const IRDocsifier& d) {
+    ObjectPtr<RelaxFrameNode> n = make_object<RelaxFrameNode>();
+    n->stmts.clear();
+    n->d = d.get();
+    n->is_func = false;
+    n->func_vars = nullptr;
+    data_ = std::move(n);
+  }
+
+  TVM_DEFINE_MUTABLE_NOTNULLABLE_OBJECT_REF_METHODS(RelaxFrame, Frame, RelaxFrameNode);
+};
+
+/*! \brief Redirected method for the ReprPrinter */
+inline std::string ReprPrintRelax(const ObjectRef& obj, const PrinterConfig& cfg) {
+  IRDocsifier d(cfg);
+  With<RelaxFrame> f(d);
+  (*f)->AddDispatchToken(d, "relax");
+  return Docsify(obj, d, *f, cfg);
+}
+
+inline IdDoc DefineVar(const relax::Var& var, const Frame& frame, const IRDocsifier& d) {
+  return d->Define(var, frame, var->name_hint().empty() ? "v" : var->name_hint());
+}
+
+inline Optional<ExprDoc> StructInfoAsAnn(const relax::Var& v, const ObjectPath& v_p,
+                                         const IRDocsifier& d, const Optional<relax::Expr>& rhs) {
+  if (!v->struct_info_.defined()) {
+    return NullOpt;
+  }
+  if (const auto* call = rhs.as<relax::CallNode>()) {
+    static const Op& call_tir_op = Op::Get("relax.call_tir");
+    if (call->op.same_as(call_tir_op)) {
+      return NullOpt;
+    }
+  }
+  return d->AsDoc<ExprDoc>(v->struct_info_, v_p->Attr("struct_info_"));
+}
+
+Array<StmtDoc> PrintSeqExpr(const relax::SeqExpr& n, const ObjectPath& n_p, const IRDocsifier& d,
+                            bool use_ret);
+
+ExprDoc PrintShapeVar(const PrimExpr& e, const ObjectPath& e_p, const IRDocsifier& d);
+
+}  // namespace printer
+}  // namespace script
+}  // namespace tvm
+
+#endif  // TVM_SCRIPT_PRINTER_RELAX_UTILS_H_
