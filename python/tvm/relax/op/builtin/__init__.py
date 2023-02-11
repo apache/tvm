a9a21ed9882d9613c752311357d071649e0b9689
Yuchen Jin <yuchenj@cs.washington.edu>
2023-02-11 09:26:36 -0800

    [Unity] e2e Relax minimum build flow (#13961)
    This PR introduces the e2e Relax lowering flow (`relax.vm.build`). Tests for each pass in the flow are added.
    
    Co-Authored-by: Altan Haan <altanh@cs.washington.edu>
    Co-Authored-by: Andrew Liu <andrewlliu@gmail.com>
    Co-Authored-by: Hongyi Jin <3231950289@qq.com>
    Co-Authored-by: Jiawei Liu <jaway.liu@gmail.com>
    Co-Authored-by: Junru Shao <junrushao1994@gmail.com>
    Co-Authored-by: Prakalp Srivastava <prakalp@octoml.ai>
    Co-Authored-by: Ruihang Lai <ruihangl@cs.cmu.edu>
    Co-Authored-by: Siyuan Feng <Hzfengsy@sjtu.edu.cn>
    Co-Authored-by: Steven S. <Lyubomirsky slyubomirsky@octoml.ai>
    Co-Authored-by: Sunghyun Park <49998730+sunggg@users.noreply.github.com>
    Co-Authored-by: Tianqi Chen <tianqi.tchen@gmail.com>
    Co-Authored-by: Yong Wu <yongcale@gmail.com>
    Co-Authored-by: Ziheng Jiang <ziheng@apache.org>

--- python/tvm/relax/op/builtin/__init__.py
+++ python/tvm/relax/op/builtin/__init__.py
@@ -15,11 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 # pylint: disable=wildcard-import, redefined-builtin
-"""Relax core operators."""
+"""Relax builtin operators."""
 
-# Operators
-from .base import *
-from .binary import *
-from .manipulate import *
-from . import builtin
-from . import memory
+from .builtin import *
