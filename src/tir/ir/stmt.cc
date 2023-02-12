325161964b91664c943b7289a46951b4ef4a18e9
Junru Shao <junrushao@apache.org>
2023-02-12 06:42:57 -0800

    [TVMScript] Enable Safe Autocasting in BufferStore (#13960)
    Previously, when writing a BufferStore statement in TVMScript, dtype
    mismatch will not be detected but silently allowed.
    
    For example, in the snippet below:
    
    ```python
    buffer[indices] = RHS
    ```
    
    even if the elements of `buffer` is floating point numbers, while RHS
    is a handle, there is no mechanism to warn, fix, or error out.
    
    This PR introduces auto casting mechanism in TVMScript to ensure that
    the dtype of RHS matches LHS. When there is possible precision loss, it
    will emit a warning so that the users could adjust accordingly.

--- src/tir/ir/stmt.cc
+++ src/tir/ir/stmt.cc
@@ -507,6 +507,12 @@ BufferStore::BufferStore(Buffer buffer, PrimExpr value, Array<PrimExpr> indices,
       << "Cannot store value with " << value.dtype().lanes() << ", expected value with "
       << index_lanes * buffer_lanes << " (" << index_lanes << " index lanes * " << buffer_lanes
       << " buffer element lanes)";
+  if (buffer->dtype.with_lanes(buffer_lanes * index_lanes) != value.dtype()) {
+    LOG(FATAL) << "TypeError: dtype mismatch on BufferStore: "      //
+               << "buffer's dtype is `" << buffer->dtype            //
+               << "`, the lanes of indexing are: `" << index_lanes  //
+               << "`, but RHS's dtype is `" << value.dtype() << "`";
+  }
 
   ObjectPtr<BufferStoreNode> node = make_object<BufferStoreNode>();
   node->buffer = std::move(buffer);
