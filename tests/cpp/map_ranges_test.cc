db600e30fe4fd0613b20b4b129c25b4ce55d2d39
Sergey Shtin <sergey.shtin@deelvin.com>
2023-02-13 20:24:03 +0300

    [Hexagon] test for map ranges added.

---
+++ tests/cpp/map_ranges_test.cc
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <tvm/tir/index_map.h>
+#include <tvm/tir/op.h>
+#include <tvm/arith/analyzer.h>
+#include <tvm/ir/type_relation.h>
+
+namespace tvm {
+namespace tir {
+
+TEST(Tensor, CalcRanges) {
+  using namespace tvm;
+
+  IndexMapNode node;
+  arith::Analyzer local_analyzer;
+
+  tvm::tir::Var i0("i0", DataType::Int(64));
+  tvm::tir::Var i1("i1", DataType::Int(64));
+  tvm::tir::Var i2("i2", DataType::Int(64));
+  tvm::tir::Var i3("i3", DataType::Int(64));
+  node.initial_indices.push_back(i0);
+  node.initial_indices.push_back(i1);
+  node.initial_indices.push_back(i2);
+  node.initial_indices.push_back(i3);
+
+  Array<Range> ranges;
+  // The values were collected form the first layer of DeeplabV3 net.
+  ranges.push_back(Range(PrimExpr(0), PrimExpr(1)));
+  ranges.push_back(Range(PrimExpr(0), PrimExpr(1)));
+  ranges.push_back(Range(PrimExpr(0), PrimExpr(144)));
+  ranges.push_back(Range(PrimExpr(0), PrimExpr(48)));
+
+  // the result of MetaSheduler trace
+  node.final_indices.push_back(floordiv(i2, PrimExpr(12)));
+
+  auto i2_48_i3 = add(mul(i2,PrimExpr(48)),i3); // (i2 * 48 + i3)
+
+  node.final_indices.push_back((floordiv(floormod(i2_48_i3, PrimExpr(64)), PrimExpr(32))));
+  node.final_indices.push_back((floordiv(floormod(i2_48_i3, PrimExpr(3072)), PrimExpr(32))));
+
+  auto i2_16_i3 = add(mul(i2,PrimExpr(16)),i3); // (i2 * 48 + i3)
+  node.final_indices.push_back(floormod(i2_16_i3, PrimExpr(32)));
+  
+  auto result = node.MapRanges(ranges, &local_analyzer);
+
+  EXPECT_EQ(result.size(), ranges.size());
+
+  PrimExpr orig_sz(1);
+  PrimExpr total_sz(1);
+
+  for (size_t i = 0; i < ranges.size(); ++i) {
+    orig_sz *= ranges[i].as<RangeNode>()->extent;
+    total_sz *= result[i].as<RangeNode>()->extent;
+  }
+  auto sz1 = local_analyzer.Simplify(total_sz);
+  auto sz2 = local_analyzer.Simplify(orig_sz);
+
+  std::ostringstream os;
+  os << (sz2 == sz1);
+  ICHECK(os.str() == "True");
+}
+
+}  // namespace tir
+}  // namespace tvm
