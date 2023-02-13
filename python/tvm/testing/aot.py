e9e93c7dff7c183285a2d6bb02d45573ebba6eb4
Mehrdad Hessar <mhessar@octoml.ai>
2023-02-13 09:20:58 -0800

    reuse copy_crt_config_header

--- python/tvm/testing/aot.py
+++ python/tvm/testing/aot.py
@@ -723,11 +723,7 @@ def run_and_check(
 
         include_path = os.path.join(base_path, "include")
         os.mkdir(include_path)
-        crt_root = tvm.micro.get_microtvm_template_projects("crt")
-        shutil.copy2(
-            pathlib.Path(crt_root) / "crt_config" / "crt_config-template.h",
-            pathlib.Path(include_path) / "crt_config.h",
-        )
+        tvm.micro.copy_crt_config_header("crt", include_path)
 
         workspace_bytes = 0
         for compiled_model in models:
