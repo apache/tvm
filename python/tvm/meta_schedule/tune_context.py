87659ea3ea72fbe2e14e892f6a7a52981575f6cd
Sunghyun Park <49998730+sunggg@users.noreply.github.com>
2023-02-17 00:20:44 -0800

    [Unity][Pass][TuningAPI] Introduce TuningAPI and MetaSchedule pass (#14014)
    Add TuningAPI and MetaSchedule tuning pass

--- python/tvm/meta_schedule/tune_context.py
+++ python/tvm/meta_schedule/tune_context.py
@@ -24,7 +24,7 @@ from typing_extensions import Literal
 # isort: on
 
 from tvm import IRModule
-from tvm._ffi import register_object
+from tvm._ffi import register_object, register_func
 from tvm.runtime import Object
 from tvm.target import Target
 from tvm.tir import PrimFunc, Schedule
@@ -41,6 +41,7 @@ if TYPE_CHECKING:
     from .space_generator import SpaceGenerator
 
 
+@register_func("tvm.meta_schedule.normalize_mod")
 def _normalize_mod(mod: Union[PrimFunc, IRModule]) -> IRModule:
     """Normalize the input to an IRModule"""
     if isinstance(mod, PrimFunc):
