330fe8d17c5f526e38a44e3f52f96f3ba556be2e
Mehrdad Hessar <mhessar@octoml.ai>
2023-02-22 09:57:13 -0800

    [CRT] USE CMake for CRT standalone libraries (#14025)
    This PR converts Makefile for standalone CRT libraries to a CMake file which is usable in template projects. After this PR we no longer need to have custom CMake scripts to build CRT standalone libraries but instead just include CMakeLists.txt file that is copied in the CRT lib and link the libraries.
    Also changed apps/bundle_deploy/Makefile which was a result of this change.

--- apps/bundle_deploy/build_model.py
+++ apps/bundle_deploy/build_model.py
@@ -26,7 +26,6 @@ from tvm import runtime as tvm_runtime
 import logging
 from tvm.relay.backend import Runtime
 from tvm.contrib import cc as _cc
-from tvm.micro import copy_crt_config_header
 
 RUNTIMES = [
     (Runtime("crt", {"system-lib": True}), "{name}_c.{ext}"),
@@ -166,8 +165,3 @@ if __name__ == "__main__":
     else:
         build_module(opts)
         build_inputs(opts)
-
-    crt_config_output_path = pathlib.Path(__file__).parent.resolve() / "build" / "crt_config"
-    if not crt_config_output_path.exists():
-        crt_config_output_path.mkdir()
-    copy_crt_config_header("crt", crt_config_output_path)
