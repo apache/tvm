987ac35b66e685dba5f2f38bdfa633fbd9ee8c79
Yong Wu <yongcale@gmail.com>
2023-02-22 16:00:06 -0800

    [Unity] Relax Recursive function (#14092)
    This PR adds TVMScript local recursive function support. It also update lambda lifting pass. Removed CalledGlobalVars, it was not used anymore. It also updates well-form pass to allow un-defined vars for recursive call

--- src/relax/transform/lambda_lift.cc
+++ src/relax/transform/lambda_lift.cc
@@ -46,35 +46,72 @@ class LambdaLifter : public ExprMutator {
 
   using ExprMutator::VisitExpr_;
 
+  void VisitBinding_(const VarBindingNode* binding) final {
+    bool is_lambda = false;
+    if (binding->value->IsInstance<FunctionNode>()) {
+      is_lambda = true;
+      recur_vars_.push_back(binding->var);
+    }
+    Expr new_value = this->VisitExpr(binding->value);
+    if (new_value->struct_info_.defined() &&
+        !new_value->struct_info_.same_as(binding->var->struct_info_)) {
+      binding->var->struct_info_ = GetStructInfo(new_value);
+      binding->var->checked_type_ = new_value->checked_type_;
+    }
+    if (new_value.same_as(binding->value)) {
+      builder_->EmitNormalized(GetRef<VarBinding>(binding));
+    } else {
+      builder_->EmitNormalized(VarBinding(binding->var, new_value));
+    }
+    if (is_lambda) {
+      recur_vars_.pop_back();
+    }
+  }
+
   Expr VisitExpr_(const CallNode* call_node) final {
     auto call = Downcast<Call>(ExprMutator::VisitExpr_(call_node));
-    if (auto const* var = call_node->op.as<VarNode>()) {
-      bool has_closure = HasClosure(GetRef<Var>(var));
-      auto val = builder_->LookupBinding(GetRef<Var>(var));
+    if (const auto* var_node = call_node->op.as<VarNode>()) {
+      auto var = GetRef<Var>(var_node);
+      bool has_closure = HasClosure(var);
+      auto val = builder_->LookupBinding(var);
+      if (const auto* fsinfo_node = GetStructInfo(var).as<FuncStructInfoNode>()) {
+        auto fsinfo = GetRef<FuncStructInfo>(fsinfo_node);
+        if (!GetStructInfo(call).same_as(fsinfo)) {
+          call->struct_info_ = fsinfo->ret;
+          call->checked_type_ = GetStaticType(fsinfo->ret);
+        }
+      }
       // Call "relax.invoke_closure" to invoke closure
-      if (has_closure && val.as<CallNode>()) {
-        Var clo_arg = GetRef<Var>(var);
+      Var clo_arg = var;
+      if (has_closure && val->IsInstance<CallNode>()) {
         if (this->var_remap_.find(var->vid) != this->var_remap_.end()) {
           clo_arg = this->var_remap_.at(var->vid);
         }
         return Call(invoke_closure_op_, {clo_arg, Tuple(call_node->args)}, {},
                     {GetStructInfo(GetRef<Expr>(call_node))});
       }
-    }
-    if (auto global_var_node = call_node->op.as<GlobalVarNode>()) {
-      String rec_name = global_var_node->name_hint;
-      auto global_var = GetRef<GlobalVar>(global_var_node);
-      auto it = lambda_map_.find(global_var);
+      auto it = lambda_map_.find(var);
       if (it != lambda_map_.end()) {
         // flatten nested call, e.g. call(y)(x) -> call(x, y))
         Array<relay::Expr> new_args;
+        Array<StructInfo> params;
         for (const auto arg : call->args) {
           new_args.push_back(arg);
+          params.push_back(StructInfoFromType(arg->checked_type()));
         }
         if (const auto* nest_call = it->second.as<CallNode>()) {
+          // Update the StructInfo accordingly
           for (const auto arg : nest_call->args) {
             new_args.push_back(arg);
+            params.push_back(StructInfoFromType(arg->checked_type()));
           }
+          StructInfo new_func_sinfo;
+          if (const auto* fsinfo = GetStructInfo(nest_call->op).as<FuncStructInfoNode>()) {
+            auto func_sinfo = GetRef<FuncStructInfo>(fsinfo);
+            new_func_sinfo = FuncStructInfo(params, func_sinfo->ret);
+          }
+          nest_call->op->struct_info_ = new_func_sinfo;
+          nest_call->op->checked_type_ = GetStaticType(new_func_sinfo);
           return Call(nest_call->op, new_args, call_node->attrs, call_node->sinfo_args);
         }
         return Call(it->second, call->args, call_node->attrs, call_node->sinfo_args);
@@ -89,11 +126,19 @@ class LambdaLifter : public ExprMutator {
     // TODO(@yongwww): consider appending inner func name into the lifted func name
     String lift_func_name = "lifted_func_" + std::to_string(lift_func_num_++);
     auto global = GlobalVar(lift_func_name);
-    Array<Var> captured_vars = FreeVars(func);
-    recur_vars_ = CalledGlobalVars(func);
-    auto all_global_vars = AllGlobalVars(func);
+    Array<Var> free_vars = FreeVars(func);
+    Array<Var> captured_vars;
 
     Array<Var> typed_captured_vars;
+    bool recursive = false;
+    for (const auto& var : free_vars) {
+      if (!recur_vars_.empty() && var == recur_vars_.back()) {
+        recursive = true;
+      } else {
+        captured_vars.push_back(var);
+      }
+    }
+
     Map<Var, Expr> rebinding_map;
     for (auto free_var : captured_vars) {
       Var var = Var(free_var->name_hint(), GetStructInfo(free_var), free_var->span);
@@ -102,12 +147,14 @@ class LambdaLifter : public ExprMutator {
     }
 
     // recursive call
-    if (!recur_vars_.empty()) {
+    if (recursive) {
       if (!captured_vars.empty()) {
         Array<Expr> fvs;
         for (auto fv : captured_vars) {
           fvs.push_back(fv);
         }
+        // it is required by block_blocker, will be updated later
+        UpdateStructInfo(global, GetStructInfo(recur_vars_.back()));
         lambda_map_.emplace(recur_vars_.back(), Call(global, fvs));
       } else {
         if (recur_vars_.size() > 0) {
@@ -162,18 +209,17 @@ class LambdaLifter : public ExprMutator {
                              /*attrs=*/new_func->attrs,
                              /*span=*/func->span);
 
-      Array<Type> param_types;
       for (Var param : closure_params) {
         CHECK(param->checked_type_.defined())
             << "relax.Function requires params to contain checked_type_";
-        param_types.push_back(param->checked_type_);
       }
     }
 
     ICHECK(lifted_func.defined());
 
     // Add the lifted function to the module.
-    UpdateStructInfo(global, GetStructInfo(lifted_func));
+    global->struct_info_ = GetStructInfo(lifted_func);
+    global->checked_type_ = lifted_func->checked_type_;
     builder_->UpdateFunction(global, lifted_func);
 
     if (!is_closure) {
@@ -242,8 +288,8 @@ class LambdaLifter : public ExprMutator {
   }
 
  private:
-  std::unordered_map<GlobalVar, Expr, ObjectPtrHash, ObjectPtrEqual> lambda_map_;
-  Array<GlobalVar> recur_vars_;
+  std::unordered_map<Var, Expr, ObjectPtrHash, ObjectPtrEqual> lambda_map_;
+  Array<Var> recur_vars_;
   IRModule mod_;
   size_t lift_func_num_ = 0;
   /*! \brief Cache ops that would be used later to reduce lookup overhead. */
