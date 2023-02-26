e62169cc8afb5b9062a40072d6d44fc817408d77
Hongyi Jin <3231950289@qq.com>
2023-02-26 11:05:47 -0500

    [Unity] Add Global info (#14132)

--- python/tvm/ir/global_info.py
+++ python/tvm/ir/global_info.py
@@ -14,13 +14,29 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Package tvm.script.ir_builder.ir"""
-from .frame import IRModuleFrame
-from .ir import (
-    decl_function,
-    def_function,
-    ir_module,
-    module_attrs,
-    module_global_infos,
-    dummy_global_info,
-)
+"""Global Info."""
+import tvm
+from tvm.runtime.object import Object
+from . import _ffi_api
+
+
+class GlobalInfo(Object):
+    """Base node for all global info that can appear in the IR"""
+
+    def __eq__(self, other):
+        """Compare two struct info for structural equivalence."""
+        return tvm.ir.structural_equal(self, other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def same_as(self, other):
+        """Overload with structural equality."""
+        return super().__eq__(other)
+
+
+class DummyGlobalInfo(GlobalInfo):
+    def __init__(self) -> None:
+        self.__init_handle_by_constructor__(
+            _ffi_api.DummyGlobalInfo,
+        )
