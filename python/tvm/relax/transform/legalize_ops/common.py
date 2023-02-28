7a5d313068d5ef84671ace18ddb848ed2e2539fd
Xiyou Zhou <xiyou@octoml.ai>
2023-02-28 11:24:05 -0800

    [Unity][Legalize] Fix Scalar Constant Legalization (#14127)
    This PR fixes the issue of loss of data type during Legalization. Previously, if we use a constant scalar in operators like `multiply`, it will automatically be converted to a python data type variable, which may lose its original data type. For example, `float16` may become python `float` and be interpreted as `float32` later.
    
    This is now fixed by avoiding scalar value conversion. The conversion could be added back once we have better support for scalar prim value.
    
    Co-authored-by: Sunghyun Park <49998730+sunggg@users.noreply.github.com>
    Co-authored-by: Wuwei Lin <wuwei@apache.org>

--- python/tvm/relax/transform/legalize_ops/common.py
+++ python/tvm/relax/transform/legalize_ops/common.py
@@ -19,6 +19,7 @@ from typing import Callable, Optional, Union
 
 import tvm
 from tvm import te
+from tvm.tir import FloatImm, IntImm
 from ...block_builder import BlockBuilder
 from ...expr import Call, Expr, Constant
 
@@ -39,10 +40,16 @@ LegalizeFunc = Callable[[BlockBuilder, Call], Expr]
 ##################### Utilities #####################
 
 
-def _try_convert_to_scalar_const(expr: Expr) -> Union[Expr, bool, float, int]:
+def _try_convert_to_scalar_const(
+    expr: Expr, python_native: bool = False
+) -> Union[Expr, FloatImm, IntImm, bool, float, int]:
     """Check if the input Expr is a scalar constant.
-    If it is, return its plain value.
+    If it is, return its plain value with the same data type or in native python type.
     If it is not, return the input expr.
+
+    Note that if the python_native flag is True, the returned value will be in native python type,
+    this might cause loss of data type for example, a float16 constant will be converted to float32
+    and a int64 constant will be converted to int32.
 
     Parameters
     ----------
@@ -51,15 +58,24 @@ def _try_convert_to_scalar_const(expr: Expr) -> Union[Expr, bool, float, int]:
 
     Returns
     --–----
-    ret : Union[Expr, bool, float, int]
-        Return a Python native value (int/float/bool) if the given
-        expr is a scalar constant. Or return the input itself
-        if it is not.
+    ret : Union[Expr, FloatImm, IntImm, bool, float, int]
+        Return a FloatImm or IntImm if the given expr is a scalar integer or float constant, and the
+        python native flag is False. Or return the plain value of the constant in native python type
+        if the python native flag is True.
+        Or return the input itself if it is not a scalar constant.
     """
     if isinstance(expr, Constant) and expr.struct_info.ndim == 0:
-        return expr.data.numpy()[()].item()
-    else:
-        return expr
+        # get the value of the scalar constant
+        value = expr.data.numpy()[()].item()
+        dtype = expr.struct_info.dtype
+        if python_native:
+            return value
+        # preserve the data type of the constant
+        if dtype.startswith("float"):
+            return tvm.tir.FloatImm(dtype, value)
+        elif dtype.startswith("int") or dtype.startswith("uint") or dtype.startswith("bool"):
+            return tvm.tir.IntImm(dtype, value)
+    return expr
 
 
 def _call_topi_without_attr(te_func: TEFunc, primfunc_name: Optional[str] = None) -> LegalizeFunc:
