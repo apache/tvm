e0c7cf7fa9a3e0dedf7a821f83212065ab600a53
Xiyou Zhou <xiyou@octoml.ai>
2023-02-28 10:54:49 -0800

    Fix type annotation & docs.

--- python/tvm/relax/transform/legalize_ops/common.py
+++ python/tvm/relax/transform/legalize_ops/common.py
@@ -19,6 +19,7 @@ from typing import Callable, Optional, Union
 
 import tvm
 from tvm import te
+from tvm.tir import FloatImm, IntImm
 from ...block_builder import BlockBuilder
 from ...expr import Call, Expr, Constant
 
@@ -41,10 +42,14 @@ LegalizeFunc = Callable[[BlockBuilder, Call], Expr]
 
 def _try_convert_to_scalar_const(
     expr: Expr, python_native: bool = False
-) -> Union[Expr, bool, float, int]:
+) -> Union[Expr, FloatImm, IntImm, bool, float, int]:
     """Check if the input Expr is a scalar constant.
     If it is, return its plain value with the same data type or in native python type.
     If it is not, return the input expr.
+
+    Note that if the python_native flag is True, the returned value will be in native python type,
+    this might cause loss of data type for example, a float16 constant will be converted to float32
+    and a int64 constant will be converted to int32.
 
     Parameters
     ----------
