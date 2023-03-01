75ee3b65c3e178a04433a2c0e865697f0c0f4316
Steven Lyubomirsky <slyubomirsky@octoml.ai>
2023-02-28 21:52:30 -0500

    More trailing whitespace

--- include/tvm/relax/analysis.h
+++ include/tvm/relax/analysis.h
@@ -315,7 +315,7 @@ TVM_DLL tvm::Array<GlobalVar> AllGlobalVars(const Expr& expr);
  * among them. For example, if there are two functions A and B, they are
  * mutually recursive if A calls B and B calls A. Another case would be with
  * three functions A, B, and C, where A calls B, B calls C, and C calls A.
- * 
+ *
  * (Note that functions do not have to call each other to reference each other.
  * For example, if a function returns another function, that is still a reference
  * that could potentially be recursive, even without a call.)
