30dc2365e7173d3a8dba64ec3d6aa375e0537f69
Yaxing Cai <caiyaxing666@gmail.com>
2023-03-03 16:57:49 -0800

    [Unity][OP] Add an operator for fused multi head attention (#14150)
    * [Unity][OP] Add an operator for fused multi head attention
    
    This PR introduces the new relax operator `R.nn.attention` for fused multi head attention, and the support of fused multi head attention to relax cutlass BYOC. The input of the operator are query, key and value tensor, with `BSNH` layout, namely `[batch size, sequence length, number of heads, dimension of heads]`. And the output shares the same layout with all input tensor.
    
    * remove useless codes, remove attrs and add memoize
    
    * add more dispatches
    
    * nit and fix rebase
    
    * fix linter
    
    * add support for bias
    
    * fix lint
    
    * BNSS layout for bias
    
    * update doc
    
    * fix typo
    
    * support bias broadcast

--- python/tvm/relax/backend/patterns.py
+++ python/tvm/relax/backend/patterns.py
@@ -113,3 +113,30 @@ def make_matmul_pattern(
     out = is_op("relax.matmul")(lhs, rhs)
 
     return _with_bias_activation_pattern(out, args, with_bias, activation)
+
+
+def make_attention_pattern(with_bias: bool = False):
+    """
+    Create pattern for fused multi head attention.
+
+    Returns
+    -------
+    pattern: DFPattern
+        The resulting pattern describing a fused multi head attention.
+
+    args: Mapping[str, DFPattern]
+        The mapping from arg name to its pattern. It can be used to extract
+        arg expression from match result.
+    """
+    query = wildcard()
+    key = wildcard()
+    value = wildcard()
+    args = {"query": query, "key": key, "value": value}
+    if with_bias:
+        bias = wildcard()
+        args["bias"] = bias
+        out = is_op("relax.nn.attention_bias")(query, key, value, bias)
+    else:
+        out = is_op("relax.nn.attention")(query, key, value)
+
+    return out, args
