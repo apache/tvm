6f35b2e7beb60ba7beefdda301609651bec3218c
spectrometerHBH <spectrometerh@gmail.com>
2023-03-09 12:28:16 -0800

    dce

---
+++ src/relax/transform/dead_code_elimination.cc
@@ -0,0 +1,185 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ *
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file tvm/relax/transform/dead_code_elimination.cc
+ * \brief Dead code elimination pass.
+ * Currently it removes:
+ *   1. Unused local VarBindings in a DataflowBlock.
+ *      The used var set is set to empty at the beginning of each DataflowBlock.
+ *      We reverse scan the DataflowBlock, if a VarBinding
+ *        - bindings to a dataflowvar, or
+ *        - is used in the used var set
+ *      We keep it and add its var to the used var set. Otherwise, we remove it.
+ *   2. Unused Relax functions in the module.
+ *      We detect the call chain from the entry function, and remove all unused functions.
+ */
+
+#include <tvm/relax/expr.h>
+#include <tvm/relax/expr_functor.h>
+#include <tvm/relax/transform.h>
+
+#include "utils.h"
+
+namespace tvm {
+namespace relax {
+
+/**
+ * \brief Detects all the functions that can be possibly called by entry function.
+ */
+class CallTracer : ExprVisitor {
+ public:
+  explicit CallTracer(IRModule mod_) : mod_{mod_}, called_funcs_{}, visiting_{} {}
+
+  void VisitExpr_(const GlobalVarNode* op) final {
+    called_funcs_.insert(GetRef<GlobalVar>(op));
+    auto func = mod_->Lookup(op->name_hint);
+    if (const auto* function_node = func.as<FunctionNode>()) {
+      VisitExpr(GetRef<Function>(function_node));
+    }
+    // else: Don't visit PrimFuncs -- we don't need to collect any tir.Calls therein.
+  }
+
+  void VisitExpr_(const CallNode* call_node) final { ExprVisitor::VisitExpr_(call_node); }
+
+  void VisitExpr_(const FunctionNode* func_node) final {
+    auto func = GetRef<Function>(func_node);
+    if (visiting_.find(func) == visiting_.end()) {
+      visiting_.insert(func);
+      for (auto param : func_node->params) {
+        ExprVisitor::VisitExpr(param);
+      }
+      ExprVisitor::VisitExpr(func_node->body);
+    }
+  }
+
+  void Trace(std::string entry) {
+    called_funcs_.insert(mod_->GetGlobalVar(entry));
+    auto main_func = mod_->Lookup(entry);
+    VisitExpr(main_func);
+  }
+
+  bool check_if_called(GlobalVar gv) { return called_funcs_.count(gv) > 0; }
+
+ private:
+  IRModule mod_;
+
+  // Record the names of all encountered functions.
+  std::unordered_set<GlobalVar, ObjectPtrHash, ObjectPtrEqual> called_funcs_;
+
+  // Record the expressions that are being visited.
+  std::unordered_set<Expr, ObjectPtrHash, ObjectPtrEqual> visiting_;
+};
+
+IRModule RemoveUnusedFunctions(IRModule mod_, Array<runtime::String> entry_funcs) {
+  auto tracer = CallTracer(mod_);
+  for (auto entry : entry_funcs) {
+    tracer.Trace(entry);
+  }
+  auto existing_functions = mod_->functions;
+  for (auto f : existing_functions) {
+    // If a function has an external linkage type, we do not remove it.
+    // Otherwise, we check the function and remove it if it is not used anywhere.
+    if (f.second->GetLinkageType() == LinkageType::kInternal && !tracer.check_if_called(f.first)) {
+      mod_->Remove(f.first);
+    }
+  }
+  return mod_;
+}
+
+class DeadCodeEliminator : public ExprMutator {
+ private:
+  Expr VisitExpr_(const VarNode* op) final {
+    ICHECK(!used_vars_.empty());
+    used_vars_.back().insert(GetRef<Var>(op));
+    return GetRef<Expr>(op);
+  }
+
+  Expr VisitExpr_(const DataflowVarNode* op) final {
+    ICHECK(!used_vars_.empty());
+    used_vars_.back().insert(GetRef<Var>(op));
+    return GetRef<Expr>(op);
+  }
+
+  void VisitBinding_(const VarBindingNode* binding) { this->VisitExpr(binding->value); }
+
+  void VisitBinding_(const MatchCastNode* binding) {
+    this->VisitExpr(binding->value);
+    this->VisitAndCheckStructInfoFieldUnchanged(binding->struct_info);
+  }
+
+  BindingBlock VisitBindingBlock_(const DataflowBlockNode* block) final {
+    // reverse scan the data flow block to find the used vars
+    used_vars_.push_back({});
+
+    std::vector<Binding> new_bindings;
+    for (auto rit = block->bindings.rbegin(); rit != block->bindings.rend(); rit++) {
+      const Var& var = (*rit)->var;
+      // only keep the used bindings or non dataflow var bindings
+      if (used_vars_.back().count(var) || !var->IsInstance<DataflowVarNode>()) {
+        new_bindings.push_back(*rit);
+        // collect the used vars
+        this->VisitBinding((*rit));
+      }
+    }
+
+    used_vars_.pop_back();
+    // reverse the bindings
+    std::reverse(new_bindings.begin(), new_bindings.end());
+    if (new_bindings.size() == block->bindings.size()) {
+      return GetRef<BindingBlock>(block);
+    } else {
+      auto n = make_object<DataflowBlockNode>(*block);
+      n->bindings = std::move(new_bindings);
+      return BindingBlock(n);
+    }
+  }
+
+  BindingBlock VisitBindingBlock_(const BindingBlockNode* block) final {
+    return GetRef<BindingBlock>(block);
+  }
+
+  std::vector<std::unordered_set<Var, ObjectPtrHash, ObjectPtrEqual>> used_vars_{{}};
+};
+
+IRModule DeadCodeElimination(const IRModule& mod, Array<runtime::String> entry_functions) {
+  DeadCodeEliminator eliminator;
+  for (const auto& gv : mod->GetGlobalVars()) {
+    auto func = mod->Lookup(gv);
+    if (func->IsInstance<FunctionNode>()) {
+      mod->Update(gv, Downcast<Function>(eliminator.VisitExpr(func)));
+    }
+  }
+  return RemoveUnusedFunctions(mod, entry_functions);
+}
+
+namespace transform {
+
+Pass DeadCodeElimination(Array<runtime::String> entry_functions) {
+  runtime::TypedPackedFunc<IRModule(IRModule, PassContext)> pass_func =
+      [=](IRModule m, PassContext pc) { return relax::DeadCodeElimination(m, entry_functions); };
+  return CreateModulePass(pass_func, 1, "DeadCodeElimination", {});
+}
+
+TVM_REGISTER_GLOBAL("relax.transform.DeadCodeElimination").set_body_typed(DeadCodeElimination);
+
+}  // namespace transform
+}  // namespace relax
+}  // namespace tvm
--- tests/python/relax/test_transform_dead_code_elimination.py
+++ tests/python/relax/test_transform_dead_code_elimination.py
@@ -15,13 +15,138 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import pytest
 import tvm
-import tvm.script
 import tvm.testing
-from tvm import relax
-from tvm.script import relax as R
-from tvm.script import tir as T
+from tvm.relax.transform import DeadCodeElimination
+from tvm.script.parser import ir as I, relax as R, tir as T
+
+
+def verify(input, expected):
+    tvm.ir.assert_structural_equal(DeadCodeElimination()(input), expected)
+
+
+def test_simple():
+    @tvm.script.ir_module
+    class Input:
+        @R.function
+        def main(
+            x: R.Tensor((2, 3, 28, 28), dtype="float32"),
+            w: R.Tensor((4, 3, 3, 3), dtype="float32"),
+            bias: R.Tensor((26, 26), dtype="float32"),
+        ) -> R.Tensor((2, 4, 26, 26), dtype="float32"):
+            # block 0
+            with R.dataflow():
+                gv: R.Tensor((2, 28, 28, 3), dtype="float32") = R.permute_dims(x, axes=[0, 2, 3, 1])
+                gv1: R.Tensor((4, 3, 3, 3), dtype="float32") = R.permute_dims(w, axes=[0, 2, 3, 1])
+                gv2: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(
+                    gv,
+                    gv1,
+                    strides=[1, 1],
+                    padding=[0, 0, 0, 0],
+                    dilation=[1, 1],
+                    groups=1,
+                    data_layout="NHWC",
+                    kernel_layout="OHWI",
+                    out_layout="NHWC",
+                    out_dtype="float32",
+                )
+                gv21: R.Tensor((2, 4, 26, 26), dtype="float32") = R.permute_dims(
+                    gv2, axes=[0, 3, 1, 2]
+                )
+                gv22: R.Tensor((2, 4, 26, 26), dtype="float32") = R.add(gv21, bias)
+                R.output(gv2)
+            return gv2
+
+    @I.ir_module
+    class Expected:
+        @R.function
+        def main(
+            x: R.Tensor((2, 3, 28, 28), dtype="float32"),
+            w: R.Tensor((4, 3, 3, 3), dtype="float32"),
+            bias: R.Tensor((26, 26), dtype="float32"),
+        ) -> R.Tensor((2, 4, 26, 26), dtype="float32"):
+            with R.dataflow():
+                gv: R.Tensor((2, 28, 28, 3), dtype="float32") = R.permute_dims(x, axes=[0, 2, 3, 1])
+                gv1: R.Tensor((4, 3, 3, 3), dtype="float32") = R.permute_dims(w, axes=[0, 2, 3, 1])
+                gv2: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(
+                    gv,
+                    gv1,
+                    strides=[1, 1],
+                    padding=[0, 0, 0, 0],
+                    dilation=[1, 1],
+                    groups=1,
+                    data_layout="NHWC",
+                    kernel_layout="OHWI",
+                    out_layout="NHWC",
+                    out_dtype="float32",
+                )
+                R.output(gv2)
+            return gv2
+
+    verify(Input, Expected)
+
+
+def test_2block():
+    @tvm.script.ir_module
+    class Input:
+        @R.function
+        def main(
+            x: R.Tensor((2, 3, 28, 28), dtype="float32"),
+            w: R.Tensor((4, 3, 3, 3), dtype="float32"),
+            bias: R.Tensor((26, 26), dtype="float32"),
+        ) -> R.Tensor((2, 4, 26, 26), dtype="float16"):
+            # block 0
+            with R.dataflow():
+                gv: R.Tensor((2, 28, 28, 3), dtype="float32") = R.permute_dims(x, axes=[0, 2, 3, 1])
+                gv1: R.Tensor((4, 3, 3, 3), dtype="float32") = R.permute_dims(w, axes=[0, 2, 3, 1])
+                gv2: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(
+                    gv,
+                    gv1,
+                    strides=[1, 1],
+                    padding=[0, 0, 0, 0],
+                    dilation=[1, 1],
+                    groups=1,
+                    data_layout="NHWC",
+                    kernel_layout="OHWI",
+                    out_layout="NHWC",
+                    out_dtype="float32",
+                )
+                gv21: R.Tensor((2, 4, 26, 26), dtype="float32") = R.permute_dims(
+                    gv2, axes=[0, 3, 1, 2]
+                )
+                gv22: R.Tensor((2, 4, 26, 26), dtype="float32") = R.add(gv21, bias)
+                R.output(gv2)
+            gv3 = R.astype(gv2, dtype="float16")
+            return gv3
+
+    @I.ir_module
+    class Expected:
+        @R.function
+        def main(
+            x: R.Tensor((2, 3, 28, 28), dtype="float32"),
+            w: R.Tensor((4, 3, 3, 3), dtype="float32"),
+            bias: R.Tensor((26, 26), dtype="float32"),
+        ) -> R.Tensor((2, 4, 26, 26), dtype="float16"):
+            with R.dataflow():
+                gv: R.Tensor((2, 28, 28, 3), dtype="float32") = R.permute_dims(x, axes=[0, 2, 3, 1])
+                gv1: R.Tensor((4, 3, 3, 3), dtype="float32") = R.permute_dims(w, axes=[0, 2, 3, 1])
+                gv2: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(
+                    gv,
+                    gv1,
+                    strides=[1, 1],
+                    padding=[0, 0, 0, 0],
+                    dilation=[1, 1],
+                    groups=1,
+                    data_layout="NHWC",
+                    kernel_layout="OHWI",
+                    out_layout="NHWC",
+                    out_dtype="float32",
+                )
+                R.output(gv2)
+            gv3: R.Tensor((2, 26, 26, 4), dtype="float16") = R.astype(gv2, dtype="float16")
+            return gv3
+
+    verify(Input, Expected)
 
 
 def check_if_func_exists(mod, func_name):
@@ -57,7 +182,7 @@ def test_unused_relax_func():
 
     mod = InputModule
     assert mod
-    new_mod = relax.transform.RemoveUnusedFunctions()(mod)
+    new_mod = DeadCodeElimination()(mod)
     assert check_if_func_exists(new_mod, "main")
     assert check_if_func_exists(new_mod, "tir_add")
     assert not check_if_func_exists(new_mod, "unused_func")
@@ -93,7 +218,7 @@ def test_unused_relax_func_custom_entry_func():
     assert mod
 
     # Test entry function other than "main".
-    new_mod = relax.transform.RemoveUnusedFunctions(entry_functions=["foo"])(mod)
+    new_mod = DeadCodeElimination(entry_functions=["foo"])(mod)
     assert check_if_func_exists(new_mod, "foo")
     assert check_if_func_exists(new_mod, "tir_add")
     assert not check_if_func_exists(new_mod, "unused_func")
@@ -128,7 +253,7 @@ def test_unused_relax_func_symbolic_shape():
     mod = InputModule
     assert mod
 
-    new_mod = relax.transform.RemoveUnusedFunctions()(mod)
+    new_mod = DeadCodeElimination()(mod)
     assert check_if_func_exists(new_mod, "main")
     assert check_if_func_exists(new_mod, "tir_add")
     assert not check_if_func_exists(new_mod, "unused_func")
@@ -163,7 +288,7 @@ def test_unused_prim_func():
 
     mod = InputModule
     assert mod
-    new_mod = relax.transform.RemoveUnusedFunctions()(mod)
+    new_mod = DeadCodeElimination()(mod)
     assert check_if_func_exists(new_mod, "main")
     assert check_if_func_exists(new_mod, "relax_add")
     # RemoveUnusedFunction pass won't remove the function with global symbol for the external linkage.
@@ -200,7 +325,7 @@ def test_multiple_unused_funcs():
     mod = InputModule
     assert mod
 
-    new_mod = relax.transform.RemoveUnusedFunctions()(mod)
+    new_mod = DeadCodeElimination()(mod)
     assert check_if_func_exists(new_mod, "main")
     # RemoveUnusedFunction pass won't remove the function with global symbol for the external linkage.
     assert check_if_func_exists(new_mod, "unused_func1")
@@ -208,4 +333,4 @@ def test_multiple_unused_funcs():
 
 
 if __name__ == "__main__":
-    pytest.main([__file__])
+    tvm.testing.main()
