4a5396ab7ed9b0667e7ba577ba5ebe9aee2264e6
Zihao <expye@outlook.com>
2023-03-12 07:49:14 -0700

    fix

--- python/tvm/tir/op.py
+++ python/tvm/tir/op.py
@@ -646,7 +646,9 @@ def tvm_warp_shuffle_up(mask, value, offset, width, warp_size):
     call : PrimExpr
         The call expression.
     """
-    return call_intrin(value.dtype, "tir.tvm_warp_shuffle_up", mask, value, offset, width, warp_size)
+    return call_intrin(
+        value.dtype, "tir.tvm_warp_shuffle_up", mask, value, offset, width, warp_size
+    )
 
 
 def tvm_warp_shuffle_down(mask, value, offset, width, warp_size):
@@ -671,7 +673,9 @@ def tvm_warp_shuffle_down(mask, value, offset, width, warp_size):
     call : PrimExpr
         The call expression.
     """
-    return call_intrin(value.dtype, "tir.tvm_warp_shuffle_down", mask, value, offset, width, warp_size)
+    return call_intrin(
+        value.dtype, "tir.tvm_warp_shuffle_down", mask, value, offset, width, warp_size
+    )
 
 
 def tvm_warp_activemask():
@@ -682,7 +686,7 @@ def tvm_warp_activemask():
     call : PrimExpr
         The call expression.
     """
-    return call_intrin("int32", "tir.tvm_warp_activemask")
+    return call_intrin("uint32", "tir.tvm_warp_activemask")
 
 
 def type_annotation(dtype):
