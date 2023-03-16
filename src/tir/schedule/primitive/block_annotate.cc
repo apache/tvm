c2c0028fefd367e33296fbeff69c50e80372182c
Zihao <expye@outlook.com>
2023-03-16 08:22:34 -0700

    cpplint

--- src/tir/schedule/primitive/block_annotate.cc
+++ src/tir/schedule/primitive/block_annotate.cc
@@ -16,8 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-#include "../utils.h"
 #include <tvm/tir/expr.h>
+
+#include "../utils.h"
 
 namespace tvm {
 namespace tir {
