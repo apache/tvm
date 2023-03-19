63e16260e111f7cfdd5dc32c097348c014594675
ganler <jaway.liu@gmail.com>
2023-03-19 16:43:05 -0500

    trim #14262 dce

--- src/relax/transform/dead_code_elimination.cc
+++ src/relax/transform/dead_code_elimination.cc
@@ -21,15 +21,6 @@
 /*!
  * \file tvm/relax/transform/dead_code_elimination.cc
  * \brief Dead code elimination pass.
- * Currently it removes:
- *   1. Unused local VarBindings in a DataflowBlock.
- *      The used var set is set to empty at the beginning of each DataflowBlock.
- *      We reverse scan the DataflowBlock, if a VarBinding
- *        - bindings to a DataflowVar, or
- *        - is used in the used var set
- *      We keep it and add its var to the used var set. Otherwise, we remove it.
- *   2. Unused Relax functions in the module.
- *      We detect the call chain from the entry function, and remove all unused functions.
  */
 
 #include <tvm/relax/analysis.h>
@@ -104,61 +95,6 @@ IRModule RemoveUnusedFunctions(IRModule mod_, Array<runtime::String> entry_funcs
   }
   return mod_;
 }
-
-class DeadCodeEliminator : public ExprMutator {
- private:
-  Expr VisitExpr_(const VarNode* op) final {
-    ICHECK(!used_vars_.empty());
-    used_vars_.back().insert(GetRef<Var>(op));
-    return GetRef<Expr>(op);
-  }
-
-  Expr VisitExpr_(const DataflowVarNode* op) final {
-    ICHECK(!used_vars_.empty());
-    used_vars_.back().insert(GetRef<Var>(op));
-    return GetRef<Expr>(op);
-  }
-
-  void VisitBinding_(const VarBindingNode* binding) { this->VisitExpr(binding->value); }
-
-  void VisitBinding_(const MatchCastNode* binding) {
-    this->VisitExpr(binding->value);
-    this->VisitAndCheckStructInfoFieldUnchanged(binding->struct_info);
-  }
-
-  BindingBlock VisitBindingBlock_(const DataflowBlockNode* block) final {
-    // reverse scan the data flow block to find the used vars
-    used_vars_.push_back({});
-
-    std::vector<Binding> new_bindings;
-    for (auto rit = block->bindings.rbegin(); rit != block->bindings.rend(); rit++) {
-      const Var& var = (*rit)->var;
-      // only keep the used bindings or non dataflow var bindings
-      if (used_vars_.back().count(var) || !var->IsInstance<DataflowVarNode>()) {
-        new_bindings.push_back(*rit);
-        // collect the used vars
-        this->VisitBinding((*rit));
-      }
-    }
-
-    used_vars_.pop_back();
-    // reverse the bindings
-    std::reverse(new_bindings.begin(), new_bindings.end());
-    if (new_bindings.size() == block->bindings.size()) {
-      return GetRef<BindingBlock>(block);
-    } else {
-      auto n = make_object<DataflowBlockNode>(*block);
-      n->bindings = std::move(new_bindings);
-      return BindingBlock(n);
-    }
-  }
-
-  BindingBlock VisitBindingBlock_(const BindingBlockNode* block) final {
-    return GetRef<BindingBlock>(block);
-  }
-
-  std::vector<std::unordered_set<Var, ObjectPtrHash, ObjectPtrEqual>> used_vars_{{}};
-};
 
 IRModule DeadCodeElimination(const IRModule& mod, Array<runtime::String> entry_functions) {
   // S1: remove unused functions to reduce the number of functions to be analyzed.
