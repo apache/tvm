adde74a2ef88cbad1f1220dbe68d5a9b20c15309
Wuwei Lin <wuwei@apache.org>
2023-03-28 15:26:13 -0700

    [Unity] Handle extern func calls in static memory planning

--- tests/python/relax/test_transform_static_plan_block_memory.py
+++ tests/python/relax/test_transform_static_plan_block_memory.py
@@ -586,6 +586,48 @@ def test_call_func_other_than_primfunc():
     tvm.ir.assert_structural_equal(mod, Module)
 
 
+def test_call_packed_external_func():
+    @I.ir_module
+    class Module:
+        @R.function
+        def main(x: R.Tensor((2, 3), "float32")):
+            alloc: R.Tensor((2, 3), dtype="float32") = R.builtin.alloc_tensor(
+                R.shape([2, 3]), dtype="float32", runtime_device_index=0
+            )
+            _ = R.call_packed("extern_func", x, alloc, sinfo_args=[R.Tuple()])
+            y: R.Tensor((2, 3), dtype="float32") = alloc
+            alloc1: R.Tensor((2, 3), dtype="float32") = R.builtin.alloc_tensor(
+                R.shape([2, 3]), dtype="float32", runtime_device_index=0
+            )
+            _1 = R.call_packed("extern_func", y, alloc1, sinfo_args=[R.Tuple()])
+            z: R.Tensor((2, 3), dtype="float32") = alloc1
+            return z
+
+    @I.ir_module
+    class Expected:
+        @R.function
+        def main(x: R.Tensor((2, 3), dtype="float32")) -> R.Tensor((2, 3), dtype="float32"):
+            storage: R.Object = R.memory.alloc_storage(
+                R.shape([24]), R.prim_value(0), R.str("global"), R.dtype("float32")
+            )
+            alloc: R.Tensor((2, 3), dtype="float32") = R.memory.alloc_tensor(
+                storage, R.prim_value(0), R.shape([2, 3]), R.dtype("float32")
+            )
+            _: R.Tuple = R.call_packed("extern_func", x, alloc, sinfo_args=(R.Tuple(),))
+            y: R.Tensor((2, 3), dtype="float32") = alloc
+            alloc1: R.Tensor((2, 3), dtype="float32") = R.builtin.alloc_tensor(
+                R.shape([2, 3]), R.dtype("float32"), R.prim_value(0)
+            )
+            _1: R.Tuple = R.call_packed("extern_func", y, alloc1, sinfo_args=(R.Tuple(),))
+            _2: R.Tuple = R.memory.kill_tensor(alloc)
+            z: R.Tensor((2, 3), dtype="float32") = alloc1
+            _3: R.Tuple = R.memory.kill_storage(storage)
+            return z
+
+    mod = relax.transform.StaticPlanBlockMemory()(Module)
+    tvm.ir.assert_structural_equal(mod, Expected)
+
+
 def test_symbolic_shape():
     @tvm.script.ir_module
     class Module:
