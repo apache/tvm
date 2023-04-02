db2134775ced9c7c1cb248d8b15e74172efa9f40
Zihao <expye@outlook.com>
2023-04-02 02:13:03 -0700

    add failed tests

--- tests/python/unittest/test_tir_reorder_block_iter_var.py
+++ tests/python/unittest/test_tir_reorder_block_iter_var.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import pytest
 import tvm
 import tvm.testing
 from tvm import tir
@@ -24,9 +25,9 @@ from tvm.tir.schedule.testing import verify_trace_roundtrip
 
 @T.prim_func
 def matmul(
-    A: T.Buffer[(128, 128), "float32"],
-    B: T.Buffer[(128, 128), "float32"],
-    C: T.Buffer[(128, 128), "float32"],
+    A: T.Buffer((128, 128), "float32"),
+    B: T.Buffer((128, 128), "float32"),
+    C: T.Buffer((128, 128), "float32"),
 ) -> None:
     for i, j, k in T.grid(128, 128, 128):
         with T.block("C"):
@@ -38,9 +39,9 @@ def matmul(
 
 @T.prim_func
 def matmul_after_reorder_block_iter_var(
-    A: T.Buffer[(128, 128), "float32"],
-    B: T.Buffer[(128, 128), "float32"],
-    C: T.Buffer[(128, 128), "float32"],
+    A: T.Buffer((128, 128), "float32"),
+    B: T.Buffer((128, 128), "float32"),
+    C: T.Buffer((128, 128), "float32"),
 ):
     for i, j, k in T.grid(128, 128, 128):
         with T.block("C"):
@@ -60,5 +61,26 @@ def test_reorder_block_iter_var():
     verify_trace_roundtrip(sch=sch, mod=matmul)
 
 
+def test_reorder_block_iter_var_fail_not_full():
+    sch = tir.Schedule(matmul, debug_mask="all")
+    C = sch.get_block("C")
+    with pytest.raises(tvm.tir.ScheduleError):
+        sch.reorder_block_iter_var(C, [2, 1])
+
+
+def test_reorder_block_iter_var_fail_not_within_bound():
+    sch = tir.Schedule(matmul, debug_mask="all")
+    C = sch.get_block("C")
+    with pytest.raises(tvm.tir.ScheduleError):
+        sch.reorder_block_iter_var(C, [-1, 3, 2])
+
+
+def test_reorder_block_iter_var_fail_not_unique():
+    sch = tir.Schedule(matmul, debug_mask="all")
+    C = sch.get_block("C")
+    with pytest.raises(tvm.tir.ScheduleError):
+        sch.reorder_block_iter_var(C, [0, 0, 2])
+
+
 if __name__ == "__main__":
-    test_reorder_block_iter_var()
+    tvm.testing.main()
