43ed742df0d7bf56d43daa0d54dec44b36a9b6a1
Xiyou Zhou <xiyou@octoml.ai>
2023-04-04 14:15:20 -0700

    Add blank line.

--- python/tvm/meta_schedule/tune.py
+++ python/tvm/meta_schedule/tune.py
@@ -73,6 +73,7 @@ def tune_tasks(
     module_equality : Optional[str]
         A string to specify the module equality testing and hashing method.
         It must be one of the followings:
+
             - "structural": Use StructuralEqual/Hash
             - "ignore-ndarray": Same as "structural", but ignore ndarray raw data during equality
                 testing and hashing.
