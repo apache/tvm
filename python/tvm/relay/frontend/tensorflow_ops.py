778749629c420121fbb9e28e9b356871e95554e8
Qingchao Shen <qingchaoshen@outlook.com>
2023-04-11 12:48:13 +0800

    remove deed code

--- python/tvm/relay/frontend/tensorflow_ops.py
+++ python/tvm/relay/frontend/tensorflow_ops.py
@@ -464,8 +464,8 @@ def _conv(opname):
             if opname == "conv":
                 attr["kernel_layout"] = "HWIO" if attr["data_format"] == "NHWC" else "OIHW"
             elif opname == "conv_transpose":
-                # conv_transpose in TVM has weights be HWOI for NHWC and IOHW for NCHW
-                attr["kernel_layout"] = "HWOI" if attr["data_format"] == "NHWC" else "IOHW"
+                # conv_transpose in TVM has weights be IOHW, because the attr["data_format"] always be NCHW when opname='conv_transpose'.
+                attr["kernel_layout"] = "IOHW"
             else:
                 attr["kernel_layout"] = "HWOI" if attr["data_format"] == "NHWC" else "OIHW"
 
