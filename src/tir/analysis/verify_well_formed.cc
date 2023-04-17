b4632026ad0e11f4e540018a1d5431b08aa24c55
Eric Lunderberg <elunderberg@octoml.ai>
2023-04-17 10:50:37 -0500

    [TIR] Require Buffer definition before use
    Implemented as part of VerifyWellFormed.  Currently, fails after
    FlattenStorage.

--- src/tir/analysis/verify_well_formed.cc
+++ src/tir/analysis/verify_well_formed.cc
@@ -26,26 +26,25 @@
 #include <tvm/tir/stmt.h>
 #include <tvm/tir/stmt_functor.h>
 
+#include <sstream>
+
 #include "../ir/functor_common.h"
 #include "tvm/ir/module.h"
 
 namespace tvm {
 namespace tir {
 
-/*! \brief Verify all Expr inside the block does not contain:
- *    1. loop vars outside the current block.
- *    2. block vars of parent blocks.
- */
-class BlockVarAccessVerifier : public StmtExprVisitor {
+template <typename Derived>
+class BaseVerifier : public StmtExprVisitor {
  public:
   static bool Verify(const PrimFunc& func, bool assert_mode) {
-    BlockVarAccessVerifier verifier(assert_mode);
+    Derived verifier(func, assert_mode);
     verifier(func->body);
     return !verifier.has_error_;
   }
 
- private:
-  explicit BlockVarAccessVerifier(bool assert_mode) : assert_mode_(assert_mode) {}
+ protected:
+  explicit BaseVerifier(const PrimFunc&, bool assert_mode) : assert_mode_(assert_mode) {}
 
   void VisitStmt(const Stmt& stmt) final {
     if (!has_error_) {
@@ -58,6 +57,20 @@ class BlockVarAccessVerifier : public StmtExprVisitor {
       StmtExprVisitor::VisitExpr(expr);
     }
   }
+
+  /*! \brief Whether it's in assert mode. */
+  bool assert_mode_;
+  /*! \brief Whether there is error. */
+  bool has_error_{false};
+};
+
+/*! \brief Verify all Expr inside the block does not contain:
+ *    1. loop vars outside the current block.
+ *    2. block vars of parent blocks.
+ */
+class BlockVarAccessVerifier : public BaseVerifier<BlockVarAccessVerifier> {
+ private:
+  using BaseVerifier::BaseVerifier;
 
   void VisitExpr_(const VarNode* op) final {
     auto it = loop_vars_.find(op);
@@ -127,16 +140,151 @@ class BlockVarAccessVerifier : public StmtExprVisitor {
  private:
   /*! \brief The map from outside loop vars to its corresponding block level. */
   std::unordered_map<const VarNode*, size_t> loop_vars_;
-  /*! \brief Whether it's in assert mode. */
-  bool assert_mode_;
   /*! \brief Current nested block stack level. */
   std::vector<const BlockNode*> block_stack_;
-  /*! \brief Whether there is error. */
-  bool has_error_{false};
+};
+
+class UndefinedBufferAccessVerifier : public BaseVerifier<UndefinedBufferAccessVerifier> {
+ private:
+  using Parent = BaseVerifier<UndefinedBufferAccessVerifier>;
+  friend class BaseVerifier<UndefinedBufferAccessVerifier>;
+
+  UndefinedBufferAccessVerifier(const PrimFunc& func, bool assert_mode)
+      : BaseVerifier(func, assert_mode) {
+    for (const Var& param : func->params) {
+      if (auto opt = func->buffer_map.Get(param)) {
+        global_defs_.emplace_back(this, opt.value(), NullOpt);
+      }
+    }
+  }
+
+  // Buffer definition sites
+  void VisitStmt_(const BufferRealizeNode* op) final {
+    Context context(this, op->buffer, GetRef<Stmt>(op));
+    Parent::VisitStmt_(op);
+  }
+  void VisitStmt_(const DeclBufferNode* op) final {
+    Context context(this, op->buffer, GetRef<Stmt>(op));
+    Parent::VisitStmt_(op);
+  }
+  void VisitStmt_(const BlockNode* op) final {
+    std::vector<Context> context;
+    for (const auto& buf : op->alloc_buffers) {
+      context.emplace_back(this, buf, GetRef<Stmt>(op));
+    }
+    for (const auto& match : op->match_buffers) {
+      context.emplace_back(this, match->buffer, GetRef<Stmt>(op));
+    }
+    Parent::VisitStmt_(op);
+  }
+
+  // Buffer usage sites
+  void VisitExpr_(const BufferLoadNode* op) final {
+    AssertDefined(op->buffer, op);
+    Parent::VisitExpr_(op);
+  }
+  void VisitStmt_(const BufferStoreNode* op) final {
+    AssertDefined(op->buffer, op);
+    Parent::VisitStmt_(op);
+  }
+
+  // AttrStmt, which may be either usage or definition, depending on
+  // the attribute.
+  void VisitStmt_(const AttrStmtNode* op) final {
+    std::vector<Context> context;
+
+    if (op->attr_key == attr::buffer_bind_scope) {
+      Array<ObjectRef> arr = Downcast<Array<ObjectRef>>(op->node);
+      ICHECK_EQ(arr.size(), 2U);
+      Buffer source = Downcast<Buffer>(arr[0]);
+      Buffer target = Downcast<Buffer>(arr[1]);
+      AssertDefined(target, op);
+      context.emplace_back(this, source, GetRef<Stmt>(op));
+    } else if (auto node = op->node.as<Buffer>()) {
+      AssertDefined(node.value(), op);
+    }
+    Parent::VisitStmt_(op);
+  }
+
+  // A context manager for scoped buffer definitions
+  struct Context {
+    Context(UndefinedBufferAccessVerifier* self, const Buffer& buffer, Optional<Stmt> definition)
+        : self_(self), buffer_(buffer) {
+      if (auto it = self_->definition_site_.find(buffer_); it != self_->definition_site_.end()) {
+        Optional<Stmt> prev = (*it).second;
+        self_->has_error_ = true;
+        if (self_->assert_mode_) {
+          auto& fatal = LOG(FATAL);
+          fatal << "Buffer " << buffer << " was defined multiple times.  "
+                << "The first definition occurred ";
+          if (prev) {
+            fatal << " in the " << prev->GetTypeKey() << ", " << prev << ".  ";
+          } else {
+            fatal << " in the PrimFunc's buffer_map.  ";
+          }
+          fatal << "The second definition occurred ";
+          if (definition) {
+            fatal << " in the " << definition->GetTypeKey() << ", " << definition << ".";
+          } else {
+            fatal << " in the PrimFunc's buffer_map.";
+          }
+        }
+      }
+      self_->definition_site_.Set(buffer_, definition);
+    }
+    ~Context() {
+      if (self_) {
+        self_->definition_site_.erase(buffer_);
+      }
+    }
+    Context& operator=(const Context&) = delete;
+    Context(const Context&) = delete;
+    Context& operator=(Context&& other) {
+      swap(std::move(other));
+      return *this;
+    }
+    Context(Context&& other) { swap(std::move(other)); }
+
+    void swap(Context&& other) {
+      std::swap(self_, other.self_);
+      std::swap(buffer_, other.buffer_);
+    }
+
+    UndefinedBufferAccessVerifier* self_{nullptr};
+    Buffer buffer_;
+  };
+
+  void AssertDefined(const Buffer& buffer, const Object* usage) {
+    auto it = definition_site_.find(buffer);
+    if (it == definition_site_.end()) {
+      has_error_ = true;
+      if (assert_mode_) {
+        Array<Buffer> defined_bufs;
+        for (const auto& [buf, definition] : definition_site_) {
+          defined_bufs.push_back(buf);
+        }
+        LOG(FATAL) << "Buffer " << buffer << "@" << buffer.get() << " was accessed as part of "
+                   << GetRef<ObjectRef>(usage) << ", without a definition.  "
+                   << "At this location, buffers " << defined_bufs << " are defined";
+      }
+    }
+  }
+
+  // A lookup table for currently-defined buffers.  The
+  // `Optional<Stmt>` contains either the location at which the buffer
+  // is defined, or NullOpt if the buffer was defined in the
+  // `buffer_map`.
+  Map<Buffer, Optional<Stmt>> definition_site_;
+
+  // A container for buffer definitions in the `buffer_map`.
+  std::vector<Context> global_defs_;
 };
 
 bool VerifyWellFormed(const PrimFunc& func, bool assert_mode) {
   if (!BlockVarAccessVerifier::Verify(func, assert_mode)) {
+    return false;
+  }
+  if (!UndefinedBufferAccessVerifier::Verify(func, assert_mode)) {
     return false;
   }
   // TODO(Siyuan): add more checks here.
--- tests/python/unittest/test_tir_analysis_verify_well_formed.py
+++ tests/python/unittest/test_tir_analysis_verify_well_formed.py
@@ -14,9 +14,12 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import pytest
+
 import tvm
 import tvm.testing
 from tvm.script import tir as T
+from tvm.ir.instrument import pass_instrument
 
 
 def test_pass_simple():
@@ -52,6 +55,119 @@ def test_fail_use_out_loop_var():
                 B[vi, vj] = A[i, vj] * 2.0
 
     assert not tvm.tir.analysis.verify_well_formed(element_wise, assert_mode=False)
+
+
+def test_pass_buffer_usage():
+    @T.prim_func
+    def func(a: T.handle):
+        # Buffer declaration as part of buffer_map
+        A = T.match_buffer(a, 128, "float32")
+
+        # Buffer declaration as part of BlockNode
+        B = T.alloc_buffer(128, "float32")
+        for i in range(128):
+            B[i] = A[i] * 2.0
+
+        # Buffer declaration in a DeclBuffer node
+        c_data = T.allocate([128], "float32")
+        C = T.decl_buffer(128, "float32", data=c_data)
+        for i in range(128):
+            C[i] = B[i] * 2.0
+
+    assert tvm.tir.analysis.verify_well_formed(func)
+
+
+def test_fail_implicit_buffer_alias():
+    @T.prim_func
+    def func(A: T.Buffer([128, 128], "float32")):
+        # Aliased buffer usage without declaration.  The `T.Buffer` in
+        # TVMScript does not actually make any TIR node, and does not
+        # count as a TIR declaration.
+        Alias = T.Buffer(128 * 128, "float32", data=A.data)
+        T.evaluate(Alias[0])
+
+    assert not tvm.tir.analysis.verify_well_formed(func, assert_mode=False)
+
+
+def test_pass_explicit_buffer_alias():
+    @T.prim_func
+    def func(A: T.Buffer([128, 128], "float32")):
+        # Aliased buffer usage with declaration.
+        Alias = T.decl_buffer(128 * 128, "float32", data=A.data)
+        T.evaluate(Alias[0])
+
+    assert tvm.tir.analysis.verify_well_formed(func)
+
+
+def matmul():
+    @T.prim_func
+    def func(a: T.handle, b: T.handle, c: T.handle) -> None:
+        A = T.match_buffer(a, [128, 128])
+        B = T.match_buffer(b, [128, 128])
+        C = T.match_buffer(c, [128, 128])
+
+        for i, j, k in T.grid(128, 128, 128):
+            with T.block("update"):
+                vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+                with T.init():
+                    C[vi, vj] = T.float32(0)
+                C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vj, vk]
+
+    return func
+
+
+def launch_env_thread():
+    @T.prim_func
+    def func(inputs: T.Buffer((64, 2, 4), "float32")) -> None:
+        bx = T.launch_thread("blockIdx.x", 64)
+        for i, j in T.grid(2, 4):
+            T.evaluate(inputs[bx, i, j])
+
+    return func
+
+
+def copy_using_env_thread():
+    shape = (64, 2, 4)
+
+    @T.prim_func
+    def func(A: T.Buffer(shape), B: T.Buffer(shape)):
+        blocks, M, N = T.meta_var(shape)
+
+        bx = T.launch_thread("blockIdx.x", blocks)
+        for i, j in T.grid(M, N):
+            B[bx, i, j] = A[bx, i, j]
+
+    return func
+
+
+@pass_instrument
+class InstrumentWellFormed:
+    def run_after_pass(self, mod, info):
+        for func in mod.functions.values():
+            tvm.tir.analysis.verify_well_formed(func)
+
+
+@pytest.mark.parametrize(
+    "generator,target",
+    [
+        (matmul, "llvm"),
+        pytest.param(
+            launch_env_thread,
+            "cuda",
+            marks=tvm.testing.Feature("cuda").marks(support_required="compile-only"),
+        ),
+        pytest.param(
+            copy_using_env_thread,
+            "cuda",
+            marks=tvm.testing.Feature("cuda").marks(support_required="compile-only"),
+        ),
+    ],
+)
+def test_well_formed_all_lowering_steps(generator, target):
+    func = generator()
+
+    with tvm.transform.PassContext(instruments=[InstrumentWellFormed()]):
+        tvm.build(func, target=target)
 
 
 if __name__ == "__main__":
