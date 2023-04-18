fb8bc447b67e7ba312573ddf78a45afec5b82636
Yong Wu <yongcale@gmail.com>
2023-04-18 12:35:51 -0700

    Update with newly built images

--- tests/scripts/task_config_build_cpu.sh
+++ tests/scripts/task_config_build_cpu.sh
@@ -49,8 +49,7 @@ fi
 
 echo set\(USE_TENSORFLOW_PATH \"/tensorflow\"\) >> config.cmake
 echo set\(USE_FLATBUFFERS_PATH \"/flatbuffers\"\) >> config.cmake
-# echo set\(USE_ETHOSN /opt/arm/ethosn-driver\) >> config.cmake
-echo set\(USE_ETHOSN OFF) >> config.cmake # TODO (yongwww): rebuild the ethosn
+echo set\(USE_ETHOSN /opt/arm/ethosn-driver\) >> config.cmake
 echo set\(USE_ETHOSN_HW OFF\) >> config.cmake
 echo set\(USE_CMSISNN OFF\) >> config.cmake
 echo set\(USE_VITIS_AI ON\) >> config.cmake
