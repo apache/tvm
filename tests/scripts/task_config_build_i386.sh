434b0d43ab28c9b98e28df181b2504a170373aec
Yong Wu <yongcale@gmail.com>
2023-04-17 18:26:38 -0700

    Update test_config

--- tests/scripts/task_config_build_i386.sh
+++ tests/scripts/task_config_build_i386.sh
@@ -28,7 +28,7 @@ echo set\(USE_RPC ON\) >> config.cmake
 echo set\(USE_MICRO ON\) >> config.cmake
 echo set\(USE_MICRO_STANDALONE_RUNTIME ON\) >> config.cmake
 echo set\(USE_PROFILER ON\) >> config.cmake
-echo set\(USE_LLVM llvm-config-4.0\) >> config.cmake
+echo set\(USE_LLVM llvm-config-12\) >> config.cmake
 echo set\(CMAKE_CXX_FLAGS -Werror\) >> config.cmake
 echo set\(USE_VTA_FSIM ON\) >> config.cmake
 echo set\(USE_VTA_TSIM ON\) >> config.cmake
