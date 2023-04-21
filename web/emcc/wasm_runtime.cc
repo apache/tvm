51fa5763aa98ac7938cf8c6951c9908abce9364b
Tianqi Chen <tqchen@users.noreply.github.com>
2023-04-21 16:52:32 -0400

    [Unity][WebGPU] Move NDArrayCache Support to relax runtime (#14689)
    [WebGPU] Move NDArrayCache Support to relax runtime
    
    This PR moves NDArrayCache support to relax runtime so
    it can be used in other native settings.

--- web/emcc/wasm_runtime.cc
+++ web/emcc/wasm_runtime.cc
@@ -58,6 +58,7 @@
 #include "src/runtime/relax_vm/executable.cc"
 #include "src/runtime/relax_vm/lm_support.cc"
 #include "src/runtime/relax_vm/memory_manager.cc"
+#include "src/runtime/relax_vm/ndarray_cache_support.cc"
 #include "src/runtime/relax_vm/vm.cc"
 
 // --- Implementations of backend and wasm runtime API. ---
@@ -120,50 +121,6 @@ TVM_REGISTER_GLOBAL("testing.object_use_count").set_body([](TVMArgs args, TVMRet
   *ret = (obj.use_count() - 1);
 });
 
-/*!
- * A NDArray cache to store pre-loaded arrays in the system.
- */
-class NDArrayCache {
- public:
-  static NDArrayCache* Global() {
-    static NDArrayCache* inst = new NDArrayCache();
-    return inst;
-  }
-
-  static void Update(String name, NDArray arr, bool override) {
-    NDArrayCache* pool = Global();
-    if (!override) {
-      ICHECK_EQ(pool->pool_.count(name), 0) << "Name " << name << " already exists in the cache";
-    }
-    pool->pool_.Set(name, arr);
-  }
-
-  static Optional<NDArray> Get(String name) {
-    NDArrayCache* pool = Global();
-    auto it = pool->pool_.find(name);
-    if (it != pool->pool_.end()) {
-      return (*it).second;
-    } else {
-      return NullOpt;
-    }
-  }
-
-  static void Remove(String name) {
-    NDArrayCache* pool = Global();
-    pool->pool_.erase(name);
-  }
-
-  static void Clear() { Global()->pool_.clear(); }
-
- private:
-  Map<String, NDArray> pool_;
-};
-
-TVM_REGISTER_GLOBAL("tvmjs.ndarray_cache.get").set_body_typed(NDArrayCache::Get);
-TVM_REGISTER_GLOBAL("tvmjs.ndarray_cache.update").set_body_typed(NDArrayCache::Update);
-TVM_REGISTER_GLOBAL("tvmjs.ndarray_cache.remove").set_body_typed(NDArrayCache::Remove);
-TVM_REGISTER_GLOBAL("tvmjs.ndarray_cache.clear").set_body_typed(NDArrayCache::Clear);
-
 void ArrayDecodeStorage(NDArray cpu_arr, std::string bytes, std::string format) {
   if (format == "f32-to-bf16") {
     std::vector<uint16_t> buffer(bytes.length() / 2);
@@ -186,40 +143,5 @@ void ArrayDecodeStorage(NDArray cpu_arr, std::string bytes, std::string format) 
 }
 
 TVM_REGISTER_GLOBAL("tvmjs.array.decode_storage").set_body_typed(ArrayDecodeStorage);
-
-class ParamModuleNode : public runtime::ModuleNode {
- public:
-  const char* type_key() const final { return "param_module"; }
-
-  PackedFunc GetFunction(const std::string& name, const ObjectPtr<Object>& sptr_to_self) final {
-    if (name == "get_params") {
-      auto params = params_;
-      return PackedFunc([params](TVMArgs args, TVMRetValue* rv) { *rv = params; });
-    } else {
-      return PackedFunc();
-    }
-  }
-
-  static Module Create(std::string prefix, int num_params) {
-    Array<NDArray> params;
-    for (int i = 0; i < num_params; ++i) {
-      std::string name = prefix + "_" + std::to_string(i);
-      auto opt = NDArrayCache::Get(name);
-      if (opt) {
-        params.push_back(opt.value());
-      } else {
-        LOG(FATAL) << "Cannot find " << name << " in cache";
-      }
-    }
-    auto n = make_object<ParamModuleNode>();
-    n->params_ = params;
-    return Module(n);
-  }
-
- private:
-  Array<NDArray> params_;
-};
-
-TVM_REGISTER_GLOBAL("tvmjs.param_module_from_cache").set_body_typed(ParamModuleNode::Create);
 }  // namespace runtime
 }  // namespace tvm
