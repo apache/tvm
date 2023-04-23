19e82ecb3a539a42a4726394720e46a4cd3532e5
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-04-23 00:43:55 -0400

    [Unity] MetaScheduleApplyDatabase using workload from records (#14702)
    This PR fixes an issue in MetaScheduleApplyDatabase, which uses the
    IRModule being queried as the base module of TIR Schedule. This will
    cause the inconsistency between the IRModule being scheduled and the
    trace of the record. For example, the trace of the record may have
    `get-block` instructions which tries to get block from a given name,
    which exists in the IRModule of the record while does not exist in the
    IRModule being queried (which is right the IRModule being scheduled).
    
    Therefore, this PR adds a case discussion. When anchor-op equality is
    not applied, we create the TIR schedule from the IRModule of the record,
    so that we can prevent the issue above from happening.
    
    This PR adds a unit test that can demonstrate the issue.

--- src/relax/transform/meta_schedule.cc
+++ src/relax/transform/meta_schedule.cc
@@ -126,15 +126,19 @@ Pass MetaScheduleApplyDatabase(Optional<String> work_dir, bool enable_warning = 
         if (Optional<meta_schedule::TuningRecord> opt_record =
                 database->QueryTuningRecord(tir_mod, target, gv->name_hint)) {
           meta_schedule::TuningRecord record = opt_record.value();
-          tir::Schedule sch =
-              tir::Schedule::Traced(tir_mod, /*seed=*/-1, /*debug_mask=*/0,
-                                    /*error_render_level=*/tir::ScheduleErrorRenderLevel::kDetail);
+          tir::Schedule sch{nullptr};
           if (!mod_eq_structural->Equal(tir_mod, record->workload->mod)) {
             // When the database lookup succeeds while structural equality check fails,
             // it implies that the anchor block based equality has been used during tuning.
             // The trace in the record cannot directly be applied to this query module.
+            sch = tir::Schedule::Traced(
+                tir_mod, /*seed=*/-1, /*debug_mask=*/0,
+                /*error_render_level=*/tir::ScheduleErrorRenderLevel::kDetail);
             meta_schedule::ScheduleUsingAnchorTrace(sch, record->trace, target);
           } else {
+            sch = tir::Schedule::Traced(
+                record->workload->mod, /*seed=*/-1, /*debug_mask=*/0,
+                /*error_render_level=*/tir::ScheduleErrorRenderLevel::kDetail);
             record->trace->ApplyToSchedule(sch, /*remove_postproc=*/false);
           }
           IRModule new_mod = sch->mod();
