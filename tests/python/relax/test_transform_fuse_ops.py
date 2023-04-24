f26eedbc716bea53ef8a8073b37df23ce8563d84
Steven Lyubomirsky <slyubomirsky@octoml.ai>
2023-04-24 17:15:21 -0400

    Update transform_fuse_ops test

--- tests/python/relax/test_transform_fuse_ops.py
+++ tests/python/relax/test_transform_fuse_ops.py
@@ -1395,13 +1395,13 @@ def test_skipping_primvalue():
         @R.function
         def main(inp: R.Tensor((2, 2), dtype="float32")) -> R.Tensor((2, 2), dtype="float32"):
             with R.dataflow():
-                lv = R.call_packed(
+                lv = R.call_pure_packed(
                     "my_func1", inp, R.prim_value(0), sinfo_args=[R.Tensor((2, 2), dtype="float32")]
                 )
-                lv1 = R.call_packed(
+                lv1 = R.call_pure_packed(
                     "my_func2", lv, R.str("str"), sinfo_args=[R.Tensor((2, 2), dtype="float32")]
                 )
-                gv = R.call_packed(
+                gv = R.call_pure_packed(
                     "my_func3",
                     lv1,
                     R.dtype("float32"),
