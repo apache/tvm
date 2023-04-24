c601f5368b2de4f397f518290eec7ee5fdc461cf
Qingchao Shen <qingchaoshen@outlook.com>
2023-04-24 13:40:06 +0800

    add CI test

--- tests/python/frontend/keras/test_forward.py
+++ tests/python/frontend/keras/test_forward.py
@@ -20,6 +20,8 @@ import tvm
 from tvm import relay
 from tvm.contrib import graph_executor
 import tvm.testing
+from packaging import version as package_version
+import pytest
 
 try:
     import tensorflow.compat.v1 as tf
@@ -209,6 +211,13 @@ class TestKeras:
             keras_model = keras_mod.models.Model(data, x)
             verify_keras_frontend(keras_model)
             verify_keras_frontend(keras_model, need_transpose=False, layout="NHWC")
+        # test invalid attribute alpha=None for LeakyReLU    
+        if package_version.parse(keras_mod.__version__.split('-tf')[0]) <= package_version.parse("2.3.1"):
+            data = keras_mod.layers.Input(shape=(2, 3, 4))
+            x = keras_mod.layers.LeakyReLU(alpha=None)(data)
+            keras_model = keras_mod.models.Model(data, x)
+            with pytest.raises(tvm.error.OpAttributeInvalid):
+                verify_keras_frontend(keras_model)        
 
     def test_forward_dense(self, keras_mod):
         """test_forward_dense"""
