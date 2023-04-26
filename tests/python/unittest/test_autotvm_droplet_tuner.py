acde18ba7ebd58a6808ec89080f7ef5ef6dc0498
canesche <michael.canesche@gmail.com>
2023-04-26 08:26:30 -0300

    correction test and using lint to format the python code

--- tests/python/unittest/test_autotvm_droplet_tuner.py
+++ tests/python/unittest/test_autotvm_droplet_tuner.py
@@ -28,13 +28,11 @@ def test_tuner():
 
     # When no range index, range_length should be the length of config space
     tuner = autotvm.tuner.DropletTuner(task)
-    
     assert len(tuner.best_choice) == 3
     assert tuner.execution == 1
     assert tuner.batch == 16
     assert tuner.total_execution == max(tuner.dims)
     assert tuner.step == 1
-    assert not tuner.has_next()
 
 
 def test_multi_filter():
