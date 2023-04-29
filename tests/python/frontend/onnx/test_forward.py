8677614cc6c5e8e3f6bf952df171e53c49103ed3
Qingchao Shen <qingchaoshen@outlook.com>
2023-04-29 09:16:55 +0800

    modify test case

--- tests/python/frontend/onnx/test_forward.py
+++ tests/python/frontend/onnx/test_forward.py
@@ -3944,6 +3944,7 @@ def test_lppool(target, dev):
         x_shape=[1, 3, 32, 32],
         kernel_shape=[2, 2],
         p=4,
+        strides=None,
         pads=None,
         out_shape=[1, 3, 32, 32],
         auto_pad="SAME_LOWER",
