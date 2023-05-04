ff237faacbd8de7c61f346dff1ce4fb6a13e6856
Yong Wu <yongcale@gmail.com>
2023-05-04 08:55:20 -0700

    [Unity] Use the upgraded images

--- ci/jenkins/unity_jenkinsfile.groovy
+++ ci/jenkins/unity_jenkinsfile.groovy
@@ -30,14 +30,14 @@
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 // NOTE: these lines are scanned by docker/dev_common.sh. Please update the regex as needed. -->
-ci_lint = 'tlcpack/ci_lint:20230322-060120-46fb2ff35'
-ci_gpu = 'tlcpackstaging/ci_gpu:20230412-201102-742c5eec7'
-ci_cpu = 'tlcpackstaging/ci_cpu:20230412-201102-742c5eec7'
+ci_lint = 'tlcpackstaging/ci_lint:20230504-142417-4d37a0a0'
+ci_gpu = 'tlcpackstaging/ci_gpu:20230504-142417-4d37a0a0'
+ci_cpu = 'tlcpackstaging/ci_cpu:20230504-142417-4d37a0a0'
 ci_wasm = 'tlcpack/ci-wasm:v0.72'
 ci_i386 = 'tlcpack/ci-i386:v0.75'
 ci_qemu = 'tlcpack/ci-qemu:v0.11'
 ci_arm = 'tlcpack/ci-arm:v0.08'
-ci_hexagon = 'tlcpack/ci-hexagon:20221025-182121-e41d0ed6e'
+ci_hexagon = 'tlcpackstaging/ci_hexagon:20230504-142417-4d37a0a0'
 // <--- End of regex-scanned config.
 
 // Parameters to allow overriding (in Jenkins UI), the images
