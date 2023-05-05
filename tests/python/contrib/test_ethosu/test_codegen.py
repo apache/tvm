[microNPU][ETHOSU] Fix Softmax quantization parameters
Fix zero point and scale values for operations according to the values in Vela, the test is updated to check case with different input and output zero point.

tests/python/contrib/test_ethosu/test_codegen.py
@@ -330,7 +330,7 @@ def test_ethosu_softmax(
     def softmax(x):
         return tf.nn.softmax(x)
 
-    infra.compare_tvm_with_tflite(softmax, [ifm_shape], accel_type)
+    infra.compare_tvm_with_tflite(softmax, [ifm_shape], accel_type, ranges=[(-1, 1)])
 
 
 @pytest.mark.parametrize("accel_type", ACCEL_TYPES)