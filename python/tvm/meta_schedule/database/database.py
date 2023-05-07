01324ef97c22cb63fb7b2885e9cd2858e991779b
Junru Shao <junrushao@apache.org>
2023-05-06 19:23:39 -0700

    [MetaSchedule] Add an API to dump a pruned database (#14783)

--- python/tvm/meta_schedule/database/database.py
+++ python/tvm/meta_schedule/database/database.py
@@ -313,6 +313,18 @@ class Database(Object):
         """
         return _ffi_api.DatabaseQueryIRModule(self, mod, target, workload_name)  # type: ignore # pylint: disable=no-member
 
+    def dump_pruned(self, destination: "Database") -> None:
+        """Dump the pruned database to files of JSONDatabase format.
+
+        Parameters
+        ----------
+        destination : Database
+            The destination database to be dumped to.
+        """
+        return _ffi_api.DatabaseDumpPruned(  # type: ignore # pylint: disable=no-member
+            self, destination
+        )
+
     def query(
         self,
         mod: IRModule,
