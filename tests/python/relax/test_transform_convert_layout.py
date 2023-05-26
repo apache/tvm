8e8a0ac361dd7632dcb9234fb8c67ae471ae2112
Masahiro Masuda <masahi129@gmail.com>
2023-05-26 13:26:57 +0900

    black

--- tests/python/relax/test_transform_convert_layout.py
+++ tests/python/relax/test_transform_convert_layout.py
@@ -1406,8 +1406,7 @@ def test_binary_ewise_scalar():
     class Input:
         @R.function
         def main(
-            x: R.Tensor((2, 3, 28, 28), "float32"),
-            w: R.Tensor((4, 3, 3, 3), "float32")
+            x: R.Tensor((2, 3, 28, 28), "float32"), w: R.Tensor((4, 3, 3, 3), "float32")
         ) -> R.Tensor(None, "float32", ndim=4):
             with R.dataflow():
                 gv: R.Tensor((2, 4, 26, 26), "float32") = R.nn.conv2d(x, w, out_dtype="float32")
@@ -1418,13 +1417,28 @@ def test_binary_ewise_scalar():
     @I.ir_module
     class Expected:
         @R.function
-        def main(x: R.Tensor((2, 3, 28, 28), dtype="float32"), w: R.Tensor((4, 3, 3, 3), dtype="float32")) -> R.Tensor((2, 4, 26, 26), dtype="float32"):
+        def main(
+            x: R.Tensor((2, 3, 28, 28), dtype="float32"), w: R.Tensor((4, 3, 3, 3), dtype="float32")
+        ) -> R.Tensor((2, 4, 26, 26), dtype="float32"):
             with R.dataflow():
                 lv: R.Tensor((2, 28, 28, 3), dtype="float32") = R.permute_dims(x, axes=[0, 2, 3, 1])
                 lv1: R.Tensor((4, 3, 3, 3), dtype="float32") = R.permute_dims(w, axes=[0, 2, 3, 1])
-                gv: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(lv, lv1, strides=[1, 1], padding=[0, 0, 0, 0], dilation=[1, 1], groups=1, data_layout="NHWC", kernel_layout="OHWI", out_layout="NHWC", out_dtype="float32")
+                gv: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(
+                    lv,
+                    lv1,
+                    strides=[1, 1],
+                    padding=[0, 0, 0, 0],
+                    dilation=[1, 1],
+                    groups=1,
+                    data_layout="NHWC",
+                    kernel_layout="OHWI",
+                    out_layout="NHWC",
+                    out_dtype="float32",
+                )
                 lv2: R.Tensor((2, 26, 26, 4), dtype="float32") = R.add(gv, R.const(1, "float32"))
-                gv2: R.Tensor((2, 4, 26, 26), dtype="float32") = R.permute_dims(lv2, axes=[0, 3, 1, 2])
+                gv2: R.Tensor((2, 4, 26, 26), dtype="float32") = R.permute_dims(
+                    lv2, axes=[0, 3, 1, 2]
+                )
                 R.output(gv2)
             return gv2
 
