fix the wrong calculation logic of cropping2d
The implementation of cropping2D is wrong. This pr fix it.

python/tvm/relay/frontend/keras.py
@@ -816,8 +816,8 @@ def _convert_cropping(
     int32_max = np.iinfo(np.int32).max
     return _op.strided_slice(
         inexpr,
-        begin=[0, 0, crop_t, crop_l],
-        end=[int32_max, int32_max, in_h - crop_b, in_w - crop_r],
+        begin=[0, crop_t, crop_l, 0],
+        end=[int32_max, in_h - crop_b, in_w - crop_r, int32_max],
     )
 
 