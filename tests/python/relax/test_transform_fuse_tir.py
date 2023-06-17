c68a00a37d5d483b54858fcd79f87472e0b1c147
masahi <masahi129@gmail.com>
2023-06-17 16:41:24 +0900

    [Unity][FuseTIR] Flatten and add tuple fields to parameters / arguments only when they are used (#15113)
    * wip
    
    * wip
    
    * works
    
    * fix
    
    * fixed
    
    * comment
    
    * more comment
    
    * adding test

--- tests/python/relax/test_transform_fuse_tir.py
+++ tests/python/relax/test_transform_fuse_tir.py
@@ -1079,5 +1079,124 @@ def test_tir_expression_in_shape():
     _check(Module, Expected)
 
 
+def test_tuple_input_unused_field():
+    @I.ir_module
+    class Module:
+        @T.prim_func
+        def reshape(
+            A: T.Buffer((T.int64(4), T.int64(8), T.int64(2048)), "float32"),
+            T_reshape: T.Buffer((T.int64(4), T.int64(8), T.int64(32), T.int64(64)), "float32"),
+        ):
+            T.func_attr({"op_pattern": 2, "tir.noalias": T.bool(True)})
+            # with T.block("root"):
+            for ax0, ax1, ax2, ax3 in T.grid(T.int64(4), T.int64(8), T.int64(32), T.int64(64)):
+                with T.block("T_reshape"):
+                    v_ax0, v_ax1, v_ax2, v_ax3 = T.axis.remap("SSSS", [ax0, ax1, ax2, ax3])
+                    T.reads(
+                        A[
+                            (
+                                ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1)
+                                // T.int64(8)
+                                + v_ax0
+                            )
+                            % T.int64(4),
+                            ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1) % T.int64(8),
+                            (v_ax2 * T.int64(64) + v_ax3) % T.int64(2048),
+                        ]
+                    )
+                    T.writes(T_reshape[v_ax0, v_ax1, v_ax2, v_ax3])
+                    T_reshape[v_ax0, v_ax1, v_ax2, v_ax3] = A[
+                        (
+                            ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1) // T.int64(8)
+                            + v_ax0
+                        )
+                        % T.int64(4),
+                        ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1) % T.int64(8),
+                        (v_ax2 * T.int64(64) + v_ax3) % T.int64(2048),
+                    ]
+
+        @R.function
+        def fused_reshape(
+            lv: R.Tuple(
+                R.Tensor((4, 8, 2048), dtype="float32"), R.Tensor((4, 8, 2048), dtype="float32")
+            )
+        ) -> R.Tensor((4, 8, 32, 64), dtype="float32"):
+            R.func_attr({"Primitive": 1})
+            cls = Module
+            with R.dataflow():
+                lv1: R.Tensor((4, 8, 2048), dtype="float32") = lv[0]
+                gv = R.call_tir(
+                    cls.reshape, (lv1,), out_sinfo=R.Tensor((4, 8, 32, 64), dtype="float32")
+                )
+                R.output(gv)
+            return gv
+
+        @R.function
+        def main(
+            tup: R.Tuple(
+                R.Tensor((4, 8, 2048), dtype="float32"), R.Tensor((4, 8, 2048), dtype="float32")
+            )
+        ) -> R.Tensor((4, 8, 32, 64), dtype="float32"):
+            cls = Module
+            with R.dataflow():
+                lv_1: R.Tensor((4, 8, 32, 64), dtype="float32") = cls.fused_reshape(tup)
+                R.output(lv_1)
+            return lv_1
+
+    @I.ir_module
+    class Expected:
+        @T.prim_func
+        def fused_reshape(
+            lv_0: T.Buffer((T.int64(4), T.int64(8), T.int64(2048)), "float32"),
+            T_reshape_handle_intermediate: T.Buffer(
+                (T.int64(4), T.int64(8), T.int64(32), T.int64(64)), "float32"
+            ),
+        ):
+            T.func_attr({"tir.noalias": T.bool(True)})
+            # with T.block("root"):
+            for ax0, ax1, ax2, ax3 in T.grid(T.int64(4), T.int64(8), T.int64(32), T.int64(64)):
+                with T.block("T_reshape"):
+                    v_ax0, v_ax1, v_ax2, v_ax3 = T.axis.remap("SSSS", [ax0, ax1, ax2, ax3])
+                    T.reads(
+                        lv_0[
+                            (
+                                ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1)
+                                // T.int64(8)
+                                + v_ax0
+                            )
+                            % T.int64(4),
+                            ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1) % T.int64(8),
+                            (v_ax2 * T.int64(64) + v_ax3) % T.int64(2048),
+                        ]
+                    )
+                    T.writes(T_reshape_handle_intermediate[v_ax0, v_ax1, v_ax2, v_ax3])
+                    T_reshape_handle_intermediate[v_ax0, v_ax1, v_ax2, v_ax3] = lv_0[
+                        (
+                            ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1) // T.int64(8)
+                            + v_ax0
+                        )
+                        % T.int64(4),
+                        ((v_ax2 * T.int64(64) + v_ax3) // T.int64(2048) + v_ax1) % T.int64(8),
+                        (v_ax2 * T.int64(64) + v_ax3) % T.int64(2048),
+                    ]
+
+        @R.function
+        def main(
+            tup: R.Tuple(
+                R.Tensor((4, 8, 2048), dtype="float32"), R.Tensor((4, 8, 2048), dtype="float32")
+            )
+        ) -> R.Tensor((4, 8, 32, 64), dtype="float32"):
+            cls = Expected
+            with R.dataflow():
+                lv: R.Tensor((4, 8, 2048), dtype="float32") = tup[0]
+                lv_1 = R.call_tir(
+                    cls.fused_reshape, (lv,), out_sinfo=R.Tensor((4, 8, 32, 64), dtype="float32")
+                )
+                R.output(lv_1)
+            return lv_1
+
+    _check(Module, Expected)
+
+
 if __name__ == "__main__":
     tvm.testing.main()
