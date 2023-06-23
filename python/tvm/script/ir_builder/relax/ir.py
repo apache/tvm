25e4712b0372a07f1c549328ebe76a8b4a28c085
Steven Lyubomirsky <slyubomirsky@octoml.ai>
2023-06-23 19:12:58 -0400

    Tweak pylint override again

--- python/tvm/script/ir_builder/relax/ir.py
+++ python/tvm/script/ir_builder/relax/ir.py
@@ -180,10 +180,9 @@ def function(is_pure: bool = True, is_private: bool = False) -> frame.FunctionFr
     frame: FunctionFrame
         The constructed function frame.
     """
-    return _ffi_api.Function(  # pylint: disable=no-member
-        is_pure,
-        is_private,
-    )  # type: ignore[attr-defined]
+    return _ffi_api.Function(  # type: ignore[attr-defined]  # pylint: disable=no-member
+        is_pure, is_private
+    )
 
 
 def arg(name: py_str, struct_info: StructInfo) -> Var:
