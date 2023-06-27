a1f8543718a4a739c44227c37a6432cbd99dee27
Rainy-Memory <2630737606@qq.com>
2023-06-27 07:52:55 +0000

    add tune test & reformat test

--- tests/python/unittest/test_meta_schedule_mma_m16n8k8_auto_tensorization.py
+++ tests/python/unittest/test_meta_schedule_mma_m16n8k8_auto_tensorization.py
@@ -16,23 +16,26 @@
 # under the License.
 """Tests for MMA m16n8k8 Auto Tensorization"""
 
+import tempfile
+import numpy as np
+
 import tvm
 from tvm import te
-from tvm.script import tir as T
 from tvm import meta_schedule as ms
-from tvm.script import tir as T
-
-# get tensor intrin
-from tvm.tir.tensor_intrin import cuda  # pylint: disable=unused-import
+from tvm._ffi import register_func
 from tvm.meta_schedule.testing.space_generation import (
     check_sketches,
     generate_design_space,
 )
+from tvm.meta_schedule.builder import LocalBuilder
 from tvm.script import ir as I
 from tvm.script import tir as T
 from tvm.target import Target
 from tvm.tir import Schedule
 from tvm.tir.schedule import Trace
+
+# get tensor intrin
+from tvm.tir.tensor_intrin import cuda  # pylint: disable=unused-import
 
 import tvm.testing
 
@@ -341,52 +344,56 @@ class MmaModule:
                             ]
 
 
-def matmul_fp16(N: int, M: int, K: int):
+def matmul_fp16(N: int, M: int, K: int, out_dtype: str):
     x = te.placeholder((N, K), name="X", dtype="float16")
     y = te.placeholder((K, M), name="Y", dtype="float16")
     k = te.reduce_axis((0, K), name="k")
     c = te.compute(  # pylint: disable=invalid-name
         (N, M),
-        lambda i, j: te.sum(x[i][k] * y[k][j], axis=[k]),
+        lambda i, j: te.sum(x[i][k].astype(out_dtype) * y[k][j].astype(out_dtype), axis=[k]),
         name="C",
     )
     return (x, y, c)
 
 
-def _design_space(mod):
+def multi_level_tiling_mma(out_dtype):
+    simplify_dict = {"float32": "f32", "float16": "f16"}
+    out_dtype = simplify_dict[out_dtype]
+    return ms.schedule_rule.MultiLevelTilingTensorCore(
+        intrin_groups=[
+            {
+                "init": f"mma_init_m16n8k8_{out_dtype}",
+                "load_a": "mma_load_m16n8k8_f16_A_shared_dyn",
+                "load_b": "mma_load_m16n8k8_f16_B_shared_dyn",
+                "compute": f"mma_sync_m16n8k8_f16f16{out_dtype}",
+                "store": f"mma_store_m16n8k8_{out_dtype}_global",
+            },
+        ],
+        structure="SSSRRSRS",
+        tile_binds=["blockIdx.x", "blockIdx.y", "threadIdx.y"],
+        max_innermost_factor=4,  # 64 // tensor intrin size
+        vector_load_lens=[1, 2, 3, 4, 8, 16],
+        reuse_read=ms.schedule_rule.ReuseType(
+            req="must",
+            levels=[4],
+            scope="shared.dyn",
+        ),
+        reuse_write=ms.schedule_rule.ReuseType(
+            req="no",
+            levels=[2],
+            scope="shared.dyn",
+        ),
+        use_software_pipeline=True,
+    )
+
+
+def _design_space(mod, out_dtype):
     return generate_design_space(
         kind="cuda-tensorcore",
         mod=mod,
         target=Target("nvidia/geforce-rtx-3080"),
         types=None,
-        sch_rules=[
-            ms.schedule_rule.MultiLevelTilingTensorCore(
-                intrin_groups=[
-                    {
-                        "init": "mma_init_m16n8k8_f16",
-                        "load_a": "mma_load_m16n8k8_f16_A_shared_dyn",
-                        "load_b": "mma_load_m16n8k8_f16_B_shared_dyn",
-                        "compute": "mma_sync_m16n8k8_f16f16f16",
-                        "store": "mma_store_m16n8k8_f16_global",
-                    },
-                ],
-                structure="SSSRRSRS",
-                tile_binds=["blockIdx.x", "blockIdx.y", "threadIdx.y"],
-                max_innermost_factor=4,  # 64 // tensor intrin size
-                vector_load_lens=[1, 2, 3, 4, 8, 16],
-                reuse_read=ms.schedule_rule.ReuseType(
-                    req="must",
-                    levels=[4],
-                    scope="shared.dyn",
-                ),
-                reuse_write=ms.schedule_rule.ReuseType(
-                    req="no",
-                    levels=[2],
-                    scope="shared.dyn",
-                ),
-                use_software_pipeline=True,
-            )
-        ],
+        sch_rules=[multi_level_tiling_mma(out_dtype)],
     )
 
 
@@ -398,8 +405,8 @@ gemm_decision = [
 
 
 def test_mma_auto_tensorization():
-    mod = te.create_prim_func(matmul_fp16(M=4096, N=4096, K=4096))
-    actual = _design_space(mod)
+    mod = te.create_prim_func(matmul_fp16(M=4096, N=4096, K=4096, out_dtype="float16"))
+    actual = _design_space(mod, "float16")
     check_sketches(
         mod,
         sketches=actual,
@@ -1180,11 +1187,16 @@ __asm__ __volatile__("cp.async.wait_group 0;");
 """
 
 
-@tvm.testing.requires_cuda
 @tvm.testing.requires_tensorcore
 def test_mma_script_after_build():
-    mod = te.create_prim_func(matmul_fp16(M=4096, N=4096, K=4096))
-    actual = _design_space(mod)
+    arch = tvm.contrib.nvcc.get_target_compute_version()
+    major, _ = tvm.contrib.nvcc.parse_compute_version(arch)
+    if major < 8:
+        # At least sm80 is required
+        return
+
+    mod = te.create_prim_func(matmul_fp16(M=4096, N=4096, K=4096, out_dtype="float16"))
+    actual = _design_space(mod, "float16")
     assert len(actual) == 1
     sketch = actual[0]
 
@@ -1221,6 +1233,83 @@ def test_mma_script_after_build():
     assert rt_mod.imported_modules[0].get_source() == expected_cuda_script
 
 
+def initializer():
+    @register_func("meta_schedule.builder.async_build")
+    def async_build(mod, target, _params):  # pylint: disable=unused-variable, unused-argument
+        # pylint: disable=import-outside-toplevel
+        from tvm.driver import build as tvm_build
+        from tvm.tir.transform import RemoveWeightLayoutRewriteBlock
+
+        # re-import here for local builder to register index_map_m16n8k8_matrixC
+        # pylint: disable=import-outside-toplevel, unused-import
+        from tvm.tir.tensor_intrin import cuda
+
+        mod = RemoveWeightLayoutRewriteBlock(skip_ndarray_rewrite=True)(mod)
+        with tvm.transform.PassContext(config={"tir.use_async_copy": 1}):
+            rt_mod = tvm_build(mod, target=target)
+        return rt_mod
+
+
+@tvm.testing.requires_tensorcore
+@tvm.testing.requires_cublas
+def test_mma_tune():
+    arch = tvm.contrib.nvcc.get_target_compute_version()
+    major, _ = tvm.contrib.nvcc.parse_compute_version(arch)
+    if major < 8:
+        # At least sm80 is required
+        return
+
+    # pylint: disable=import-outside-toplevel
+    from tvm.contrib import cublas
+
+    def tune(out_dtype):
+        M, N, K = 1024, 1024, 1024
+        target = Target("nvidia/geforce-rtx-3080")
+        func = te.create_prim_func(matmul_fp16(N=N, M=M, K=K, out_dtype=out_dtype)).with_attr(
+            {"global_symbol": "main"}
+        )
+        mod = tvm.IRModule({"main": func})
+
+        with tempfile.TemporaryDirectory() as work_dir:
+            db = ms.tir_integration.tune_tir(
+                mod=mod,
+                target=target,
+                work_dir=work_dir,
+                max_trials_global=8,
+                builder=LocalBuilder(
+                    f_build="meta_schedule.builder.async_build", initializer=initializer
+                ),
+                space=ms.space_generator.PostOrderApply(
+                    sch_rules=[multi_level_tiling_mma(out_dtype=out_dtype)],
+                ),
+            )
+            sch = db.query_schedule(mod, target=target, workload_name="main")
+            with tvm.transform.PassContext(config={"tir.use_async_copy": 1}):
+                rt_mod = tvm.build(sch.mod, target=target)
+
+        a_np = np.random.uniform(0, 1, size=(M, K)).astype("float16")
+        b_np = np.random.uniform(0, 1, size=(K, N)).astype("float16")
+        A_cublas = te.placeholder((M, K), name="A", dtype="float16")
+        B_cublas = te.placeholder((K, N), name="B", dtype="float16")
+        C_cublas = cublas.matmul(A_cublas, B_cublas, dtype=out_dtype)
+        s = te.create_schedule(C_cublas.op)
+        dev = tvm.cuda(0)
+        f_cublas = tvm.build(s, [A_cublas, B_cublas, C_cublas], target)
+        a_cublas = tvm.nd.array(a_np.astype("float16"), dev)
+        b_cublas = tvm.nd.array(b_np.astype("float16"), dev)
+        c_cublas = tvm.nd.array(np.zeros((M, N), dtype=C_cublas.dtype), dev)
+        f_cublas(a_cublas, b_cublas, c_cublas)
+        a_tvm = tvm.nd.array(a_np, device=tvm.cuda(0))
+        b_tvm = tvm.nd.array(b_np, device=tvm.cuda(0))
+        c_tvm = tvm.nd.array(np.empty((M, N)).astype(out_dtype), device=tvm.cuda(0))
+        rt_mod(a_tvm, b_tvm, c_tvm)
+        assert np.allclose(c_tvm.numpy(), c_cublas.numpy(), rtol=1e-2)
+
+    tune("float16")
+    tune("float32")
+
+
 if __name__ == "__main__":
     test_mma_auto_tensorization()
     test_mma_script_after_build()
+    test_mma_tune()
