7f1589145e3a8d0a9001a19e8fc29f39c9d5b431
Rainy-Memory <2630737606@qq.com>
2023-06-27 02:29:24 +0000

    fix lint

--- python/tvm/driver/build_module.py
+++ python/tvm/driver/build_module.py
@@ -36,9 +36,6 @@ from tvm.tir.buffer import Buffer
 from tvm.tir.expr import Var
 from tvm.driver import _ffi_api as _driver_ffi
 
-# import index_map_m16n8k8_matrixC here for tuning
-from tvm.tir.tensor_intrin import cuda  # pylint: disable=unused-import
-
 from . import _ffi_api as ffi
 
 
