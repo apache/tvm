b792f959304dfe7b1d391f57e5276032a7e4390f
Qingchao Shen <qingchaoshen@outlook.com>
2023-06-29 20:33:32 +0800

    Update test_forward.py

--- tests/python/frontend/keras/test_forward.py
+++ tests/python/frontend/keras/test_forward.py
@@ -169,10 +169,11 @@ class TestKeras:
         verify_keras_frontend(keras_model)
         # test default axis (e.g., -1)
         data1 = keras_mod.layers.Input(shape=(1, 2, 2))
-        data2 = keras_mod.layers.Input(shape=(1, 2, 4))
+        data2 = keras_mod.layers.Input(shape=(1, 2, 2))
         merge_func = keras_mod.layers.Concatenate()
         out = merge_func([data1, data2])
         keras_model = keras_mod.models.Model([data1, data2], out)
+        verify_keras_frontend(keras_model)
 
     def test_forward_merge_dot(self, keras_mod):
         """test_forward_merge_dot"""
