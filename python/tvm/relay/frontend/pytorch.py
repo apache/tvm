66a99a99757e7ec82ba51aa1967ac3f163adda69
haoyang9804 <haoyanghkust@gmail.com>
2023-07-24 14:49:57 +0800

    add tests for Fix an adaptive_max_pool1d operator conversion bug

--- python/tvm/relay/frontend/pytorch.py
+++ python/tvm/relay/frontend/pytorch.py
@@ -4292,7 +4292,7 @@ class PyTorchOpConverter:
             self.current_op.pop()
 
         return [
-            _wrap_const(outputs[ret_name]) for ret_name in ret_names if outputs[ret_name] != None
+            _wrap_const(outputs[ret_name]) for ret_name in ret_names if outputs[ret_name] is not None
         ]
 
     def _set_parameter_source_name(self, op_node, outputs):
