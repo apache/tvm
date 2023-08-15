bd7a4bd3b95bc38c14ab76ceced0e9d3374127cb
Wuwei Lin <wuwei@apache.org>
2023-08-15 12:29:40 -0700

    lint

--- python/tvm/tir/transform/transform.py
+++ python/tvm/tir/transform/transform.py
@@ -233,7 +233,8 @@ def StorageRewrite():
 def PointerValueTypeRewrite():
     """
     Rewrite the pointer content type of arguments, as well as Alloc internal to the function to use
-    the most frequently accessed type for load/store to avoid pointer casting in backend when possible.
+    the most frequently accessed type for load/store to avoid pointer casting in backend when
+    possible.
 
     Returns
     -------
