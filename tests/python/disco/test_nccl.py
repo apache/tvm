7556592dd2cf788c192ae440373f242a327af864
Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
2023-08-28 09:30:00 -0700

    [Disco][Test] MultiHeadAttention Testcase (#15632)

--- tests/python/disco/test_nccl.py
+++ tests/python/disco/test_nccl.py
@@ -173,8 +173,166 @@ def test_mlp():  # pylint: disable=too-many-locals
     np.testing.assert_allclose(Y_result, Y_expected, rtol=1e-4, atol=1e-4)
 
 
+def test_attention():  # pylint: disable=too-many-locals
+    num_workers = 2
+    devices = [1, 2]
+
+    # pylint: disable=invalid-name
+    @tvm.script.ir_module
+    class Attention:  # pylint: disable=too-few-public-methods
+        @R.function
+        def main(
+            x: R.Tensor((1, 10, 128), "float32"),
+            Wq: R.Tensor((128, 512), "float32"),
+            Wk: R.Tensor((128, 512), "float32"),
+            Wv: R.Tensor((128, 512), "float32"),
+            Wo: R.Tensor((512, 128), "float32"),
+        ) -> R.Tensor((128, 128), "float32"):
+            R.func_attr({"global_symbol": "main"})
+            with R.dataflow():
+                # q
+                lv0: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wq)
+                lv1: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv0, [1, 10, 8, 64])
+                lv2: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv1, [0, 2, 1, 3])
+                # k
+                lv3: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wk)
+                lv4: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv3, [1, 10, 8, 64])
+                lv5: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv4, [0, 2, 1, 3])
+                # v
+                lv6: R.Tensor((1, 10, 512), "float32") = R.matmul(x, Wv)
+                lv7: R.Tensor((1, 10, 8, 64), "float32") = R.reshape(lv6, [1, 10, 8, 64])
+                lv8: R.Tensor((1, 8, 10, 64), "float32") = R.permute_dims(lv7, [0, 2, 1, 3])
+                # softmax(q @ k / sqrt(dk))
+                lv9: R.Tensor((1, 8, 64, 10), "float32") = R.permute_dims(lv5, [0, 1, 3, 2])
+                lv10: R.Tensor((1, 8, 10, 10), "float32") = R.matmul(lv2, lv9)
+                lv11: R.Tensor((1, 8, 10, 10), "float32") = R.multiply(
+                    lv10, R.const(1 / 8, "float32")
+                )
+                lv12: R.Tensor((1, 8, 10, 10), "float32") = R.nn.softmax(lv11, axis=-1)
+                # attn_weight @ v
+                lv13: R.Tensor((1, 8, 10, 64), "float32") = R.matmul(lv12, lv8)
+                lv14: R.Tensor((1, 10, 8, 64), "float32") = R.permute_dims(lv13, [0, 2, 1, 3])
+                lv15: R.Tensor((1, 10, 512), "float32") = R.reshape(lv14, [1, 10, 512])
+                # attn_output @ o
+                lv16: R.Tensor((1, 10, 128), "float32") = R.matmul(lv15, Wo)
+                R.output(lv16)
+            return lv16
+
+    @tvm.script.ir_module
+    class ShardedAttention:  # pylint: disable=too-few-public-methods
+        @R.function
+        def main(
+            x: R.Tensor((1, 10, 128), "float32"),
+            Wq: R.Tensor((128, 256), "float32"),  # shard along axis 1
+            Wk: R.Tensor((128, 256), "float32"),  # shard along axis 1
+            Wv: R.Tensor((128, 256), "float32"),  # shard along axis 1
+            Wo: R.Tensor((256, 128), "float32"),  # shard along axis 0
+        ) -> R.Tensor((128, 128), "float32"):
+            R.func_attr({"global_symbol": "main"})
+            with R.dataflow():
+                # q
+                lv0: R.Tensor((1, 10, 256), "float32") = R.matmul(x, Wq)
+                lv1: R.Tensor((1, 10, 4, 64), "float32") = R.reshape(lv0, [1, 10, 4, 64])
+                lv2: R.Tensor((1, 4, 10, 64), "float32") = R.permute_dims(lv1, [0, 2, 1, 3])
+                # k
+                lv3: R.Tensor((1, 10, 256), "float32") = R.matmul(x, Wk)
+                lv4: R.Tensor((1, 10, 4, 64), "float32") = R.reshape(lv3, [1, 10, 4, 64])
+                lv5: R.Tensor((1, 4, 10, 64), "float32") = R.permute_dims(lv4, [0, 2, 1, 3])
+                # v
+                lv6: R.Tensor((1, 10, 256), "float32") = R.matmul(x, Wv)
+                lv7: R.Tensor((1, 10, 4, 64), "float32") = R.reshape(lv6, [1, 10, 4, 64])
+                lv8: R.Tensor((1, 4, 10, 64), "float32") = R.permute_dims(lv7, [0, 2, 1, 3])
+                # softmax(q @ k / sqrt(dk))
+                lv9: R.Tensor((1, 4, 64, 10), "float32") = R.permute_dims(lv5, [0, 1, 3, 2])
+                lv10: R.Tensor((1, 4, 10, 10), "float32") = R.matmul(lv2, lv9)
+                lv11: R.Tensor((1, 4, 10, 10), "float32") = R.multiply(
+                    lv10, R.const(1 / 8, "float32")
+                )
+                lv12: R.Tensor((1, 4, 10, 10), "float32") = R.nn.softmax(lv11, axis=-1)
+                # attn_weight @ v
+                lv13: R.Tensor((1, 4, 10, 64), "float32") = R.matmul(lv12, lv8)
+                lv14: R.Tensor((1, 10, 4, 64), "float32") = R.permute_dims(lv13, [0, 2, 1, 3])
+                lv15: R.Tensor((1, 10, 256), "float32") = R.reshape(lv14, [1, 10, 256])
+                # attn_output @ o
+                lv16: R.Tensor((1, 10, 128), "float32") = R.matmul(lv15, Wo)
+                lv17: R.Tensor((1, 10, 128), "float32") = R.ccl.allreduce(lv16, "sum")
+                R.output(lv17)
+            return lv17
+
+    # pylint: enable=invalid-name
+    target = tvm.target.Target(
+        {
+            "kind": "cuda",
+            "max_shared_memory_per_block": 49152,
+            "max_threads_per_block": 1024,
+            "thread_warp_size": 32,
+            "registers_per_block": 65536,
+            "arch": "sm_80",
+        }
+    )
+
+    def relax_build(mod, target):
+        with target:
+            mod = rx.get_pipeline("zero")(mod)  # pylint: disable=no-value-for-parameter
+            mod = dl.ApplyDefaultSchedule(  # pylint: disable=not-callable
+                dl.gpu.Matmul(),
+                dl.gpu.GEMV(),
+                dl.gpu.Reduction(),
+                dl.gpu.GeneralReduction(),
+                dl.gpu.Fallback(),
+            )(mod)
+            return rx.build(mod, target="cuda")
+
+    # pylint: disable=invalid-name
+    X = np.random.randn(1, 10, 128).astype("float32")
+    Wq = np.random.randn(128, 512).astype("float32")
+    Wk = np.random.randn(128, 512).astype("float32")
+    Wv = np.random.randn(128, 512).astype("float32")
+    Wo = np.random.randn(512, 128).astype("float32")
+    Y_expected = VirtualMachine(relax_build(Attention, target), device=tvm.cuda(0))["main"](
+        tvm.nd.array(X, device=tvm.cuda(0)),
+        tvm.nd.array(Wq, device=tvm.cuda(0)),
+        tvm.nd.array(Wk, device=tvm.cuda(0)),
+        tvm.nd.array(Wv, device=tvm.cuda(0)),
+        tvm.nd.array(Wo, device=tvm.cuda(0)),
+    ).numpy()
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = tmpdir + "/test.so"
+        relax_build(ShardedAttention, target).export_library(path)
+
+        sess = di.ThreadedSession(num_workers=num_workers)
+        sess.init_ccl("nccl", *devices)
+        mod = sess.load_vm_module(path)
+
+        d_X = sess.empty((1, 10, 128), "float32")
+        d_Wq = sess.empty((128, 256), "float32")
+        d_Wk = sess.empty((128, 256), "float32")
+        d_Wv = sess.empty((128, 256), "float32")
+        d_Wo = sess.empty((256, 128), "float32")
+
+        d_X.debug_copy_from(0, X)
+        d_X.debug_copy_from(1, X)
+        d_Wq.debug_copy_from(0, Wq[:, :256])
+        d_Wq.debug_copy_from(1, Wq[:, 256:])
+        d_Wk.debug_copy_from(0, Wk[:, :256])
+        d_Wk.debug_copy_from(1, Wk[:, 256:])
+        d_Wv.debug_copy_from(0, Wv[:, :256])
+        d_Wv.debug_copy_from(1, Wv[:, 256:])
+        d_Wo.debug_copy_from(0, Wo[:256, :])
+        d_Wo.debug_copy_from(1, Wo[256:, :])
+        d_Y = mod["main"](d_X, d_Wq, d_Wk, d_Wv, d_Wo)
+        Y_result = tvm.nd.empty((1, 10, 128), "float32", device=tvm.cuda(0))
+        sess.copy_from_worker_0(Y_result, d_Y)
+        sess.sync_worker_0()
+        Y_result = Y_result.numpy()
+    # pylint: enable=invalid-name
+    np.testing.assert_allclose(Y_result, Y_expected, rtol=1e-3, atol=1e-3)
+
+
 if __name__ == "__main__":
     test_init()
     test_broadcast_from_zero()
     test_allreduce()
     test_mlp()
+    test_attention()
