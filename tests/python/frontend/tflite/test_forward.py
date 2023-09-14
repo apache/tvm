06826e710060a67858275368ed808aca21e29fca
Tlopex <68688494+tlopex@users.noreply.github.com>
2023-09-14 15:00:35 +0800

    Update test_forward.py

--- tests/python/frontend/tflite/test_forward.py
+++ tests/python/frontend/tflite/test_forward.py
@@ -2684,7 +2684,15 @@ def _test_greater_equal(data):
 
 def _test_less(data):
     """One iteration of less"""
-    return _test_elemwise(math_ops.less, data)
+    return _test_elemwise(
+        math_ops.less,
+        data,
+        fused_activation_function,
+        quantized,
+        qnn_op,
+        same_qnn_params=True,
+        comparison_op=True,
+    )
 
 
 #######################################################################
@@ -2816,6 +2824,7 @@ def _test_elemwise_qnn_out_range(qnn_op):
         _test_greater: (-150, 150),
         _test_squared_difference: (0, 65025),
         _test_floor_divide: (-150, 150),
+        _test_less: (-150, 150),
     }
 
     return qnn_out_range[qnn_op]
@@ -2851,6 +2860,7 @@ def test_all_elemwise():
     _test_forward_elemwise_quantized(_test_squared_difference, np.int8)
     _test_forward_elemwise(_test_greater_equal)
     _test_forward_elemwise(_test_less)
+    _test_forward_elemwise_quantized(_test_less)
     _test_forward_elemwise(_test_less_equal)
     _test_forward_elemwise(_test_equal)
     _test_forward_elemwise_quantized(_test_equal)
