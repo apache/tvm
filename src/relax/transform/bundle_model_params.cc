207a222b1d3d61805b64f77df33729cfedc3159b
Wuwei Lin <wuwei@apache.org>
2023-09-15 11:12:36 -0700

    fix typo

--- src/relax/transform/bundle_model_params.cc
+++ src/relax/transform/bundle_model_params.cc
@@ -66,7 +66,7 @@ class ModelParamBundler : public ExprMutator {
       var_to_expr_.Set(func->params[i], TupleGetItem(var_param_tuple, i - num_input));
     }
 
-    func = WithoutAttr(func, kAttrNumInput);
+    func = WithoutAttr(func, attr::kNumInput);
     func.CopyOnWrite()->params = params;
 
     return ExprMutator::VisitExpr_(func.get());
