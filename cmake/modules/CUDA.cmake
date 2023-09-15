906653ee1a0d546232580efcad8adefc4a77be6e
Junru Shao <junrushao1994@gmail.com>
2023-09-15 15:52:27 +0000

    [Runtime][CUDA] NVTX Integration
    This PR enables NVTX integration with TVM runtime. It exposes the
    following interfaces:
    
    ```
    TVM_NVTX_FUNC_SCOPE(); // A macro to mark function scope
    NVTXScopedRange _scope("my_nvtx_loop"); // An RAII-managed NVTX scope
    ```
    
    If TVM is not built against NVTX, those operations will be dissolved as
    no-op in runtime with no side effects.

--- cmake/modules/CUDA.cmake
+++ cmake/modules/CUDA.cmake
@@ -81,6 +81,14 @@ if(USE_CUDA)
     list(APPEND RUNTIME_SRCS ${CONTRIB_CURAND_SRC_CU})
   endif(USE_CURAND)
 
+  if(USE_NVTX)
+    message(STATUS "Build with NVTX support")
+    message(STATUS "${CUDA_NVTX_LIBRARY}")
+    cmake_minimum_required(VERSION 3.13) # to compile CUDA code
+    enable_language(CUDA)
+    list(APPEND TVM_RUNTIME_LINKER_LIBS ${CUDA_NVTX_LIBRARY})
+  endif(USE_NVTX)
+
   if(USE_GRAPH_EXECUTOR_CUDA_GRAPH)
     if(NOT USE_GRAPH_EXECUTOR)
       message(FATAL_ERROR "CUDA Graph is only supported by graph executor, please set USE_GRAPH_EXECUTOR=ON")
