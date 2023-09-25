92318253da9b20f1596268cf3e1f1dbfa8450ba8
Danilo <dmilosevic252@gmail.com>
2023-09-25 15:43:53 +0200

    Fix formatting

--- tests/python/frontend/onnx/test_forward.py
+++ tests/python/frontend/onnx/test_forward.py
@@ -2920,7 +2920,6 @@ def test_batch_norm_dynamic_subgraph(target, dev):
     """test_batch_norm_dynamic_subgraph"""
 
     def verify_batch_norm_dynamic_subgraph(in_shape, o_shape):
-
         batchnorm = onnx.helper.make_node(
             "BatchNormalization", inputs=["x", "scale", "B", "mean", "var"], outputs=["Y"]
         )
@@ -5377,6 +5376,7 @@ def test_softplus(target, dev):
     input_data = np.random.randn(1, 32, 32, 3).astype("float32")
     verify_softplus(input_data)
 
+
 def test_load_cumsum():
     """test_load_cumsum"""
 
@@ -5390,14 +5390,14 @@ def test_load_cumsum():
             "cumsum_graph",
             inputs=[
                 helper.make_tensor_value_info("X", onnx.TensorProto.DOUBLE, input_shape),
-                helper.make_tensor_value_info("axis", onnx.TensorProto.INT32, [1], 'axis'),
+                helper.make_tensor_value_info("axis", onnx.TensorProto.INT32, [1], "axis"),
             ],
-            outputs=
-            [helper.make_tensor_value_info("Y", onnx.TensorProto.DOUBLE, input_shape)],
+            outputs=[helper.make_tensor_value_info("Y", onnx.TensorProto.DOUBLE, input_shape)],
         )
         return helper.make_model(graph)
 
     from_onnx(create_cumsum_model())
+
 
 @tvm.testing.parametrize_targets
 def test_cumsum(target, dev):
@@ -6651,7 +6651,6 @@ def test_qlinearconv(target, dev):
         bias=False,
         per_channel_quantization=False,
     ):
-
         x_array = np.random.randint(low=0, high=255, size=x_shape).astype("uint8")
         w_array = np.random.uniform(low=0, high=255, size=w_shape).astype("uint8")
 
@@ -7011,7 +7010,6 @@ def test_qlinearadd(target, dev):
     """test_qlinearadd"""
 
     def verify_qlinearadd(a_shape, b_shape, c_shape):
-
         _ = np.random.random(a_shape).astype("float32")
         _ = np.random.random(b_shape).astype("float32")
 
@@ -7044,7 +7042,6 @@ def test_qlinearmul(target, dev):
     """test_qlinearmul"""
 
     def verify_qlinearmul(a_shape, b_shape, c_shape):
-
         _ = np.random.random(a_shape).astype("float32")
         _ = np.random.random(b_shape).astype("float32")
 
@@ -7079,7 +7076,6 @@ def test_qlinearleakyrelu(target, dev):
     """test_qlinearleakyrelu"""
 
     def verify_qlinearleakyrelu(inshape, kwargs):
-
         in_array = np.random.random(inshape).astype("float32")
         node = helper.make_node("LeakyRelu", ["X"], ["Y"], **kwargs)
 
@@ -7107,7 +7103,6 @@ def test_qlinearsigmoid(target, dev):
     """test_qlinearsigmoid"""
 
     def verify_qlinearsigmoid(a_shape):
-
         _ = np.random.random(a_shape).astype("float32")
 
         input_nodes = [helper.make_tensor_value_info("a", TensorProto.FLOAT, list(a_shape))]
@@ -7133,7 +7128,6 @@ def test_qlinearsoftmax(target, dev):
     """test_qlinearsoftmax"""
 
     def verify_qlinearsoftmax(a_shape):
-
         _ = np.random.random(a_shape).astype("float32")
 
         input_nodes = [helper.make_tensor_value_info("a", TensorProto.FLOAT, list(a_shape))]
@@ -7909,7 +7903,6 @@ def test_scan(target, dev):
         scan_output_directions,
         opset,
     ):
-
         body_input_shapes = copy.deepcopy(input_shapes)
         num_state_inputs = len(input_shapes) - num_scan_inputs
 
