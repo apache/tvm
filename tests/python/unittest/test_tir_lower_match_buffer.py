bfed48f004d21aba039feecff9eb38af4ba23793
Wuwei Lin <wuwei@apache.org>
2023-09-25 17:29:50 -0700

    Revert "[TensorIR][Visitor] Visit buffer members in `match_buffer`'s in block visitor functions (#15153)"
    This reverts commit 34637d7ee38f2636b1948548a39c15838d7a8db6.

--- tests/python/unittest/test_tir_lower_match_buffer.py
+++ tests/python/unittest/test_tir_lower_match_buffer.py
@@ -18,7 +18,6 @@
 import pytest
 
 import tvm
-import tvm.testing
 from tvm.script import tir as T
 
 
@@ -531,4 +530,12 @@ def test_fail_match_func_param():
 
 
 if __name__ == "__main__":
-    tvm.testing.main()
+    test_buffer_load_store()
+    test_opaque_access()
+    test_high_dim_opaque_access()
+    test_recursive_match()
+    test_symbolic_match()
+    test_rank0_buffer()
+    test_fail_load_store()
+    test_fail_buffer_bind()
+    test_fail_match_func_param()
