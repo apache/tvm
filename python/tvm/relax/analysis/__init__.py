fc837efa7e72f027907b98c1f83b00ddb4f94dc2
Siyuan Feng <hzfengsy@sjtu.edu.cn>
2023-10-01 22:28:11 +0800

    [Unity] Avoid Using Wildcard Imports
    Wildcard importing is not recommended in Python projects:
    https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
    
    I acknowledge that disabling it will bring extra effort to adding new
    operators, analysis and transforms. But it will make the code clear, also
    benefit the API reference doc.

--- python/tvm/relax/analysis/__init__.py
+++ python/tvm/relax/analysis/__init__.py
@@ -14,8 +14,31 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=wildcard-import, redefined-builtin
 """Relax IR analysis. """
 
-from .analysis import *
+from .analysis import (
+    BaseCheckResult,
+    all_global_vars,
+    all_vars,
+    bound_vars,
+    contains_impure_call,
+    defined_symbolic_vars,
+    derive_call_ret_struct_info,
+    detect_recursion,
+    erase_to_well_defined,
+    free_symbolic_vars,
+    free_vars,
+    get_static_type,
+    get_var2val,
+    has_reshape_pattern,
+    name_to_binding,
+    post_order_visit,
+    remove_all_unused,
+    struct_info_base_check,
+    struct_info_lca,
+    suggest_layout_transforms,
+    tir_vars_in_struct_info,
+    udchain,
+    well_formed,
+)
 from .estimate_memory_usage import estimate_memory_usage
--- python/tvm/relax/op/ccl/__init__.py
+++ python/tvm/relax/op/ccl/__init__.py
@@ -14,6 +14,5 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=wildcard-import
 """CCL related operators."""
-from .ccl import *
+from .ccl import allgather, allreduce, broadcast_from_worker0, scatter_from_worker0
