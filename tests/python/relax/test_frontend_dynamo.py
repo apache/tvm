cff31ce6fc38f00fc4ef6831847c092499086033
guoyaol <guoyaoli00@gmail.com>
2023-10-06 16:40:08 -0400

    correct tests for tensor as index

--- tests/python/relax/test_frontend_dynamo.py
+++ tests/python/relax/test_frontend_dynamo.py
@@ -436,6 +436,7 @@ def test_masked_fill():
         InplaceMaskedFill(), [([256, 256], "bool"), ([256, 256], "float32")], {}, Expected1
     )
 
+
 @tvm.testing.requires_gpu
 def test_getitem():
     import torch
@@ -443,8 +444,7 @@ def test_getitem():
 
     class Select1(Module):
         def forward(self, input1, input2):
-            result = input1[torch.arange(
-                input1.shape[0]), input2.argmax(dim=-1),]
+            result = input1[:, input2.argmax(dim=-1), :]
             return result
 
     @I.ir_module
@@ -454,22 +454,43 @@ def test_getitem():
             with R.dataflow():
                 lv: R.Tensor((1,), dtype="int64") = R.argmax(
                     inp_1, axis=-1, keepdims=False)
-                lv1: R.Tensor((1,), dtype="int64") = R.arange(
-                    R.prim_value(0), R.prim_value(1), R.prim_value(1), dtype="int64")
-                lv2: R.Tensor((1, 77, 1280), dtype="float32") = R.take(
-                    inp_0, lv1, axis=0)
-                lv3: R.Tensor((1, 1, 1280), dtype="float32") = R.take(
-                    lv2, lv, axis=1)
-                lv4: R.Tensor((1, 1, 1280), dtype="float32") = R.strided_slice(
-                    lv3, axes=[2], begin=[0], end=[1280], strides=[1], assume_inbound=False)
-                lv5: R.Tensor((1, 1, 1280), dtype="float32") = R.reshape(
-                    lv4, R.shape([1, 1, 1280]))
-                gv: R.Tensor((1, 1, 1280), dtype="float32") = lv5
+                lv1: R.Tensor((1, 1, 1280), dtype="float32") = R.take(
+                    inp_0, lv, axis=1)
+                lv2: R.Tensor((1, 1, 1280), dtype="float32") = R.strided_slice(
+                    lv1, axes=[0, 2], begin=[0, 0], end=[1, 1280], strides=[1, 1], assume_inbound=False)
+                lv3: R.Tensor((1, 1, 1280), dtype="float32") = R.reshape(
+                    lv2, R.shape([1, 1, 1280]))
+                gv: R.Tensor((1, 1, 1280), dtype="float32") = lv3
                 R.output(gv)
             return gv
 
+    @I.ir_module
+    class Expected2:
+        @R.function
+        def main(inp_0: R.Tensor((1, 77, 1280), dtype="float32")) -> R.Tensor((1, 77, 1280), dtype="float32"):
+            with R.dataflow():
+                lv: R.Tensor((1,), dtype="int64") = R.arange(R.prim_value(
+                    0), R.prim_value(1), R.prim_value(1), dtype="int64")
+                lv1: R.Tensor((1, 77, 1280), dtype="float32") = R.take(
+                    inp_0, lv, axis=0)
+                lv2: R.Tensor((1, 77, 1280), dtype="float32") = R.strided_slice(
+                    lv1, axes=[1, 2], begin=[0, 0], end=[77, 1280], strides=[1, 1], assume_inbound=False)
+                lv3: R.Tensor((1, 77, 1280), dtype="float32") = R.reshape(
+                    lv2, R.shape([1, 77, 1280]))
+                gv: R.Tensor((1, 77, 1280), dtype="float32") = lv3
+                R.output(gv)
+            return gv
+
+    class Select2(Module):
+        def forward(self, input1):
+            result = input1[torch.arange(1),]
+            return result
+
     verify_dynamo_model(
         Select1(), [([1, 77, 1280], "float32"), ([1, 77], "float32")], {}, Expected1)
+    verify_dynamo_model(
+        Select2(), [([1, 77, 1280], "float32")], {}, Expected2)
+
 
 if __name__ == "__main__":
     tvm.testing.main()
