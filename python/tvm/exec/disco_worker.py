eb0ae208280b150705e9bb9de24c789825e882b2
Junru Shao <junrushao@apache.org>
2023-10-07 00:44:30 -0700

    [Disco][Fix] Remove Dependency to PyTest
    Disco worker originally automatically import `tvm.testing.disco` for
    convenient unittesting. However, `tvm.testing` is a special subpackage
    that introduces many unnecessary dependencies, for example, pytest. This
    PR removes such dependencies by directly moving the testing function
    registration logic to the entry file.

--- python/tvm/exec/disco_worker.py
+++ python/tvm/exec/disco_worker.py
@@ -20,8 +20,41 @@ import os
 import sys
 
 from tvm import runtime as _  # pylint: disable=unused-import
-from tvm._ffi import get_global_func
-from tvm.testing import disco as _  # pylint: disable=unused-import
+from tvm._ffi import get_global_func, register_func
+from tvm.runtime import NDArray, ShapeTuple, String
+from tvm.runtime.ndarray import array
+
+
+@register_func("tests.disco.add_one")
+def _add_one(x: int) -> int:  # pylint: disable=invalid-name
+    return x + 1
+
+
+@register_func("tests.disco.add_one_float", override=True)
+def _add_one_float(x: float):  # pylint: disable=invalid-name
+    return x + 0.5
+
+
+@register_func("tests.disco.add_one_ndarray", override=True)
+def _add_one_ndarray(x: NDArray) -> NDArray:  # pylint: disable=invalid-name
+    return array(x.numpy() + 1)
+
+
+@register_func("tests.disco.str", override=True)
+def _str_func(x: str):  # pylint: disable=invalid-name
+    return x + "_suffix"
+
+
+@register_func("tests.disco.str_obj", override=True)
+def _str_obj_func(x: String):  # pylint: disable=invalid-name
+    assert isinstance(x, String)
+    return String(x + "_suffix")
+
+
+@register_func("tests.disco.shape_tuple", override=True)
+def _shape_tuple_func(x: ShapeTuple):  # pylint: disable=invalid-name
+    assert isinstance(x, ShapeTuple)
+    return ShapeTuple(list(x) + [4, 5])
 
 
 def main():
