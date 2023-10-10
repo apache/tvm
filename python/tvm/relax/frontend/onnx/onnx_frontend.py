42ad6a767591dae2ee02c071964f8a2e302f4ee3
Thrsu <hll3020244288@163.com>
2023-10-10 21:05:37 +0800

    Fix MaxPool TypeError

--- python/tvm/relax/frontend/onnx/onnx_frontend.py
+++ python/tvm/relax/frontend/onnx/onnx_frontend.py
@@ -1424,7 +1424,7 @@ class MaxPool(OnnxOpConverter):
         dilations = attr.get("dilations", [1, 1])
         kernel_shape = attr.get("kernel_shape")
         pads = attr.get("pads", 0)
-        strides = attr.get("strides", 1)
+        strides = attr.get("strides", [1, 1])
 
         assert len(kernel_shape) == 2, "Currently only 2D pooling is supported."
         assert auto_pad in [
