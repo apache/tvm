9770484f75554f22a91a1a96b182d4a5ad6fe7ab
Tlopex <68688494+tlopex@users.noreply.github.com>
2023-11-03 22:49:17 +0800

    Update test_sort.py

--- tests/python/contrib/test_sort.py
+++ tests/python/contrib/test_sort.py
@@ -14,18 +14,20 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""Configure pytest"""
+import numpy as np
 import tvm
 import tvm.testing
 from tvm import te
 from tvm.topi.cuda import sort_by_key
-import numpy as np
 
 
 def test_sort():
+    """Tests sort function"""
     n = 2
-    l = 5
+    lens = 5
     m = 3
-    data = te.placeholder((n, l, m), name="data")
+    data = te.placeholder((n, lens, m), name="data")
     sort_num = te.placeholder((n, m), name="sort_num", dtype="int32")
     axis = 1
     is_ascend = False
@@ -38,7 +40,7 @@ def test_sort():
         dtype="int32",
         name="sort_tensor",
     )
-    input = [
+    input_data = [
         [[1, 2, 3], [2, 4.5, 3.5], [1.1, 0.5, 1], [3.2, -5, 0.5], [1.5, 0, 0]],
         [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]],
     ]
@@ -52,7 +54,7 @@ def test_sort():
     target = "llvm"
     s = te.create_schedule(out.op)
     f = tvm.build(s, [data, sort_num, out], target)
-    a = tvm.nd.array(np.array(input).astype(data.dtype), dev)
+    a = tvm.nd.array(np.array(input_data).astype(data.dtype), dev)
     b = tvm.nd.array(np.array(sort_num_input).astype(sort_num.dtype), dev)
     c = tvm.nd.array(np.zeros(a.shape, dtype=out.dtype), dev)
     f(a, b, c)
@@ -60,6 +62,7 @@ def test_sort():
 
 
 def test_sort_np():
+    """Tests sort function using numpy"""
     dshape = (1, 2, 3, 4, 5, 6)
     axis = 4
     reduced_shape = (1, 2, 3, 4, 6)
@@ -92,6 +95,7 @@ def test_sort_np():
 
 
 def test_sort_by_key_gpu():
+    """Tests sort function using gpu"""
     size = 6
     keys = te.placeholder((size,), name="keys", dtype="int32")
     values = te.placeholder((size,), name="values", dtype="int32")
