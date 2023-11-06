26054bc03a5256f061fac0b1dbba50fc3a263713
krishnaraj36 <quic_kvegiraj@quicinc.com>
2023-11-06 22:33:14 +0530

    Fix the import error

--- tests/python/contrib/test_clml/test_network.py
+++ tests/python/contrib/test_clml/test_network.py
@@ -21,7 +21,7 @@ import numpy as np
 from tvm import relay
 from tvm.relay import testing
 from tvm.contrib import utils
-from infrastructure import build_and_run, Device
+from test_clml.infrastructure import build_and_run, Device
 import pytest
 
 
