35cc37941cee648eef94b13d4c69c523e37878cc
Tlopex <68688494+tlopex@users.noreply.github.com>
2023-11-06 11:48:10 +0800

    Update test_random.py

--- tests/python/contrib/test_random.py
+++ tests/python/contrib/test_random.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 """Configure pytest"""
+# pylint: disable=invalid-name
 import threading
 import numpy as np
 import tvm
@@ -28,8 +29,8 @@ def test_randint():
     """Tests randint function"""
     m = 10240
     n = 10240
-    input_a = random.randint(-127, 128, size=(m, n), dtype="int32")
-    s = te.create_schedule(input_a.op)
+    A = random.randint(-127, 128, size=(m, n), dtype="int32")
+    s = te.create_schedule(A.op)
 
     def verify(target="llvm"):
         if not tvm.testing.device_enabled(target):
@@ -39,13 +40,13 @@ def test_randint():
             print("skip because extern function is not available")
             return
         dev = tvm.cpu(0)
-        f = tvm.build(s, [input_a], target)
-        a = tvm.nd.array(np.zeros((m, n), dtype=input_a.dtype), dev)
+        f = tvm.build(s, [A], target)
+        a = tvm.nd.array(np.zeros((m, n), dtype=A.dtype), dev)
         f(a)
-        _na = a.numpy()
-        assert abs(np.mean(_na)) < 0.3
-        assert np.min(_na) == -127
-        assert np.max(_na) == 127
+        na = a.numpy()
+        assert abs(np.mean(na)) < 0.3
+        assert np.min(na) == -127
+        assert np.max(na) == 127
 
     verify()
 
@@ -54,8 +55,8 @@ def test_uniform():
     """Tests uniform function"""
     m = 10240
     n = 10240
-    input_a = random.uniform(0, 1, size=(m, n))
-    s = te.create_schedule(input_a.op)
+    A = random.uniform(0, 1, size=(m, n))
+    s = te.create_schedule(A.op)
 
     def verify(target="llvm"):
         if not tvm.testing.device_enabled(target):
@@ -65,13 +66,13 @@ def test_uniform():
             print("skip because extern function is not available")
             return
         dev = tvm.cpu(0)
-        f = tvm.build(s, [input_a], target)
-        a = tvm.nd.array(np.zeros((m, n), dtype=input_a.dtype), dev)
+        f = tvm.build(s, [A], target)
+        a = tvm.nd.array(np.zeros((m, n), dtype=A.dtype), dev)
         f(a)
-        op_na = a.numpy()
-        assert abs(np.mean(op_na) - 0.5) < 1e-1
-        assert abs(np.min(op_na) - 0.0) < 1e-3
-        assert abs(np.max(op_na) - 1.0) < 1e-3
+        na = a.numpy()
+        assert abs(np.mean(na) - 0.5) < 1e-1
+        assert abs(np.min(na) - 0.0) < 1e-3
+        assert abs(np.max(na) - 1.0) < 1e-3
 
     verify()
 
@@ -80,8 +81,8 @@ def test_normal():
     """Tests normal function"""
     m = 10240
     n = 10240
-    input_a = random.normal(3, 4, size=(m, n))
-    s = te.create_schedule(input_a.op)
+    A = random.normal(3, 4, size=(m, n))
+    s = te.create_schedule(A.op)
 
     def verify(target="llvm"):
         if not tvm.testing.device_enabled(target):
@@ -91,12 +92,12 @@ def test_normal():
             print("skip because extern function is not available")
             return
         dev = tvm.cpu(0)
-        f = tvm.build(s, [input_a], target)
-        a = tvm.nd.array(np.zeros((m, n), dtype=input_a.dtype), dev)
+        f = tvm.build(s, [A], target)
+        a = tvm.nd.array(np.zeros((m, n), dtype=A.dtype), dev)
         f(a)
-        _na = a.numpy()
-        assert abs(np.mean(_na) - 3) < 1e-1
-        assert abs(np.std(_na) - 4) < 1e-2
+        na = a.numpy()
+        assert abs(np.mean(na) - 3) < 1e-1
+        assert abs(np.std(na) - 4) < 1e-2
 
     verify()
 
