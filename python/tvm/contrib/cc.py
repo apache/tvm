1fec2b39f308ca73e73697e62b0f982977da12f0
Yaxing Cai <caiyaxing666@gmail.com>
2023-11-30 22:48:17 +0000

    [Unity] Enable ccache for `nn.SourceModule`
    This PR is a mirror PR of #16176, and enables ccache in `nn.SourceModule` compilation.

--- python/tvm/contrib/cc.py
+++ python/tvm/contrib/cc.py
@@ -64,7 +64,7 @@ def get_cc():
     return None
 
 
-def create_shared(output, objects, options=None, cc=None):
+def create_shared(output, objects, options=None, cc=None, cwd=None, ccache_env=None):
     """Create shared library.
 
     Parameters
@@ -80,13 +80,19 @@ def create_shared(output, objects, options=None, cc=None):
 
     cc : Optional[str]
         The compiler command.
+
+    cwd : Optional[str]
+        The urrent working directory.
+
+    ccache_env : Optional[Dict[str, str]]
+        The environment variable for ccache. Set `None` to disable ccache by default.
     """
     cc = cc or get_cc()
 
     if _is_linux_like():
-        _linux_compile(output, objects, options, cc, compile_shared=True)
+        _linux_compile(output, objects, options, cc, cwd, ccache_env, compile_shared=True)
     elif _is_windows_like():
-        _windows_compile(output, objects, options)
+        _windows_compile(output, objects, options, cwd, ccache_env)
     else:
         raise ValueError("Unsupported platform")
 
@@ -139,7 +145,7 @@ def create_staticlib(output, inputs, ar=None):
         raise ValueError("Unsupported platform")
 
 
-def create_executable(output, objects, options=None, cc=None):
+def create_executable(output, objects, options=None, cc=None, cwd=None, ccache_env=None):
     """Create executable binary.
 
     Parameters
@@ -155,13 +161,19 @@ def create_executable(output, objects, options=None, cc=None):
 
     cc : Optional[str]
         The compiler command.
+
+    cwd : Optional[str]
+        The urrent working directory.
+
+    ccache_env : Optional[Dict[str, str]]
+        The environment variable for ccache. Set `None` to disable ccache by default.
     """
     cc = cc or get_cc()
 
     if _is_linux_like():
-        _linux_compile(output, objects, options, cc)
+        _linux_compile(output, objects, options, cc, cwd, ccache_env)
     elif _is_windows_like():
-        _windows_compile(output, objects, options)
+        _windows_compile(output, objects, options, cwd, ccache_env)
     else:
         raise ValueError("Unsupported platform")
 
@@ -275,7 +287,9 @@ def cross_compiler(
     return _fcompile
 
 
-def _linux_compile(output, objects, options, compile_cmd, compile_shared=False):
+def _linux_compile(
+    output, objects, options, compile_cmd, cwd=None, ccache_env=None, compile_shared=False
+):
     cmd = [compile_cmd]
     if compile_cmd != "nvcc":
         if compile_shared or output.endswith(".so") or output.endswith(".dylib"):
@@ -294,7 +308,15 @@ def _linux_compile(output, objects, options, compile_cmd, compile_shared=False):
         cmd += objects
     if options:
         cmd += options
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    env = None
+    if ccache_env is not None:
+        if shutil.which("ccache"):
+            cmd.insert(0, "ccache")
+            env = os.environ.copy()
+            env.update(ccache_env)
+        else:
+            raise ValueError("ccache not found")
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd, env=env)
     (out, _) = proc.communicate()
     if proc.returncode != 0:
         msg = "Compilation error:\n"
@@ -303,7 +325,7 @@ def _linux_compile(output, objects, options, compile_cmd, compile_shared=False):
         raise RuntimeError(msg)
 
 
-def _windows_compile(output, objects, options):
+def _windows_compile(output, objects, options, cwd=None, ccache_env=None):
     cmd = ["clang"]
     cmd += ["-O2"]
 
@@ -318,9 +340,19 @@ def _windows_compile(output, objects, options):
     cmd += objects
     if options:
         cmd += options
+    env = None
+    if ccache_env is not None:
+        if shutil.which("ccache"):
+            cmd.insert(0, "ccache")
+            env = os.environ.copy()
+            env.update(ccache_env)
+        else:
+            raise ValueError("ccache not found")
 
     try:
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd, env=env
+        )
         (out, _) = proc.communicate()
     except FileNotFoundError:
         raise RuntimeError(
