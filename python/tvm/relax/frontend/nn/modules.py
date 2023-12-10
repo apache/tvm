8256ee0bc7af706cb4146fb66cced5dab34af2ed
Charlie Ruan <charlieruan2001130@gmail.com>
2023-12-10 18:15:50 -0500

    Support out dtype for multiLinear as well

--- python/tvm/relax/frontend/nn/modules.py
+++ python/tvm/relax/frontend/nn/modules.py
@@ -192,7 +192,9 @@ class MultiLinear(Module):
         self.out_dtype = out_dtype
         self.weight = Parameter((total_out_features, in_features), dtype)
         if bias:
-            self.bias = Parameter((total_out_features,), dtype)
+            self.bias = Parameter(
+                (total_out_features,), dtype=dtype if out_dtype is None else out_dtype
+            )
         else:
             self.bias = None
 
@@ -220,6 +222,18 @@ class MultiLinear(Module):
             x = x + self.bias
         results = op.split(x, sections, axis=-1)
         return results
+
+    def to(self, dtype: Optional[str] = None) -> None:
+        """
+        Override to() such that we do not convert bias if there is `out_dtype`.
+        Otherwise, we might run into dtype mismatch when computing `x + self.bias`
+        since x is of type `out_dtype` and bias may become `dtype`.
+        """
+        self.weight.to(dtype=dtype)
+        if self.bias is not None and self.out_dtype is None:
+            self.bias.to(dtype=dtype)
+        if dtype is not None and isinstance(getattr(self, "dtype", None), str):
+            self.dtype = dtype  # pylint: disable=attribute-defined-outside-init
 
 
 class Conv1D(Module):
