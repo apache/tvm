b5b980e33a6af22793279d5bb85cc45c2b525799
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2023-12-10 22:33:06 -0500

    [Unity] Support out dtype for nn.Linear and nn.MultiLinear (#16220)

--- python/tvm/relax/frontend/nn/modules.py
+++ python/tvm/relax/frontend/nn/modules.py
@@ -132,7 +132,7 @@ class Linear(Module):
         self.out_dtype = out_dtype
         self.weight = Parameter((out_features, in_features), dtype)
         if bias:
-            self.bias = Parameter((out_features,), dtype)
+            self.bias = Parameter((out_features,), dtype=dtype if out_dtype is None else out_dtype)
         else:
             self.bias = None
 
@@ -159,6 +159,18 @@ class Linear(Module):
             x = x + self.bias
         return x
 
+    def to(self, dtype: Optional[str] = None) -> None:
+        """
+        Override to() such that we do not convert bias if there is `out_dtype`.
+        Otherwise, we might run into dtype mismatch when computing `x + self.bias`
+        since x is of type `out_dtype` and bias becomes `dtype`, potentially different.
+        """
+        self.weight.to(dtype=dtype)
+        if self.bias is not None and self.out_dtype is None:
+            self.bias.to(dtype=dtype)
+        if dtype is not None and isinstance(getattr(self, "dtype", None), str):
+            self.dtype = dtype  # pylint: disable=attribute-defined-outside-init
+
 
 class MultiLinear(Module):
     """A layer that applies multiple linear transformations to the input."""
@@ -180,7 +192,9 @@ class MultiLinear(Module):
         self.out_dtype = out_dtype
         self.weight = Parameter((total_out_features, in_features), dtype)
         if bias:
-            self.bias = Parameter((total_out_features,), dtype)
+            self.bias = Parameter(
+                (total_out_features,), dtype=dtype if out_dtype is None else out_dtype
+            )
         else:
             self.bias = None
 
@@ -208,6 +222,18 @@ class MultiLinear(Module):
             x = x + self.bias
         results = op.split(x, sections, axis=-1)
         return results
+
+    def to(self, dtype: Optional[str] = None) -> None:
+        """
+        Override to() such that we do not convert bias if there is `out_dtype`.
+        Otherwise, we might run into dtype mismatch when computing `x + self.bias`
+        since x is of type `out_dtype` and bias becomes `dtype`, potentially different.
+        """
+        self.weight.to(dtype=dtype)
+        if self.bias is not None and self.out_dtype is None:
+            self.bias.to(dtype=dtype)
+        if dtype is not None and isinstance(getattr(self, "dtype", None), str):
+            self.dtype = dtype  # pylint: disable=attribute-defined-outside-init
 
 
 class Conv1D(Module):
