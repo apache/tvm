0ef00cb3388fc71fa8443e7900b170a78144610a
Eric Lunderberg <elunderberg@octoml.ai>
2023-12-15 15:09:15 -0600

    Revert #16236
    The buf reported in #16237 can be resolved by tracking variable usage
    in a thread extent.

--- src/tir/transforms/ir_utils.cc
+++ src/tir/transforms/ir_utils.cc
@@ -750,13 +750,8 @@ Pass ConvertSSA() {
     tir::IRConvertSSA converter;
     Map<GlobalVar, BaseFunc> functions;
     bool made_change = false;
-    // FIXME: This is just a temporal workaround to ensure free vars
-    // in device function have the same pointer as the host function
     for (auto [gvar, base_func] : mod->functions) {
       if (auto* ptr = base_func.as<tir::PrimFuncNode>()) {
-        if (!ptr->HasNonzeroAttr(tir::attr::kIsEntryFunc)) {
-          continue;
-        }
         auto updated = converter.VisitPrimFunc(GetRef<tir::PrimFunc>(ptr));
         if (!updated.same_as(base_func)) {
           made_change = true;
@@ -764,19 +759,6 @@ Pass ConvertSSA() {
         }
       }
       functions.Set(gvar, base_func);
-    }
-    for (auto [gvar, base_func] : mod->functions) {
-      if (auto* ptr = base_func.as<tir::PrimFuncNode>()) {
-        if (ptr->HasNonzeroAttr(tir::attr::kIsEntryFunc)) {
-          continue;
-        }
-        auto updated = converter.VisitPrimFunc(GetRef<tir::PrimFunc>(ptr));
-        if (!updated.same_as(base_func)) {
-          made_change = true;
-          base_func = updated;
-        }
-        functions.Set(gvar, base_func);
-      }
     }
     if (made_change) {
       mod.CopyOnWrite()->functions = std::move(functions);
