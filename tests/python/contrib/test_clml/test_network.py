3a57a40c1ba40e1c330346905f8db72775fc9992
krishnaraj36 <quic_kvegiraj@quicinc.com>
2023-12-20 13:50:00 +0530

    [RUNTIME][CLML] Fix for CLML ops and enable more test case (#15896)
    * [RUNTIME][CLML] Fix for few clml ops
    
    Fixed the dense operator and enhance clml network testcase
    
    * [RUNTIME][CLML] Fix for dense layer and float16
    
    Fixed the dense layer issue in network level and improved
    converage of dense layer with clml
    Fixed float16 crash error.
    
    * Update comment for dense pattern
    
    * fix in clml test cases
    
    * Enable more test cases and few fixes
    
    * Fix the import error
    
    * Fix the import error
    
    * Fix in batchnorm testcase
    
    * Restructure clml test case and enable vm executor
    
    * Fix the import error in clml test network
    
    * Fix the test failure for vm tests
    
    * Update clml.py

--- tests/python/contrib/test_clml/test_network.py
+++ tests/python/contrib/test_clml/test_network.py
@@ -21,158 +21,137 @@ import numpy as np
 from tvm import relay
 from tvm.relay import testing
 from tvm.contrib import utils
-from test_clml.infrastructure import build_and_run, Device
+from test_clml.infrastructure import build_and_run, build_and_run_vm
 import pytest
 
 
-def _build_and_run_network(mod, params, inputs, data, device, atol, rtol, tvm_log=""):
+def _build_and_run_network(remote, mod, params, input_data, target, executor_type, tvm_log=""):
     """Helper function to build and run a network."""
 
     outputs = []
     for clml in [True, False]:
-        outputs.append(
-            build_and_run(mod, data, 1, params, device, enable_clml=clml, tune_log=tvm_log)[0][0]
-        )
+        if executor_type == "ge":
+            outputs.append(
+                build_and_run(
+                    remote,
+                    mod,
+                    params,
+                    input_data,
+                    target,
+                    enable_clml=clml,
+                    stat_file=tvm_log,
+                )
+            )
+        else:
+            outputs.append(
+                build_and_run_vm(
+                    remote,
+                    mod,
+                    params,
+                    input_data,
+                    target,
+                    enable_clml=clml,
+                    stat_file=tvm_log,
+                )
+            )
     return outputs
 
 
-def _get_keras_model(keras_model, inputs_dict, data):
-    """Convert Keras graph to relay."""
-    inputs = {}
-    for name, (shape, _) in inputs_dict.items():
-        inputs[keras_model.input_names[0]] = shape
+def get_network(name, batch_size, dtype="float32"):
+    """Get the symbol definition and random weight of a network
 
-    from tensorflow.keras.layers import Input
-    from tensorflow.keras.models import Model
+    Parameters
+    ----------
+    name: str
+        The name of the network, can be 'resnet-18', 'resnet-50', 'vgg-16', 'inception_v3', 'mobilenet', ...
+    batch_size: int
+        batch size
+    dtype: str
+        Data type
 
-    def get_bottom_top_model(model, layer_name):
-        layer = model.get_layer(layer_name)
-        bottom_input = model.layers[0].input
-        bottom_output = layer.output
-        bottom_model = Model(bottom_input, bottom_output)
-        return bottom_model
+    Returns
+    -------
+    net: tvm.IRModule
+        The relay function of network definition
+    params: dict
+        The random parameters for benchmark
+    input_shape: tuple
+        The shape of input tensor
+    output_shape: tuple
+        The shape of output tensor
+    """
+    input_shape = (batch_size, 3, 224, 224)
+    output_shape = (batch_size, 1000)
 
-    keras_model = get_bottom_top_model(keras_model, "predictions")
-    ref_output = keras_model.predict(data["input_1"].transpose(0, 2, 3, 1))
-
-    mod, params = relay.frontend.from_keras(keras_model, inputs, layout="NCHW")
-    return mod, params, ref_output
-
-
-@pytest.mark.parametrize("dtype", ["float16"])
-@tvm.testing.requires_openclml
-def test_mobilenet(device, dtype):
-    def get_model():
-        from tensorflow.keras.applications import MobileNet
-        import tensorflow as tf
-
-        tf.keras.backend.clear_session()
-
-        mobilenet = MobileNet(
-            include_top=True, weights=None, input_shape=(224, 224, 3), classes=1000
+    if name == "mobilenet":
+        net, params = testing.mobilenet.get_workload(batch_size=batch_size, dtype=dtype)
+    elif name == "inception_v3":
+        input_shape = (batch_size, 3, 299, 299)
+        net, params = testing.inception_v3.get_workload(batch_size=batch_size, dtype=dtype)
+    elif "resnet" in name:
+        n_layer = int(name.split("-")[1])
+        net, params = testing.resnet.get_workload(
+            num_layers=n_layer, batch_size=batch_size, dtype=dtype
         )
-        inputs = {mobilenet.input_names[0]: ((1, 3, 224, 224), "float32")}
-
-        data = {}
-        np.random.seed(0)
-
-        for name, (shape, dtype) in inputs.items():
-            if dtype == "uint8":
-                low, high = 0, 1
-            else:
-                low, high = -1, 1
-            data[name] = np.random.uniform(low, high, shape).astype(dtype)
-
-        mod, params, ref_outputs = _get_keras_model(mobilenet, inputs, data)
-        return mod, params, inputs, data, ref_outputs
-
-    mod, params, inputs, input_data, ref_outputs = get_model()
-    outputs = _build_and_run_network(
-        mod, params, inputs, input_data, device=device, atol=1e-5, rtol=1e-5
-    )
-
-    opencl_sort = np.argsort(outputs[1].asnumpy()).flatten()
-    clml_sort = np.argsort(outputs[0].asnumpy()).flatten()
-    tvm.testing.assert_allclose(opencl_sort[:10], clml_sort[:10], rtol=1e-5, atol=1e-5)
-
-
-@pytest.mark.parametrize("dtype", ["float16"])
-@tvm.testing.requires_openclml
-def test_inception_v3(device, dtype):
-    def get_model():
-        from tensorflow.keras.applications import InceptionV3
-        import tensorflow as tf
-
-        tf.keras.backend.clear_session()
-
-        inceptionV3 = InceptionV3(
-            include_top=True, weights=None, input_shape=(299, 299, 3), classes=1000
+    elif "vgg" in name:
+        n_layer = int(name.split("-")[1])
+        net, params = testing.vgg.get_workload(
+            num_layers=n_layer, batch_size=batch_size, dtype=dtype
         )
-        inputs = {inceptionV3.input_names[0]: ((1, 3, 299, 299), "float16")}
+    elif "densenet" in name:
+        n_layer = int(name.split("-")[1])
+        net, params = testing.densenet.get_workload(
+            densenet_size=n_layer, batch_size=batch_size, dtype=dtype
+        )
+    elif "squeezenet" in name:
+        version = name.split("_v")[1]
+        net, params = testing.squeezenet.get_workload(
+            batch_size=batch_size, version=version, dtype=dtype
+        )
+    else:
+        raise ValueError("Unsupported network: " + name)
 
-        data = {}
-        np.random.seed(0)
-        for name, (shape, dtype) in inputs.items():
-            if dtype == "uint8":
-                low, high = 0, 1
-            else:
-                low, high = -2, 1
-            data[name] = np.random.uniform(low, high, shape).astype(dtype)
+    initializer = relay.testing.init.Xavier()
+    for param_name in list(params.keys()):
+        filter_data = np.zeros(params[param_name].shape).astype(params[param_name].dtype)
+        if len(filter_data.shape) > 1:
+            initializer("weight", filter_data)
+        else:
+            initializer("bias", filter_data)
+        params[param_name] = tvm.nd.array(filter_data)
 
-        mod, params, ref_outputs = _get_keras_model(inceptionV3, inputs, data)
-        return mod, params, inputs, data, ref_outputs
-
-    mod, params, inputs, input_data, ref_outputs = get_model()
-    outputs = _build_and_run_network(
-        mod, params, inputs, input_data, device=device, atol=1e-5, rtol=1e-5
-    )
-
-    opencl_sort = np.argsort(outputs[1].asnumpy()).flatten()
-    clml_sort = np.argsort(outputs[0].asnumpy()).flatten()
-    tvm.testing.assert_allclose(opencl_sort[:5], clml_sort[:5], rtol=1e-5, atol=1e-5)
+    return net, params, {"data": (input_shape, dtype)}, output_shape
 
 
-@pytest.mark.parametrize("dtype", ["float16"])
+executor_type = tvm.testing.parameter("ge", "vm")
+
+
+@pytest.mark.parametrize("dtype", ["float32", "float16"])
+@pytest.mark.parametrize(
+    "name",
+    [
+        "resnet-18",
+        "resnet-34",
+        "resnet-50",
+        "inception_v3",
+        "mobilenet",
+    ],
+)
 @tvm.testing.requires_openclml
-def test_resnet50v2(device, dtype):
-    def get_model():
-        from tensorflow.keras.applications import ResNet50V2
-        import tensorflow as tf
+@tvm.testing.parametrize_targets("opencl -device=adreno")
+def test_network(remote, name, dtype, target, executor_type):
+    print("Network evaluating .. " + name + " " + dtype)
+    np.random.seed(0)
+    mod, params, inputs, _ = get_network(name, 1, dtype=dtype)
+    input_data = {}
 
-        tf.keras.backend.clear_session()
+    for name, (shape, dtype) in inputs.items():
+        input_data[name] = np.random.uniform(-1.0, 1.0, shape).astype(dtype)
 
-        model = ResNet50V2(include_top=True, weights=None, input_shape=(224, 224, 3), classes=1000)
-        inputs_dict = {model.input_names[0]: ((1, 3, 224, 224), "float32")}
-
-        data = {}
-        np.random.seed(0)
-
-        for name, (shape, dtype) in inputs_dict.items():
-            if dtype == "uint8":
-                low, high = 0, 1
-            else:
-                low, high = -1, 1
-            data[name] = np.random.uniform(low, high, shape).astype(dtype)
-
-        """Convert Keras graph to relay."""
-        inputs = {}
-        for name, (shape, _) in inputs_dict.items():
-            inputs[model.input_names[0]] = shape
-
-        ref_outputs = model.predict(data["input_1"].transpose(0, 2, 3, 1))
-
-        mod, params = relay.frontend.from_keras(model, inputs, layout="NCHW")
-
-        return mod, params, inputs, data, ref_outputs
-
-    mod, params, inputs, input_data, ref_outputs = get_model()
-    outputs = _build_and_run_network(
-        mod, params, inputs, input_data, device=device, atol=1e-5, rtol=1e-5
-    )
-
+    outputs = _build_and_run_network(remote, mod, params, input_data, target, executor_type)
     opencl_sort = np.argsort(outputs[1].asnumpy()).flatten()
     clml_sort = np.argsort(outputs[0].asnumpy()).flatten()
-    tvm.testing.assert_allclose(opencl_sort[:10], clml_sort[:10], rtol=1e-5, atol=1e-5)
+    tvm.testing.assert_allclose(opencl_sort[-5:], clml_sort[-5:], rtol=0, atol=0)
 
 
 if __name__ == "__main__":
