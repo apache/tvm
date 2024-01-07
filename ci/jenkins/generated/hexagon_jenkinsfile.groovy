9caa179ce6937183519b87f4fbfb4895b9113f4d
Wuwei Lin <wuwei@apache.org>
2024-01-07 13:22:24 -0700

    [CI] Update docker images tag to 20240105-165030-51bdaec6 (#16344)
    * [CI] Update docker images tag to 20240103-070130-97f6e6507
    
    * Update images tag to 20240105-165030-51bdaec6
    
    * Show cuda info in ci runner
    
    * [CI] Set NVIDIA_DISABLE_REQUIRE=true

--- ci/jenkins/generated/hexagon_jenkinsfile.groovy
+++ ci/jenkins/generated/hexagon_jenkinsfile.groovy
@@ -60,7 +60,7 @@
 // 'python3 jenkins/generate.py'
 // Note: This timestamp is here to ensure that updates to the Jenkinsfile are
 // always rebased on main before merging:
-// Generated at 2023-06-09T15:32:58.328389
+// Generated at 2024-01-06T14:50:09.557404
 
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 // These are set at runtime from data in ci/jenkins/docker-images.yml, update
@@ -112,7 +112,7 @@ properties([
 upstream_revision = null
 
 // command to start a docker container
-docker_run = 'docker/bash.sh --env CI --env PLATFORM --env TVM_SHARD_INDEX --env TVM_NUM_SHARDS --env RUN_DISPLAY_URL --env PLATFORM --env SKIP_SLOW_TESTS --env TEST_STEP_NAME'
+docker_run = 'docker/bash.sh --env CI --env PLATFORM --env TVM_SHARD_INDEX --env TVM_NUM_SHARDS --env RUN_DISPLAY_URL --env PLATFORM --env SKIP_SLOW_TESTS --env TEST_STEP_NAME --env NVIDIA_DISABLE_REQUIRE=true'
 docker_build = 'docker/build.sh'
 // timeout in minutes
 max_time = 180
