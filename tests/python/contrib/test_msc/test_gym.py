b6111bf8d4b8fc86333b3dda7887ac6ca3b6d61b
Archermmt <archermmt@126.com>
2024-01-08 21:02:42 +0800

    add gym test

---
+++ tests/python/contrib/test_msc/test_gym.py
@@ -0,0 +1,273 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+""" Test Gym in MSC. """
+
+import json
+import pytest
+import torch
+
+import tvm.testing
+from tvm.contrib.msc.pipeline import MSCManager
+from tvm.contrib.msc.core.tools import ToolType
+from tvm.contrib.msc.core.utils.namespace import MSCFramework
+from tvm.contrib.msc.core import utils as msc_utils
+
+requires_tensorrt = pytest.mark.skipif(
+    tvm.get_global_func("relax.ext.tensorrt", True) is None,
+    reason="TENSORRT is not enabled",
+)
+
+
+def _get_config(
+    model_type,
+    compile_type,
+    tools_config,
+    inputs,
+    outputs,
+    atol=1e-2,
+    rtol=1e-2,
+    optimize_type=None,
+):
+    """Get msc config"""
+
+    path = "_".join(["test_gym", model_type, compile_type] + list(tools_config.keys()))
+    return {
+        "workspace": msc_utils.msc_dir(path),
+        "verbose": "critical",
+        "model_type": model_type,
+        "inputs": inputs,
+        "outputs": outputs,
+        "dataset": {"loader": "from_random", "max_iter": 5},
+        "prepare": {"profile": {"benchmark": {"repeat": 10}}},
+        "baseline": {
+            "run_type": model_type,
+            "profile": {"check": {"atol": atol, "rtol": rtol}, "benchmark": {"repeat": 10}},
+        },
+        "optimize": {
+            "run_type": optimize_type or model_type,
+            "profile": {"check": {"atol": atol, "rtol": rtol}, "benchmark": {"repeat": 10}},
+            **tools_config,
+        },
+        "compile": {
+            "run_type": compile_type,
+            "profile": {"check": {"atol": atol, "rtol": rtol}, "benchmark": {"repeat": 10}},
+        },
+    }
+
+
+def get_tool_config(tool_type):
+    """Get config for the tool"""
+
+    config = {}
+    if tool_type == ToolType.PRUNER:
+        config = {
+            "plan_file": "msc_pruner.json",
+            "strategys": [{"method": "per_channel", "density": 0.8}],
+            "gym_configs": [
+                {
+                    "env": {
+                        "executors": {
+                            "action_space": {
+                                "method": "action_prune_density",
+                                "start": 0.4,
+                                "end": 0.8,
+                                "step": 0.4,
+                            }
+                        },
+                        "max_tasks": 3,
+                    },
+                    "agent": {"agent_type": "search.grid", "executors": {}},
+                }
+            ],
+        }
+    elif tool_type == ToolType.QUANTIZER:
+        # pylint: disable=import-outside-toplevel
+        from tvm.contrib.msc.core.tools.quantize import QuantizeStage
+
+        config = {
+            "plan_file": "msc_quantizer.json",
+            "strategys": [
+                {
+                    "method": "gather_maxmin",
+                    "op_types": ["nn.conv2d", "msc.linear"],
+                    "tensor_types": ["input", "output"],
+                    "stages": [QuantizeStage.GATHER],
+                },
+                {
+                    "method": "gather_max_per_channel",
+                    "op_types": ["nn.conv2d", "msc.linear"],
+                    "tensor_types": ["weight"],
+                    "stages": [QuantizeStage.GATHER],
+                },
+                {
+                    "method": "calibrate_maxmin",
+                    "op_types": ["nn.conv2d", "msc.linear"],
+                    "tensor_types": ["input", "output"],
+                    "stages": [QuantizeStage.CALIBRATE],
+                },
+                {
+                    "method": "quantize_normal",
+                    "op_types": ["nn.conv2d", "msc.linear"],
+                    "tensor_types": ["input", "weight"],
+                },
+                {
+                    "method": "dequantize_normal",
+                    "op_types": ["nn.conv2d", "msc.linear"],
+                    "tensor_types": ["output"],
+                },
+            ],
+            "gym_configs": [
+                {
+                    "env": {
+                        "executors": {
+                            "action_space": {
+                                "method": "action_quantize_scale",
+                                "start": 0.8,
+                                "end": 1.2,
+                                "step": 0.2,
+                            }
+                        },
+                        "max_tasks": 3,
+                    },
+                    "agent": {"agent_type": "search.grid", "executors": {}},
+                }
+            ],
+        }
+    return {tool_type: config}
+
+
+def _get_torch_model(name, is_training=False):
+    """Get model from torch vision"""
+
+    # pylint: disable=import-outside-toplevel
+    try:
+        import torchvision
+
+        model = getattr(torchvision.models, name)()
+        if is_training:
+            model = model.train()
+        else:
+            model = model.eval()
+        return model
+    except:  # pylint: disable=bare-except
+        print("please install torchvision package")
+        return None
+
+
+def _check_manager(manager, expected_info):
+    """Check the manager results"""
+
+    model_info = manager.runner.model_info
+    passed, err = True, ""
+    if not manager.report["success"]:
+        passed = False
+        err = "Failed to run pipe for {} -> {}".format(manager.model_type, manager.compile_type)
+    if not msc_utils.dict_equal(model_info, expected_info):
+        passed = False
+        err = "Model info {} mismatch with expected {}".format(model_info, expected_info)
+    manager.destory()
+    if not passed:
+        raise Exception("{}\nReport:{}".format(err, json.dumps(manager.report, indent=2)))
+
+
+def _test_from_torch(
+    compile_type,
+    tools_config,
+    expected_info,
+    is_training=False,
+    atol=1e-1,
+    rtol=1e-1,
+    optimize_type=None,
+):
+    torch_model = _get_torch_model("resnet50", is_training)
+    if torch_model:
+        if torch.cuda.is_available():
+            torch_model = torch_model.to(torch.device("cuda:0"))
+        config = _get_config(
+            MSCFramework.TORCH,
+            compile_type,
+            tools_config,
+            inputs=[["input_0", [1, 3, 224, 224], "float32"]],
+            outputs=["output"],
+            atol=atol,
+            rtol=rtol,
+            optimize_type=optimize_type,
+        )
+        manager = MSCManager(torch_model, config)
+        manager.run_pipe()
+        _check_manager(manager, expected_info)
+
+
+def get_model_info(compile_type):
+    """Get the model info"""
+
+    if compile_type == MSCFramework.TVM:
+        return {
+            "inputs": [
+                {"name": "input_0", "shape": [1, 3, 224, 224], "dtype": "float32", "layout": "NCHW"}
+            ],
+            "outputs": [{"name": "output", "shape": [1, 1000], "dtype": "float32", "layout": "NC"}],
+            "nodes": {
+                "total": 229,
+                "input": 1,
+                "nn.conv2d": 53,
+                "nn.batch_norm": 53,
+                "get_item": 53,
+                "nn.relu": 49,
+                "nn.max_pool2d": 1,
+                "add": 16,
+                "nn.adaptive_avg_pool2d": 1,
+                "reshape": 1,
+                "msc.linear_bias": 1,
+            },
+        }
+    if compile_type == MSCFramework.TENSORRT:
+        return {
+            "inputs": [
+                {"name": "input_0", "shape": [1, 3, 224, 224], "dtype": "float32", "layout": "NCHW"}
+            ],
+            "outputs": [{"name": "output", "shape": [1, 1000], "dtype": "float32", "layout": ""}],
+            "nodes": {"total": 2, "input": 1, "msc_tensorrt": 1},
+        }
+    raise TypeError("Unexpected compile_type " + str(compile_type))
+
+
+@tvm.testing.requires_cuda
+@pytest.mark.parametrize("tool_type", [ToolType.PRUNER, ToolType.QUANTIZER])
+def test_tvm_gym(tool_type):
+    """Test tools for tvm with gym"""
+
+    tool_config = get_tool_config(tool_type)
+    _test_from_torch(
+        MSCFramework.TVM, tool_config, get_model_info(MSCFramework.TVM), is_training=True
+    )
+
+
+@requires_tensorrt
+@pytest.mark.parametrize("tool_type", [ToolType.PRUNER])
+def test_tensorrt_gym(tool_type):
+    """Test tools for tensorrt with gym"""
+
+    tool_config = get_tool_config(tool_type)
+    _test_from_torch(
+        MSCFramework.TENSORRT, tool_config, get_model_info(MSCFramework.TENSORRT), is_training=False
+    )
+
+
+if __name__ == "__main__":
+    tvm.testing.main()
