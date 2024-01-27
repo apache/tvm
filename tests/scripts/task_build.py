a15fd1d6a21e9bedb622ffb9fd025c50624d734d
Yong Wu <yongcale@gmail.com>
2024-01-27 09:43:45 -0800

    [CI] Update image tag to 20240126-070121-8ade9c30e (#16435)
    * [CI] update image tag to 20240119-070113-614a7a9e3
    
    * Add sccache region
    
    * add default sccache region
    
    * Use local cache if no aws credential found for sccache bucket
    
    * Use newer images 20240126-070121-8ade9c30e

--- tests/scripts/task_build.py
+++ tests/scripts/task_build.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="List pytest nodeids for a folder")
     parser.add_argument("--sccache-bucket", required=False, help="sccache bucket name")
+    parser.add_argument("--sccache-region", required=False, help="sccache region")
     parser.add_argument("--build-dir", default="build", help="build folder")
     parser.add_argument("--cmake-target", help="optional build target")
     args = parser.parse_args()
@@ -46,9 +47,11 @@ if __name__ == "__main__":
     build_dir = build_dir.relative_to(REPO_ROOT)
 
     if use_sccache:
-        if args.sccache_bucket:
+        if args.sccache_bucket and "AWS_ACCESS_KEY_ID" in os.environ:
             env["SCCACHE_BUCKET"] = args.sccache_bucket
+            env["SCCACHE_REGION"] = args.sccache_region if args.sccache_region else "us-west-2"
             logging.info(f"Using sccache bucket: {args.sccache_bucket}")
+            logging.info(f"Using sccache region: {env['SCCACHE_REGION']}")
         else:
             logging.info(f"No sccache bucket set, using local cache")
         env["CXX"] = "/opt/sccache/c++"
