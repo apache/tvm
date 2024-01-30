46a0b232954f029ebba2453c5ac4334ff120d871
LeiWang <leiwang1999@outlook.com>
2024-01-30 08:43:07 -0400

    refactor multi thread to multi proc tuning

--- python/tvm/dlight/base/utils.py
+++ python/tvm/dlight/base/utils.py
@@ -16,20 +16,48 @@
 # under the License.
 import tvm
 import os
+from tvm.contrib.popen_pool import PopenPoolExecutor, StatusKind, MapResult
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import numpy as np
 from typing import List, Tuple, Optional
-from tvm import tir
+from tvm import tir, IRModule
+from tvm.runtime import Module
+from tvm.tir import Schedule
 from tvm import dlight as dl
 from .analysis import get_root_block, get_reduction_blocks
+from .roller.arch import Arch
+from ..base.roller.rasterization import NoRasterization
+import tempfile
+import re
 
+def match_global_kernel(source: str) -> int:
+    pattern = r"__global__\s+void\s+[__launch_bounds__\(\d+\)\s+]\w+"
+    matched = re.findall(pattern, source)
+    assert len(matched) > 1 # may have statement before kernel
+    return source.index(matched[0])
 
+def get_rasterization_code(pannel_width:int = 8) -> str:
+    return f"""
+        const int MAX_BLOCK_N = {pannel_width};
+        const auto baseBlockIdx = blockIdx.x + gridDim.x *blockIdx.y;
+        const auto totalPanel = (gridDim.x * gridDim.y +MAX_BLOCK_N * gridDim.x - 1) / (MAX_BLOCK_N * gridDim.x);
+        const auto totalBlock = gridDim.x * gridDim.y;
+        const auto panelIdx = baseBlockIdx / (MAX_BLOCK_N *gridDim.x);
+        const auto strideLd = panelIdx + 1 < totalPanel ?MAX_BLOCK_N : (totalBlock - panelIdx * (MAX_BLOCK_N *gridDim.x)) / gridDim.x;
+        const auto bx = (panelIdx & 1) ? gridDim.x -(baseBlockIdx - panelIdx * MAX_BLOCK_N * gridDim.x) /strideLd - 1 : (baseBlockIdx - panelIdx * MAX_BLOCK_N *gridDim.x) / strideLd;
+        const auto by = (baseBlockIdx - panelIdx * MAX_BLOCK_N *gridDim.x) % strideLd + panelIdx * MAX_BLOCK_N;
+        const auto bz = blockIdx.z;
+        const dim3 blockIdx(bx, by, bz);
+    """
+    
+    ...
+    
 class CompileResult:
     """
     Class to store the result of compilation
     """
 
-    def __init__(self, config, sch, mod):
+    def __init__(self, config, sch, mod: Module):
         self.config = config
         self.sch = sch
         self.mod = mod
@@ -90,36 +118,9 @@ def _apply_config(
     return None
 
 
-def _apply_and_build(
-    func,
-    config,
-    arch,
-) -> Tuple[Optional[List[tir.Schedule]], Optional[tvm.IRModule], Optional[tvm.runtime.Module]]:
-    sch = _apply_config(func, config)
-
-    if sch is None:
-        return config, sch, None
-
-    # TODO(@lei): is tvm.build thread safe?
-    try:
-        with tvm.transform.PassContext(
-            config={"tir.use_async_copy": True, "tir.merge_static_smem": False}
-        ):
-            mod = tvm.build(sch.mod["main"], target=arch.target)
-    except Exception as e_msg:
-        print(f"build with config {config} failed ")
-        mod = None
-    return config, sch, mod
-
-
-def apply_and_build_parallel(
-    func,
-    configs,
-    arch,
-    num_repeats=5,
-) -> CompileResult:
+def apply_and_build_parallel(func, configs, arch, num_repeats=5, max_workers=10) -> CompileResult:
     cpresults = []
-    
+
     def var_warpper(v):
         if isinstance(v, tvm.tir.Var):
             assert v.name in config.opt_shapes
@@ -135,7 +136,9 @@ def apply_and_build_parallel(
         if arg.dtype == "int8":
             profile_tensors.append(
                 tvm.nd.array(
-                    np.random.randint(-127, 128, [var_warpper(i) for i in arg.shape]).astype(arg.dtype),
+                    np.random.randint(-127, 128, [var_warpper(i) for i in arg.shape]).astype(
+                        arg.dtype
+                    ),
                     device=arch.device,
                 )
             )
@@ -147,31 +150,74 @@ def apply_and_build_parallel(
                 )
             )
 
-    num_procs = min(len(configs), os.cpu_count(), 10)
-    with ThreadPoolExecutor(max_workers=num_procs) as executor:
-        all_tasks = []
-        for config in configs:
-            t = executor.submit(
-                _apply_and_build,
-                func,
-                config,
-                arch,
+    max_workers = min(len(configs), os.cpu_count(), max_workers)
+
+    # apply config in thread parallel
+    _sched: List[Schedule] = []
+    with ThreadPoolExecutor(max_workers=4) as schduler:
+        futures = {
+            schduler.submit(lambda f, c: _apply_config(f, c), func, config) for config in configs
+        }
+        for future in as_completed(futures):
+            _sched.append(future.result())
+
+    builder = PopenPoolExecutor(max_workers=max_workers)
+
+    # build in process parallel
+    def _build(context) -> str:
+        idx, mod, arch = context
+        config = configs[idx]
+        # TODO(lei):
+        # this is a trick to implement rasteration, will be removed in the future
+        @tvm.register_func(func_name="tvm_callback_cuda_postproc", override=True)
+        def tvm_callback_cuda_postproc(code, _):
+            index = code.index("{", match_global_kernel(code))
+            if not isinstance(config.rasterization_plan, NoRasterization):
+                factor = config.rasterization_plan.panel_width_
+                rasterization_code = get_rasterization_code(factor)
+                code = code[: index + 2] + rasterization_code + code[index + 2 :]
+            return code
+
+        with tvm.transform.PassContext(
+            config={"tir.use_async_copy": True, "tir.merge_static_smem": False}
+        ):
+            rt_mod = tvm.build(mod["main"], target=arch.target)
+
+        from tvm.contrib.tar import tar  # pylint: disable=import-outside-toplevel
+
+        artifact_path = os.path.join(tempfile.mkdtemp(), "tvm_tmp_mod." + tar.output_format)
+        code = rt_mod.imported_modules[0].get_source()
+        rt_mod.export_library(artifact_path, fcompile=tar)
+        return idx, code, artifact_path
+
+    for map_result in builder.map_with_error_catching(
+        _build,
+        [(i, sch.mod, arch) for i, sch in enumerate(_sched)],
+    ):
+        if map_result.status == StatusKind.TIMEOUT:
+            print("[FastDlight] LocalBuilder: Timeout")
+        elif map_result.status == StatusKind.EXCEPTION:
+            print("[FastDlight] LocalBuilder: An exception occurred ", map_result.value)
+            continue
+        elif map_result.status == StatusKind.COMPLETE:
+            idx, code, artifact_path = map_result.value
+            assert artifact_path is not None, "artifact_path is None"
+
+            sch = _sched[idx]
+            config = configs[idx]
+            rt_mod = tvm.runtime.load_module(artifact_path)
+            cpresult = CompileResult(config, sch, rt_mod)
+            timer_cuda_mod = rt_mod.time_evaluator(
+                rt_mod.entry_name, arch.device, number=num_repeats
             )
-            all_tasks.append(t)
+            cpresult.profile_tensors = profile_tensors
+            cpresult.time_evaluator = timer_cuda_mod
+            cpresult.code = code
+            cpresults.append(cpresult)
+        else:
+            raise ValueError(f"Unreachable: unexpected result: {map_result}")
 
-    for future in as_completed(all_tasks):
-        config, sch, mod = future.result()
-        if sch is None:
-            continue
-
-        if mod is None:
-            continue
-        cpresult = CompileResult(config, sch, mod)
-
-        timer_cuda_mod = mod.time_evaluator(mod.entry_name, arch.device, number=num_repeats)
-        cpresult.profile_tensors = profile_tensors
-        cpresult.time_evaluator = timer_cuda_mod
-        cpresults.append(cpresult)
+    del builder
 
     best = None
     best_latency = 1e9
@@ -199,60 +245,5 @@ def apply_and_build(
     arch,
     parallel_build=False,
 ) -> Tuple[List[CompileResult], CompileResult]:
-    if parallel_build:
-        return apply_and_build_parallel(func, configs, arch)
-    cpresults = []
-    for config in configs:
-        config, sch, mod = _apply_and_build(func, config, arch)
-
-        def var_warpper(v):
-            if isinstance(v, tvm.tir.Var):
-                print(config.opt_shapes)
-                assert v.name in config.opt_shapes
-                return config.opt_shapes[v.name]
-            elif isinstance(v, tvm.tir.IntImm):
-                return v.value
-            else:
-                raise RuntimeError("Not supported type: ", type(v))
-
-        cpresult = CompileResult(config, sch, mod)
-        profile_tensors = []
-        for param in func.params:
-            arg = func.buffer_map[param]
-            if arg.dtype == "int8":
-                profile_tensors.append(
-                    tvm.nd.array(
-                        np.random.randint(-127, 128, [var_warpper(i) for i in arg.shape]).astype(arg.dtype),
-                        device=arch.device,
-                    )
-                )
-            else:
-                profile_tensors.append(
-                    tvm.nd.array(
-                        np.random.uniform(0, 1, [var_warpper(i) for i in arg.shape]).astype(arg.dtype),
-                        device=arch.device,
-                    )
-                )
-        if mod:
-            timer_cuda_mod = mod.time_evaluator(mod.entry_name, arch.device, number=5)
-            cpresult.profile_tensors = profile_tensors
-            cpresult.time_evaluator = timer_cuda_mod
-            cpresults.append(cpresult)
-
-    best = None
-    best_latency = 1e9
-    for cpresult in cpresults:
-        config = cpresult.config
-        try:
-            latency = cpresult.profile()
-        except Exception as e_mesg:
-            print("[FastDlight] Evaluation with config failed: ", e_mesg)
-            continue
-        print("[FastDlight] Evaluation with config ", config)
-        print("[FastDlight] Time cost of this config: {:.3f} ms".format(latency * 1e3))
-        cpresult.latency = latency
-        if latency < best_latency:
-            best_latency = latency
-            best = cpresult
-
-    return cpresults, best
+    max_workers = 10 if parallel_build else 1
+    return apply_and_build_parallel(func, configs, arch, max_workers)
