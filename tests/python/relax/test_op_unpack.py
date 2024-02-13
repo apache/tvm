fa1ecb8f29e72fb5c8981f7b4393b25fc784fa37
Eric Lunderberg <elunderberg@octoml.ai>
2024-02-13 07:26:08 -0600

    [Relax] Implement operators to read runtime DLTensor* information
    Relax is capable of expressing tensors whose element type is unknown.
    However, these must typically be replaced with a known dtype prior to
    compilation, as most operators require known data types prior to
    legalization.  This can be done by using a `relax::MatchCast` node,
    such as accepting a parameter `arg: R.Tensor([16,16])`, then defining
    the dtype using `R.match_cast(arg, R.Tensor([16,16],'float16'))`.
    
    However, using a `R.match_cast` node requires knowing which data type
    should be used in the new `R.Tensor`, and raises an error for an
    incorrect data type.  If an argument may be one of two distinct data
    types, `R.match_cast` cannot be used to check which data type is in
    use.
    
    This commit adds Relax operators to read the runtime values of a
    `DLTensor*` argument.  These can be be used to normalize arguments
    prior to a compute step.  For example, pre-processing a model weight
    that may be provided in either `float16` or `bfloat16` format.

---
+++ tests/python/relax/test_op_unpack.py
@@ -0,0 +1,127 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import tvm.testing
+
+from tvm import relax
+from tvm.ir import Op
+from tvm.script import ir as I, relax as R
+
+# Parameterization for reading dtype of DLTensor.  Chosen to have
+# multiple distinct type codes, number of lanes, and widths.
+dtype = tvm.testing.parameter(
+    "int32",
+    "int64",
+    "float32",
+    "float32x4",
+    "bfloat",
+    "e4m3_float8",
+)
+shape = tvm.testing.parameter(
+    [],
+    [16],
+    [128, 256],
+    [1] * 64,
+)
+
+
+def test_tensor_dtype_code(dtype):
+    @I.ir_module
+    class mod:
+        @R.function
+        def main(A: R.Tensor):
+            return relax.Call(Op.get("relax.tensor_dtype_code"), [A])
+
+    built = relax.build(mod)
+    vm = relax.VirtualMachine(built, tvm.cpu())
+
+    arg = tvm.nd.empty([16], dtype)
+    res = vm["main"](arg)
+
+    expected_type_code = tvm.runtime.DataType(dtype).type_code
+    assert res == expected_type_code
+
+
+def test_tensor_dtype_bits(dtype):
+    @I.ir_module
+    class mod:
+        @R.function
+        def main(A: R.Tensor):
+            return relax.Call(Op.get("relax.tensor_dtype_bits"), [A])
+
+    built = relax.build(mod)
+    vm = relax.VirtualMachine(built, tvm.cpu())
+
+    arg = tvm.nd.empty([16], dtype)
+    res = vm["main"](arg)
+
+    expected_type_bits = tvm.runtime.DataType(dtype).bits
+    assert res == expected_type_bits
+
+
+def test_tensor_dtype_lanes(dtype):
+    @I.ir_module
+    class mod:
+        @R.function
+        def main(A: R.Tensor):
+            return relax.Call(Op.get("relax.tensor_dtype_lanes"), [A])
+
+    built = relax.build(mod)
+    vm = relax.VirtualMachine(built, tvm.cpu())
+
+    arg = tvm.nd.empty([16], dtype)
+    res = vm["main"](arg)
+
+    expected_type_lanes = tvm.runtime.DataType(dtype).lanes
+    assert res == expected_type_lanes
+
+
+def test_tensor_ndim(shape):
+    @I.ir_module
+    class mod:
+        @R.function
+        def main(A: R.Tensor):
+            return relax.Call(Op.get("relax.tensor_ndim"), [A])
+
+    built = relax.build(mod)
+    vm = relax.VirtualMachine(built, tvm.cpu())
+
+    arg = tvm.nd.empty(shape, "int32")
+    res = vm["main"](arg)
+
+    assert res == len(shape)
+
+
+def test_tensor_shape(shape):
+    @I.ir_module
+    class mod:
+        @R.function
+        def main(A: R.Tensor, axis: R.Prim("int64")):
+            return relax.Call(Op.get("relax.tensor_shape_i"), [A, axis])
+
+    built = relax.build(mod)
+    vm = relax.VirtualMachine(built, tvm.cpu())
+
+    arg = tvm.nd.empty(shape, "int32")
+
+    res = [vm["main"](arg, i) for i, _ in enumerate(shape)]
+
+    tvm.ir.assert_structural_equal(res, shape)
+
+
+if __name__ == "__main__":
+    tvm.testing.main()
