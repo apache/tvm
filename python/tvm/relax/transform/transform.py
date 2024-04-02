48efd3c5cbb2db177766b0da19889f865b956dc5
Chris Sullivan <csullivan@octo.ai>
2024-04-02 20:00:33 +0000

    [Relax] Add module pass tvm.relax.transform.CheckForSpecialCase

--- python/tvm/relax/transform/transform.py
+++ python/tvm/relax/transform/transform.py
@@ -694,6 +694,35 @@ def BindSymbolicVars(
     return _ffi_api.BindSymbolicVars(binding_map, func_name)  # type: ignore
 
 
+def CheckForSpecialCase(
+    special_case: Mapping[Union[str, tvm.tir.Var, Var], Union[tvm.tir.PrimExpr, Expr]],
+    func_name: Optional[str] = None,
+) -> tvm.ir.transform.Pass:
+    """Bind params of function of the module to constant tensors to produce a
+    special case
+
+    Parameters
+    ----------
+    special_case : Mapping[Union[str, tvm.tir.Var, Var], Union[tvm.tir.PrimExpr, Expr]],
+        The map from symbolic varname to integer.
+
+    func_name : Optional[str]
+        The function name to be special cased. If None (default), all
+        functions within the module will be updated.
+
+    Returns
+    -------
+    ret: tvm.ir.transform.Pass
+    """
+    # Relax uses int64 for symbolic variables, but the FFI
+    # converts python integers into int32.
+    special_case = {
+        key: tvm.tir.const(value, "int64") if isinstance(value, int) else value
+        for key, value in special_case.items()
+    }
+    return _ffi_api.CheckForSpecialCase(special_case, func_name)  # type: ignore
+
+
 def RunCodegen(
     target_options: Optional[dict] = None,
     entry_functions: Optional[List[str]] = None,
