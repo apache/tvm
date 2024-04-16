apache/tvm

commit 3e802d12f1270a5ee92088211db663df311bbaa6

[Relax,Topi] Allow passing workspace to thrust to avoid allocations (#16851)
* [Relax,Topi] Allow passing workspace to thrust to avoid allocations

python/tvm/relax/frontend/nn/op.py
@@ -2241,6 +2241,112 @@ def cumsum(
     return wrap_nested(_op.cumsum(data._expr, axis, dtype, exclusive), name)
 
 
+def sort(x: Tensor, axis: int = -1, descending: bool = False, name="sort"):
+    """Performs sorting along the given axis and returns an array
+    in sorted order.
+
+    Parameters
+    ----------
+    x : Tensor
+        The input tensor.
+
+    axis : int
+        Axis along which to sort the input tensor.
+        By default the last axis of the input is used.
+
+    descending : bool
+        Whether to sort in descending order, the default is False
+
+    name : str
+        Name hint.
+
+    Returns
+    -------
+    out : Tensor
+        The sorted tensor.
+    """
+    return wrap_nested(_op.sort(x, axis, descending), name=name)
+
+
+def argsort(
+    data: Tensor, axis: int = -1, descending: bool = False, dtype: str = "int32", name="argsort"
+):
+    """Performs sorting along the given axis and returns an array of indices
+    having same shape as an input array that index data in sorted order.
+
+    Parameters
+    ----------
+    data : Tensor
+        The input data tensor.
+
+    axis : int
+        Axis long which to sort the input tensor.
+
+    descending : bool
+        Whether to sort in descending order, the default is False
+
+    dtype : str
+        The data type of the output indices.
+
+    name : str
+        Name hint.
+
+    Returns
+    -------
+    out : Tensor
+        The indices of the sorted tensor.
+    """
+    return wrap_nested(_op.argsort(data, axis, descending, dtype), name=name)
+
+
+def topk(
+    data: Tensor,
+    k: int = 1,
+    axis: int = -1,
+    ret_type: str = "both",
+    largest: bool = True,
+    dtype: str = "int32",
+    name: str = "topk",
+):
+    """Get the top k elements in an input tensor along the given axis.
+
+    ret_type specifies the return type, can be one of ("both", "values", "indices").
+
+    Parameters
+    ----------
+    data : Tensor
+        The input data tensor.
+
+    k : int
+        Number of top elements to select. Return all elements if k < 1.
+
+    axis : int
+        Axis long which to sort the input tensor.
+
+    ret_type: str
+        The return type [both, values, indices].
+        "both": return both top k data and indices.
+        "values": return top k data only.
+        "indices": return top k indices only.
+
+    largest : bool
+        Whether to return largest or smallest elements.
+        The k smallest elements are returned if largest is False.
+
+    dtype : str
+        The data type of the indices output.
+
+    name : str
+        Name hint.
+
+    Returns
+    -------
+    out : Tensor or Tuple[Tensor, Tensor]
+        The computed result.
+    """
+    return wrap_nested(_op.topk(data, k, axis, ret_type, largest, dtype), name=name)
+
+
 def multinomial_from_uniform(
     prob: Tensor,
     uniform_sample: Tensor,

python/tvm/relax/backend/dispatch_sort_scan.py
@@ -17,13 +17,16 @@
 # pylint: disable=invalid-name, unused-argument, redefined-argument-from-local
 """Dispatch sort and scan operators to platform dependent implementation."""
 
-from tvm import topi, dlight, relax
+from functools import reduce
+from operator import mul
+
+from tvm import DataType, dlight, relax, topi
+from tvm.contrib.thrust import can_use_thrust
 from tvm.ir import Op
 from tvm.ir.module import IRModule
 from tvm.ir.transform import PassContext, module_pass
-from tvm.target import Target
-from tvm.contrib.thrust import can_use_thrust
 from tvm.relax import PyExprMutator, expr_functor
+from tvm.target import Target
 
 
 @expr_functor.mutator
@@ -80,23 +83,24 @@ class SortScanDispatcher(PyExprMutator):
         if call.op.name == "relax.sort":
             tgt = self._get_target(call.struct_info)
             te_func = topi.sort
+            kwargs = {}
             with tgt:
                 if can_use_thrust(tgt, "tvm.contrib.thrust.sort"):
                     te_func = topi.cuda.sort_thrust
+                    kwargs["workspace"] = self.allocate_workspace(call)
                 elif tgt.kind.name == "cuda":
                     te_func = topi.cuda.sort
             return self.builder_.call_te(
-                te_func,
-                call.args[0],
-                call.attrs.axis,
-                not call.attrs.descending,
+                te_func, call.args[0], call.attrs.axis, not call.attrs.descending, **kwargs
             )
         if call.op.name == "relax.argsort":
             tgt = self._get_target(call.struct_info)
             te_func = topi.argsort
+            kwargs = {}
             with tgt:
                 if can_use_thrust(tgt, "tvm.contrib.thrust.sort"):
                     te_func = topi.cuda.argsort_thrust
+                    kwargs["workspace"] = self.allocate_workspace(call)
                 elif tgt.kind.name == "cuda":
                     te_func = topi.cuda.argsort
             return self.builder_.call_te(
@@ -105,12 +109,15 @@ class SortScanDispatcher(PyExprMutator):
                 axis=call.attrs.axis,
                 is_ascend=not call.attrs.descending,
                 dtype=call.attrs.dtype,
+                **kwargs,
             )
         if call.op.name == "relax.topk":
             tgt = self._get_target(call.struct_info)
             te_func = topi.topk
+            kwargs = {}
             if can_use_thrust(tgt, "tvm.contrib.thrust.sort"):
                 te_func = topi.cuda.topk_thrust
+                kwargs["workspace"] = self.allocate_workspace(call)
             elif tgt.kind.name == "cuda":
                 te_func = topi.cuda.topk
             tir_call = self.builder_.call_te(
@@ -121,6 +128,7 @@ class SortScanDispatcher(PyExprMutator):
                 ret_type=call.attrs.ret_type,
                 is_ascend=not call.attrs.largest,
                 dtype=call.attrs.dtype,
+                **kwargs,
             )
             if tgt.kind.name != "cuda":
                 return tir_call
@@ -130,22 +138,50 @@ class SortScanDispatcher(PyExprMutator):
         if call.op.name in ("relax.cumprod", "relax.cumsum"):
             tgt = self._get_target(call.struct_info)
             axis = int(call.attrs.axis) if call.attrs.axis is not None else call.attrs.axis
-            te_func = topi.cuda.cumsum if tgt.kind.name == "cuda" else topi.cumsum
-            if call.op.name == "relax.cumprod":
-                te_func = topi.cuda.cumprod if tgt.kind.name == "cuda" else topi.cumprod
-            tir_call = self.builder_.call_te(
-                te_func,
-                call.args[0],
-                axis,
-                call.attrs.dtype,
-                call.attrs.exclusive,
-            )
+            kwargs = {}
+            with tgt:
+                if call.op.name == "relax.cumsum":
+                    te_func = topi.cuda.cumsum if tgt.kind.name == "cuda" else topi.cumsum
+                    if can_use_thrust(tgt, "tvm.contrib.thrust.sum_scan"):
+                        kwargs["workspace"] = self.allocate_workspace(call)
+                elif call.op.name == "relax.cumprod":
+                    te_func = topi.cuda.cumprod if tgt.kind.name == "cuda" else topi.cumprod
+                else:
+                    raise ValueError(f"Unsupported op: {call.op.name}")
+                tir_call = self.builder_.call_te(
+                    te_func,
+                    call.args[0],
+                    axis,
+                    call.attrs.dtype,
+                    call.attrs.exclusive,
+                    **kwargs,
+                )
             if tgt.kind.name != "cuda":
                 return tir_call
             # apply dlight gpu fallback
             self._apply_dlight_gpu_fallback(tgt, tir_call)
             return tir_call
         return super().visit_call_(call)
+
+    def estimate_thrust_workspace_size(self, call: relax.Call) -> int:
+        """
+        Estimate the workspace size for thrust sort/argsort/topk/cumsum
+        """
+        input_shape = call.args[0].struct_info.shape
+        input_byte_per_elem = DataType(call.args[0].struct_info.dtype).bits // 8
+        input_size = reduce(mul, input_shape, 1) * input_byte_per_elem
+        # Most GPU algorithms take O(n) space or less, we choose 2N + 4MB as a safe estimation
+        return 2 * input_size + 4 * 1024 * 1024
+
+    def allocate_workspace(self, call: relax.Call) -> relax.Var:
+        """
+        Allocate workspace for thrust sort/argsort/topk.
+        """
+        workspace_size = self.estimate_thrust_workspace_size(call)
+        alloc = relax.op.builtin.alloc_tensor(
+            relax.ShapeExpr((workspace_size,)), "uint8", runtime_device_index=0
+        )
+        return self.builder_.emit(alloc)
 
 
 @module_pass(opt_level=0, name="DispatchSortScan")

python/tvm/te/operation.py
@@ -333,15 +333,15 @@ def extern(
             )
         types.add(t.dtype)
 
-    if dtype is None:
-        if len(types) != 1:
-            raise ValueError("Cannot infer output type, please provide dtype argument")
-        infered_type = types.pop()
-        dtype = [infered_type for _ in shape]
-    if isinstance(dtype, str):
-        dtype = [dtype]
-
     if out_buffers is None:
+        if dtype is None:
+            if len(types) != 1:
+                raise ValueError("Cannot infer output type, please provide dtype argument")
+            infered_type = types.pop()
+            dtype = [infered_type for _ in shape]
+        if isinstance(dtype, str):
+            dtype = [dtype]
+
         for shp, dt in zip(shape, dtype):
             output_placeholders.append(
                 tvm.tir.decl_buffer(shp, dt, name, elem_offset=tvm.tir.Var("elem_offset", "int32"))

python/tvm/topi/cuda/scan.py
@@ -272,7 +272,12 @@ def get_reduction_from_exclusive_scan(data, ex_scan_output, binop=tvm.tir.generi
 
 
 def scan_thrust(
-    data, output_dtype, exclusive=True, return_reduction=False, binop=tvm.tir.generic.add
+    data,
+    output_dtype,
+    exclusive=True,
+    return_reduction=False,
+    binop=tvm.tir.generic.add,
+    workspace=None,
 ):
     """Do exclusive or inclusive scan on 1D or multidimensional input, using thrust.
 
@@ -297,6 +302,11 @@ def scan_thrust(
         thrust function, arbitrariy callables are not supported. Currently only
         tvm.tir.generic.add can be passed in.
 
+    workspace: Optional[tvm.te.Tensor]
+        A buffer to store intermediate results. The size of the workspace should be sufficiently
+        large, this can be obtained by overestimation or memory usage profiling. If None, it will
+        fallback to use thrust internal memory allocation.
+
     Returns
     -------
     output : tvm.te.Tensor
@@ -309,14 +319,24 @@ def scan_thrust(
     data_buf = tvm.tir.decl_buffer(data.shape, data.dtype, "data_buf", data_alignment=8)
     output_buf = tvm.tir.decl_buffer(data.shape, output_dtype, "output_buf", data_alignment=8)
 
+    workspace_buf = (
+        tvm.tir.decl_buffer(workspace.shape, workspace.dtype, "workspace_buf", data_alignment=8)
+        if workspace is not None
+        else None
+    )
+
+    def f_compute(ins, outs):
+        args = [_get_thrust_func_name(binop), ins[0], outs[0], exclusive]
+        if workspace is not None:
+            args.append(ins[1])
+        return tvm.tir.call_packed(*args)
+
     output = te.extern(
         [data.shape],
-        [data],
-        lambda ins, outs: tvm.tir.call_packed(
-            _get_thrust_func_name(binop), ins[0], outs[0], exclusive
-        ),
+        [data] if workspace is None else [data, workspace],
+        f_compute,
         dtype=[output_dtype],
-        in_buffers=[data_buf],
+        in_buffers=[data_buf] if workspace is None else [data_buf, workspace_buf],
         out_buffers=[output_buf],
         name="exclusive_scan_thrust",
         tag="exclusive_scan_thrust_gpu",
@@ -337,6 +357,7 @@ def exclusive_scan(
     output_dtype=None,
     binop=tvm.tir.generic.add,
     identity_value=0,
+    workspace=None,
 ):
     """Do exclusive scan on 1D or multidimensional input.
 
@@ -367,6 +388,11 @@ def exclusive_scan(
         your operator and i is the identity_value then a * i = a for all a in the domain of
         your operation.
 
+    workspace: Optional[tvm.te.Tensor]
+        A buffer to store intermediate results if thrust is enabled. The size of the workspace
+        should be sufficiently large, this can be obtained by overestimation or memory usage
+        profiling. If None, it will fallback to use thrust internal memory allocation.
+
     Returns
     -------
     output : tvm.te.Tensor
@@ -378,11 +404,15 @@ def exclusive_scan(
     """
 
     def do_scan(data, output_dtype):
-
         # TODO: add support for a prod_scan
         if _can_use_scan_thrust(binop):
             return scan_thrust(
-                data, output_dtype, exclusive=True, return_reduction=return_reduction, binop=binop
+                data,
+                output_dtype,
+                exclusive=True,
+                return_reduction=return_reduction,
+                binop=binop,
+                workspace=workspace,
             )
 
         if ndim == 1:
@@ -457,7 +487,9 @@ def exclusive_scan(
     return output
 
 
-def inclusive_scan(data, axis=-1, output_dtype=None, binop=tvm.tir.generic.add, identity_value=0):
+def inclusive_scan(
+    data, axis=-1, output_dtype=None, binop=tvm.tir.generic.add, identity_value=0, workspace=None
+):
     """Do inclusive scan on 1D or multidimensional input.
 
     Parameters
@@ -481,6 +513,11 @@ def inclusive_scan(data, axis=-1, output_dtype=None, binop=tvm.tir.generic.add, 
         your operator and i is the identity_value then a * i = a for all a in the domain of
         your operation.
 
+    workspace: Optional[tvm.te.Tensor]
+        A buffer to store intermediate results if thrust is enabled. The size of the workspace
+        should be sufficiently large, this can be obtained by overestimation or memory usage
+        profiling. If None, it will fallback to use thrust internal memory allocation.
+
     Returns
     -------
     output : tvm.te.Tensor
@@ -497,14 +534,19 @@ def inclusive_scan(data, axis=-1, output_dtype=None, binop=tvm.tir.generic.add, 
         if axis != ndim - 1:
             axes = swap(list(range(ndim)), axis)
             data = transpose(data, axes)
-        output = scan_thrust(data, output_dtype, exclusive=False, binop=binop)
+        output = scan_thrust(data, output_dtype, exclusive=False, binop=binop, workspace=workspace)
         if axis != ndim - 1:
             axes = swap(list(range(ndim)), axis)
             output = transpose(output, axes)
         return output
 
     ex_scan = exclusive_scan(
-        data, axis, output_dtype=output_dtype, binop=binop, identity_value=identity_value
+        data,
+        axis,
+        output_dtype=output_dtype,
+        binop=binop,
+        identity_value=identity_value,
+        workspace=workspace,
     )
 
     if output_dtype is not None and data.dtype != output_dtype and output_dtype != "":
@@ -551,6 +593,7 @@ def scanop(
     axis: Optional[int] = None,
     dtype: Optional[str] = None,
     exclusive: Optional[bool] = None,
+    workspace: Optional[tvm.te.Tensor] = None,
 ) -> tvm.te.Tensor:
     """Cumulative binary operator (scan) with similar axis behavior as np.cumsum and np.cumprod.
 
@@ -587,6 +630,8 @@ def scanop(
         the cumulative operation of the first (j-1) elements. Otherwise, it would be the
         cumulative operation of the first j elements.
 
+    workspace: Optional[tvm.te.Tensor]
+
     Returns
     -------
     result : tvm.te.Tensor
@@ -599,10 +644,20 @@ def scanop(
     axis = get_const_int(axis)
     if exclusive is not None and exclusive:
         return exclusive_scan(
-            data, axis, output_dtype=dtype, binop=binop, identity_value=identity_value
+            data,
+            axis,
+            output_dtype=dtype,
+            binop=binop,
+            identity_value=identity_value,
+            workspace=workspace,
         )
     return inclusive_scan(
-        data, axis, output_dtype=dtype, binop=binop, identity_value=identity_value
+        data,
+        axis,
+        output_dtype=dtype,
+        binop=binop,
+        identity_value=identity_value,
+        workspace=workspace,
     )
 
 
@@ -611,6 +666,7 @@ def cumsum(
     axis: Optional[int] = None,
     dtype: Optional[int] = None,
     exclusive: Optional[bool] = None,
+    workspace: Optional[tvm.te.Tensor] = None,
 ) -> tvm.te.Tensor:
     """Numpy style cumsum op. Return the cumulative sum of the elements along a given axis.
 
@@ -633,6 +689,11 @@ def cumsum(
         the sum of the first (j-1) elements. Otherwise, it would be the sum of
         the first j elements.
 
+    workspace: Optional[tvm.te.Tensor]
+        A buffer to store intermediate results if thrust is enabled. The size of the workspace
+        should be sufficiently large, this can be obtained by overestimation or memory usage
+        profiling. If None, it will fallback to use thrust internal memory allocation.
+
     Returns
     -------
     result : tvm.te.Tensor
@@ -646,6 +707,7 @@ def cumsum(
         axis=axis,
         dtype=dtype,
         exclusive=exclusive,
+        workspace=workspace,
     )
 
 
@@ -654,6 +716,7 @@ def cumprod(
     axis: Optional[int] = None,
     dtype: Optional[int] = None,
     exclusive: Optional[bool] = None,
+    workspace: Optional[tvm.te.Tensor] = None,
 ):
     """Numpy style cumprod op. Return the cumulative product of the elements along a given axis.
 
@@ -676,6 +739,11 @@ def cumprod(
         the product of the first (j-1) elements. Otherwise, it would be the product of
         the first j elements.
 
+    workspace: Optional[tvm.te.Tensor]
+        A buffer to store intermediate results if thrust is enabled. The size of the workspace
+        should be sufficiently large, this can be obtained by overestimation or memory usage
+        profiling. If None, it will fallback to use thrust internal memory allocation.
+
     Returns
     -------
     result : tvm.te.Tensor
@@ -689,4 +757,5 @@ def cumprod(
         axis=axis,
         dtype=dtype,
         exclusive=exclusive,
+        workspace=workspace,
     )

python/tvm/topi/cuda/sort.py
@@ -682,7 +682,7 @@ def sort(data, axis=-1, is_ascend=1):
     return out
 
 
-def sort_thrust(data, axis=-1, is_ascend=1):
+def sort_thrust(data, axis=-1, is_ascend=1, workspace=None):
     """Performs sorting along the given axis and returns an array of
     sorted values with the same shape as the input data.
 
@@ -696,6 +696,12 @@ def sort_thrust(data, axis=-1, is_ascend=1):
 
     is_ascend : boolean, optional
         Whether to sort in ascending or descending order.
+
+    workspace: Optional[tvm.te.Tensor]
+        A buffer to store intermediate results. The size of the workspace should be sufficiently
+        large, this can be obtained by overestimation or memory usage profiling. If None, it will
+        fallback to use thrust internal memory allocation.
+
 
     Returns
     -------
@@ -714,15 +720,20 @@ def sort_thrust(data, axis=-1, is_ascend=1):
 
     value_buf = tvm.tir.decl_buffer(data.shape, data.dtype, "value_buf", data_alignment=8)
     indices_buf = tvm.tir.decl_buffer(data.shape, dtype, "out_buf", data_alignment=8)
+
+    def f_compute(ins, outs):
+        args = ["tvm.contrib.thrust.sort", ins[0], outs[0], outs[1], is_ascend]
+        if workspace is not None:
+            args.append(ins[1])
+        return tvm.tir.call_packed(*args)
+
     out = te.extern(
         [data.shape, data.shape],
-        [data],
+        [data] if workspace is None else [data, workspace],
         ## TODO(mbrookhart): This thrust function is actually doing argsort, not sort
         ## For performance, we should probably rename the contrib function and add
         ## a pure sort
-        lambda ins, outs: tvm.tir.call_packed(
-            "tvm.contrib.thrust.sort", ins[0], outs[0], outs[1], is_ascend
-        ),
+        f_compute,
         out_buffers=[value_buf, indices_buf],
         name="sort_gpu",
         tag="sort_gpu",
@@ -801,7 +812,7 @@ def argsort(data, axis=-1, is_ascend=1, dtype="float32", ret_type="indices"):
     return outs[0], outs[1]
 
 
-def argsort_thrust(data, axis=-1, is_ascend=1, dtype="float32", ret_type="indices"):
+def argsort_thrust(data, axis=-1, is_ascend=1, dtype="float32", ret_type="indices", workspace=None):
     """Performs sorting along the given axis and returns an array of indices
     having same shape as an input array that index data in sorted order.
 
@@ -824,12 +835,17 @@ def argsort_thrust(data, axis=-1, is_ascend=1, dtype="float32", ret_type="indice
         "both": return both sorted data and indices.
         "indices": return sorted indices only.
 
+    workspace : Optional[tvm.te.Tensor]
+        A buffer to store intermediate results. The size of the workspace should be sufficiently
+        large, this can be obtained by overestimation or memory usage profiling. If None, it will
+        fallback to use thrust internal memory allocation.
+
     Returns
     -------
     out : tvm.te.Tensor
         The output of this function.
     """
-    return topk_thrust(data, 0, axis, ret_type, is_ascend, dtype)
+    return topk_thrust(data, 0, axis, ret_type, is_ascend, dtype, workspace)
 
 
 def schedule_sort(outs):
@@ -972,7 +988,9 @@ def topk(data, k=1, axis=-1, ret_type="both", is_ascend=False, dtype="int64"):
     return output
 
 
-def topk_thrust(data, k=1, axis=-1, ret_type="both", is_ascend=False, dtype="int64"):
+def topk_thrust(
+    data, k=1, axis=-1, ret_type="both", is_ascend=False, dtype="int64", workspace=None
+):
     """Get the top k elements in an input tensor along the given axis.
 
     Parameters
@@ -998,6 +1016,11 @@ def topk_thrust(data, k=1, axis=-1, ret_type="both", is_ascend=False, dtype="int
     dtype : string, optional
         The data type of the indices output.
 
+    workspace : Optional[tvm.te.Tensor]
+        A buffer to store intermediate results. The size of the workspace should be sufficiently
+        large, this can be obtained by overestimation or memory usage profiling. If None, it will
+        fallback to use thrust internal memory allocation.
+
     Returns
     -------
     out : tvm.te.Tensor or List[tvm.te.Tensor]
@@ -1013,20 +1036,30 @@ def topk_thrust(data, k=1, axis=-1, ret_type="both", is_ascend=False, dtype="int
         data = transpose(data, axes)
 
     data_buf = tvm.tir.decl_buffer(data.shape, data.dtype, "data_buf", data_alignment=8)
+    if workspace is not None:
+        workspace_buf = tvm.tir.decl_buffer(
+            workspace.shape, workspace.dtype, "workspace_buf", data_alignment=8
+        )
+    else:
+        workspace_buf = None
     out_bufs = [
         tvm.tir.decl_buffer(data.shape, data.dtype, "value_buf", data_alignment=8),
         tvm.tir.decl_buffer(data.shape, dtype, "indices_buf", data_alignment=8),
     ]
 
+    def f_compute(ins, outs):
+        args = ["tvm.contrib.thrust.sort", ins[0], outs[0], outs[1], is_ascend]
+        if workspace is not None:
+            args.append(ins[1])
+        return tvm.tir.call_packed(*args)
+
     is_ascend = 1 if is_ascend else 0
 
     out = te.extern(
         [data.shape, data.shape],
-        [data],
-        lambda ins, outs: tvm.tir.call_packed(
-            "tvm.contrib.thrust.sort", ins[0], outs[0], outs[1], is_ascend
-        ),
-        in_buffers=[data_buf],
+        [data] if workspace is None else [data, workspace],
+        f_compute,
+        in_buffers=[data_buf] if workspace is None else [data_buf, workspace_buf],
         out_buffers=out_bufs,
         name="topk_gpu",
         tag="topk_gpu",
@@ -1120,7 +1153,7 @@ def sort_by_key(keys, values, axis=-1, is_ascend=1):
     return out[0], out[1]
 
 
-def stable_sort_by_key_thrust(keys, values, for_scatter=False):
+def stable_sort_by_key_thrust(keys, values, for_scatter=False, workspace=None):
     """Sort values with respect to keys using thrust.
     Both keys and values will be sorted and returned.
     Sorting is done via stable sort, so relative ordering among
@@ -1140,6 +1173,11 @@ def stable_sort_by_key_thrust(keys, values, for_scatter=False):
         The output keys (indices) are all positive.
         This option is introduced to optimize the scatter implementation.
 
+    workspace : Optional[tvm.te.Tensor]
+        A buffer to store intermediate results. The size of the workspace should be sufficiently
+        large, this can be obtained by overestimation or memory usage profiling. If None, it will
+        fallback to use thrust internal memory allocation.
+
     Returns
     -------
     keys_sorted : tvm.te.Tensor
@@ -1150,17 +1188,36 @@ def stable_sort_by_key_thrust(keys, values, for_scatter=False):
     """
     keys_buf = tvm.tir.decl_buffer(keys.shape, keys.dtype, "keys_buf", data_alignment=8)
     values_buf = tvm.tir.decl_buffer(values.shape, values.dtype, "values_buf", data_alignment=8)
+    workspace_buf = (
+        tvm.tir.decl_buffer(workspace.shape, workspace.dtype, "workspace_buf", data_alignment=8)
+        if workspace is not None
+        else None
+    )
     out_bufs = [
         tvm.tir.decl_buffer(keys.shape, keys.dtype, "keys_buf", data_alignment=8),
         tvm.tir.decl_buffer(keys.shape, values.dtype, "values_buf", data_alignment=8),
     ]
+
+    def f_compute(ins, outs):
+        args = [
+            "tvm.contrib.thrust.stable_sort_by_key",
+            ins[0],
+            ins[1],
+            outs[0],
+            outs[1],
+            for_scatter,
+        ]
+        if workspace is not None:
+            args.append(ins[2])
+        return tvm.tir.call_packed(*args)
+
     out = te.extern(
         [keys.shape, values.shape],
-        [keys, values],
-        lambda ins, outs: tvm.tir.call_packed(
-            "tvm.contrib.thrust.stable_sort_by_key", ins[0], ins[1], outs[0], outs[1], for_scatter
-        ),
-        in_buffers=[keys_buf, values_buf],
+        [keys, values] if workspace is None else [keys, values, workspace],
+        f_compute,
+        in_buffers=[keys_buf, values_buf]
+        if workspace is None
+        else [keys_buf, values_buf, workspace_buf],
         out_buffers=out_bufs,
         dtype=[keys.dtype, values.dtype],
         name="stable_sort_by_key",

commit 8304d3a37fcde599feb26f52478d1f2788e80ad2

[Relax][Frontend] Fix sort, argsort and topk in nn module
Fixes errors introduced in #16851 and add test cases.

python/tvm/relax/frontend/nn/op.py
@@ -2265,7 +2265,7 @@ def sort(x: Tensor, axis: int = -1, descending: bool = False, name="sort"):
     out : Tensor
         The sorted tensor.
     """
-    return wrap_nested(_op.sort(x, axis, descending), name=name)
+    return wrap_nested(_op.sort(x._expr, axis, descending), name=name)
 
 
 def argsort(
@@ -2296,7 +2296,7 @@ def argsort(
     out : Tensor
         The indices of the sorted tensor.
     """
-    return wrap_nested(_op.argsort(data, axis, descending, dtype), name=name)
+    return wrap_nested(_op.argsort(data._expr, axis, descending, dtype), name=name)
 
 
 def topk(
@@ -2344,7 +2344,7 @@ def topk(
     out : Tensor or Tuple[Tensor, Tensor]
         The computed result.
     """
-    return wrap_nested(_op.topk(data, k, axis, ret_type, largest, dtype), name=name)
+    return wrap_nested(_op.topk(data._expr, k, axis, ret_type, largest, dtype), name=name)
 
 
 def multinomial_from_uniform(

tests/python/relax/test_frontend_nn_op.py
@@ -1188,5 +1188,34 @@ def test_renormalize_top_p_top_k_prob():
     )
 
 
+def test_sort_argsort_topk():
+    class Model(Module):
+        def foo(self, x: Tensor):
+            z0 = op.sort(x, axis=-1, descending=True)
+            z1 = op.argsort(x, axis=-1, descending=False)
+            z2 = op.topk(x, k=2, axis=-1)
+            return z0, z1, z2
+
+    @I.ir_module
+    class Expected:
+        @R.function
+        def foo(x: R.Tensor(("seq_len", 64), dtype="float16")):
+            R.func_attr({"num_input": 1})
+            with R.dataflow():
+                sort = R.sort(x, axis=-1, descending=True)
+                argsort = R.argsort(x, axis=-1, descending=False, dtype="int32")
+                topk = R.topk(x, k=2, axis=-1, ret_type="both", largest=True, dtype="int32")
+                topk_0 = topk[0]
+                topk_1 = topk[1]
+                gv = sort, argsort, (topk_0, topk_1)
+                R.output(gv)
+            return gv
+
+    m = Model()
+    mod, _ = m.export_tvm({"foo": {"x": spec.Tensor(("seq_len", 64), "float16")}})
+
+    tvm.ir.assert_structural_equal(mod, Expected)
+
+
 if __name__ == "__main__":
     tvm.testing.main()