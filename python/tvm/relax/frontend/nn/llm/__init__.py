a1639a5776021656f0d16800fd90033ba04389bb
Siyuan Feng <hzfengsy@sjtu.edu.cn>
2024-08-01 09:57:37 +0800

    [Relax] Add KVCache Interface for Relax NNModule
    Introduce kv cache interface for Relax NNModule to support paged attention.
    Note that the implementation is migrated from MLC-llm
    
    Co-authored-by: Bohan Hou <bohanhou@andrew.cmu.edu>
    Co-authored-by: Ruihang Lai <ruihangl@cs.cmu.edu>
    Co-authored-by: Hongyi Jin <hongyij@andrew.cmu.edu>
    Co-authored-by: krishnaraj36 <quic_kvegiraj@quicinc.com>

---
+++ python/tvm/relax/frontend/nn/llm/__init__.py
@@ -0,0 +1,22 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""LLM support for PyTorch-like API to build IRModules."""
+
+from . import kv_cache, position_embedding
+from .position_embedding import llama_rope
+from .tree_attn import tree_attn
+from .kv_cache import PagedKVCache
