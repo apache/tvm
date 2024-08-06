7039ed86e640a8427c0e63e81c6735345066ed22
tqchen <tianqi.tchen@gmail.com>
2024-08-06 16:06:58 -0400

    [FFI] Enable ObjectPtr and tests
    This commit enables object ptr and tests.
    Move most of the friend class to internal.

--- ffi/tests/example/test_object.cc
+++ ffi/tests/example/test_object.cc
@@ -1,12 +1,35 @@
 #include <gtest/gtest.h>
+#include <tvm/ffi/memory.h>
 #include <tvm/ffi/object.h>
 
 namespace {
 
 using namespace tvm::ffi;
 
-TEST(Object, Default) {
-  // Object* x = new Object();
+class IntObj : public Object {
+ public:
+  int64_t value;
+
+  IntObj(int64_t value) : value(value) {}
+};
+
+TEST(Object, RefCounter) {
+  ObjectPtr<IntObj> a = make_object<IntObj>(11);
+  ObjectPtr<IntObj> b = a;
+
+  EXPECT_EQ(a->value, 11);
+
+  EXPECT_EQ(a.use_count(), 2);
+  b.reset();
+  EXPECT_EQ(a.use_count(), 1);
+  EXPECT_TRUE(b == nullptr);
+  EXPECT_EQ(b.use_count(), 0);
+
+  ObjectPtr<IntObj> c = std::move(a);
+  EXPECT_EQ(c.use_count(), 1);
+  EXPECT_TRUE(a == nullptr);
+
+  EXPECT_EQ(c->value, 11);
 }
 
 }  // namespace
