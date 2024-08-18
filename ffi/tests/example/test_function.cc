09394e3faebc3da1468f13fe8cd49fc334a966e0
tqchen <tianqi.tchen@gmail.com>
2024-08-18 17:14:29 -0400

    [FFI] Function support
    Co-authored-by: Junru Shao <junrushao@apache.org>

---
+++ ffi/tests/example/test_function.cc
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+#include <gtest/gtest.h>
+#include <tvm/ffi/any.h>
+#include <tvm/ffi/memory.h>
+#include <tvm/ffi/function.h>
+
+#include "./testing_object.h"
+
+namespace {
+
+using namespace tvm::ffi;
+using namespace tvm::ffi::testing;
+
+TEST(Func, FromPacked) {
+  Function fadd1 = Function::FromPacked(
+    [](int32_t num_args, const AnyView* args, Any* rv) {
+      EXPECT_EQ(num_args, 1);
+      int32_t a = args[0];
+      *rv = a + 1;
+    }
+  );
+  int b = fadd1(1);
+  EXPECT_EQ(b, 2);
+
+  Function fadd2 = Function::FromPacked(
+    [](int32_t num_args, const AnyView* args, Any* rv) {
+      EXPECT_EQ(num_args, 1);
+      TInt a = args[0];
+      EXPECT_EQ(a.use_count(), 2);
+      *rv = a->value + 1;
+    }
+  );
+  EXPECT_EQ(fadd2(TInt(12)).operator int(), 13);
+}
+
+TEST(Func, FromUnpacked) {
+  // try decution
+  Function fadd1 = Function::FromUnpacked(
+    [](const int32_t& a) -> int { return a +  1; }
+  );
+  int b = fadd1(1);
+  EXPECT_EQ(b, 2);
+
+   // convert that triggers error
+  EXPECT_THROW(
+      {
+        try {
+         fadd1(1.1);
+        } catch (const Error& error) {
+          EXPECT_EQ(error->kind, "TypeError");
+          EXPECT_STREQ(
+            error->message.c_str(),
+            "Mismatched type on argument #0 when calling: `(0: int) -> int`. "
+            "Expected `int` but got `float`");
+          throw;
+        }
+      },
+      ::tvm::ffi::Error);
+
+   // convert that triggers error
+   EXPECT_THROW(
+      {
+        try {
+         fadd1();
+        } catch (const Error& error) {
+          EXPECT_EQ(error->kind, "TypeError");
+          EXPECT_STREQ(
+            error->message.c_str(),
+            "Mismatched number of arguments when calling: `(0: int) -> int`. "
+            "Expected 1 but got 0 arguments");
+          throw;
+        }
+      },
+      ::tvm::ffi::Error);
+
+  // try decution
+   Function fpass_and_return = Function::FromUnpacked(
+       [](TInt x, int value, AnyView z) -> Function {
+         EXPECT_EQ(x.use_count(), 2);
+         EXPECT_EQ(x->value, value);
+         if (auto opt = z.TryAs<int>()) {
+           EXPECT_EQ(value, *opt);
+         }
+         return Function::FromUnpacked([value](int x) -> int { return x + value; });
+       },
+       "fpass_and_return");
+   TInt a(11);
+   Function fret = fpass_and_return(std::move(a), 11, 11);
+   EXPECT_EQ(fret(12).operator int(), 23);
+
+   EXPECT_THROW(
+       {
+         try {
+           fpass_and_return();
+         } catch (const Error& error) {
+           EXPECT_EQ(error->kind, "TypeError");
+           EXPECT_STREQ(error->message.c_str(),
+                        "Mismatched number of arguments when calling: "
+                        "`fpass_and_return(0: test.Int, 1: int, 2: AnyView) -> object.Function`. "
+                        "Expected 3 but got 0 arguments");
+           throw;
+         }
+       },
+       ::tvm::ffi::Error);
+}
+}  // namespace
---
+++ ffi/tests/example/testing_object.h
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef TVM_FFI_TESTING_OBJECT_H_
+#define TVM_FFI_TESTING_OBJECT_H_
+#include <tvm/ffi/memory.h>
+#include <tvm/ffi/object.h>
+
+namespace tvm {
+namespace ffi {
+namespace testing {
+
+class TNumberObj : public Object {
+ public:
+  // declare as one slot, with float as overflow
+  static constexpr uint32_t _type_child_slots = 1;
+  static constexpr const char* _type_key = "test.Number";
+  TVM_FFI_DECLARE_BASE_OBJECT_INFO(TNumberObj, Object);
+};
+
+class TNumber : public ObjectRef {
+ public:
+  TVM_FFI_DEFINE_NULLABLE_OBJECT_REF_METHODS(TNumber, ObjectRef, TNumberObj);
+};
+
+class TIntObj : public TNumberObj {
+ public:
+  int64_t value;
+
+  TIntObj(int64_t value) : value(value) {}
+
+  static constexpr const char* _type_key = "test.Int";
+  TVM_FFI_DECLARE_FINAL_OBJECT_INFO(TIntObj, TNumberObj);
+};
+
+class TInt : public TNumber {
+ public:
+  explicit TInt(int64_t value) {
+    data_ = make_object<TIntObj>(value);
+  }
+
+  TVM_FFI_DEFINE_NOTNULLABLE_OBJECT_REF_METHODS(TInt, TNumber, TIntObj);
+};
+
+class TFloatObj : public TNumberObj {
+ public:
+  double value;
+
+  TFloatObj(double value) : value(value) {}
+
+  static constexpr const char* _type_key = "test.Float";
+  TVM_FFI_DECLARE_FINAL_OBJECT_INFO(TFloatObj, TNumberObj);
+};
+
+class TFloat : public TNumber {
+ public:
+  explicit TFloat(double value) {
+    data_ = make_object<TFloatObj>(value);
+  }
+
+  TVM_FFI_DEFINE_NULLABLE_OBJECT_REF_METHODS(TFloat, TNumber, TFloatObj);
+};
+}  // namespace testing
+}  // namespace ffi
+}  // namespace tvm
+#endif  // TVM_FFI_TESTING_OBJECT_H_
