61b2ef6684aefbf8b4ed9bd159ed5140d694d6f1
Ubuntu <msra@h100dev000000.kqarijmwtzbupcz1jwcy3okidh.bx.internal.cloudapp.net>
2024-08-22 08:22:00 +0000

    [tl] Update dp algorithm

--- tl_pipeline/dp.py
+++ tl_pipeline/dp.py
@@ -13,9 +13,19 @@ class Schedule:
     def time(self) -> float:
         return max(self.hw_time["tensor_core"], self.hw_time["cuda_core"], self.hw_time["tma"])
 
+    def __lt__(self, other):
+        return isinstance(other, Schedule) and self.order != other.order and self.time < other.time
+    
+    def __eq__(self, other):
+        return isinstance(other, Schedule) and self.order == other.order
+
+    def __hash__(self):
+        return hash(tuple(self.order))
+    
     def __repr__(self) -> str:
         nodes_str = [repr(node) for node in self.order]
-        s = f"len={len(nodes_str)}, "
+        # s = f"len={len(nodes_str)}, "
+        s = ""
         s += ", ".join(nodes_str)
         s += "\n"
         s += f"Time:{self.time}, "
@@ -26,61 +36,66 @@ class Schedule:
         return s
     
 dp_dict = {}    
+topk = 10
 i = 0
 issue_latency = 0.001
 
-def get_schedule(nodes: List[Node], in_node: Node, in_node_dsts: List[Node], graph: Graph) -> Schedule:
-    schedule = dp_dict[frozenset(nodes)]
-    new_order = [in_node] + schedule.order
-    new_hw_time = {"tensor_core":0, "cuda_core":0, "tma":0}
-    # node: (start_time, end_time)
-    issue_dict = {} 
-    last_start_time = 0
-    for cur_node in new_order:
-        start_time = max(new_hw_time[cur_node.hw_usage], last_start_time + issue_latency)
-        for pred_node in new_order:
-            # Need to issue after previous sync node done
-            if pred_node in issue_dict and pred_node._sync_type == "sync":
-                start_time = max(start_time, issue_dict[pred_node][1] + issue_latency)
-            if graph.has_edge(pred_node, cur_node):
-                edge = graph.get_edge_data(pred_node, cur_node)
-                if edge['type'] == "data":
-                    assert pred_node in issue_dict, "error: pred_node not in issue_dict"
-                    # Data denpendency
+def get_schedule(nodes: List[Node], in_node: Node, in_node_dsts: List[Node], graph: Graph) -> List[Schedule]:
+    schedule_list = []
+    for schedule in dp_dict[frozenset(nodes)]:
+        new_order = [in_node] + schedule.order
+        new_hw_time = {"tensor_core":0, "cuda_core":0, "tma":0}
+        # node: (start_time, end_time)
+        issue_dict = {} 
+        last_start_time = 0
+        for cur_node in new_order:
+            start_time = max(new_hw_time[cur_node.hw_usage], last_start_time + issue_latency)
+            for pred_node in new_order:
+                # Need to issue after previous sync node done
+                if pred_node in issue_dict and pred_node._sync_type == "sync":
                     start_time = max(start_time, issue_dict[pred_node][1] + issue_latency)
-        issue_dict[cur_node] = [start_time, start_time + cur_node.time]
-        new_hw_time[cur_node.hw_usage] = start_time + cur_node.time
-        last_start_time = start_time
-    return Schedule(new_order, new_hw_time)
+                if graph.has_edge(pred_node, cur_node):
+                    edge = graph.get_edge_data(pred_node, cur_node)
+                    if edge['type'] == "data":
+                        assert pred_node in issue_dict, "error: pred_node not in issue_dict"
+                        # Data denpendency
+                        start_time = max(start_time, issue_dict[pred_node][1] + issue_latency)
+            issue_dict[cur_node] = [start_time, start_time + cur_node.time]
+            new_hw_time[cur_node.hw_usage] = start_time + cur_node.time
+            last_start_time = start_time
+        schedule_list.append(Schedule(new_order, new_hw_time))
+    return sorted(schedule_list)
 
 
-def dp(graph: Graph, nodes: List[Node]):
+def dp(graph: Graph, prev_sub_graphs: List[List[Node]]):
     global dp_dict
     global i
-    pred_nodes = {}
-    
-    subgraph = graph.subgraph(nodes.copy())
-    for node in subgraph.nodes:
-        for pred in graph.predecessors(node):
-            valid = True
-            if pred not in subgraph:
-                for pred_succ in graph.successors(pred):
-                    if pred_succ not in subgraph:
-                        valid = False
-                        break
-                if valid:
-                    pred_nodes[pred] = [succ for succ in graph.successors(pred) if succ in subgraph]
-    # for node in pred_nodes:
-    #     print(node)
-    for in_node, in_node_dsts in pred_nodes.items():
-        print(f"iter {i}")
-        i += 1
-        schedule = get_schedule(nodes, in_node, in_node_dsts, graph)
-        new_nodes = nodes + [in_node]
-        if frozenset(new_nodes) in dp_dict and dp_dict[frozenset(new_nodes)].time < schedule.time:
-            continue
-        dp_dict[frozenset(new_nodes)] = schedule
-        dp(graph, nodes + [in_node])
+    # set(sub_graph_nodes): [in_node_0, in_node_1, ...]
+    sub_graphs_dict = {}
+    for prev_nodes in prev_sub_graphs:
+        subgraph = graph.subgraph(prev_nodes.copy())
+        for node in subgraph.nodes:
+            for pred in graph.predecessors(node):
+                valid = True
+                if pred not in subgraph:
+                    for pred_succ in graph.successors(pred):
+                        if pred_succ not in subgraph:
+                            valid = False
+                            break
+                    if valid:
+                        key = frozenset(prev_nodes + [pred])
+                        if key not in sub_graphs_dict:
+                            sub_graphs_dict[key] = []
+                        sub_graphs_dict[key].append((pred, prev_nodes))
+    for new_nodes_set, nodes in sub_graphs_dict.items():
+        schedule_list = []
+        for in_node, prev_nodes in nodes:
+            schedule_list.extend(get_schedule(prev_nodes, in_node, [], graph))
+        dp_dict[new_nodes_set] = sorted(set(schedule_list))[:topk]
+    sub_graphs = list(list(n) for n in list(sub_graphs_dict))
+    if len(sub_graphs) > 0:
+        dp(graph, sub_graphs)
+
 
 def duplicate_with_stream(graph: Graph, stream: int) -> Tuple[Graph, Node]:
     node_num = len(graph.nodes)
@@ -112,10 +127,12 @@ if __name__ == "__main__":
     print(duplicate_graph.edges(data=True))
     # for node in graph.nodes:
     #     print(node)
-    dp_dict[frozenset([output_node])] = Schedule([output_node], {"tensor_core":0, "cuda_core":0, "tma":0})
-    dp(duplicate_graph, [output_node])
+    dp_dict[frozenset([output_node])] = [Schedule([output_node], {"tensor_core":0, "cuda_core":0, "tma":0})]
+    dp(duplicate_graph, [[output_node]])
 
     # for k, v in dp_dict.items():
     #     print(k)
     #     print(v)
-    print("results", dp_dict[frozenset(list(duplicate_graph.nodes))])
+    print("results:")
+    for schedule in dp_dict[frozenset(list(duplicate_graph.nodes))]:
+        print(schedule)
