9db6ddbd1865c32a6cbe164ba655078194dc542c
Ubuntu <msra@h100dev000000.kqarijmwtzbupcz1jwcy3okidh.bx.internal.cloudapp.net>
2024-08-23 08:58:40 +0000

    [tl] Add memory dimension to dp

--- tl_pipeline/dp.py
+++ tl_pipeline/dp.py
@@ -1,9 +1,11 @@
 import networkx as nx
 from graph import *
+from functools import lru_cache
 
 class Schedule:
-    def __init__(self, order, hw_time):
+    def __init__(self, order: List[Node], sync: List[Tuple[Node, Node]], hw_time: Dict):
         self.order = order
+        self.sync = sync
         self.hw_time = {}
         self.hw_time["tensor_core"] = hw_time["tensor_core"]
         self.hw_time["cuda_core"] = hw_time["cuda_core"]
@@ -14,19 +16,22 @@ class Schedule:
         return max(self.hw_time["tensor_core"], self.hw_time["cuda_core"], self.hw_time["tma"])
 
     def __lt__(self, other):
-        return isinstance(other, Schedule) and self.order != other.order and self.time < other.time
+        return isinstance(other, Schedule) and (self.order != other.order or self.sync != other.sync) and self.time < other.time
     
     def __eq__(self, other):
-        return isinstance(other, Schedule) and self.order == other.order
+        return isinstance(other, Schedule) and self.order == other.order and self.sync == other.sync
 
     def __hash__(self):
-        return hash(tuple(self.order))
+        return hash((tuple(self.order), tuple(tuple(pair) for pair in self.sync)))
     
     def __repr__(self) -> str:
         nodes_str = [repr(node) for node in self.order]
+        sync_str = ["(" + repr(n0) + ", " + repr(n1) + ")" for n0, n1 in self.sync]
         # s = f"len={len(nodes_str)}, "
         s = ""
         s += ", ".join(nodes_str)
+        s += "\n"
+        s += ",".join(sync_str)
         s += "\n"
         s += f"Time:{self.time}, "
         s += f"tensor_core:{self.hw_time['tensor_core']}, "
@@ -38,11 +43,20 @@ class Schedule:
 dp_dict = {}    
 topk = 10
 i = 0
+smem_interval = 1024
+reg_interval = 1024
+smem_max_cap = 4 * 1024
+reg_max_cap = 4 * 1024
 issue_latency = 0.001
 
-def get_schedule(nodes: List[Node], in_node: Node, in_node_dsts: List[Node], graph: Graph) -> List[Schedule]:
+def get_schedule(nodes: List[Node], in_node: Node, graph: nx.DiGraph, mem_cap: List[float]) -> List[Schedule]:
     schedule_list = []
-    for schedule in dp_dict[frozenset(nodes)]:
+    smem_cap, reg_cap = mem_cap
+    schedules = []
+    for s in range(int(smem_cap / smem_interval)):
+        for r in range(int(reg_cap / reg_interval)):
+            schedules.extend(dp_dict[frozenset(nodes)][s][r])
+    for schedule in schedules:
         new_order = [in_node] + schedule.order
         new_hw_time = {"tensor_core":0, "cuda_core":0, "tma":0}
         # node: (start_time, end_time)
@@ -52,22 +66,91 @@ def get_schedule(nodes: List[Node], in_node: Node, in_node_dsts: List[Node], gra
             start_time = max(new_hw_time[cur_node.hw_usage], last_start_time + issue_latency)
             for pred_node in new_order:
                 # Need to issue after previous sync node done
-                if pred_node in issue_dict and pred_node._sync_type == "sync":
+                if pred_node in issue_dict and pred_node.sync_type == "sync":
                     start_time = max(start_time, issue_dict[pred_node][1] + issue_latency)
                 if graph.has_edge(pred_node, cur_node):
                     edge = graph.get_edge_data(pred_node, cur_node)
                     if edge['type'] == "data":
-                        assert pred_node in issue_dict, "error: pred_node not in issue_dict"
+                        assert pred_node in issue_dict, "Error: pred_node not in issue_dict"
                         # Data denpendency
                         start_time = max(start_time, issue_dict[pred_node][1] + issue_latency)
             issue_dict[cur_node] = [start_time, start_time + cur_node.time]
             new_hw_time[cur_node.hw_usage] = start_time + cur_node.time
             last_start_time = start_time
-        schedule_list.append(Schedule(new_order, new_hw_time))
+        
+        new_schedule = Schedule(new_order, schedule.sync, new_hw_time)
+        smem_fp, reg_fp = calculate_memory_footprint(new_schedule, graph)
+
+        if smem_fp <= smem_cap and reg_fp <= reg_cap:
+            schedule_list.append(new_schedule)
+
+        # Add syncs conditions for async nodes
+        if in_node.sync_type == "async":
+            for sync_dst_node in schedule.order:
+                if sync_dst_node.name == "Output":
+                    continue
+                # If no hardware resource conflict, no need to add sync
+                if sync_dst_node.hw_usage != in_node.hw_usage and sync_dst_node.output_buffer not in in_node.input_buffer:
+                    continue
+                new_schedule = Schedule(new_order, schedule.sync + [(in_node, sync_dst_node)], new_hw_time)
+                smem_fp, reg_fp = calculate_memory_footprint(new_schedule, graph)
+                if smem_fp <= smem_cap and reg_fp <= reg_cap:
+                    schedule_list.append(new_schedule)
     return sorted(schedule_list)
 
+@lru_cache(None)
+def get_data_dependency_inputs(node: Node, graph: nx.DiGraph) -> List[Node]:
+    return [pred for pred in graph.predecessors(node) if graph.get_edge_data(pred, node).get('type') == 'data']
 
-def dp(graph: Graph, prev_sub_graphs: List[List[Node]]):
+@lru_cache(None)
+def get_data_dependency_outputs(node: Node, graph: nx.DiGraph) -> List[Node]:
+    return [succ for succ in graph.successors(node) if graph.get_edge_data(node, succ).get('type') == 'data']
+
+def calculate_memory_footprint(schedule: Schedule, graph: nx.DiGraph) -> List[float]:
+    smem_fp = 0
+    reg_fp = 0
+    smem_usage = 0
+    reg_usage = 0
+
+    def allocate_outputs(node: Node):
+        nonlocal smem_usage, reg_usage
+        if node.output_buffer == "smem":
+            smem_usage += node.dsize
+        elif node.output_buffer == "register":
+            reg_usage += node.dsize
+
+    def free_outputs(node: Node):
+        nonlocal smem_usage, reg_usage
+        if node.output_buffer == "smem":
+            smem_usage -= node.dsize
+        elif node.output_buffer == "register":
+            reg_usage -= node.dsize
+    
+    ref_dict = {node: [] for node in schedule.order}
+    for ordered_node in schedule.order:
+        for pred in get_data_dependency_inputs(ordered_node, graph):
+            if pred in schedule.order:
+                ref_dict[pred].append(ordered_node)
+    for ordered_node in schedule.order:
+        sync_srcs = [src for src, dst in schedule.sync if dst == ordered_node]
+        for pred in list(set(get_data_dependency_inputs(ordered_node, graph) + sync_srcs)):
+            if pred in schedule.order:
+                for pred_src in get_data_dependency_inputs(pred, graph):
+                    if pred_src in schedule.order:
+                        if len(ref_dict[pred_src]) == 0:
+                            continue
+                        ref_dict[pred_src].remove(pred)
+                        if len(ref_dict[pred_src]) == 0:
+                            free_outputs(pred_src)
+        allocate_outputs(ordered_node)
+        if smem_usage > smem_fp:
+            smem_fp = smem_usage
+        if reg_usage > reg_fp:
+            reg_fp = reg_usage
+    # print(smem_fp, reg_fp)
+    return [smem_fp, reg_fp]
+
+def dp(graph: nx.DiGraph, prev_sub_graphs: List[List[Node]]):
     global dp_dict
     global i
     # set(sub_graph_nodes): [in_node_0, in_node_1, ...]
@@ -88,23 +171,31 @@ def dp(graph: Graph, prev_sub_graphs: List[List[Node]]):
                             sub_graphs_dict[key] = []
                         sub_graphs_dict[key].append((pred, prev_nodes))
     for new_nodes_set, nodes in sub_graphs_dict.items():
-        schedule_list = []
-        for in_node, prev_nodes in nodes:
-            schedule_list.extend(get_schedule(prev_nodes, in_node, [], graph))
-        dp_dict[new_nodes_set] = sorted(set(schedule_list))[:topk]
+        dp_dict[new_nodes_set] = []
+        for s in range(smem_max_cap // smem_interval):
+            smem_cap = smem_interval * (s + 1)
+            dp_dict[new_nodes_set].append([])
+            for r in range(reg_max_cap // reg_interval):
+                reg_cap = reg_interval * (r + 1)
+                schedule_list = []
+                for in_node, prev_nodes in nodes:
+                    schedule_list.extend(get_schedule(prev_nodes, in_node, graph, [smem_cap, reg_cap]))
+                dp_dict[new_nodes_set][s].append(sorted(set(schedule_list))[:topk])
+
     sub_graphs = list(list(n) for n in list(sub_graphs_dict))
     if len(sub_graphs) > 0:
         dp(graph, sub_graphs)
 
 
-def duplicate_with_stream(graph: Graph, stream: int) -> Tuple[Graph, Node]:
+def duplicate_with_stream(graph: nx.DiGraph, stream: int) -> Tuple[nx.DiGraph, Node]:
     node_num = len(graph.nodes)
     node_list = []
     duplicate_graph = nx.DiGraph()
     for i in range(stream):
-        mapping = {node: Node(node.id, [], node.name+f'_{i}', node._sync_type, node.hw_usage, node.time, node.buffer_layer) for node in graph.nodes}
+        mapping = {node: Node(node.id, node.name+f'_{i}', node.sync_type, node.hw_usage, node.time, node.input_buffer, node.output_buffer) for node in graph.nodes}
         g = nx.relabel_nodes(graph, mapping)
-        node_list.extend(g.nodes)
+        ordered_ndoes = sorted(g.nodes)
+        node_list.extend(ordered_ndoes)
         duplicate_graph = nx.compose(duplicate_graph, g)
         
         if i > 0:
@@ -112,7 +203,7 @@ def duplicate_with_stream(graph: Graph, stream: int) -> Tuple[Graph, Node]:
                 duplicate_graph.add_edge(node_list[(i - 1) * node_num + nid], node_list[i * node_num + nid], type="control.issue")
     
     out_nodes = [node for node in duplicate_graph.nodes if duplicate_graph.out_degree(node) == 0]
-    output_node = Node(node_num * stream, [], "Output", "sync", "cuda_core", 0, [])
+    output_node = Node(node_num * stream, "Output", "sync", "cuda_core", 0, ["register"], "register")
     
     for out_node in out_nodes:
         duplicate_graph.add_edge(out_node, output_node, type="data")
@@ -120,14 +211,18 @@ def duplicate_with_stream(graph: Graph, stream: int) -> Tuple[Graph, Node]:
 
 if __name__ == "__main__":
     stream = 3
-    _, graph = load_model("inputs.json")
+    graph = load_model("inputs.json")
     duplicate_graph, output_node = duplicate_with_stream(graph, stream)
 
     print(duplicate_graph)
     print(duplicate_graph.edges(data=True))
     # for node in graph.nodes:
     #     print(node)
-    dp_dict[frozenset([output_node])] = [Schedule([output_node], {"tensor_core":0, "cuda_core":0, "tma":0})]
+    dp_dict[frozenset([output_node])] = []
+    for s in range(smem_max_cap // smem_interval):
+        dp_dict[frozenset([output_node])].append([])
+        for r in range(reg_max_cap // reg_interval):
+            dp_dict[frozenset([output_node])][s].append([Schedule([output_node], [], {"tensor_core":0, "cuda_core":0, "tma":0})])
     dp(duplicate_graph, [[output_node]])
 
     # for k, v in dp_dict.items():
--- tl_pipeline/graph.py
+++ tl_pipeline/graph.py
@@ -1,216 +1,45 @@
 from typing import Any, List, Dict, Optional, Tuple, Union
-import heapq
-
-class Edge:
-    def __init__(self, src_node: 'Node', dst_node: 'Node', src_id: int, dst_id: int, w: int):
-        self.w = w
-        self.src_node = src_node
-        self.dst_node = dst_node
-        self.src_id = src_id
-        self.dst_id = dst_id
-    
-    def __repr__(self) -> str:
-        return "<Edge, " + self.src_node.name + "--(" + str(self.w) + ")-->" + self.dst_node.name + ">"
-
-class DataDependencyEdge(Edge):
-    def __init__(self, src_node: 'Node', dst_node: 'Node', src_id: int, dst_id: int, w: int):
-        super().__init__(src_node, dst_node, src_id, dst_id, w)
-
-    def __repr__(self) -> str:
-        return "<DataDependencyEdge, " + self.src_node.name + "--(" + str(self.w) + ")-->" + self.dst_node.name + ">"
-
-class CondDependencyEdge(Edge):
-    def __init__(self, src_node: 'Node', dst_node: 'Node', src_id: int, dst_id: int, w: int):
-        super().__init__(src_node, dst_node, src_id, dst_id, w)
-
-    def __repr__(self) -> str:
-        return "<CondDependencyEdge, " + self.src_node.name + "--(" + str(self.w) + ")-->" + self.dst_node.name + ">"
+from math import prod
 
 class Node:
-    def __init__(self, node_id: int, inputs: List[Union[Tuple['Node', int], 'Node', None]], name: str, sync_type: str, hw_usage: List[str], time: float, buffer_layer: List[str]):
+    def __init__(
+            self, 
+            node_id: int, 
+            name: str, 
+            sync_type: str, 
+            hw_usage: str, 
+            time: float, 
+            input_buffer: List[str],
+            output_buffer: str
+        ):
         self.id = node_id
         self.name = name
-        self._out_edges = []
-        self._in_edges = []
-        self._shapes = []
-        self._dtypes = []
-        self._tag = {}
-        self._sync_type = sync_type
+        # self._out_edges = []
+        # self._in_edges = []
+        # self._shapes = []
+        # self._dtypes = []
+        # self._tag = {}
+        self.shape = [16, 16]
+        self.dtype = "float16"
+        self.sync_type = sync_type
         self.hw_usage = hw_usage
         self.time = time
-        self.buffer_layer = buffer_layer
+        self.input_buffer = input_buffer
+        self.output_buffer = output_buffer
         assert sync_type in ['sync', 'async'], 'invaild sync_type!'
 
-        for i, node in enumerate(inputs):
-            if node is None:
-                inputs[i] = PlaceHolderNode()
-
-        for dst_id, n in enumerate(inputs):
-            if isinstance(n, Node):
-                n = (n, 0)
-            assert(len(n) == 2)
-            src_node, src_id = n[0], n[1]
-            edge = DataDependencyEdge(src_node, self, src_id, dst_id, 0)
-            self._in_edges.append(edge)
-            src_node._out_edges.append(edge)
-
     @property
-    def inputs(self) -> List[Edge]:
-        return self._in_edges
+    def dsize(self) -> int:
+        assert self.dtype in ['float16', 'float32'], 'invaild dtype!'
+        dtype_size = {'float16': 2, 'float32': 4}
+        return dtype_size[self.dtype] * prod(self.shape)
 
-    @property
-    def outputs(self) -> List[Edge]:
-        return self._out_edges
-
-    def set_inputs(self, i: int, edge: Edge):
-        assert i < len(self._in_edges)
-        self._in_edges[i] = edge
-
-    def set_outputs(self, i: int, edge: Edge):
-        assert i < len(self._out_edges)
-        self._out_edges[i] = edge
-
-    def get_shape(self, id: int = 0) -> List[int]:
-        return self._shapes[id]
-
-    def set_shape(self, shape: List[int], id=0, overwrite=False) -> None:
-        if len(self._shapes) <= id:
-            self._shapes.extend([None for _ in range(id - len(self._shapes) + 1)])
-        elif self._shapes[id] is not None and not overwrite:
-            assert self._shapes[id] == list(map(int, shape)), (self._shapes, list(map(int, shape)))
-        self._shapes[id] = list(map(int, shape))
-
-    def is_placeholder(self):
-        return False
-
-    def is_output(self):
-        return False
-
-    def add_tag(self, k: str, v: Any = True) -> None:
-        self._tag[k] = v
-
-    def get_tag(self, k: str) -> Any:
-        if k not in self._tag:
-            return None
-        return self._tag[k]
-
-    def num_outputs(self) -> int:
-        if len(self.outputs) == 0:
-            return 0
-        return max([e.src_id for e in self.outputs]) + 1
-
-    def get_ir(self) -> str:
-        raise NotImplementedError()
-
+    def __lt__(self, other: 'Node') -> bool:
+        return self.id < other.id
+    
     def __repr__(self) -> str:
         # return "<Node, " + self.name + ">"
         return self.name
-
-class PlaceHolderNode(Node):
-    def __init__(self, name=""):
-        super().__init__([], "PlaceHolder " + name, "sync")
-
-    def is_placeholder(self):
-        return True
-
-    def get_ir(self) -> str:
-        return "placeholder"
-
-class OutputNode(Node):
-    def __init__(self, node, id=0):
-        super().__init__([(node, id)], "Output ", "sync")
-        # self.set_shape(node.get_shape(id))
-        # self.set_dtype(node.get_dtype(id))
-
-    def is_output(self):
-        return True
-
-    def get_ir(self) -> str:
-        return "output"
-
-class Graph:
-    def __init__(self, nodes: List['Node']):
-        self.nodes = nodes
-        self.node_count = len(self.nodes)
-        self.dist = [[float('inf')] * self.node_count for _ in range(self.node_count)]
-        self.init_dist()
-        self.edge_count = 0
-        for node in self.nodes:
-            self.edge_count += len(node.outputs)
-
-        print("dist:")
-        print(self.dist)
-        print("edge_count:")
-        print(self.edge_count)
-
-    def init_dist(self):
-        for i in range(self.node_count):
-            self.dist[i][i] = 0
-        for node in self.nodes:
-            for edge in node.outputs:
-                self.add_edge(node, edge.dst_node, edge.w)
-
-    def exist_edge(self, n0: 'Node', n1: 'Node') -> bool:
-        assert n0 in self.nodes, "n0 not in graph!"
-        assert n1 in self.nodes, "n1 not in graph!"
-        for edge in n0.outputs:
-            if n1 == edge.dst_node:
-                return True
-        return False
-
-    def update_dist(self, u, v, w):
-        dist = self.dist.copy()
-        if dist[u][v] > w:
-            dist[u][v] = w
-    
-        for i in range(self.node_count):
-            for j in range(self.node_count):
-                if dist[i][v] > dist[i][u] + w:
-                    dist[i][v] = dist[i][u] + w
-                if dist[u][j] > w + dist[v][j]:
-                    dist[u][j] = w + dist[v][j]
-                if dist[i][j] > dist[i][u] + w + dist[v][j]:
-                    dist[i][j] = dist[i][u] + w + dist[v][j]
-
-        # Detect negetive ring
-        for i in range(self.node_count):
-            for j in range(self.node_count):
-                if dist[i][v] > dist[i][u] + w:
-                    return None
-                if dist[u][j] > w + dist[v][j]:
-                    return None
-                if dist[i][j] > dist[i][u] + w + dist[v][j]:
-                    return None
-        return dist
-
-
-    def add_edge(self, n0, n1, w) -> bool:
-        self.dist[n0.id][n1.id] = w
-        dist = self.update_dist(n0.id, n1.id, w)
-        if dist is None:
-            print("Invalid, negetive ring detected.")
-            return False # Invalid   
-        self.dist = dist
-        print("updated dist:")
-        print(self.dist)
-        return True
-
-    def del_edge(self, n0, n1):
-        pass
-
-    def check_legality(self) -> bool:
-        return True
-
-def print_graph(nodes: List['Node']):
-    for node in nodes:
-        print(node)
-        for edge in node.inputs:
-            print(edge)
-        print('-'*100)
-
-def get_path_value(n0: 'Node', n1: 'Node') -> int:
-
-    return 
 
 
 import networkx as nx
@@ -222,8 +51,7 @@ def load_model(fname: str) -> List[Node]:
     with open(fname) as f:
         a = json.load(f)
     node_map = {item[0] : None for item in a}
-    ordered_nodes = []
-    for node_id, name, sync_type, hw_usage, time, buffer_layer, is_output, inputs in a:
+    for node_id, name, sync_type, hw_usage, input_buffer, output_buffer, inputs, time in a:
         input_list = []
         for src_node, src_id in inputs:
             if src_node not in node_map:
@@ -231,10 +59,9 @@ def load_model(fname: str) -> List[Node]:
             else:
                 assert node_map[src_node] is not None, "Detected ring in topo order {}->{} !".format(src_node, node_id)
                 input_list.append([node_map[src_node], src_id])
-        node = Node(node_id, input_list, name, sync_type, hw_usage, time, buffer_layer)
+        node = Node(node_id, name, sync_type, hw_usage, time, input_buffer, output_buffer)
         for src_node, _ in inputs:
             assert node_map[src_node] is not None
             graph.add_edge(node_map[src_node], node, weight=0, type="data")
         node_map[node_id] = node
-        ordered_nodes.append(node)
-    return ordered_nodes, graph
+    return graph
