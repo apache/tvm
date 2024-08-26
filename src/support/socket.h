d5d5ebb601a1fee5be3ff52bb8520497db1b99de
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-08-26 07:29:40 -0400

    [Support] Fix the Read/Write of socket stream (#17284)
    This PR fixes the `dmlc::Stream::Read/Write` for TCP socket.
    Given socket does not guarantee that all data are send received/sent
    in a single shot, we need to use `RecvAll/SendAll`.

--- src/support/socket.h
+++ src/support/socket.h
@@ -553,9 +553,9 @@ class TCPSocket : public Socket, public dmlc::Stream {
     return data;
   }
 
-  size_t Read(void* data, size_t size) final { return Recv(data, size); }
+  size_t Read(void* data, size_t size) final { return RecvAll(data, size); }
 
-  size_t Write(const void* data, size_t size) final { return Send(data, size); }
+  size_t Write(const void* data, size_t size) final { return SendAll(data, size); }
 };
 
 /*! \brief helper data structure to perform poll */
