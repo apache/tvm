1c6642ca22107e2333989c1b572ca9983697c059
Yu Cheng <yu.cheng@pku.edu.cn>
2024-09-11 06:21:01 +0000

    update mha_pipeline.py

--- tl_scripts/mha_pipeline.py
+++ tl_scripts/mha_pipeline.py
@@ -48,11 +48,18 @@ def flashattn(batch, heads, seq_len, dim, is_casual, block_M, block_N):
         K_shared: T.Buffer([block_N, dim], dtype),
         acc_s: T.Buffer([block_M, block_N], accum_dtype),
         k: T.int32,
+        bx: T.int32,
         by: T.int32,
         bz: T.int32,
     ):
         T.copy(K[bz, k * block_N : (k + 1) * block_N, by, :], K_shared)
-        T.clear(acc_s)
+        if is_casual:
+            for i, j in T.Parallel(block_M, block_N):
+                acc_s[i, j] = T.if_then_else(
+                    bx * block_M + i >= k * block_N + j, 0, -T.infinity(acc_s.dtype)
+                )
+        else:
+            T.clear(acc_s)
         T.gemm(Q_shared, K_shared, acc_s, transpose_B=True, policy=T.GemmWarpPolicy.FullRow)
 
     @T.macro
@@ -79,15 +86,21 @@ def flashattn(batch, heads, seq_len, dim, is_casual, block_M, block_N):
         scores_sum: T.Buffer([block_M], accum_dtype),
         logsum: T.Buffer([block_M], accum_dtype),
     ):
-        for i, j in T.Parallel(block_M, dim):
-            acc_s[i, j] *= scale
         T.copy(scores_max, scores_max_prev)
         T.fill(scores_max, -T.infinity(accum_dtype))
         T.reduce_max(acc_s, scores_max, dim=1, clear=False)
+        # To do causal softmax, we need to set the scores_max to 0 if it is -inf
+        # This process is called Check_inf in FlashAttention3 code, and it only need to be done
+        # in the first ceil_div(kBlockM, kBlockN) steps.
+        # for i in T.Parallel(block_M):
+        #     scores_max[i] = T.if_then_else(scores_max[i] == -T.infinity(accum_dtype), 0, scores_max[i])
         for i in T.Parallel(block_M):
-            scores_scale[i] = T.exp2(scores_max_prev[i] - scores_max[i])
+            scores_scale[i] = T.exp2(scores_max_prev[i] * scale - scores_max[i] * scale)
         for i, j in T.Parallel(block_M, block_N):
-            acc_s[i, j] = T.exp2(acc_s[i, j] - scores_max[i])
+            # Instead of computing exp(x - max), we compute exp2(x * log_2(e) -
+            # max * log_2(e)) This allows the compiler to use the ffma
+            # instruction instead of fadd and fmul separately.
+            acc_s[i, j] = T.exp2(acc_s[i, j] * scale - scores_max[i] * scale)
         T.reduce_sum(acc_s, scores_sum, dim=1)
         for i in T.Parallel(block_M):
             logsum[i] = logsum[i] * scores_scale[i] + scores_sum[i]
@@ -106,6 +119,7 @@ def flashattn(batch, heads, seq_len, dim, is_casual, block_M, block_N):
             Q_shared = T.alloc_shared([block_M, dim], dtype)
             K_shared = T.alloc_shared([block_N, dim], dtype)
             V_shared = T.alloc_shared([block_N, dim], dtype)
+            O_shared = T.alloc_shared([block_M, dim], dtype)
             acc_s = T.alloc_fragment([block_M, block_N], accum_dtype)
             acc_s_cast = T.alloc_fragment([block_M, block_N], dtype)
             acc_o = T.alloc_fragment([block_M, dim], accum_dtype)
@@ -121,17 +135,21 @@ def flashattn(batch, heads, seq_len, dim, is_casual, block_M, block_N):
             T.fill(logsum, 0)
             T.fill(scores_max, -T.infinity(accum_dtype))
 
+            # loop_range = (
+            #     T.ceildiv((bx + 1) * block_M, block_N) if is_casual else T.ceildiv(seq_len, block_N)
+            # )
             loop_range = (
-                T.ceildiv((bx + 1) * block_M, block_N) if is_casual else T.ceildiv(seq_len, block_N)
+                T.min(T.ceildiv(seq_len, block_N), T.ceildiv((bx + 1) * block_M, block_N)) if is_casual else T.ceildiv(seq_len, block_N)
             )
-            # Body
-            for k in T.Pipelined(loop_range, num_stages=2, order=[0,2,1], stage=[0,0,1], group=[[0,1], [2,3,4,5,6,7,8,9,10,11], [12]]):
-                MMA0(K, Q_shared, K_shared, acc_s, k, by, bz)
+
+            for k in T.Pipelined(loop_range, num_stages=1, order=[0,2,1], stage=[0,0,1], group=[[0,1], [2,3,4,5,6,7,8,9,10], [11]]):
+                MMA0(K, Q_shared, K_shared, acc_s, k, bx, by, bz)
                 Softmax(acc_s, acc_s_cast, acc_o, scores_max, scores_max_prev, scores_scale, scores_sum, logsum)
                 MMA1(V, V_shared, acc_s_cast, acc_o, k, by, bz)
             for i, j in T.Parallel(block_M, dim):
                 acc_o[i, j] /= logsum[i]
-            T.copy(acc_o, Output[bz, bx * block_M : (bx + 1) * block_M, by, :])
+            T.copy(acc_o, O_shared)
+            T.copy(O_shared, Output[bz, bx * block_M : (bx + 1) * block_M, by, :])
 
     return main
 
@@ -143,14 +161,14 @@ def ref_program(Q, K, V, casual):
 
 
 if __name__ == "__main__":
-    BATCH, H, N_CTX, D_HEAD = 8, 8, 2048, 256
-    casual = False
+    BATCH, H, N_CTX, D_HEAD = 64, 12, 1024, 128
+    casual = True
     flops_per_matmul = 2.0 * BATCH * H * N_CTX * N_CTX * D_HEAD
     total_flops = 2 * flops_per_matmul
     if casual:
         total_flops *= 0.5
     BLOCK_M = 128
-    BLOCK_N = 80 # if D_HEAD <= 128 else 32
+    BLOCK_N = 128 # if D_HEAD <= 128 else 32
     program = flashattn(BATCH, H, N_CTX, D_HEAD, casual, BLOCK_M, BLOCK_N)
     ref_program = partial(ref_program, casual=casual)
     mod, params = tl.lower(program)
