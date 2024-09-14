1cfc75d2599dec9d26caf241473057afb4dabc7b
Masahiro Hiramori <mhg00g13@gmail.com>
2024-09-14 21:37:01 +0900

    cleanup `_transpose()`

--- python/tvm/relax/frontend/torch/fx_translator.py
+++ python/tvm/relax/frontend/torch/fx_translator.py
@@ -1029,6 +1029,12 @@ class TorchFXImporter:
         dims = args[1] if isinstance(args[1], (torch.Size, tuple, list)) else args[1:]
         return self.block_builder.emit(relax.op.tile(x, dims))
 
+    def _transpose(self, node: fx.Node) -> relax.Var:
+        args = self.retrieve_args(node)
+        full_idx = list(range(len(self.shape_of(args[0]))))
+        full_idx[args[1]], full_idx[args[2]] = full_idx[args[2]], full_idx[args[1]]
+        return self.block_builder.emit(relax.op.permute_dims(args[0], full_idx))
+
     ########## DataType ##########
 
     def _float(self, node: fx.Node) -> relax.Var:
@@ -1206,12 +1212,6 @@ class TorchFXImporter:
         else:
             dim = 0
         return self.block_builder.emit(relax.op.split(x, chunks, dim))
-
-    def _transpose(self, node: fx.Node) -> relax.Var:
-        args = self.retrieve_args(node)
-        full_idx = list(range(len(self.shape_of(args[0]))))
-        full_idx[args[1]], full_idx[args[2]] = full_idx[args[2]], full_idx[args[1]]
-        return self.block_builder.emit(relax.op.permute_dims(args[0], full_idx))
 
     def _index_select(self, node: fx.Node) -> relax.Var:
         x = self.env[node.args[0]]
