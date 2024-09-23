47b6f71b269a4528a0e41d7cabf94c110cd0373e
Wuwei Lin <wuwei@apache.org>
2024-09-22 17:34:21 -0700

    lint

--- python/tvm/script/ir_builder/tir/triton.py
+++ python/tvm/script/ir_builder/tir/triton.py
@@ -1,4 +1,5 @@
 """Triton kernel integration with TIR"""
+
 from typing import Tuple, List, Union, Any, Dict
 
 import triton
@@ -23,13 +24,16 @@ class TritonKernel(BaseKernel):
         self.func = func
 
     def compile_to_device_module(
-        self, grid: List[Union[int, tir.PrimExpr]], *args: List[Any], **kwargs: Dict[str, Any]
+        self,
+        launch_args: List[Union[int, tir.PrimExpr]],
+        *args: List[Any],
+        **kwargs: Dict[str, Any],
     ) -> Tuple[str, Module, List[Any]]:
         """Compile the kernel to a device module.
 
         Parameters
         ----------
-        grid : List[int]
+        launch_args : List[int]
             The grid size of the kernel. A list of one to three expressions, representing the number
             of
             "blockIdx.x", "blockIdx.y", and "blockIdx.z" respectively.
@@ -45,6 +49,7 @@ class TritonKernel(BaseKernel):
         ptx = triton_kernel.asm["ptx"]
         assert kernel_metadata.num_ctas == 1, "Cluster is not supported"
         num_warps = kernel_metadata.num_warps
+        grid = launch_args
         launch_param_tags = ["threadIdx.x"] + ["blockIdx.x", "blockIdx.y", "blockIdx.z"][
             : len(grid)
         ]
