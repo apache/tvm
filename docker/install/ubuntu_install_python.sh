5298b1298a8bb9166ef99dedef9979f2719c2416
Masahiro Hiramori <mhg00g13@gmail.com>
2024-10-02 22:29:48 +0900

    [CI] Upgrade CI (#17425)
    * upgrade ci

--- docker/install/ubuntu_install_python.sh
+++ docker/install/ubuntu_install_python.sh
@@ -33,10 +33,13 @@ if [ "$#" -lt 1 ]; then
 fi
 PYTHON_VERSION=$1
 
-if [ "${PYTHON_VERSION}" != "3.7" ] && [ "${PYTHON_VERSION}" != "3.8" ]; then
-    echo "Only 3.7 and 3.8 versions are supported in this script."
-    exit -1
-fi
+case "$PYTHON_VERSION" in
+    3.7|3.8|3.9) ;;
+    *)
+        echo "Only 3.7, 3.8, and 3.9 versions are supported in this script."
+        exit -1
+        ;;
+esac
 
 apt-get update
 
@@ -47,22 +50,23 @@ apt-install-and-clear -y \
 apt-install-and-clear -y software-properties-common
 
 release=$(lsb_release -sc)
-if [ "${release}" == "bionic" ]; then
-    if [ "${PYTHON_VERSION}" == "3.8" ]; then
-        add-apt-repository -y ppa:deadsnakes/ppa
-    fi
-elif [ "${release}" == "focal" ]; then
-    if [ "${PYTHON_VERSION}" == "3.7" ]; then
-        add-apt-repository -y ppa:deadsnakes/ppa
-    fi
-elif [ "${release}" == "jammy" ]; then
-    if [ "${PYTHON_VERSION}" == "3.8" ]; then
-        add-apt-repository -y ppa:deadsnakes/ppa
-    fi
-else
-    echo "Don't know which version of python to install for lsb-release ${release}"
-    exit 2
-fi
+case "${release}" in
+    bionic)
+        [ "${PYTHON_VERSION}" == "3.8" ] && add-apt-repository -y ppa:deadsnakes/ppa
+        ;;
+    focal)
+        [ "${PYTHON_VERSION}" == "3.7" ] && add-apt-repository -y ppa:deadsnakes/ppa
+        ;;
+    jammy)
+        if [ "${PYTHON_VERSION}" == "3.8" ] || [ "${PYTHON_VERSION}" == "3.9" ]; then
+            add-apt-repository -y ppa:deadsnakes/ppa
+        fi
+        ;;
+    *)
+        echo "Don't know which version of python to install for lsb-release ${release}"
+        exit 2
+        ;;
+esac
 
 # Install python and pip. Don't modify this to add Python package dependencies,
 # instead modify install_python_package.sh
@@ -84,7 +88,6 @@ export PYTHONNOUSERSITE=1
 venv_dir="$(python3 -c "import os.path;print(os.path.dirname(\"${TVM_VENV}\"))")"
 mkdir -p "${venv_dir}"
 python3 -mvenv "${TVM_VENV}"
-. "${TVM_VENV}/bin/activate"
 
 # NOTE: Only in python3.9 does venv guarantee it creates the python3.X binary.
 # This is needed so that cmake's find_package(PythonInterp) works inside the venv.
@@ -95,15 +98,15 @@ fi
 
 # Update pip to match version used to produce requirements-hashed.txt. This step
 # is necessary so that pip's dependency solver is recent.
-pip_spec=$(cat /install/python/bootstrap/lockfiles/constraints-${PYTHON_VERSION}.txt | grep 'pip==')
-pip3 install -U --require-hashes -r <(echo "${pip_spec}") \
+pip_spec=$(tac /install/python/bootstrap/lockfiles/constraints-${PYTHON_VERSION}.txt | grep -m 1 'pip==')
+${TVM_VENV}/bin/pip install -U --require-hashes -r <(echo "${pip_spec}") \
      -c /install/python/bootstrap/lockfiles/constraints-${PYTHON_VERSION}.txt
 
 # Python configuration
-pip3 config set global.no-cache-dir true  # Never cache packages
+${TVM_VENV}/bin/pip config set global.no-cache-dir true  # Never cache packages
 
 # Now install the remaining base packages.
-pip3 install \
+${TVM_VENV}/bin/pip install \
      --require-hashes \
      -r /install/python/bootstrap/lockfiles/constraints-${PYTHON_VERSION}.txt
 
@@ -114,7 +117,6 @@ setfacl -R -m group:tvm-venv:rwx "${TVM_VENV}"
 
 # Prevent further use of pip3 via the system.
 # There may be multiple (i.e. from python3-pip apt package and pip3 install -U).
-deactivate
 while [ "$(which pip3)" != "" ]; do
     rm "$(which pip3)"
 done
