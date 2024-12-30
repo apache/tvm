154973302a4b80643bc171ff5d968782a3d6ce36
Siva <quic_sivb@quicinc.com>
2024-12-30 22:19:16 +0530

    [RELAX][PASS] Annotate Custom Scope layout pass for Adreno GPU
    Texture scope annotation is handled by
    - Layout conversion from 4D to 5D with convert_layout pass
    - Legalization of ops with Adreno specific legalization
    - FuseOps & FuseTIR
    - Now, over the fused TIR annotate the scopes by hint_on_device
    - RealizeVDevice will take care of injecting to_device as needed.
    - Also, introduced SpecializeTIRParams to update the fused
    TIR the prim function buffer var map with new scope information.
    
    Changes in FuseOps and FuseTIR are to forward op attr and op pattern info.
    This info is used for Texture specific scoping decisions.

--- python/tvm/dlight/adreno/__init__.py
+++ python/tvm/dlight/adreno/__init__.py
@@ -14,23 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Legalize high-level operator calls in Relax functions to call_tir."""
-from . import binary
-from . import ccl
-from . import create
-from . import datatype
-from . import distributed
-from . import grad
-from . import image
-from . import index
-from . import inspect_op
-from . import linear_algebra
-from . import manipulate
-from . import nn
-from . import qdq
-from . import search
-from . import statistical
-from . import unary
-
-# Device specific legalizations
-from . import adreno
+"""
+Adreno schedule rules.
+"""
+from .convolution import Conv2d
