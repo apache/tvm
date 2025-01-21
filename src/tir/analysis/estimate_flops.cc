7d156474d4011dab5bc39467bdf965aff645afea
Patrik Persson <patrik.persson@inceptron.io>
2025-01-21 11:29:50 +0100

    added a visit to the AttrStmtNode body

--- src/tir/analysis/estimate_flops.cc
+++ src/tir/analysis/estimate_flops.cc
@@ -138,7 +138,7 @@ class FlopEstimator : private ExprFunctor<TResult(const PrimExpr& n)>,
   }
 
   TResult VisitExpr_(const BufferLoadNode* op) override { return TResult(); }
-  TResult VisitStmt_(const AttrStmtNode* op) override { return TResult(); }
+  TResult VisitStmt_(const AttrStmtNode* op) override { return VisitStmt(op->body); }
   TResult VisitStmt_(const BufferStoreNode* store) override { return VisitExpr(store->value); }
   TResult VisitStmt_(const BlockRealizeNode* block) override {
     return VisitStmt(block->block->body);
