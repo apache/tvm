2cbd35d5d1d0ff18459af2584241db50fe5e84e7
Tianqi Chen <tqchen@users.noreply.github.com>
2025-02-12 12:43:09 -0500

    [PYTHON] Build cython by default (#17637)
    Historically we have been maintaining cython and ctypes for FFI.
    One reason was cython was not that available then.
    Cython have quite good availability now and also enjoys great
    perf benefit. This PR changes the CMake to make cython as required
    dependency of the python package and build cython by default.
    It also removes ctypes testcases in favor of all cython tests.
    
    If things go well, we can consider remove ctypes and focus on
    cython support onward.

--- tests/scripts/task_python_hexagon.sh
+++ tests/scripts/task_python_hexagon.sh
@@ -26,7 +26,10 @@ if [ $# -ge 1 ] && [[ "$1" = "--device" ]]; then
 fi
 
 source tests/scripts/setup-pytest-env.sh
-make cython3
+
+# setup cython
+cd python; python3 setup.py build_ext --inplace; cd ..
+
 
 if [[ "${device_serial}" == "simulator" ]]; then
     export TVM_TRACKER_PORT=9190
@@ -49,9 +52,9 @@ fi
 
 export ANDROID_SERIAL_NUMBER=${device_serial}
 if [ "${device_serial}" == "simulator" ]; then
-    run_pytest ctypes python-contrib-hexagon tests/python/contrib/test_hexagon
+    run_pytest python-contrib-hexagon tests/python/contrib/test_hexagon
 else
-    run_pytest ctypes python-contrib-hexagon tests/python/contrib/test_hexagon -n=$num_of_devices
+    run_pytest python-contrib-hexagon tests/python/contrib/test_hexagon -n=$num_of_devices
 fi
 
 if [[ "${device_serial}" == "simulator" ]]; then
