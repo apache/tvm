6bde56ceb789af6a3834dca40b2b7f824047ab4d
tqchen <tqchenml@gmail.com>
2025-02-13 14:57:25 -0500

    Cleanup and focus on the integrated version

--- include/tvm/ffi/ffi.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TVM_FFI_H_
-#define TVM_FFI_H_
-#include "./core/core.h"
-#include "./ext/dict.h"
-#include "./ext/error.h"
-#include "./ext/func.h"
-#include "./ext/list.h"
-#include "./ext/str.h"
-#endif // TVM_FFI_H_
