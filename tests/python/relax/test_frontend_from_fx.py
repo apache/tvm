0002db219f20de140ff32a67109d15019d6f72c3
Shushi Hong <820958424@qq.com>
2025-02-24 22:46:56 +0800

    Update test_frontend_from_fx.py

--- tests/python/relax/test_frontend_from_fx.py
+++ tests/python/relax/test_frontend_from_fx.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import operator
 import pytest
 import torch
 import torch.nn.functional as F
