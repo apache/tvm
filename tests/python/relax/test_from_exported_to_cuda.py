0778b3b83bdec98c7ef69ccde20e65751fbeac42
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-08 16:17:51 -0500

    test from exported to cuda

--- tests/python/relax/test_from_exported_to_cuda.py
+++ tests/python/relax/test_from_exported_to_cuda.py
@@ -16,26 +16,26 @@
 # under the License.
 
 import tvm
-import tvm.testing
 from tvm import relax
+import tvm.testing
 import numpy as np
 import torch
+from torch.export import export
+from tvm.relax.frontend.torch import from_exported_program
 from torch.nn import Softmax
 
 
-def test_softmax_non_last_dim_large_tensor():
+def assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module):
     """
-    Tests ingesting a PyTorch exported model that uses softmax on a large 
-    tensor, with the softmax dimension not being that last dimension, and 
-    running it with CUDA.
+    This util ensures that a torch module can successfully be exported to TVM 
+    using torch.export and that the resuling IR program gives the same result 
+    as PyTorch when ran on CUDA.
     """
-    torch_model = Softmax(dim=2).eval()
-    raw_data = np.random.rand(10, 4, 32, 16384).astype("float32")
     torch_data = torch.from_numpy(raw_data)
     example_args = (torch_data,)
 
     with torch.no_grad():
-        exported_program = export(torch_model, example_args)
+        exported_program = export(torch_module, example_args)
         mod_from_torch = from_exported_program(
             exported_program, keep_params_as_input=True
         )
@@ -52,11 +52,22 @@ def test_softmax_non_last_dim_large_tensor():
     gpu_params = [tvm.nd.array(p, dev) for p in tvm_params["main"]]
     gpu_out = vm["main"](gpu_data, *gpu_params)
 
-    pytorch_out = torch_model(torch_data).detach().numpy()
+    pytorch_out = torch_module(torch_data).detach().numpy()
     actual = gpu_out[0].numpy()
     desired = pytorch_out
     np.testing.assert_allclose(actual=actual, desired=desired, rtol=1e-5, 
                                atol=1e-5) 
+
+
+def test_softmax_non_last_dim_large_tensor():
+    """
+    Tests ingesting a PyTorch exported model that uses softmax on a large 
+    tensor, with the softmax dimension not being that last dimension, and 
+    running it with CUDA.
+    """
+    torch_module = Softmax(dim=2).eval()
+    raw_data = np.random.rand(10, 4, 32, 16384).astype("float32")
+    assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module)
 
 
 if __name__ == "__main__":
