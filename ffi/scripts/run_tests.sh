28960330b0bcbe57701a5033a4d7a67392d3aff3
tqchen <tianqi.tchen@gmail.com>
2025-03-09 09:27:02 -0400

    CMake links ok

--- ffi/scripts/run_tests.sh
+++ ffi/scripts/run_tests.sh
@@ -5,7 +5,6 @@ BUILD_TYPE=RelWithDebugInfo
 
 rm -rf build/CMakeFiles build/CMakeCache.txt
 cmake -G Ninja -S . -B build  -DTVM_FFI_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-  -DTVM_FFI_BUILD_REGISTRY=ON \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
 cmake --build build --parallel 16 --clean-first --config ${BUILD_TYPE} --target tvm_ffi_tests
 GTEST_COLOR=1 ctest -V -C ${BUILD_TYPE} --test-dir build --output-on-failure
