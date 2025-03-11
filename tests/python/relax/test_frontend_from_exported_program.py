6d876a658c6d5f8da9850f4b6a7dc9aa9af07583
Shushi Hong <820958424@qq.com>
2025-03-11 23:31:37 +0800

    Update test_frontend_from_exported_program.py

--- tests/python/relax/test_frontend_from_exported_program.py
+++ tests/python/relax/test_frontend_from_exported_program.py
@@ -144,7 +144,7 @@ def test_extended_unary_ops():
     class expected_celu:
         @R.function
         def main(
-                input_1: R.Tensor((1, 3, 10, 10), dtype="float32")
+            input_1: R.Tensor((1, 3, 10, 10), dtype="float32")
         ) -> R.Tuple(R.Tensor((1, 3, 10, 10), dtype="float32")):
             with R.dataflow():
                 lv: R.Tensor((1, 3, 10, 10), dtype="float32") = R.exp(input_1)
@@ -154,10 +154,8 @@ def test_extended_unary_ops():
                 lv_scaled: R.Tensor((1, 3, 10, 10), dtype="float32") = R.multiply(R.const(1.0, "float32"), lv_min)
                 lv_relu_x: R.Tensor((1, 3, 10, 10), dtype="float32") = R.nn.relu(input_1)
                 lv_celu: R.Tensor((1, 3, 10, 10), dtype="float32") = R.add(lv_scaled, lv_relu_x)
-
                 gv: R.Tuple(R.Tensor((1, 3, 10, 10), dtype="float32")) = (lv_celu,)
                 R.output(gv)
-
             return gv
 
     verify_model(Celu1(), example_args, {}, expected_celu)
@@ -228,26 +226,18 @@ def test_extended_unary_ops():
     class expected_elu:
         @R.function
         def main(
-                input_1: R.Tensor((1, 3, 10, 10), dtype="float32")
+            input_1: R.Tensor((1, 3, 10, 10), dtype="float32")
         ) -> R.Tuple(R.Tensor((1, 3, 10, 10), dtype="float32")):
             # block 0
             with R.dataflow():
                 lv_exp: R.Tensor((1, 3, 10, 10), dtype="float32") = R.exp(input_1)
-                lv_one_minus_exp: R.Tensor((1, 3, 10, 10), dtype="float32") = R.subtract(
-                    R.const(1.0, dtype="float32"), lv_exp
-                )
+                lv_one_minus_exp: R.Tensor((1, 3, 10, 10), dtype="float32") = R.subtract(R.const(1.0, dtype="float32"), lv_exp)
                 lv_relu_one_minus_exp: R.Tensor((1, 3, 10, 10), dtype="float32") = R.nn.relu(lv_one_minus_exp)
-
-                lv_scaled: R.Tensor((1, 3, 10, 10), dtype="float32") = R.multiply(
-                    R.const(-1.0, dtype="float32"), lv_relu_one_minus_exp
-                )
-
+                lv_scaled: R.Tensor((1, 3, 10, 10), dtype="float32") = R.multiply(R.const(-1.0, dtype="float32"), lv_relu_one_minus_exp)
                 lv_relu_x: R.Tensor((1, 3, 10, 10), dtype="float32") = R.nn.relu(input_1)
                 lv_elu: R.Tensor((1, 3, 10, 10), dtype="float32") = R.add(lv_scaled, lv_relu_x)
-
                 gv: R.Tuple(R.Tensor((1, 3, 10, 10), dtype="float32")) = (lv_elu,)
                 R.output(gv)
-
             return gv
 
     verify_model(Elu(), example_args, {}, expected_elu)
