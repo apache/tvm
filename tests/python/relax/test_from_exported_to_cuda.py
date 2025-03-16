9ee0672319c19334fbd5de9c8fdaa0eeff6ed6d7
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-16 11:56:27 -0400

    cleanup

--- tests/python/relax/test_from_exported_to_cuda.py
+++ tests/python/relax/test_from_exported_to_cuda.py
@@ -15,11 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# TODO remove
-import sys
-sys.path.append('/ssd1/htalendr/tvm/python')
-
-
 import tvm
 from tvm import relax
 import tvm.testing
@@ -289,28 +284,25 @@ def test_linalg_vector_norm(target, dev):
 @tvm.testing.parametrize_targets("cuda")
 
 def test_batch_norm(target, dev):
-
-    # TODO no momentum
+    # No momentum, eval
     raw_data = np.random.randn(8,8,4,4).astype(np.float32)
-    # raw_data = np.array([[[[10.0]],[[20.0]]]]).astype(np.float32)
     torch_module0 = nn.BatchNorm2d(8, eps=1e-02, momentum=0.0, 
                                    affine=False, track_running_stats=True, 
-                                   device=None, dtype=None).eval() # TODO make test pass without .eval() !
+                                   device=None, dtype=None).eval()
     assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module0, target, dev)
-    # TODO correct output above should be [9.95, 19.9] https://chatgpt.com/c/67cf1bc1-1934-8006-9b22-8166c46ee1bc
 
-    # TODO with momentum
-    # raw_data = np.random.randn(1,4,2,2).astype(np.float32)
-    # torch_module0 = nn.BatchNorm2d(4, eps=1e-05, momentum=0.0, 
-    #                                affine=False, track_running_stats=True, 
-    #                                device=None, dtype=None).eval()
-    # assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module0, target, dev)
+    # With momentum, eval
+    raw_data = np.random.randn(1,4,2,2).astype(np.float32)
+    torch_module0 = nn.BatchNorm2d(4, eps=1e-05, momentum=0.0, 
+                                   affine=False, track_running_stats=True, 
+                                   device=None, dtype=None).eval()
+    assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module0, target, dev)
 
 
-    # TODO uncomment this one with simpler arguments?
-    # raw_data = np.random.randn(4,2,2,2).astype(np.float32)
-    # torch_module0 = nn.BatchNorm2d(2).eval()
-    # assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module0, target, dev)
+    # Default args, eval
+    raw_data = np.random.randn(4,2,2,2).astype(np.float32)
+    torch_module0 = nn.BatchNorm2d(2).eval()
+    assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module0, target, dev)
 
 
 if __name__ == "__main__":
