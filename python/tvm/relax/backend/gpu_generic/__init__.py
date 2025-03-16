a3e8e7a546a662c8cd6df111c9942c32873402e2
Siyuan Feng <hzfengsy@sjtu.edu.cn>
2025-03-16 14:31:14 +0800

    remove tir folder

--- python/tvm/relax/backend/gpu_generic/__init__.py
+++ python/tvm/relax/backend/gpu_generic/__init__.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 """The Relax Metal backend compilation pipeline and other passes."""
+from .cumsum import gpu_2d_continuous_cumsum
 from .pipeline import (
     dataflow_lower_passes,
     finalize_passes,
@@ -22,4 +23,4 @@ from .pipeline import (
     legalize_passes,
     library_dispatch_passes,
 )
-from . import tir
+from .sampling import generic_get_sample_index, gpu_multinomial_from_uniform
