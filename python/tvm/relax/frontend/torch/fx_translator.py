8ad9bffb1124c1553c1bc9c3e4ae5d8a84826afa
Shushi Hong <820958424@qq.com>
2025-03-18 05:13:05 +0800

    [Relax][PyTorch] Add support for lerp, select and clone ops (#17760)
    This PR supports Pytorch `lerp`, `select` and `clone` ops for Relax

--- python/tvm/relax/frontend/torch/fx_translator.py
+++ python/tvm/relax/frontend/torch/fx_translator.py
@@ -376,6 +376,16 @@ class TorchFXImporter(BaseFXGraphImporter):
 
         return self._max_pool2d_impl(x, kernel_size, stride, padding, dilation, ceil_mode)
 
+    ########## Linear Interpolation ##########
+
+    def _lerp(self, node: fx.Node) -> relax.Var:
+        start = self.env[node.args[0]]
+        end = self.env[node.args[1]]
+        weight = self.env[node.args[2]]
+        return self.block_builder.emit(
+            relax.op.add(start, relax.op.multiply(weight, relax.op.subtract(end, start)))
+        )
+
     ########## Manipulation ##########
 
     def _chunk(self, node: fx.Node) -> relax.Var:
@@ -413,6 +423,12 @@ class TorchFXImporter(BaseFXGraphImporter):
         x = self.env[node.args[0]]
         shape = self.shape_of(x)
         return relax.const(reduce(lambda x, y: x * y, [s.value for s in shape]), "int32")
+
+    def _select(self, node: fx.Node) -> relax.Var:
+        x = self.env[node.args[0]]
+        dim = node.args[1]
+        index = relax.const(node.args[2], "int64")
+        return self.block_builder.emit(relax.op.take(x, index, dim))
 
     def _size(self, node: fx.Node) -> relax.Expr:
         x = self.env[node.args[0]]
@@ -737,6 +753,8 @@ class TorchFXImporter(BaseFXGraphImporter):
             "scaled_dot_product_attention": self._scaled_dot_product_attention,
             "stochastic_depth": lambda node: self.env[node.args[0]],
             "unbind": self._unbind,
+            # linear interpolation
+            "lerp": self._lerp,
             # statistical
             "mean": self._mean,
             "sum": self._sum,
@@ -759,6 +777,7 @@ class TorchFXImporter(BaseFXGraphImporter):
             "repeat": self._repeat,
             "reshape": self._reshape,
             "scatter": self._scatter,
+            "select": self._select,
             "size": self._size,
             "split": self._split,
             "squeeze": self._squeeze,
@@ -772,6 +791,7 @@ class TorchFXImporter(BaseFXGraphImporter):
             "view": self._reshape,
             # tensor creation
             "arange": self._arange,
+            "clone": lambda node: self.env[node.args[0]],
             "empty": self._empty,
             "empty_like": self._empty_like,
             "fill_": self._inplace_fill,
