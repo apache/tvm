e99d659d1753c7e4abb7c39698983717fc5cd159
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-23 15:25:07 -0400

    legalize tests

--- python/tvm/topi/nn/batch_norm.py
+++ python/tvm/topi/nn/batch_norm.py
@@ -111,11 +111,18 @@ def batch_norm(
     shape = [1] * len(data.shape)
     shape[axis] = data.shape[axis]
 
+
+    data_mean = topi.sum(data, axis=reduce_axes) / shape_prod
+    data_mean_rs = topi.reshape(data_mean, shape)
+    data_var = (
+        topi.sum((data - data_mean_rs) * (data - data_mean_rs), axis=reduce_axes) / shape_prod
+    )
+    data_var_rs = topi.reshape(data_var, shape)
+
+
     if training:
         moving_mean_rs = topi.reshape(moving_mean, shape)
         moving_var_rs = topi.reshape(moving_var, shape)
-
-
 
         out = (data - moving_mean_rs) / topi.math.sqrt(moving_var_rs + epsilon)
 
@@ -123,12 +130,6 @@ def batch_norm(
         reduce_axes = list(range(len(data.shape)))
         reduce_axes.remove(axis)
         shape_prod = reduce(lambda x, y: x * y, [data.shape[ax] for ax in reduce_axes], 1)
-        data_mean = topi.sum(data, axis=reduce_axes) / shape_prod
-        data_mean_rs = topi.reshape(data_mean, shape)
-        data_var = (
-            topi.sum((data - data_mean_rs) * (data - data_mean_rs), axis=reduce_axes) / shape_prod
-        )
-        data_var_rs = topi.reshape(data_var, shape)
 
         print("data is", data)
         print("data_mean_rs is", data_mean_rs)
