a9de5efcfce9637c449af392292f701512bda128
Hugo Latendresse <hugolatendresse@gmail.com>
2025-03-23 20:26:14 -0400

    all tests pass

--- tests/python/relax/test_transform_legalize_ops_nn.py
+++ tests/python/relax/test_transform_legalize_ops_nn.py
@@ -14,8 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import sys
-sys.path.append('/ssd1/htalendr/tvm/python') # Refer to local TVM build
 
 import pytest
 
