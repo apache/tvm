5dd9f3fed61bf4f02a82543c3c8d2ac25b2b7046
Hugo Latendresse <hugolatendresse@gmail.com>
2025-04-02 13:59:32 -0400

    cleanup + unit test

--- tests/python/relax/test_from_exported_to_cuda.py
+++ tests/python/relax/test_from_exported_to_cuda.py
@@ -466,31 +466,38 @@ def test_chunk_too_many(target, dev):
     torch_module = ChunkModel(chunks=chunks, dim=dim).eval()
     assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module, target, dev)
 
-@tvm.testing.parametrize_targets("cuda")
-def test_arange_default(target, dev):
-    raw_data = np.random.rand(5).astype("int64")
 
-    class ArangeModel(nn.Module):
+@tvm.testing.parametrize_targets("cuda")
+def test_arange(target, dev):
+    # arange.default
+    raw_data = np.array([0, 0, 0, 0, 0])
+
+    class ArangeDefaultModel(nn.Module):
         def forward(self, x):
             return x + torch.arange(5)
 
-    torch_module = ArangeModel().eval()
-
+    torch_module = ArangeDefaultModel().eval()
     assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module, target, dev)
 
+    # arange.start
+    raw_data = np.array([0, 0, 0])
 
-# TODO
-# @tvm.testing.parametrize_targets("cuda")
-# def test_arange_start_step(target, dev):
-#     raw_data = np.random.rand(3).astype("int64")
+    class ArangeStartModel(nn.Module):
+        def forward(self, x):
+            return x + torch.arange(1, 4)
 
-#     class ArangeModel(nn.Module):
-#         def forward(self, x):
-#             return x + torch.arange(1, 2.5, 0.5)
+    torch_module = ArangeStartModel().eval()
+    assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module, target, dev)
 
-#     torch_module = ArangeModel().eval()
+    # arange.start_step
+    raw_data = np.array([0.0, 0.0, 0.0], dtype=np.float32)
 
-#     assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module, target, dev)
+    class ArangeStartStopModel(nn.Module):
+        def forward(self, x):
+            return x + torch.arange(1, 2.5, 0.5, dtype=torch.float32)
+
+    torch_module = ArangeStartStopModel().eval()
+    assert_torch_output_vs_tvm_from_exported_to_cuda(raw_data, torch_module, target, dev)
 
 
 @tvm.testing.parametrize_targets("cuda")
