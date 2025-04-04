acaa231b48443da09b4fc4e4dc9dfb5e13debf78
deivanayakisankaralingam <deiva@Deivanayaki.>
2025-04-04 07:59:37 +0000

    remove trailing spaces in softplus func

--- python/tvm/relax/transform/legalize_ops/nn.py
+++ python/tvm/relax/transform/legalize_ops/nn.py
@@ -534,9 +534,9 @@ def _nn_silu(bb: BlockBuilder, call: Call) -> Expr:
 @register_legalize("relax.nn.softplus")
 def _nn_softplus(bb: BlockBuilder, call: Call) -> Expr:
     return bb.call_te(
-        topi.nn.softplus, 
-        call.args[0], 
-        call.attrs.beta, 
+        topi.nn.softplus,
+        call.args[0],
+        call.attrs.beta,
         call.attrs.threshold,
     )
 
