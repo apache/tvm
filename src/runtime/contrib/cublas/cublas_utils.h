78b863a992360bbb939e7b62c4822b7b8b9b7443
Annanya <annanyapr@gmail.com>
2025-04-07 03:30:25 -0400

    Some formatting changes

--- src/runtime/contrib/cublas/cublas_utils.h
+++ src/runtime/contrib/cublas/cublas_utils.h
@@ -116,7 +116,7 @@ inline cudaDataType_t GetCudaDataType(DLDataType type) {
       case 64:
         return CUDA_R_64F;
     }
-  } else if (type.code == kDLBfloat){
+  } else if (type.code == kDLBfloat) {
     switch (type.bits) {
       case 16:
         return CUDA_R_16BF;
