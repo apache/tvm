57b20acb76f2c38a06111b159633e8dc4d449dd1
deivanayakisankaralingam <deiva@Deivanayaki.>
2025-04-10 10:19:26 +0000

    fix lint issue in pad file import statement

--- python/tvm/topi/nn/pad.py
+++ python/tvm/topi/nn/pad.py
@@ -14,15 +14,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Pad the data by constant value """
 from __future__ import absolute_import as _abs
 
 import tvm
 from tvm import te
+from tvm.tir import if_then_else
 
 from .. import tag
 from ..utils import equal_const_int
-from tvm.tir import if_then_else
 
 
 @tvm.te.tag_scope(tag=tag.INJECTIVE + ",pad")
