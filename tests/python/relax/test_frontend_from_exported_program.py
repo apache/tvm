be7afe667c5750ec1488e40aff736cb7c44d3537
deivanayakisankaralingam <deiva@Deivanayaki.>
2025-04-10 09:37:02 +0000

    remove trailing whitespaces

--- tests/python/relax/test_frontend_from_exported_program.py
+++ tests/python/relax/test_frontend_from_exported_program.py
@@ -1945,9 +1945,9 @@ def test_pad():
         ) -> R.Tuple(R.Tensor((1, 3, 14, 12), dtype="float32")):
             with R.dataflow():
                 lv: R.Tensor((1, 3, 14, 12), dtype="float32") = R.nn.pad(
-                    x, 
-                    pad_value = R.const(0.0, "float32"), 
-                    pad_width=[0, 0, 0, 0, 2, 2, 1, 1], 
+                    x,
+                    pad_value = R.const(0.0, "float32"),
+                    pad_width=[0, 0, 0, 0, 2, 2, 1, 1],
                     pad_mode="constant",
                 )
                 gv: R.Tuple(R.Tensor((1, 3, 14, 12), dtype="float32")) = (lv,)
