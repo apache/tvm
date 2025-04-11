d4476b542876d566cf4f100a2111d5725697c219
deivanayakisankaralingam <deiva@Deivanayaki.>
2025-04-11 11:04:24 +0000

    fix lint issues by formatting the code

--- python/tvm/relax/frontend/torch/exported_program_translator.py
+++ python/tvm/relax/frontend/torch/exported_program_translator.py
@@ -513,8 +513,14 @@ class ExportedProgramImporter(BaseFXGraphImporter):
             with self.block_builder.dataflow():
 
                 # Find all the missing function types
-                missing_func_types = list({node.target.__name__ for node in nodes
-                if node.op == "call_function" and node.target.__name__ not in self.convert_map})
+                missing_func_types = list(
+                    {
+                        node.target.__name__
+                        for node in nodes
+                        if node.op == "call_function"
+                        and node.target.__name__ not in self.convert_map
+                    }
+                )
                 assert not missing_func_types, f"Unsupported function types {missing_func_types}"
 
                 # Translate the model.
--- python/tvm/relax/frontend/torch/fx_translator.py
+++ python/tvm/relax/frontend/torch/fx_translator.py
@@ -885,9 +885,15 @@ class TorchFXImporter(BaseFXGraphImporter):
             output = None
             with self.block_builder.dataflow():
 
-               # Find all the missing function types
-                missing_func_types = list({node.target.__name__ for node in graph.nodes
-                if node.op == "call_function" and node.target.__name__ not in self.convert_map})
+                # Find all the missing function types
+                missing_func_types = list(
+                    {
+                        node.target.__name__
+                        for node in nodes
+                        if node.op == "call_function"
+                        and node.target.__name__ not in self.convert_map
+                    }
+                )
                 assert not missing_func_types, f"Unsupported function types {missing_func_types}"
 
                 # Translate model parameters.
