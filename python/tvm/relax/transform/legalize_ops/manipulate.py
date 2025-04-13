a4116621882bac510f16a5d3a58930ca118b5685
Hugo Latendresse <hugolatendresse@gmail.com>
2025-04-13 16:43:02 -0400

    able to call relax.collapse_sum_like_TWO

--- python/tvm/relax/transform/legalize_ops/manipulate.py
+++ python/tvm/relax/transform/legalize_ops/manipulate.py
@@ -45,14 +45,14 @@ def _reshape(
 
 register_legalize("relax.broadcast_to", _reshape(topi.broadcast_to, "broadcast_to"))
 register_legalize("relax.reshape", _reshape(topi.reshape, "reshape"))
-register_legalize( # TODO how about _TWO ????
+register_legalize( 
     "relax.collapse_sum_like",
     _reshape(topi.collapse_sum, "collapse_sum", is_collapse_sum_like=True),
 )
-# register_legalize( # TODO how about _TWO ????
-#     "relax.collapse_sum_like_TWO",
-#     _reshape(topi.collapse_sum, "collapse_sum", is_collapse_sum_like=True),
-# )
+register_legalize( # TODO try to call a topi directly? 
+    "relax.collapse_sum_like_TWO",
+    _reshape(topi.collapse_sum, "collapse_sum", is_collapse_sum_like=True),
+)
 
 register_legalize("relax.collapse_sum_to", _reshape(topi.collapse_sum, "collapse_sum"))
 
