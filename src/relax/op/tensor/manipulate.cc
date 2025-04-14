8adbc6180da6a342daea17fa08a6e550ec58f99a
Hugo Latendresse <hugolatendresse@gmail.com>
2025-04-14 02:56:07 -0400

    concat2 still passes

--- src/relax/op/tensor/manipulate.cc
+++ src/relax/op/tensor/manipulate.cc
@@ -544,7 +544,7 @@ TVM_REGISTER_OP("relax.concat2")
     .add_argument("tensors", "Tuple of Tensors", "The input list of tensors.")
     .set_attr<FInferStructInfo>("FInferStructInfo", InferStructInfoConcat2)
     .set_attr<FRelaxInferLayout>("FRelaxInferLayout", InferLayoutConcat2)
-    .set_attr<TMixedPrecisionPolicy>("TMixedPrecisionPolicy", MixedPrecisionPolicyKind::kFollow)
+    // .set_attr<TMixedPrecisionPolicy>("TMixedPrecisionPolicy", MixedPrecisionPolicyKind::kFollow)
     .set_attr<Bool>("FPurity", Bool(true));
 
 
