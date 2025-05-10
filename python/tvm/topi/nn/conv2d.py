fd4fe6ea39e2bfce34f04844d45ce94f1eabe9dd
Qingchao Shen <qingchaoshen@outlook.com>
2025-05-10 12:10:51 +0800

    Update conv2d.py

--- python/tvm/topi/nn/conv2d.py
+++ python/tvm/topi/nn/conv2d.py
@@ -745,7 +745,9 @@ def conv(
     ]
     for out_dim in out_dimensions:
         if isinstance(out_dim, int) and out_dim <= 0:
-            raise ValueError(f"Invalid conv parameters: lead to negative output shape {out_dimensions}. ")
+            raise ValueError(
+                f"Invalid conv parameters: lead to negative output shape {out_dimensions}. "
+            )
     # compute graph
     pad_before = list(np.array([0, 0] + pad_begin)[data_permutation_from])
     pad_after = list(np.array([0, 0] + pad_end)[data_permutation_from])
