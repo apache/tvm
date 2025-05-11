5233178a41a3c2cd4365950dcc4fa5e3a1fec19a
tqchen <tianqi.tchen@gmail.com>
2025-05-10 20:21:26 -0400

    [FFI][JVM] Upgrade tvm4j to latest FFI
    This PR updates TVM4J to use the latest FFI

--- jvm/core/src/main/java/org/apache/tvm/TVMObject.java
+++ jvm/core/src/main/java/org/apache/tvm/TVMObject.java
@@ -17,20 +17,25 @@
 
 package org.apache.tvm;
 
-// Type code used in API calls
-public enum ArgTypeCode {
-  INT(0), UINT(1), FLOAT(2), HANDLE(3), NULL(4), TVM_TYPE(5),
-  DLDEVICE(6), ARRAY_HANDLE(7), NODE_HANDLE(8), MODULE_HANDLE(9),
-  FUNC_HANDLE(10), STR(11), BYTES(12), NDARRAY_CONTAINER(13);
+/**
+ * Base class of all TVM Objects.
+ */
+public class TVMObject extends TVMValue {
+  protected long handle;
+  public final int typeIndex;
 
-  public final int id;
-
-  private ArgTypeCode(int id) {
-    this.id = id;
+  public TVMObject(long handle, int typeIndex) {
+    this.handle = handle;
+    this.typeIndex = typeIndex;
   }
 
-  @Override
-  public String toString() {
-    return String.valueOf(id);
+  public void release() {
+    Base.checkCall(Base._LIB.tvmFFIObjectFree(this.handle));
+    this.handle = 0;
+  }
+
+  @Override protected void finalize() throws Throwable {
+    release();
+    super.finalize();
   }
 }
