0faf419989a93572787896b19d01fa1bd84eb7ce
Youngsik Yang <vacu9708@gmail.com>
2025-05-14 01:45:14 +0900

    [Doc][Relax][MetaSchedule] Fix errors in the e2e_opt_model.py tutorial
    This PR addresses the following issues:
    - Memory binding error("Did you forget to bind?")
    This error occurs when the compiler attempts to use operations that
    are not properly bound to GPU memory, due to occasionally generated
    invalid GPU schedules that result in memory misalignment.
    	- **Fix**: Added GPU code verification to SpaceGenerator()
    	that filters out schedules involving memory misalignment.
    - <class 'tvm.ir.container.Array'> has no attribute numpy
    This error occurs because vm["main"]() returns an Array of NDArray
    	- **Fix**: Extracted the NDArray out of the returned Array
    	before calling .numpy().

--- python/tvm/meta_schedule/tune_context.py
+++ python/tvm/meta_schedule/tune_context.py
@@ -28,6 +28,7 @@ from tvm._ffi import register_object, register_func
 from tvm.runtime import Object
 from tvm.target import Target
 from tvm.tir import PrimFunc, Schedule
+from tvm.meta_schedule import postproc
 
 from . import _ffi_api
 from .logging import Logger, get_logger, get_logging_func
@@ -119,7 +120,12 @@ class TuneContext(Object):
                 target = Target(target)
         if space_generator is not None:
             if not isinstance(space_generator, SpaceGenerator):
-                space_generator = SpaceGenerator.create(space_generator)
+                if "gpu" in target.keys:
+                    space_generator = SpaceGenerator.create(
+                        space_generator, postprocs=[postproc.VerifyGPUCode()]
+                    )
+                else:
+                    space_generator = SpaceGenerator.create(space_generator)
         if search_strategy is not None:
             if not isinstance(search_strategy, SearchStrategy):
                 search_strategy = SearchStrategy.create(search_strategy)
