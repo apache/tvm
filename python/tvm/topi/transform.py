2bc069979a00d6bb47b11ad1c6bb4ebec7b420b8
Qingchao Shen <qingchaoshen@outlook.com>
2025-05-23 13:37:50 +0800

    Update transform.py

--- python/tvm/topi/transform.py
+++ python/tvm/topi/transform.py
@@ -96,9 +96,7 @@ def expand_like(a, shape_like, axis):
         axis_index = 0
         for i in range(0, len(idxs)):
             if i not in real_axis:
-                dim = tvm.tir.if_then_else(
-                    a.shape[len(indices)] !=1, idxs[i], 0
-                )
+                dim = tvm.tir.if_then_else(a.shape[len(indices)] != 1, idxs[i], 0)
                 indices.append(dim)
                 axis_index += 1
         return a(*indices)
