bf24cab232c288eff48c0780819a070adcf7ba34
Ruihang Lai <ruihangl@cs.cmu.edu>
2025-05-31 07:53:07 -0400

    [Python] Fix library lookup path for pip installed packages (#18026)

--- python/tvm/libinfo.py
+++ python/tvm/libinfo.py
@@ -65,7 +65,7 @@ def get_dll_directories():
         dll_path.extend(split_env_var("PATH", ";"))
 
     # Pip lib directory
-    dll_path.append(os.path.join(ffi_dir, ".."))
+    dll_path.append(ffi_dir)
     # Default cmake build directory
     dll_path.append(os.path.join(source_dir, "build"))
     dll_path.append(os.path.join(source_dir, "build", "Release"))
