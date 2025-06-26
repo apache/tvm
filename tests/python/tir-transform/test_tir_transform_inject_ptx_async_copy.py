5a6a36a966ee5e0cd105d8a5cd1360e3b038def1
Kathryn-cat <jinqic@alumni.cmu.edu>
2025-06-26 16:57:19 -0400

    fix

--- tests/python/tir-transform/test_tir_transform_inject_ptx_async_copy.py
+++ tests/python/tir-transform/test_tir_transform_inject_ptx_async_copy.py
@@ -254,20 +254,10 @@ cast_smem_ptr_to_int(const void* const smem_ptr)
 #else
 #define TVM_ENABLE_L2_PREFETCH 0
 #endif
-
-#ifdef _WIN32
-  using uint = unsigned int;
-  using uchar = unsigned char;
-  using ushort = unsigned short;
-  using int64_t = long long;
-  using uint64_t = unsigned long long;
-#else
-  #define uint unsigned int
-  #define uchar unsigned char
-  #define ushort unsigned short
-  #define int64_t long long
-  #define uint64_t unsigned long long
-#endif
+#include <cstdint>
+using uint = unsigned int;
+using uchar = unsigned char;
+using ushort = unsigned short;
 extern "C" __global__ void __launch_bounds__(16) main_kernel(float* __restrict__ A, float* __restrict__ B, float* __restrict__ C);
 extern "C" __global__ void __launch_bounds__(16) main_kernel(float* __restrict__ A, float* __restrict__ B, float* __restrict__ C) {
   __shared__ float A_shared[64];
