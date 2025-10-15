c66302c9c5bdd34de8a6d3f70d1744b991699bf7
tqchen <tianqi.tchen@gmail.com>
2025-10-15 10:45:38 -0400

    Update

--- python/tvm/testing/_ffi_api.py
+++ python/tvm/testing/_ffi_api.py
@@ -16,6 +16,8 @@
 # under the License.
 """FFI APIs for tvm.testing"""
 import tvm_ffi
+# must import testing before init_ffi_api
+import tvm_ffi.testing
 
 
 tvm_ffi.init_ffi_api("testing", __name__)
