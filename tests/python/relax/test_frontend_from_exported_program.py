f76135c2f327c38bce66f5ee7a188a27999af0aa
tlopex <820958424@qq.com>
2025-11-07 16:18:35 -0500

    f2

--- tests/python/relax/test_frontend_from_exported_program.py
+++ tests/python/relax/test_frontend_from_exported_program.py
@@ -3715,9 +3715,15 @@ def test_interpolate():
             return gv
 
     example_args = (torch.randn(1, 3, 112, 112, dtype=torch.float32),)
-    verify_model(InterpolateBilinear(), example_args, {}, expected_bilinear, run_ep_decomposition=True)
-    verify_model(InterpolateNearest(), example_args, {}, expected_nearest, run_ep_decomposition=True)
-    verify_model(InterpolateBicubic(), example_args, {}, expected_bicubic, run_ep_decomposition=True)
+    verify_model(
+        InterpolateBilinear(), example_args, {}, expected_bilinear, run_ep_decomposition=True
+    )
+    verify_model(
+        InterpolateNearest(), example_args, {}, expected_nearest, run_ep_decomposition=True
+    )
+    verify_model(
+        InterpolateBicubic(), example_args, {}, expected_bicubic, run_ep_decomposition=True
+    )
 
 
 def test_mean():
