a4aaa5b984765d8c26fcdd0d1d06eb1a1e8242e6
Guan-Ming (Wesley) Chiu <105915352+guan404ming@users.noreply.github.com>
2025-11-19 12:24:31 +0800

    Enhance python linting scripts to support revision-based checks

--- docker/lint.sh
+++ docker/lint.sh
@@ -55,10 +55,18 @@ function run_lint_step() {
             cmd=( tests/lint/cpplint.sh )
             ;;
         flake8)
-            cmd=( tests/lint/flake8.sh )
+            if [ $inplace_fix -eq 0 ]; then
+                cmd=( tests/lint/flake8.sh )
+            else
+                cmd=( tests/lint/flake8.sh --rev origin/main )
+            fi
             ;;
         pylint)
-            cmd=( tests/lint/pylint.sh )
+            if [ $inplace_fix -eq 0 ]; then
+                cmd=( tests/lint/pylint.sh )
+            else
+                cmd=( tests/lint/pylint.sh --rev origin/main )
+            fi
             ;;
         python_format)
             if [ $inplace_fix -eq 0 ]; then
