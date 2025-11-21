1723ff98f21240f0e0dbbdbcfa8402526a5552c4
Guan-Ming (Wesley) Chiu <105915352+guan404ming@users.noreply.github.com>
2025-11-21 18:46:10 +0800

    Update file patterns for specific linting hooks

--- docker/lint.sh
+++ docker/lint.sh
@@ -90,7 +90,11 @@ function run_lint_step() {
     shift
 
     if [ $validate_only -eq 0 ]; then
-        run_docker -it "ci_lint" "${cmd[@]}"
+        if [ -t 0 ]; then
+            run_docker -it "ci_lint" "${cmd[@]}"
+        else
+            run_docker "ci_lint" "${cmd[@]}"
+        fi
     fi
 }
 
