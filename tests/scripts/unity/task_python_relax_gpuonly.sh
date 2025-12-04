766f198e163779f68a825baf6adcd31c28abf9be
Siva <quic_sivb@quicinc.com>
2025-12-05 00:22:51 +0530

    Enable OpenCL target.

--- tests/scripts/unity/task_python_relax_gpuonly.sh
+++ tests/scripts/unity/task_python_relax_gpuonly.sh
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export TVM_TEST_TARGETS="llvm;cuda"
+export TVM_TEST_TARGETS="llvm;cuda;opencl"
 export PYTEST_ADDOPTS="-m gpu $PYTEST_ADDOPTS"
 export TVM_RELAY_TEST_TARGETS="cuda"
 export TVM_INTEGRATION_TESTSUITE_NAME=python-integration-gpu
