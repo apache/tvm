39967d799b065651590cb1060982fa0eb3b87743
Guan-Ming Chiu <guanmingchiu@gmail.com>
2026-01-06 01:32:37 +0800

    [Relax] Add FRelaxInferLayout for scatter_elements operator

--- tests/python/relax/test_transform_convert_layout.py
+++ tests/python/relax/test_transform_convert_layout.py
@@ -5327,5 +5327,60 @@ def test_conv2d_flip():
     verify(Input, Expected)
 
 
+def test_conv2d_scatter_elements():
+    @I.ir_module
+    class Input:
+        @R.function
+        def main(
+            x: R.Tensor((2, 3, 28, 28), "float32"),
+            w: R.Tensor((4, 3, 3, 3), "float32"),
+            indices: R.Tensor((2, 4, 26, 26), "int64"),
+        ) -> R.Tensor(None, "float32", ndim=4):
+            with R.dataflow():
+                data: R.Tensor((2, 4, 26, 26), "float32") = R.nn.conv2d(x, w, out_dtype="float32")
+                updates: R.Tensor((2, 4, 26, 26), "float32") = R.nn.relu(data)
+                gv = R.scatter_elements(data, indices, updates, axis=1)
+                R.output(gv)
+            return gv
+
+    @I.ir_module
+    class Expected:
+        @R.function
+        def main(
+            x: R.Tensor((2, 3, 28, 28), dtype="float32"),
+            w: R.Tensor((4, 3, 3, 3), dtype="float32"),
+            indices: R.Tensor((2, 4, 26, 26), dtype="int64"),
+        ) -> R.Tensor(None, dtype="float32", ndim=4):
+            with R.dataflow():
+                lv: R.Tensor((2, 28, 28, 3), dtype="float32") = R.permute_dims(x, axes=[0, 2, 3, 1])
+                lv1: R.Tensor((4, 3, 3, 3), dtype="float32") = R.permute_dims(w, axes=[0, 2, 3, 1])
+                data: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.conv2d(
+                    lv,
+                    lv1,
+                    strides=[1, 1],
+                    padding=[0, 0, 0, 0],
+                    dilation=[1, 1],
+                    groups=1,
+                    data_layout="NHWC",
+                    kernel_layout="OHWI",
+                    out_layout="NHWC",
+                    out_dtype="float32",
+                )
+                updates: R.Tensor((2, 26, 26, 4), dtype="float32") = R.nn.relu(data)
+                lv2: R.Tensor((2, 26, 26, 4), dtype="int64") = R.permute_dims(
+                    indices, axes=[0, 2, 3, 1]
+                )
+                lv3: R.Tensor((2, 26, 26, 4), dtype="float32") = R.scatter_elements(
+                    data, lv2, updates, axis=3, reduction="update"
+                )
+                gv: R.Tensor((2, 4, 26, 26), dtype="float32") = R.permute_dims(
+                    lv3, axes=[0, 3, 1, 2]
+                )
+                R.output(gv)
+            return gv
+
+    verify(Input, Expected)
+
+
 if __name__ == "__main__":
     tvm.testing.main()
