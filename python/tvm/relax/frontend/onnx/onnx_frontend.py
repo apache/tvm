9c7cff31e95dd385ce0943716d785f07bad853c5
locnd182644 <duyloca1@gmail.com>
2026-01-19 16:32:33 +0700

    [Relax][Onnx] Support Multi Input Ops with Multidirectional Broadcasting
    - Compute target shape for Multidirectional Broadcasting
    - Workflow: broadcast_to -> stack -> reduce ops

--- python/tvm/relax/frontend/onnx/onnx_frontend.py
+++ python/tvm/relax/frontend/onnx/onnx_frontend.py
@@ -1660,6 +1660,22 @@ class Sqrt(OnnxOpConverter):
         return relax.op.sqrt(inputs[0])
 
 
+def compute_broadcast_shape(shape_a, shape_b):
+    """Compute target shape for Multidirectional Broadcasting"""
+    rank = max(len(shape_a), len(shape_b))
+
+    a = (1,) * (rank - len(shape_a)) + tuple(shape_a)
+    b = (1,) * (rank - len(shape_b)) + tuple(shape_b)
+
+    target = []
+    for ai, bi in zip(a, b):
+        if ai == bi or ai == 1 or bi == 1:
+            target.append(max(ai, bi))
+        else:
+            raise ValueError(f"Cannot broadcast {ai} and {bi}")
+    return tuple(target)
+
+
 class MultiInputBase(OnnxOpConverter):
     """Converts an onnx MultiInputBase node into an equivalent Relax expression."""
 
@@ -1675,9 +1691,15 @@ class MultiInputBase(OnnxOpConverter):
             output = cls.numpy_op(*np_inputs)  # pylint: disable=not-callable
             return relax.const(output, output.dtype)
 
-        # Expand inputs, stack them, then perform minimum over the new axis.
-        inputs = [bb.normalize(relax.op.expand_dims(i, axis=0)) for i in inputs]
-        stacked_tensor = relax.op.concat(inputs, axis=0)
+        input_shapes = [inp.struct_info.shape for inp in inputs]
+        current_target_shape = input_shapes[0]
+        for next_shape in input_shapes[1:]:
+            current_target_shape = compute_broadcast_shape(current_target_shape, next_shape)
+        print("target shape", current_target_shape)
+
+        # broadcast_to, stack them, then perform minimum over the new axis.
+        inputs = [bb.normalize(relax.op.broadcast_to(i, current_target_shape)) for i in inputs]
+        stacked_tensor = bb.normalize(relax.op.stack(inputs, axis=0))
         return cls.relax_op(stacked_tensor, axis=0)  # pylint: disable=not-callable
 
 
