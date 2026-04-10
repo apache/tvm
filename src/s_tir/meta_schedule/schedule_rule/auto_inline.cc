0a530185977599d5dfc00f45583b1a66f91877ed
cchung100m <cchung100m@users.noreply.github.com>
2026-04-11 07:59:03 +0800

    Add root block is wrapped in a For loop into guard in CheckInline

--- src/s_tir/meta_schedule/schedule_rule/auto_inline.cc
+++ src/s_tir/meta_schedule/schedule_rule/auto_inline.cc
@@ -164,8 +164,14 @@ inline InlineType AutoInlineNode::CheckInline(const s_tir::Schedule& sch,
     if (ann.value() == "disable") return InlineType::kNoInline;
   }
   // Last cond: Check inline into the consumers or the spatial producer
-  // Skip if this is the root block (no parent scope), as it cannot be inlined
-  if (block_sref->parent == nullptr) {
+  // Skip if:
+  // - root block (no parent scope).
+  // - root block is wrapped in a For loop, then parent is non-null, but there is still no enclosing SBlockNode.
+  const StmtSRefNode* p = block_sref->parent;                   
+  for (; p != nullptr; p = p->parent) {                                                   
+    if (p->stmt->IsInstance<SBlockNode>()) break;
+  }                                                                                       
+  if (p == nullptr) {                                           
     return InlineType::kNoInline;
   }
   tirx::StmtSRef scope_block = s_tir::GetScopeRoot(sch->state(), block_sref,
