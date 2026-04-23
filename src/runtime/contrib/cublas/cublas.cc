067a2c72a314d9c05f6c44d080c0becbd855fe06
Kokoro2336 <2529677678@qq.com>
2026-04-23 19:58:04 +0800

    fix: version number.

--- src/runtime/contrib/cublas/cublas.cc
+++ src/runtime/contrib/cublas/cublas.cc
@@ -170,7 +170,7 @@ void CallCublasLt(cublasLtHandle_t hdl, cudaStream_t stream,
   } else if (TypeMatch(A->dtype, kDLInt, 8)) {
     ab_type = CUDA_R_8I;
   } else if (TypeMatch(A->dtype, DataType::TypeCode::kFloat8_e4m3fn, 8)) {
-#if CUDART_VERSION >= 11800
+#if CUDART_VERSION >= 11080
     TVM_FFI_ICHECK(TypeMatch(B->dtype, DataType::TypeCode::kFloat8_e4m3fn, 8));
     ab_type = CUDA_R_8F_E4M3;
 #else
@@ -205,7 +205,7 @@ void CallCublasLt(cublasLtHandle_t hdl, cudaStream_t stream,
                                                       &bias->data, sizeof(float*)));
   }
 
-#if CUDART_VERSION >= 11800
+#if CUDART_VERSION >= 11080
   if (scaleA != nullptr) {
     auto scaleA_data = static_cast<char*>(scaleA->data) + scaleA->byte_offset;
     CHECK_CUBLAS_ERROR(cublasLtMatmulDescSetAttribute(op_desc, CUBLASLT_MATMUL_DESC_A_SCALE_POINTER,
