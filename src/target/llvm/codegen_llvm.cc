169eafa190a0a82156ec76e1d583a7ce2967e059
actink <actink@163.com>
2026-05-19 21:30:50 +0800

    [TIR] Fixup max/min with input containing NAN
    fixed #19579

--- src/target/llvm/codegen_llvm.cc
+++ src/target/llvm/codegen_llvm.cc
@@ -1598,13 +1598,21 @@ llvm::Value* CodeGenLLVM::VisitExpr_(const ModNode* op) {
 llvm::Value* CodeGenLLVM::VisitExpr_(const MinNode* op) {
   llvm::Value* a = MakeValue(op->a);
   llvm::Value* b = MakeValue(op->b);
-  return builder_->CreateSelect(CreateLT(op->a.dtype(), a, b), a, b);
+  if (op->a.dtype().is_int() || op->a.dtype().is_uint()) {
+    return builder_->CreateSelect(CreateLT(op->a.dtype(), a, b), a, b);
+  } else {
+    return builder_->CreateMinimum(a, b);
+  }
 }
 
 llvm::Value* CodeGenLLVM::VisitExpr_(const MaxNode* op) {
   llvm::Value* a = MakeValue(op->a);
   llvm::Value* b = MakeValue(op->b);
-  return builder_->CreateSelect(CreateGT(op->a.dtype(), a, b), a, b);
+  if (op->a.dtype().is_int() || op->a.dtype().is_uint()) {
+    return builder_->CreateSelect(CreateGT(op->a.dtype(), a, b), a, b);
+  } else {
+    return builder_->CreateMaximum(a, b);
+  }
 }
 
 llvm::Value* CodeGenLLVM::VisitExpr_(const EQNode* op) {
