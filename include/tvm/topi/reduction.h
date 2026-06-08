5ee9d3b83b27f13d385c50971716d2e4f97f4bac
flashmouse <flashmosue2012@gmail.com>
2026-06-08 17:51:12 +0800

    fix issue-18676

--- include/tvm/topi/reduction.h
+++ include/tvm/topi/reduction.h
@@ -184,7 +184,9 @@ inline Tensor DoCommReduce(const Tensor& data, FReduce func,
 inline Tensor CommReduce(const Tensor& data, const ffi::Optional<ffi::Array<int64_t>>& axis,
                          FReduce func, bool keepdims, bool atleast1d) {
   auto ndim = data->shape.size();
-  TVM_FFI_ICHECK_NE(ndim, 0) << "Cannot reduce a 0 dim Tensor";
+  if (ndim == 0) {
+    return topi::identity(data, data->op->name + "_red", kCommReduce);
+  }
   auto real_axis = GetRealAxis(static_cast<int>(ndim), axis);
   auto target_shape = MakeReduceTargetShape(real_axis, data, keepdims, atleast1d);
   return DoCommReduce(data, func, target_shape, real_axis,
