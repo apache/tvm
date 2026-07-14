d020e47d059bca882185d8516f25b0c1978438fd
Tianqi Chen <tqchen@users.noreply.github.com>
2026-07-14 18:23:25 +0000

    Preserve dynamic shape authorities in TFLite

--- python/tvm/relax/frontend/tflite/tflite_frontend.py
+++ python/tvm/relax/frontend/tflite/tflite_frontend.py
@@ -6780,7 +6780,7 @@ class OperatorConverter:
         shape_tensor = input_tensors[1]
         if self.has_expr(shape_tensor.tensor_idx):
             shape_expr = self.get_expr(shape_tensor.tensor_idx)
-            shape = self.bb.emit(relax.op.tensor_to_shape(shape_expr))
+            shape = self.bb.emit_output(relax.op.tensor_to_shape(shape_expr))
         else:
             shape = to_int_list(self.get_tensor_value(shape_tensor))
         return relax.op.broadcast_to(data, shape)
