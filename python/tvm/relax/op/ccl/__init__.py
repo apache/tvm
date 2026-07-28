9dee0276de8d3a792411dae0ea1d0829bf8edd8b
yuyi <yuyi@hpclab>
2026-07-28 19:17:21 +0800

    [Relax][Op] Add gather_to_worker0

--- python/tvm/relax/op/ccl/__init__.py
+++ python/tvm/relax/op/ccl/__init__.py
@@ -17,4 +17,4 @@
 # under the License.
 """CCL related operators."""
 
-from .ccl import allgather, allreduce, broadcast_from_worker0, scatter_from_worker0
+from .ccl import allgather, allreduce, broadcast_from_worker0, scatter_from_worker0, gather_to_worker0
