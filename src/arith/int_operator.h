95aee0b5bef9d7d4b9d02365a5d3613b7cef6352
Tianqi Chen <tqchen@users.noreply.github.com>
2026-09-17 14:33:23 +0000

    [Arith] Use native GCD for fitting BigInt operands

--- src/arith/int_operator.h
+++ src/arith/int_operator.h
@@ -158,6 +158,11 @@ inline int64_t ZeroAwareGCD(int64_t a, int64_t b) {
 inline ffi::BigInt ZeroAwareGCD(ffi::BigInt a, ffi::BigInt b) {
   if (a < 0) a = -a;
   if (b < 0) b = -b;
+  auto a_int64 = a.as<int64_t>();
+  auto b_int64 = b.as<int64_t>();
+  if (a_int64.has_value() && b_int64.has_value()) {
+    return ZeroAwareGCD(*a_int64, *b_int64);
+  }
   if (a < b) std::swap(a, b);
   if (b == 0) return a;
   // perform GCD (greatest common divisor)
