30410bdd2f051a25e525ccc0feb07b786cf21f9d
Tianqi Chen <tqchen@users.noreply.github.com>
2026-09-25 12:42:39 +0000

    Fill generated AST locations once before compilation

--- python/tvm/script/parser/transpile.py
+++ python/tvm/script/parser/transpile.py
@@ -2511,4 +2511,4 @@ class IRBuilderTranspiler(ast.NodeTransformer):
                 root, result, python_functions, check_well_formed=check_well_formed
             )
         )
-        return ast.fix_missing_locations(ast.Module(translated, [])), result
+        return ast.Module(translated, []), result
